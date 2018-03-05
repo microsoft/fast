21c5b5665a30bdbe7cb7e92abb06cdf8119c1c56
Nick Rice <nicholas.rice119@gmail.com>
2018-03-05 14:47:55 -0800

    adding clean script

---
+++ packages/fast-react-components-base/build/clean.js
@@ -0,0 +1,28 @@
+/**
+ * Utility for cleaning directories.
+ * Usage: node build/clean.js %path%
+ */
+const rimraf = require("rimraf");
+const path = require("path");
+const argv = require("yargs").argv;
+
+/**
+ * All paths passed to the clean script
+ */
+const paths = argv._;
+
+/**
+ * Function to remove a given path
+ */
+function cleanPath(path) {
+    rimraf(path, () => {
+        console.log(`${path} cleaned`);
+    })
+}
+
+/**
+ * Clean all paths
+ */
+if (Array.isArray(paths)) {
+    paths.forEach(cleanPath);
+}
