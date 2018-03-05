c4a2acb0566381c5174a92e31b26201b76be44b5
Nicholas Rice <nicholas.rice119@gmail.com>
2018-03-05 15:43:58 -0800

    feat: adds fast-react-component-base directory (#10)
    * ignore www directories
    
    * adding boilerplate files
    
    * adds aditional ignores
    
    * fixes lint errors
    
    * adding tslinting
    
    * adding webpack and app
    
    * adding clean script
    
    * adding clean to build script
    
    * using tsx
    
    * correcting gitignore
    
    * removing unused dependnecy
    
    * adding peer dependencies

---
+++ packages/fast-react-components-base/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const appDir = path.resolve(__dirname, "./app");
+const outDir = path.resolve(__dirname, "./www");
+
+module.exports = {
+    entry: path.resolve(appDir, "index.tsx"),
+    output: {
+        path: outDir,
+        filename: "[name].js"
+    },
+    mode: process.env.NODE_ENV || "development",
+    module: {
+        rules: [
+            {
+                test: /.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin({
+            tslint: path.resolve(__dirname, "../../tslint.json")
+        }),
+        new HtmlWebpackPlugin({
+            contentBase: outDir,
+        })
+    ],
+    devServer: {
+        port: 7000,
+        compress: false,
+        open: true,
+        overlay: true 
+    }
+}
--- packages/fast-react-jss-manager/src/jss.ts
+++ packages/fast-react-jss-manager/src/jss.ts
@@ -1,8 +1,8 @@
-import { create, SheetsManager } from "jss";
+import JSS, { create, SheetsManager } from "jss";
 import presets from "jss-preset-default";
 
-const jss = create(presets);
-const stylesheetManager = new SheetsManager();
+const jss: JSS = create(presets);
+const stylesheetManager: SheetsManager = new SheetsManager();
 
 export default jss;
 export { stylesheetManager };
