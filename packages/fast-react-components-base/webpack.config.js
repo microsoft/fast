663988c43a54d2560f0a5091f3cbff4586885381
Nick Rice <nicholas.rice119@gmail.com>
2018-03-05 14:07:07 -0800

    adding webpack and app

--- packages/fast-react-components-base/webpack.config.js
+++ packages/fast-react-components-base/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const appDir = path.resolve(__dirname, "./app");
 const outDir = path.resolve(__dirname, "./www");
@@ -8,5 +10,35 @@ module.exports = {
     output: {
         path: outDir,
         filename: "[name].js"
+    },
+    mode: process.env.NODE_ENV || "development",
+    module: {
+        rules: [
+            {
+                test: /.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin({
+            tslint: path.resolve(__dirname, "../../tslint.json")
+        }),
+        new HtmlWebpackPlugin({
+            contentBase: outDir,
+        })
+    ],
+    devServer: {
+        port: 7000,
+        compress: false,
+        open: true,
+        overlay: true 
     }
 }
