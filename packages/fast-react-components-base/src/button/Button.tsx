a7b0897ed3a74b618dc6745c5addf73c1d5dea12
Nick Rice <nicholas.rice119@gmail.com>
2018-03-05 18:23:05 -0800

    stubs in button

---
+++ packages/fast-react-components-base/src/button/Button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Foundation from "../foundation";
+import IButtonProps from "./Button.props";
+
+class Button extends Foundation<any, any> {
+
+}
+
+export default Button;
+export { IButtonProps };
---
+++ packages/fast-react-components-base/src/button/index.ts
@@ -0,0 +1,5 @@
+import Button from "./Button";
+
+export * from "./Button";
+export * from "./Button.props";
+export default Button;
