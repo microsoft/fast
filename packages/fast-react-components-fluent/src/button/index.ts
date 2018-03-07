8b9afb32c6dc77d5fe687ed33349a38322f398df
JASON FALK <jasfalk@JASONs-MacBook-Pro.local>
2018-03-07 10:58:51 -0800

    stubs in button styling

--- packages/fast-react-components-fluent/src/button/index.ts
+++ packages/fast-react-components-fluent/src/button/index.ts
@@ -2,15 +2,97 @@ import * as React from "react";
 import { Button, IButtonProps, IButtonManagedClasses } from "@microsoft/fast-react-components-base";
 import manageJss, { ComponentStyles } from "@microsoft/fast-react-jss-manager";
 import {IFluentDesignSystem} from "../design-system";
+function applyBackplate(): any {
+    return {
+        backgroundColor: (config: IFluentDesignSystem): string => {
+            return config.controlBackplateColorSecondary;
+        },
+        borderRadius: (config: IFluentDesignSystem): string => {
+            return config.controlBackplateRadius + "px";
+        },
+        border: (config: IFluentDesignSystem): string => {
+            return config.controlBorderSize + "px " + config.controlBorderStyle + " " + config.controlBorderColor;
+        },
+        lineHeight: (config: IFluentDesignSystem): string => {
+            // The formula below vertically adjusts the text to compensate for the border.
+            return config.controlHeight - (config.controlBorderSize * 2) + "px";
+        },
+        color: (config: IFluentDesignSystem): string => {
+            return config.controlBackplateTextColor;
+        }
+    };
+}
 
 const styles: ComponentStyles<IButtonManagedClasses, IFluentDesignSystem> = {
     button: {
-        color: (config: IFluentDesignSystem): string => {
-            return config.foregroundColor;
+// Controls: all
+        "height": (config: IFluentDesignSystem): string => {
+            // Control height should be based on a function that sets the proper limits such as 20, 24, 28, 32, 36, 40, and 44.
+            return config.controlHeight + "px";
         },
-        backgroundColor: (config: IFluentDesignSystem): string => {
-            return config.brandColor;
-        }
+        "font-size": (config: IFluentDesignSystem): string => {
+            return config.controlTextSize + "px";
+        },
+        "transition": (config: IFluentDesignSystem): string => {
+            return config.controlBackplateTransition;
+        },
+        "display": "inline-block",
+        "outline": "none",
+        "overflow": "hidden",
+        "whiteSpace": "nowrap",
+        // TODO: Add a way to change the margin.
+        "marginTop": "12px",
+// Controls: with backplates
+        ...applyBackplate(),
+        // "backgroundColor": (config: IFluentDesignSystem): string => {
+        //     return config.controlBackplateColorSecondary;
+        // },
+        // "borderRadius": (config: IFluentDesignSystem): string => {
+        //     return config.color + "px";
+        // },
+        // "border": (config: IFluentDesignSystem): string => {
+        //     return config.controlBorderSize + "px " + config.controlBorderStyle + " " + config.controlBorderColor;
+        // },
+        // "lineHeight": (config: IFluentDesignSystem): string => {
+        //     // The formula below vertically adjusts the text to compensate for the border.
+        //     return config.controlHeight - (config.controlBorderSize * 2) + "px";
+        // },
+        // "color": (config: IFluentDesignSystem): string => {
+        //     return config.controlBackplateTextColor;
+        // },
+// Control: Controls with variable content.
+        "padding": (config: IFluentDesignSystem): string => {
+            return "0px " + config.controlTextPaddingHorizontal + "px";
+        },
+        "textAlign": "center",
+        // Reset: Can remove when it is part of the reset.
+        "boxSizing": "border-box",
+        "&:hover": {
+ // Controls: with backplates on hover
+           // TODO: Colors for states like Hover, Focus, and Active should be generated from a function.
+            background: "rgba(0,0,0,.3)",
+            // We should create a function for the fluent elevation system.
+            boxShadow: "0 4px 9px rgba(0, 0, 0, 0.15), 0 0.75px 2.25px rgba(0, 0, 0, 0.08)",
+            transitionProperty: "box-shadow, transform",
+            transitionDuration: "600ms",
+            transitionTimingFunction: "cubic-bezier(0.16, 1, 0.29, 0.99)",       },
+         "&:focus": {
+ // Controls: with backplates on focus
+           // TODO: Colors for states like Hover, Focus, and Active should be generated from a function.
+            background: "rgba(0,0,0,.3)",
+            borderColor: "rgba(0,0,0,.4)",
+        },
+        "&:active": {
+// Controls: with backplates on active
+            // TODO: Colors for states like Hover, Focus, and Active should be generated from a function.
+            background: "rgba(0,0,0,.2)",
+            borderColor: "rgba(0,0,0,.1)",
+            // We should create a function for the fluent elevation system.
+            boxShadow: "0 2px 4.5px rgba(0, 0, 0, 0.15), 0 0.375px 1.125px rgba(0, 0, 0, 0.08)",
+            transitionProperty: "box-shadow, transform",
+            transitionDuration: "600ms",
+            transitionTimingFunction: "cubic-bezier(0.16, 1, 0.29, 0.99)",
+        },
     },
 };
 
--- packages/fast-react-components-fluent/src/design-system/index.ts
+++ packages/fast-react-components-fluent/src/design-system/index.ts
@@ -1,15 +1,49 @@
 export interface IFluentDesignSystem {
-    foregroundColor: string;
-    backgroundColor: string;
-    brandColor: string;
-    size: number;
+    controlBackplateColorPrimary: string;
+    controlBackplateColorSecondary: string;
+    controlBackplateTransition: string;
+    controlBackplateTextColor: string;
+    controlLinkTextColor: string;
+    controlTextSize: number;
+    controlTextPaddingHorizontal: number;
+    controlBackplateRadius: number;
+    controlBackplateZoom: number;
+    controlBorderSize: number;
+    controlBorderColor: string;
+    controlBorderStyle: string;
+    controlMarginTop: number;
+    controlHeight: number;
+    typographyColor: string;
+    typographySize: number;
+    systemAccentColor: string;
+    systemDensityModifier: number;
+    systemGridUnit: number;
+    controlCursorDisabled: string;
+    controlCursorActive: string;
 }
 
 const fluentDesignSystemDefaults: IFluentDesignSystem = {
-    foregroundColor: "#000",
-    backgroundColor: "#FFF",
-    brandColor: "#0078D4",
-    size: 1
+    controlBackplateColorPrimary: "#005fa7",
+    controlBackplateColorSecondary: "rgba(0,0,0,.2)",
+    controlBackplateTransition: "all .2s ease-in-out",
+    controlBackplateTextColor: "#000",
+    controlLinkTextColor: "#000",
+    controlTextSize: 14,
+    controlTextPaddingHorizontal: 16,
+    controlBackplateRadius: 2,
+    controlBackplateZoom: 2,
+    controlBorderSize: 2,
+    controlBorderColor: "transparent",
+    controlBorderStyle: "solid",
+    controlMarginTop: 12,
+    controlHeight: 44,
+    typographyColor: "#FFF",
+    typographySize: 15,
+    systemAccentColor: "#0078D4",
+    systemDensityModifier: 1,
+    systemGridUnit: 4,
+    controlCursorDisabled: "not-allowed",
+    controlCursorActive: "default"
 };
 
 export default fluentDesignSystemDefaults;
