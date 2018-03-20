a5c3c9f4ce96b803dced48b17c2a2ef524cc8f50
Nick Rice <nicholas.rice119@gmail.com>
2018-03-20 15:07:52 -0700

    fixing the last lint errors

--- packages/fast-animation/__tests__/index.ts
+++ packages/fast-animation/__tests__/index.ts
@@ -1,11 +1,12 @@
-import Animate from "../lib/animate";
+/* tslint:disable:no-string-literal */
+import Animate, { IAnimateOptions } from "../lib/animate";
 class AnimateMock extends Animate {}
 
 describe("Animate initilization", () => {
-    const fakeElement = document.createElement("div");
+    const fakeElement: HTMLElement = document.createElement("div");
 
     test("should register a single input element", () => {
-       expect(new AnimateMock(fakeElement).animationTarget).toBe(fakeElement);
+       expect(new AnimateMock(fakeElement)["animationTarget"]).toBe(fakeElement);
     });
 
     test("should correctly assign options", () => {
@@ -14,180 +15,201 @@ describe("Animate initilization", () => {
 });
 
 describe("getPropertiesToAnimate", () => {
-    const fakeElement = document.createElement("div");
+    const fakeElement: HTMLElement = document.createElement("div");
 
     test("should return an empty array when no options are set", () => {
-        expect(new AnimateMock(fakeElement).getPropertiesToAnimate()).toEqual([]);
+        expect(new AnimateMock(fakeElement)["getPropertiesToAnimate"]()).toEqual([]);
     });
 
     test('should return an array with a single index of "transform" when x is set', () => {
-        expect(new AnimateMock(fakeElement, { x: 20 }).getPropertiesToAnimate()).toEqual(["transform"]);
+        expect(new AnimateMock(fakeElement, { x: 20 })["getPropertiesToAnimate"]()).toEqual(["transform"]);
     });
 
     test('should return an array with a single index of "transform" when y is set', () => {
-        expect(new AnimateMock(fakeElement, { y: 20 }).getPropertiesToAnimate()).toEqual(["transform"]);
+        expect(new AnimateMock(fakeElement, { y: 20 })["getPropertiesToAnimate"]()).toEqual(["transform"]);
     });
 
     test('should return an array with a single index of "transform" when rotate is set', () => {
-        expect(new AnimateMock(fakeElement, { rotate: 20 }).getPropertiesToAnimate()).toEqual(["transform"]);
+        expect(new AnimateMock(fakeElement, { rotate: 20 })["getPropertiesToAnimate"]()).toEqual(["transform"]);
     });
 
     test('should return an array with a single index of "transform" when scale is set', () => {
-        expect(new AnimateMock(fakeElement, { scale: 20 }).getPropertiesToAnimate()).toEqual(["transform"]);
+        expect(new AnimateMock(fakeElement, { scale: 20 })["getPropertiesToAnimate"]()).toEqual(["transform"]);
     });
 
     test('should return an array with a single index of "transform" when scale is set', () => {
-        expect(new AnimateMock(fakeElement, { scale: [20, 10] }).getPropertiesToAnimate()).toEqual(["transform"]);
+        expect(new AnimateMock(fakeElement, { scale: [20, 10] })["getPropertiesToAnimate"]()).toEqual(["transform"]);
     });
 
     test('should return an array with a single index of "opacity" when opacity is set', () => {
-        expect(new AnimateMock(fakeElement, { opacity: 0 }).getPropertiesToAnimate()).toEqual(["opacity"]);
+        expect(new AnimateMock(fakeElement, { opacity: 0 })["getPropertiesToAnimate"]()).toEqual(["opacity"]);
     });
 
     test('should return an array with both "transform" and "opacity" when opacity and x are set', () => {
-        const properties = new AnimateMock(fakeElement, { x: 20, opacity: 0 }).getPropertiesToAnimate();
+        const properties: string[] = new AnimateMock(fakeElement, { x: 20, opacity: 0 })["getPropertiesToAnimate"]();
 
         expect(properties.includes("opacity")).toBe(true);
         expect(properties.includes("transform")).toBe(true);
     });
 
     test('should return an array with both "transform" and "opacity" when opacity and y are set', () => {
-        const properties = new AnimateMock(fakeElement, { y: 20, opacity: 0 }).getPropertiesToAnimate();
+        const properties: string[] = new AnimateMock(fakeElement, { y: 20, opacity: 0 })["getPropertiesToAnimate"]();
 
         expect(properties.includes("opacity")).toBe(true);
         expect(properties.includes("transform")).toBe(true);
     });
 
     test('should return an array with both "transform" and "opacity" when opacity and rotate are set', () => {
-        const properties = new AnimateMock(fakeElement, { rotate: 20, opacity: 0 }).getPropertiesToAnimate();
+        const properties: string[] = new AnimateMock(fakeElement, { rotate: 20, opacity: 0 })["getPropertiesToAnimate"]();
 
         expect(properties.includes("opacity")).toBe(true);
         expect(properties.includes("transform")).toBe(true);
     });
 
     test('should return an array with both "transform" and "opacity" when opacity and scale are set', () => {
-        const properties = new AnimateMock(fakeElement, { scale: 20, opacity: 0 }).getPropertiesToAnimate();
+        const properties: string[] = new AnimateMock(fakeElement, { scale: 20, opacity: 0 })["getPropertiesToAnimate"]();
 
         expect(properties.includes("opacity")).toBe(true);
         expect(properties.includes("transform")).toBe(true);
     });
 
     test('should return an array with "transform" when more than one transform properties are set', () => {
-        const properties = new AnimateMock(fakeElement, { scale: 20, rotate: 20, x: 20, y: 20 }).getPropertiesToAnimate();
+        const properties: string[] = new AnimateMock(fakeElement, { scale: 20, rotate: 20, x: 20, y: 20 })["getPropertiesToAnimate"]();
 
         expect(properties).toEqual(["transform"]);
     });
 });
 
 describe("formatTransformFunction", () => {
-    const animation = new AnimateMock(document.createElement("div"));
+    const animation: AnimateMock = new AnimateMock(document.createElement("div"));
 
     test("should correctly format the x config", () => {
-        expect(animation.formatTransformFunction("x", 20)).toBe("translateX(20px)");
+        expect(animation["formatTransformFunction"]("x", 20)).toBe("translateX(20px)");
     });
     test("should correctly format the y config", () => {
-        expect(animation.formatTransformFunction("y", 20)).toBe("translateY(20px)");
+        expect(animation["formatTransformFunction"]("y", 20)).toBe("translateY(20px)");
     });
     test("should correctly format the x config", () => {
-        expect(animation.formatTransformFunction("x", "20%")).toBe("translateX(20%)");
+        expect(animation["formatTransformFunction"]("x", "20%")).toBe("translateX(20%)");
     });
     test("should correctly format the y config", () => {
-        expect(animation.formatTransformFunction("y", "20%")).toBe("translateY(20%)");
+        expect(animation["formatTransformFunction"]("y", "20%")).toBe("translateY(20%)");
     });
     test("should correctly format the rotate config", () => {
-        expect(animation.formatTransformFunction("rotate", 20)).toBe("rotate(20deg)");
+        expect(animation["formatTransformFunction"]("rotate", 20)).toBe("rotate(20deg)");
     });
     test("should correctly format the scale config", () => {
-        expect(animation.formatTransformFunction("scale", 20)).toBe("scale(20)");
+        expect(animation["formatTransformFunction"]("scale", 20)).toBe("scale(20)");
     });
     test("should correctly format the scale config", () => {
-        expect(animation.formatTransformFunction("scale", [20, 10])).toBe("scale(20,10)");
+        expect(animation["formatTransformFunction"]("scale", [20, 10])).toBe("scale(20,10)");
     });
     test("should return empty string if function cannot be converted to a transform function", () => {
-        expect(animation.formatTransformFunction("foobar", 20)).toBe("");
+        expect(animation["formatTransformFunction"]("foobar", 20)).toBe("");
     });
 });
 
 describe("getOptionKeyframeValues", () => {
-    const fakeElement = document.createElement("div");
+    const fakeElement: HTMLElement = document.createElement("div");
 
     test("should correclty create opacity keyframe value", () => {
-        expect(new AnimateMock(fakeElement, { opacity: 0 }).getOptionKeyframeValues()).toEqual({ opacity: "0" });
+        expect(new AnimateMock(fakeElement, { opacity: 0 })["getOptionKeyframeValues"]()).toEqual({ opacity: "0" });
     });
 
     test("should correclty create x keyframe value", () => {
-        expect(new AnimateMock(fakeElement, { x: 20 }).getOptionKeyframeValues()).toEqual({ transform: "translateX(20px)" });
+        expect(new AnimateMock(fakeElement, { x: 20 })["getOptionKeyframeValues"]()).toEqual({ transform: "translateX(20px)" });
     });
 
     test("should correclty create opacity keyframe value", () => {
-        expect(new AnimateMock(fakeElement, { y: 20 }).getOptionKeyframeValues()).toEqual({ transform: "translateY(20px)" });
+        expect(new AnimateMock(fakeElement, { y: 20 })["getOptionKeyframeValues"]()).toEqual({ transform: "translateY(20px)" });
     });
 
     test("should correclty create x keyframe value", () => {
-        expect(new AnimateMock(fakeElement, { x: "20%" }).getOptionKeyframeValues()).toEqual({ transform: "translateX(20%)" });
+        expect(new AnimateMock(fakeElement, { x: "20%" })["getOptionKeyframeValues"]()).toEqual({ transform: "translateX(20%)" });
     });
 
     test("should correclty create opacity keyframe value", () => {
-        expect(new AnimateMock(fakeElement, { y: "20%" }).getOptionKeyframeValues()).toEqual({ transform: "translateY(20%)" });
+        expect(new AnimateMock(fakeElement, { y: "20%" })["getOptionKeyframeValues"]()).toEqual({ transform: "translateY(20%)" });
     });
 
     test("should correclty create opacity keyframe value", () => {
-        expect(new AnimateMock(fakeElement, { scale: 20 }).getOptionKeyframeValues()).toEqual({ transform: "scale(20)" });
+        expect(new AnimateMock(fakeElement, { scale: 20 })["getOptionKeyframeValues"]()).toEqual({ transform: "scale(20)" });
     });
 
     test("should correclty create opacity keyframe value", () => {
-        expect(new AnimateMock(fakeElement, { scale: [20, 10] }).getOptionKeyframeValues()).toEqual({ transform: "scale(20,10)" });
+        expect(new AnimateMock(fakeElement, { scale: [20, 10] })["getOptionKeyframeValues"]()).toEqual({ transform: "scale(20,10)" });
     });
 
     test("should correclty create opacity keyframe value", () => {
-        expect(new AnimateMock(fakeElement, { rotate: 20 }).getOptionKeyframeValues()).toEqual({ transform: "rotate(20deg)" });
+        expect(new AnimateMock(fakeElement, { rotate: 20 })["getOptionKeyframeValues"]()).toEqual({ transform: "rotate(20deg)" });
     });
 });
 
 describe("getOptionKeyframes", () => {
-    const fakeElement = document.createElement("div");
+    const fakeElement: HTMLElement = document.createElement("div");
 
     // All keyframes will have an initial property value of `undefined` because we rely on the native browsers 'getComputedStyle'
     // method to obtain initial values, which in a node environment return undefined.
     test("should correctly return keyframes with the x option", () => {
-        expect(new AnimateMock(fakeElement, { x: 20 }).getOptionKeyframes()).toEqual([{ transform: undefined }, { transform: "translateX(20px)" }]);
+        expect(new AnimateMock(fakeElement, { x: 20 })["getOptionKeyframes"]()).toEqual([
+            { transform: undefined },
+            { transform: "translateX(20px)" }
+        ]);
     });
 
     test("should correctly return keyframes with the y option", () => {
-        expect(new AnimateMock(fakeElement, { y: 20 }).getOptionKeyframes()).toEqual([{ transform: undefined }, { transform: "translateY(20px)" }]);
+        expect(new AnimateMock(fakeElement, { y: 20 })["getOptionKeyframes"]()).toEqual([
+            { transform: undefined },
+            { transform: "translateY(20px)" }
+        ]);
     });
 
     test("should correctly return keyframes with the scale option", () => {
-        expect(new AnimateMock(fakeElement, { scale: 20 }).getOptionKeyframes()).toEqual([{ transform: undefined }, { transform: "scale(20)" }]);
+        expect(new AnimateMock(fakeElement, { scale: 20 })["getOptionKeyframes"]()).toEqual([
+            { transform: undefined },
+            { transform: "scale(20)" }
+        ]);
     });
 
     test("should correctly return keyframes with the independent scale option", () => {
-        expect(new AnimateMock(fakeElement, { scale: [20, 10] }).getOptionKeyframes()).toEqual([{ transform: undefined }, { transform: "scale(20,10)" }]);
+        expect(new AnimateMock(fakeElement, { scale: [20, 10] })["getOptionKeyframes"]()).toEqual([
+            { transform: undefined },
+            { transform: "scale(20,10)" }
+        ]);
     });
 
     test("should correctly return keyframes with the rotate option", () => {
-        expect(new AnimateMock(fakeElement, { rotate: 20 }).getOptionKeyframes()).toEqual([{ transform: undefined }, { transform: "rotate(20deg)" }]);
+        expect(new AnimateMock(fakeElement, { rotate: 20 })["getOptionKeyframes"]()).toEqual([
+            { transform: undefined },
+            { transform: "rotate(20deg)" }
+        ]);
     });
 
     test("should correctly return keyframes with the opacity option", () => {
-        expect(new AnimateMock(fakeElement, { opacity: 1 }).getOptionKeyframes()).toEqual([{ opacity: "" }, { opacity: "1" }]);
+        expect(new AnimateMock(fakeElement, { opacity: 1 })["getOptionKeyframes"]()).toEqual([{ opacity: "" }, { opacity: "1" }]);
     });
 
     test("should correctly return keyframes with both an opacity and transform function", () => {
-        expect(new AnimateMock(fakeElement, { opacity: 1, scale: 2 }).getOptionKeyframes()).toEqual([{ opacity: "", scale: undefined }, { opacity: "1", transform: "scale(2)" }]);
+        expect(new AnimateMock(fakeElement, { opacity: 1, scale: 2 })["getOptionKeyframes"]()).toEqual([
+            { opacity: "", scale: undefined },
+            { opacity: "1", transform: "scale(2)" }
+        ]);
     });
 
     test("should correctly return keyframes with both two transform functions", () => {
-        expect(new AnimateMock(fakeElement, { x: 20, scale: 2 }).getOptionKeyframes()).toEqual([{ transform: undefined }, { transform: "translateX(20px) scale(2)" }]);
+        expect(new AnimateMock(fakeElement, { x: 20, scale: 2 })["getOptionKeyframes"]()).toEqual([
+            { transform: undefined },
+            { transform: "translateX(20px) scale(2)" }
+        ]);
     });
 });
 
 describe("sortOffsets", () => {
     test("should sort offset strings in ascending order", () => {
-        const setOne = ["0", "1"];
-        const setTwo = ["1", "0"];
-        const setThree = [".72", ".29", "1", ".1", "0"];
-        const sortOffsets = new AnimateMock(document.createElement("div")).sortOffsets;
+        const setOne: string[] = ["0", "1"];
+        const setTwo: string[] = ["1", "0"];
+        const setThree: string[] = [".72", ".29", "1", ".1", "0"];
+        const sortOffsets: (offsets: string[]) => string[] = new AnimateMock(document.createElement("div"))["sortOffsets"];
 
         expect(sortOffsets(setOne)).toEqual(["0", "1"]);
         expect(sortOffsets(setTwo)).toEqual(["0", "1"]);
@@ -196,10 +218,13 @@ describe("sortOffsets", () => {
 });
 
 describe("consolidateKeyframes", () => {
-    const fakeElement = document.createElement("div");
-    const expectedOne = [{offset: 0, opacity: "", transform: undefined}, {offset: 1, opacity: "0", transform: "translateX(20px) translateY(20px) rotate(20deg) scale(20)"}];
-    const sourceOne = { scale: 20, opacity: 0, rotate: 20, x: 20, y: 20 };
-    const customKeyframes = [
+    const fakeElement: HTMLElement = document.createElement("div");
+    const expectedOne: any[] = [
+        {offset: 0, opacity: "", transform: undefined},
+        {offset: 1, opacity: "0", transform: "translateX(20px) translateY(20px) rotate(20deg) scale(20)"}
+    ];
+    const sourceOne: IAnimateOptions = { scale: 20, opacity: 0, rotate: 20, x: 20, y: 20 };
+    const customKeyframes: AnimationKeyFrame[] = [
         { opacity: 0 },
         { opacity: 0.75, offset: 0.75 },
         { opacity: 1 }
@@ -209,16 +234,20 @@ describe("consolidateKeyframes", () => {
     });
 
     test("should correctly consolidate after addKeyframes is used", () => {
-        const mock = new AnimateMock(fakeElement);
+        const mock: AnimateMock = new AnimateMock(fakeElement);
         mock.addKeyframes(customKeyframes);
 
         expect(mock.keyframes).toEqual([{opacity: 0, offset: 0}, {opacity: 0.75, offset: 0.75 }, {opacity: 1, offset: 1}]);
     });
 
     test("should correclty consolidate both options and added keyframes", () => {
-        const mock = new AnimateMock(fakeElement, { scale: 20 });
+        const mock: AnimateMock = new AnimateMock(fakeElement, { scale: 20 });
         mock.addKeyframes(customKeyframes);
 
-        expect(mock.keyframes).toEqual([{offset: 0, opacity: 0, transform: undefined}, {offset: 0.75, opacity: 0.75}, {offset: 1, opacity: 1, transform: "scale(20)"}]);
+        expect(mock.keyframes).toEqual([
+            {offset: 0, opacity: 0, transform: undefined},
+            {offset: 0.75, opacity: 0.75},
+            {offset: 1, opacity: 1, transform: "scale(20)"}
+        ]);
     });
 });
--- packages/fast-animation/lib/animateFrom/index.ts
+++ packages/fast-animation/lib/animateFrom/index.ts
@@ -1,5 +1,5 @@
 import Animate, { AnimationMode } from "../animate";
 
 export default class extends Animate {
-    protected mode = AnimationMode.animateFrom;
+    protected mode: AnimationMode = AnimationMode.animateFrom;
 }
