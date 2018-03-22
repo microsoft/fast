a95db3f389030a76894856aab1176b7730087238
Nick Rice <nirice@microsoft.com>
2018-03-22 15:37:11 -0700

    fixes tslint errors

--- packages/fast-components-react-base/src/button/Button.tsx
+++ packages/fast-components-react-base/src/button/Button.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 import Foundation, { HandledProps } from "../foundation";
 import { IButtonProps, ButtonHTMLTags } from "./Button.props";
 import { IInjectedProps } from "@microsoft/fast-jss-manager-react";
-import { IButtonClassNameContract } from "@microsoft/fast-components-class-name-contracts"
+import { IButtonClassNameContract } from "@microsoft/fast-components-class-name-contracts";
 
 /* tslint:disable-next-line */
 class Button extends Foundation<IButtonProps & IInjectedProps<IButtonClassNameContract>,  React.AllHTMLAttributes<HTMLElement>, {}> {
