ed480559500d0c83ea774458da452edbe7615289
nicholasrice <nicholas.rice119@gmail.com>
2018-03-23 16:51:53 -0700

    add lint tasks to all projects

--- packages/fast-animation/webpack/webpack.common.config.js
+++ packages/fast-animation/webpack/webpack.common.config.js
@@ -24,13 +24,6 @@ const config = {
     },
     module: {
       rules: [
-            // {
-            //     test: /.jsx?$/,
-            //     use: [
-            //         { loader: 'babel-loader' }
-            //     ],
-            //     exclude: /node_modules/,
-            // },
             {
                 test: /\.tsx?$/,
                 exclude: /\.(test|spec)/,
