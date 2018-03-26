0382860a18c9a3de466fba7d42685000ebc87b52
Nick Rice <nirice@microsoft.com>
2018-03-26 09:20:38 -0700

    package fixes

---
+++ packages/fast-jss-manager/README.md
@@ -0,0 +1,2 @@
+# fast-jss-manager
+FAST jss-manager is provides common code used by implementations of a JSS Manager and other libraries using JSS stylesheets.
--- packages/fast-jss-manager/src/index.ts
+++ packages/fast-jss-manager/src/index.ts
@@ -1,13 +1,13 @@
 /**
  * Type definition for a function that resolves to a CSS property value. It optionally expects a config object.
- * @param Config - This describes the design-system configuration values that will be available to all
+ * @param T - This describes the design-system configuration values that will be available to all
  * property functions that resolve to a CSS value.
  */
 export type CSSRuleResolver<T> = (config?: T) => string;
 
 /**
  * Definition of a set of css properties for any given HTML class.
- * @param T - This describes the design-system configuration values that will be available to all
+ * @param Config - This describes the design-system configuration values that will be available to all
  * property functions that resolve to a CSS value.
  */
 export interface ICSSRules<T> {
@@ -21,6 +21,6 @@ export interface ICSSRules<T> {
  * @param Config - This describes the design-system configuration values that will be available to all
  * property functions that resolve to a CSS value.
  */
-export type ComponentStyles<Contract, Config> = {
-    [P in keyof Contract]: ICSSRules<Config>;
+export type ComponentStyles<T, C> = {
+    [P in keyof T]: ICSSRules<C>;
 };
