dcfce3076a497b3d60e0b1eac3d854a15c5acf73
Nick Rice <nirice@microsoft.com>
2018-03-26 15:43:44 -0700

    remove hyphenation

--- packages/fast-jss-manager/src/index.ts
+++ packages/fast-jss-manager/src/index.ts
@@ -1,13 +1,13 @@
 /**
  * Type definition for a function that resolves to a CSS property value. It optionally expects a config object.
- * @param T - This describes the design-system configuration values that will be available to all
+ * @param T - This describes the design system configuration values that will be available to all
  * property functions that resolve to a CSS value.
  */
 export type CSSRuleResolver<T> = (config?: T) => string;
 
 /**
  * Definition of a set of css properties for any given HTML class.
- * @param Config - This describes the design-system configuration values that will be available to all
+ * @param Config - This describes the design system configuration values that will be available to all
  * property functions that resolve to a CSS value.
  */
 export interface ICSSRules<T> {
@@ -18,7 +18,7 @@ export interface ICSSRules<T> {
  * Describes a JSS style object.
  * @param Contract - This is the stylesheet contract, which is an enumeration of all keys available on
  * the JSS style object.
- * @param Config - This describes the design-system configuration values that will be available to all
+ * @param Config - This describes the design system configuration values that will be available to all
  * property functions that resolve to a CSS value.
  */
 export type ComponentStyles<T, C> = {
