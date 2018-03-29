a845f10f499ffbfa79725147237929327852ff76
nicholasrice <nicholas.rice119@gmail.com>
2018-03-29 16:31:46 -0700

    migrate style code

---
+++ packages/fast-components-styles-msft/src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Export component styles
+ */
+import ButtonStyles from "./button";
+export {ButtonStyles};
+
+/**
+ * Export design system defaults and typings
+ */
+import DesignSystemDefaults from "./design-system";
+export * from "./design-system";
+export {DesignSystemDefaults};
+
+/**
+ * Export utilities
+ */
+export * from "./utilities/breakpoints";
+export * from "./utilities/fonts";
+export * from "./utilities/typography";
+export * from "./utilities/units";
---
+++ packages/fast-components-styles-msft/src/utilities/breakpoints.ts
@@ -0,0 +1,20 @@
+export interface IBreakpoints {
+    vp1: number;
+    vp2: number;
+    vp3: number;
+    vp4: number;
+    vp5: number;
+    vp6: number;
+}
+
+/**
+ * These are the minimum widths for the breakpoint
+ */
+export const breakpoints: IBreakpoints = {
+    vp1: 0,
+    vp2: 540,
+    vp3: 768,
+    vp4: 1084,
+    vp5: 1400,
+    vp6: 1779
+};
