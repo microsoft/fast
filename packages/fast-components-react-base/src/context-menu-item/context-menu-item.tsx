ed9e6aa75362e63c9e0db1f326e00a0b7a3e8b26
nicholasrice <nicholas.rice119@gmail.com>
2018-04-04 16:58:39 -0700

    adding context-menu-item foundation

---
+++ packages/fast-components-react-base/src/context-menu-item/context-menu-item.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import Foundation, {HandledProps} from "../foundation";
+import {
+    ContextMenuItemProps,
+    IContextMenuItemHandledProps,
+    IContextMenuItemManagedClasses,
+    IContextMenuItemUnhandledProps,
+} from "./context-menu-item.props";
+
+export enum ContextMenuItemRole {
+    menuitem = "menuitem",
+    menuitemcheckbox = "menuitemcheckbox",
+    menuitemradio = "menuitemradio"
+}
+
+/* tslint:disable-next-line */
+class ContextMenuItem extends Foundation<IContextMenuItemHandledProps & IContextMenuItemManagedClasses, IContextMenuItemUnhandledProps, {}> {
+    public static defaultProps: IContextMenuItemHandledProps = {
+        role: ContextMenuItemRole.menuitem
+    };
+
+    protected handledProps: HandledProps<IContextMenuItemHandledProps & IContextMenuItemManagedClasses> = {
+        managedClasses: void 0,
+        children: void 0
+    };
+
+    /**
+     * Renders the component
+     */
+    public render(): React.ReactElement<HTMLLIElement> {
+        return (
+            <li
+                role={ContextMenuItemRole[this.props.role] || ContextMenuItem.defaultProps.role}
+            >
+                {this.props.children}
+            </li>
+        );
+    }
+}
