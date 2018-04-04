bed9a87ec5c60b7b596ab5b5f95ba6e0f4b3b7aa
nicholasrice <nicholas.rice119@gmail.com>
2018-04-04 16:58:39 -0700

    adding context-menu-item foundation

---
+++ packages/fast-components-react-base/src/context-menu-item/context-menu-item.props.ts
@@ -0,0 +1,19 @@
+import * as React from "react";
+import {IContextMenuItemClassNameContract, IManagedClasses} from "@microsoft/fast-components-class-name-contracts";
+import {ContextMenuItemRole} from "./context-menu-item";
+
+export interface IContextMenuItemHandledProps {
+    /**
+     * The children of the context-menu-item
+     */
+    children?: React.ReactNode;
+
+    /**
+     * The role attribute of the context-menu-item
+     */
+    role?: ContextMenuItemRole;
+}
+
+export interface IContextMenuItemUnhandledProps extends React.LiHTMLAttributes<HTMLLIElement> {}
+export interface IContextMenuItemManagedClasses extends IManagedClasses<IContextMenuItemClassNameContract> {}
+export type ContextMenuItemProps = IContextMenuItemHandledProps & IContextMenuItemUnhandledProps & IContextMenuItemManagedClasses;
