ce7ceabd54790dbdfb527c5977bfb06cd0604197
nicholasrice <nicholas.rice119@gmail.com>
2018-04-05 14:34:23 -0700

    update package locks and abstracting managed-classes

--- packages/fast-components-react-base/src/context-menu-item/examples.data.ts
+++ packages/fast-components-react-base/src/context-menu-item/examples.data.ts
@@ -2,20 +2,22 @@ import {ISnapshotTestSuite} from "@microsoft/fast-jest-snapshots-react";
 import {IManagedClasses} from "@microsoft/fast-components-class-name-contracts";
 import ContextMenuItem, {IContextMenuItemHandledProps, IContextMenuItemManagedClasses} from "./context-menu-item";
 
+const managedClasses: IContextMenuItemManagedClasses = {
+    managedClasses: {
+        contextMenuItem: "context-menu-item"
+    }
+};
+
 const examples: ISnapshotTestSuite<IContextMenuItemHandledProps & IContextMenuItemManagedClasses> = {
     name: "context-menu-item",
     component: ContextMenuItem,
     data: [
         {
-            managedClasses: {
-                contextMenuItem: "context-menu-item"
-            },
+            ...managedClasses,
             children: "child"
         },
         {
-            managedClasses: {
-                contextMenuItem: "context-menu-item"
-            },
+            ...managedClasses,
             children: "child",
             role: "invalid-role" as any
         }
