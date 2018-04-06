451c9ad2d4cf3ad3a96026d30918a2aeba39ab2c
marjonlynch <marjon@microsoft.com>
2018-04-06 14:09:19 -0700

    merge issue resolved

--- packages/fast-components-react-msft/src/index.ts
+++ packages/fast-components-react-msft/src/index.ts
@@ -3,12 +3,15 @@ export { Button };
 
 import Divider from "./divider";
 export { Divider };
+<<<<<<< HEAD
 
 import Image from "./image";
 export {Image};
 
 import Toggle from "./toggle";
 export { Toggle };
+=======
+>>>>>>> merge issue resolved
 
 import Hypertext from "./hypertext";
 export { Hypertext };
