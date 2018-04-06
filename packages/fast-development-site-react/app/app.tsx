11a1ab2b41603a5eb911462ee9e5a2830b2aee49
Jane Chu <7559015+janechu@users.noreply.github.com>
2018-04-06 11:33:27 -0700

    fix a few tslint issues in react development site

--- packages/fast-development-site-react/app/app.tsx
+++ packages/fast-development-site-react/app/app.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Site, { SiteMenu, SiteMenuItem, SiteCategory, SiteCategoryIcon, SiteCategoryItem } from "../src";
+import Site, { SiteCategory, SiteCategoryIcon, SiteCategoryItem, SiteMenu, SiteMenuItem } from "../src";
 
 import Button from "./components/button";
 import Paragraph from "./components/paragraph";
 import PolymerHeading from "./components/polymer-heading";
+
+/* tslint:disable-next-line */
+const buildingBlocksPath = "M25.2,16h3V30H2.2V4h14V7l7-7,9,9Zm-21,0h10V6H4.2Zm10,2H4.2V28h10Zm2-7v5h5Zm0,7V28h10V18Zm.47-9,6.53,6.54L29.73,9,23.2,2.46Z";
 
 const render: any = (): void => {
     ReactDOM.render(
@@ -17,7 +20,7 @@ const render: any = (): void => {
                 <SiteCategoryIcon slot="category-icon">
                     <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                         <title>building-blocks</title>
-                        <path d="M25.2,16h3V30H2.2V4h14V7l7-7,9,9Zm-21,0h10V6H4.2Zm10,2H4.2V28h10Zm2-7v5h5Zm0,7V28h10V18Zm.47-9,6.53,6.54L29.73,9,23.2,2.46Z"/>
+                        <path d={buildingBlocksPath}/>
                     </svg>
                 </SiteCategoryIcon>
             </SiteCategory>
@@ -71,7 +74,6 @@ const render: any = (): void => {
                     </SiteCategory>
                 </SiteCategory>
             </SiteCategory>
-            
         </Site>,
         document.getElementById("root")
     );
