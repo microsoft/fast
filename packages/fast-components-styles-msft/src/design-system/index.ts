5a9cf2cc287b01ea392a22edfd9e96c85dd2cc7b
nalogiudice <random.interval@gmail.com>
2018-04-11 13:34:41 -0700

    Updates errors found via tslint

--- packages/fast-components-styles-msft/src/design-system/index.ts
+++ packages/fast-components-styles-msft/src/design-system/index.ts
@@ -10,25 +10,4 @@ const designSystemDefaults: IDesignSystem = {
     brandColor: "#0078D4"
 };
 
-/**
- * Returns RGBA from hex value with optional alpha
- * @param hex : Hex value
- * @param alpha : Optional alpha
- */
-export function hexToRGB(hex: string, alpha: number): string {
-    if (hex.length === 4) {
-        hex += hex.slice(1,4);
-    }
-
-    let r = parseInt(hex.slice(1, 3), 16),
-        g = parseInt(hex.slice(3, 5), 16),
-        b = parseInt(hex.slice(5, 7), 16);
-
-    if (alpha) {
-        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
-    } else {
-        return "rgb(" + r + ", " + g + ", " + b + ")";
-    }
-}
-
 export default designSystemDefaults;
--- packages/fast-components-styles-msft/src/toggle/index.ts
+++ packages/fast-components-styles-msft/src/toggle/index.ts
@@ -1,93 +1,93 @@
-import {IDesignSystem, hexToRGB} from "../design-system";
-import {ComponentStyles} from "@microsoft/fast-jss-manager";
-import {toPx} from "../utilities/units";
-import {typeRamp} from "../utilities/typography";
-import {IToggleClassNameContract} from "@microsoft/fast-components-class-name-contracts";
+import { IDesignSystem } from "../design-system";
+import { ComponentStyles } from "@microsoft/fast-jss-manager";
+import { toPx } from "../utilities/units";
+import { typeRamp } from "../utilities/typography";
+import { IToggleClassNameContract } from "@microsoft/fast-components-class-name-contracts";
 import * as Chroma from "chroma-js";
 
 const styles: ComponentStyles<IToggleClassNameContract, IDesignSystem> = {
     toggle: {
-        '& span': {
-            userSelect: 'none',
-            marginTop: '0',
-            paddingBottom: '0'
+        "& span": {
+            userSelect: "none",
+            marginTop: "0",
+            paddingBottom: "0"
         },
-        '&[aria-disabled="true"]': {
+        "&[aria-disabled='true']": {
             color: (config: IDesignSystem): string => {
                 return Chroma(config.foregroundColor).alpha(0.5).css();
             }
         }
     },
     toggle_label: {
-        display: 'inline-block',
+        display: "inline-block",
         fontSize: toPx(typeRamp.t8.vp3.fontSize),
         lineHeight: toPx(typeRamp.t8.vp3.lineHeight),
         marginTop: toPx(21),
         paddingBottom: toPx(7),
-        float: 'left',
-        clear: 'left',
-        '& + div': {
-            marginTop: '0',
-            float: 'left',
-            clear: 'left',
-            '& + span': {
-                float: 'left',
+        float: "left",
+        clear: "left",
+        "& + div": {
+            marginTop: "0",
+            float: "left",
+            clear: "left",
+            "& + span": {
+                float: "left",
                 marginLeft: toPx(5)
             }
         }
     },
     toggle_wrapper: {
-        position: 'relative'
+        position: "relative"
     },
     toggle_button: {
-        position: 'absolute',
-        pointerEvents: 'none',
+        position: "absolute",
+        pointerEvents: "none",
         left: toPx(5),
         top: toPx(5),
-        transition: 'all .1s ease',
+        transition: "all .1s ease",
         backgroundColor: (config: IDesignSystem): string => {
             return config.backgroundColor;
-        },   
+        },
         content: "''",
         borderRadius: toPx(10),
         width: toPx(10),
         height: toPx(10)
     },
     toggle_input: {
-        position: 'relative',
-        margin: '0',
+        position: "relative",
+        margin: "0",
         width: toPx(44),
         height: toPx(20),
-        background: 'transparent',
+        background: "transparent",
         border: `${toPx(1)} solid`,
         borderColor: (config: IDesignSystem): string => {
             return config.foregroundColor;
         },
         borderRadius: toPx(20),
-        appearance: 'none',
-        cursor: 'pointer',
-        '@media screen and (-ms-high-contrast)': {
-            '&:after, &:checked+span': {
+        appearance: "none",
+        cursor: "pointer",
+        "@media screen and (-ms-high-contrast)": {
+            "&:after, &:checked+span": {
                 background: (config: IDesignSystem): string => {
                     return config.backgroundColor;
                 }
             }
         },
-        '@media screen and (-ms-high-contrast: black-on-white)': {
-            '&:after, &:checked+span': {
+        "@media screen and (-ms-high-contrast: black-on-white)": {
+            "&:after, &:checked+span": {
                 background: (config: IDesignSystem): string => {
                     return config.foregroundColor;
                 }
             }
         },
-        '&:checked': {
+        "&:checked": {
             backgroundColor: (config: IDesignSystem): string => {
                 return config.brandColor;
             },
             borderColor: (config: IDesignSystem): string => {
                 return config.brandColor;
             },
-            '&:hover': {
+            "&:hover": {
                 backgroundColor: (config: IDesignSystem): string => {
                     return Chroma(config.brandColor).alpha(0.8).css();
                 },
@@ -95,58 +95,58 @@ const styles: ComponentStyles<IToggleClassNameContract, IDesignSystem> = {
                     return Chroma(config.brandColor).alpha(0.8).css();
                 }
             },
-            '&:focus': {
+            "&:focus": {
                 borderColor: (config: IDesignSystem): string => {
                     return config.brandColor;
                 }
             },
-            '& + span': {
-                left: '28px',
+            "& + span": {
+                left: "28px",
                 backgroundColor: (config: IDesignSystem): string => {
                     return config.backgroundColor;
                 }
             },
-            '&:disabled': {
-                cursor: 'not-allowed',
+            "&:disabled": {
+                cursor: "not-allowed",
                 background: (config: IDesignSystem): string => {
                     return Chroma(config.foregroundColor).alpha(0.2).css();
                 },
-                borderColor: 'transparent',
-                '& + span': {
+                borderColor: "transparent",
+                "& + span": {
                     background: (config: IDesignSystem): string => {
                         return Chroma(config.foregroundColor).alpha(0.2).css();
                     }
                 },
-                '&:hover': {
-                    borderColor: 'transparent'
+                "&:hover": {
+                    borderColor: "transparent"
                 }
             }
         },
-        '&:not(:checked)': {
-            background: 'transparent',
+        "&:not(:checked)": {
+            background: "transparent",
             borderColor: (config: IDesignSystem): string => {
                 return config.foregroundColor;
             },
-            '& + span': {
+            "& + span": {
                 backgroundColor: (config: IDesignSystem): string => {
                     return config.foregroundColor;
                 }
             },
-            '&:disabled': {
-                cursor: 'not-allowed',
-                background: 'transparent',
+            "&:disabled": {
+                cursor: "not-allowed",
+                background: "transparent",
                 borderColor: (config: IDesignSystem): string => {
                     return Chroma(config.foregroundColor).alpha(0.2).css();
                 },
-                '& + span': {
+                "& + span": {
                     backgroundColor: (config: IDesignSystem): string => {
                         return Chroma(config.foregroundColor).alpha(0.2).css();
                     }
                 }
             }
         },
-        '&:focus': {
-            outline: '0'
+        "&:focus": {
+            outline: "0"
         }
     }
 };
