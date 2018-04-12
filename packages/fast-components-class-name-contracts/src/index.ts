6eb37c437d39ac41c792782a3208c84739615118
Chris Holt <chhol@microsoft.com>
2018-04-12 14:31:11 -0700

    alphabetize exports that were automatically merged

--- packages/fast-components-class-name-contracts/src/index.ts
+++ packages/fast-components-class-name-contracts/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./button";
 export * from "./divider";
 export * from "./image";
-export * from "./toggle";
 export * from "./managed-classes";
+export * from "./toggle";
 export * from "./typography";
