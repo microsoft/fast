b6ad435ec47939887102bba0e9f113335f81df8a
Jeff Smith <jes@microsoft.com>
2018-04-13 11:58:37 -0700

    Updated READ ME

--- packages/fast-components-class-name-contracts-msft/src/managed-classes.ts
+++ packages/fast-components-class-name-contracts-msft/src/managed-classes.ts
@@ -1,13 +1,1 @@
-// import { IManagedClasses, ClassNames } from "@microsoft/fast-components-class-name-contracts-base";
 export * from "@microsoft/fast-components-class-name-contracts-base";
-/**
-
-/**
- * Class names under a given object keys
- */
-// export {ClassNames}
-/**
- * The interface for class names passed as props
- */
-// export {IManagedClasses};
-
