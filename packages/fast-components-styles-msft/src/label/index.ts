1fbece8f8feaf1d8b19530e9ad2eda3b33f25185
nalogiudice <random.interval@gmail.com>
2018-04-17 10:21:25 -0700

    Removes unneeded const in JSS

--- packages/fast-components-styles-msft/src/label/index.ts
+++ packages/fast-components-styles-msft/src/label/index.ts
@@ -5,16 +5,12 @@ import { toPx } from "../utilities/units";
 import { applyType } from "../utilities/typography";
 import { applyScreenReader } from "../utilities/screen-reader";
 
-const baseStyles: any = {
-    ...applyType("t8", "vp3"),
-    display: "inline-block",
-    marginTop: toPx(0),
-    paddingBottom: toPx(7),
-};
-
 const styles: ComponentStyles<ILabelClassNameContract, IDesignSystem> = {
     label: {
-        baseStyles
+        ...applyType("t8", "vp3"),
+        display: "inline-block",
+        marginTop: toPx(0),
+        paddingBottom: toPx(7)
     },
     label_hidden: {
         ...applyScreenReader()
