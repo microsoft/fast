f690d286fe0bdfce0342512c73655d1ada8f1569
nicholasrice <nicholas.rice119@gmail.com>
2018-04-20 13:58:51 -0700

    adding comments

--- packages/fast-jss-utilities/src/direction.ts
+++ packages/fast-jss-utilities/src/direction.ts
@@ -6,10 +6,16 @@ export enum Direction {
 const left: string = "left";
 const right: string = "right";
 
+/**
+ * Returns "right" when dir is "rtl", otherwise returns "left"
+ */
 export function locLeft(dir: Direction): string {
     return dir === Direction.rtl ? right : left;
 }
 
+/**
+ * returns "left" when dir is "rtl", otherwise returns "right"
+ */
 export function locRight(dir: Direction): string {
     return dir === Direction.rtl ? left : right;
 }
--- packages/fast-jss-utilities/src/spacing.ts
+++ packages/fast-jss-utilities/src/spacing.ts
@@ -3,22 +3,38 @@ import { Direction, locLeft, locRight } from "./direction";
 const margin = "margin";
 const padding = "padding";
 
+/**
+ * Returns "margin-left" when dir is "rtl", otherwise returns "margin-right"
+ */
 export function locMarginRight(dir: Direction): string {
     return `${margin}-${locRight(dir)}`;
 }
 
+/**
+ * Returns "margin-right" when dir is "rtl", otherwise returns "margin-left"
+ */
 export function locMarginLeft(dir: Direction): string {
     return `${margin}-${locLeft(dir)}`;
 }
 
+/**
+ * Returns "padding-left" when dir is "rtl", otherwise returns "margin-right"
+ */
 export function locPaddingRight(dir: Direction): string {
     return `${padding}-${locRight(dir)}`;
 }
 
+/**
+ * Returns "padding-right" when dir is "rtl", otherwise returns "margin-left"
+ */
 export function locPaddingLeft(dir: Direction): string {
     return `${padding}-${locLeft(dir)}`;
 }
 
+/**
+ * Localizes top/left/bottom/right formatted arguments, such as the format used by CSS's padding and margin
+ * properties. 
+ */
 export function locSpacing(dir): (...args: string[]) => string {  
     let space = " ";
     return (...args: string[]): string => {
