0040801dcc8db7a80487bf34a54e99d24d8e265b
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2018-04-27 08:57:20 -0700

    Revert "refactor(jss-manager): refactor react JSS Manager to treat all styles as dynamic (#332)"
    This reverts commit 610a192d601eee6a5d1838f3b498038146311a13.

--- packages/fast-jss-manager-react/src/manage-jss.tsx
+++ packages/fast-jss-manager-react/src/manage-jss.tsx
@@ -5,6 +5,8 @@
  */
 import * as React from "react";
 import jss, { stylesheetManager } from "./jss";
+import { getDynamicStyles } from "jss";
+import getStaticStyles from "./utilities/get-static-styles";
 import { isEmptyObject } from "./utilities/object";
 import { SheetsManager, StyleSheet } from "jss";
 import { IDesignSystemProviderProps } from "./design-system-provider";
@@ -25,6 +27,32 @@ export interface IJSSManagerState {
      * Stores a JSS stylesheet containing all config-driven styles rules for a component
      */
     dynamicStyleSheet?: any;
+
+    /**
+     * Stores a JSS stylesheet containing all static style rules for a component
+     */
+    staticStyleSheet?: any;
+}
+
+/**
+ * Defines a object that has been separated into dynamic and static stylesheets
+ */
+export interface ISeparatedStylesheet<T, C> {
+    /**
+     * The static styles for a given component and stylesheet combination
+     * TODO: these are always static so they shouldn't use CSSRuleResolver
+     */
+    staticStyles?: ComponentStyles<T, C>;
+
+    /**
+     * Store the jss stylesheet so that multiple components can access it
+     */
+    staticStyleSheet?: any;
+
+    /**
+     * The dynamic styles for a given component and stylesheet combination
+     */
+    dynamicStyles?: ComponentStyles<T, C>;
 }
 
 /**
@@ -67,9 +95,9 @@ function manageJss<S, C>(styles?: ComponentStyles<S, C>): <T>(Component: React.C
             private static styleMap: WeakMap<ComponentStyles<S, C>, string> = new WeakMap();
 
             /**
-             * All static and dynamic styles associated with the component
+             * Store references to all separated stylesheets
              */
-            private static styles: ComponentStyles<S, C>;
+            private static separatedStyles: {[key: string]: ISeparatedStylesheet<S, C>} = {};
 
             /**
              * Tracks the ID of an instance of the JSSManager. Multiple instances can have the same ID
@@ -81,11 +109,44 @@ function manageJss<S, C>(styles?: ComponentStyles<S, C>): <T>(Component: React.C
             constructor(props: T) {
                 super(props);
 
-                JSSManager.styles = styles;
+                // On construction, check if the style object or component object have already been used.
+                // If not, we need to store them in our weakmaps for later use
+                if (!Boolean(JSSManager.styleMap.get(styles))) {
+                    JSSManager.styleMap.set(styles, uniqueId());
+                }
 
+                if (!Boolean(JSSManager.componentMap.get(Component))) {
+                    JSSManager.componentMap.set(Component, uniqueId());
+                }
+
+                const styleId: string = JSSManager.styleMap.get(styles);
+                const componentId: string = JSSManager.componentMap.get(Component);
+
+                // Store the instance id as a combination of the generated IDs
+                this.instanceId = `${componentId}${styleId}`;
+                let separatedStylesInstance: ISeparatedStylesheet<S, C> = JSSManager.separatedStyles[this.instanceId];
+
+                // Check if we have a separatedStyles object stored at the instanceId key.
+                // If we don"t, we need to create that object
+                if (!Boolean(separatedStylesInstance)) {
+                    separatedStylesInstance = JSSManager.separatedStyles[this.instanceId] = this.separateStyles(styles);
+                }
+
+                // Now lets store those newly created stylesheet objects in state so we can easily reference them later
+                // Since dynamic styles can be different across components, we should create the dynamic styles as a
+                // new object so that identity checks between dynamic stylesheets do not pass.
                 const state: IJSSManagerState = {};
 
-                if (Boolean(JSSManager.styles)) {
+                // Extract the static stylesheet and dynamic style object and apply them to the state
+                // object if they exist
+                const staticStyleSheet: StyleSheet = separatedStylesInstance.staticStyleSheet;
+                const dynamicStyles: StyleSheet = separatedStylesInstance.dynamicStyles;
+
+                if (Boolean(staticStyleSheet)) {
+                    state.staticStyleSheet = separatedStylesInstance.staticStyleSheet;
+                }
+
+                if (Boolean(dynamicStyles)) {
                     state.dynamicStyleSheet = this.createDynamicStyleSheet();
                 }
 
@@ -102,6 +163,11 @@ function manageJss<S, C>(styles?: ComponentStyles<S, C>): <T>(Component: React.C
             }
 
             public componentWillMount(): void {
+                if (Boolean(this.state.staticStyleSheet)) {
+                    JSSManager.stylesheetManager.add(this.state.staticStyleSheet, this.state.staticStyleSheet);
+                    JSSManager.stylesheetManager.manage(this.state.staticStyleSheet);
+                }
+
                 if (Boolean(this.state.dynamicStyleSheet)) {
                     // It appears we need to update the stylesheet for any style properties defined as functions
                     // to work.
@@ -134,6 +200,10 @@ function manageJss<S, C>(styles?: ComponentStyles<S, C>): <T>(Component: React.C
             }
 
             public componentWillUnmount(): void {
+                if (this.hasStaticStyleSheet()) {
+                    JSSManager.stylesheetManager.unmanage(this.state.staticStyleSheet);
+                }
+
                 if (this.hasDynamicStyleSheet()) {
                     this.state.dynamicStyleSheet.detach();
                     jss.removeStyleSheet(this.state.dynamicStyleSheet);
@@ -162,23 +232,56 @@ function manageJss<S, C>(styles?: ComponentStyles<S, C>): <T>(Component: React.C
              */
             private createDynamicStyleSheet(): any {
                 return jss.createStyleSheet(
-                    merge({}, JSSManager.styles, this.props.jssStyleSheet),
+                    merge({}, JSSManager.separatedStyles[this.instanceId].dynamicStyles, this.props.jssStyleSheet),
                     { link: true }
                 );
+            }
+
+            /**
+             * Checks to see if this component has an associated static stylesheet
+             */
+            private hasStaticStyleSheet(): boolean {
+                return Boolean(JSSManager.separatedStyles[this.instanceId].staticStyleSheet);
             }
 
             /**
              * Checks to see if this component has an associated dynamic stylesheet
              */
             private hasDynamicStyleSheet(): boolean {
-                return Boolean(JSSManager.styles || this.props.jssStyleSheet);
+                return Boolean(JSSManager.separatedStyles[this.instanceId].dynamicStyles || this.props.jssStyleSheet);
+            }
+
+            /**
+             * Separates a single ComponentStyles object into an ISeparatedStylesheet object
+             * If either a dynamic or static stylesheet is empty (there are no styles) then that
+             * key will not be created.
+             */
+            private separateStyles(componentStyles: ComponentStyles<S, C>): ISeparatedStylesheet<S, C> {
+                const dynamicStyles: ComponentStyles<S, C> = getDynamicStyles(componentStyles);
+                const staticStyles: ComponentStyles<S, C> = getStaticStyles(componentStyles);
+                const separatedStyles: ISeparatedStylesheet<S, C> = {};
+
+                if (Boolean(dynamicStyles) && !isEmptyObject(dynamicStyles)) {
+                    separatedStyles.dynamicStyles = dynamicStyles;
+                }
+
+                if (Boolean(staticStyles) && !isEmptyObject(staticStyles)) {
+                    separatedStyles.staticStyles = staticStyles;
+                    separatedStyles.staticStyleSheet = jss.createStyleSheet(staticStyles);
+                }
+
+                return separatedStyles;
             }
 
             /**
              * Merges static and dynamic stylesheet classnames into one object
              */
             private getClassNames(): ClassNames<S> {
-                const classNames: Partial<ClassNames<S>> = {};
+                let classNames: Partial<ClassNames<S>> = {};
+
+                if (this.hasStaticStyleSheet()) {
+                    classNames = Object.assign({}, this.state.staticStyleSheet.classes);
+                }
 
                 if (this.hasDynamicStyleSheet()) {
                     for (const key in this.state.dynamicStyleSheet.classes) {
