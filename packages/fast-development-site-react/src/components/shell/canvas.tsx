59fce3ccd279f9d1ddddc70cc4230dc76edba549
Nick Rice <nicholas.rice119@gmail.com>
2018-04-27 09:50:24 -0700

    adding two pages

--- packages/fast-development-site-react/src/components/shell/canvas.tsx
+++ packages/fast-development-site-react/src/components/shell/canvas.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import manageJss, { ComponentStyles, IJSSManagerProps, IManagedClasses } from "@microsoft/fast-jss-manager-react";
 import { IDevSiteDesignSystem } from "../design-system";
 import { ComponentView } from "../site";
-import { Route, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import { RouteComponentProps } from "react-router";
 
 /* tslint:disable-next-line */
@@ -23,12 +23,32 @@ const style: ComponentStyles<IShellCanvasManagedClasses, IDevSiteDesignSystem> =
 };
 
 class ShellCanvas extends React.Component<ICanvasHeaderProps & IManagedClasses<IShellCanvasManagedClasses>, {}> {
-
     public render(): JSX.Element {
         return (
-            <div className={this.props.managedClasses.shellCanvas}>
-                {this.props.children}
+            <div className={this.props.managedClasses.shell_canvas}>
+                <Switch>
+                    <Route path={this.props.match.url} exact={true} component={this.renderDetailPage} />
+                    <Route
+                        path={this.props.match.url + `/${ComponentView[ComponentView.examples]}`}
+                        exact={true}
+                        component={this.renderExamplesPage}
+                    />
+                </Switch>
             </div>
+        );
+    }
+
+    private renderDetailPage = (): React.ReactElement<HTMLElement> => {
+        return (
+            <h1>Detail</h1>
+        );
+    }
+
+    private renderExamplesPage = (): React.ReactNode => {
+        return (
+            <React.Fragment>
+                {this.props.children}
+            </React.Fragment>
         );
     }
 }
