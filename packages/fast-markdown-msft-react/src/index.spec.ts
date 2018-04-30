kant/fast-dna

feat(markdown): add utility to convert markdown to msft component strings

packages/fast-markdown-msft-react/src/index.spec.ts
@@ -1,0 +1,94 @@
+import * as MarkdownIt from "markdown-it";
+import FASTMarkdownIt from "./index";
+
+const md: any = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+    xhtmlOut: true
+});
+
+md.use(FASTMarkdownIt);
+
+const newlineRegex: RegExp = /\r\n|\n/g;
+
+describe("Convert markdown to HTML", function(): void {
+
+    test("should convert a link to a hyperlink with a blank target", function(): void {
+        const markdown: string = "https://test.com";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            /* tslint:disable-next-line */
+            "<Typography tag=\"p\" typeLevel={2}><Hypertext text=\"https://test.com\" href=\"https://test.com\" target=\"_blank\">https://test.com</Hypertext></Typography>"
+        );
+    });
+
+    test("should convert a (c) to a copywrite symbol", function(): void {
+        const markdown: string = "(c)";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<Typography tag=\"p\" typeLevel={2}>©</Typography>"
+        );
+    });
+
+    test("should allow HTML to be in markdown", function(): void {
+        const markdown: string = "<a href=\"#\">hello world</a>";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            `<Typography tag=\"p\" typeLevel={2}><a href="#">hello world</a></Typography>`
+        );
+    });
+
+    test("should convert a markdown link to a fluent web hyperlink", function(): void {
+        const markdown: string = "[Test](/test)";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<Typography tag=\"p\" typeLevel={2}><Hypertext text=\"Test\" href=\"/test\">Test</Hypertext></Typography>"
+        );
+    });
+
+    test("should convert a markdown unordered bullet list to a list", function(): void {
+        const markdown: string = "* list item";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<ul><li><Typography tag=\"p\" typeLevel={2}>list item</Typography></li></ul>"
+        );
+    });
+
+    test("should convert a markdown ordered list to a fluent web list", function(): void {
+        const markdown: string = "1. list item";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<ol><li><Typography tag=\"p\" typeLevel={2}>list item</Typography></li></ol>"
+        );
+    });
+
+    test("should convert a markdown heading to a fluent web heading", function(): void {
+        const markdown: string = "# Hello world";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<Typography id=\"hello-world\" tag=\"h1\" typeLevel={3}>Hello world</Typography>"
+        );
+    });
+
+    test("should convert any code snippets into a PrismCode react component", function(): void {
+        const markdown: string = "```\n{}\n```";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            `<PrismCode>{\"{}\\n\"}</PrismCode>`
+        );
+    });
+
+    test("should convert any code snippets with a language specified into a PrismCode react component", function(): void {
+        const markdown: string = "```jsx\n{}\n```";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            `<PrismCode className="language-jsx">{\"{}\\n\"}</PrismCode>`
+        );
+    });
+
+    test("should convert any inline text with {, }, < and > into their HTML codes", function(): void {
+        const markdown: string = "some text with { and } and < and >";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            `<Typography tag=\"p\" typeLevel={2}>some text with &#123; and &#125; and &#60; and &#62;</Typography>`
+        );
+    });
+
+    test("should convert dividers", function(): void {
+        const markdown: string = "---";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<Divider />"
+        );
+    });
+});

packages/fast-markdown-msft-react/src/index.ts
@@ -1,0 +1,56 @@
+import CreateRule from "./rule";
+import { MarkdownIt } from "markdown-it";
+
+enum ComponentType {
+    heading = "heading",
+    hyperlink = "hyperlink",
+    list = "list",
+    paragraph = "paragraph"
+}
+
+export interface ITokens<T> extends Array<T> {
+    [idx: number]: T;
+}
+
+export interface IToken {
+    children: ITokens<IToken>;
+    tag: string;
+    content: string;
+    info: string;
+    type: string;
+    attrs: string[][];
+    attrPush: (attributes: string[]) => void;
+}
+
+class FastMarkdownIt {
+    constructor(md: MarkdownIt) {
+        md.core.ruler.push("fast", (new CreateRule(md) as any));
+        md.renderer.rules.paragraph_open = function(): string {
+            return `<Typography tag="p" typeLevel={2}>`;
+        };
+        md.renderer.rules.heading_open = function(tokens: ITokens<IToken>, idx: number): string {
+            const id: string = tokens[idx + 1].children[0].content.toLowerCase().replace(/\s/g, "-").replace(/[^a-z\-]/g, "");
+            return `<Typography id="${id}" tag="${tokens[idx].tag}" typeLevel={${parseInt(tokens[idx].tag.charAt(1), 10) + 2}}>`;
+        };
+        md.renderer.rules.text = (tokens: ITokens<IToken>, idx: number): string => {
+            return this.replaceSpecialCharacters(tokens[idx].content);
+        };
+        md.renderer.rules.fence = function(tokens: ITokens<IToken>, idx: number): string {
+            let codeSnippet: string = `<PrismCode${tokens[idx].info ? ` className="language-${tokens[idx].info}"` : ""}>`;
+            codeSnippet += `{${JSON.stringify(tokens[idx].content, null, 2)}}`;
+            codeSnippet += `</PrismCode>`;
+
+            return codeSnippet;
+        };
+    }
+
+    private replaceSpecialCharacters = (content: string): string => {
+        return content.replace(/{/g, "&#123;").replace(/}/g, "&#125;").replace(/</g, "&#60;").replace(/>/g, "&#62;");
+    }
+}
+
+function plugin(md: MarkdownIt): void {
+    const fastMarkdownIt: FastMarkdownIt = new FastMarkdownIt(md);
+}
+
+export default plugin;

microsoft/fast

packages/fast-markdown-msft-react/src/index.spec.ts
@@ -1,0 +1,94 @@
+import * as MarkdownIt from "markdown-it";
+import FASTMarkdownIt from "./index";
+
+const md: any = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+    xhtmlOut: true
+});
+
+md.use(FASTMarkdownIt);
+
+const newlineRegex: RegExp = /\r\n|\n/g;
+
+describe("Convert markdown to HTML", function(): void {
+
+    test("should convert a link to a hyperlink with a blank target", function(): void {
+        const markdown: string = "https://test.com";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            /* tslint:disable-next-line */
+            "<Typography tag=\"p\" typeLevel={2}><Hypertext text=\"https://test.com\" href=\"https://test.com\" target=\"_blank\">https://test.com</Hypertext></Typography>"
+        );
+    });
+
+    test("should convert a (c) to a copywrite symbol", function(): void {
+        const markdown: string = "(c)";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<Typography tag=\"p\" typeLevel={2}>©</Typography>"
+        );
+    });
+
+    test("should allow HTML to be in markdown", function(): void {
+        const markdown: string = "<a href=\"#\">hello world</a>";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            `<Typography tag=\"p\" typeLevel={2}><a href="#">hello world</a></Typography>`
+        );
+    });
+
+    test("should convert a markdown link to a FAST MSFT hypertext", function(): void {
+        const markdown: string = "[Test](/test)";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<Typography tag=\"p\" typeLevel={2}><Hypertext text=\"Test\" href=\"/test\">Test</Hypertext></Typography>"
+        );
+    });
+
+    test("should convert a markdown unordered bullet list to a list", function(): void {
+        const markdown: string = "* list item";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<ul><li><Typography tag=\"p\" typeLevel={2}>list item</Typography></li></ul>"
+        );
+    });
+
+    test("should convert a markdown ordered list to a list", function(): void {
+        const markdown: string = "1. list item";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<ol><li><Typography tag=\"p\" typeLevel={2}>list item</Typography></li></ol>"
+        );
+    });
+
+    test("should convert a markdown heading to a FAST MSFT heading", function(): void {
+        const markdown: string = "# Hello world";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<Heading id=\"hello-world\" tag=\"h1\" typeLevel={3}>Hello world</Heading>"
+        );
+    });
+
+    test("should convert any code snippets into a PrismCode react component", function(): void {
+        const markdown: string = "```\n{}\n```";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            `<PrismCode>{\"{}\\n\"}</PrismCode>`
+        );
+    });
+
+    test("should convert any code snippets with a language specified into a PrismCode react component", function(): void {
+        const markdown: string = "```jsx\n{}\n```";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            `<PrismCode className="language-jsx">{\"{}\\n\"}</PrismCode>`
+        );
+    });
+
+    test("should convert any inline text with {, }, < and > into their HTML codes", function(): void {
+        const markdown: string = "some text with { and } and < and >";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            `<Typography tag=\"p\" typeLevel={2}>some text with &#123; and &#125; and &#60; and &#62;</Typography>`
+        );
+    });
+
+    test("should convert dividers", function(): void {
+        const markdown: string = "---";
+        expect(md.render(markdown).replace(newlineRegex, "")).toBe(
+            "<Divider />"
+        );
+    });
+});

packages/fast-markdown-msft-react/src/index.ts
@@ -1,0 +1,56 @@
+import CreateRule from "./rule";
+import { MarkdownIt } from "markdown-it";
+
+enum ComponentType {
+    heading = "heading",
+    hyperlink = "hyperlink",
+    list = "list",
+    paragraph = "paragraph"
+}
+
+export interface ITokens<T> extends Array<T> {
+    [idx: number]: T;
+}
+
+export interface IToken {
+    children: ITokens<IToken>;
+    tag: string;
+    content: string;
+    info: string;
+    type: string;
+    attrs: string[][];
+    attrPush: (attributes: string[]) => void;
+}
+
+class FastMarkdownIt {
+    constructor(md: MarkdownIt) {
+        md.core.ruler.push("fast", (new CreateRule(md) as any));
+        md.renderer.rules.paragraph_open = function(): string {
+            return `<Typography tag="p" typeLevel={2}>`;
+        };
+        md.renderer.rules.heading_open = function(tokens: ITokens<IToken>, idx: number): string {
+            const id: string = tokens[idx + 1].children[0].content.toLowerCase().replace(/\s/g, "-").replace(/[^a-z\-]/g, "");
+            return `<Heading id="${id}" tag="${tokens[idx].tag}" typeLevel={${parseInt(tokens[idx].tag.charAt(1), 10) + 2}}>`;
+        };
+        md.renderer.rules.text = (tokens: ITokens<IToken>, idx: number): string => {
+            return this.replaceSpecialCharacters(tokens[idx].content);
+        };
+        md.renderer.rules.fence = function(tokens: ITokens<IToken>, idx: number): string {
+            let codeSnippet: string = `<PrismCode${tokens[idx].info ? ` className="language-${tokens[idx].info}"` : ""}>`;
+            codeSnippet += `{${JSON.stringify(tokens[idx].content, null, 2)}}`;
+            codeSnippet += `</PrismCode>`;
+
+            return codeSnippet;
+        };
+    }
+
+    private replaceSpecialCharacters = (content: string): string => {
+        return content.replace(/{/g, "&#123;").replace(/}/g, "&#125;").replace(/</g, "&#60;").replace(/>/g, "&#62;");
+    }
+}
+
+function plugin(md: MarkdownIt): void {
+    const fastMarkdownIt: FastMarkdownIt = new FastMarkdownIt(md);
+}
+
+export default plugin;