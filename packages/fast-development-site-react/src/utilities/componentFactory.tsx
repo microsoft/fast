18df294e5d732576b5aab67b096a6437d2240732
Chris Holt <chhol@microsoft.com>
2018-05-02 12:20:04 -0700

    add design system provider with optional design system to component factory

--- packages/fast-development-site-react/src/utilities/componentFactory.tsx
+++ packages/fast-development-site-react/src/utilities/componentFactory.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import componentExampleFactory from "./componentExampleFactory";
 import { SiteCategory } from "../";
 
-export default function componentFactory(examples: any): JSX.Element[] {
+export default function componentFactory<T>(examples: any, designSystem?: T): JSX.Element[] {
     return Object.keys(examples).map((exampleKey: string, index: number) => {
         return (
             <SiteCategory key={index} slot={"category"} name={examples[exampleKey].name}>
-                {componentExampleFactory(examples, exampleKey)}
+                {componentExampleFactory<T>(examples, exampleKey, designSystem)}
             </SiteCategory>
         );
     });
