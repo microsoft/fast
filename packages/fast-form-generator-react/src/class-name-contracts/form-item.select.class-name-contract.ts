4aeef73284af98c9266fa1d9004e39ed011c35ef
Nicolas LoGiudice <random.interval@gmail.com>
2018-05-03 20:02:00 -0700

    feat(form generator): adds styling scaffolding and JSS for select (#363)
    * Adds text field JSS and other scaffolding
    
    * Updates snapshots and package
    
    * Removes line from package to test remote build
    
    * Readding package data
    
    * Changes travis file for testing remote build
    
    * Updates travis.yml
    
    * Updates travis.yml
    
    * Resets travis and packages - fix to be done in other branch
    
    * Fixes bad rebase
    
    * Cleans up JSS

---
+++ packages/fast-form-generator-react/src/class-name-contracts/form-item.select.class-name-contract.ts
@@ -0,0 +1,10 @@
+/**
+ * Select class name contract
+ */
+export default interface IFormItemSelectClassNameContract {
+    formItemSelect: string;
+    formItemSelect_label: string;
+    formItemSelect_span: string;
+    formItemSelect_input: string;
+    formItemSelect_option: string;
+}
