5f322570a9f7f8d9a579fb49c24670e462b67147
nalogiudice <random.interval@gmail.com>
2018-05-09 16:47:45 -0700

    Continued styling

--- packages/fast-components-styles-msft/src/checkbox/index.ts
+++ packages/fast-components-styles-msft/src/checkbox/index.ts
@@ -4,103 +4,186 @@ import { ICheckboxClassNameContract } from "@microsoft/fast-components-class-nam
 import { applyTypeRampConfig } from "../utilities/typography";
 import { toPx } from "@microsoft/fast-jss-utilities";
 import * as Chroma from "chroma-js";
+import { relative } from "path";
 
 const styles: ComponentStyles<ICheckboxClassNameContract, IDesignSystem> = {
     checkbox: {
         display: "flex",
-        flexDirection: "row"
+        flexDirection: "row",
+        verticalAlign: "middle"
     },
+    // checkbox_input: {
+    //     position: "absolute",
+    //     cursor: "pointer",
+    //     width: toPx(20),
+    //     height: toPx(20),
+    //     appearance: "none",
+    //     border: `${toPx(1)} solid`,
+    //     borderRadius: toPx(2),
+    //     borderColor: (config: IDesignSystem): string => {
+    //         return Chroma.mix(config.foregroundColor, config.backgroundColor, 0.46).css();
+    //     },
+    //     "&:after, &:before": {
+    //         position: "absolute",
+    //         display: "block",
+    //         content: "''",
+    //         width: toPx(1),
+    //         // TODO: Issue #309 https://github.com/Microsoft/fast-dna/issues/309
+    //         // background: (config: IDesignSystem): string => {
+    //         //     return config.foregroundColor;
+    //         // },
+    //         background: "black"
+    //     },
+    //     "&:focus": {
+    //         outline: "none",
+    //         borderWidth: toPx(2),
+    //         "&:checked": {
+    //             "&:before": {
+    //                 left: toPx(4),
+    //                 top: toPx(8)
+    //             },
+    //             "&:after": {
+    //                 left: toPx(9),
+    //                 top: toPx(3)
+    //             }
+    //         },
+    //         "&:indeterminate, &:indeterminate:checked": {
+    //             "&:before": {
+    //                 left: toPx(3),
+    //                 top: toPx(3)
+    //             },
+    //             "&:after": {
+    //                 display: "none"
+    //             }
+    //         }
+    //     },
+    //     "&:checked": {
+    //         "&:before": {
+    //             height: toPx(5),
+    //             left: toPx(5),
+    //             top: toPx(9),
+    //             transform: "rotate(-45deg)"
+    //         },
+    //         "&:after": {
+    //             height: toPx(11),
+    //             left: toPx(10),
+    //             top: toPx(4),
+    //             transform: "rotate(45deg)"
+    //         }
+    //     },
+    //     "&:indeterminate, &:indeterminate:checked": {
+    //         "&:before": {
+    //             display: "block",
+    //             transform: "none",
+    //             left: toPx(4),
+    //             top: toPx(4),
+    //             height: toPx(10),
+    //             width: toPx(10)
+    //         },
+    //         "&:after": {
+    //             display: "none"
+    //         }
+    //     }
+    // },
     checkbox_input: {
         position: "absolute",
         cursor: "pointer",
         width: toPx(20),
         height: toPx(20),
-        appearance: "none",
+        opacity: "0",
         border: `${toPx(1)} solid`,
         borderRadius: toPx(2),
+        boxSizing: "content-box",
+        margin: "0",
         borderColor: (config: IDesignSystem): string => {
             return Chroma.mix(config.foregroundColor, config.backgroundColor, 0.46).css();
         },
-        "&:after, &:before": {
-            position: "absolute",
-            display: "block",
-            content: "''",
-            width: toPx(1),
-            // TODO: Issue #309 https://github.com/Microsoft/fast-dna/issues/309
-            // background: (config: IDesignSystem): string => {
-            //     return config.foregroundColor;
-            // },
-            background: "black"
-        },
         "&:focus": {
             outline: "none",
-            borderWidth: toPx(2),
+            "& + span": {
+                borderWidth: toPx(2)
+            },
             "&:checked": {
-                "&:before": {
-                    left: toPx(4),
-                    top: toPx(8)
-                },
-                "&:after": {
-                    left: toPx(9),
-                    top: toPx(3)
+                "& + span": {
+                    "&:after, &:before": {
+                        position: "absolute",
+                        zIndex: "1",
+                        content: "''",
+                        borderRadius: toPx(2),
+                        width: toPx(2),
+                        background: "black"
+                    }
                 }
             },
-            "&:indeterminate, &:indeterminate:checked": {
-                "&:before": {
-                    left: toPx(3),
-                    top: toPx(3)
-                },
-                "&:after": {
-                    display: "none"
-                }
-            }
+            // "&:indeterminate, &:indeterminate:checked": {
+            //     "& + span": {
+            //         "&:before": {
+            //             top: toPx(4),
+            //             left: toPx(11)
+            //         },
+            //         "&:after": {
+            //             top: toPx(9),
+            //             left: toPx(6),
+            //         }
+            //     }
+            // }
         },
         "&:checked": {
-            "&:before": {
-                height: toPx(5),
-                left: toPx(5),
-                top: toPx(9),
-                transform: "rotate(-45deg)"
-            },
-            "&:after": {
-                height: toPx(11),
-                left: toPx(10),
-                top: toPx(4),
-                transform: "rotate(45deg)"
-            }
-        },
-        "&:indeterminate, &:indeterminate:checked": {
-            "&:before": {
-                display: "block",
-                transform: "none",
-                left: toPx(4),
-                top: toPx(4),
-                height: toPx(10),
-                width: toPx(10)
-            },
-            "&:after": {
-                display: "none"
+            "& + span": {
+                "&:after, &:before": {
+                    position: "absolute",
+                    zIndex: "1",
+                    content: "''",
+                    borderRadius: toPx(2),
+                    width: toPx(2),
+                    background: "black"
+                }
             }
         }
     },
     checkbox_span: {
-        "&:after, &:before": {
-            position: "absolute",
-            zIndex: "1",
-            content: "''",
-            width: toPx(1),
-            // TODO: Issue #309 https://github.com/Microsoft/fast-dna/issues/309
-            // background: (config: IDesignSystem): string => {
-            //     return config.foregroundColor;
-            // },
-            background: "black"
+        position: "relative",
+        border: `${toPx(1)} solid`,
+        borderColor: (config: IDesignSystem): string => {
+            return Chroma.mix(config.foregroundColor, config.backgroundColor, 0.46).css();
         },
-        "& $checkbox_input &:checked": {
-            "&:after, &:before": {
-                borderRadius: toPx(2),
-                width: toPx(2)
-            }
+        borderRadius: toPx(2),
+        display: "inline-block",
+        width: "20px",
+        height: "20px",
+        "&:before": {
+            top: toPx(4),
+            left: toPx(11),
+            height: toPx(12),
+            transform: "rotate(40deg)"
+        },
+        "&:after": {
+            top: toPx(9),
+            left: toPx(6),
+            height: toPx(6),
+            transform: "rotate(-45deg)"
+        },
+        "&:focus": {
+            // borderWidth: toPx(2)
         }
+        // "&:after, &:before": {
+        //     width: toPx(1),
+        //     // TODO: Issue #309 https://github.com/Microsoft/fast-dna/issues/309
+        //     // background: (config: IDesignSystem): string => {
+        //     //     return config.foregroundColor;
+        //     // },
+        //     background: "black"
+        // },
+        // "& $checkbox_input:checked": {
+        //     "&:after, &:before": {
+        //         position: "absolute",
+        //         zIndex: "1",
+        //         content: "''",
+        //         borderRadius: toPx(2),
+        //         width: toPx(2),
+        //         background: "black"
+        //     }
+        // }
     },
     checkbox_label: {
         color: (config: IDesignSystem): string => {
