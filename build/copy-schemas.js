fe914d3e27a51f37aea8ad125b183b229c7f58c9
Jane Chu <7559015+janechu@users.noreply.github.com>
2018-05-11 16:00:54 -0700

    update the comments in the copy-schemas script

--- build/copy-schemas.js
+++ build/copy-schemas.js
@@ -1,24 +1,19 @@
 /**
- * Utility for copy files to directories.
+ * Utility for copying schemas to their dist folders.
  * Usage: node build/copy-schemas.js
  */
 const path = require("path");
 const fs = require("fs");
 const glob = require("glob");
 
-/**
- * All files passed to the copy script
- * Note: the first match will be referred to in the src argument and the rest of the matches
- * will be in the _ array. All of the paths are combined to create the complete source path array.
- */
 const rootDir = path.resolve(process.cwd());
 const srcSchemaPaths = "src/**/*.schema.json";
 const destDir = "dist";
 
 /**
- * Function to copy a src file a given path
+ * Function to copy schema files to their dist folder
  */
-function copyPath() {
+function copySchemaFiles() {
     const resolvedSrcSchemaPaths = path.resolve(rootDir, srcSchemaPaths);
 
     glob(resolvedSrcSchemaPaths, void(0), function(error, files) {
@@ -32,4 +27,4 @@ function copyPath() {
 /**
  * Copy all files
  */
-copyPath();
+copySchemaFiles();
