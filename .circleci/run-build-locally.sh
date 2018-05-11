0e11579e790db2b3b0afab5703a6bf2b2d51dbe9
Aaron Wentzel <awentzel@users.noreply.github.com>
2018-05-10 17:28:45 -0700

    update circle-ci local bash script syntax error

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_CI_TOKEN}: \
     --request POST \
-    --form revision=403b42af280ec3023a06826f4edaceda8a1f8a2d\
+    --form revision=38387d3e7a37b449ff958c43593e4e1a2fe42401 \
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/Microsoft/fast-dna/tree/master
