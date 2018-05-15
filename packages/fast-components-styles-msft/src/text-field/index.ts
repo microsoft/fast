0b44fc4c3ae6a2d0d6a8fb75e7bac1e0bd2544a6
emathiasen <eric.mathiasen@gmail.com>
2018-05-15 11:04:33 -0700

    refactor "applyMixedColor" method

--- packages/fast-components-styles-msft/src/text-field/index.ts
+++ packages/fast-components-styles-msft/src/text-field/index.ts
@@ -5,67 +5,38 @@ import { toPx } from "@microsoft/fast-jss-utilities";
 import { applyType } from "../utilities/typography";
 import * as Chroma from "chroma-js";
 
-function applyMixedColor(incomingProperty: string, mixValue?: number, alpha?: number): ICSSRules<IDesignSystem> {
-
-    function applyColors(config: IDesignSystem): string {
+function applyMixedColor(mixValue?: number, alpha?: number): (config: IDesignSystem) => string {
+    return function applyColors(config: IDesignSystem): string {
         if (alpha) {
             return Chroma.mix(config.foregroundColor, config.backgroundColor, mixValue).alpha(alpha).css();
         } else {
             return Chroma.mix(config.foregroundColor, config.backgroundColor, mixValue).css();
         }
-    }
-
-    switch (incomingProperty) {
-        case "background":
-            return {background: (config: IDesignSystem): string => {
-                return applyColors(config);
-            }
-        };
-        case "backgroundColor":
-            return {backgroundColor: (config: IDesignSystem): string => {
-                return applyColors(config);
-            }
-        };
-        case "borderColor":
-            return {borderColor: (config: IDesignSystem): string => {
-                return applyColors(config);
-            }
-        };
-        case "color":
-            return {color: (config: IDesignSystem): string => {
-                return applyColors(config);
-            }
-        };
-        case "outlineColor":
-            return {outlineColor: (config: IDesignSystem): string => {
-                return applyColors(config);
-            }
-        };
-    }
+    };
 }
 
 const styles: ComponentStyles<ITextFieldClassNameContract, IDesignSystem> = {
     textField: {
         ...applyType("t7", "vp1"),
-        ...applyMixedColor("color", 0.46),
+        color: applyMixedColor(0.46),
         fontWeight: "100",
         outline: `${toPx(1)} solid transparent`,
         border: `${toPx(1)} solid transparent`,
-        ...applyMixedColor("borderColor", 0.46),
+        borderColor: applyMixedColor(0.46),
         boxSizing: "border-box",
         borderRadius: toPx(2),
         padding: toPx(10),
         margin: toPx(10),
         "&:focus": {
-            ...applyMixedColor("outlineColor", 0.38),
-            ...applyMixedColor("borderColor", 0.38)
+            outlineColor: applyMixedColor(0.38),
+            borderColor: applyMixedColor(0.38)
         },
         "&:disabled": {
             background: (config: IDesignSystem): string => {
                 return config.backgroundColor;
             },
-            ...applyMixedColor("borderColor", 0.863),
-            ...applyMixedColor("color", 0.80),
+            borderColor: applyMixedColor(0.863),
+            color: applyMixedColor(0.80),
             cursor: "not-allowed"
         }
     }
