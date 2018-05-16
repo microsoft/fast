52380907b49a5ae4af1cf0386129e491e0f917f1
nalogiudice <random.interval@gmail.com>
2018-05-16 11:59:35 -0700

    Fixes vertical alignment svgs, updates const for readability

--- packages/fast-form-generator-react/src/form/form-item.align-vertical.style.ts
+++ packages/fast-form-generator-react/src/form/form-item.align-vertical.style.ts
@@ -2,6 +2,15 @@ import { applyInputBackplateStyle, applyLabelStyle, applyWrapperStyle, colors } 
 import { ComponentStyles, ICSSRules } from "@microsoft/fast-jss-manager";
 import { IFormItemAlignVerticalClassNameContract } from "../class-name-contracts/";
 
+/* tslint:disable */
+const topDark: string = "url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+PHRpdGxlPnZ0MjwvdGl0bGU+PHBhdGggZD0iTTExLjYxLDQuNTl2LjQ2SDQuNDFWNC41OVptLTUuODYuOTNoMS44djUuOTFINS43NVpNNi4yMSwxMWguOTJWNkg2LjIxWk04LjQ3LDUuNTJoMS44VjkuNThIOC40N1ptLjQ2LDMuNjVoLjkzVjZIOC45M1oiLz48L3N2Zz4=) center no-repeat";
+const topLight: string = "url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiBmaWxsPSJ3aGl0ZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2aWV3Qm94PSIwIDAgMTYgMTYiPjx0aXRsZT52dDI8L3RpdGxlPjxwYXRoIGQ9Ik0xMS42MSw0LjU5di40Nkg0LjQxVjQuNTlabS01Ljg2LjkzaDEuOHY1LjkxSDUuNzVaTTYuMjEsMTFoLjkyVjZINi4yMVpNOC40Nyw1LjUyaDEuOFY5LjU4SDguNDdabS40NiwzLjY1aC45M1Y2SDguOTNaIi8+PC9zdmc+) center no-repeat";
+const centerDark: string = "url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+PHRpdGxlPnZtMjwvdGl0bGU+PHBhdGggZD0iTTEyLjE5LDguMjRIMTAuNjR2Mi4xSDguNTRWOC4yNEg3LjQ3djMuMTdINS4zOFY4LjI0SDMuODNWNy43SDUuMzhWNC41M0g3LjQ3VjcuN0g4LjU0VjUuNjFoMi4xVjcuN2gxLjU1Wk02Ljk0LDUuMDdINS44NnY1LjhINi45NFpNMTAuMSw2LjE0SDlWOS44NUgxMC4xWiIvPjwvc3ZnPg==) center no-repeat";
+const centerLight: string = "url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiBmaWxsPSJ3aGl0ZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2aWV3Qm94PSIwIDAgMTYgMTYiPjx0aXRsZT52bTI8L3RpdGxlPjxwYXRoIGQ9Ik0xMi4xOSw4LjI0SDEwLjY0djIuMUg4LjU0VjguMjRINy40N3YzLjE3SDUuMzhWOC4yNEgzLjgzVjcuN0g1LjM4VjQuNTNINy40N1Y3LjdIOC41NFY1LjYxaDIuMVY3LjdoMS41NVpNNi45NCw1LjA3SDUuODZ2NS44SDYuOTRaTTEwLjEsNi4xNEg5VjkuODVIMTAuMVoiLz48L3N2Zz4=) center no-repeat";
+const bottomDark: string = "url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNiAxNiI+PHRpdGxlPnZiMjwvdGl0bGU+PHBhdGggZD0iTTExLjYzLDExdi40Nkg0LjM3VjExWm0tNC4wOS0uNDdINS43MlY0LjU2SDcuNTRaTTcuMDcsNUg2LjE0djVoLjkzWm0zLjIxLDUuNDlIOC40NlY2LjQyaDEuODJaTTkuODEsNi44NEg4Ljg4djMuMjFoLjkzWiIvPjwvc3ZnPg==) center no-repeat";
+const bottomLight: string = "url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZmlsbD0id2hpdGUiIGRhdGEtbmFtZT0iTGF5ZXIgMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2aWV3Qm94PSIwIDAgMTYgMTYiPjx0aXRsZT52YjI8L3RpdGxlPjxwYXRoIGQ9Ik0xMS42MywxMXYuNDZINC4zN1YxMVptLTQuMDktLjQ3SDUuNzJWNC41Nkg3LjU0Wk03LjA3LDVINi4xNHY1aC45M1ptMy4yMSw1LjQ5SDguNDZWNi40MmgxLjgyWk05LjgxLDYuODRIOC44OHYzLjIxaC45M1oiLz48L3N2Zz4=) center no-repeat";
+/* tslint:enable */
+
 const styles: ComponentStyles<IFormItemAlignVerticalClassNameContract, {}> = {
     formItemAlignVertical: {
         ...applyWrapperStyle()
@@ -11,31 +20,25 @@ const styles: ComponentStyles<IFormItemAlignVerticalClassNameContract, {}> = {
     },
     formItemAlignVertical_input__top: {
         ...applyInputBackplateStyle(),
-        /* tslint:disable-next-line */
-        background: "url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDE2IDE2OyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHBhdGggZD0iTTE1LjgsMC42djFIMC4ydi0xSDE1Ljh6IE0zLjEsMi42SDd2MTIuOEgzLjFWMi42eiBNNC4xLDE0LjRoMlYzLjZoLTJWMTQuNHogTTksMi42aDMuOXY4LjhIOVYyLjZ6IE0xMCwxMC41aDJWMy42aC0yVjEwLjV6Ii8+PC9zdmc+) center no-repeat",
+        background: topDark,
         "&:checked": {
-            /* tslint:disable-next-line */
-            background: "url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDE2IDE2OyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHN0eWxlIHR5cGU9InRleHQvY3NzIj4uc3Qwe2ZpbGw6I0ZGRkZGRjt9PC9zdHlsZT48cGF0aCBjbGFzcz0ic3QwIiBkPSJNMTUuOCwwLjZ2MUgwLjJ2LTFIMTUuOHogTTMuMSwyLjZIN3YxMi44SDMuMVYyLjZ6IE00LjEsMTQuNGgyVjMuNmgtMlYxNC40eiBNOSwyLjZoMy45djguOEg5VjIuNnogTTEwLDEwLjVoMlYzLjZoLTJWMTAuNXoiLz48L3N2Zz4=) center no-repeat",
+            background: topLight,
             backgroundColor: colors.pink
         }
     },
     formItemAlignVertical_input__center: {
         ...applyInputBackplateStyle(),
-        /* tslint:disable-next-line */
-        background: "url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDE2IDE2OyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHBhdGggZD0iTTE1LjgsOC41aC0yLjl2My45SDlWOC41SDd2NS45SDMuMVY4LjVIMC4ydi0xaDIuOVYxLjZIN3Y1LjloMlYzLjZoMy45djMuOWgyLjlWOC41eiBNNiwyLjZoLTJ2MTAuOGgyVjIuNnogTTExLjksNC42aC0ydjYuOWgyVjQuNnoiLz48L3N2Zz4=) center no-repeat",
+        background: centerDark,
         "&:checked": {
-            /* tslint:disable-next-line */
-            background: "url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDE2IDE2OyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHN0eWxlIHR5cGU9InRleHQvY3NzIj4uc3Qwe2ZpbGw6I0ZGRkZGRjt9PC9zdHlsZT48cGF0aCBjbGFzcz0ic3QwIiBkPSJNMTUuOCw4LjVoLTIuOXYzLjlIOVY4LjVIN3Y1LjlIMy4xVjguNUgwLjJ2LTFoMi45VjEuNkg3djUuOWgyVjMuNmgzLjl2My45aDIuOUMxNS44LDcuNSwxNS44LDguNSwxNS44LDguNXogTTYsMi42SDR2MTAuOGgyVjIuNnogTTExLjksNC42aC0ydjYuOWgyVjQuNnoiLz48L3N2Zz4=) center no-repeat",
+            background: centerLight,
             backgroundColor: colors.pink
         }
     },
     formItemAlignVertical_input__bottom: {
         ...applyInputBackplateStyle(),
-        /* tslint:disable-next-line */
-        background: "url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDE2IDE2OyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHBhdGggZD0iTTE1LjgsMTQuNHYxSDAuMnYtMUgxNS44eiBNNywxMy40SDMuMVYwLjZIN1YxMy40eiBNNiwxLjZoLTJ2MTAuOGgyVjEuNnogTTEyLjksMTMuNEg5VjQuNmgzLjlWMTMuNHogTTExLjksNS41aC0ydjYuOWgyVjUuNXoiLz48L3N2Zz4=) center no-repeat",
+        background: bottomDark,
         "&:checked": {
-            /* tslint:disable-next-line */
-            background: "url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDE2IDE2OyIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHN0eWxlIHR5cGU9InRleHQvY3NzIj4uc3Qwe2ZpbGw6I0ZGRkZGRjt9PC9zdHlsZT48cGF0aCBjbGFzcz0ic3QwIiBkPSJNMTUuOCwxNC40djFIMC4ydi0xSDE1Ljh6IE03LDEzLjRIMy4xVjAuNkg3VjEzLjR6IE02LDEuNkg0djEwLjhoMlYxLjZ6IE0xMi45LDEzLjRIOVY0LjZoMy45VjEzLjR6IE0xMS45LDUuNWgtMnY2LjloMlY1LjV6Ii8+PC9zdmc+) center no-repeat",
+            background: bottomLight,
             backgroundColor: colors.pink
         }
     }
