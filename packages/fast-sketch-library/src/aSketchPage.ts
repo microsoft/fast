56028c904b231bdde36dacfa8c3b35c0b2fe880f
Nick Rice <nicholas.rice119@gmail.com>
2018-05-31 15:27:30 -0800

    adding better layer names

--- packages/fast-sketch-library/src/aSketchPage.ts
+++ packages/fast-sketch-library/src/aSketchPage.ts
@@ -1,4 +1,4 @@
-import { Page, nodeToSketchLayers, SymbolMaster } from "@brainly/html-sketchapp";
+import { Page, Text, nodeToSketchLayers, SymbolMaster } from "@brainly/html-sketchapp";
 
 export function getAsketchSymbols(source): JSON[] {
     const nodes = Array.from(document.querySelectorAll(source.selectors));
@@ -29,7 +29,14 @@ function convertNodeToSketchLayers(node: Element): any[] {
     const layers = nodeToSketchLayers(node);
 
     return layers.map((layer: any) => {
-        layer.setName("New layer");
+        if (!(layer instanceof Text) && node.classList && node.classList.length) {
+            let classes = Array.from(node.classList).join(" ");
+
+            // Trim unique class ids created by JSS
+            let trimmed = classes.replace(/[-0-9]+/, "");
+            layer.setName(trimmed.replace(/[^A-Za-z0-9_]/g, " "))
+        }
+
         return layer;
     });
 }
