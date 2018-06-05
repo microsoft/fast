88c4d2a6cfe15ca730c36fcd689810af66b4aeb2
Chris Holt <chhol@microsoft.com>
2018-06-05 16:47:06 -0700

    remove unused import

--- packages/fast-components-styles-msft/src/utilities/colors.ts
+++ packages/fast-components-styles-msft/src/utilities/colors.ts
@@ -1,5 +1,4 @@
 import * as Chroma from "chroma-js";
-import { IDesignSystem } from "../design-system";
 
 export function applyMixedColor(color1: string, color2: string, mixValue: number, alpha: number = 1): string {
     return Chroma.mix(color1, color2, mixValue).alpha(alpha).css();
