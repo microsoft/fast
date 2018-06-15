83c08b218191688bbad6b5f87af645579e1e91f9
Chris Holt <chhol@microsoft.com>
2018-06-15 11:18:35 -0700

    fix(fast-layouts-react): fix export issue to allow Column, Grid, and Page components to be imported from the root level of the package (#554)

--- packages/fast-layouts-react/src/index.ts
+++ packages/fast-layouts-react/src/index.ts
@@ -2,9 +2,21 @@ import Canvas from "./canvas";
 export { Canvas };
 export * from "./canvas";
 
+import Column from "./column";
+export { Column };
+export * from "./column";
+
 import Container from "./container";
 export { Container };
 export * from "./container";
+
+import Grid from "./grid";
+export { Grid };
+export * from "./grid";
+
+import Page from "./page";
+export { Page };
+export * from "./page";
 
 import Pane from "./pane";
 export { Pane };
