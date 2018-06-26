f015e1ae96d0be84ac9a5a96cdfa7af7c3943f65
Jeff Smith <jes@microsoft.com>
2018-06-26 14:11:27 -0700

    fix for text field example not rendering

--- packages/fast-components-react-msft/src/text-field/examples.data.tsx
+++ packages/fast-components-react-msft/src/text-field/examples.data.tsx
@@ -1,13 +1,20 @@
+import * as React from "react";
 import { ISnapshotTestSuite } from "@microsoft/fast-jest-snapshots-react";
 import { IManagedClasses } from "@microsoft/fast-jss-manager-react";
 import TextField from "./index";
 import { ITextFieldHandledProps, TextFieldType } from "@microsoft/fast-components-react-base";
 import * as schema from "@microsoft/fast-components-react-base/dist/text-field/text-field.schema.json";
+import Documentation from "./.tmp/documentation";
 
 export default {
     name: "text field",
     component: TextField,
     schema: schema as any,
+    documentation: <Documentation />,
+    detailData: {
+        type: TextFieldType.email,
+        placeholder: "name@email.com"
+    },
     data: [
         {
             placeholder: "Placeholder",
