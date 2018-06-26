a19a3178b431a9b0d01fc41154f715f8d12032e4
Jane Chu <7559015+janechu@users.noreply.github.com>
2018-06-26 12:25:15 -0700

    addressing PR comment

--- packages/fast-jss-manager-angular/app/app.component.ts
+++ packages/fast-jss-manager-angular/app/app.component.ts
@@ -1,35 +1,39 @@
 import { Component } from "@angular/core";
 
+export interface IDesignSystem {
+  red: any;
+  blue: any;
+  purple: any;
+}
+
 @Component({
   selector: "app-root",
   template: `<div>
-                <design-system [config]="redConfig">
+                <design-system [config]="config.red">
                   <example>red</example>
                 </design-system>
-                <design-system [config]="blueConfig">
+                <design-system [config]="config.blue">
                   <example>blue</example>
-                  <design-system [config]="purpleConfig">
+                  <design-system [config]="config.purple">
                     <example>purple</example>
                   </design-system>
                 </design-system>
             </div>`
 })
 export class AppComponent {
-  private redConfig: any;
-  private blueConfig: any;
-  private purpleConfig: any;
+  private config: IDesignSystem;
 
   constructor() {
-    this.redConfig = {
-      color: "red"
-    };
-
-    this.blueConfig = {
-      color: "blue"
-    };
-
-    this.purpleConfig = {
-      color: "purple"
-    };
+    this.config = {
+      red: {
+        color: "red"
+      },
+      blue: {
+        color: "blue"
+      },
+      purple: {
+        color: "purple"
+      }
+    }
   }
 }
