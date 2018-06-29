dcf945b04d2f68071d09581f9a99eea4a0c0bdaa
Jeff Smith <jes@microsoft.com>
2018-06-29 10:05:16 -0700

    updated background property

--- packages/fast-development-site-react/src/components/site/component-wrapper.tsx
+++ packages/fast-development-site-react/src/components/site/component-wrapper.tsx
@@ -41,7 +41,7 @@ const styles: ComponentStyles<IComponentWrapperManagedClasses, IDevSiteDesignSys
         borderBottom: componentWrapperBorder,
     },
     componentWrapper__transparent: {
-        background: `transparent 0 ${toPx(16)}/${toPx(16)} ${checker} repeat`
+        background: `${checker}`
     },
     componentWrapperExamples: {
         "&:last-child:not(:nth-child(3n + 3)), &:nth-child(3n + 1)": {
