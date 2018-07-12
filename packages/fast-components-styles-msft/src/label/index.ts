3f3061c70d641130feb52ad660978a48dc63702b
Jeff Smith <jes@microsoft.com>
2018-07-12 12:28:43 -0700

    added dark theme support for label

--- packages/fast-components-styles-msft/src/label/index.ts
+++ packages/fast-components-styles-msft/src/label/index.ts
@@ -1,14 +1,18 @@
-import { IDesignSystem } from "../design-system";
+import designSystemDefaults, { IDesignSystem } from "../design-system";
 import { ComponentStyles } from "@microsoft/fast-jss-manager";
 import { ILabelClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
 import { toPx } from "@microsoft/fast-jss-utilities";
 import { applyType } from "../utilities/typography";
 import { applyScreenReader } from "@microsoft/fast-jss-utilities";
+import { get } from "lodash-es";
 
 const styles: ComponentStyles<ILabelClassNameContract, IDesignSystem> = {
     label: {
         ...applyType("t8", "vp3"),
-        display: "inline-block"
+        display: "inline-block",
+        color: (config: IDesignSystem): string => {
+            return get(config, "foregroundColor") || designSystemDefaults.foregroundColor;
+        }
     },
     label_hidden: {
         ...applyScreenReader()
