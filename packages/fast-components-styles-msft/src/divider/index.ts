85d07d77f806b72ed2a2d8a6f012e5934e252e13
Jeff Smith <37851214+eljefe223@users.noreply.github.com>
2018-07-13 13:26:27 -0700

    fix(divider): remove hard coded color value on divider to support theming (#675)
    * theme support on divider
    
    * added dark theme support for divider
    
    * lint error fix
    
    * addressed PR comment

--- packages/fast-components-styles-msft/src/divider/index.ts
+++ packages/fast-components-styles-msft/src/divider/index.ts
@@ -1,16 +1,21 @@
-import { IDesignSystem } from "../design-system";
+import designSystemDefaults, { IDesignSystem } from "../design-system";
 import { ComponentStyles } from "@microsoft/fast-jss-manager";
 import { IDividerClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
 import { toPx } from "@microsoft/fast-jss-utilities";
+import { get } from "lodash-es";
+import Chroma from "chroma-js";
 
 const styles: ComponentStyles<IDividerClassNameContract, IDesignSystem> = {
+
     divider: {
         boxSizing: "content-box",
         height: toPx(0),
         marginTop: toPx(12),
         marginBottom: toPx(12),
         border: "none",
-        borderTop: `${toPx(1)} solid rgba(0,0,0,0.2)`
+        borderTop: (config: IDesignSystem): string => {
+            return `${toPx(1)} solid ${Chroma(get(config, "foregroundColor") || designSystemDefaults.foregroundColor).alpha(0.2).css()}`;
+        }
     }
 };
 
