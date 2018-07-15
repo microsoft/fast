615c524e8e0916259ba99c31c978986092b29aa3
Chris Holt <chhol@microsoft.com>
2018-07-15 02:46:22 -0700

    add unit tests for hypertext

--- packages/fast-components-react-base/src/hypertext/hypertext.spec.ts
+++
@@ -1,6 +0,0 @@
-import examples from "./examples.data";
-import { generateSnapshots } from "@microsoft/fast-jest-snapshots-react";
-
-describe("hypertext", (): void => {
-    generateSnapshots(examples);
-});
---
+++ packages/fast-components-react-base/src/hypertext/hypertext.spec.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import * as ShallowRenderer from "react-test-renderer/shallow";
+import * as Adapter from "enzyme-adapter-react-16";
+import { configure, shallow } from "enzyme";
+import examples from "./examples.data";
+import { generateSnapshots } from "@microsoft/fast-jest-snapshots-react";
+import { IHypertextClassNameContract } from "./hypertext";
+import {
+    HypertextProps,
+    IHypertextHandledProps,
+    IHypertextManagedClasses,
+    IHypertextUnhandledProps
+} from "./hypertext.props";
+
+/*
+ * Configure Enzyme
+ */
+configure({adapter: new Adapter()});
+
+describe("hypertext", (): void => {
+    generateSnapshots(examples);
+});
+
+describe("hypertext unit-tests", (): void => {
+    let Component: React.ComponentClass<IHypertextHandledProps & IHypertextManagedClasses>;
+    let managedClasses: IHypertextClassNameContract;
+
+    beforeEach(() => {
+        Component = examples.component;
+        managedClasses = {
+            hypertext: "hypertext-class"
+        };
+    });
+
+    test("should correctly manage unhandledProps", () => {
+        const handledProps: IHypertextHandledProps & IHypertextManagedClasses = {
+            managedClasses
+        };
+
+        const unhandledProps: IHypertextUnhandledProps = {
+            "aria-hidden": true
+        };
+
+        const props: HypertextProps = {...handledProps, ...unhandledProps};
+
+        const rendered: any = shallow(
+            <Component {...props} />
+        );
+
+        expect(rendered.props()["aria-hidden"]).not.toBe(undefined);
+        expect(rendered.props()["aria-hidden"]).toEqual(true);
+    });
+});
