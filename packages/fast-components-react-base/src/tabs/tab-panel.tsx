34c13f1f1f35c7e7cef53c963e9edd55e9639efb
Jane Chu <7559015+janechu@users.noreply.github.com>
2018-08-04 15:24:17 -0700

    add handled props defaults to sub components

--- packages/fast-components-react-base/src/tabs/tab-panel.tsx
+++ packages/fast-components-react-base/src/tabs/tab-panel.tsx
@@ -3,6 +3,11 @@ import Foundation, { HandledProps } from "../foundation";
 import { ITabPanelHandledProps, ITabPanelUnhandledProps } from "./tab-panel.props";
 
 class TabPanel extends Foundation<ITabPanelHandledProps, ITabPanelUnhandledProps, {}> {
+    protected handledProps: HandledProps<ITabPanelHandledProps> = {
+        children: void 0,
+        slot: void 0
+    };
+
     public render(): JSX.Element {
         return (
             <React.Fragment>
--- packages/fast-components-react-base/src/tabs/tab.tsx
+++ packages/fast-components-react-base/src/tabs/tab.tsx
@@ -3,6 +3,11 @@ import Foundation, { HandledProps } from "../foundation";
 import { ITabHandledProps, ITabUnhandledProps } from "./tab.props";
 
 class Tab extends Foundation<ITabHandledProps, ITabUnhandledProps, {}> {
+    protected handledProps: HandledProps<ITabHandledProps> = {
+        children: void 0,
+        slot: void 0
+    };
+
     public render(): JSX.Element {
         return (
             <React.Fragment>
