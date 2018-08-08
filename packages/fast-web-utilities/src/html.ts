d3fa0027f25e6fbcf689cc417d7a0a782ffa7cf0
Chris Holt <chhol@microsoft.com>
2018-08-08 10:38:59 -0700

    fix(fast-web-utilities): fix an issue where getClientRectWithMargin threw an error in EDGE and IE strict modes due to readonly properties (#767)
    * fix typings and return new object for getClientRectWithMargin to ensure compliance with IE/Edge strict mode
    
    * add typing where missing

--- packages/fast-web-utilities/src/html.ts
+++ packages/fast-web-utilities/src/html.ts
@@ -1,13 +1,30 @@
+export interface IClientRectWithMargin {
+    width: number;
+    height: number;
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+}
+
 /**
  * Gets the client bounding rectangle including any margins of an element.
  */
-export function getClientRectWithMargin(element: HTMLElement): ClientRect | DOMRect {
+export function getClientRectWithMargin(element: HTMLElement): IClientRectWithMargin {
     if (!element) {
         return;
     }
 
-    const clone: any = element.getBoundingClientRect();
+    const rect: ClientRect = element.getBoundingClientRect();
     const style: CSSStyleDeclaration = window.getComputedStyle(element, null);
+    const clone: IClientRectWithMargin = {
+        width: rect.width,
+        height: rect.height,
+        top: rect.top,
+        bottom: rect.bottom,
+        left: rect.left,
+        right: rect.right
+    };
 
     clone.width += convertStylePropertyPixelsToNumber(style, "margin-left");
     clone.width += convertStylePropertyPixelsToNumber(style, "margin-right");
