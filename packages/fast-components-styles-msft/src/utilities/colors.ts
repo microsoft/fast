6f98fdf964593073e0ddce9843afc703f8764221
Nick Rice <nicholas.rice119@gmail.com>
2018-08-08 17:45:05 -0700

    adding wcag contrast colors

--- packages/fast-components-styles-msft/src/utilities/colors.ts
+++ packages/fast-components-styles-msft/src/utilities/colors.ts
@@ -1,5 +1,7 @@
 import Chroma from "chroma-js";
 import withDesignSystemDefaults, { IDesignSystem } from "../design-system";
+import { contrast, WCAGAAContrastRatios } from "@microsoft/fast-jss-utilities";
+import { curry } from "lodash-es";
 
 export function applyMixedColor(color1: string, color2: string, mixValue: number, alpha: number = 1): string {
     return Chroma.mix(color1, color2, mixValue).alpha(alpha).css();
@@ -10,3 +12,14 @@ export enum ContrastModifiers {
     hover = 1,
     disabled = 3
 }
+
+/**
+ * An instance of the contrast function with it's target ratio pre-bound
+ */
+// TODO: these need to use the scaleContrast tooling
+export const normalContrast: (operandColor: string, referenceColor: string) => string = curry(contrast)(WCAGAAContrastRatios.normal);
+
+/**
+ * An instance of the contrast function with it's target ratio pre-bound
+ */
+export const largeContrast: (operandColor: string, referenceColor: string) => string = curry(contrast)(WCAGAAContrastRatios.large);
