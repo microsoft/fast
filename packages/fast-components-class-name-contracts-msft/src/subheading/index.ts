415775d209a3b4b9a8c5c2f036d6a92693ca4d4c
Mark Jones <marjon@microsoft.com>
2018-08-30 13:14:41 -0700

    ci found lint issue, fixed

--- packages/fast-components-class-name-contracts-msft/src/subheading/index.ts
+++ packages/fast-components-class-name-contracts-msft/src/subheading/index.ts
@@ -2,10 +2,10 @@
  * The class name contract for the subheading component
  */
 export interface ISubheadingClassNameContract {
-    subheading_1: string,
-    subheading_2: string,
-    subheading_3: string,
-    subheading_4: string,
-    subheading_5: string,
-    subheading_6: string
+    subheading_1: string;
+    subheading_2: string;
+    subheading_3: string;
+    subheading_4: string;
+    subheading_5: string;
+    subheading_6: string;
 }
