1bfd8b982d14886d3feb09bdc7835fa961cb1049
Mark Jones <marjon@microsoft.com>
2018-08-30 11:13:48 -0700

    changed all subheading styles to use fontweight.normal

--- packages/fast-components-styles-msft/src/subheading/index.ts
+++ packages/fast-components-styles-msft/src/subheading/index.ts
@@ -5,13 +5,13 @@ import { fontWeight } from "../utilities/fonts";
 
 const styles: ComponentStyles<ISubheadingClassNameContract, IDesignSystem> = {
     subheading_1: {
-        fontWeight: `${fontWeight.bold}`
+        fontWeight: `${fontWeight.normal}`
     },
     subheading_2: {
-        fontWeight: `${fontWeight.bold}`
+        fontWeight: `${fontWeight.normal}`
     },
     subheading_3: {
-        fontWeight: `${fontWeight.bold}`
+        fontWeight: `${fontWeight.normal}`
     },
     subheading_4: {
         fontWeight: `${fontWeight.normal}`
