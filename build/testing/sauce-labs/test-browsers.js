16b7418ee64cc3851d5918a3b1c942dd8cb1afd0
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2018-09-14 17:12:53 -0700

    add git branch name to use as build name on sauce labs

--- build/testing/sauce-labs/test-browsers.js
+++ build/testing/sauce-labs/test-browsers.js
@@ -26,6 +26,8 @@
 const { Builder } = require("selenium-webdriver");
 const chalk = require('chalk');
 let { Configure, Phases } = require("../config-browsers.js");
+const { spawn } = require('child_process');
+const git = spawn('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
 
 // Retrive user/key from environment variables
 const username = process.env.SAUCE_LABS_USER;
@@ -63,109 +65,131 @@ const getConfiguration = (phases) => {
     } catch(err) {
         console.error(err)
     }
-
 }
 
 // Assign browser configurations to an array
 let browsers = getConfiguration(process.argv[2]);
 
-// Execute Selenium/Appium Web Drivers on Sauce Labs for each browser configuration
-var flows = browsers.map(function (browser) {
+function Test(branchName) {
+    console.log("Testing Git branch: ", branchName);
 
-    // Setup capabilities
-    let caps = {
-            name: "FAST-DNA MSFT Documentation",
-            build: "test-build-0022",
-            tags: "msft-docs",
-            appiumVersion: browser.appiumVersion,
-            platform: browser.platform,
-            platformName: browser.platformName,
-            platformVersion: browser.platformVersion,
-            deviceName: browser.deviceName,
-            orientation: browser.deviceOrientation,
-            browserName: browser.browserName,
-            version: browser.version,
-            screen_resolution: browser.screenResolution,
-            language: browser.language,
-            locale: browser.locale,
-            username: username,
-            password: accessKey,
-            extendedDebugging: true
-    };
+    // Execute Selenium/Appium Web Drivers on Sauce Labs for each browser configuration
+    var flows = browsers.map(function (browser) {
 
-    // Setup WebDriver
-    let driver = new Builder()
-            .withCapabilities(caps)
-            .usingServer(remoteHub)
-            .build();
+        // Setup capabilities
+        let caps = {
+                name: "FAST-DNA MSFT Documentation",
+                build: branchName,
+                tags: "msft-docs",
+                appiumVersion: browser.appiumVersion,
+                platform: browser.platform,
+                platformName: browser.platformName,
+                platformVersion: browser.platformVersion,
+                deviceName: browser.deviceName,
+                orientation: browser.deviceOrientation,
+                browserName: browser.browserName,
+                version: browser.version,
+                screen_resolution: browser.screenResolution,
+                language: browser.language,
+                locale: browser.locale,
+                username: username,
+                password: accessKey,
+                extendedDebugging: true
+        };
 
-    // Start session and execute test cases
-    driver.getSession().then(async function (session) {
+        // Setup WebDriver
+        let driver = new Builder()
+                .withCapabilities(caps)
+                .usingServer(remoteHub)
+                .build();
 
-            let sessionId = session.id_;
+        // Start session and execute test cases
+        driver.getSession().then(async function (session) {
 
-            try {
-                    await driver.get(domain);
+                let sessionId = session.id_;
 
-                    // Note: There are many ways to nagivate/find elements on a page
-                    // unfortunately, not all are cross-browser compliant and so
-                    // for example, we can't use xpath. As a result, we're using
-                    // navigation though not necessarily most performant but effective.
+                try {
+                        await driver.get(domain);
 
-                    // After browsers matrix is determined we can fine tune and
-                    // optimize to use other selectors to see what works. If we're
-                    // t8esting mostly on modern browsers this isn't as much a limitation.
+                        // Note: There are many ways to nagivate/find elements on a page
+                        // unfortunately, not all are cross-browser compliant and so
+                        // for example, we can't use xpath. As a result, we're using
+                        // navigation though not necessarily most performant but effective.
 
-                    // Recommendation, add in data-test attributes to each component,
-                    // page, layout, or container we want to capture and select on that.
+                        // After browsers matrix is determined we can fine tune and
+                        // optimize to use other selectors to see what works. If we're
+                        // t8esting mostly on modern browsers this isn't as much a limitation.
 
-                    // Iterate each components documentation
-                    await driver.navigate().to(domain + "/components/button/");
-                    await driver.navigate().to(domain + "/components/caption/");
-                    await driver.navigate().to(domain + "/components/checkbox/");
-                    await driver.navigate().to(domain + "/components/dialog/");
-                    await driver.navigate().to(domain + "/components/divider/");
-                    await driver.navigate().to(domain + "/components/flipper/");
-                    await driver.navigate().to(domain + "/components/heading/");
-                    await driver.navigate().to(domain + "/components/hypertext/");
-                    await driver.navigate().to(domain + "/components/image/");
-                    await driver.navigate().to(domain + "/components/label/");
-                    await driver.navigate().to(domain + "/components/metatext/");
-                    await driver.navigate().to(domain + "/components/paragraph/");
-                    await driver.navigate().to(domain + "/components/subheading/");
-                    await driver.navigate().to(domain + "/components/text-field/");
-                    await driver.navigate().to(domain + "/components/toggle/");
-                    await driver.navigate().to(domain + "/components/typography/");
+                        // Recommendation, add in data-test attributes to each component,
+                        // page, layout, or container we want to capture and select on that.
 
-                    // Turn on Developer tools and iterate each component
-                    // xpath selectors do not work across browsers. This in particular breaks in Internet Explorer
-                    // await driver.findElement(By.xpath("//button[text()='dev tools']")).click();
-                    // one possible solution would be to inject data attributes for each component we can bind to.
-                    // For example, data-test="friendly name"
-                    // driver.findElement(By.cssSelector('[data-element="city"]'))
+                        // Iterate each components documentation
+                        await driver.navigate().to(domain + "/components/button/");
+                        await driver.navigate().to(domain + "/components/caption/");
+                        await driver.navigate().to(domain + "/components/checkbox/");
+                        await driver.navigate().to(domain + "/components/dialog/");
+                        await driver.navigate().to(domain + "/components/divider/");
+                        await driver.navigate().to(domain + "/components/flipper/");
+                        await driver.navigate().to(domain + "/components/heading/");
+                        await driver.navigate().to(domain + "/components/hypertext/");
+                        await driver.navigate().to(domain + "/components/image/");
+                        await driver.navigate().to(domain + "/components/label/");
+                        await driver.navigate().to(domain + "/components/metatext/");
+                        await driver.navigate().to(domain + "/components/paragraph/");
+                        await driver.navigate().to(domain + "/components/subheading/");
+                        await driver.navigate().to(domain + "/components/text-field/");
+                        await driver.navigate().to(domain + "/components/toggle/");
+                        await driver.navigate().to(domain + "/components/typography/");
 
-                    // Navigate to example views for each component
-                    await driver.navigate().to(domain + "/components/button/examples");
-                    await driver.navigate().to(domain + "/components/caption/examples");
-                    await driver.navigate().to(domain + "/components/checkbox/examples");
-                    await driver.navigate().to(domain + "/components/dialog/examples");
-                    await driver.navigate().to(domain + "/components/divider/examples");
-                    await driver.navigate().to(domain + "/components/flipper/examples");
-                    await driver.navigate().to(domain + "/components/heading/examples");
-                    await driver.navigate().to(domain + "/components/hypertext/examples");
-                    await driver.navigate().to(domain + "/components/image/examples");
-                    await driver.navigate().to(domain + "/components/label/examples");
-                    await driver.navigate().to(domain + "/components/metatext/examples");
-                    await driver.navigate().to(domain + "/components/paragraph/examples");
-                    await driver.navigate().to(domain + "/components/subheading/examples");
-                    await driver.navigate().to(domain + "/components/text-field/examples");
-                    await driver.navigate().to(domain + "/components/toggle/examples");
-                    await driver.navigate().to(domain + "/components/typography/examples");
+                        // Turn on Developer tools and iterate each component
+                        // xpath selectors do not work across browsers. This in particular breaks in Internet Explorer
+                        // await driver.findElement(By.xpath("//button[text()='dev tools']")).click();
+                        // one possible solution would be to inject data attributes for each component we can bind to.
+                        // For example, data-test="friendly name"
+                        // driver.findElement(By.cssSelector('[data-element="city"]'))
 
+                        // Navigate to example views for each component
+                        await driver.navigate().to(domain + "/components/button/examples");
+                        await driver.navigate().to(domain + "/components/caption/examples");
+                        await driver.navigate().to(domain + "/components/checkbox/examples");
+                        await driver.navigate().to(domain + "/components/dialog/examples");
+                        await driver.navigate().to(domain + "/components/divider/examples");
+                        await driver.navigate().to(domain + "/components/flipper/examples");
+                        await driver.navigate().to(domain + "/components/heading/examples");
+                        await driver.navigate().to(domain + "/components/hypertext/examples");
+                        await driver.navigate().to(domain + "/components/image/examples");
+                        await driver.navigate().to(domain + "/components/label/examples");
+                        await driver.navigate().to(domain + "/components/metatext/examples");
+                        await driver.navigate().to(domain + "/components/paragraph/examples");
+                        await driver.navigate().to(domain + "/components/subheading/examples");
+                        await driver.navigate().to(domain + "/components/text-field/examples");
+                        await driver.navigate().to(domain + "/components/toggle/examples");
+                        await driver.navigate().to(domain + "/components/typography/examples");
 
-            } finally {
-                    await driver.quit();
-            }
-    })
+                } finally {
+                        await driver.quit();
+                }
+        })
+    });
+}
+
+/**
+ * Get Branch Name using child_process and Git which runs Asynchronously
+ */
+var getBranchName = new Promise(function(resolve, reject) {
+    git.stdout.on('data', (data) => {
+        resolve(`${data}`);
+    });
 });
 
+/**
+ * Once Branch name returns start testing
+ */
+getBranchName.then(function(branchName) {
+
+    // slice off '/n' at the end of string
+    branchName = branchName.slice(0, -2);
+
+    // Run Testing
+    Test(branchName);
+});
