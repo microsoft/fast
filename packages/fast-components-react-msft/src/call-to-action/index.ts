c3c102a6ba8d2698461d0a294a770935dedecb7f
Nick Rice <nicholas.rice119@gmail.com>
2018-09-18 12:50:00 -0700

    correct call-to-action export

--- packages/fast-components-react-msft/src/call-to-action/index.ts
+++ packages/fast-components-react-msft/src/call-to-action/index.ts
@@ -2,6 +2,14 @@ import * as React from "react";
 import { ICallToActionClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
 import manageJss, { IJSSManagerProps } from "@microsoft/fast-jss-manager-react";
 import { CallToActionStyles, IDesignSystem } from "@microsoft/fast-components-styles-msft";
-import CallToAction, { ICallToActionHandledProps } from "./call-to-action";
+import MSFTCallToAction, { ICallToActionHandledProps } from "./call-to-action";
 
-export default manageJss(CallToActionStyles)(CallToAction);
+/*
+ * The type returned by manageJss type is very complicated so we'll let the
+ * compiler infer the type instead of re-declaring just for the package export
+ */
+/* tslint:disable-next-line:typedef */
+const CallToAction = manageJss(CallToActionStyles)(MSFTCallToAction);
+type CallToAction = InstanceType<typeof CallToAction>;
+
+export { CallToAction };
