f4ada7bd64965c24695d62c6b07b5dc6da3a9f2e
Jane Chu <7559015+janechu@users.noreply.github.com>
2018-09-19 13:39:57 -0700

    chore: clean up build scripts

--- build/testing/config-browsers.js
+++ build/testing/config-browsers.js
@@ -13,18 +13,186 @@
  * released or as known unstable combinations become apparent.
  *
  */
+const Windows8_IE = {
+    'platform': 'Windows 8.1',
+    'browserName': 'internet explorer',
+    'version': '11.0',
+    'screenResolution': '1024x768'
+};
+const Windows10_MicrosoftEdge = {
+    'platform': 'Windows 10',
+    'browserName': 'MicrosoftEdge',
+    'version': '17.17134',
+    'screenResolution': '1280x1024'
+};
+const Windows10_IE = {
+    'platform': 'Windows 10',
+    'browserName': 'internet explorer',
+    'version': '11.103',
+    'screenResolution': '1400x1050'
+};
+const Windows10_Chrome_68 = {
+    'platform': 'Windows 10',
+    'browserName': 'chrome',
+    'version': '68.0',
+    'screenResolution': '1920x1200'
+};
+const Windows10_Chrome_67 = {
+    'platform': 'Windows 10',
+    'browserName': 'chrome',
+    'version': '67.0',
+    'screenResolution': '1920x1200'
+};
+const Windows10_Firefox = {
+    'platform': 'Windows 10',
+    'browserName': 'firefox',
+    'version': '61.0',
+    'screenResolution': '2560x1600'
+};
+const MacOS10_Safari_11_1 = {
+    'platform': 'macOS 10.13',
+    'browserName': 'safari',
+    'version': '11.1',
+    'screenResolution': '2048x1536'
+};
+const MacOS10_Safari_11_0 = {
+    'platform': 'macOS 10.12',
+    'browserName': 'safari',
+    'version': '11.0',
+    'screenResolution': '2048x1536'
+};
+const MacOS10_Safari_10_1 = {
+    'platform': 'macOS 10.12',
+    'browserName': 'safari',
+    'version': '10.1',
+    'screenResolution': '2360x1770'
+};
+const MacOS10_Firefox = {
+    'platform': 'macOS 10.13',
+    'browserName': 'firefox',
+    'version': '62.0',
+    'screenResolution': '2360x1770'
+};
+const MacOS10_Chrome = {
+    'platform': 'OS X 10.11',
+    'browserName': 'chrome',
+    'version': '69.0',
+    'screenResolution': '2048x1536'
+};
+const IOS_IPhoneX_Safari_Portrait = {
+    'platformName': 'iOS',
+    'platformVersion': '11.3',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'iPhone X Simulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Safari'
+};
+const IOS_IPhone8_Safari_Portrait = {
+    'platformName': 'iOS',
+    'platformVersion': '11.2',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'iPhone 8 Plus Simulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Safari'
+};
+const IOS_IPhone7_Safari_Portrait = {
+    'platformName': 'iOS',
+    'platformVersion': '11.1',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'iPhone 7 Simulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Safari'
+};
+const IOS_IPadPro_Safari_Portrait = {
+    'platformName': 'iOS',
+    'platformVersion': '9.3',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'iPad Pro Simulator',
+    'appiumVersion': '1.7.1',
+    'browserName': 'Safari'
+};
+const IOS_IPad_Safari_Portrait = {
+    'platformName': 'iOS',
+    'platformVersion': '10.0',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'iPad Retina Simulator',
+    'appiumVersion': '1.8.0',
+    'browserName': 'Safari'
+};
+const IOS_IPhone6S_Safari_Portrait = {
+    'platformName': 'iOS',
+    'platformVersion': '11.0',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'iPhone 6s Simulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Safari'
+};
+const Android_7_1_Chrome_Portrait = {
+    'platformName': 'Android',
+    'platformVersion': '7.1',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'Samsung Galaxy S7 Edge FHD GoogleAPI Emulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Chrome',
+    'language': 'en',
+    'locale': 'en_US'
+};
+const Android_7_0_Chrome_Portrait = {
+    'platformName': 'Android',
+    'platformVersion': '7.0',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'Samsung Galaxy Tab A 10 GoogleAPI Emulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Chrome',
+    'language': 'en',
+    'locale': 'en_US'
+};
+const Android_6_0_Chrome_Portrait = {
+    'platformName': 'Android',
+    'platformVersion': '6.0',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'Android Emulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Chrome',
+    'language': 'en',
+    'locale': 'en_US'
+};
+const Android_5_1_Chrome_Portrait = {
+    'platformName': 'Android',
+    'platformVersion': '5.1',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'Android GoogleAPI Emulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Browser'
+};
+const Android_5_0_Chrome_Portrait = {
+    'platformName': 'Android',
+    'platformVersion': '5.0',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'Android Emulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Browser'
+};
+const Android_4_4_Chrome_Portrait = {
+    'platformName': 'Android',
+    'platformVersion': '4.4',
+    'deviceOrientation': 'PORTRAIT',
+    'deviceName': 'LG Nexus 4 Emulator',
+    'appiumVersion': '1.8.1',
+    'browserName': 'Browser'
+};
 
  /**
  * Phases of software development
  * @type Enum
 */
-const Phases = Object.freeze({
+const Phase = Object.freeze({
     "alpha": "alpha",
     "beta": "beta",
     "release": "release"
 })
 
-exports.Phases = Phases;
+exports.Phase = Phase;
 
 /**
  * Platform configurations sourced from Selenium and Appium online Configurators.  
@@ -34,293 +202,56 @@ exports.Phases = Phases;
  * https://www.browserstack.com/automate/node 
  */
 var Configure = (function () {
-    function _configure(phases) {
+    function _configure(phase) {
 
-        switch (phases) {
-            case Phases.alpha:
+        switch (phase) {
+            case Phase.alpha:
                 return [
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'MicrosoftEdge',
-                        'version': '17.17134',
-                        'screenResolution': '1280x1024'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'internet explorer',
-                        'version': '11.103',
-                        'screenResolution': '1400x1050'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'chrome',
-                        'version': '68.0',
-                        'screenResolution': '1920x1200'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'firefox',
-                        'version': '61.0',
-                        'screenResolution': '2560x1600'
-                    },
-                    {
-                        'platform': 'macOS 10.13',
-                        'browserName': 'safari',
-                        'version': '11.1',
-                        'screenResolution': '2048x1536'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '11.3',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPhone X Simulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'Android',
-                        'platformVersion': '6.0',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'Android Emulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Chrome',
-                        'language': 'en',
-                        'locale': 'en_US'
-                    }
+                    Windows10_MicrosoftEdge,
+                    Windows10_IE,
+                    Windows10_Chrome_68,
+                    Windows10_Firefox,
+                    MacOS10_Safari_11_1,
+                    IOS_IPhoneX_Safari_Portrait,
+                    Android_6_0_Chrome_Portrait
                 ];
-            
-            case Phases.beta:
-                
+            case Phase.beta:
                 return [
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'MicrosoftEdge',
-                        'version': '17.17134',
-                        'screenResolution': '1280x1024'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'internet explorer',
-                        'version': '11.103',
-                        'screenResolution': '1400x1050'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'chrome',
-                        'version': '68.0',
-                        'screenResolution': '1920x1200'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'firefox',
-                        'version': '61.0',
-                        'screenResolution': '2560x1600'
-                    },
-                    {
-                        'platform': 'macOS 10.13',
-                        'browserName': 'safari',
-                        'version': '11.1',
-                        'screenResolution': '2048x1536'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '11.3',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPhone X Simulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '9.3',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPad Pro Simulator',
-                        'appiumVersion': '1.7.1',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'Android',
-                        'platformVersion': '6.0',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'Android Emulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Chrome',
-                        'language': 'en',
-                        'locale': 'en_US'
-                    }
+                    Windows10_MicrosoftEdge,
+                    Windows10_IE,
+                    Windows10_Chrome_68,
+                    Windows10_Firefox,
+                    MacOS10_Safari_11_1,
+                    IOS_IPhoneX_Safari_Portrait,
+                    IOS_IPadPro_Safari_Portrait,
+                    Android_6_0_Chrome_Portrait
                 ];
-                
-            case Phases.release:
+            case Phase.release:
             default:
                 return [
-                    {
-                        'platform': 'Windows 8.1',
-                        'browserName': 'internet explorer',
-                        'version': '11.0',
-                        'screenResolution': '1024x768'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'MicrosoftEdge',
-                        'version': '17.17134',
-                        'screenResolution': '1280x1024'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'internet explorer',
-                        'version': '11.103',
-                        'screenResolution': '1400x1050'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'chrome',
-                        'version': '67.0',
-                        'screenResolution': '1920x1080'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'chrome',
-                        'version': '68.0',
-                        'screenResolution': '1920x1200'
-                    },
-                    {
-                        'platform': 'Windows 10',
-                        'browserName': 'firefox',
-                        'version': '61.0',
-                        'screenResolution': '2560x1600'
-                    },
-                    {
-                        'platform': 'macOS 10.13',
-                        'browserName': 'safari',
-                        'version': '11.1',
-                        'screenResolution': '1920x1440'
-                    },
-                    {
-                        'platform': 'macOS 10.13',
-                        'browserName': 'firefox',
-                        'version': '62.0',
-                        'screenResolution': '2360x1770'
-                    },
-                    {
-                        'platform': 'macOS 10.12',
-                        'browserName': 'safari',
-                        'version': '11.0',
-                        'screenResolution': '2048x1536'
-                    },
-                    {
-                        'platform': 'macOS 10.12',
-                        'browserName': 'safari',
-                        'version': '10.1',
-                        'screenResolution': '2360x1770'
-                    },
-                    {
-                        'platform': 'OS X 10.11',
-                        'browserName': 'chrome',
-                        'version': '69.0',
-                        'screenResolution': '2048x1536'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '11.3',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPhone X Simulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '11.2',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPhone 8 Plus Simulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '11.1',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPhone 7 Simulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '11.0',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPhone 6s Simulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '10.0',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPad Retina Simulator',
-                        'appiumVersion': '1.8.0',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'iOS',
-                        'platformVersion': '9.3',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'iPad Pro Simulator',
-                        'appiumVersion': '1.7.1',
-                        'browserName': 'Safari'
-                    },
-                    {
-                        'platformName': 'Android',
-                        'platformVersion': '7.1',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'Samsung Galaxy S7 Edge FHD GoogleAPI Emulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Chrome',
-                        'language': 'en',
-                        'locale': 'en_US'
-                    },
-                    {
-                        'platformName': 'Android',
-                        'platformVersion': '7.0',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'Samsung Galaxy Tab A 10 GoogleAPI Emulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Chrome',
-                        'language': 'en',
-                        'locale': 'en_US'
-                    },
-                    {
-                        'platformName': 'Android',
-                        'platformVersion': '6.0',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'Android Emulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Chrome',
-                        'language': 'en',
-                        'locale': 'en_US'
-                    },
-                    {
-                        'platformName': 'Android',
-                        'platformVersion': '5.1',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'Android GoogleAPI Emulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Browser'
-                    },
-                    {
-                        'platformName': 'Android',
-                        'platformVersion': '5.0',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'Android Emulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Browser'
-                    },
-                    {
-                        'platformName': 'Android',
-                        'platformVersion': '4.4',
-                        'deviceOrientation': 'PORTRAIT',
-                        'deviceName': 'LG Nexus 4 Emulator',
-                        'appiumVersion': '1.8.1',
-                        'browserName': 'Browser'
-                    }
+                    Windows8_IE,
+                    Windows10_MicrosoftEdge,
+                    Windows10_IE,
+                    Windows10_Chrome_67,
+                    Windows10_Chrome_68,
+                    Windows10_Firefox,
+                    MacOS10_Safari_10_1,
+                    MacOS10_Safari_11_0,
+                    MacOS10_Safari_11_1,
+                    MacOS10_Firefox,
+                    MacOS10_Chrome,
+                    IOS_IPhoneX_Safari_Portrait,
+                    IOS_IPhone8_Safari_Portrait,
+                    IOS_IPhone7_Safari_Portrait,
+                    IOS_IPhone6S_Safari_Portrait,
+                    IOS_IPad_Safari_Portrait,
+                    IOS_IPadPro_Safari_Portrait,
+                    Android_6_0_Chrome_Portrait,
+                    Android_7_1_Chrome_Portrait,
+                    Android_7_0_Chrome_Portrait,
+                    Android_5_1_Chrome_Portrait,
+                    Android_5_0_Chrome_Portrait,
+                    Android_4_4_Chrome_Portrait
                 ];
         }
     }
