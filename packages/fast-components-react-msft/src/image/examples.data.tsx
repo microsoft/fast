dd0a2224d68d3e1c8e003f917bc41abcf91aa45e
Chris Holt <chhol@microsoft.com>
2018-09-19 14:49:22 -0700

    feat(image): update viewport logic in favor of slots (#914)
    * refactor(image): remove viewport specific logic in favor of slots
    
    * instead of returning fragment, just return child
    
    * add return type

--- packages/fast-components-react-msft/src/image/examples.data.tsx
+++ packages/fast-components-react-msft/src/image/examples.data.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { IComponentFactoryExample } from "@microsoft/fast-development-site-react";
 import { Image } from "./index";
-import { IImageHandledProps } from "@microsoft/fast-components-react-base";
+import { IImageHandledProps, ImageSlot } from "@microsoft/fast-components-react-base";
 import schema from "@microsoft/fast-components-react-base/dist/image/image.schema.json";
 import Documentation from "./.tmp/documentation";
 
@@ -11,47 +11,30 @@ export default {
     schema: schema as any,
     documentation: <Documentation />,
     detailData: {
-        vp1: "https://placehold.it/300x300/3E3E3E/171717",
+        src: "https://placehold.it/300x300/3E3E3E/171717",
         alt: "Placeholder with grey background and dimension watermark without any imagery"
     },
     data: [
         {
-            vp5: "https://placehold.it/600x600/2F2F2F/171717",
-            vp4: "https://placehold.it/600x600/2F2F2F/171717",
-            vp3: "https://placehold.it/500x500/2F2F2F/171717",
-            vp2: "https://placehold.it/400x400/2F2F2F/171717",
-            vp1: "https://placehold.it/300x300/2F2F2F/171717",
+            src: "https://placehold.it/300x300/2F2F2F/171717",
+            alt: "Placeholder with grey background and dimension watermark without any imagery",
+            children: [
+                (<source srcSet="https://placehold.it/450x450/2F2F2F/171717" media="(min-width: 1400px)" slot={ImageSlot.source}/>),
+                (<source srcSet="https://placehold.it/300x300/2F2F2F/171717" media="(min-width: 1084px)" slot={ImageSlot.source}/>),
+                (<source srcSet="https://placehold.it/200x200/2F2F2F/171717" media="(min-width: 768px)" slot={ImageSlot.source}/>),
+                (<source srcSet="https://placehold.it/100x100/2F2F2F/171717" media="(min-width: 540px)" slot={ImageSlot.source}/>),
+                (<source srcSet="https://placehold.it/75x75/2F2F2F/171717" media="(min-width: 0px)"slot={ImageSlot.source}/>)
+            ]
+        },
+        {
+            src: "https://placehold.it/200x150/2F2F2F/171717",
             alt: "Placeholder with grey background and dimension watermark without any imagery"
         },
         {
             src: "https://placehold.it/300x300/2F2F2F/171717",
             /* tslint:disable-next-line */
-            srcSet: "https://placehold.it/600x600/2F2F2F/171717 2048w, https://placehold.it/600x600/2F2F2F/171717 1778w, https://placehold.it/1600x600/2F2F2F/171717 1399w, https://placehold.it/500x500/2F2F2F/171717 1083w, https://placehold.it/400x400/2F2F2F/171717 767w, https://placehold.it/300x300/2F2F2F/171717 1w",
-            sizes: "100vw",
+            srcSet: "https://placehold.it/500x500/2F2F2F/171717 1083w, https://placehold.it/400x400/2F2F2F/171717 767w, https://placehold.it/300x300/2F2F2F/171717 1w",
             alt: "Placeholder with grey background and dimension watermark without any imagery"
-        },
-        {
-            src: "https://placehold.it/300x300/2F2F2F/171717",
-            alt: "Placeholder with grey background and dimension watermark without any imagery"
-        },
-        {
-            vp5: "https://placehold.it/600x600/2F2F2F/171717",
-            vp4: "https://placehold.it/600x600/2F2F2F/171717",
-            vp3: "https://placehold.it/500x500/2F2F2F/171717",
-            vp2: "https://placehold.it/400x400/2F2F2F/171717",
-            vp1: "https://placehold.it/300x300/2F2F2F/171717",
-            alt: "Placeholder with grey background and dimension watermark without any imagery"
-        },
-        {
-            src: "https://placehold.it/539x300/2F2F2F/171717",
-            /* tslint:disable-next-line */
-            srcSet: "https://placehold.it/2048x600/2F2F2F/171717 2048w, https://placehold.it/1778x600/2F2F2F/171717 1778w, https://placehold.it/1399x600/2F2F2F/171717 1399w, https://placehold.it/1083x500/2F2F2F/171717 1083w, https://placehold.it/767x400/2F2F2F/171717 767w, https://placehold.it/539x300/2F2F2F/171717 1w",
-            sizes: "100vw",
-            alt: "Placeholder with grey background and dimension watermark without any imagery"
-        },
-        {
-            src: "https://placehold.it/539x300/2F2F2F/171717",
-            alt: "Placeholder with grey background and dimension watermark without any imagery"
-        },
+        }
     ]
 } as IComponentFactoryExample<IImageHandledProps>;
