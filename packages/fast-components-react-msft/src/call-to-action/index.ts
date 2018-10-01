ef017472615053ae67b4cb839ccd157bab7b1b9b
Nick Rice <nicholas.rice119@gmail.com>
2018-10-01 14:32:45 -0700

    correcting call to action exports

--- packages/fast-components-react-msft/src/call-to-action/index.ts
+++ packages/fast-components-react-msft/src/call-to-action/index.ts
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { ICallToActionClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
-import manageJss, { IJSSManagerProps } from "@microsoft/fast-jss-manager-react";
+import manageJss, { IJSSManagerProps, JSSManagerProps } from "@microsoft/fast-jss-manager-react";
 import { CallToActionStyles, IDesignSystem } from "@microsoft/fast-components-styles-msft";
-import MSFTCallToAction, { ICallToActionHandledProps } from "./call-to-action";
+import MSFTCallToAction, {
+    CallToActionAppearance,
+    CallToActionProps as MSFTCallToActionProps,
+    ICallToActionHandledProps as IMSFTCallToActionHandledProps,
+    ICallToActionUnhandledProps
+} from "./call-to-action";
 
 /*
  * The type returned by manageJss type is very complicated so we'll let the
@@ -12,4 +17,14 @@ import MSFTCallToAction, { ICallToActionHandledProps } from "./call-to-action";
 const CallToAction = manageJss(CallToActionStyles)(MSFTCallToAction);
 type CallToAction = InstanceType<typeof CallToAction>;
 
-export { CallToAction };
+interface ICallToActionHandledProps extends JSSManagerProps<MSFTCallToActionProps, ICallToActionClassNameContract, IDesignSystem> {}
+type CallToActionProps = ICallToActionHandledProps & ICallToActionUnhandledProps;
+
+export {
+    CallToAction,
+    CallToActionAppearance,
+    CallToActionProps,
+    ICallToActionClassNameContract,
+    ICallToActionHandledProps,
+    ICallToActionUnhandledProps
+};
