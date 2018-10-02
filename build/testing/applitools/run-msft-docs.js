6945d777df6b0f0f75a4442ff672c82954e2a498
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2018-10-02 14:00:56 -0700

    chore: add applitools for testing UI regressions (#973)
    * Applitools sample installation and configuration for testing
    
    * add new script file for selenium driver with JavaScript
    
    * update code formatting
    
    * update to move test files into root testing folder
    
    * update package dependencies and image code
    
    * Update configuration of file and log messages
    
    * update handle result log messages
    
    * add firefox browser to matrix
    
    * update to extract out script from harness
    
    * update to encapsulate configs
    
    * update to get branch name synchronously
    
    * update to cleanup how things are pulling configurations
    
    * update to fix session not found and more window snapshots
    
    * update to move script test under applitools since it has required bindings based on applitools
    
    * update to cleanup applitools files
    
    * update to remove code duplication
    
    * update to use backticks'
    
    * remove archived files from repository until they are needed
    
    * update to proper casing based on class vs properties
    
    * update to remove unused package
    
    * update to coding standard format
    
    * update spelling
    
    * update comments in code
    
    * update formatting from single quotes to double quotes
    
    * update string formatting to ES6
    
    * update to remove extra lines at end of inner functions
    
    * add space after and before {}
    
    * remove extra space before =
    
    * update formatting of extra line in function calls
    
    * add gitignore for .exe's
    
    * update docs for binary downloads
    
    * updated gitignore and temporarily remove binaries

---
+++ build/testing/applitools/run-msft-docs.js
@@ -0,0 +1,148 @@
+"use strict";
+
+let spawn = require("child_process").spawnSync;
+
+/**
+ * Get Branch Name using child_process and Git which runs Asynchronously
+ */
+class Config {
+    constructor() {
+        this.domainName = "https://msft-docs.fast-dna.net";
+        this.appName = "FAST-DNA";
+        this.siteName = "MSFT Doc Site";
+    }
+
+    /**
+     * Retrieves the Git Branch for the local user account
+     */
+    branchName() {
+        return spawn('git', ['rev-parse', '--abbrev-ref', 'HEAD']).output[1].toString().trim();
+    }
+}
+exports.Config = Config;
+
+/**
+ * Testing script that can be used across services  
+ */
+class Run {
+    constructor(eyes, driver, website) {
+
+        console.log("Starting tests on: %s", website);
+
+        driver.get(website);
+
+        // Note: There are many ways to navigate/find/select elements on a page.
+        // Not all are cross-browser compliant and for example, we can't 
+        // use xpath in all browser drivers. As a result, we're using
+        // navigation though not necessarily most performant but effective.
+
+        // Iterate each components documentation
+
+        driver.navigate().to(`${website}/components/button/`);
+        eyes.checkWindow("button");
+                    
+        driver.navigate().to(`${website}/components/caption/`);
+        eyes.checkWindow("caption");
+        
+        driver.navigate().to(`${website}/components/checkbox/`);
+        eyes.checkWindow("checkbox");
+        
+        driver.navigate().to(`${website}/components/dialog/`);
+        eyes.checkWindow("dialog");
+        
+        driver.navigate().to(`${website}/components/divider/`);
+        eyes.checkWindow("divider");
+        
+        driver.navigate().to(`${website}/components/flipper/`);
+        eyes.checkWindow("flipper");
+        
+        driver.navigate().to(`${website}/components/heading/`);
+        eyes.checkWindow("heading");
+        
+        driver.navigate().to(`${website}/components/hypertext/`);
+        eyes.checkWindow("hypertext");
+        
+        driver.navigate().to(`${website}/components/image/`);
+        eyes.checkWindow("image");
+        
+        driver.navigate().to(`${website}/components/label/`);
+        eyes.checkWindow("label");
+        
+        driver.navigate().to(`${website}/components/metatext/`);
+        eyes.checkWindow("metatext");
+        
+        driver.navigate().to(`${website}/components/paragraph/`);
+        eyes.checkWindow("paragraph");
+        
+        driver.navigate().to(`${website}/components/subheading/`);
+        eyes.checkWindow("subheading");
+        
+        driver.navigate().to(`${website}/components/text-field/`);
+        eyes.checkWindow("text-field");
+        
+        driver.navigate().to(`${website}/components/toggle/`);
+        eyes.checkWindow("toggle");
+        
+        driver.navigate().to(`${website}/components/typography/`);
+        eyes.checkWindow("typography");
+        
+        // Turn on Developer tools and iterate each component
+        // Xpath selectors do not work in Internet Explorer
+        // driver.findElement(By.xpath("//button[text()='dev tools']")).click();
+        // one possible solution would be to inject data attributes for each component we can bind to.
+        // For example, data-test="friendly name"
+        // driver.findElement(By.cssSelector('[data-element="city"]'))
+
+        // Navigate to example views for each component
+        driver.navigate().to(`${website}/components/button/examples`);
+        eyes.checkWindow("button example");
+        
+        driver.navigate().to(`${website}/components/caption/examples`);
+        eyes.checkWindow("caption example");
+        
+        driver.navigate().to(`${website}/components/checkbox/examples`);
+        eyes.checkWindow("checkbox example");
+        
+        driver.navigate().to(`${website}/components/dialog/examples`);
+        eyes.checkWindow("dialog example");
+        
+        driver.navigate().to(`${website}/components/divider/examples`);
+        eyes.checkWindow("divider example");
+
+        driver.navigate().to(`${website}/components/flipper/examples`);
+        eyes.checkWindow("flipper example");
+        
+        driver.navigate().to(`${website}/components/heading/examples`);
+        eyes.checkWindow("heading example");
+        
+        driver.navigate().to(`${website}/components/hypertext/examples`);
+        eyes.checkWindow("hypertext example");
+        
+        driver.navigate().to(`${website}/components/image/examples`);
+        eyes.checkWindow("image example");
+        
+        driver.navigate().to(`${website}/components/label/examples`);
+        eyes.checkWindow("label example");
+        
+        driver.navigate().to(`${website}/components/metatext/examples`);
+        eyes.checkWindow("metatext example");
+        
+        driver.navigate().to(`${website}/components/paragraph/examples`);
+        eyes.checkWindow("paragraph example");
+        
+        driver.navigate().to(`${website}/components/subheading/examples`);
+        eyes.checkWindow("subheading example");
+        
+        driver.navigate().to(`${website}/components/text-field/examples`);
+        eyes.checkWindow("text-field example");
+        
+        driver.navigate().to(`${website}/components/toggle/examples`);
+        eyes.checkWindow("toggle example");
+        
+        driver.navigate().to(`${website}/components/typography/examples`);
+        eyes.checkWindow("typograph example");
+            
+    }
+}
+
+exports.Run = Run;
---
+++ build/testing/applitools/test-javascript.js
@@ -0,0 +1,144 @@
+/**
+ * @name
+ * Test Visual User Interfaces
+ * 
+ * @description
+ * Used to generate baselines and test the difference of those baselines against future changes.
+ * 
+ * @example
+ * To execute on CLI, run `node build/testing/applitools/test-javascript.js` from root directory.
+ * 
+ * @requires
+ * You must create environment variables if executing this script locally using CLI. 
+ * 
+ * This can be done one of two recommended ways.
+ * 1. Update your `~/.bashrc` file.
+ *    export APPLITOOLS_API_KEY=[some value]
+ * 
+ * 2. Create a `.env` file at the root of your project
+ *    APPLITOOLS_API_KEY=[some value]
+ * 
+ * - Cirlce CI has these values included as environment variables. 
+ * - The api key can be found in the portal on Applitools.
+ * 
+ * Selenium WebDriver Dependencies:
+ * 1. chromedriver.exe is required to be downloaded (https://chromedriver.storage.googleapis.com/index.html) and placed in `./applitools/` folder
+ * 2. geckodriver.exe is required to be downloaded (https://github.com/mozilla/geckodriver/releases/tag/v0.22.0) and placed in `./applitools/` folder
+ * For additional drivers visit: https://www.seleniumhq.org/download/ 
+ * 
+ */
+
+"use strict";
+
+const { Builder, Capabilities } = require("selenium-webdriver");
+const chalk = require("chalk");
+const Eyes = require("eyes.selenium").Eyes;
+const { Config, Run } = require("./run-msft-docs.js");
+
+/**
+ * Setup and SDK and batch
+ * @param {*} eyes 
+ * @param {*} branch
+ */
+function setup() {
+    // Initialize SDK using API Key from environment variables
+    const domain = "https://eyesapi.applitools.com";
+    const config = new Config();
+    const eyes = new Eyes(domain);
+
+    // Set Applitools Dashboard variables
+    eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
+    eyes.setBatch(config.branchName(), process.env.APPLITOOLS_BATCH_ID, 0);
+    eyes.setIgnoreCaret(true);
+
+    return eyes;
+}
+
+/**
+ * Startup function used to connect to Applitools and execute tests
+ */
+function main() {
+    // Setup Applitools
+    let eyes = setup();
+
+    // Set viewports
+    const viewportLandscape = { width: 1920, height: 1200 };
+    const viewportPortrait = { width: 600, height: 800 };
+    
+    // Execute Chrome driver tests
+    browserDriver(eyes, Capabilities.chrome(), viewportLandscape, viewportPortrait);
+    
+    // Execute FireFox driver tests
+    browserDriver(eyes, Capabilities.firefox(), viewportLandscape, viewportPortrait);
+}
+
+/**
+ * Setup FireFox WebDriver and run tests
+ * @param {*} eyes
+ * @param {*} webDriver
+ * @param {*} viewportLandscape 
+ * @param {*} viewportPortrait 
+ */
+function browserDriver(eyes, webDriver, viewportLandscape, viewportPortrait) {
+    const innerDriver = new Builder()
+        .withCapabilities(webDriver)
+        .build();
+
+    innerDriver.getSession().then(function(session) {
+        runTest(eyes, innerDriver, viewportLandscape);
+        runTest(eyes, innerDriver, viewportPortrait);
+
+        if (innerDriver) {
+            innerDriver.quit(); 
+        }
+    });
+}
+
+/**
+ * Execute tests against different drivers (browsers) and viewports
+ * @param {*} eyes 
+ * @param {*} innerDriver 
+ * @param {*} viewportSize 
+ */
+function runTest(eyes, innerDriver, viewportSize) {
+    // Config for AppName, SiteName must be set before opening eyes
+    const config = new Config();
+
+    eyes.open(innerDriver, config.appName, config.siteName, viewportSize)
+        .then(function (driver) {
+            try {
+                new Run(eyes, driver, config.domainName);
+
+                eyes.close(false).then(function (result) { 
+                    handleResult(result);
+                });
+        
+            } finally {
+                eyes.abortIfNotClosed();
+            }
+        });
+}
+
+/**
+ * Handle the result by logging status to output
+ * @param {*} result 
+ */
+function handleResult(result) {
+    const url = result.appUrls.session;
+    const totalSteps = result.steps;
+
+    if (result.isNew) {
+        console.log(chalk.yellow("\nNew Baseline Created: %d steps \nView Results at %s"), totalSteps, url);
+    } else if (result.isPassed) {
+        console.log(chalk.green("\nAll Steps Passed: %d steps \nView Results at %s"), totalSteps, url);
+    } else {
+        console.log(chalk.red("\nTest Failed:\n\t Matches=%d \n\t Missing=%s \n\t MisMatches=%s \nView Results at %s\n"),
+            result.matches, 
+            result.missing, 
+            result.mismatches, 
+            url
+        );
+    } 
+}
+
+main();
