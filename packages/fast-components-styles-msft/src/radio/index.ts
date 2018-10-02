577c1716bf333e731fab2f61a283c96efc26eba3
khamudom <khamu@microsoft.com>
2018-10-02 10:29:23 -0700

    update styling to look similar on muliple browsers

--- packages/fast-components-styles-msft/src/radio/index.ts
+++ packages/fast-components-styles-msft/src/radio/index.ts
@@ -3,7 +3,6 @@ import { ComponentStyles, ComponentStyleSheet, ICSSRules } from "@microsoft/fast
 import { IRadioClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
 import {
     disabledContrast,
-    ensureForegroundNormal,
     ensureNormalContrast,
     hoverContrast,
     normalContrast
@@ -13,6 +12,14 @@ import {
 const styles: ComponentStyles<IRadioClassNameContract, IDesignSystem> = (config: IDesignSystem): ComponentStyleSheet<IRadioClassNameContract, IDesignSystem>  => {
     const designSystem: IDesignSystem = withDesignSystemDefaults(config);
     const backgroundColor: string = designSystem.backgroundColor;
+    const foregroundColor: string = ensureNormalContrast(config.contrast, designSystem.foregroundColor, designSystem.backgroundColor);
+    const radioColor: string = normalContrast(designSystem.contrast, foregroundColor, backgroundColor);
+    const radioHover: string = hoverContrast(designSystem.contrast, foregroundColor, backgroundColor);
+    const radioDisabled: string = disabledContrast(
+        designSystem.contrast,
+        foregroundColor,
+        backgroundColor
+    );
 
     return {
         radio: {
@@ -20,13 +27,65 @@ const styles: ComponentStyles<IRadioClassNameContract, IDesignSystem> = (config:
             flexDirection: "row",
             alignItems: "center"
         },
-        radio__disabled: {
-            cursor: "not-allowed"
-        },
         radio_input: {
-            background: backgroundColor
+            position: "absolute",
+            width: "20px",
+            height: "20px",
+            appearance: "none",
+            borderRadius: "50%",
+            boxSizing: "content-box",
+            margin: "0",
+            zIndex: "1",
+            boxShadow: `inset 0 0 0 1px ${radioColor}`,
+            "&:hover": {
+                boxShadow: `inset 0 0 0 1px ${radioHover}`,
+            },
+            "&:focus": {
+                outline: "none",
+                boxShadow: `inset 0 0 0 2px ${radioColor}`,
+            },
+            "&:checked": {
+                "& + span": {
+                    "&::after, &::before": {
+                        position: "absolute",
+                        zIndex: "1",
+                        content: "\"\"",
+                        borderRadius: "50%",
+                        background: radioColor
+                    }
+                }
+            },
         },
-        radio_stateIndicator: {}
+        radio_stateIndicator: {
+            position: "relative",
+            borderRadius: "50%",
+            display: "inline-block",
+            width: "20px",
+            height: "20px",
+            flexShrink: "0",
+            marginRight: "5px",
+            "&::before, &::after": {
+                width: "2px"
+           },
+            "&::before": {
+                top: "4px",
+                left: "11px",
+                height: "12px",
+                transform: "rotate(40deg)"
+            },
+            "&::after": {
+                top: "9px",
+                left: "6px",
+                height: "6px",
+                transform: "rotate(-45deg)"
+            }
+        },
+        radio__disabled: {
+            cursor: "not-allowed",
+            "& $radio_input": {
+                boxShadow: `inset 0 0 0 1px ${radioDisabled}`
+            },
+        },
     };
 };
 
