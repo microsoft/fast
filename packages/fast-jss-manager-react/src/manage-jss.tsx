90f95448dcd4c364c224794640ba25bed782fba8
nicholasrice <nicholas.rice119@gmail.com>
2018-10-03 19:59:12 -0700

    stubbing out HOC

--- packages/fast-jss-manager-react/src/manage-jss.tsx
+++ packages/fast-jss-manager-react/src/manage-jss.tsx
@@ -10,6 +10,7 @@ import { IDesignSystemProviderProps } from "./design-system-provider";
 import * as propTypes from "prop-types";
 import { ClassNames, ComponentStyles, ComponentStyleSheet, IManagedClasses } from "@microsoft/fast-jss-manager";
 import { isEqual, merge, omit } from "lodash-es";
+import { Consumer } from "./context";
 
 // hoist-non-react-statics does not seem to be a properly formatted ES6 module, so we need to require it instead
 // TODO https://github.com/Microsoft/fast-dna/issues/512
@@ -50,175 +51,197 @@ Pick<
  * higher order component. That higher-order component can then be used to compose a component
  * with styles managed
  * @param S - The stylesheet class-name contract
- * @param C - The stylesheet configuration object
+ * @param C - The stylesheet design-system configuration object
  * @param T - The component prop interface
  */
 function manageJss<S, C>(
     styles?: ComponentStyles<S, C>
 ): <T>(
     Component: React.ComponentType<T & IManagedClasses<S>>
-) => React.ComponentClass<JSSManagerProps<T, S, C>> {
+) => React.SFC<{}> {
     return function<T>(
         Component: React.ComponentType<T & IManagedClasses<S>>
-    ): React.ComponentClass<JSSManagerProps<T, S, C>> {
-
-        // Define the manager higher-order component inside of the return method of the higher-order function.
-        class JSSManager extends React.Component<JSSManagerProps<T, S, C>, IJSSManagerState> {
-            // TODO: figure out if there is a better way to type this object
-            public static contextTypes: any = {
-                designSystem: propTypes.any
-            };
-
-            /**
-             * The style manager is responsible for attaching and detaching style elements when
-             * components mount and un-mount
-             */
-            private static stylesheetManager: SheetsManager = stylesheetManager;
-
-            constructor(props: JSSManagerProps<T, S, C>) {
-                super(props);
-
-                const state: IJSSManagerState = {};
-
-                if (Boolean(styles)) {
-                    state.styleSheet = this.createStyleSheet();
-                }
-
-                this.state = state;
+    ): React.SFC<JSSManagerProps<T, S, C>> {
+        return (props: JSSManagerProps<T, S, C>): React.ReactElement<{}> => {
+            function render(designSystem: C): React.ReactNode {
+                return "JSSManager goes here";
             }
 
-            /**
-             * Updates a dynamic stylesheet with context
-             */
-            public updateStyleSheet(nextContext?: any): void {
-                if (!Boolean(this.state.styleSheet)) {
-                    return;
-                }
-
-                if (typeof styles === "function") {
-                    this.resetStyleSheet();
-                } else {
-                    this.state.styleSheet.update(nextContext && nextContext.designSystem ? nextContext.designSystem : this.designSystem);
-                }
-            }
-
-            public componentWillMount(): void {
-                if (Boolean(this.state.styleSheet)) {
-                    // It appears we need to update the stylesheet for any style properties defined as functions
-                    // to work.
-                    this.state.styleSheet.attach();
-                    this.updateStyleSheet();
-                }
-            }
-
-            public componentWillUpdate(nextProps: JSSManagerProps<T, S, C>, nextState: IJSSManagerState, nextContext: any): void {
-                if (!isEqual(this.context, nextContext)) {
-                    this.updateStyleSheet(nextContext);
-                }
-            }
-
-            public componentDidUpdate(prevProps: JSSManagerProps<T, S, C>, prevState: IJSSManagerState): void {
-                if (this.props.jssStyleSheet !== prevProps.jssStyleSheet) {
-                    this.resetStyleSheet();
-                }
-            }
-
-            public componentWillUnmount(): void {
-                this.removeStyleSheet();
-            }
-
-            public render(): React.ReactNode {
-                return (
-                    <Component
-                        {...omit(this.props, ["jssStyleSheet"])}
-                        managedClasses={this.getClassNames()}
-                    />
-                );
-            }
-
-            /**
-             * Get the design-system context to update the stylesheet with
-             */
-            private get designSystem(): any {
-                return this.context && this.context.designSystem ? this.context.designSystem : {};
-            }
-
-            /**
-             * Remove a JSS stylesheet
-             */
-            private removeStyleSheet(): void {
-                if (this.hasStyleSheet()) {
-                    this.state.styleSheet.detach();
-                    stylesheetRegistry.remove(this.state.styleSheet);
-                    jss.removeStyleSheet(this.state.styleSheet);
-                }
-            }
-
-            /**
-             * Reset a JSS stylesheet relative to current props
-             */
-            private resetStyleSheet(): any {
-                this.removeStyleSheet();
-                this.setState((previousState: IJSSManagerState, props: JSSManagerProps<T, S, C>): Partial<IJSSManagerState> => {
-                    return {
-                        styleSheet: this.hasStyleSheet() ? this.createStyleSheet() : null
-                    };
-                }, (): void => {
-                    if (this.hasStyleSheet()) {
-                        this.state.styleSheet.attach().update(this.designSystem);
-                    }
-                });
-            }
-
-            /**
-             * Creates a JSS stylesheet from the dynamic portion of an associated style object and any style object passed
-             * as props
-             */
-            private createStyleSheet(): any {
-                const stylesheet: ComponentStyleSheet<S, C> = typeof styles === "function"
-                    ? styles(this.designSystem)
-                    : styles;
-
-                const jssSheet: any =  jss.createStyleSheet(
-                    merge({}, stylesheet, this.props.jssStyleSheet),
-                    { link: true }
-                );
-
-                stylesheetRegistry.add(jssSheet);
-
-                return jssSheet;
-            }
-
-            /**
-             * Checks to see if this component has an associated dynamic stylesheet
-             */
-            private hasStyleSheet(): boolean {
-                return Boolean(styles || this.props.jssStyleSheet);
-            }
-
-            /**
-             * Merges static and dynamic stylesheet classnames into one object
-             */
-            private getClassNames(): ClassNames<S> {
-                const classNames: Partial<ClassNames<S>> = {};
-
-                if (this.hasStyleSheet()) {
-                    for (const key in this.state.styleSheet.classes) {
-                        if (this.state.styleSheet.classes.hasOwnProperty(key)) {
-                            classNames[key] = typeof classNames[key] !== "undefined"
-                                ? `${classNames[key]} ${this.state.styleSheet.classes[key]}`
-                                : this.state.styleSheet.classes[key];
-                        }
-                    }
-                }
-
-                return classNames as ClassNames<S>;
-            }
-        }
-
-        return hoistNonReactStatics(JSSManager, Component);
+            return (
+                <Consumer>
+                    {render}
+                </Consumer>
+            );
+        };
     };
 }
+
+// function manageJss<S, C>(
+//     styles?: ComponentStyles<S, C>
+// ): <T>(
+//     Component: React.ComponentType<T & IManagedClasses<S>>
+// ) => React.ComponentClass<JSSManagerProps<T, S, C>> {
+//     return function<T>(
+//         Component: React.ComponentType<T & IManagedClasses<S>>
+//     ): React.ComponentClass<JSSManagerProps<T, S, C>> {
+
+//         // Define the manager higher-order component inside of the return method of the higher-order function.
+//         class JSSManager extends React.Component<JSSManagerProps<T, S, C>, IJSSManagerState> {
+//             // TODO: figure out if there is a better way to type this object
+//             public static contextTypes: any = {
+//                 designSystem: propTypes.any
+//             };
+
+//             /**
+//              * The style manager is responsible for attaching and detaching style elements when
+//              * components mount and un-mount
+//              */
+//             private static stylesheetManager: SheetsManager = stylesheetManager;
+
+//             constructor(props: JSSManagerProps<T, S, C>) {
+//                 super(props);
+
+//                 const state: IJSSManagerState = {};
+
+//                 if (Boolean(styles)) {
+//                     state.styleSheet = this.createStyleSheet();
+//                 }
+
+//                 this.state = state;
+//             }
+
+//             /**
+//              * Updates a dynamic stylesheet with context
+//              */
+//             public updateStyleSheet(nextContext?: any): void {
+//                 if (!Boolean(this.state.styleSheet)) {
+//                     return;
+//                 }
+
+//                 if (typeof styles === "function") {
+//                     this.resetStyleSheet();
+//                 } else {
+//                     this.state.styleSheet.update(nextContext && nextContext.designSystem ? nextContext.designSystem : this.designSystem);
+//                 }
+//             }
+
+//             public componentWillMount(): void {
+//                 if (Boolean(this.state.styleSheet)) {
+//                     // It appears we need to update the stylesheet for any style properties defined as functions
+//                     // to work.
+//                     this.state.styleSheet.attach();
+//                     this.updateStyleSheet();
+//                 }
+//             }
+
+//             public componentWillUpdate(nextProps: JSSManagerProps<T, S, C>, nextState: IJSSManagerState, nextContext: any): void {
+//                 if (!isEqual(this.context, nextContext)) {
+//                     this.updateStyleSheet(nextContext);
+//                 }
+//             }
+
+//             public componentDidUpdate(prevProps: JSSManagerProps<T, S, C>, prevState: IJSSManagerState): void {
+//                 if (this.props.jssStyleSheet !== prevProps.jssStyleSheet) {
+//                     this.resetStyleSheet();
+//                 }
+//             }
+
+//             public componentWillUnmount(): void {
+//                 this.removeStyleSheet();
+//             }
+
+//             public render(): React.ReactNode {
+//                 return (
+//                     <Component
+//                         {...omit(this.props, ["jssStyleSheet"])}
+//                         managedClasses={this.getClassNames()}
+//                     />
+//                 );
+//             }
+
+//             /**
+//              * Get the design-system context to update the stylesheet with
+//              */
+//             private get designSystem(): any {
+//                 return this.context && this.context.designSystem ? this.context.designSystem : {};
+//             }
+
+//             /**
+//              * Remove a JSS stylesheet
+//              */
+//             private removeStyleSheet(): void {
+//                 if (this.hasStyleSheet()) {
+//                     this.state.styleSheet.detach();
+//                     stylesheetRegistry.remove(this.state.styleSheet);
+//                     jss.removeStyleSheet(this.state.styleSheet);
+//                 }
+//             }
+
+//             /**
+//              * Reset a JSS stylesheet relative to current props
+//              */
+//             private resetStyleSheet(): any {
+//                 this.removeStyleSheet();
+//                 this.setState((previousState: IJSSManagerState, props: JSSManagerProps<T, S, C>): Partial<IJSSManagerState> => {
+//                     return {
+//                         styleSheet: this.hasStyleSheet() ? this.createStyleSheet() : null
+//                     };
+//                 }, (): void => {
+//                     if (this.hasStyleSheet()) {
+//                         this.state.styleSheet.attach().update(this.designSystem);
+//                     }
+//                 });
+//             }
+
+//             /**
+//              * Creates a JSS stylesheet from the dynamic portion of an associated style object and any style object passed
+//              * as props
+//              */
+//             private createStyleSheet(): any {
+//                 const stylesheet: ComponentStyleSheet<S, C> = typeof styles === "function"
+//                     ? styles(this.designSystem)
+//                     : styles;
+
+//                 const jssSheet: any =  jss.createStyleSheet(
+//                     merge({}, stylesheet, this.props.jssStyleSheet),
+//                     { link: true }
+//                 );
+
+//                 stylesheetRegistry.add(jssSheet);
+
+//                 return jssSheet;
+//             }
+
+//             /**
+//              * Checks to see if this component has an associated dynamic stylesheet
+//              */
+//             private hasStyleSheet(): boolean {
+//                 return Boolean(styles || this.props.jssStyleSheet);
+//             }
+
+//             /**
+//              * Merges static and dynamic stylesheet classnames into one object
+//              */
+//             private getClassNames(): ClassNames<S> {
+//                 const classNames: Partial<ClassNames<S>> = {};
+
+//                 if (this.hasStyleSheet()) {
+//                     for (const key in this.state.styleSheet.classes) {
+//                         if (this.state.styleSheet.classes.hasOwnProperty(key)) {
+//                             classNames[key] = typeof classNames[key] !== "undefined"
+//                                 ? `${classNames[key]} ${this.state.styleSheet.classes[key]}`
+//                                 : this.state.styleSheet.classes[key];
+//                         }
+//                     }
+//                 }
+
+//                 return classNames as ClassNames<S>;
+//             }
+//         }
+
+//         return hoistNonReactStatics(JSSManager, Component);
+//     };
+// }
 
 export default manageJss;
 export * from "@microsoft/fast-jss-manager";
