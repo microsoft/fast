69486331b1c5b4033436b7b51d2aadcd21d534b2
Chris Holt <chhol@microsoft.com>
2018-10-04 10:21:15 -0700

    remove const declarations of color values

--- packages/fast-components-styles-msft/src/card/index.ts
+++ packages/fast-components-styles-msft/src/card/index.ts
@@ -9,15 +9,13 @@ const styles: ComponentStyles<ICardClassNameContract, IDesignSystem> = (
     config: IDesignSystem
 ): ComponentStyleSheet<ICardClassNameContract, IDesignSystem> => {
     const designSystem: IDesignSystem = withDesignSystemDefaults(config);
-    const foregroundColor: string = designSystem.foregroundColor;
-    const backgroundColor: string = designSystem.backgroundColor;
 
     return {
         card: {
             width: "100%",
             height: "100%",
-            background: backgroundColor,
-            ...elevation(ElevationMultiplier.e4, foregroundColor)(designSystem)
+            background: designSystem.backgroundColor,
+            ...elevation(ElevationMultiplier.e4, designSystem.foregroundColor)(designSystem)
         }
     };
 };
