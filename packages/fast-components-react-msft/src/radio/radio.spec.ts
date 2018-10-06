bd43afcc9aae8e2590b61000b485d8fc65db2157
Kham Udom <khamu@microsoft.com>
2018-10-06 09:22:06 -0700

    fix unit test error

--- packages/fast-components-react-msft/src/radio/radio.spec.ts
+++
@@ -1,7 +0,0 @@
-
-import examples from "./examples.data";
-import { generateSnapshots } from "@microsoft/fast-jest-snapshots-react";
-
-describe("radio snapshots", (): void => {
-    generateSnapshots(examples);
-});
