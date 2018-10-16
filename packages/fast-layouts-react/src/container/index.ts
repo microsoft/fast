335734117a048937091bff7528d945798828c63d
Nick Rice <nicholas.rice119@gmail.com>
2018-10-16 12:21:36 -0700

    update container

--- packages/fast-layouts-react/src/container/index.ts
+++ packages/fast-layouts-react/src/container/index.ts
@@ -1,5 +1,27 @@
-import Container from "./container";
+import * as React from "react";
+import manageJss, { ManagedJSSProps } from "@microsoft/fast-jss-manager-react";
+import {
+    Container as BaseContainer,
+    ContainerClassNamesContract,
+    ContainerHandledProps as BaseContainerHandledProps,
+    ContainerManagedClasses,
+    ContainerProps as BaseContainerProps,
+    containerStyleSheet,
+    ContainerUnhandledProps
+} from "./container";
+import { Subtract } from "utility-types";
 
-export default Container;
-export * from "./container";
-export * from "./container.props";
+/* tslint:disable-next-line:typedef */
+const Container = manageJss(containerStyleSheet)(BaseContainer);
+type Container = typeof Container;
+
+interface ContainerHandledProps extends Subtract<BaseContainerHandledProps, ContainerManagedClasses> {}
+type ContainerProps = ManagedJSSProps<BaseContainerProps, ContainerClassNamesContract, undefined>;
+
+export {
+    Container,
+    ContainerProps,
+    ContainerHandledProps,
+    ContainerUnhandledProps,
+    ContainerClassNamesContract
+};
