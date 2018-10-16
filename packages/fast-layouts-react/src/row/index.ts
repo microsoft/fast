ad83b2500a32e4f6f6ff86996ca1e6c87a9975aa
Nick Rice <nicholas.rice119@gmail.com>
2018-10-16 12:40:52 -0700

    update row

--- packages/fast-layouts-react/src/row/index.ts
+++ packages/fast-layouts-react/src/row/index.ts
@@ -1,5 +1,29 @@
-import Row from "./row";
+import * as React from "react";
+import manageJss, { ManagedJSSProps } from "@microsoft/fast-jss-manager-react";
+import {
+    Row as BaseRow,
+    RowClassNamesContract,
+    RowHandledProps as BaseRowHandledProps,
+    RowManagedClasses,
+    RowProps as BaseRowProps,
+    RowResizeDirection,
+    rowStyleSheet,
+    RowUnhandledProps
+} from "./row";
+import { Subtract } from "utility-types";
 
-export default Row;
-export * from "./row";
-export * from "./row.props";
+/* tslint:disable-next-line:typedef */
+const Row = manageJss(rowStyleSheet)(BaseRow);
+type Row = typeof Row;
+
+interface RowHandledProps extends Subtract<BaseRowHandledProps, RowManagedClasses> {}
+type RowProps = ManagedJSSProps<BaseRowProps, RowClassNamesContract, undefined>;
+
+export {
+    Row,
+    RowProps,
+    RowHandledProps,
+    RowUnhandledProps,
+    RowClassNamesContract,
+    RowResizeDirection
+};
