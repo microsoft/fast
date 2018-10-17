3a632810d9402a94cb41906642c5c4f7b8904132
Nick Rice <nicholas.rice119@gmail.com>
2018-10-17 09:54:23 -0700

    removing types of menu-items - we will use a single menu item instead

--- packages/fast-components-react-base/src/context-menu-item-checkbox/index.ts
+++
@@ -1,4 +0,0 @@
-import ContextMenuItemCheckbox from "./context-menu-item-checkbox";
-
-export default ContextMenuItemCheckbox;
-export * from "./context-menu-item-checkbox";
--- packages/fast-components-react-base/src/context-menu-item-radio/index.ts
+++
@@ -1,4 +0,0 @@
-import ContextMenuItemRadio from "./context-menu-item-radio";
-
-export default ContextMenuItemRadio;
-export * from "./context-menu-item-radio";
