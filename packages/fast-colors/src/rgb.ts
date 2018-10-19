a44a101d6d804b1d1ad1d0a4fe6e491066bf7f37
ramiyer1998 <ramiyer1998@gmail.com>
2018-10-18 21:04:31 -0400

    Changed RgbJSON from Interface to Class

--- packages/fast-colors/src/rgb.ts
+++ packages/fast-colors/src/rgb.ts
@@ -1,88 +1,49 @@
 
 export function hexToRgb(hex: string): string {
-  let processedHex: IRgbJSON;
-  processedHex = properRGBValues(hex);
-  let r: string;
-  let g: string;
-  let b: string;
-  let a: string;
-  let rgb: boolean;
-  let rgba: boolean;
-
-  r = processedHex.r;
-  g = processedHex.g;
-  b = processedHex.b;
-  a = processedHex.a;
-  rgb = processedHex.rgb;
-  rgba = processedHex.rgba;
-
-  if (r.length === 1) {
-    r += r;
-  }
-  if (g.length === 1) {
-    g += g;
-  }
-  if (b.length === 1) {
-    b += b;
-  }
+  const processedHex: RgbJSON = properRGBValues(hex);
+  const r: string = processedHex.r;
+  const g: string = processedHex.g;
+  const b: string = processedHex.b;
+  const a: string = processedHex.a;
+  const rgb: boolean = processedHex.rgb;
+  const rgba: boolean = processedHex.rgba;
 
   if (rgb) {
     return `rgb(${parseInt(r, 16)}, ${parseInt(g, 16)}, ${parseInt(b, 16)})`;
   } else if (rgba) {
-    if (a.length === 1) {
-      a += a;
-    }
     return `rgba(${parseInt(r, 16)}, ${parseInt(g, 16)}, ${parseInt(b, 16)}, ${(parseInt(a, 16) / 255)})`;
   }
 }
 
-interface IRgbJSON {
-  r: string;
-  g: string;
-  b: string;
-  a: string;
-  rgb: boolean;
-  rgba: boolean;
+class RgbJSON {
+  public r: string;
+  public g: string;
+  public b: string;
+  public a: string;
+  public rgb: boolean;
+  public rgba: boolean;
+
+  constructor(r: string, g: string, b: string, a: string, rgb: boolean, rgba: boolean) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+    this.rgb = rgb;
+    this.rgba = rgba;
+  }
 }
 
-function properRGBValues(hex: string): IRgbJSON {
+function properRGBValues(hex: string): RgbJSON {
   hex = hex.replace("#", "");
   if (3 === hex.length && /^([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-    return {
-      r: hex.charAt(0),
-      g: hex.charAt(1),
-      b: hex.charAt(2),
-      a: "",
-      rgb: true,
-      rgba: false
-    };
+    return new RgbJSON((hex.charAt(0) + hex.charAt(0)), (hex.charAt(1) + hex.charAt(1)), (hex.charAt(2) + hex.charAt(2)), "", true, false);
   } else if (4 === hex.length && /^([A-Fa-f0-9]{4}){1,2}$/.test(hex)) {
-    return {
-      r: hex.charAt(0),
-      g: hex.charAt(1),
-      b: hex.charAt(2),
-      a: hex.charAt(3),
-      rgb: false,
-      rgba: true
-    };
+    return new RgbJSON(hex.charAt(0) + hex.charAt(0), hex.charAt(1) + hex.charAt(1), hex.charAt(2) + hex.charAt(2),
+hex.charAt(3) + hex.charAt(3), false, true);
   } else if (6 === hex.length && /^([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-    return {
-      r : hex.substring(0, 2),
-      g : hex.substring(2, 4),
-      b : hex.substring(4, 6),
-      a: "",
-      rgb: true,
-      rgba: false
-    };
+    return new RgbJSON(hex.substring(0, 2), hex.substring(2, 4), hex.substring(4, 6), "", true, false);
   } else if (8 === hex.length && /^([A-Fa-f0-9]{4}){1,2}$/.test(hex)) {
-    return {
-      r: hex.substring(0, 2),
-      g: hex.substring(2, 4),
-      b: hex.substring(4, 6),
-      a: hex.substring(6, 8),
-      rgb: false,
-      rgba: true
-    };
+    return new RgbJSON(hex.substring(0, 2), hex.substring(2, 4), hex.substring(4, 6), hex.substring(6, 8), false, true);
   } else {
     throw new Error("Not a convertible hex value");
   }
