93b74c2e3df2b8a87cac27729ddacb0d304f4cf0
Nick Rice <nicholas.rice119@gmail.com>
2018-10-22 15:57:40 -0700

    adding elevation to context-menu

--- packages/fast-components-styles-msft/src/context-menu/index.ts
+++ packages/fast-components-styles-msft/src/context-menu/index.ts
@@ -2,21 +2,29 @@ import designSystemDefaults, {
     DesignSystem,
     withDesignSystemDefaults,
 } from "../design-system";
-import { ComponentStyles } from "@microsoft/fast-jss-manager";
+import { ComponentStyles, ComponentStyleSheet } from "@microsoft/fast-jss-manager";
 import { ContextMenuClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
 import { foregroundLarge } from "../utilities/colors";
+import { elevation, ElevationMultiplier } from "../utilities/elevation";
 
-const styles: ComponentStyles<ContextMenuClassNameContract, DesignSystem> = {
-    contextMenu: {
-        border: (config: DesignSystem): string => {
-            return `1px solid ${foregroundLarge(config)}`;
+const styles: ComponentStyles<ContextMenuClassNameContract, DesignSystem> = (
+    config: DesignSystem
+): ComponentStyleSheet<ContextMenuClassNameContract, DesignSystem> => {
+    const designSystem: DesignSystem = withDesignSystemDefaults(config);
+
+    return {
+        contextMenu: {
+            border: `1px solid ${foregroundLarge(designSystem)}`,
+            ...elevation(ElevationMultiplier.e4, designSystem.foregroundColor)(
+                designSystem
+            ),
+            margin: "0",
+            padding: "0",
+            maxWidth: "369px", // Should really be 368, however chrome draws the border incorrectly at even numbers
+            minWidth: "64px",
+            borderRadius: "4px",
         },
-        margin: "0",
-        padding: "0",
-        maxWidth: "369px", // Should really be 368, however chrome draws the border incorrectly at even numbers
-        minWidth: "64px",
-        borderRadius: "4px",
-    },
+    };
 };
 
 export default styles;
