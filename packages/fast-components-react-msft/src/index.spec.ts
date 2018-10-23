3675858a07153e74fdfc4643b3021b4c96c2c916
nalogiudice <random.interval@gmail.com>
2018-10-23 11:44:51 -0700

    test prettier

--- packages/fast-components-react-msft/src/index.spec.ts
+++ packages/fast-components-react-msft/src/index.spec.ts
@@ -3,10 +3,12 @@ import { configure } from "enzyme";
 import * as path from "path";
 import { includesAllSubdirectoriesAsNamedExports } from "../../../build/helpers/file-includes-all-subdirectories-as-named-exports";
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 describe("index.ts", (): void => {
     test("Should export all components in the src directory", () => {
-        expect(includesAllSubdirectoriesAsNamedExports(path.resolve(__dirname, "index.ts"))).toEqual([]);
+        expect(
+            includesAllSubdirectoriesAsNamedExports(path.resolve(__dirname, "index.ts"))
+        ).toEqual([]);
     });
 });
