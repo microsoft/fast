7646c7b6e36f77184cdad61b162339db3ef88547
nalogiudice <random.interval@gmail.com>
2018-10-29 13:54:58 -0700

    removes comment

--- build/helpers/__tests__/src/index.ts
+++ build/helpers/__tests__/src/index.ts
@@ -8,6 +8,4 @@ export * from "./foo";
 /**
  * Example formatting from fast-component-react-msft
  */
-// import Bar from "./bar";
-// export { Bar };
-export * from "./bar"; // does nothing, because bar/index.ts -> bar/bar.ts
+export * from "./bar";
