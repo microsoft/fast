mptap/fast-dna

feat: update fast-viewer-react API

packages/fast-viewer-react/webpack.config.js
@@ -1,44 +1,44 @@
-const webpack = require('webpack');
-const path = require('path');
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const appDir = path.resolve(__dirname, "./app");
+const outDir = path.resolve(__dirname, "./www");
 
 module.exports = {
-    devtool: 'inline-source-map',
-    entry: './app/app.tsx',
+    devtool: "inline-source-map",
+    entry: path.resolve(appDir, "index.tsx"),
     output: {
-        path: path.resolve('./www'),
-        publicPath: '/',
-        filename: 'app.js'
+        path: path.resolve("./www"),
+        publicPath: "/",
+        filename: "app.js"
     },
     mode: process.env.NODE_ENV || "development",
     resolve: {
-        extensions: ['.js', '.ts', '.tsx'],
+        extensions: [".js", ".ts", ".tsx"],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: [
-                    { loader: 'ts-loader' },
-                    { loader: 'tslint-loader' }
+                    { loader: "ts-loader" },
+                    { loader: "tslint-loader" }
                 ],
                 exclude: /node_modules/
             }
         ]
     },
     plugins: [
-        new ForkTsCheckerWebpackPlugin({
-            tslint: path.resolve(__dirname, "./tslint.json")
-        }),
         new HtmlWebpackPlugin({
-            title: 'FAST viewer',
-            template: path.resolve(__dirname, './app/index.html')
+            title: "FAST viewer",
+            contentBase: outDir
         })
     ],
     devServer: {
         compress: false,
         historyApiFallback: true,
+        open: true,
         port: 3000
     }
 }

microsoft/fast

packages/fast-viewer-react/webpack.config.js
@@ -1,44 +1,43 @@
-const webpack = require('webpack');
-const path = require('path');
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const appDir = path.resolve(__dirname, "./app");
+const outDir = path.resolve(__dirname, "./www");
 
 module.exports = {
-    devtool: 'inline-source-map',
-    entry: './app/app.tsx',
+    devtool: "inline-source-map",
+    entry: path.resolve(appDir, "index.tsx"),
     output: {
-        path: path.resolve('./www'),
-        publicPath: '/',
-        filename: 'app.js'
+        path: path.resolve("./www"),
+        publicPath: "/",
+        filename: "app.js"
     },
     mode: process.env.NODE_ENV || "development",
     resolve: {
-        extensions: ['.js', '.ts', '.tsx'],
+        extensions: [".js", ".ts", ".tsx"],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: [
-                    { loader: 'ts-loader' },
-                    { loader: 'tslint-loader' }
+                    { loader: "ts-loader" }
                 ],
                 exclude: /node_modules/
             }
         ]
     },
     plugins: [
-        new ForkTsCheckerWebpackPlugin({
-            tslint: path.resolve(__dirname, "./tslint.json")
-        }),
         new HtmlWebpackPlugin({
-            title: 'FAST viewer',
-            template: path.resolve(__dirname, './app/index.html')
+            title: "FAST viewer",
+            contentBase: outDir
         })
     ],
     devServer: {
         compress: false,
         historyApiFallback: true,
+        open: true,
         port: 3000
     }
 }