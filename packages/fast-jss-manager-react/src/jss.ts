ff6a2ebb38769d8d31344487249266bbf8ea7ff2
Nicholas Rice <nicholas.rice119@gmail.com>
2018-11-21 11:37:01 -0800

    fix: explicitly define JSS types in the JSS manager

--- packages/fast-jss-manager-react/src/jss.ts
+++ packages/fast-jss-manager-react/src/jss.ts
@@ -1,8 +1,14 @@
 import JSS, { create, SheetsManager, SheetsRegistry } from "jss";
 import presets from "jss-preset-default";
 
-const jss: JSS = create(presets());
-const stylesheetManager: SheetsManager = new SheetsManager();
-const stylesheetRegistry: SheetsRegistry = new SheetsRegistry();
+/**
+ * We should type these more accurately when JSS implements typings, but for now
+ * we should explicitly type these as any so that projects using the
+ * "noImplicitAny" compiler flag don't throw when they define jss types
+ * https://github.com/Microsoft/fast-dna/issues/1155
+ */
+const jss: any = create(presets());
+const stylesheetManager: any = new SheetsManager();
+const stylesheetRegistry: any = new SheetsRegistry();
 
 export { jss, stylesheetManager, stylesheetRegistry };
