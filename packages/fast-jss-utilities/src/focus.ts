3d276a9e4084e03003fb5978d6f1a2690fe813e1
nicholasrice <nicholas.rice119@gmail.com>
2018-12-05 23:28:53 -0800

    adding focus-visible function

--- packages/fast-jss-utilities/src/focus.ts
+++ packages/fast-jss-utilities/src/focus.ts
@@ -1,29 +1,10 @@
 import { canUseDOM } from "exenv-es6";
+import { canUseFocusVisible } from "@microsoft/fast-web-utilities";
 
 /**
  * Returns ':focus-visible' if the browser supports it and `:focus` if focus-visible is not supported
  */
-let _canUseFocusVisible: boolean = false;
 
 export function focusVisible(): ":focus" | ":focus-visible" {
-    if (typeof _canUseFocusVisible === "boolean") {
-        return _canUseFocusVisible;
-    }
-
-    if (!canUseDOM()) {
-        _canUseFocusVisible = false;
-    }
-
-    // Check to see if the document supports the focus-visible elemtn
-    const styleElement: HTMLStyleElement = document.createElement("style");
-    document.head.appendChild(styleElement);
-
-    try {
-        (styleElement.sheet as any).insertRule("foo:focus-visible {color:inherit}", 0);
-        _canUseFocusVisible = true;
-    } catch (e) {
-        _canUseFocusVisible = false;
-    }
-
-    return _canUseFocusVisible as boolean;
+    return canUseFocusVisible() ? ":focus-visible" : ":focus";
 }
