062c84ea172a0d0418048f6247c29c54db67b8b7
Stephane Comeau <scomea@microsoft.com>
2018-12-07 10:36:50 -0800

    fix examples

--- packages/fast-components-react-msft/src/action-toggle/examples.data.tsx
+++ packages/fast-components-react-msft/src/action-toggle/examples.data.tsx
@@ -5,6 +5,32 @@ import Documentation from "./.tmp/documentation";
 import { ComponentFactoryExample } from "@microsoft/fast-development-site-react";
 
 const testDestination: string = "https://www.microsoft.com/en-us/";
+const selectedTestGlyph: any = (classname: string): React.ReactNode => {
+    return (
+        <svg
+            className={classname}
+            width="6"
+            height="12"
+            viewBox="0 0 6 12"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M0 0H1V12H0V0ZM6 0V12H5V0H6Z" />
+        </svg>
+    );
+};
+const unselectedTestGlyph: any = (classname: string): React.ReactNode => {
+    return (
+        <svg
+            className={classname}
+            width="7"
+            height="12"
+            viewBox="0 0 7 12"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M0.0175781 11.4551L5.4668 6L0.0175781 0.544922L0.544922 0.0175781L6.5332 6L0.544922 11.9824L0.0175781 11.4551Z" />
+        </svg>
+    );
+};
 
 export default {
     name: "Action toggle",
@@ -16,41 +42,18 @@ export default {
         unselectedContent: "Play",
         selectedLabel: "Pause",
         unselectedLabel: "Play",
-        selectedGlyph: (classname?: string): React.ReactNode => {
-            return (
-                <svg
-                    className={classname}
-                    width="6"
-                    height="12"
-                    viewBox="0 0 6 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path d="M0 0H1V12H0V0ZM6 0V12H5V0H6Z" />
-                </svg>
-            );
-        },
-        unselectedGlyph: (classname?: string): React.ReactNode => {
-            return (
-                <svg
-                    className={classname}
-                    width="10"
-                    height="14"
-                    viewBox="0 0 10 14"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path d="M10 7L0 14V0L10 7ZM1 12.0781L8.25781 7L1 1.92188V12.0781Z" />
-                </svg>
-            );
-        },
+        selectedGlyph: selectedTestGlyph,
+        unselectedGlyph: unselectedTestGlyph,
     },
     data: [
         {
-            appearance: ActionToggleAppearance.primary,
+            selectedContent: "Pause",
+            unselectedContent: "Play",
+            selectedLabel: "Pause",
+            unselectedLabel: "Play",
+            selectedGlyph: selectedTestGlyph,
+            unselectedGlyph: unselectedTestGlyph,
             "data-sketch-symbol": "Action toggle - primary",
         } as any,
-        {
-            appearance: ActionToggleAppearance.lightweight,
-            "data-sketch-symbol": "Action toggle - lightweight",
-        },
     ],
 } as ComponentFactoryExample<ActionToggleProps>;
