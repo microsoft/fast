ed963982028603d80d0b9227f3ace25c0a1e7001
Aaron Wentzel <awentzel@users.noreply.github.com>
2018-12-12 18:43:15 -0700

    update link for components to point to root

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -41,7 +41,7 @@ const siteConfig = {
   headerLinks: [
     {doc: 'readme', label: 'Docs'},
     {href: "https://github.com/microsoft/fast-dna", label: "GitHub"},
-    {href: "https://msft-docs.fast-dna.net/foundation/breadcrumb/", label: "Components"}
+    {href: "https://msft-docs.fast-dna.net", label: "Components"}
     // {doc: 'doc4', label: 'API'},
     // {page: 'help', label: 'Help'},
     // {blog: true, label: 'Blog'},
