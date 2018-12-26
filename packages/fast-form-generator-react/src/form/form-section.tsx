d8cf4f23fcbd0fa968e85aadad5c0a3e9c5e4bbf
Jane Chu <7559015+janechu@users.noreply.github.com>
2018-12-26 14:28:42 -0800

    feat: update children to use a combobox (#1223)

--- packages/fast-form-generator-react/src/form/form-section.tsx
+++ packages/fast-form-generator-react/src/form/form-section.tsx
@@ -515,7 +515,7 @@ class FormSection extends React.Component<
                         this.props.schema.reactProperties[reactProperty].type ===
                         "children"
                 )
-                .map((reactProperty: string) => {
+                .map((reactProperty: string, index: number) => {
                     let childOptions: ChildOptionItem[] = [];
 
                     if (this.props.schema.reactProperties[reactProperty].ids) {
@@ -533,7 +533,9 @@ class FormSection extends React.Component<
                     return (
                         <FormItemChildren
                             key={reactProperty}
-                            title={
+                            required={false}
+                            index={index}
+                            label={
                                 this.props.schema.reactProperties[reactProperty].title ||
                                 this.props.untitled
                             }
