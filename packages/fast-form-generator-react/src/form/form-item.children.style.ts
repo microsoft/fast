3af8e0fde7a25ad81625e06a492a4ccacc1df808
Jane Chu <7559015+janechu@users.noreply.github.com>
2018-12-26 14:28:42 -0800

    feat: update children to use a combobox (#1223)

--- packages/fast-form-generator-react/src/form/form-item.children.style.ts
+++ packages/fast-form-generator-react/src/form/form-item.children.style.ts
@@ -1,18 +1,16 @@
-import { ellipsis, toPx } from "@microsoft/fast-jss-utilities";
+import { ellipsis, focusVisible, toPx } from "@microsoft/fast-jss-utilities";
 import {
-    applyAddItemStyle,
     applyAriaHiddenStyles,
     applyCleanListStyle,
+    applyControl,
     applyGlobalStyle,
-    applyHeaderStyle,
     applyInputStyle,
-    applyListItemStyle,
-    applyPopupHeadingStyles,
-    applyPopupMenuStyles,
+    applyLabelStyle,
+    applyRemoveItemStyle,
+    applySoftRemove,
     colors,
-    localizePadding,
-    thickLine,
-    trashcan,
+    insetStrongBoxShadow,
+    rightArrow,
 } from "../utilities/form-input.style";
 import { ComponentStyles, CSSRules } from "@microsoft/fast-jss-manager";
 import { FormItemChildrenClassNameContract } from "../class-name-contracts/";
@@ -20,94 +18,88 @@ import { FormItemChildrenClassNameContract } from "../class-name-contracts/";
 const styles: ComponentStyles<FormItemChildrenClassNameContract, {}> = {
     "@global": {
         ...applyGlobalStyle(),
+        'body > div > li[draggable="true"] button': {
+            display: "none",
+        },
     },
     formItemChildren: {
+        position: "relative",
         display: "flex",
         flexDirection: "column",
     },
-    formItemChildren_inputWrapper: {
-        display: "flex",
-        marginTop: toPx(12),
-        "& input": {
-            ...applyInputStyle(),
-            flex: "2",
-            marginRight: toPx(8),
-        },
-        "& button": {
-            fontSize: toPx(15),
-            maxWidth: toPx(374),
-            flex: "1",
-            display: "inline-block",
-            padding: `${toPx(11)} ${toPx(12)} ${toPx(10)}`,
-            border: `${toPx(2)} solid transparent`,
-            borderRadius: toPx(2),
+    formItemChildren_control: {
+        ...applyControl(),
+        verticalAlign: "middle",
+        "& a": {
             cursor: "pointer",
-            overflow: "hidden",
-            lineHeight: "1",
-            textAlign: "center",
-            whiteSpace: "nowrap",
-            verticalAlign: "bottom",
-            transition: "all 0.2s ease-in-out",
-            color: colors.white,
-            background: colors.pink,
-            "&:hover": {
-                background: colors.lightPink,
-            },
-            "&:focus": {
-                outline: "none",
-                background: colors.darkPink,
+            width: "100%",
+            display: "inline-block",
+            "& span, i": {
+                ...ellipsis(),
+                width: "100%",
+                display: "inline-block",
             },
         },
     },
-    formItemChildren_childOptionsMenu: {
-        ...applyCleanListStyle(),
-        "& li": {
-            "& button": {
-                ...applyAddItemStyle(),
-                ...localizePadding(12, 8, 8, 24),
-                ...thickLine,
-            },
-        },
-    },
-    formItemChildren_childOptionsTextButton: {
-        ...applyAddItemStyle(),
-        ...localizePadding(12, 8, 8, 24),
-        ...thickLine,
-        display: "flex",
-        width: "fit-content",
-    },
-    formItemChildren_header: {
-        ...applyHeaderStyle(),
-    },
-    formItemChildren_existingChildren: {
-        ...applyPopupHeadingStyles(),
-        "& ul": {
-            ...applyAriaHiddenStyles(),
-        },
+    formItemChildren_controlLabel: {
+        ...applyLabelStyle(),
+        display: "block",
+        marginTop: toPx(12),
     },
     formItemChildren_addedChildren: {
         ...applyCleanListStyle(),
-        ...applyListItemStyle(),
     },
-    formItemChildren_optionMenu: {
+    formItemChildren_childrenList: {
         ...applyCleanListStyle(),
-        ...applyPopupMenuStyles(),
-    },
-    formItemChildren_optionMenu__listItem: {
-        "& button": {
-            "&::before": {
-                position: "absolute",
-                content: "''",
-                opacity: ".6",
-                pointerEvents: "none",
-                top: toPx(12),
-                width: toPx(16),
-                height: toPx(16),
-                left: toPx(12),
-                /* tslint:disable-next-line */
-                background: trashcan,
+        ...applyAriaHiddenStyles(),
+        background: colors.white,
+        maxHeight: "200px",
+        overflow: "auto",
+        position: "absolute",
+        right: "0",
+        left: "0",
+        zIndex: "1",
+        "& li": {
+            padding: "10px 8px 10px",
+            fontSize: "14px",
+            textAlign: "left",
+            '&[aria-selected="true"]': {
+                background: colors.pink,
             },
         },
+    },
+    formItemChildren_childrenListControl: {
+        position: "relative",
+    },
+    formItemChildren_childrenListInput: {
+        ...applyInputStyle(),
+        marginTop: "8px",
+        paddingRight: "36px",
+        width: "100%",
+    },
+    formItemChildren_childrenListTrigger: {
+        position: "absolute",
+        right: "0",
+        bottom: "0",
+        height: "36px",
+        width: "36px",
+        background: rightArrow,
+        transform: "rotate(90deg)",
+        border: "0",
+        [`&${focusVisible()}`]: {
+            ...insetStrongBoxShadow(colors.pink),
+            outline: "none",
+        },
+    },
+    formItemChildren_delete: {
+        ...applySoftRemove(),
+        cursor: "pointer",
+        position: "relative",
+        verticalAlign: "middle",
+    },
+    formItemChildren_deleteButton: {
+        ...applyRemoveItemStyle(),
+        cursor: "pointer",
     },
 };
 
