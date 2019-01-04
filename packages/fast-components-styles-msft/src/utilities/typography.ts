f77eeb34296e364baf420b91832c97e79bdf066c
jason Falk <falkicon@me.com>
2019-01-03 21:06:05 -0800

    update the T4 type ramp to 28 size and 36 line-height

--- packages/fast-components-styles-msft/src/utilities/typography.ts
+++ packages/fast-components-styles-msft/src/utilities/typography.ts
@@ -50,8 +50,8 @@ export const typeRamp: TypeRamp = {
         lineHeight: 44,
     },
     t4: {
-        fontSize: 24,
-        lineHeight: 32,
+        fontSize: 28,
+        lineHeight: 36,
     },
     t5: {
         fontSize: 20,
