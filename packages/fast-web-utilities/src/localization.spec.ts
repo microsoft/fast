d8fad8aae4714c4a9f5b7d7e2d79663b7a8be169
Chris Holt <chhol@microsoft.com>
2019-01-22 11:07:05 -0800

    add additional test

--- packages/fast-web-utilities/src/localization.spec.ts
+++ packages/fast-web-utilities/src/localization.spec.ts
@@ -1,9 +1,14 @@
 import isRTL from "./localization";
 
 describe("Check to see if a locale is RTL", (): void => {
+    test("should return false if locale is not valid", (): void => {
+        expect(isRTL("")).toBe(false);
+    });
+
     test("en", (): void => {
         expect(isRTL("en")).toBe(false);
     });
+
     test("en-rtl", (): void => {
         expect(isRTL("en-rtl")).toBe(true);
     });
