ec8eefe2047ca6c3a8b1cdd439319113f69eb81a
Chris Holt <chhol@microsoft.com>
2019-01-22 10:49:58 -0800

    feat: add direction and localization helpers to fast-web-utilities

--- packages/fast-web-utilities/src/index.ts
+++ packages/fast-web-utilities/src/index.ts
@@ -2,5 +2,6 @@ export * from "./aria";
 export * from "./dom";
 export * from "./html";
 export * from "./key-codes";
+export * from "./localization";
 export * from "./numbers";
 export * from "./strings";
---
+++ packages/fast-web-utilities/src/localization.ts
@@ -0,0 +1,20 @@
+/**
+ * Expose ltr and rtl strings
+ */
+export enum Direction {
+    ltr = "ltr",
+    rtl = "rtl",
+}
+
+export const localeDirectionMapping: { [key: string]: Direction } = {
+    en: Direction.ltr,
+    "en-rtl": Direction.rtl,
+};
+
+export default function isRTL(locale: string): boolean {
+    if (localeDirectionMapping[locale]) {
+        return localeDirectionMapping[locale] === Direction.rtl;
+    }
+
+    return false;
+}
