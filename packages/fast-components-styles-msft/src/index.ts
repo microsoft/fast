30e761cdfe56ab71553a1c84fb3b165ff7a60640
Jeff Smith <jes@microsoft.com>
2019-01-24 11:19:20 -0800

    added export of focus

--- packages/fast-components-styles-msft/src/index.ts
+++ packages/fast-components-styles-msft/src/index.ts
@@ -146,6 +146,7 @@ export * from "./utilities/breakpoints";
 export * from "./utilities/colors";
 export * from "./utilities/density";
 export * from "./utilities/elevation";
+export * from "./utilities/focus";
 export * from "./utilities/fonts";
 export * from "./utilities/height";
 export * from "./utilities/keyof-to-type";
