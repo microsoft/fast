69a63540bf0ce1be3ebc6490aaba0b04d3eeb145
Jane Chu <7559015+janechu@users.noreply.github.com>
2019-01-26 23:38:09 -0800

    chore: update the testing application UI

--- packages/fast-css-editor-react/app/app.tsx
+++ packages/fast-css-editor-react/app/app.tsx
@@ -1,212 +1,136 @@
 import * as React from "react";
-import Site, {
-    SiteCategory,
-    SiteCategoryDocumentation,
-    SiteCategoryItem,
-    SiteProps,
-    SiteTitle,
-    SiteTitleBrand,
-} from "@microsoft/fast-development-site-react";
-import CSSEditor, {
-    CSSEditorProps,
-    CSSPosition,
-    CSSPositionProps,
-    CSSSpacing,
-    CSSSpacingProps,
-    PositionValue,
-} from "../src";
-import CSSEditorSchema from "../src/editor.schema.json";
-import CSSPositionSchema from "../src/position/position.schema.json";
-import CSSSpacingSchema from "../src/spacing/spacing.schema.json";
+import { DesignSystemProvider } from "@microsoft/fast-jss-manager-react";
+
+import { editorComponents, updateCallbacks } from "./exports.constants";
 
 export interface AppState {
-    editorDetail: CSSEditorProps;
-    editorExample: CSSEditorProps;
-    positionDetail: CSSPositionProps;
-    positionExample: CSSPositionProps;
-    spacingDetail: CSSSpacingProps;
-    spacingExample: CSSSpacingProps;
+    data: any;
+    activeIndex: number;
 }
 
-export enum EditorView {
-    detail = "editorDetail",
-    example = "editorExample",
-    positionDetail = "positionDetail",
-    positionExample = "positionExample",
-    spacingDetail = "spacingDetail",
-    spacingExample = "spacingExample",
+export interface EditorComponentOptions {
+    displayName: string;
+    component: React.ComponentClass;
 }
 
-class App extends React.Component<undefined, AppState> {
+const designSystemDefaults: any = {
+    foregroundColor: "#000",
+    backgroundColor: "#FFF",
+    brandColor: "#0078D4",
+};
+
+class App extends React.Component<{}, AppState> {
     public state: AppState;
 
-    constructor(props: undefined) {
+    constructor(props: {}) {
         super(props);
 
         this.state = {
-            editorDetail: {
-                position: PositionValue.static,
-            },
-            editorExample: {
-                position: PositionValue.absolute,
-                left: "4px",
-                top: "12px",
-            },
-            positionDetail: {
-                position: PositionValue.static,
-            },
-            positionExample: {
-                position: PositionValue.absolute,
-                left: "1px",
-                top: "5px",
-            },
-            spacingDetail: {},
-            spacingExample: {},
+            data: {},
+            activeIndex: 0,
         };
     }
 
     public render(): JSX.Element {
         return (
-            <Site formChildOptions={[]}>
-                <SiteTitle slot={"title"}>
-                    <SiteTitleBrand>FAST</SiteTitleBrand> CSS React editor
-                </SiteTitle>
-                <SiteCategory slot={"category"} name={"Components"}>
-                    <SiteCategory
-                        slot={"category"}
-                        name={"CSS Editor"}
-                        schema={CSSEditorSchema}
-                        component={CSSEditor}
+            <DesignSystemProvider designSystem={designSystemDefaults}>
+                <div
+                    style={{
+                        fontFamily:
+                            "Segoe UI, SegoeUI, Helvetica Neue, Helvetica, Arial, sans-serif",
+                    }}
+                >
+                    <div
+                        style={{
+                            minWidth: "250px",
+                            minHeight: "100vh",
+                            padding: "0 0 0 10px",
+                            boxSizing: "border-box",
+                            color: designSystemDefaults.foregroundColor,
+                            background: designSystemDefaults.backgroundColor,
+                            float: "left",
+                        }}
                     >
-                        <SiteCategoryDocumentation
-                            slot={"canvas-detail-view-documentation"}
-                        >
-                            <div>CSS Editor</div>
-                        </SiteCategoryDocumentation>
-                        <SiteCategoryItem
-                            slot={"canvas-example-view"}
-                            data={Object.assign({}, this.state.editorExample, {
-                                onChange: this.handleEditorExampleUpdate,
-                            })}
-                        />
-                        <SiteCategoryItem
-                            slot={"canvas-detail-view-example"}
-                            data={Object.assign({}, this.state.editorDetail, {
-                                onChange: this.handleEditorDetailUpdate,
-                            })}
-                        />
-                    </SiteCategory>
-                    <SiteCategory
-                        slot={"category"}
-                        name={"CSS Position"}
-                        schema={CSSPositionSchema}
-                        component={CSSPosition}
+                        {this.renderCSSEditorComponent()}
+                    </div>
+                    <div
+                        style={{
+                            float: "left",
+                            marginLeft: "8px",
+                        }}
                     >
-                        <SiteCategoryDocumentation
-                            slot={"canvas-detail-view-documentation"}
+                        <div>
+                            <select onChange={this.handleComponentUpdate}>
+                                {this.renderOptions()}
+                            </select>
+                        </div>
+                        <pre
+                            style={{
+                                padding: "12px",
+                                background: "rgb(244, 245, 246)",
+                                borderRadius: "4px",
+                            }}
                         >
-                            <div>CSS Position</div>
-                        </SiteCategoryDocumentation>
-                        <SiteCategoryItem
-                            slot={"canvas-example-view"}
-                            data={Object.assign({}, this.state.positionExample, {
-                                onPositionUpdate: this.handlePositionExampleUpdate,
-                            })}
-                        />
-                        <SiteCategoryItem
-                            slot={"canvas-detail-view-example"}
-                            data={Object.assign({}, this.state.positionDetail, {
-                                onPositionUpdate: this.handlePositionDetailUpdate,
-                            })}
-                        />
-                    </SiteCategory>
-                    <SiteCategory
-                        slot={"category"}
-                        name={"CSS Spacing"}
-                        schema={CSSSpacingSchema}
-                        component={CSSSpacing}
-                    >
-                        <SiteCategoryDocumentation
-                            slot={"canvas-detail-view-documentation"}
-                        >
-                            <div>CSS Spacing</div>
-                        </SiteCategoryDocumentation>
-                        <SiteCategoryItem
-                            slot={"canvas-example-view"}
-                            data={Object.assign({}, this.state.spacingExample, {
-                                onSpacingUpdate: this.handleSpacingExampleUpdate,
-                            })}
-                        />
-                        <SiteCategoryItem
-                            slot={"canvas-detail-view-example"}
-                            data={Object.assign({}, this.state.spacingDetail, {
-                                onSpacingUpdate: this.handleSpacingDetailUpdate,
-                            })}
-                        />
-                    </SiteCategory>
-                </SiteCategory>
-            </Site>
+                            {JSON.stringify(this.state.data, null, 2)}
+                        </pre>
+                        <div style={{ position: "relative" }}>
+                            <div
+                                style={{
+                                    backgroundColor: "red",
+                                    width: "50px",
+                                    height: "50px",
+                                    ...this.state.data,
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </DesignSystemProvider>
         );
     }
 
-    private handleEditorDetailUpdate = (updateEditorDetail: any): void => {
-        this.handleExampleUpdate(updateEditorDetail, EditorView.detail);
+    private handleComponentUpdate = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+        this.setState({
+            activeIndex: parseInt(e.target.value, 10),
+            data: {},
+        });
     };
 
-    private handleEditorExampleUpdate = (updateEditorExample: any): void => {
-        this.handleExampleUpdate(updateEditorExample, EditorView.example);
+    private handleComponentOnChange = (data: any): void => {
+        this.setState({
+            data,
+        });
     };
 
-    private handleExampleUpdate = (updateExample: any, stateKey: EditorView): void => {
-        const stateUpdate: Partial<AppState> = {};
-        stateUpdate[stateKey] = updateExample;
+    private renderCSSEditorComponent(): React.ReactNode {
+        const callbacks: any = {};
 
-        this.setState(stateUpdate as AppState);
-    };
-
-    private handlePositionDetailUpdate = (updatePositionDetail: any): void => {
-        this.handleExampleUpdate(
-            Object.assign(
-                {},
-                { onPositionUpdate: this.state.positionDetail.onPositionUpdate },
-                updatePositionDetail
-            ),
-            EditorView.positionDetail
+        updateCallbacks.forEach(
+            (updateCallback: string): any => {
+                callbacks[updateCallback] = this.handleComponentOnChange;
+            }
         );
-    };
 
-    private handlePositionExampleUpdate = (updatePositionExample: any): void => {
-        this.handleExampleUpdate(
-            Object.assign(
-                {},
-                { onPositionUpdate: this.state.positionExample.onPositionUpdate },
-                updatePositionExample
-            ),
-            EditorView.positionExample
+        return React.createElement(
+            editorComponents[this.state.activeIndex].component as any,
+            {
+                ...this.state.data,
+                ...callbacks,
+            }
         );
-    };
+    }
 
-    private handleSpacingDetailUpdate = (updateSpacingDetail: any): void => {
-        this.handleExampleUpdate(
-            Object.assign(
-                {},
-                { onSpacingUpdate: this.state.spacingDetail.onSpacingUpdate },
-                updateSpacingDetail
-            ),
-            EditorView.spacingDetail
+    private renderOptions(): React.ReactNode {
+        return editorComponents.map(
+            (editorComponent: EditorComponentOptions, index: number) => {
+                return (
+                    <option key={index} value={index}>
+                        {editorComponent.displayName}
+                    </option>
+                );
+            }
         );
-    };
-
-    private handleSpacingExampleUpdate = (updateSpacingExample: any): void => {
-        this.handleExampleUpdate(
-            Object.assign(
-                {},
-                { onSpacingUpdate: this.state.spacingExample.onSpacingUpdate },
-                updateSpacingExample
-            ),
-            EditorView.spacingExample
-        );
-    };
+    }
 }
 
 export default App;
--- packages/fast-css-editor-react/webpack.config.js
+++ packages/fast-css-editor-react/webpack.config.js
@@ -4,49 +4,70 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const appDir = path.resolve(__dirname, "./app");
 const outDir = path.resolve(__dirname, "./www");
 
-module.exports = {
-    devtool: "inline-source-map",
-    entry: path.resolve(appDir, "index.tsx"),
-    output: {
-        path: outDir,
-        publicPath: "/",
-        filename: "[name].js"
-    },
-    mode: process.env.NODE_ENV || "development",
-    module: {
-        rules: [
-            {
-                test: /.tsx?$/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            transpileOnly: true
-                        }
-                    }
-                ]
+
+module.exports = (env, args) => {
+    const isProduction = args.mode === "production";
+
+    return {
+        devtool: isProduction ? "none" : "inline-source-map",
+        entry: {
+            app: path.resolve(appDir, "index.tsx")
+        },
+        output: {
+            path: outDir,
+            publicPath: "/",
+            filename: isProduction ? "[name]-[contenthash].js" : "[name].js"
+        },
+        mode: args.mode || "development",
+        optimization: {
+            splitChunks: {
+                chunks: "all",
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                    },
+                },
             }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            contentBase: outDir,
-        })
-    ],
-    resolve: {
-        extensions: [".js", ".tsx", ".ts", ".json"],
-        alias: {
-            fbjs: path.resolve('./node_modules/fbjs'),
-            'lodash-es': path.resolve('./node_modules/lodash-es'),
-            react: path.resolve('./node_modules/react'),
-            'react-dom': path.resolve('./node_modules/react-dom'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /.tsx?$/,
+                    use: [
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                compilerOptions: {
+                                    declaration: false,
+                                }
+                            }
+                        }
+                    ],
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "FAST documentation",
+                contentBase: outDir,
+            }),
+        ],
+        resolve: {
+            extensions: [".js", ".tsx", ".ts", ".json"],
+            alias: {
+                fbjs: path.resolve('./node_modules/fbjs'),
+                'lodash-es': path.resolve('./node_modules/lodash-es'),
+                react: path.resolve('./node_modules/react'),
+                'react-dom': path.resolve('./node_modules/react-dom'),
+            }
+        },
+        devServer: {
+            compress: false,
+            historyApiFallback: true,
+            open: true,
+            overlay: true,
+            port: 7008
         }
-    },
-    devServer: {
-        compress: false,
-        historyApiFallback: true,
-        open: true,
-        overlay: true,
-        port: 7008
     }
 }
