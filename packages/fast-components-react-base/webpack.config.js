5926db7a33101e095c437e7279f2358772bd677b
Jane Chu <7559015+janechu@users.noreply.github.com>
2019-02-06 10:59:13 -0800

    chore: fix a build break in a test file (#1361)

--- packages/fast-components-react-base/webpack.config.js
+++ packages/fast-components-react-base/webpack.config.js
@@ -6,49 +6,63 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const appDir = path.resolve(__dirname, "./app");
 const outDir = path.resolve(__dirname, "./www");
 
-module.exports = {
-    entry: path.resolve(appDir, "index.tsx"),
-    output: {
-        path: outDir,
-        publicPath: "/",
-        filename: "[name].js"
-    },
-    mode: process.env.NODE_ENV || "development",
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /.tsx?$/,
-                use: [
-                    {
-                        loader: "ts-loader"
-                    }
-                ],
-                exclude: /node_modules/
+module.exports = (env, args) => {
+    const isProduction = args.mode === "production";
+
+    return {
+        entry: path.resolve(appDir, "index.tsx"),
+        output: {
+            path: outDir,
+            publicPath: "/",
+            filename: isProduction ? "[name]-[contenthash].js" : "[name].js"
+        },
+        mode: args.mode || "development",
+        optimization: {
+            splitChunks: {
+                chunks: "all",
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                    },
+                },
             }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            contentBase: outDir,
-        }),
-        new WebpackShellPlugin({
-            onBuildStart: [
-                `npm run convert:readme`
+        },
+        resolve: {
+            extensions: ['.js', '.ts', '.tsx'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /.tsx?$/,
+                    use: [
+                        {
+                            loader: "ts-loader"
+                        }
+                    ],
+                    exclude: /node_modules/
+                }
             ]
-        }),
-        new BundleAnalyzerPlugin({
-            // Remove this to inspect bundle sizes.
-            analyzerMode: "disabled"
-        })
-    ],
-    devServer: {
-        compress: false,
-        historyApiFallback: true,
-        open: true,
-        overlay: true,
-        port: 7000,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                contentBase: outDir,
+            }),
+            new WebpackShellPlugin({
+                onBuildStart: [
+                    `npm run convert:readme`
+                ]
+            }),
+            new BundleAnalyzerPlugin({
+                // Remove this to inspect bundle sizes.
+                analyzerMode: "disabled"
+            })
+        ],
+        devServer: {
+            compress: false,
+            historyApiFallback: true,
+            open: true,
+            overlay: true,
+            port: 7000,
+        }
     }
 }
