582b741b2ee4309590e39c6d55cfc1cb09743bbf
nalogiudice <random.interval@gmail.com>
2019-02-07 12:08:57 -0800

    fixes import

--- packages/fast-components-react-base/src/badge/badge.spec.tsx
+++ packages/fast-components-react-base/src/badge/badge.spec.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as Adapter from "enzyme-adapter-react-16";
 import { configure, mount, shallow } from "enzyme";
-import { BadgeClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
 import Badge, { BadgeHandledProps, BadgeProps, BadgeUnhandledProps } from "./index";
 
 /*
