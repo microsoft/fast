a411a041609d953a32822165b32f71466a255435
Nicholas Rice <nicholas.rice119@gmail.com>
2019-02-07 12:13:05 -0800

    feat: add MSFT palette-based color utilities (#1363)
    * migrate color utilities
    
    * fixing linting errors
    
    * fixing other issues
    
    * fixing failing test
    
    * todo cleanup
    
    * export color utilities
    
    * remove design system export
    
    * updates from code-review
    
    * change key text
    
    * update language

---
+++ packages/fast-components-styles-msft/src/utilities/color/accent-foreground-cut.ts
@@ -0,0 +1,55 @@
+import { Swatch } from "./palette";
+import { black, white } from "./color-constants";
+import chroma from "chroma-js";
+import { memoize } from "lodash-es";
+import { DesignSystem, ensureDesignSystemDefaults } from "../../design-system";
+import { accentSwatch } from "./accent";
+import { ColorRecipe } from "./common";
+
+/**
+ * Function to derive accentForegroundCut from an input background and target contrast ratio
+ */
+const accentForegroundCutAlgorithm: (
+    backgroundColor: Swatch,
+    targetContrast: number
+) => Swatch = memoize(
+    (backgroundColor: Swatch, targetContrast: number): Swatch => {
+        return chroma.contrast(white, backgroundColor) >= targetContrast ? white : black;
+    },
+    (backgroundColor: Swatch, targetContrast: number): string => {
+        return backgroundColor.concat(targetContrast.toString());
+    }
+);
+
+/**
+ * Factory to create a accent-foreground-cut function that operates on a target contrast ratio
+ */
+function accentForegroundCutFactory(contrast: number): ColorRecipe {
+    function accentForegroundCutInternal(designSystem: DesignSystem): Swatch;
+    function accentForegroundCutInternal(
+        backgroundResolver: (d: DesignSystem) => Swatch
+    ): (designSystem: DesignSystem) => Swatch;
+    function accentForegroundCutInternal(arg: any): any {
+        if (typeof arg === "function") {
+            return ensureDesignSystemDefaults(
+                (designSystem: DesignSystem): Swatch => {
+                    return accentForegroundCutAlgorithm(arg(designSystem), contrast);
+                }
+            );
+        } else {
+            return accentForegroundCutAlgorithm(accentSwatch(arg), contrast);
+        }
+    }
+
+    return accentForegroundCutInternal;
+}
+
+/**
+ * Cut text for normal sized text, less than 18pt normal weight
+ */
+export const accentForegroundCut: ColorRecipe = accentForegroundCutFactory(4.5);
+
+/**
+ * Cut text for large sized text, greater than 18pt or 16pt and bold
+ */
+export const accentForegroundCutLarge: ColorRecipe = accentForegroundCutFactory(3);
