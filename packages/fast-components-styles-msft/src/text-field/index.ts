45491996660df86d761e4e21aaa42a6accbc54e6
nalogiudice <random.interval@gmail.com>
2019-02-12 13:23:11 -0800

    adds common input field pattern

--- packages/fast-components-styles-msft/src/text-field/index.ts
+++ packages/fast-components-styles-msft/src/text-field/index.ts
@@ -1,15 +1,10 @@
-import { applyTypeRampConfig } from "../utilities/typography";
 import { DesignSystem, withDesignSystemDefaults } from "../design-system";
 import { TextFieldClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
-import { applyFocusVisible, toPx } from "@microsoft/fast-jss-utilities";
-import { get } from "lodash-es";
+import { toPx } from "@microsoft/fast-jss-utilities";
 import { ComponentStyles, ComponentStyleSheet } from "@microsoft/fast-jss-manager";
-import { fontWeight } from "../utilities/fonts";
-import { foregroundNormal } from "../utilities/colors";
 import { density } from "../utilities/density";
 import { defaultHeight, maxHeight, minHeight } from "../utilities/height";
-import outlinePattern from "../patterns/outline";
-import typographyPattern from "../patterns/typography";
+import { inputFieldStyles } from "../patterns/input-field";
 
 const styles: ComponentStyles<TextFieldClassNameContract, DesignSystem> = (
     config: DesignSystem
@@ -18,35 +13,10 @@ const styles: ComponentStyles<TextFieldClassNameContract, DesignSystem> = (
 
     return {
         textField: {
-            ...applyTypeRampConfig("t7"),
-            ...outlinePattern.rest,
-            ...typographyPattern.rest,
-            fontFamily: "inherit",
-            fontWeight: fontWeight.normal.toString(),
-            boxSizing: "border-box",
-            borderRadius: toPx(designSystem.cornerRadius),
-            padding: "10px",
-            margin: "0",
+            ...inputFieldStyles(designSystem),
             height: density(defaultHeight)(designSystem),
             minHeight: toPx(minHeight),
             maxHeight: toPx(maxHeight),
-            "&:hover": {
-                ...outlinePattern.hover,
-            },
-            ...applyFocusVisible({
-                ...outlinePattern.focus,
-            }),
-            "&:disabled": {
-                ...outlinePattern.disabled,
-                ...typographyPattern.disabled,
-                cursor: "not-allowed",
-                "&::placeholder": {
-                    ...typographyPattern.disabled,
-                },
-            },
-            "&::placeholder": {
-                color: foregroundNormal,
-            },
         },
     };
 };
