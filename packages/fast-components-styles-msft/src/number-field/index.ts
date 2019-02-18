0836281c0b3091d7eda2a40cc9b0408701f97f84
nalogiudice <random.interval@gmail.com>
2019-02-18 11:58:41 -0800

    updates styles

--- packages/fast-components-styles-msft/src/number-field/index.ts
+++ packages/fast-components-styles-msft/src/number-field/index.ts
@@ -1,14 +1,10 @@
 import { DesignSystem, withDesignSystemDefaults } from "../design-system";
 import { ComponentStyles, ComponentStyleSheet } from "@microsoft/fast-jss-manager";
 import { NumberFieldClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
-import { applyFocusVisible, toPx } from "@microsoft/fast-jss-utilities";
+import { toPx } from "@microsoft/fast-jss-utilities";
 import { density } from "../utilities/density";
 import { defaultHeight, maxHeight, minHeight } from "../utilities/height";
-import { foregroundNormal } from "../utilities/colors";
-import outlinePattern from "../patterns/outline";
-import typographyPattern from "../patterns/typography";
-import { applyTypeRampConfig } from "../utilities/typography";
-import { fontWeight } from "../utilities/fonts";
+import { inputFieldStyles } from "../patterns/input-field";
 
 /* tslint:disable-next-line */
 const styles: ComponentStyles<NumberFieldClassNameContract, DesignSystem> = (
@@ -18,32 +14,7 @@ const styles: ComponentStyles<NumberFieldClassNameContract, DesignSystem> = (
 
     return {
         numberField: {
-            ...applyTypeRampConfig("t7"),
-            ...outlinePattern.rest,
-            ...typographyPattern.rest,
-            fontFamily: "inherit",
-            fontWeight: fontWeight.normal.toString(),
-            boxSizing: "border-box",
-            borderRadius: toPx(designSystem.cornerRadius),
-            padding: "10px",
-            margin: "0",
-            "&:hover": {
-                ...outlinePattern.hover,
-            },
-            ...applyFocusVisible({
-                ...outlinePattern.focus,
-            }),
-            "&:disabled": {
-                ...outlinePattern.disabled,
-                ...typographyPattern.disabled,
-                cursor: "not-allowed",
-                "&::placeholder": {
-                    ...typographyPattern.disabled,
-                },
-            },
-            "&::placeholder": {
-                color: foregroundNormal,
-            },
+            ...inputFieldStyles(designSystem),
             height: density(defaultHeight)(designSystem),
             minHeight: toPx(minHeight),
             maxHeight: toPx(maxHeight),
