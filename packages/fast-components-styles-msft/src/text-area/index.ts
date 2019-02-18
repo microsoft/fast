45be9df1c0a7db5f5c379f23dc04f7f9c80e8f00
nalogiudice <random.interval@gmail.com>
2019-02-18 08:39:29 -0800

    updates rebase

--- packages/fast-components-styles-msft/src/text-area/index.ts
+++ packages/fast-components-styles-msft/src/text-area/index.ts
@@ -1,15 +1,7 @@
-import { applyTypeRampConfig } from "../utilities/typography";
 import { DesignSystem, withDesignSystemDefaults } from "../design-system";
 import { TextAreaClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
-import { applyFocusVisible, toPx } from "@microsoft/fast-jss-utilities";
-import { get } from "lodash-es";
+import { toPx } from "@microsoft/fast-jss-utilities";
 import { ComponentStyles, ComponentStyleSheet } from "@microsoft/fast-jss-manager";
-import { fontWeight } from "../utilities/fonts";
-import {
-    disabledContrast,
-    ensureForegroundNormal,
-    foregroundNormal,
-} from "../utilities/colors";
 import { density } from "../utilities/density";
 import { defaultHeight, maxHeight, minHeight } from "../utilities/height";
 import { inputFieldStyles } from "../patterns/input-field";
@@ -26,23 +18,6 @@ const styles: ComponentStyles<TextAreaClassNameContract, DesignSystem> = (
             minHeight: toPx(minHeight * 2),
             maxHeight: toPx(maxHeight * 2),
             maxWidth: "100%",
-            "&:hover": {
-                ...outlinePattern.hover,
-            },
-            ...applyFocusVisible({
-                ...outlinePattern.focus,
-            }),
-            "&:disabled": {
-                ...outlinePattern.disabled,
-                ...typographyPattern.disabled,
-                cursor: "not-allowed",
-                "&::placeholder": {
-                    ...typographyPattern.disabled,
-                },
-            },
-            "&::placeholder": {
-                color: foregroundNormal,
-            },
         },
     };
 };
--- packages/fast-components-styles-msft/src/text-field/index.ts
+++ packages/fast-components-styles-msft/src/text-field/index.ts
@@ -1,11 +1,7 @@
-import { applyTypeRampConfig } from "../utilities/typography";
 import { DesignSystem, withDesignSystemDefaults } from "../design-system";
 import { TextFieldClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
-import { applyFocusVisible, toPx } from "@microsoft/fast-jss-utilities";
-import { get } from "lodash-es";
+import { toPx } from "@microsoft/fast-jss-utilities";
 import { ComponentStyles, ComponentStyleSheet } from "@microsoft/fast-jss-manager";
-import { fontWeight } from "../utilities/fonts";
-import { foregroundNormal } from "../utilities/colors";
 import { density } from "../utilities/density";
 import { defaultHeight, maxHeight, minHeight } from "../utilities/height";
 import { inputFieldStyles } from "../patterns/input-field";
@@ -21,23 +17,6 @@ const styles: ComponentStyles<TextFieldClassNameContract, DesignSystem> = (
             height: density(defaultHeight)(designSystem),
             minHeight: toPx(minHeight),
             maxHeight: toPx(maxHeight),
-            "&:hover": {
-                ...outlinePattern.hover,
-            },
-            ...applyFocusVisible({
-                ...outlinePattern.focus,
-            }),
-            "&:disabled": {
-                ...outlinePattern.disabled,
-                ...typographyPattern.disabled,
-                cursor: "not-allowed",
-                "&::placeholder": {
-                    ...typographyPattern.disabled,
-                },
-            },
-            "&::placeholder": {
-                color: foregroundNormal,
-            },
         },
     };
 };
