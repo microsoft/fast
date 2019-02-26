43c932b1f92313fad1977240af2bd7ed2bef8f3e
Nick Rice <nicholas.rice119@gmail.com>
2019-02-26 13:03:37 -0800

    fixing color count

--- packages/fast-components-styles-msft/src/utilities/color/color-constants.ts
+++ packages/fast-components-styles-msft/src/utilities/color/color-constants.ts
@@ -3,7 +3,7 @@ import { ColorPaletteConfig, parseColorHexRGB } from "@microsoft/fast-colors";
 export const white: string = "#FFFFFF";
 export const black: string = "#000000";
 export const accent: string = "#0078D4";
-export const paletteSteps: number = 64;
+export const paletteSteps: number = 63;
 
 /**
  * Default palette sources
