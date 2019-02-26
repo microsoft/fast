6f054c69ae9822eb8a68dc5dfeb19a02f7aa17cc
Nicholas Rice <nicholas.rice119@gmail.com>
2019-02-26 11:31:06 -0800

    feat: add neutral-foreground states (#1439)
    * adding tests
    
    * update readme and removing unused code
    
    * rename spec
    
    * cleanup

--- packages/fast-components-styles-msft/src/utilities/color/neutral-foreground.ts
+++ packages/fast-components-styles-msft/src/utilities/color/neutral-foreground.ts
@@ -1,11 +1,20 @@
 import { memoize } from "lodash-es";
-import { isDarkTheme, palette, PaletteType, Swatch } from "./palette";
-import { SwatchResolver } from "./common";
+import { isDarkTheme, palette, Palette, PaletteType, Swatch } from "./palette";
+import {
+    ColorRecipe,
+    StatefulSwatch,
+    StatefulSwatchToColorRecipeFactory,
+    SwatchResolver,
+    SwatchStates,
+} from "./common";
 import {
     DesignSystem,
     ensureDesignSystemDefaults,
     withDesignSystemDefaults,
 } from "../../design-system";
+
+export const neutralForegroundDeltaHover: number = 4;
+export const neutralForegroundDeltaActive: number = 8;
 
 /**
  * Function to derive neutralForeground from color inputs.
@@ -13,11 +22,25 @@ import {
  * the color that has the most contrast against the background. If contrast
  * cannot be retrieved correctly, function returns black.
  */
-const neutralForegroundAlgorithm: SwatchResolver = memoize(
-    (designSystem: DesignSystem): Swatch => {
-        return isDarkTheme(designSystem)
+const neutralForegroundAlgorithm: (
+    designSystem: DesignSystem
+) => StatefulSwatch = memoize(
+    (designSystem: DesignSystem): StatefulSwatch => {
+        const neutralPalette: Palette = palette(PaletteType.neutral)(designSystem);
+        const isDark: boolean = isDarkTheme(designSystem);
+        const direction: 1 | -1 = isDark ? 1 : -1;
+        const restColor: Swatch = isDark
             ? neutralForegroundLight(designSystem)
             : neutralForegroundDark(designSystem);
+        const restIndex: number = neutralPalette.indexOf(restColor);
+        const hoverIndex: number = restIndex + neutralForegroundDeltaHover * direction;
+        const activeIndex: number = restIndex + neutralForegroundDeltaActive * direction;
+
+        return {
+            rest: neutralPalette[restIndex],
+            hover: neutralPalette[hoverIndex],
+            active: neutralPalette[activeIndex],
+        };
     },
     (designSystem: DesignSystem): string => {
         return designSystem.backgroundColor;
@@ -38,14 +61,14 @@ export function neutralForegroundDark(designSystem: DesignSystem): Swatch {
     return palette(PaletteType.neutral)(designSystem)[58];
 }
 
-export function neutralForeground(designSystem: DesignSystem): Swatch;
+export function neutralForeground(designSystem: DesignSystem): StatefulSwatch;
 export function neutralForeground(
-    backgroundResolver: (designSystem: DesignSystem) => Swatch
-): (designSystem: DesignSystem) => Swatch;
+    backgroundResolver: SwatchResolver
+): (designSystem: DesignSystem) => StatefulSwatch;
 export function neutralForeground(arg: any): any {
     if (typeof arg === "function") {
         return ensureDesignSystemDefaults(
-            (designSystem: DesignSystem): Swatch => {
+            (designSystem: DesignSystem): StatefulSwatch => {
                 const backgroundColor: Swatch = arg(designSystem);
                 return neutralForegroundAlgorithm(
                     Object.assign({}, designSystem, {
@@ -58,3 +81,16 @@ export function neutralForeground(arg: any): any {
         return neutralForegroundAlgorithm(withDesignSystemDefaults(arg));
     }
 }
+
+export const neutralForegroundRest: ColorRecipe = StatefulSwatchToColorRecipeFactory(
+    SwatchStates.rest,
+    neutralForeground
+);
+export const neutralForegroundHover: ColorRecipe = StatefulSwatchToColorRecipeFactory(
+    SwatchStates.hover,
+    neutralForeground
+);
+export const neutralForegroundActive: ColorRecipe = StatefulSwatchToColorRecipeFactory(
+    SwatchStates.active,
+    neutralForeground
+);
