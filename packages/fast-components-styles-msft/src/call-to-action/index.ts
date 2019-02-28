e7438449b3b430b934e4088beea95ded813a95e7
Jeff Smith <37851214+eljefe223@users.noreply.github.com>
2019-02-27 17:19:46 -0800

    feat: add new color system to call to action, card, checkbox and context menu (#1459)
    * fix: update to use esModuleInterop in the TypeScript configuration files (#1211)
    
    * feat: add MSFT palette-based color utilities (#1363)
    
    * migrate color utilities
    
    * fixing linting errors
    
    * fixing other issues
    
    * fixing failing test
    
    * todo cleanup
    
    * export color utilities
    
    * remove design system export
    
    * updates from code-review
    
    * change key text
    
    * update language
    
    * feat: update neutral-outline and neutral-stealth offsets (#1403)
    
    * chore: fixing color unit tests (#1415)
    
    * feat: audit and update component handled and unhandled prop contracts (#1417)
    
    * update checkbox unhandled props to align correctly extend HTMLDivAttributes
    
    * update hypertext prop and unhandled prop contract to be accurate
    
    * update radio handled prop contract
    
    * update tab unhandled prop contract
    
    * update text area unhandled prop contract
    
    * update toggle prop contract and text-field prop and unhandled prop contract
    
    * typography, badge, heading, metatext
    
    * update text action and add link to bug for unhandled props
    
    * fix tests and correct image children issue
    
    * feat: add selected color to all fill recipes  (#1420)
    
    * adding selected color
    
    * adding unit tests
    
    * ensure exports
    
    * feat: add neutral fill input recipe (#1425)
    
    * adding neutral fill input recipe
    
    * adding exports
    
    * removing else statement
    
    * feat: add color library with color space conversions (rpg, hsl, hsv, xyz, lab, lch), blending functions, and palette generation (#1432)
    
    * feat: add neutral-foreground states (#1439)
    
    * adding tests
    
    * update readme and removing unused code
    
    * rename spec
    
    * cleanup
    
    * add new color system to call to action, card, checkbox and context menu
    
    * alphabetize imports
    
    * missed checkbox focus value
    
    * added active states

--- packages/fast-components-styles-msft/src/call-to-action/index.ts
+++ packages/fast-components-styles-msft/src/call-to-action/index.ts
@@ -113,11 +113,14 @@ const styles: ComponentStyles<CallToActionClassNameContract, DesignSystem> = (
         callToAction__disabled: {
             "& $callToAction_glyph": {
                 fill: neutralForegroundRest,
+<<<<<<< HEAD
             },
             "&:hover": {
                 "& $callToAction_glyph": {
                     transform: "none",
                 },
+=======
+>>>>>>> feat: add new color system to call to action, card, checkbox and context menu (#1459)
             },
         },
     };
