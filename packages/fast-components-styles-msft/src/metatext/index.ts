fbc5846cce56ff34320b77717c62116666492702
Jeff Smith <37851214+eljefe223@users.noreply.github.com>
2019-02-27 17:07:09 -0800

    feat: add new color system to dialog, divider, flipper, hypertext, label, metatext and input field (#1460)
    * fix: update to use esModuleInterop in the TypeScript configuration files (#1211)
    
    * feat: add MSFT palette-based color utilities (#1363)
    
    * migrate color utilities
    
    * fixing linting errors
    
    * fixing other issues
    
    * fixing failing test
    
    * todo cleanup
    
    * export color utilities
    
    * remove design system export
    
    * updates from code-review
    
    * change key text
    
    * update language
    
    * feat: update neutral-outline and neutral-stealth offsets (#1403)
    
    * chore: fixing color unit tests (#1415)
    
    * feat: audit and update component handled and unhandled prop contracts (#1417)
    
    * update checkbox unhandled props to align correctly extend HTMLDivAttributes
    
    * update hypertext prop and unhandled prop contract to be accurate
    
    * update radio handled prop contract
    
    * update tab unhandled prop contract
    
    * update text area unhandled prop contract
    
    * update toggle prop contract and text-field prop and unhandled prop contract
    
    * typography, badge, heading, metatext
    
    * update text action and add link to bug for unhandled props
    
    * fix tests and correct image children issue
    
    * feat: add selected color to all fill recipes  (#1420)
    
    * adding selected color
    
    * adding unit tests
    
    * ensure exports
    
    * feat: add neutral fill input recipe (#1425)
    
    * adding neutral fill input recipe
    
    * adding exports
    
    * removing else statement
    
    * feat: add color library with color space conversions (rpg, hsl, hsv, xyz, lab, lch), blending functions, and palette generation (#1432)
    
    * feat: add neutral-foreground states (#1439)
    
    * adding tests
    
    * update readme and removing unused code
    
    * rename spec
    
    * cleanup
    
    * add new color system to divider, flipper, hypertext, label, input
    
    * added hover and active to hypertext
    
    * added the right states

--- packages/fast-components-styles-msft/src/metatext/index.ts
+++ packages/fast-components-styles-msft/src/metatext/index.ts
@@ -5,23 +5,17 @@ import {
     CSSRules,
 } from "@microsoft/fast-jss-manager";
 import { MetatextClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
-import { get } from "lodash-es";
-import Chroma from "chroma-js";
+import { neutralForegroundHint } from "../utilities/color";
 import { applyTypeRampConfig } from "../utilities/typography";
 
 /* tslint:disable-next-line */
 const styles: ComponentStyles<MetatextClassNameContract, DesignSystem> = (
     config: DesignSystem
 ): ComponentStyleSheet<MetatextClassNameContract, DesignSystem> => {
-    const backgroundColor: string =
-        get(config, "backgroundColor") || designSystemDefaults.backgroundColor;
-    const foregroundColor: string =
-        get(config, "foregroundColor") || designSystemDefaults.foregroundColor;
-
     return {
         metatext: {
             ...applyTypeRampConfig("t7"),
-            color: Chroma.mix(backgroundColor, foregroundColor, 0.6).css(),
+            color: neutralForegroundHint,
         },
     };
 };
