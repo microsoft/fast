029c99908a13ac1e396c2440e5ab2bfc9f974585
Nick Rice <nicholas.rice119@gmail.com>
2019-04-12 12:12:50 -0700

    adding more math utilities and updating tests

--- packages/fast-jss-utilities/src/math.spec.ts
+++ packages/fast-jss-utilities/src/math.spec.ts
@@ -1,4 +1,4 @@
-import { multiply } from "./math";
+import { add, divide, multiply, subtract } from "./math";
 
 describe("multiply", (): void => {
     test("should return a function", (): void => {
@@ -19,5 +19,75 @@ describe("multiply", (): void => {
 
     test("should invoke function arguments with input config", (): void => {
         expect(multiply((conf: number): number => conf, 2)(8)).toBe(16);
+        expect(multiply(10, (conf: number): number => conf, 2)(8)).toBe(160);
+    });
+});
+
+describe("divide", (): void => {
+    test("should return a function", (): void => {
+        expect(typeof divide(2, 4)).toBe("function");
+    });
+
+    test("should divide two numbers", (): void => {
+        expect(divide(4, 2)(undefined)).toBe(2);
+    });
+
+    test("should accept any number of arguments", (): void => {
+        expect(divide(2, 1, 1, 1, 1, 1)()).toBe(2);
+    });
+
+    test("should accept a function that resolves to a number", (): void => {
+        expect(divide((): number => 2, 2)()).toBe(1);
+    });
+
+    test("should invoke function arguments with input config", (): void => {
+        expect(divide((conf: number): number => conf, 10)(1000)).toBe(100);
+        expect(divide(1000, (conf: number): number => conf, 10)(2)).toBe(50);
+    });
+});
+
+describe("add", (): void => {
+    test("should return a function", (): void => {
+        expect(typeof add(2, 4)).toBe("function");
+    });
+
+    test("should add two numbers", (): void => {
+        expect(add(4, 2)(undefined)).toBe(6);
+    });
+
+    test("should accept any number of arguments", (): void => {
+        expect(add(2, 1, 1, 1, 1, 1)()).toBe(7);
+    });
+
+    test("should accept a function that resolves to a number", (): void => {
+        expect(add((): number => 2, 2)()).toBe(4);
+    });
+
+    test("should invoke function arguments with input config", (): void => {
+        expect(add((conf: number): number => conf, 10)(1000)).toBe(1010);
+        expect(add(20, (conf: number): number => conf, 10)(1000)).toBe(1030);
+    });
+});
+
+describe("subtract", (): void => {
+    test("should return a function", (): void => {
+        expect(typeof subtract(2, 4)).toBe("function");
+    });
+
+    test("should subtract two numbers", (): void => {
+        expect(subtract(2, 4)(undefined)).toBe(-2);
+    });
+
+    test("should accept any number of arguments", (): void => {
+        expect(subtract(12, 1, 1, 1, 1, 1)()).toBe(7);
+    });
+
+    test("should accept a function that resolves to a number", (): void => {
+        expect(subtract((): number => 2, 2)()).toBe(0);
+    });
+
+    test("should invoke function arguments with input config", (): void => {
+        expect(subtract((conf: number): number => conf, 10)(1000)).toBe(990);
+        expect(subtract(10, (conf: number): number => conf, 10)(1000)).toBe(-1000);
     });
 });
--- packages/fast-jss-utilities/src/math.ts
+++ packages/fast-jss-utilities/src/math.ts
@@ -1,20 +1,76 @@
+export type MathUtilityArgumentList<T> = Array<number | ((designSystem: T) => number)>;
+export type MathUtilityFunction<T> = (
+    ...args: MathUtilityArgumentList<T>
+) => (designSystem?: T) => number;
+
+function performOperation<T>(
+    opperation: (a: number, b: number) => number
+): MathUtilityFunction<T> {
+    return (...args: MathUtilityArgumentList<T>): ((designSystem?: T) => number) => {
+        return (designSystem?: T): number => {
+            const firstArg: number | ((designSystem: T) => number) = args[0];
+            let value: number =
+                typeof firstArg === "function" ? firstArg(designSystem) : firstArg;
+
+            for (let i: number = 1; i < args.length; i++) {
+                const currentValue: number | ((designSystem: T) => number) = args[i];
+                value = opperation(
+                    value,
+                    typeof currentValue === "function"
+                        ? currentValue(designSystem)
+                        : currentValue
+                );
+            }
+
+            return value;
+        };
+    };
+}
+
+const _add: MathUtilityFunction<unknown> = performOperation(
+    (a: number, b: number): number => a + b
+);
+const _subtract: MathUtilityFunction<unknown> = performOperation(
+    (a: number, b: number): number => a - b
+);
+const _multiply: MathUtilityFunction<unknown> = performOperation(
+    (a: number, b: number): number => a * b
+);
+const _divide: MathUtilityFunction<unknown> = performOperation(
+    (a: number, b: number): number => a / b
+);
+/**
+ * Adds numbers or functions that accept a design system and return a number.
+ */
+export function add<T>(
+    ...args: MathUtilityArgumentList<T>
+): (designSystem?: T) => number {
+    return _add.apply(this, args);
+}
+
+/**
+ * Subtract numbers or functions that accept a design system and return a number.
+ */
+export function subtract<T>(
+    ...args: MathUtilityArgumentList<T>
+): (designSystem?: T) => number {
+    return _subtract.apply(this, args);
+}
+
 /**
  * Multiplies numbers or functions that accept a design system and return a number.
  */
 export function multiply<T>(
-    ...args: Array<number | ((designSystem?: T) => number)>
+    ...args: MathUtilityArgumentList<T>
 ): (designSystem?: T) => number {
-    return (designSystem?: T): number => {
-        let value: number = 1;
+    return _multiply.apply(this, args);
+}
 
-        for (const currentValue of args) {
-            value =
-                value *
-                (typeof currentValue === "function"
-                    ? currentValue(designSystem)
-                    : currentValue);
-        }
-
-        return value;
-    };
+/**
+ * Divides numbers or functions that accept a design system and return a number.
+ */
+export function divide<T>(
+    ...args: MathUtilityArgumentList<T>
+): (designSystem?: T) => number {
+    return _divide.apply(this, args);
 }
