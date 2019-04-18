ff1b7c5d7ffb9fa706875f780e340835ad3b3f24
Nick Rice <nicholas.rice119@gmail.com>
2019-04-18 14:03:45 -0700

    fixing tests

--- packages/fast-app-shell-react/src/app-shell/app-shell.spec.tsx
+++ packages/fast-app-shell-react/src/app-shell/app-shell.spec.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { AppShell } from "./index";
 import Adapter from "enzyme-adapter-react-16";
-import { configure, shallow } from "enzyme";
+import { configure, mount } from "enzyme";
 
 configure({ adapter: new Adapter() });
 
 describe("AppShell", (): void => {
     test("should not throw", (): void => {
         expect(() => {
-            shallow(<AppShell apps={[]} />);
+            mount(
+                <AppShell
+                    apps={[
+                        {
+                            name: "test",
+                            id: "test",
+                            rootPath: "/test",
+                            render: (): JSX.Element => <h1>hello world</h1>,
+                            icon: <svg />,
+                        },
+                    ]}
+                />
+            );
         }).not.toThrow();
     });
 });
