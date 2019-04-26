c52d5f53edad80f63620793ff6b160b5fb5cc608
Brian Heston <brian@fractalmouse.com>
2019-04-26 13:03:24 -0700

    Updated elevation function to use the correct design system accessor structure

--- packages/fast-components-styles-msft/src/select/index.ts
+++ packages/fast-components-styles-msft/src/select/index.ts
@@ -1,11 +1,11 @@
-import { DesignSystem, withDesignSystemDefaults } from "../design-system";
-import { ComponentStyles, ComponentStyleSheet } from "@microsoft/fast-jss-manager";
+import { DesignSystem } from "../design-system";
+import { ComponentStyles } from "@microsoft/fast-jss-manager";
 import { format, toPx } from "@microsoft/fast-jss-utilities";
 import {
     ButtonClassNameContract,
     SelectClassNameContract,
 } from "@microsoft/fast-components-class-name-contracts-msft";
-import { elevation, ElevationMultiplier } from "../utilities/elevation";
+import { applyElevation, ElevationMultiplier } from "../utilities/elevation";
 import {
     neutralFillStealthRest,
     neutralForegroundRest,
@@ -28,50 +28,44 @@ export const selectDisplayButtonOverrides: ComponentStyles<
     },
 };
 
-const styles: ComponentStyles<SelectClassNameContract, DesignSystem> = (
-    config: DesignSystem
-): ComponentStyleSheet<SelectClassNameContract, DesignSystem> => {
-    const designSystem: DesignSystem = withDesignSystemDefaults(config);
+const styles: ComponentStyles<SelectClassNameContract, DesignSystem> = {
+    select: {
+        minWidth: "276px",
+        maxWidth: "374px",
+    },
 
-    return {
-        select: {
-            minWidth: "276px",
-            maxWidth: "374px",
-        },
+    select_toggleGlyph: {
+        fill: neutralForegroundRest,
+    },
 
-        select_toggleGlyph: {
-            fill: neutralForegroundRest,
-        },
+    select__disabled: {
+        opacity: ".3",
+    },
 
-        select__disabled: {
-            opacity: ".3",
-        },
+    select_menu: {
+        background: neutralFillStealthRest,
+        ...applyFloatingCornerRadius(),
+        ...applyElevation(ElevationMultiplier.e11),
+        zIndex: "1",
+        position: "absolute",
+        width: "100%",
+        margin: "0",
+        padding: format("{0} 0", toPx<DesignSystem>(designUnit)),
+        maxWidth: "374px",
+        minWidth: "276px",
+        maxHeight: "328px",
+        overflow: "auto",
+    },
 
-        select_menu: {
-            background: neutralFillStealthRest,
-            ...applyFloatingCornerRadius(),
-            ...elevation(ElevationMultiplier.e11)(designSystem),
-            zIndex: "1",
-            position: "absolute",
-            width: "100%",
-            margin: "0",
-            padding: format("{0} 0", toPx<DesignSystem>(designUnit)),
-            maxWidth: "374px",
-            minWidth: "276px",
-            maxHeight: "328px",
-            overflow: "auto",
+    select__multiSelectable: {
+        "& $select_menu": {
+            position: "static",
+            boxShadow: "none",
+            border: "1px solid",
+            borderColor: neutralOutlineRest,
         },
-
-        select__multiSelectable: {
-            "& $select_menu": {
-                position: "static",
-                boxShadow: "none",
-                border: "1px solid",
-                borderColor: neutralOutlineRest,
-            },
-        },
-        select_menu__open: {},
-    };
+    },
+    select_menu__open: {},
 };
 
 export default styles;
