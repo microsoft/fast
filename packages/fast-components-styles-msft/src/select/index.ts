a13dfb0a19df60fc1a02738beb769dde3ca01387
Stephane Comeau <scomea@microsoft.com>
2019-05-02 15:23:16 -0700

    styling tweaks

--- packages/fast-components-styles-msft/src/select/index.ts
+++ packages/fast-components-styles-msft/src/select/index.ts
@@ -1,10 +1,13 @@
-import { DesignSystem, withDesignSystemDefaults } from "../design-system";
-import { ComponentStyles, ComponentStyleSheet } from "@microsoft/fast-jss-manager";
+import DesignSystemDefaults, {
+    DesignSystem,
+    ensureDesignSystemDefaults,
+} from "../design-system";
+import { ComponentStyles } from "@microsoft/fast-jss-manager";
 import {
     applyLocalizedProperty,
-    Direction,
     ellipsis,
     format,
+    localizeSpacing,
     toPx,
 } from "@microsoft/fast-jss-utilities";
 import { SelectClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
@@ -19,76 +22,69 @@ import { applyFloatingCornerRadius } from "../utilities/border";
 import { designUnit } from "../utilities/design-system";
 import { inputFieldStyles } from "../patterns/input-field";
 
-const styles: ComponentStyles<SelectClassNameContract, DesignSystem> = (
-    config: DesignSystem
-): ComponentStyleSheet<SelectClassNameContract, DesignSystem> => {
-    const designSystem: DesignSystem = withDesignSystemDefaults(config);
-    const direction: Direction = designSystem.direction;
+const styles: ComponentStyles<SelectClassNameContract, DesignSystem> = {
+    select: {
+        minWidth: "276px",
+        maxWidth: "374px",
+    },
 
-    return {
-        select: {
-            minWidth: "276px",
-            maxWidth: "374px",
+    select_button: {
+        height: height(),
+        width: "100%",
+        ...inputFieldStyles(),
+    },
+
+    select_buttonContentRegion: {
+        display: "grid",
+        gridTemplateColumns: "1fr auto",
+        alignItems: "center",
+        justifyItems: "start",
+    },
+
+    select_buttonDisplayText: {
+        ...ellipsis(),
+        textAlign: ensureDesignSystemDefaults(
+            (designSystem: DesignSystem): string =>
+                applyLocalizedProperty("left", "right", designSystem.direction)
+        ),
+        width: "100%",
+    },
+
+    select_toggleGlyph: {
+        margin: (designSystem: DesignSystem): string => {
+            return localizeSpacing(
+                (designSystem && designSystem.direction) || DesignSystemDefaults.direction
+            )(format("0 0 0 {0}", horizontalSpacing())(designSystem));
         },
+        fill: neutralForegroundRest,
+        width: glyphSize,
+        height: glyphSize,
+        gridColumnStart: "2",
+    },
 
-        select_button: {
-            height: height(),
-            width: "100%",
-            ...inputFieldStyles(),
+    select_menu: {
+        background: neutralFillStealthRest,
+        ...applyFloatingCornerRadius(),
+        ...applyElevation(ElevationMultiplier.e11),
+        zIndex: "1",
+        position: "absolute",
+        width: "100%",
+        margin: "0",
+        padding: format("{0} 0", toPx<DesignSystem>(designUnit)),
+        maxWidth: "374px",
+        minWidth: "276px",
+        maxHeight: "328px",
+        overflow: "auto",
+    },
+
+    select__multiSelectable: {
+        "& $select_menu": {
+            position: "static",
+            boxShadow: "none",
+            border: "1px solid",
+            borderColor: neutralOutlineRest,
         },
-
-        select_buttonContentRegion: {
-            display: "grid",
-            gridTemplateColumns: "1fr auto",
-            alignItems: "center",
-            justifyItems: "start",
-        },
-
-        select_buttonDisplayText: {
-            ...ellipsis(),
-            textAlign: applyLocalizedProperty("left", "right", direction),
-            width: "100%",
-        },
-
-        select_toggleGlyph: {
-            [applyLocalizedProperty(
-                "margin-left",
-                "margin-right",
-                direction
-            )]: horizontalSpacing(),
-            fill: neutralForegroundRest,
-            width: glyphSize,
-            height: glyphSize,
-            gridColumnStart: "2",
-        },
-
-        select__disabled: {},
-
-        select_menu: {
-            background: neutralFillStealthRest,
-            ...applyFloatingCornerRadius(),
-            ...applyElevation(ElevationMultiplier.e11),
-            zIndex: "1",
-            position: "absolute",
-            width: "100%",
-            margin: "0",
-            padding: format("{0} 0", toPx<DesignSystem>(designUnit)),
-            maxWidth: "374px",
-            minWidth: "276px",
-            maxHeight: "328px",
-            overflow: "auto",
-        },
-
-        select__multiSelectable: {
-            "& $select_menu": {
-                position: "static",
-                boxShadow: "none",
-                border: "1px solid",
-                borderColor: neutralOutlineRest,
-            },
-        },
-        select_menu__open: {},
-    };
+    },
 };
 
 export default styles;
