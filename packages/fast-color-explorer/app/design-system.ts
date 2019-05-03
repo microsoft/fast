c0303213be5a8996ec14a41dc5e436d2b6f09b46
nicholasrice <nicholas.rice119@gmail.com>
2019-05-02 22:00:37 -0700

    re-use values

--- packages/fast-color-explorer/app/design-system.ts
+++ packages/fast-color-explorer/app/design-system.ts
@@ -8,14 +8,7 @@ import {
     neutralPaletteConfig,
 } from "@microsoft/fast-components-styles-msft";
 import { ColorRGBA64, parseColorHexRGB } from "@microsoft/fast-colors";
-
-export const accentColors: string[] = [
-    "#0078d4",
-    "#107c10",
-    "#5c2d91",
-    "#d83b01",
-    "#f2c812",
-];
+import { AccentColors } from "./control-pane";
 
 /* tslint:disable-next-line */
 export interface ColorsDesignSystem extends DesignSystem {}
@@ -24,6 +17,8 @@ export const colorsDesignSystem: ColorsDesignSystem = Object.assign(
     DesignSystemDefaults,
     {
         neutralPalette: createColorPalette(new ColorRGBA64(0.5, 0.5, 0.5, 1)),
-        accentPalette: createColorPalette(parseColorHexRGB("#0078D4") as ColorRGBA64),
+        accentPalette: createColorPalette(parseColorHexRGB(
+            AccentColors.blue
+        ) as ColorRGBA64),
     }
 );
