a95325f301ac2757b249321aad48d5060be54e91
Nick Rice <nicholas.rice119@gmail.com>
2019-06-10 14:06:07 -0700

    wire in capability to disable design system addon

--- packages/fast-storybook-presets/README.md
+++ packages/fast-storybook-presets/README.md
@@ -12,3 +12,20 @@ module.exports = ["@microsoft/fast-storybook-presets"];
 ```
 
 See [preset documentation](https://storybook.js.org/docs/presets/introduction/) for more details on adding [Storybook](https://storybook.js.org) presets.
+
+## Options
+The `@microsoft/fast-storybook-presets` can be configured with options. To configure the presets, adjust the preset include to resemble the following:
+```js
+module.exports = [{
+    name: "@microsoft/fast-storybook-presets",
+    options: { /* add options here */ }
+}]
+
+```
+
+### `designSystemAddon` - Configure the design system addon
+```js
+{
+    enabled: true, // Includes the @microsoft/fast-storybook-design-system-addon
+}
+```
--- packages/fast-storybook-presets/src/index.ts
+++ packages/fast-storybook-presets/src/index.ts
@@ -1,11 +1,24 @@
 /**
  * Options object to configure the FAST DNA presets
  */
-interface FASTDNAPresetOptions {}
+interface FASTDNAPresetOptions {
+    designSystemAddon: {
+        enabled: boolean;
+    };
+}
 
-export function addons(entry = []): string[] {
-    return [
-        ...entry,
-        require.resolve("@microsoft/fast-storybook-design-system-addon/register"),
-    ];
+const optionDefaults: FASTDNAPresetOptions = {
+    designSystemAddon: {
+        enabled: true,
+    },
+};
+
+export function addons(entry = [], options: FASTDNAPresetOptions): string[] {
+    const withDefaults: FASTDNAPresetOptions = { ...optionDefaults, ...options };
+
+    return withDefaults.designSystemAddon.enabled
+        ? entry.concat([
+              require.resolve("@microsoft/fast-storybook-design-system-addon/register"),
+          ])
+        : entry;
 }
