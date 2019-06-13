1faffe3b43da3b9866c652a573bd185323b486ac
Jeff Smith <jes@microsoft.com>
2019-06-13 13:58:41 -0700

    refined convert script and deleted json files

--- build/convert-schemas.js
+++ build/convert-schemas.js
@@ -10,25 +10,47 @@ const rootDir = path.resolve(process.cwd());
 const srcSchemaPaths = "src/**/*.schema.ts";
 const destDir = "dist";
 
+let exit = 0;
+
 /**
- * Function to copy schema files to their dist folder
+ * Function to convert and copy schema files to their dist folder
  */
 function copySchemaFiles() {
     const resolvedSrcSchemaPaths = path.resolve(rootDir, srcSchemaPaths);
 
-    glob(resolvedSrcSchemaPaths, void 0, function(error, files) {
-        files.forEach(filePath => {
-            console.log("Prior to conversion", filePath);
-            filePath = filePath.substr(0, filePath.lastIndexOf(".ts")) + ".json";
-            console.log("post conversion", filePath);
-            const destSchemaPath = filePath.replace(/(\bsrc\b)(?!.*\1)/, destDir);
-            fs.copyFileSync(filePath, destSchemaPath);
-            console.log(destSchemaPath);
-        });
+    glob.sync(resolvedSrcSchemaPaths, void 0).forEach(function(filePath) {
+        let destSchemaPath = filePath.replace(/(\bsrc\b)(?!.*\1)/, destDir);
+        destSchemaPath =
+            destSchemaPath.substr(0, destSchemaPath.lastIndexOf(".ts")) + ".json";
+        fs.copyFileSync(filePath, destSchemaPath);
+        const fileContents = fs.readFileSync(destSchemaPath).toString();
+        const commonJS = "module.exports = ".concat(
+            fileContents.substring(fileContents.indexOf("{"))
+        );
+        const tempPath = path.resolve(
+            path.dirname(filePath),
+            `.tmp/${path.basename(filePath)}.js`
+        );
+
+        try {
+            fs.writeFileSync(tempPath, commonJS);
+            const schemaData = require(tempPath);
+            const json = JSON.stringify(schemaData, null, 2);
+            fs.writeFileSync(destSchemaPath, json);
+        } catch (e) {
+            console.error(e);
+            exit = 1;
+        } finally {
+            if (fs.existsSync(tempPath)) {
+                fs.unlinkSync(tempPath);
+            }
+        }
     });
+
+    process.exit(exit);
 }
 
 /**
- * Copy all files
+ * Convert and Copy all files
  */
 copySchemaFiles();
