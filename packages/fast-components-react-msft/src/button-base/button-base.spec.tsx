071d44af1332f91ba832d9f4fd86f431b570b9f7
Chris Holt <chhol@microsoft.com>
2019-06-19 13:37:26 -0700

    feat: add classname to MSFT button for when either before or after content and children exist

--- packages/fast-components-react-msft/src/button-base/button-base.spec.tsx
+++ packages/fast-components-react-msft/src/button-base/button-base.spec.tsx
@@ -3,6 +3,7 @@ import Adapter from "enzyme-adapter-react-16";
 import { configure, mount, shallow } from "enzyme";
 import ButtonBase from "./button-base";
 import {
+    ButtonBaseClassNameContract,
     ButtonBaseHandledProps,
     ButtonBaseProps,
     ButtonBaseUnhandledProps,
@@ -16,6 +17,12 @@ configure({ adapter: new Adapter() });
 
 describe("button", (): void => {
     const href: string = "https://www.microsoft.com";
+    const managedClasases: ButtonBaseClassNameContract = {
+        button: "button",
+        button_beforeContent: "button_beforeContent",
+        button_afterContent: "button_afterContent",
+        button__hasBeforeOrAfterAndChildren: "button__hasBeforeOrAfterAndChildren",
+    };
 
     test("should have a displayName that matches the component name", () => {
         expect(`${DisplayNamePrefix}${(ButtonBase as any).name}`).toBe(
@@ -110,5 +117,107 @@ describe("button", (): void => {
                 .childAt(1)
                 .type()
         ).toEqual("svg");
+    });
+
+    test("should add a `hasBeforeOrAfterAndChildren` className when `beforeContent` prop and children exist", () => {
+        const props: ButtonBaseHandledProps = {
+            children: "Foo",
+            managedClasses: managedClasases,
+            beforeContent: (classname?: string): React.ReactNode => {
+                return (
+                    <svg
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={classname}
+                    >
+                        <path d="M10.3906 9.39844C11.099 9.64323" fill="black" />
+                    </svg>
+                );
+            },
+        };
+
+        const rendered: any = mount(<ButtonBase {...props} />);
+
+        expect(rendered.find("button").prop("className")).toContain(
+            managedClasases.button__hasBeforeOrAfterAndChildren
+        );
+    });
+
+    test("should add a `hasBeforeOrAfterAndChildren` className when `afterContent` prop and children exist", () => {
+        const props: ButtonBaseHandledProps = {
+            children: "Foo",
+            managedClasses: managedClasases,
+            afterContent: (classname?: string): React.ReactNode => {
+                return (
+                    <svg
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={classname}
+                    >
+                        <path d="M10.3906 9.39844C11.099 9.64323" fill="black" />
+                    </svg>
+                );
+            },
+        };
+
+        const rendered: any = mount(<ButtonBase {...props} />);
+
+        expect(rendered.find("button").prop("className")).toContain(
+            managedClasases.button__hasBeforeOrAfterAndChildren
+        );
+    });
+
+    test("should NOT add a `hasBeforeOrAfterAndChildren` className when `beforeContent` prop is passed and no children exist", () => {
+        const props: ButtonBaseHandledProps = {
+            managedClasses: managedClasases,
+            beforeContent: (classname?: string): React.ReactNode => {
+                return (
+                    <svg
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={classname}
+                    >
+                        <path d="M10.3906 9.39844C11.099 9.64323" fill="black" />
+                    </svg>
+                );
+            },
+        };
+
+        const rendered: any = mount(<ButtonBase {...props} />);
+
+        expect(rendered.find("button").prop("className")).not.toContain(
+            managedClasases.button__hasBeforeOrAfterAndChildren
+        );
+    });
+
+    test("should NOT add a `hasBeforeOrAfterAndChildren` className when `afterContent` prop is passed and no children exist", () => {
+        const props: ButtonBaseHandledProps = {
+            managedClasses: managedClasases,
+            afterContent: (classname?: string): React.ReactNode => {
+                return (
+                    <svg
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={classname}
+                    >
+                        <path d="M10.3906 9.39844C11.099 9.64323" fill="black" />
+                    </svg>
+                );
+            },
+        };
+
+        const rendered: any = mount(<ButtonBase {...props} />);
+
+        expect(rendered.find("button").prop("className")).not.toContain(
+            managedClasases.button__hasBeforeOrAfterAndChildren
+        );
     });
 });
