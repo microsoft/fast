d2bc7a83a92e571e4ade7ed212f0e22161139d6e
Nicholas Rice <nicholas.rice119@gmail.com>
2019-06-19 10:44:35 -0700

    fix: correcting typography schema export name (#1873)

--- packages/fast-components-react-msft/src/typography/index.ts
+++ packages/fast-components-react-msft/src/typography/index.ts
@@ -10,7 +10,7 @@ import {
     TypographyTag,
     TypographyUnhandledProps,
 } from "@microsoft/fast-components-react-base";
-import typograophySchema from "./typography.schema";
+import typographySchema from "./typography.schema";
 import manageJss, { ManagedJSSProps } from "@microsoft/fast-jss-manager-react";
 import { DesignSystem, TypographyStyles } from "@microsoft/fast-components-styles-msft";
 import { Subtract } from "utility-types";
@@ -37,7 +37,11 @@ export {
     TypographyUnhandledProps,
     Typography,
     TypographyProps,
-    typograophySchema,
+    typographySchema,
+    /**
+     * @deprecated
+     */
+    typographySchema as typograophySchema,
     TypographySize,
     TypographyTag,
 };
