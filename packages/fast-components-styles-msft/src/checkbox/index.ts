37d9dd75cb0128838c4eba8a9905cfd9436c8ac2
Brian Heston <brian@fractalmouse.com>
2019-06-19 14:28:45 -0700

    Clean up imports

--- packages/fast-components-styles-msft/src/checkbox/index.ts
+++ packages/fast-components-styles-msft/src/checkbox/index.ts
@@ -1,15 +1,9 @@
-import {
-    DesignSystem,
-    DesignSystemResolver,
-    withDesignSystemDefaults,
-} from "../design-system";
-import { ComponentStyles, ComponentStyleSheet } from "@microsoft/fast-jss-manager";
+import { DesignSystem, DesignSystemResolver } from "../design-system";
+import { ComponentStyles } from "@microsoft/fast-jss-manager";
 import { CheckboxClassNameContract } from "@microsoft/fast-components-class-name-contracts-base";
 import {
     add,
     applyFocusVisible,
-    applyLocalizedProperty,
-    Direction,
     directionSwitch,
     divide,
     format,
@@ -34,7 +28,7 @@ import {
 import { applyDisabledState } from "../utilities/disabled";
 import { applyScaledTypeRamp } from "../utilities/typography";
 import { designUnit, outlineWidth } from "../utilities/design-system";
-import { applyCursorPointer, applyCursorDefault } from "../utilities/cursor";
+import { applyCursorDefault, applyCursorPointer } from "../utilities/cursor";
 
 const inputSize: DesignSystemResolver<string> = toPx(
     add(divide(heightNumber(), 2), designUnit)
