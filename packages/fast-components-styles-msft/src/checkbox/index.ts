e09e992d447c3248f17e905d8ab5aab9bbe6bac4
Brian Heston <brian@fractalmouse.com>
2019-06-19 12:18:05 -0700

    Set cursor to pointer on hover

--- packages/fast-components-styles-msft/src/checkbox/index.ts
+++ packages/fast-components-styles-msft/src/checkbox/index.ts
@@ -34,6 +34,7 @@ import {
 import { applyDisabledState } from "../utilities/disabled";
 import { applyScaledTypeRamp } from "../utilities/typography";
 import { designUnit, outlineWidth } from "../utilities/design-system";
+import { applyCursorPointer, applyCursorDefault } from "../utilities/cursor";
 
 const inputSize: DesignSystemResolver<string> = toPx(
     add(divide(heightNumber(), 2), designUnit)
@@ -77,11 +78,14 @@ const styles: ComponentStyles<CheckboxClassNameContract, DesignSystem> = {
             toPx<DesignSystem>(outlineWidth),
             neutralOutlineRest
         ),
+        "&:enabled": {
+            ...applyCursorPointer(),
+        },
         "&:hover:enabled": {
             background: neutralFillInputHover,
             borderColor: neutralOutlineHover,
         },
-        "&:active": {
+        "&:active:enabled": {
             background: neutralFillInputActive,
             borderColor: neutralOutlineActive,
         },
@@ -109,6 +113,7 @@ const styles: ComponentStyles<CheckboxClassNameContract, DesignSystem> = {
         },
     },
     checkbox_label: {
+        ...applyCursorPointer(),
         color: neutralForegroundRest,
         ...applyScaledTypeRamp("t7"),
     },
@@ -142,6 +147,9 @@ const styles: ComponentStyles<CheckboxClassNameContract, DesignSystem> = {
     },
     checkbox__disabled: {
         ...applyDisabledState(),
+        "& $checkbox_label": {
+            ...applyCursorDefault(),
+        },
     },
 };
 
