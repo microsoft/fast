dfdcc32cba8c194d986604a874b01e65c15579e5
Nick Rice <nicholas.rice119@gmail.com>
2019-07-02 15:37:59 -0700

    converting card

---
+++ packages/fast-components-react-msft/src/card/card.stories.tsx
@@ -0,0 +1,20 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import { Card } from "./";
+import { Image } from "../image";
+import { Heading, HeadingSize, HeadingTag } from "../heading";
+
+storiesOf("Card", module)
+    .add("With image", () => (
+        <Card>
+            <Image src="https://placehold.it/300x300/414141" alt="Placeholder image" />
+        </Card>
+    ))
+    .add("With heading and image", () => (
+        <Card>
+            <Heading tag={HeadingTag.h3} size={HeadingSize._4}>
+                Heading
+            </Heading>
+            <Image src="https://placehold.it/300x300/414141" alt="Placeholder image" />
+        </Card>
+    ));
