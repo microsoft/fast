5e78e8bcc130eaef9caf52244476e85eaba16c84
Nick Rice <nicholas.rice119@gmail.com>
2019-07-02 13:54:09 -0700

    converting action trigger

---
+++ packages/fast-components-react-msft/src/action-trigger/action-trigger.stories.tsx
@@ -0,0 +1,66 @@
+import { storiesOf } from "@storybook/react";
+import React, { useState } from "react";
+import { ActionTrigger, ActionTriggerAppearance } from "./";
+import { glyphFactory, SVGGlyph } from "../../assets/svg-element";
+import { action } from "@storybook/addon-actions";
+
+storiesOf("Action trigger", module)
+    .add("Default", () => (
+        <ActionTrigger
+            glyph={glyphFactory(SVGGlyph.download)}
+            onClick={action("onClick")}
+        >
+            Download
+        </ActionTrigger>
+    ))
+    .add("Icon only", () => (
+        <ActionTrigger
+            glyph={glyphFactory(SVGGlyph.download)}
+            onClick={action("onClick")}
+        />
+    ))
+    .add("Justified", () => (
+        <ActionTrigger
+            glyph={glyphFactory(SVGGlyph.download)}
+            appearance={ActionTriggerAppearance.justified}
+            onClick={action("onClick")}
+        >
+            Download
+        </ActionTrigger>
+    ))
+    .add("Lightweight", () => (
+        <ActionTrigger
+            glyph={glyphFactory(SVGGlyph.download)}
+            appearance={ActionTriggerAppearance.lightweight}
+            onClick={action("onClick")}
+        >
+            Download
+        </ActionTrigger>
+    ))
+    .add("Outline", () => (
+        <ActionTrigger
+            glyph={glyphFactory(SVGGlyph.download)}
+            appearance={ActionTriggerAppearance.outline}
+            onClick={action("onClick")}
+        >
+            Download
+        </ActionTrigger>
+    ))
+    .add("Primary", () => (
+        <ActionTrigger
+            glyph={glyphFactory(SVGGlyph.download)}
+            appearance={ActionTriggerAppearance.primary}
+            onClick={action("onClick")}
+        >
+            Download
+        </ActionTrigger>
+    ))
+    .add("Stealth", () => (
+        <ActionTrigger
+            glyph={glyphFactory(SVGGlyph.download)}
+            appearance={ActionTriggerAppearance.stealth}
+            onClick={action("onClick")}
+        >
+            Download
+        </ActionTrigger>
+    ));
