48577a53bc5fa9854bf53ba316dfbc58fd3e14c1
nicholasrice <nicholas.rice119@gmail.com>
2019-07-03 13:54:48 -0700

    migrate outline-button

---
+++ packages/fast-components-react-msft/src/outline-button/outline-button.stories.tsx
@@ -0,0 +1,43 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import { OutlineButton } from "./";
+import { glyphFactory, SVGGlyph } from "../../assets/svg-element";
+
+storiesOf("Outline button", module)
+    .add("Default", () => <OutlineButton>Outline button</OutlineButton>)
+    .add("Anchor", () => <OutlineButton href="#">Anchor button</OutlineButton>)
+    .add("Before content", () => (
+        <OutlineButton beforeContent={glyphFactory(SVGGlyph.download)}>
+            With before content
+        </OutlineButton>
+    ))
+    .add("After content", () => (
+        <OutlineButton afterContent={glyphFactory(SVGGlyph.robot)}>
+            With after content
+        </OutlineButton>
+    ))
+    .add("Only before content", () => (
+        <OutlineButton beforeContent={glyphFactory(SVGGlyph.robot)} />
+    ))
+    .add("Only after content", () => (
+        <OutlineButton afterContent={glyphFactory(SVGGlyph.robot)} />
+    ))
+    .add("Disabled", () => (
+        <OutlineButton
+            disabled={true}
+            beforeContent={glyphFactory(SVGGlyph.download)}
+            afterContent={glyphFactory(SVGGlyph.user)}
+        >
+            Disabled outline button
+        </OutlineButton>
+    ))
+    .add("Disabled anchor", () => (
+        <OutlineButton
+            href={"#"}
+            disabled={true}
+            beforeContent={glyphFactory(SVGGlyph.download)}
+            afterContent={glyphFactory(SVGGlyph.user)}
+        >
+            Disabled outline anchor button
+        </OutlineButton>
+    ));
