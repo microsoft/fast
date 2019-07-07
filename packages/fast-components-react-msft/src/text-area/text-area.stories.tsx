70f6a5d48c5036ceb4c6e2fff0f83d6222ddf335
nicholasrice <nicholas.rice119@gmail.com>
2019-07-07 16:43:11 -0700

    migrate text area

---
+++ packages/fast-components-react-msft/src/text-area/text-area.stories.tsx
@@ -0,0 +1,44 @@
+import { storiesOf } from "@storybook/react";
+import React, { useState } from "react";
+import { TextArea } from "./";
+import { uniqueId } from "lodash-es";
+import { Label } from "../label";
+import { action } from "@storybook/addon-actions";
+
+/**
+ * Simple state manager to track and update checked properties
+ */
+function TextAreaStateHandler(props: {
+    children: (
+        value: string,
+        onChange: React.ChangeEventHandler<HTMLTextAreaElement>
+    ) => JSX.Element;
+}): JSX.Element {
+    const [checked, setChecked]: [
+        string,
+        React.Dispatch<React.SetStateAction<string>>
+    ] = useState("");
+
+    function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>): void {
+        setChecked(e.target.value);
+        action("onChange")(e);
+    }
+
+    return props.children(checked, handleChange);
+}
+
+storiesOf("Text area", module)
+    .add("Unhandled", () => <TextArea onChange={action("onChange")} />)
+    .add("Handled", () => {
+        function render(
+            value: string,
+            onChange: React.ChangeEventHandler<HTMLTextAreaElement>
+        ): JSX.Element {
+            return <TextArea value={value} onChange={onChange} />;
+        }
+        return <TextAreaStateHandler>{render}</TextAreaStateHandler>;
+    })
+    .add("Placeholder", () => (
+        <TextArea placeholder={"Placeholder"} onChange={action("onChange")} />
+    ))
+    .add("Disabled", () => <TextArea onChange={action("onChange")} disabled={true} />);
