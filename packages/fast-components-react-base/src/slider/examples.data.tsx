923bc900c4319d82faaec1a0bca1bbf877944b90
nicholasrice <nicholas.rice119@gmail.com>
2019-07-07 18:40:52 -0700

    fixing build

--- packages/fast-components-react-base/src/slider/examples.data.tsx
+++ packages/fast-components-react-base/src/slider/examples.data.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-import Slider, {
-    SliderHandledProps,
-    SliderManagedClasses,
-    SliderUnhandledProps,
-} from "./slider";
-import { sliderSchema } from "../index";
-import Documentation from "./.tmp/documentation";
-import { ComponentFactoryExample } from "@microsoft/fast-development-site-react";
-
-const managedClasses: SliderManagedClasses = {
-    managedClasses: {
-        slider: "slider",
-        slider__horizontal: "slider__horizontal",
-        slider__vertical: "slider__vertical",
-        slider_layoutRegion: "slider_layoutRegion",
-        slider_thumb: "slider__thumb",
-        slider_thumb__upperValue: "slider_thumb__upperValue",
-        slider_thumb__lowerValue: "slider_thumb__lowerValue",
-        slider_backgroundTrack: "slider_backgroundTrack",
-        slider_foregroundTrack: "slider_foregroundTrack",
-        slider__disabled: "slider__disabled",
-        slider__modeSingle: "slider__modeSingle",
-        slider__modeAdjustLower: "slider__modeAdjustLower",
-        slider__modeAdjustUpper: "slider__modeAdjustUpper",
-        slider__modeAdjustBoth: "slider__modeAdjustBoth",
-        slider__rtl: "slider__rtl",
-    },
-};
-
-const examples: ComponentFactoryExample<SliderHandledProps> = {
-    name: "Slider",
-    component: Slider,
-    schema: sliderSchema as any,
-    documentation: <Documentation />,
-    detailData: {
-        ...managedClasses,
-    },
-    data: [
-        {
-            ...managedClasses,
-        },
-    ],
-};
-
-export default examples;
+// import React from "react";
+// import Slider, {
+//     SliderHandledProps,
+//     SliderManagedClasses,
+//     SliderUnhandledProps,
+// } from "./slider";
+// import { sliderSchema } from "../index";
+// import Documentation from "./.tmp/documentation";
+// import { ComponentFactoryExample } from "@microsoft/fast-development-site-react";
+//
+// const managedClasses: SliderManagedClasses = {
+//     managedClasses: {
+//         slider: "slider",
+//         slider__horizontal: "slider__horizontal",
+//         slider__vertical: "slider__vertical",
+//         slider_layoutRegion: "slider_layoutRegion",
+//         slider_thumb: "slider__thumb",
+//         slider_thumb__upperValue: "slider_thumb__upperValue",
+//         slider_thumb__lowerValue: "slider_thumb__lowerValue",
+//         slider_backgroundTrack: "slider_backgroundTrack",
+//         slider_foregroundTrack: "slider_foregroundTrack",
+//         slider__disabled: "slider__disabled",
+//         slider__modeSingle: "slider__modeSingle",
+//         slider__modeAdjustLower: "slider__modeAdjustLower",
+//         slider__modeAdjustUpper: "slider__modeAdjustUpper",
+//         slider__modeAdjustBoth: "slider__modeAdjustBoth",
+//         slider__rtl: "slider__rtl",
+//     },
+// };
+//
+// const examples: ComponentFactoryExample<SliderHandledProps> = {
+//     name: "Slider",
+//     component: Slider,
+//     schema: sliderSchema as any,
+//     documentation: <Documentation />,
+//     detailData: {
+//         ...managedClasses,
+//     },
+//     data: [
+//         {
+//             ...managedClasses,
+//         },
+//     ],
+// };
+//
+// export default examples;
--- packages/fast-components-react-base/src/viewport-positioner/examples.data.tsx
+++ packages/fast-components-react-base/src/viewport-positioner/examples.data.tsx
@@ -1,39 +1,39 @@
-import React from "react";
-import { ComponentFactoryExample } from "@microsoft/fast-development-site-react";
-import { viewportPositionerSchema } from "../index";
-import ViewportPositioner, {
-    ViewportPositionerHandledProps,
-    ViewportPositionerManagedClasses,
-} from "./viewport-positioner";
-import Documentation from "./.tmp/documentation";
-
-const managedClasses: ViewportPositionerManagedClasses = {
-    managedClasses: {
-        viewportPositioner: "viewportPositioner",
-        viewportPositioner__left: "viewportPositioner__left",
-        viewportPositioner__right: "viewportPositioner__right",
-        viewportPositioner__top: "viewportPositioner__top",
-        viewportPositioner__bottom: "viewportPositioner__bottom",
-        viewportPositioner__horizontalInset: "viewportPositioner__horizontalInset",
-        viewportPositioner__verticalInset: "viewportPositioner__verticalInset",
-    },
-};
-
-const examples: ComponentFactoryExample<ViewportPositionerHandledProps> = {
-    name: "Viewport positioner",
-    component: ViewportPositioner,
-    schema: viewportPositionerSchema as any,
-    documentation: <Documentation />,
-    detailData: {
-        ...managedClasses,
-        children: "Viewport positioner",
-    },
-    data: [
-        {
-            ...managedClasses,
-            children: "Viewport positioner",
-        },
-    ],
-};
-
-export default examples;
+// import React from "react";
+// import { ComponentFactoryExample } from "@microsoft/fast-development-site-react";
+// import { viewportPositionerSchema } from "../index";
+// import ViewportPositioner, {
+//     ViewportPositionerHandledProps,
+//     ViewportPositionerManagedClasses,
+// } from "./viewport-positioner";
+// import Documentation from "./.tmp/documentation";
+//
+// const managedClasses: ViewportPositionerManagedClasses = {
+//     managedClasses: {
+//         viewportPositioner: "viewportPositioner",
+//         viewportPositioner__left: "viewportPositioner__left",
+//         viewportPositioner__right: "viewportPositioner__right",
+//         viewportPositioner__top: "viewportPositioner__top",
+//         viewportPositioner__bottom: "viewportPositioner__bottom",
+//         viewportPositioner__horizontalInset: "viewportPositioner__horizontalInset",
+//         viewportPositioner__verticalInset: "viewportPositioner__verticalInset",
+//     },
+// };
+//
+// const examples: ComponentFactoryExample<ViewportPositionerHandledProps> = {
+//     name: "Viewport positioner",
+//     component: ViewportPositioner,
+//     schema: viewportPositionerSchema as any,
+//     documentation: <Documentation />,
+//     detailData: {
+//         ...managedClasses,
+//         children: "Viewport positioner",
+//     },
+//     data: [
+//         {
+//             ...managedClasses,
+//             children: "Viewport positioner",
+//         },
+//     ],
+// };
+//
+// export default examples;
