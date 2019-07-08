edc3eb2725ea42993735b6c6c943cbb7621bc952
nicholasrice <nicholas.rice119@gmail.com>
2019-07-07 17:44:54 -0700

    remove old app

--- packages/fast-components-react-base/app/components/examples.data.tsx
+++
@@ -1,8 +0,0 @@
-import reactHTMLElementSchema from "./react-html-element.schema";
-import ReactHTMLElement from "./react-html-element";
-
-export default {
-    name: "HTMLElement",
-    schema: reactHTMLElementSchema,
-    component: ReactHTMLElement,
-};
