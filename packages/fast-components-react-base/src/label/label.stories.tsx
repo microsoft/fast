edcf7f9dcea458c8e970ed813c1385418120c86a
Chris Holt <chhol@microsoft.com>
2019-07-08 11:43:28 -0700

    migrate label

---
+++ packages/fast-components-react-base/src/label/label.stories.tsx
@@ -0,0 +1,22 @@
+import { storiesOf } from "@storybook/react";
+import React, { useState } from "react";
+import Label, { LabelTag } from "./";
+import { uniqueId } from "lodash-es";
+
+storiesOf("Label", module)
+    .add("Label", () => <Label>Label</Label>)
+    .add("Label with label tag", () => <Label tag={LabelTag.label}>Label</Label>)
+    .add("Label with legend tag", () => <Label tag={LabelTag.legend}>Legend</Label>)
+    .add("Hidden label", () => (
+        <Label tag={LabelTag.legend} hidden={true}>
+            Hidden label
+        </Label>
+    ))
+    .add("Label with htmlFor attribute", () => (
+        <React.Fragment>
+            <Label tag={LabelTag.legend} htmlFor={"id01"}>
+                Label with htmlFor
+            </Label>
+            <input id={"id01"} type={"text"} />
+        </React.Fragment>
+    ));
