f75fb5d79dca4a9146bc73de272b7a34b5e122a4
Stephane Comeau <scomea@microsoft.com>
2019-07-26 19:26:58 -0700

    feat: add enter key behavior to CSS prop editor (#2012)

--- packages/fast-tooling-react/src/css-property-editor/property-editor-row.props.ts
+++ packages/fast-tooling-react/src/css-property-editor/property-editor-row.props.ts
@@ -56,6 +56,15 @@ export interface CSSPropertyEditorRowHandledProps
      *  Called when row loses focus
      */
     onRowBlur: (rowKey: string, rowIndex: number) => void;
+
+    /**
+     *  Enter key press on value edit input
+     */
+    onValueInputKeyDown: (
+        rowKey: string,
+        rowIndex: number,
+        event: React.KeyboardEvent
+    ) => void;
 }
 
 export type CSSPropertyEditorRowProps = CSSPropertyEditorRowHandledProps &
