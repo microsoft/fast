e750b0a761e283eb02949d1ff1e1157dfb60d629
Jeff Smith <jes@microsoft.com>
2019-07-29 14:32:23 -0700

    added tests

--- packages/fast-tooling-react/src/css-editor/border/border.spec.tsx
+++ packages/fast-tooling-react/src/css-editor/border/border.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Adapter from "enzyme-adapter-react-16";
 import { configure, mount, shallow } from "enzyme";
+import { BorderStyleValue } from "./border.props"
 import CSSBorder from "./border";
 import { CSSBorderClassNameContract } from "./border.style";
 
@@ -41,5 +42,85 @@ describe("CSSBorder", () => {
         expect(rendered.find(`.${managedClasses.cssBorder_input}`).prop("value")).toBe(
             borderWidth
         );
+    });
+    test("should call onChange when state updates", () => {
+        const borderWidth: string = "3px";
+        const callback: any = jest.fn();
+        const rendered: any = mount(
+            <CSSBorder
+                data={{ borderWidth }}
+                managedClasses={managedClasses}
+                onChange={callback}
+            />
+        );
+
+        expect(callback).toHaveBeenCalledTimes(0);
+
+        rendered.setState({ borderWidth: "10px" });
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    test("should set borderColor state in onChange event", () => {
+        const borderColor: string = "#000";
+        const newBorderColor: string = "#FFF";
+        const callback: any = jest.fn();
+        const rendered: any = mount(
+            <CSSBorder
+                data={{ borderColor }}
+                managedClasses={managedClasses}
+                onChange={callback}
+            />
+        );
+
+        expect(rendered.state("borderColor")).toBe(borderColor);
+
+        rendered
+            .find("input").at(0)
+            .simulate("change", { target: { value: newBorderColor } });
+
+        expect(rendered.state("borderColor")).toBe(newBorderColor);
+    });
+
+    test("should set borderStyle state in onChange event", () => {
+        const borderStyle: BorderStyleValue = BorderStyleValue.solid;
+        const newBorderStyle: BorderStyleValue = BorderStyleValue.dotted;
+        const callback: any = jest.fn();
+        const rendered: any = mount(
+            <CSSBorder
+                data={{ borderStyle }}
+                managedClasses={managedClasses}
+                onChange={callback}
+            />
+        );
+
+        expect(rendered.state("borderStyle")).toBe(borderStyle);
+
+        rendered
+            .find(`.${managedClasses.cssBorder_select}`)
+            .simulate("change", { target: { value: newBorderStyle } });
+
+        expect(rendered.state("borderStyle")).toBe(newBorderStyle);
+    });
+
+    test("should set borderWidth state in onChange event", () => {
+        const borderWidth: string = "1px";
+        const newBorderWidth: string = "10px";
+        const callback: any = jest.fn();
+        const rendered: any = mount(
+            <CSSBorder
+                data={{ borderWidth }}
+                managedClasses={managedClasses}
+                onChange={callback}
+            />
+        );
+
+        expect(rendered.state("borderWidth")).toBe(borderWidth);
+
+        rendered
+            .find(`.${managedClasses.cssBorder_input}`)
+            .simulate("change", { target: { value: newBorderWidth } });
+
+        expect(rendered.state("borderWidth")).toBe(newBorderWidth);
     });
 });
