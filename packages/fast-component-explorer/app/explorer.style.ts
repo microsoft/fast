a0ebea91b3349d20f87d1b0dc52371104ddf4882
Nick Rice <nicholas.rice119@gmail.com>
2019-07-31 12:53:14 -0700

    implement variable fonts

--- packages/fast-component-explorer/app/explorer.style.ts
+++ packages/fast-component-explorer/app/explorer.style.ts
@@ -26,14 +26,23 @@ export interface ExplorerClassNameContract {
 }
 
 const style: ComponentStyles<ExplorerClassNameContract, DesignSystem> = {
-    "@font-face": {
-        fontFamily: "Segoe UI",
-        src:
-            "url('//c.s-microsoft.com/static/fonts/segoe-ui/west-european/Normal/latest.woff2') format('woff2')",
-    },
+    "@font-face": [
+        {
+            fontFamily: "SegoeUIVF",
+            src:
+                "url(https://res.cloudinary.com/fast-dna/raw/upload/v1558051831/SegoeUI-Roman-VF_web.ttf) format('truetype')",
+            fontWeight: "1 1000",
+        },
+        {
+            fontFamily: "Segoe UI",
+            src:
+                "url('//c.s-microsoft.com/static/fonts/segoe-ui/west-european/Normal/latest.woff2') format('woff2')",
+        },
+    ] as any,
     "@global": {
         "body, html": {
-            fontFamily: "Segoe UI, SegoeUI, Helvetica Neue, Helvetica, Arial, sans-serif",
+            fontFamily:
+                "SegoeUIVF, Segoe UI, SegoeUI, Helvetica Neue, Helvetica, Arial, sans-serif",
             fontSize: "12px",
             padding: "0",
             margin: "0",
