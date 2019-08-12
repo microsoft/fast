04dc6cd00b1e6c6df53448769cffca21c1b908eb
Nick Rice <nicholas.rice119@gmail.com>
2019-08-12 10:19:31 -0700

    implement reduce

--- packages/fast-web-utilities/src/class-names.ts
+++ packages/fast-web-utilities/src/class-names.ts
@@ -3,29 +3,17 @@ import { isFunction, isString } from "lodash-es";
 type ClassNamesArg = string | (() => string) | [string | (() => string), boolean];
 
 export function classNames(...args: ClassNamesArg[]): string {
-    let classes: string = "";
-    let leadingChar: string = "";
-    const addLeadingChar: (value: string) => string = (value: string): string =>
-        (leadingChar += value);
+    return args.reduce<string>((accum: string, value: ClassNamesArg): string => {
+        const leadingChar: string = accum.length ? " " : "";
+        const normalizedValue: string =
+            Array.isArray(value) && value[1]
+                ? classNames.call(null, value[0])
+                : isFunction(value)
+                    ? value()
+                    : isString(value)
+                        ? value
+                        : "";
 
-    for (let i: number = 0, length: number = args.length; i < length; i++) {
-        const arg: ClassNamesArg = args[i];
-        leadingChar = classes.length === 0 ? "" : " ";
-
-        if (isString(arg)) {
-            classes += addLeadingChar(arg);
-        } else if (isFunction(arg)) {
-            classes += addLeadingChar(arg());
-        } else if (Array.isArray(arg) && arg[1]) {
-            const value: string | (() => string) = arg[0];
-
-            if (isString(value)) {
-                classes += addLeadingChar(value);
-            } else if (isFunction(value)) {
-                classes += addLeadingChar(value());
-            }
-        }
-    }
-
-    return classes;
+        return !normalizedValue.length ? accum : accum + leadingChar + normalizedValue;
+    }, "");
 }
