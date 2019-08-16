214f20d3ae0f8faf17d38ca3806316fa7be507f6
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2019-08-16 08:16:16 -0700

    add insights SDK

--- packages/fast-component-explorer/app/index.tsx
+++ packages/fast-component-explorer/app/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import HTML5Backend from "react-dnd-html5-backend";
 import { DragDropContext } from "react-dnd";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import { ReactPlugin, withAITracking } from "@microsoft/applicationinsights-react-js";
+import { createBrowserHistory } from "history";
 
 /**
  * Create the root node
@@ -11,10 +14,30 @@ const root: HTMLElement = document.createElement("div");
 root.setAttribute("id", "root");
 document.body.appendChild(root);
 
-const DragDropApp: any = DragDropContext(HTML5Backend)(App);
+/**
+ * Create Application Insights SDK for Telemetry
+ * https://docs.microsoft.com/en-us/azure/azure-monitor/learn/nodejs-quick-start
+ */
+const browserHistory = createBrowserHistory({ basename: "" });
+var reactPlugin = new ReactPlugin();
+var appInsights = new ApplicationInsights({
+    config: {
+        instrumentationKey: "ecda4203-b38b-42ad-b181-f0f7f2b1d8fa",
+        extensions: [reactPlugin],
+        extensionConfig: {
+            [reactPlugin.identifier]: { history: browserHistory },
+        },
+    },
+});
+appInsights.loadAppInsights();
+
+const AppFoundation: any = withAITracking(
+    reactPlugin,
+    DragDropContext(HTML5Backend)(App)
+);
 
 function render(): void {
-    ReactDOM.render(<DragDropApp />, root);
+    ReactDOM.render(<AppFoundation />, root);
 }
 
 render();
