2e6bbaa355d3312665e3097c61600068f4b11525
Nick Rice <nicholas.rice119@gmail.com>
2019-08-20 10:54:22 -0700

    add deprecation notice to jss export

--- packages/fast-jss-manager-react/src/index.ts
+++ packages/fast-jss-manager-react/src/index.ts
@@ -5,7 +5,14 @@ export {
     DesignSystemProvider,
     DesignSystemProviderProps,
 } from "./design-system-provider";
-export { stylesheetRegistry, jss } from "./jss";
+export {
+    stylesheetRegistry,
+    /**
+     * @deprecated
+     * use JSSManager.jss to access the JSS instance
+     */
+    jss
+} from "./jss";
 export { ManagedJSSProps } from "./jss-manager";
 export { Consumer as DesignSystemConsumer } from "./context";
 export { JSSManager } from "./jss-manager";
