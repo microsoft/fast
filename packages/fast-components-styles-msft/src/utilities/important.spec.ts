acb88b1c63e8df7b0cae8bda94c051417a1ec717
khamudom <khamu@microsoft.com>
2019-09-05 08:39:51 -0700

    update important function and test to also use DesignSystemResolver

--- packages/fast-components-styles-msft/src/utilities/important.spec.ts
+++ packages/fast-components-styles-msft/src/utilities/important.spec.ts
@@ -1,8 +1,15 @@
 import { importantValue } from "./important";
+import defaultDesignSystem from "../design-system";
+import { height } from "./density";
 
 describe("important", (): void => {
     test("adds !important to a passed in css value", (): void => {
-        const result: string = importantValue("width: 10px");
-        expect(result).toEqual("width: 10px !important");
+        const result: string = importantValue("10px")(defaultDesignSystem);
+        expect(result).toEqual("10px !important");
+    });
+
+    test("adds !important to a design system function", (): void => {
+        const result: string = importantValue(height())(defaultDesignSystem);
+        expect(result).toEqual("32px !important");
     });
 });
--- packages/fast-components-styles-msft/src/utilities/important.ts
+++ packages/fast-components-styles-msft/src/utilities/important.ts
@@ -1,6 +1,14 @@
+import {
+    checkDesignSystemResolver,
+    DesignSystem,
+    DesignSystemResolver
+} from "../design-system";
+
 /**
  * Sets a css value to be '!important'.
  */
-export function importantValue(value: string): string {
-    return `${value} !important`;
+export function importantValue(value: string | DesignSystemResolver<string>): DesignSystemResolver<string> {
+    return (designSystem: DesignSystem): string => {
+        return `${checkDesignSystemResolver(value, designSystem)} !important`;
+    }
 }
