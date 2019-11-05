a3e30ee4033700b053c84e487430f6da325cd32b
Stephane Comeau <scomea@microsoft.com>
2019-11-04 17:05:14 -0800

    fix: modal dialogs actively take focus (#2319)
    * Force document focus to modal dialog.
    
    * run prettier and undo an unintentional change
    
    * minor correction
    
    * remove unused var
    
    * add tab management to modal
    
    * fix broken tests
    
    * respond to feedback (minor tweaks)
    
    * add tests
    
    * check for adding key listener, add some tests
    
    * add to read me
    
    * run prettier

--- packages/fast-components-react-msft/src/dialog/dialog.stories.tsx
+++ packages/fast-components-react-msft/src/dialog/dialog.stories.tsx
@@ -1,10 +1,20 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import { Dialog } from "./";
+import { Button } from "../button";
 
 storiesOf("Dialog", module)
     .add("Default", () => <Dialog visible={true} />)
-    .add("Modal", () => <Dialog visible={true} modal={true} />)
+    .add("Modal", () => (
+        <div>
+            <Button>Outside Button 1</Button>
+            <Dialog visible={true} modal={true}>
+                <Button>Button 1</Button>
+                <Button>Button 2</Button>
+            </Dialog>
+            <Button>Outside Button 2</Button>
+        </div>
+    ))
     .add("Custom size", () => (
         <Dialog visible={true} contentHeight="300px" contentWidth="500px" />
     ));
