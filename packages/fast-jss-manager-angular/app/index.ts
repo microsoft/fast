b7118e1deedeb13e4a31808b4397c8d909c901b7
Chris Holt <chhol@microsoft.com>
2019-11-07 11:28:57 -0800

    chore: delete angular jss manager package (#2413)

--- packages/fast-jss-manager-angular/app/index.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { AppModule } from "./app.module";
-
-/* tslint:disable-next-line */
-const Polyfills: any = require("./polyfills");
-
-platformBrowserDynamic().bootstrapModule(AppModule);
