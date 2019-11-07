d6bc35a05cb16ccf683ae3ba7ea9caa4c30cb525
Jane Chu <7559015+janechu@users.noreply.github.com>
2019-11-07 08:20:56 +0800

    fix: allows oneOfs to be undefined if no values have been set (#2404)

--- packages/fast-tooling-react/src/form/form-section.tsx
+++ packages/fast-tooling-react/src/form/form-section.tsx
@@ -370,6 +370,9 @@ class FormSection extends React.Component<
             typeof this.state.oneOfAnyOf !== "undefined" &&
             this.props.schema[this.state.oneOfAnyOf.type]
         ) {
+            const unselectedOption: React.ReactNode = (
+                <option value={-1}>{"Select an option"}</option>
+            );
             const options: React.ReactNode = getOneOfAnyOfSelectOptions(
                 this.props.schema,
                 this.state
@@ -381,6 +384,7 @@ class FormSection extends React.Component<
                     activeIndex={this.state.oneOfAnyOf.activeIndex}
                     onUpdate={this.handleAnyOfOneOfClick}
                 >
+                    {unselectedOption}
                     {options}
                 </FormOneOfAnyOf>
             );
