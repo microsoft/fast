22fdf7566f6519807c540067a5b86e580f7ac479
Nicholas Rice <nicholas.rice119@gmail.com>
2019-11-15 14:31:59 -0800

    chore: ensure prettier works correctly (#2446)
    * target specific prettier version
    
    * install consistent prettier versions and fix build break
    
    * pretty pretty
    
    * fix version of fast-color-explorer
    
    * update yarn.lock

--- packages/fast-color-explorer/app/gradient.tsx
+++ packages/fast-color-explorer/app/gradient.tsx
@@ -57,7 +57,9 @@ class BaseGradient extends React.Component<GradientProps, {}> {
                 this.props.markedColor !== undefined &&
                 item.toUpperCase() === this.props.markedColor.toUpperCase()
             ) {
-                classNames = `${classNames} ${this.props.managedClasses.gradient_item__marked}`;
+                classNames = `${classNames} ${
+                    this.props.managedClasses.gradient_item__marked
+                }`;
             }
 
             return (
