6ae4c42c4e6ca84639de31dbbd495a16478a9c93
Chris Holt <chhol@microsoft.com>
2019-11-25 17:06:11 -0800

    fix: remove cursor pointer from typography and apply to semantic components (#2468)

--- packages/fast-components-styles-msft/src/paragraph/index.ts
+++ packages/fast-components-styles-msft/src/paragraph/index.ts
@@ -2,9 +2,11 @@ import { DesignSystem } from "../design-system";
 import { ComponentStyles } from "@microsoft/fast-jss-manager";
 import { ParagraphClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
 import { applyFontWeightNormal } from "../utilities/fonts";
+import { applyCursorDefault } from "../utilities/cursor";
 
 const styles: ComponentStyles<ParagraphClassNameContract, DesignSystem> = {
     paragraph: {
+        ...applyCursorDefault(),
         ...applyFontWeightNormal(),
     },
     paragraph__1: {},
