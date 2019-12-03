7a71e02b42428e70158264c35f1ac8d9cf105a93
Stephane Comeau <scomea@microsoft.com>
2019-12-03 10:06:55 -0800

    update tests

--- packages/fast-components-react-base/src/horizontal-overflow/horizontal-overflow.spec.tsx
+++ packages/fast-components-react-base/src/horizontal-overflow/horizontal-overflow.spec.tsx
@@ -305,7 +305,7 @@ describe("horizontal overflow", (): void => {
                     [10, 20, 20, 50, 20],
                     10
                 )
-        ).toBe(-0);
+        ).toBe(0);
 
         expect(
             renderedWithImages
@@ -316,7 +316,7 @@ describe("horizontal overflow", (): void => {
                     [50, 50, 50, 50],
                     100
                 )
-        ).toBe(-50);
+        ).toBe(50);
     });
     test("should an state property `itemsHeight`", () => {
         const renderedWithImages: any = mount(
