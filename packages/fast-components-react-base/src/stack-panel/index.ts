ce18b363efb169dd5fd14255ad53d4ba452f1aa9
Stephane Comeau <scomea@microsoft.com>
2019-12-09 13:55:47 -0800

    Stack panel base functionality

---
+++ packages/fast-components-react-base/src/stack-panel/index.ts
@@ -0,0 +1,6 @@
+import StackPanel from "./stack-panel";
+import StackPanelSchema from "./stack-panel.schema";
+
+export { StackPanelSchema };
+export default StackPanel;
+export * from "./stack-panel";
