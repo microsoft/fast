59867ad49326671c994e8b8d0ef92b888e5d9203
Jane Chu <7559015+janechu@users.noreply.github.com>
2019-12-16 12:59:39 -0800

    feat: add a JSON schema for react children (#2529)

---
+++ packages/fast-tooling-react/src/data-utilities/react-children.schema.spec.ts
@@ -0,0 +1,33 @@
+import jest from "jest";
+import ajv from "ajv";
+import { reactChildrenSchema } from "./";
+
+const validator: ajv.Ajv = new ajv({ schemaId: "auto", allErrors: true });
+const validationFn: ajv.ValidateFunction = validator.compile(reactChildrenSchema);
+
+describe("reactChildrenSchema", () => {
+    test("should be valid against a number", () => {
+        expect(validationFn(1)).toEqual(true);
+    });
+    test("should be valid against a string", () => {
+        expect(validationFn("foo")).toEqual(true);
+    });
+    test("should be valid against null", () => {
+        expect(validationFn(null)).toEqual(true);
+    });
+    test("should be valid against a component interface", () => {
+        expect(validationFn({ id: "foo", props: {} })).toEqual(true);
+    });
+    test("should be valid against an array containing a number", () => {
+        expect(validationFn([1])).toEqual(true);
+    });
+    test("should be valid against an array containing a string", () => {
+        expect(validationFn(["foo"])).toEqual(true);
+    });
+    test("should be valid against an array contianing null", () => {
+        expect(validationFn([null])).toEqual(true);
+    });
+    test("should be valid against an array containing a component interface", () => {
+        expect(validationFn([{ id: "foo", props: {} }])).toEqual(true);
+    });
+});
