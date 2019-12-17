60252a61dbb213db02334b37f140c1f26f14d291
Nick Rice <nicholas.rice119@gmail.com>
2019-12-17 13:28:27 -0800

    add concat-md

--- packages/fast-components-react-msft/foo.js
+++
@@ -1,6 +0,0 @@
-const linkify = require("linkify-it")();
-const path = require("path");
-
-console.log(
-    linkify.match(path.resolve(__dirname, "./api/interfaces/actiontogglehandledprops.md"))
-);
