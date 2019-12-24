bbb17b9a33778c699c0fdfb006c27959aa029ec7
nicholasrice <nicholas.rice119@gmail.com>
2019-12-23 17:14:15 -0800

    plugging documentation into storybook

--- packages/fast-components-react-msft/.storybook/config.js
+++ packages/fast-components-react-msft/.storybook/config.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { configure, addParameters, addDecorator } from "@storybook/react";
 import { create, Global } from "@storybook/theming";
+import { addReadme } from "storybook-readme";
+
 // Import from dist cause storybook throws up if we import the named import from the index
 import theme from "@microsoft/fast-storybook-presets/dist/theme";
 
+addDecorator(addReadme);
 addParameters({
     options: {
         theme,
--- packages/fast-components-react-msft/src/breadcrumb/breadcrumb.stories.tsx
+++ packages/fast-components-react-msft/src/breadcrumb/breadcrumb.stories.tsx
@@ -2,14 +2,21 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 import { Breadcrumb } from "./";
 import { Hypertext } from "../hypertext";
+import API from "./.tmp/API.md";
 
 function separator(className?: string): JSX.Element {
     return <span className={className}>/</span>;
 }
-storiesOf("Breadcrumb", module).add("Default", () => (
-    <Breadcrumb separator={separator}>
-        <Hypertext href="#">Item one</Hypertext>
-        <Hypertext href="#">Item Two</Hypertext>
-        <Hypertext>Item Three</Hypertext>
-    </Breadcrumb>
-));
+storiesOf("Breadcrumb", module)
+    .addParameters({
+        readme: {
+            sidebar: API,
+        },
+    })
+    .add("Default", () => (
+        <Breadcrumb separator={separator}>
+            <Hypertext href="#">Item one</Hypertext>
+            <Hypertext href="#">Item Two</Hypertext>
+            <Hypertext>Item Three</Hypertext>
+        </Breadcrumb>
+    ));
