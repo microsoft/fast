a7b68941a9b08375ba3cd539a440558cdede5f26
nicholasrice <nicholas.rice119@gmail.com>
2019-12-29 20:01:13 -0800

    remove newlines

--- packages/fast-components-react-msft/typedoc-theme/helpers-src/member-comment.ts
+++ packages/fast-components-react-msft/typedoc-theme/helpers-src/member-comment.ts
@@ -1,5 +1,8 @@
 import { DeclarationReflection } from "typedoc";
 
 export function memberComment(this: DeclarationReflection): string {
-    return this.comment && this.comment.shortText ? this.comment.shortText : "";
+    return (this.comment && this.comment.shortText ? this.comment.shortText : "").replace(
+        /(\r\n|\n|\r)/gm,
+        " "
+    ); // Newlines are syntax in Markdown, so strip all newlines out
 }
