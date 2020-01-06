02ef0ad29389bceecf6ece67cb23cb3988063c16
Chris Holt <chhol@microsoft.com>
2020-01-06 13:36:45 -0800

    chore: remove deprecated packages

--- packages/fast-browser-extensions/src/extension/ExtensionApi/RuntimeApi/index.ts
+++
@@ -1,8 +0,0 @@
-import OnMessageExternal from "./on-message-external";
-
-export default class RuntimeApi {
-    /**
-     * Remove a context menu by id
-     */
-    public onMessageExternal: OnMessageExternal = new OnMessageExternal();
-}
--- packages/fast-browser-extensions/src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This is the export file. It should export everything necessary to communicate with the browser extension
- */
-export {
-    regions,
-    themes,
-    onMenuItemClick,
-    createContextMenus,
-} from "./extension/messaging";
