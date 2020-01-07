a7b53f199f6750c8cf1abfff888a7245482b2e33
Nicholas Rice <nicholas.rice119@gmail.com>
2020-01-07 10:14:29 -0800

    feat: adds figma plugin boilerplate (#2548)
    * boilerplate figma
    
    * plugin wired with React
    
    * update yarn lock
    
    * fixing comments
    
    * adding tslint config

---
+++ packages/fast-figma-plugin-msft/README.md
@@ -0,0 +1,3 @@
+# FAST Figma Plugin MSFT
+A Figma Plugin for designing MSFT components and experiences.
+
---
+++ packages/fast-figma-plugin-msft/src/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Main plugin file responsible for Figma document manipulation.
+ * This file has full access to the Figma API.
+ */
+
+/**
+ * Show UI on plugin launch
+ */
+figma.showUI(__html__);
