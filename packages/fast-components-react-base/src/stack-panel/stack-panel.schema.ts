ec5e73e7e8232c7d43343e831645982bb2a75129
Stephane Comeau <scomea@microsoft.com>
2020-01-13 11:07:40 -0800

    add tests and schema update

--- packages/fast-components-react-base/src/stack-panel/stack-panel.schema.ts
+++ packages/fast-components-react-base/src/stack-panel/stack-panel.schema.ts
@@ -4,7 +4,50 @@ export default {
     description: "A  stack panel component's schema definition.",
     type: "object",
     id: "@microsoft/fast-components-react-base/stack-panel",
-    properties: {},
+    properties: {
+        enableVirtualization: {
+            title: "Enable virtualization",
+            type: "boolean",
+        },
+        neverVirtualizeIndexes: {
+            title: "Indexes of items not to virtualize",
+            type: "array",
+        },
+        defaultItemSpan: {
+            title: "Default item span",
+            type: "number",
+        },
+        itemSpanOverrides: {
+            title: "Item span overrides",
+            type: "array",
+        },
+        preloadBufferLength: {
+            title: "Preload buffer length",
+            type: "number",
+        },
+        orientation: {
+            title: "Orientation",
+            type: "string",
+            default: "horizontal",
+            enum: ["horizontal", "vertical"],
+        },
+        nextItemPeek: {
+            title: "Next item peek",
+            type: "number",
+        },
+        enableSmoothScrolling: {
+            title: "Enable smooth scrolling",
+            type: "boolean",
+        },
+        scrollDuration: {
+            title: "Scroll duration",
+            type: "number",
+        },
+        initiallyVisibleItemIndex: {
+            title: "Initially visible item index",
+            type: "number",
+        },
+    },
     reactProperties: {
         children: {
             title: "Children",
