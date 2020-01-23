garion100/fast-dna

feat: add default classes and stylings to differentiate when the value of an item is not set but a default has been provided

packages/fast-tooling-react/src/form/controls/control.number-field.style.ts
@@ -1,5 +1,5 @@
 import { ComponentStyles } from "@microsoft/fast-jss-manager-react";
-import { applyInputStyle } from "../../style";
+import { applyInputStyle, defaultFont } from "../../style";
 
 /**
  * Number field class name contract
@@ -7,14 +7,19 @@ import { applyInputStyle } from "../../style";
 export interface NumberFieldControlClassNameContract {
     numberFieldControl?: string;
     numberFieldControl__disabled?: string;
+    numberFieldControl__default?: string;
 }
 
 const styles: ComponentStyles<NumberFieldControlClassNameContract, {}> = {
     numberFieldControl: {
         ...applyInputStyle(),
         width: "100%",
+        "&$numberFieldControl__default": {
+            ...defaultFont,
+        },
     },
     numberFieldControl__disabled: {},
+    numberFieldControl__default: {},
 };
 
 export default styles;

packages/fast-tooling-react/src/form/controls/control.select.style.ts
@@ -1,5 +1,5 @@
 import { ComponentStyles, CSSRules } from "@microsoft/fast-jss-manager-react";
-import { applySelectInputStyles, applySelectSpanStyles } from "../../style";
+import { applySelectInputStyles, applySelectSpanStyles, defaultFont } from "../../style";
 
 /**
  * Select class name contract
@@ -7,17 +7,22 @@ import { applySelectInputStyles, applySelectSpanStyles } from "../../style";
 export interface SelectControlClassNameContract {
     selectControl?: string;
     selectControl__disabled?: string;
+    selectControl__default?: string;
     selectControl_input?: string;
 }
 
 const styles: ComponentStyles<SelectControlClassNameContract, {}> = {
     selectControl: {
         ...applySelectSpanStyles(),
+        "&$selectControl__default $selectControl_input": {
+            ...defaultFont,
+        },
     },
     selectControl__disabled: {},
     selectControl_input: {
         ...applySelectInputStyles(),
     },
+    selectControl__default: {},
 };
 
 export default styles;

microsoft/fast

packages/fast-tooling-react/src/form/controls/control.number-field.style.ts
@@ -1,5 +1,5 @@
 import { ComponentStyles } from "@microsoft/fast-jss-manager-react";
-import { inputStyle } from "../../style";
+import { defaultFontStyle, inputStyle } from "../../style";
 
 /**
  * Number field class name contract
@@ -7,14 +7,19 @@ import { inputStyle } from "../../style";
 export interface NumberFieldControlClassNameContract {
     numberFieldControl?: string;
     numberFieldControl__disabled?: string;
+    numberFieldControl__default?: string;
 }
 
 const styles: ComponentStyles<NumberFieldControlClassNameContract, {}> = {
     numberFieldControl: {
         ...inputStyle,
         width: "100%",
+        "&$numberFieldControl__default": {
+            ...defaultFontStyle,
+        },
     },
     numberFieldControl__disabled: {},
+    numberFieldControl__default: {},
 };
 
 export default styles;

packages/fast-tooling-react/src/form/controls/control.select.style.ts
@@ -1,5 +1,5 @@
 import { ComponentStyles, CSSRules } from "@microsoft/fast-jss-manager-react";
-import { selectInputStyle, selectSpanStyle } from "../../style";
+import { defaultFontStyle, selectInputStyle, selectSpanStyle } from "../../style";
 
 /**
  * Select class name contract
@@ -7,17 +7,22 @@ import { selectInputStyle, selectSpanStyle } from "../../style";
 export interface SelectControlClassNameContract {
     selectControl?: string;
     selectControl__disabled?: string;
+    selectControl__default?: string;
     selectControl_input?: string;
 }
 
 const styles: ComponentStyles<SelectControlClassNameContract, {}> = {
     selectControl: {
         ...selectSpanStyle,
+        "&$selectControl__default $selectControl_input": {
+            ...defaultFontStyle,
+        },
     },
     selectControl__disabled: {},
     selectControl_input: {
         ...selectInputStyle,
     },
+    selectControl__default: {},
 };
 
 export default styles;