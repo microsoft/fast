b5736e4da812496a40e5b1f63d702987423bcc09
nicholasrice <nicholas.rice119@gmail.com>
2020-01-27 21:19:10 -0800

    create factory fn

--- packages/fast-figma-plugin-msft/src/utilities/node.ts
+++ packages/fast-figma-plugin-msft/src/utilities/node.ts
@@ -1,5 +1,3 @@
-import { ColorRGBA64 } from "@microsoft/fast-colors";
-
 /**
  * Returns the selected node if a single node is
  * selected, otherwise returns null
@@ -10,68 +8,27 @@ export function getActiveNode(): SceneNode | null {
     return selection.length === 1 ? selection[0] : null;
 }
 
-/**
- * Strict type assertions for Figma nodes
- */
-export function isDocumentNode(node: BaseNode): node is DocumentNode {
-    return node.type === "DOCUMENT";
+function isNodeType<T extends BaseNode>(type: NodeType): (node: BaseNode) => node is T {
+    return (node: BaseNode): node is T => node.type === type;
 }
 
-export function isPageNode(node: BaseNode): node is PageNode {
-    return node.type === "PAGE";
-}
-
-export function isSliceNode(node: BaseNode): node is SliceNode {
-    return node.type === "SLICE";
-}
-
-export function isFrameNode(node: BaseNode): node is FrameNode {
-    return node.type === "FRAME";
-}
-
-export function isGroupNode(node: BaseNode): node is GroupNode {
-    return node.type === "GROUP";
-}
-
-export function isComponentNode(node: BaseNode): node is ComponentNode {
-    return node.type === "COMPONENT";
-}
-
-export function isInstanceNode(node: BaseNode): node is InstanceNode {
-    return node.type === "INSTANCE";
-}
-
-export function isBooleanOperationNode(node: BaseNode): node is BooleanOperationNode {
-    return node.type === "BOOLEAN_OPERATION";
-}
-
-export function isVectorNode(node: BaseNode): node is VectorNode {
-    return node.type === "VECTOR";
-}
-
-export function isStarNode(node: BaseNode): node is StarNode {
-    return node.type === "STAR";
-}
-
-export function isLineNode(node: BaseNode): node is LineNode {
-    return node.type === "LINE";
-}
-
-export function isEllipseNode(node: BaseNode): node is EllipseNode {
-    return node.type === "ELLIPSE";
-}
-
-export function isPolygonNode(node: BaseNode): node is PolygonNode {
-    return node.type === "POLYGON";
-}
-
-export function isRectangleNode(node: BaseNode): node is RectangleNode {
-    return node.type === "RECTANGLE";
-}
-
-export function isTextNode(node: BaseNode): node is TextNode {
-    return node.type === "TEXT";
-}
+export const isDocumentNode = isNodeType<DocumentNode>("DOCUMENT");
+export const isPageNode = isNodeType<PageNode>("PAGE");
+export const isSliceNode = isNodeType<SliceNode>("SLICE");
+export const isFrameNode = isNodeType<FrameNode>("FRAME");
+export const isGroupNode = isNodeType<GroupNode>("GROUP");
+export const isComponentNode = isNodeType<ComponentNode>("COMPONENT");
+export const isInstanceNode = isNodeType<InstanceNode>("INSTANCE");
+export const isBooleanOperationNode = isNodeType<BooleanOperationNode>(
+    "BOOLEAN_OPERATION"
+);
+export const isVectorNode = isNodeType<VectorNode>("VECTOR");
+export const isStarNode = isNodeType<StarNode>("STAR");
+export const isLineNode = isNodeType<LineNode>("LINE");
+export const isEllipseNode = isNodeType<EllipseNode>("ELLIPSE");
+export const isPolygonNode = isNodeType<PolygonNode>("POLYGON");
+export const isRectangleNode = isNodeType<RectangleNode>("RECTANGLE");
+export const isTextNode = isNodeType<TextNode>("TEXT");
 
 export function isSceneNode(node: BaseNode): node is SceneNode {
     return [
