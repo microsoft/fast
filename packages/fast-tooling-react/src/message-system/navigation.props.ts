6e665dd1c816fbb00f27990596e6de39b781122f
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-02-06 16:18:18 -0800

    feat: add message system registration class and adjust message system to use a dictionary to account for multiple data sets (#2662)

---
+++ packages/fast-tooling-react/src/message-system/navigation.props.ts
@@ -0,0 +1,92 @@
+import { DataType } from "../data-utilities/types";
+
+export interface TreeNavigation {
+    /**
+     * A dictionary of items
+     */
+    [key: string]: TreeNavigationItem;
+}
+
+export interface TreeNavigationItem {
+    /**
+     * The ID for this item
+     */
+    self: string;
+
+    /**
+     * The ID for the parent of this item
+     */
+    parent: string | null;
+
+    /**
+     * The relative data location
+     */
+    relativeDataLocation: string;
+
+    /**
+     * The schema location
+     */
+    schemaLocation: string;
+
+    /**
+     * The schema
+     */
+    schema: any;
+
+    /**
+     * The data
+     */
+    data: any;
+
+    /**
+     * The display text
+     */
+    text: string;
+
+    /**
+     * The data type, this will result in a different
+     * icons used
+     */
+    type: DataType;
+
+    /**
+     * The ids belonging to the text as a dropdown
+     */
+    items: string[];
+}
+
+export interface TreeNavigationConfig {
+    /**
+     * The component schemas tree navigation
+     */
+    0: TreeNavigation;
+
+    /**
+     * The id for the root component
+     */
+    1: string;
+}
+
+export interface TreeNavigationConfigResolver {
+    /**
+     * The root level tree navigation
+     */
+    0: TreeNavigationConfig;
+
+    /**
+     * Additional tree navigation to add to the dictionary
+     */
+    1: { [key: string]: TreeNavigationConfig };
+}
+
+export interface TreeNavigationConfigDictionary {
+    /**
+     * The dictionary of tree navigation configs
+     */
+    0: { [key: string]: TreeNavigationConfig };
+
+    /**
+     * The key of the root tree navigation config
+     */
+    1: string;
+}
