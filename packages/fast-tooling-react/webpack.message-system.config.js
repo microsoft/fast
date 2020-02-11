73f6b21a761b12b0957e9de89a8ead7c1737e670
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-02-10 17:25:24 -0800

    fix: update the message system to combine declaring a new worker and registration from a single class (#2670)
    BREAKING CHANGE: updates the message system to be a single class instantiation which includes the web worker implementation and the registry

---
+++ packages/fast-tooling-react/webpack.message-system.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const appDir = path.resolve(__dirname, "./app");
+const outDir = path.resolve(__dirname, "./dist");
+
+module.exports = {
+    entry: {
+        "message-system": path.resolve(__dirname, "./src/message-system/webworker.ts"),
+    },
+    output: {
+        path: outDir,
+        filename: "[name].min.js",
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            compilerOptions: {
+                                declaration: false,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".tsx", ".ts", ".json"],
+    },
+};
