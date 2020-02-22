487cc05f6c69b7459420162cb9e6a9320b60a7aa
James Burnside <jaburnsi@microsoft.com>
2020-02-21 18:10:52 -0800

    fix test error

--- packages/fast-components-react-base/src/horizontal-overflow/horizontal-overflow.spec.tsx
+++ packages/fast-components-react-base/src/horizontal-overflow/horizontal-overflow.spec.tsx
@@ -207,7 +207,7 @@ describe("horizontal overflow", (): void => {
         expect(rendered.state("itemsHeight")).toBe(100);
     });
     test("should set `itemHeight` to the fixedHeight if children have been passed but a fixedHeight has been set", () => {
-        const rendered = mount(
+        const rendered: any = mount(
             <HorizontalOverflow managedClasses={managedClasses} fixedHeight={100}>
                 <img key="image1" src="https://placehold.it/200x200?text=1" />
             </HorizontalOverflow>
