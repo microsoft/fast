ac70e247b83a7cf1bca28ed0b0dd02c0c476dda0
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-03-03 15:25:36 -0800

    feat: convert the Navigation component API to use the message system (#2718)

--- packages/fast-tooling-react/app/pages/navigation/example.data.ts
+++ packages/fast-tooling-react/app/pages/navigation/example.data.ts
@@ -1,162 +1,50 @@
-import {
-    NavigationDataType,
-    TreeNavigation,
-} from "../../../src/navigation/navigation.props";
-import { ChildOptionItem } from "../../../src";
-import { get } from "lodash-es";
-
-import noChildrenSchema from "./no-children.schema.json";
-import childrenSchema from "./children.schema.json";
+import noChildrenSchema from "./no-children.schema";
+import childrenSchema from "./children.schema";
+import { DataDictionary } from "../../../src/message-system/data.props";
 
 const noChildren: any = {
     text: "Hello world",
 };
 
-const children: any = {
-    foo: "Bar",
-    children: [
-        "Foo",
-        {
-            id: get(noChildrenSchema, "id"),
-            props: noChildren,
-        },
-        {
-            id: get(childrenSchema, "id"),
-            props: {
+const children: DataDictionary<any> = [
+    {
+        "": {
+            schemaId: childrenSchema.id,
+            data: {
                 children: [
                     {
-                        id: get(childrenSchema, "id"),
-                        props: {
-                            children: [
-                                {
-                                    id: get(childrenSchema, "id"),
-                                    props: {
-                                        children: [
-                                            {
-                                                id: get(childrenSchema, "id"),
-                                                props: {
-                                                    children: {
-                                                        id: get(noChildrenSchema, "id"),
-                                                        props: noChildren,
-                                                    },
-                                                },
-                                            },
-                                        ],
-                                    },
-                                },
-                                {
-                                    id: get(childrenSchema, "id"),
-                                    props: {
-                                        children: {
-                                            id: get(noChildrenSchema, "id"),
-                                            props: noChildren,
-                                        },
-                                    },
-                                },
-                            ],
-                        },
+                        id: "children[0]",
+                        dataLocation: "children",
+                    },
+                    {
+                        id: "children[1]",
+                        dataLocation: "children",
                     },
                 ],
             },
         },
-        {
-            id: get(childrenSchema, "id"),
-            props: {
-                children: {
-                    id: get(noChildrenSchema, "id"),
-                    props: noChildren,
-                },
+        "children[0]": {
+            parent: {
+                id: "",
+                dataLocation: "children",
+            },
+            schemaId: noChildrenSchema.id,
+            data: {
+                text: "bar",
             },
         },
-        {
-            id: get(childrenSchema, "id"),
-            props: {
-                children: [
-                    {
-                        id: get(childrenSchema, "id"),
-                        props: {
-                            children: {
-                                id: get(noChildrenSchema, "id"),
-                                props: noChildren,
-                            },
-                        },
-                    },
-                    {
-                        id: get(childrenSchema, "id"),
-                        props: {
-                            children: {
-                                id: get(noChildrenSchema, "id"),
-                                props: noChildren,
-                            },
-                        },
-                    },
-                ],
+        "children[1]": {
+            parent: {
+                id: "",
+                dataLocation: "children",
+            },
+            schemaId: noChildrenSchema.id,
+            data: {
+                text: "bat",
             },
         },
-    ],
-    object: {
-        children: [
-            {
-                id: get(noChildrenSchema, "id"),
-                props: noChildren,
-            },
-            "Foo",
-        ],
     },
-    array: [
-        {
-            children: [
-                {
-                    id: get(noChildrenSchema, "id"),
-                    props: noChildren,
-                },
-                "Bar",
-            ],
-        },
-    ],
-};
-
-const navigationData: TreeNavigation[] = [
-    {
-        text: "Child A",
-        type: NavigationDataType.component,
-        dataLocation: "children[0]",
-        items: [
-            {
-                text: "Child B",
-                type: NavigationDataType.component,
-                dataLocation: "children[0].props.children[0]",
-            },
-            {
-                text: "Child C",
-                type: NavigationDataType.component,
-                dataLocation: "children[0].props.children[1]",
-                items: [
-                    {
-                        text: "Child D",
-                        type: NavigationDataType.component,
-                        dataLocation: "children[0].props.children[1].props.children",
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        text: "Child E",
-        type: NavigationDataType.component,
-        dataLocation: "children[1]",
-    },
+    "",
 ];
 
-const childOptions: ChildOptionItem[] = [
-    {
-        component: null,
-        schema: noChildrenSchema,
-    },
-    {
-        component: null,
-        schema: childrenSchema,
-    },
-];
-
-export { children, childOptions, navigationData, noChildren };
+export { children, noChildren };
