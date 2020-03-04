29f16971ae4cfbd419744927d781f94ce96e788e
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-03-04 12:45:07 -0800

    chore: move the generate function to the tooling package (#2733)

--- packages/fast-tooling-react/src/data-utilities/index.ts
+++ packages/fast-tooling-react/src/data-utilities/index.ts
@@ -1,11 +1,9 @@
 import { ChildOptionItem } from "./types";
-import { getDataFromSchema } from "./generate";
 import { mapDataToCodePreview, mapDataToComponent } from "./mapping";
 import { Plugin, PluginProps } from "./plugin";
 
 export {
     ChildOptionItem,
-    getDataFromSchema,
     mapDataToComponent,
     mapDataToCodePreview,
     Plugin,
