5284c331c8eeb3f3e5f60c68bf57a50df789d718
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-03-04 12:24:28 -0800

    chore: remove react specific schema definition (#2727)

--- packages/fast-tooling-react/src/data-utilities/react-child.schema.ts
+++
@@ -1,6 +0,0 @@
-import reactElementSchema from "./react-element.schema";
-import reactTextSchema from "./react-text.schema";
-
-export default {
-    oneOf: [...reactElementSchema.oneOf, ...reactTextSchema.oneOf],
-};
