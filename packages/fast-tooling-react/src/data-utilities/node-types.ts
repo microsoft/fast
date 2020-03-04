a905009065671135d55ce4592c1407bb92f62788
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-03-04 12:53:24 -0800

    chore: remove a check for react primitives now that data is not tied to react interfaces (#2735)

--- packages/fast-tooling-react/src/data-utilities/node-types.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Determines if a node is a primitive JS type that React can render
- */
-export function isPrimitiveReactNode(node: unknown): node is string | number {
-    const nodeType: string = typeof node;
-    return nodeType === "string" || nodeType === "number";
-}
