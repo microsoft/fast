e6cd1c832c77196f274b2d068012ae99d0dc7acb
nicholasrice <nicholas.rice119@gmail.com>
2020-03-05 11:27:03 -0800

    add common styles

---
+++ packages/fast-components/src/styles/display.ts
@@ -0,0 +1,57 @@
+import { css } from "../fast-dna/index";
+
+export type CSSDisplayPropertyValue =
+    | "inline"
+    | "block"
+    | "contents"
+    | "flex"
+    | "grid"
+    | "inline-block"
+    | "inline-flex"
+    | "inline-grid"
+    | "inline-table"
+    | "list-item"
+    | "run-in"
+    | "table"
+    | "table-caption"
+    | "table-column-group"
+    | "table-header-group"
+    | "table-footer-group"
+    | "table-row-group"
+    | "table-cell"
+    | "table-column"
+    | "table-row"
+    | "none"
+    | "initial"
+    | "inherit";
+
+const hidden = css`
+    :host([hidden]) {
+        display: none;
+    }
+`;
+
+/* tslint:disable */
+const cache: { [key: string]: ReturnType<typeof css> } = {};
+
+/**
+ * Apply the common styles for every component
+ * @param display The CSS display property value
+ */
+export function display(display: CSSDisplayPropertyValue): ReturnType<typeof css> {
+    if (cache.hasOwnProperty(display)) {
+        return cache[display];
+    }
+
+    const style = css`
+        ${hidden} :host {
+            display: ${display};
+        }
+    `;
+
+    cache[display] = style;
+
+    return style;
+}
+
+/* tslint:disable */
