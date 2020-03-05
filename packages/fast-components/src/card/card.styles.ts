06e66014faa722e0d1defe60b0a87acc85801da7
Chris Holt <chhol@microsoft.com>
2020-03-05 08:22:42 -0800

    remove custom properties for width and height

--- packages/fast-components/src/card/card.styles.ts
+++ packages/fast-components/src/card/card.styles.ts
@@ -3,13 +3,12 @@ import { css } from "@microsoft/fast-element";
 export const CardStyles = css`
     :host {
         --elevation: 4;
-        --height: "100%";
-        --width: "100%";
         display: block;
+        box-sizing: border-box;
+        height: "100%";
+        width: "100%";
         background: var(--neutral-layer-card);
-        border-radius: calc(var(--elevated-corner-radius));
-        width: var(--width);
-        height: var(--height);
+        border-radius: var(--elevated-corner-radius);
         box-shadow: 0 0 calc(var(--elevation) * 1px) rgba(0, 0, 0, 0.5);
     }
 `;
