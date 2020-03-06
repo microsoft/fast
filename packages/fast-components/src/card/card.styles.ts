63b0faba3291888177d50ba45ad462a76af2c8e5
Chris Holt <chhol@microsoft.com>
2020-03-06 10:00:00 -0800

    update use of custom variables as method of changing height/width

--- packages/fast-components/src/card/card.styles.ts
+++ packages/fast-components/src/card/card.styles.ts
@@ -3,10 +3,10 @@ import { css } from "@microsoft/fast-element";
 export const CardStyles = css`
     :host {
         --elevation: 4;
+        height: var(--card-height, 200px);
+        width: var(--card-width, 200px);
         display: block;
         box-sizing: border-box;
-        height: "100%";
-        width: "100%";
         background: var(--neutral-layer-card);
         border-radius: var(--elevated-corner-radius);
         box-shadow: 0 0 calc(var(--elevation) * 1px) rgba(0, 0, 0, 0.5);
