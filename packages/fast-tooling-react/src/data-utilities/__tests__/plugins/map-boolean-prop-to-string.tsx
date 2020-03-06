04ef84fbcafb38b0dc3a34000a9e92cf1a43c7d2
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-03-06 14:38:21 -0800

    chore: remove viewer content component and utility files that do not map to the new data dictionary interface (#2740)

--- packages/fast-tooling-react/src/data-utilities/__tests__/plugins/map-boolean-prop-to-string.tsx
+++
@@ -1,7 +0,0 @@
-import { Plugin, PluginProps } from "../../index";
-
-export default class MapBooleanPropToString extends Plugin<PluginProps> {
-    public resolver(data: boolean): string {
-        return data.toString();
-    }
-}
