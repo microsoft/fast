431edd8a8c7d0551b5371b8093d0438690961d04
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-03-06 14:38:21 -0800

    chore: remove viewer content component and utility files that do not map to the new data dictionary interface (#2740)

--- packages/fast-tooling-react/src/data-utilities/index.ts
+++
@@ -1,5 +0,0 @@
-import { ChildOptionItem } from "./types";
-import { mapDataToCodePreview, mapDataToComponent } from "./mapping";
-import { Plugin, PluginProps } from "./plugin";
-
-export { ChildOptionItem, mapDataToComponent, mapDataToCodePreview, Plugin, PluginProps };
