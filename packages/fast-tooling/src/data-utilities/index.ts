17a34f382d37b8d3f2244fadbafe3e83fd3a7d55
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-03-09 14:23:36 -0700

    feat: add a data mapper (#2756)

--- packages/fast-tooling/src/data-utilities/index.ts
+++ packages/fast-tooling/src/data-utilities/index.ts
@@ -2,5 +2,6 @@ export * from "./array";
 export * from "./duplicate";
 export * from "./generate";
 export * from "./location";
+export * from "./mapping";
 export * from "./relocate";
 export * from "./types";
