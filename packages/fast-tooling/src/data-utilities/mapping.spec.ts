261889ab9ee4b853b5c053b8c00c242d894011ff
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-03-09 14:23:36 -0700

    feat: add a data mapper (#2756)

---
+++ packages/fast-tooling/src/data-utilities/mapping.spec.ts
@@ -0,0 +1,125 @@
+import { mapDataDictionary, MapperConfig } from "./mapping";
+
+describe("mapDataDictionary", () => {
+    test("should call a passed mapper function on a single data dictionary item", () => {
+        const mapper: any = jest.fn();
+
+        mapDataDictionary({
+            dataDictionary: {
+                "": {
+                    schemaId: "foo",
+                    data: {},
+                },
+            },
+            dataDictionaryKey: "",
+            schemaDictionary: {
+                foo: {
+                    type: "object",
+                },
+            },
+            mapper,
+        });
+
+        expect(mapper).toHaveBeenCalledTimes(1);
+    });
+    test("should call a passed mapper function on multiple dictionary items", () => {
+        const mapper: any = jest.fn();
+
+        mapDataDictionary({
+            dataDictionary: {
+                "": {
+                    schemaId: "foo",
+                    data: {
+                        a: "b",
+                    },
+                },
+                foo: {
+                    schemaId: "foo",
+                    parent: {
+                        id: "",
+                        dataLocation: "children",
+                    },
+                    data: {
+                        c: "d",
+                    },
+                },
+            },
+            dataDictionaryKey: "",
+            schemaDictionary: {
+                foo: {
+                    type: "object",
+                },
+            },
+            mapper,
+        });
+
+        expect(mapper).toHaveBeenCalledTimes(2);
+    });
+    test("should map a single dictionary entry", () => {
+        const mapper: any = function(config: MapperConfig): any {
+            return config.resolvedData;
+        };
+
+        const result: any = mapDataDictionary({
+            dataDictionary: {
+                "": {
+                    schemaId: "foo",
+                    data: {
+                        a: "b",
+                    },
+                },
+            },
+            dataDictionaryKey: "",
+            schemaDictionary: {
+                foo: {
+                    type: "object",
+                },
+            },
+            mapper,
+        });
+
+        expect(result).toEqual({
+            a: "b",
+        });
+    });
+    test("should map multiple dictionary entries", () => {
+        const mapper: any = function(config: MapperConfig): any {
+            return config.resolvedData;
+        };
+
+        const result: any = mapDataDictionary({
+            dataDictionary: {
+                "": {
+                    schemaId: "foo",
+                    data: {
+                        a: "b",
+                    },
+                },
+                foo: {
+                    schemaId: "foo",
+                    parent: {
+                        id: "",
+                        dataLocation: "children",
+                    },
+                    data: {
+                        c: "d",
+                    },
+                },
+            },
+            dataDictionaryKey: "",
+            schemaDictionary: {
+                foo: {
+                    type: "object",
+                },
+            },
+            mapper,
+        });
+
+        expect(result).toEqual({
+            a: "b",
+            children: {
+                c: "d",
+            },
+        });
+    });
+});
