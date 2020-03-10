4b3b478994a8a50b6a797f043e78f82296477cdc
nicholasrice <nicholas.rice119@gmail.com>
2020-03-10 12:14:44 -0700

    add tracking issue for TODO

--- packages/fast-components/src/styles/elevation.ts
+++ packages/fast-components/src/styles/elevation.ts
@@ -3,6 +3,7 @@
  *
  * TODO: The --background-luminance will need to be derived from JavaScript. For now
  * this is hard-coded to a 1, the relative luminance of pure white.
+ * https://github.com/microsoft/fast-dna/issues/2778
  */
 export const ambientShadow =
     "0 0 calc((var(--elevation) * 0.225px) + 2px) rgba(0, 0, 0, calc(.11 * (2 - var(--background-luminance, 1))))";
