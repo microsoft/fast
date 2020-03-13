cdf1b597f20982074d40a42b7ddc5befc921a3f5
nicholasrice <nicholas.rice119@gmail.com>
2020-03-13 10:31:49 -0700

    initial swatch

--- packages/fast-figma-plugin-msft/src/core/ui/components/test.ts
+++ packages/fast-figma-plugin-msft/src/core/ui/components/test.ts
@@ -1,9 +1,11 @@
 import { ThemeDesignerDrawer } from "./drawer";
 import { FASTDesignSystemProvider } from "@microsoft/fast-components";
 import { StealthButton } from "./stealth-button";
+import { Swatch } from "./swatch";
 
 /* tslint:disable */
 ThemeDesignerDrawer;
 FASTDesignSystemProvider;
 StealthButton;
+Swatch;
 /* tslint:enable */
