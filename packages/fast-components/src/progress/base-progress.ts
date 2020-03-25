60783c6dfaff07257840a06827d07b11d8dc7c0b
eljefe223 <jes@microsoft.com>
2020-03-25 12:11:02 -0700

    marked with public

--- packages/fast-components/src/progress/base-progress.ts
+++ packages/fast-components/src/progress/base-progress.ts
@@ -2,16 +2,16 @@ import { attr, FastElement, nullableNumberConverter } from "@microsoft/fast-elem
 
 export class BaseProgress extends FastElement {
     @attr({ converter: nullableNumberConverter })
-    value: number;
+    public value: number;
 
     @attr({ converter: nullableNumberConverter })
-    min: number;
+    public min: number;
 
     @attr({ converter: nullableNumberConverter })
-    max: number;
+    public max: number;
 
     @attr({ mode: "boolean" })
-    paused;
+    public paused;
     private pausedChanged(): void {
         this.paused ? this.classList.add("paused") : this.classList.remove("paused");
     }
