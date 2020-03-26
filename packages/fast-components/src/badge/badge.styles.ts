4847b11bf15856cba680cfc7df0a57d576c53d46
eljefe223 <jes@microsoft.com>
2020-03-26 13:02:27 -0700

    updated badge based on new spec

--- packages/fast-components/src/badge/badge.styles.ts
+++ packages/fast-components/src/badge/badge.styles.ts
@@ -5,56 +5,25 @@ export const BadgeStyles = css`
     ${display("inline-block")} :host {
         box-sizing: border-box;
         font-family: var(--body-font);
-    }
-
-    .badge {
-        border-radius: calc(var(--corner-radius) * 1px);
-        padding: calc(var(--design-unit) * 0.5px) calc(var(--design-unit) * 1px);
         ${/* Font size, weight, and line height are temporary - 
             replace when adaptive typography is figured out */ ""} font-size: 12px;
         font-weight: 400px;
         line-height: 18px;
     }
 
+    .badge {
+        border-radius: calc(var(--corner-radius) * 1px);
+        padding: calc(var(--design-unit) * 0.5px) calc(var(--design-unit) * 1px);
+    }
+
     :host(.circular) .badge {
         border-radius: 100px;
         padding: 0 calc(var(--design-unit) * 1px);
+        ${/* Need to work with Brian on width and height here */ ""} height: calc((var(--height-number) - (var(--design-unit) * 3)) * 1px);
+        min-width: calc((var(--height-number) - (var(--design-unit) * 3)) * 1px);
         display: flex;
         align-items: center;
         justify-content: center;
         box-sizing: border-box;
-    }
-
-    :host(.small) .badge {
-        ${/* Font size, weight, and line height are temporary - 
-            replace when adaptive typography is figured out */ ""} font-size: 10px;
-        line-height: 16px;
-    }
-
-    :host(.medium)) .badge {
-        ${/* Font size, weight, and line height are temporary - 
-            replace when adaptive typography is figured out */ ""} font-size: 12px;
-        line-height: 18px;
-    }
-
-    :host(.large) .badge {
-        ${/* Font size, weight, and line height are temporary - 
-            replace when adaptive typography is figured out */ ""} font-size: 14px;
-        line-height: 20px;
-    }
-
-    :host(.circular.small) .badge {
-        ${/* Need to work with Brian on width and height here */ ""} height: calc((var(--height-number) - (var(--design-unit) * 3)) * 1px);
-        min-width: calc((var(--height-number) - (var(--design-unit) * 3)) * 1px);
-    }
-
-    :host(.circular.medium) .badge {
-        ${/* Need to work with Brian on width and height here */ ""} height: calc((var(--height-number) - (var(--design-unit) * 1.5)) * 1px);
-        min-width: calc((var(--height-number) - (var(--design-unit) * 1.5)) * 1px);
-    }
-
-    :host(.circular.large) .badge {
-        ${/* Need to work with Brian on width and height here */ ""} height: calc(var(--height-number) * 1px);
-        min-width: calc(var(--height-number) * 1px);
     }
 `;
--- packages/fast-components/src/badge/badge.ts
+++ packages/fast-components/src/badge/badge.ts
@@ -1,13 +1,5 @@
 import { attr, FastElement } from "@microsoft/fast-element";
 
-export enum BadgeSize {
-    small = "small",
-    medium = "medium",
-    large = "large",
-}
-
-let lastBadgeSize: string;
-
 export class Badge extends FastElement {
     @attr({ attribute: "fill" })
     public fill: string;
@@ -18,31 +10,7 @@ export class Badge extends FastElement {
     @attr({ attribute: "color" })
     public color: string;
 
-    @attr
-    public size: BadgeSize;
-    private sizeChanged(): void {
-        this.classList.remove(lastBadgeSize);
-        switch (this.size) {
-            case BadgeSize.small:
-                this.classList.add(BadgeSize.small);
-                lastBadgeSize = BadgeSize.small;
-                break;
-            case BadgeSize.medium:
-                this.classList.add(BadgeSize.medium);
-                lastBadgeSize = BadgeSize.medium;
-                break;
-            case BadgeSize.large:
-                this.classList.add(BadgeSize.large);
-                lastBadgeSize = BadgeSize.large;
-                break;
-            default:
-                this.classList.add(BadgeSize.medium);
-                lastBadgeSize = BadgeSize.medium;
-                break;
-        }
-    }
-
-    @attr
+    @attr({ mode: "boolean" })
     public circular: boolean;
     private circularChanged(): void {
         this.circular
