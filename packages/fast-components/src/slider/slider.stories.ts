e57ed6c9d1dfc4a3b5ee64d71c061cb5cb271069
Mark Jones <marjon@microsoft.com>
2020-03-26 10:31:52 -0700

    tinkering until underlying framework bug fixed

--- packages/fast-components/src/slider/slider.stories.ts
+++ packages/fast-components/src/slider/slider.stories.ts
@@ -1,6 +1,8 @@
 import { FASTSlider } from ".";
 import { FASTDesignSystemProvider } from "../design-system-provider";
 import Examples from "./fixtures/base.html";
+import { STORY_RENDERED } from "@storybook/core-events";
+import addons from "@storybook/addons";
 
 // Prevent tree-shaking
 FASTSlider;
@@ -10,8 +12,14 @@ export default {
     title: "Slider",
 };
 
+addons.getChannel().addListener(STORY_RENDERED, (name: string) => {
+    if (name.toLowerCase().startsWith("slider")) {
+        setChangeHandler();
+    }
+});
+
 function setChangeHandler(): void {
-    document.querySelectorAll("fast-slider").forEach(el => {
+    document.querySelectorAll(".fastest-slider").forEach(el => {
         console.log("found another slider...");
         if (el instanceof FASTSlider) {
             console.log("attaching handler for onchange");
