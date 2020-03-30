bb96422824e81fcd068b4be7e03bcee2a3d1c124
nicholasrice <nicholas.rice119@gmail.com>
2020-03-30 13:10:26 -0700

    remove un-necessary file

--- packages/fast-components/src/checkbox/checkbox.ts
+++ packages/fast-components/src/checkbox/checkbox.ts
@@ -1,10 +1,8 @@
 import { attr, observable } from "@microsoft/fast-element";
 import { keyCodeSpace } from "@microsoft/fast-web-utilities";
 import { FormAssociated } from "../form-associated";
-import { consumer } from "../context";
 
 /* tslint:disable:member-ordering */
-@consumer
 export class Checkbox extends FormAssociated<HTMLInputElement> {
     @attr({ attribute: "readonly", mode: "boolean" })
     public readOnly: boolean; // Map to proxy element
