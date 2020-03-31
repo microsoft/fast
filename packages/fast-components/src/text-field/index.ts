4f07b3022f62d83adc84fe5bf4e33a308fb03186
nicholasrice <nicholas.rice119@gmail.com>
2020-03-31 10:46:14 -0700

    convert text-field

--- packages/fast-components/src/text-field/index.ts
+++ packages/fast-components/src/text-field/index.ts
@@ -2,6 +2,17 @@ import { customElement } from "@microsoft/fast-element";
 import { TextField } from "./text-field";
 import { TextFieldTemplate as template } from "./text-field.template";
 import { TextFieldStyles as styles } from "./text-field.styles";
+import { consumer } from "../design-system-provider/design-system-consumer";
+import {
+    neutralforegroundrest,
+    neutralfillinputrest,
+    neutraloutlinerest,
+    neutralfillinputhover,
+    neutraloutlinehover,
+    neutralfocus,
+    neutralfillrest,
+    neutralfillhover,
+} from "../styles/recipes";
 
 @customElement({
     name: "fast-text-field",
@@ -10,6 +21,18 @@ import { TextFieldStyles as styles } from "./text-field.styles";
     shadowOptions: {
         delegatesFocus: true,
     },
+})
+@consumer({
+    recipes: [
+        neutralforegroundrest,
+        neutralfillinputrest,
+        neutraloutlinerest,
+        neutralfillinputhover,
+        neutraloutlinehover,
+        neutralfocus,
+        neutralfillrest,
+        neutralfillhover,
+    ],
 })
 export class FASTTextField extends TextField {}
 export * from "./text-field.template";
