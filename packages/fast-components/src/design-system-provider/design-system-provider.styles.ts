8620c61830742715b6b1f57a066cc75159a3f2d7
nicholasrice <nicholas.rice119@gmail.com>
2020-03-31 10:41:58 -0700

    fix body font

--- packages/fast-components/src/design-system-provider/design-system-provider.styles.ts
+++ packages/fast-components/src/design-system-provider/design-system-provider.styles.ts
@@ -4,5 +4,6 @@ import { display } from "../styles";
 export const DesignSystemProviderStyles = css`
     ${display("block")} .inner {
         background-color: var(--background-color);
+        font-family: Segoe UI, sans-serif;
     }
 `;
