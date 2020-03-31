ed1a0e2688d7640fb0810a95e5a5ebd64380edad
nicholasrice <nicholas.rice119@gmail.com>
2020-03-31 10:34:57 -0700

    convert progress

--- packages/fast-components/src/progress/progress-ring/index.ts
+++ packages/fast-components/src/progress/progress-ring/index.ts
@@ -2,11 +2,20 @@ import { customElement } from "@microsoft/fast-element";
 import { BaseProgress } from "../base-progress";
 import { ProgressRingTemplate as template } from "./progress-ring.template";
 import { ProgressRingStyles as styles } from "./progress-ring.styles";
+import { consumer } from "../../design-system-provider/design-system-consumer";
+import {
+    neutralfillrest,
+    accentfillrest,
+    neutralforegroundhint,
+} from "../../styles/recipes";
 
 @customElement({
     name: "fast-progress-ring",
     template,
     styles,
+})
+@consumer({
+    recipes: [neutralfillrest, accentfillrest, neutralforegroundhint],
 })
 export class FASTProgressRing extends BaseProgress {}
 export * from "./progress-ring.template";
--- packages/fast-components/src/progress/progress/index.ts
+++ packages/fast-components/src/progress/progress/index.ts
@@ -2,11 +2,20 @@ import { customElement } from "@microsoft/fast-element";
 import { BaseProgress } from "../base-progress";
 import { ProgressTemplate as template } from "./progress.template";
 import { ProgressStyles as styles } from "./progress.styles";
+import { consumer } from "../../design-system-provider/design-system-consumer";
+import {
+    neutralfillrest,
+    accentfillrest,
+    neutralforegroundhint,
+} from "../../styles/recipes";
 
 @customElement({
     name: "fast-progress",
     template,
     styles,
+})
+@consumer({
+    recipes: [neutralfillrest, accentfillrest, neutralforegroundhint],
 })
 export class FASTProgress extends BaseProgress {}
 export * from "./progress.template";
