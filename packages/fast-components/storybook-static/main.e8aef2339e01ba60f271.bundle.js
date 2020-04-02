172aaaf993934a7d4bef18174ee81e709ae74c86
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2020-04-01 19:37:56 -0700

    udpate to patch security vulnerability dependency for jest

---
+++ packages/fast-components/storybook-static/main.e8aef2339e01ba60f271.bundle.js
@@ -0,0 +1,10 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [0],
+    {
+        372: function(n, o, p) {
+            p(373), (n.exports = p(486));
+        },
+        395: function(n, o) {},
+    },
+    [[372, 1, 2]],
+]);
