c924b75c104f25d94153d5a755f6b0adb0a47347
Chris Holt <chhol@microsoft.com>
2020-04-03 15:51:56 -0700

    fix: export missing components and styles

--- packages/fast-components/src/index.ts
+++ packages/fast-components/src/index.ts
@@ -1,9 +1,14 @@
 export * from "./badge";
+export * from "./card";
 export * from "./checkbox";
 export * from "./design-system-provider";
 export * from "./dialog";
+export * from "./divider";
 export * from "./name-tag";
 export * from "./progress";
 export * from "./switch";
 export * from "./text-area";
 export * from "./text-field";
+
+// export our style goodness
+export * from "./styles";
