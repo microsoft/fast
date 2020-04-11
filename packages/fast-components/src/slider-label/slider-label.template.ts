623b94ff17d4e5b72fc6ef656ca13fdc3a5c90a9
Mark Jones <marjon@microsoft.com>
2020-04-10 18:45:08 -0700

    fixed after running prettier

--- packages/fast-components/src/slider-label/slider-label.template.ts
+++ packages/fast-components/src/slider-label/slider-label.template.ts
@@ -2,24 +2,12 @@ import { html, ref, when } from "@microsoft/fast-element";
 import { SliderLabel } from "./slider-label";
 
 export const SliderLabelTemplate = html<SliderLabel>`
-    <div
-        ${ref("root")}
-        part="root"
-        class="root"
-        style=${x => x.positionStyle}
-    >
-    <div class="container">
-        ${when(
-            x => !x.hideMark,
-            html`
-            <div class="mark">
-            </div>    
-        `
-        )}
-        <div class="label">
-            <slot>
-            </slot>
+    <div ${ref("root")} part="root" class="root" style=${x => x.positionStyle}>
+        <div class="container">
+            ${when(x => !x.hideMark, html` <div class="mark"></div> `)}
+            <div class="label">
+                <slot> </slot>
+            </div>
         </div>
-    </div>
     </div>
 `;
