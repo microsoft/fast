3747bb894881016e8181727c8e37edb085d15f7f
nicholasrice <nicholas.rice119@gmail.com>
2020-04-22 15:56:52 -0700

    leverage template to apply classes and tabindex

--- packages/web-components/fast-components/src/flipper/flipper.ts
+++ packages/web-components/fast-components/src/flipper/flipper.ts
@@ -8,31 +8,10 @@ export enum FlipperDirection {
 export class Flipper extends FASTElement {
     @attr({ mode: "boolean" })
     public disabled: boolean;
-    public disabledChanged(): void {
-        this.disabled
-            ? this.classList.add("disabled")
-            : this.classList.remove("disabled");
-    }
 
     @attr({ attribute: "aria-hidden", mode: "fromView", converter: booleanConverter })
     public hiddenFromAT: boolean = true;
-    public hiddenFromATChanged(): void {
-        this.hiddenFromAT === false
-            ? this.setAttribute("tabindex", "0")
-            : this.setAttribute("tabindex", "-1");
-    }
 
     @attr
     public direction: FlipperDirection = FlipperDirection.next;
-    public directionChanged(
-        oldValue: FlipperDirection,
-        newValue: FlipperDirection
-    ): void {
-        if (this.direction) {
-            this.classList.remove(`${oldValue}`);
-            this.classList.add(`${newValue}`);
-        } else {
-            this.classList.remove(`${oldValue}`);
-        }
-    }
 }
