microsoft/fast-react

chore: add the creator app to sites

sites/fast-creator/app/components/theme-selector/index.tsx
@@ -1,0 +1,28 @@
+import React from "react";
+import { Label, Toggle } from "@microsoft/fast-components-react-msft";
+import { selectorStyle } from "../style";
+import { Theme, ThemeSelectorProps } from "./theme-selector.props";
+
+export const ThemeSelector: React.FC<ThemeSelectorProps> = ({
+    id,
+    theme,
+    onUpdateTheme,
+}: React.PropsWithChildren<ThemeSelectorProps>): React.ReactElement => {
+    return (
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <Label htmlFor={id} style={selectorStyle}>
+                Dark mode
+            </Label>
+            <Toggle
+                inputId={id}
+                onClick={onUpdateTheme}
+                selectedMessage={""}
+                unselectedMessage={""}
+                statusMessageId={"theme"}
+                selected={theme === Theme.dark}
+            />
+        </div>
+    );
+};
+
+export * from "./theme-selector.props";

sites/fast-creator/app/creator.props.ts
@@ -1,0 +1,95 @@
+import { ManagedClasses } from "@microsoft/fast-components-class-name-contracts-base";
+import { Direction } from "@microsoft/fast-web-utilities";
+import { DataDictionary, LinkedData } from "@microsoft/fast-tooling";
+import { CreatorClassNameContract } from "./creator.style";
+import { Theme } from "./components";
+import { LinkedDataStack } from "./msft-components/example-data";
+
+/**
+ * Data for a single view
+ */
+export interface ProjectFileView {
+    dataDictionary: DataDictionary<unknown>;
+}
+
+/**
+ * A project file is a culmination of all views which should contain the data for that view
+ */
+export interface ProjectFile {
+    /**
+     * The accent color
+     */
+    accentColor: string;
+
+    /**
+     * The direction of the viewer
+     */
+    direction: Direction;
+
+    /**
+     * The creator theme
+     */
+    theme: Theme;
+
+    /**
+     * The active dictionaryId to show
+     */
+    activeDictionaryId: string;
+
+    /**
+     * The active view
+     */
+    activeView: string;
+
+    /**
+     * The x position in the preview
+     */
+    xCoord: number;
+
+    /**
+     * The y position in the preview
+     */
+    yCoord: number;
+
+    /**
+     * The width of the preview
+     */
+    width: number;
+
+    /**
+     * The height of the preview
+     */
+    height: number;
+
+    /**
+     * The selected device for the preview
+     */
+    deviceId: string;
+
+    /**
+     * The views
+     */
+    views: {
+        [key: string]: ProjectFileView;
+    };
+
+    /**
+     * The stack of linked data that needs to be resolved
+     * Note: This should be null when it's used as a project file
+     */
+    linkedDataStack: LinkedDataStack[] | null;
+
+    /**
+     * The ditionary ids that correspond to the linked data
+     * Note: This should be null when it's used as a project file
+     */
+    linkedDataIdsForStack: LinkedData[] | null;
+}
+
+export type CreatorManagedClasses = ManagedClasses<CreatorClassNameContract>;
+
+export type CreatorHandledProps = CreatorManagedClasses;
+
+export type CreatorProps = CreatorHandledProps;
+
+export type CreatorState = ProjectFile;

sites/fast-creator/app/preview.tsx
@@ -1,0 +1,139 @@
+import { DesignSystem } from "@microsoft/fast-components-styles-msft";
+import React from "react";
+import Foundation, { HandledProps } from "@microsoft/fast-components-foundation-react";
+import manageJss, { DesignSystemProvider } from "@microsoft/fast-jss-manager-react";
+import {
+    DataDictionary,
+    DataMessageOutgoing,
+    InitializeMessageOutgoing,
+    mapDataDictionary,
+    MessageSystemNavigationTypeAction,
+    MessageSystemOutgoing,
+    MessageSystemType,
+    NavigationMessageOutgoing,
+    SchemaDictionary,
+    UpdateNavigationMessageIncoming,
+} from "@microsoft/fast-tooling";
+import { reactMapper } from "@microsoft/fast-tooling-react";
+import { Background } from "@microsoft/fast-components-react-msft";
+import { reactResolver } from "./utilities";
+import { componentDictionary } from "./msft-components/dictionary";
+import style from "./preview.style";
+import { Theme } from "./components";
+import { designSystemLinkedDataId } from "./creator";
+
+export interface PreviewState {
+    activeDictionaryId: string;
+    dataDictionary: DataDictionary<unknown> | void;
+    schemaDictionary: SchemaDictionary;
+    theme: Theme;
+}
+
+class Preview extends Foundation<{}, {}, PreviewState> {
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            activeDictionaryId: "",
+            dataDictionary: void 0,
+            schemaDictionary: {},
+            theme: Theme.light,
+        };
+    }
+
+    public render(): React.ReactNode {
+        if (this.state.dataDictionary !== undefined) {
+            return (
+                <Background
+                    value={
+                        (this.state.dataDictionary[0][designSystemLinkedDataId]
+                            .data as DesignSystem).backgroundColor
+                    }
+                    dir={
+                        (this.state.dataDictionary[0][designSystemLinkedDataId]
+                            .data as DesignSystem).direction
+                    }
+                >
+                    <DesignSystemProvider
+                        designSystem={
+                            this.state.dataDictionary[0][designSystemLinkedDataId].data
+                        }
+                    >
+                        {this.renderMappedComponents()}
+                    </DesignSystemProvider>
+                </Background>
+            );
+        }
+
+        return null;
+    }
+
+    public componentDidMount(): void {
+        window.addEventListener("message", this.handleMessage);
+    }
+
+    private renderMappedComponents(): React.ReactNode {
+        if (this.state.dataDictionary !== undefined) {
+            return mapDataDictionary({
+                dataDictionary: this.state.dataDictionary,
+                schemaDictionary: this.state.schemaDictionary,
+                mapper: reactMapper(componentDictionary),
+                resolver: reactResolver(
+                    this.state.activeDictionaryId,
+                    this.handleUpdateDictionaryId
+                ),
+            });
+        }
+
+        return null;
+    }
+
+    private handleMessage = (message: MessageEvent): void => {
+        let messageData: unknown;
+
+        try {
+            messageData = JSON.parse(message.data);
+        } catch (e) {
+            return;
+        }
+
+        if (messageData !== undefined) {
+            switch ((messageData as MessageSystemOutgoing).type) {
+                case MessageSystemType.initialize:
+                    this.setState({
+                        dataDictionary: (messageData as InitializeMessageOutgoing)
+                            .dataDictionary,
+                        schemaDictionary: (messageData as InitializeMessageOutgoing)
+                            .schemaDictionary,
+                    });
+                    break;
+                case MessageSystemType.data:
+                    this.setState({
+                        dataDictionary: (messageData as DataMessageOutgoing)
+                            .dataDictionary,
+                    });
+                    break;
+                case MessageSystemType.navigation:
+                    this.setState({
+                        activeDictionaryId: (messageData as NavigationMessageOutgoing)
+                            .activeDictionaryId,
+                    });
+                    break;
+            }
+        }
+    };
+
+    private handleUpdateDictionaryId = (dictionaryId: string): void => {
+        window.postMessage(
+            JSON.stringify({
+                type: MessageSystemType.navigation,
+                action: MessageSystemNavigationTypeAction.update,
+                activeDictionaryId: dictionaryId,
+                activeNavigationConfigId: "",
+            } as UpdateNavigationMessageIncoming),
+            "*"
+        );
+    };
+}
+
+export default manageJss(style)(Preview as React.ComponentType);

microsoft/fast

sites/fast-creator/app/components/theme-selector/index.tsx
@@ -1,0 +1,29 @@
+import React from "react";
+import { Label, Toggle } from "@microsoft/fast-components-react-msft";
+import { StandardLuminance } from "@microsoft/fast-components-styles-msft";
+import { selectorStyle } from "../style";
+import { ThemeSelectorProps } from "./theme-selector.props";
+
+export const ThemeSelector: React.FC<ThemeSelectorProps> = ({
+    id,
+    theme,
+    onUpdateTheme,
+}: React.PropsWithChildren<ThemeSelectorProps>): React.ReactElement => {
+    return (
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <Label htmlFor={id} style={selectorStyle}>
+                Dark mode
+            </Label>
+            <Toggle
+                inputId={id}
+                onClick={onUpdateTheme}
+                selectedMessage={""}
+                unselectedMessage={""}
+                statusMessageId={"theme"}
+                selected={theme === StandardLuminance.DarkMode}
+            />
+        </div>
+    );
+};
+
+export * from "./theme-selector.props";

sites/fast-creator/app/creator.props.ts
@@ -1,0 +1,95 @@
+import { ManagedClasses } from "@microsoft/fast-components-class-name-contracts-base";
+import { Direction } from "@microsoft/fast-web-utilities";
+import { DataDictionary, LinkedData } from "@microsoft/fast-tooling";
+import { StandardLuminance } from "@microsoft/fast-components-styles-msft";
+import { CreatorClassNameContract } from "./creator.style";
+import { LinkedDataStack } from "./msft-components/example-data";
+
+/**
+ * Data for a single view
+ */
+export interface ProjectFileView {
+    dataDictionary: DataDictionary<unknown>;
+}
+
+/**
+ * A project file is a culmination of all views which should contain the data for that view
+ */
+export interface ProjectFile {
+    /**
+     * The accent color
+     */
+    accentColor: string;
+
+    /**
+     * The direction of the viewer
+     */
+    direction: Direction;
+
+    /**
+     * The creator theme
+     */
+    theme: StandardLuminance;
+
+    /**
+     * The active dictionaryId to show
+     */
+    activeDictionaryId: string;
+
+    /**
+     * The active view
+     */
+    activeView: string;
+
+    /**
+     * The x position in the preview
+     */
+    xCoord: number;
+
+    /**
+     * The y position in the preview
+     */
+    yCoord: number;
+
+    /**
+     * The width of the preview
+     */
+    width: number;
+
+    /**
+     * The height of the preview
+     */
+    height: number;
+
+    /**
+     * The selected device for the preview
+     */
+    deviceId: string;
+
+    /**
+     * The views
+     */
+    views: {
+        [key: string]: ProjectFileView;
+    };
+
+    /**
+     * The stack of linked data that needs to be resolved
+     * Note: This should be null when it's used as a project file
+     */
+    linkedDataStack: LinkedDataStack[] | null;
+
+    /**
+     * The ditionary ids that correspond to the linked data
+     * Note: This should be null when it's used as a project file
+     */
+    linkedDataIdsForStack: LinkedData[] | null;
+}
+
+export type CreatorManagedClasses = ManagedClasses<CreatorClassNameContract>;
+
+export type CreatorHandledProps = CreatorManagedClasses;
+
+export type CreatorProps = CreatorHandledProps;
+
+export type CreatorState = ProjectFile;

sites/fast-creator/app/preview.tsx
@@ -1,0 +1,135 @@
+import { DesignSystem } from "@microsoft/fast-components-styles-msft";
+import React from "react";
+import Foundation, { HandledProps } from "@microsoft/fast-components-foundation-react";
+import manageJss, { DesignSystemProvider } from "@microsoft/fast-jss-manager-react";
+import {
+    DataDictionary,
+    DataMessageOutgoing,
+    InitializeMessageOutgoing,
+    mapDataDictionary,
+    MessageSystemNavigationTypeAction,
+    MessageSystemOutgoing,
+    MessageSystemType,
+    NavigationMessageOutgoing,
+    SchemaDictionary,
+    UpdateNavigationMessageIncoming,
+} from "@microsoft/fast-tooling";
+import { reactMapper } from "@microsoft/fast-tooling-react";
+import { Background } from "@microsoft/fast-components-react-msft";
+import { StandardLuminance } from "@microsoft/fast-components-styles-msft";
+import { reactResolver } from "./utilities";
+import { componentDictionary } from "./msft-components/dictionary";
+import style from "./preview.style";
+import { designSystemLinkedDataId } from "./creator";
+
+export interface PreviewState {
+    activeDictionaryId: string;
+    dataDictionary: DataDictionary<unknown> | void;
+    schemaDictionary: SchemaDictionary;
+    theme: StandardLuminance;
+}
+
+class Preview extends Foundation<{}, {}, PreviewState> {
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            activeDictionaryId: "",
+            dataDictionary: void 0,
+            schemaDictionary: {},
+            theme: StandardLuminance.LightMode,
+        };
+    }
+
+    public render(): React.ReactNode {
+        if (this.state.dataDictionary !== undefined) {
+            return (
+                <DesignSystemProvider
+                    designSystem={
+                        this.state.dataDictionary[0][designSystemLinkedDataId].data
+                    }
+                >
+                    <Background
+                        dir={
+                            (this.state.dataDictionary[0][designSystemLinkedDataId]
+                                .data as DesignSystem).direction
+                        }
+                    >
+                        {this.renderMappedComponents()}
+                    </Background>
+                </DesignSystemProvider>
+            );
+        }
+
+        return null;
+    }
+
+    public componentDidMount(): void {
+        window.addEventListener("message", this.handleMessage);
+    }
+
+    private renderMappedComponents(): React.ReactNode {
+        if (this.state.dataDictionary !== undefined) {
+            return mapDataDictionary({
+                dataDictionary: this.state.dataDictionary,
+                schemaDictionary: this.state.schemaDictionary,
+                mapper: reactMapper(componentDictionary),
+                resolver: reactResolver(
+                    this.state.activeDictionaryId,
+                    this.handleUpdateDictionaryId
+                ),
+            });
+        }
+
+        return null;
+    }
+
+    private handleMessage = (message: MessageEvent): void => {
+        let messageData: unknown;
+
+        try {
+            messageData = JSON.parse(message.data);
+        } catch (e) {
+            return;
+        }
+
+        if (messageData !== undefined) {
+            switch ((messageData as MessageSystemOutgoing).type) {
+                case MessageSystemType.initialize:
+                    this.setState({
+                        dataDictionary: (messageData as InitializeMessageOutgoing)
+                            .dataDictionary,
+                        schemaDictionary: (messageData as InitializeMessageOutgoing)
+                            .schemaDictionary,
+                    });
+                    break;
+                case MessageSystemType.data:
+                    this.setState({
+                        dataDictionary: (messageData as DataMessageOutgoing)
+                            .dataDictionary,
+                    });
+                    break;
+                case MessageSystemType.navigation:
+                    this.setState({
+                        activeDictionaryId: (messageData as NavigationMessageOutgoing)
+                            .activeDictionaryId,
+                    });
+                    break;
+            }
+        }
+    };
+
+    private handleUpdateDictionaryId = (dictionaryId: string): void => {
+        window.postMessage(
+            JSON.stringify({
+                type: MessageSystemType.navigation,
+                action: MessageSystemNavigationTypeAction.update,
+                activeDictionaryId: dictionaryId,
+                activeNavigationConfigId: "",
+            } as UpdateNavigationMessageIncoming),
+            "*"
+        );
+    };
+}
+
+export default manageJss(style)(Preview as React.ComponentType);