b27e0426cb87b3b308fb4274732abc472572a456
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-04-28 13:40:15 -0700

    chore: update explorer site to tooling v2 (#3026)

--- sites/fast-component-explorer/app/utilities/configs/context-menu.ts
+++ sites/fast-component-explorer/app/utilities/configs/context-menu.ts
@@ -1,41 +1,106 @@
 import {
     ContextMenu,
-    contextMenuItemSchema,
-    ContextMenuProps,
-    contextMenuSchema,
+    contextMenuItemSchema2,
+    contextMenuSchema2,
 } from "@microsoft/fast-components-react-msft";
 import Guidance from "../../.tmp/context-menu/guidance";
+import textSchema from "../../msft-component-helpers/text.schema";
 import { ComponentViewConfig } from "./data.props";
 
-const contextMenuConfig: ComponentViewConfig<ContextMenuProps> = {
-    schema: contextMenuSchema,
+const contextMenuConfig: ComponentViewConfig = {
+    schema: contextMenuSchema2,
     component: ContextMenu,
     guidance: Guidance,
     scenarios: [
         {
             displayName: "Basic",
-            data: {
-                children: [
-                    {
-                        id: contextMenuItemSchema.id,
-                        props: {
-                            children: "Context menu item 1",
+            dataDictionary: [
+                {
+                    root: {
+                        schemaId: contextMenuItemSchema2.id,
+                        data: {
+                            children: [
+                                {
+                                    id: "children0",
+                                },
+                                {
+                                    id: "children1",
+                                },
+                                {
+                                    id: "children2",
+                                },
+                            ],
                         },
                     },
-                    {
-                        id: contextMenuItemSchema.id,
-                        props: {
-                            children: "Context menu item 2",
+                    children0: {
+                        parent: {
+                            id: "root",
+                            dataLocation: "children",
+                        },
+                        schemaId: contextMenuItemSchema2.id,
+                        data: {
+                            children: [
+                                {
+                                    id: "children00",
+                                },
+                            ],
                         },
                     },
-                    {
-                        id: contextMenuItemSchema.id,
-                        props: {
-                            children: "Context menu item 3",
+                    children00: {
+                        parent: {
+                            id: "children0",
+                            dataLocation: "children",
+                        },
+                        schemaId: textSchema.id,
+                        data: "Context menu item 1",
+                    },
+                    children1: {
+                        parent: {
+                            id: "root",
+                            dataLocation: "children",
+                        },
+                        schemaId: contextMenuItemSchema2.id,
+                        data: {
+                            children: [
+                                {
+                                    id: "children10",
+                                },
+                            ],
                         },
                     },
-                ],
-            },
+                    children10: {
+                        parent: {
+                            id: "children1",
+                            dataLocation: "children",
+                        },
+                        schemaId: textSchema.id,
+                        data: "Context menu item 2",
+                    },
+                    children2: {
+                        parent: {
+                            id: "root",
+                            dataLocation: "children",
+                        },
+                        schemaId: contextMenuItemSchema2.id,
+                        data: {
+                            children: [
+                                {
+                                    id: "children20",
+                                },
+                            ],
+                        },
+                    },
+                    children20: {
+                        parent: {
+                            id: "children2",
+                            dataLocation: "children",
+                        },
+                        schemaId: textSchema.id,
+                        data: "Context menu item 3",
+                    },
+                },
+                "root",
+            ],
         },
     ],
 };
