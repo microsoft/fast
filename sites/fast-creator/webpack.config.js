0591220518f6f2decf1cbde8b7bf6adfa8252b6b
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-04-30 17:21:09 -0700

    chore: consolidate schema and component dictionaries (#3040)

--- sites/fast-creator/webpack.config.js
+++ sites/fast-creator/webpack.config.js
@@ -98,7 +98,7 @@ module.exports = (env, args) => {
                     "react-dnd-html5-backend"
                 ),
                 react: path.resolve(rootNodeModules, "react"),
-                "react-dnd": path.resolve(nodeModules, "react-dnd"),
+                "react-dnd": path.resolve(rootNodeModules, "react-dnd"),
                 "react-dom": path.resolve(rootNodeModules, "react-dom"),
             },
         },
