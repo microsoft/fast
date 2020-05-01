4d25439a3b1bfd15b3d6ab1d079d3a808cc9bdd8
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-04-30 17:21:09 -0700

    chore: consolidate schema and component dictionaries (#3040)

--- sites/fast-creator/app/msft-components/README.md
+++ sites/fast-creator/app/msft-components/README.md
@@ -3,6 +3,4 @@
 This folder contains:
 
 1. The unpublished schemas that correspond with the tooling v2 update
-2. A dictionary of msft react components - `dictionary.ts`
-3. Example data to include with the components when they are created - `example-data.ts`
-4. Wrapped msft react components - `wrapped-components.ts`
+2. Example data to include with the components when they are created - `example-data.ts`
--- sites/fast-creator/app/preview.tsx
+++ sites/fast-creator/app/preview.tsx
@@ -17,7 +17,7 @@ import { reactMapper, ViewerCustomAction } from "@microsoft/fast-tooling-react";
 import { Background } from "@microsoft/fast-components-react-msft";
 import { StandardLuminance } from "@microsoft/fast-components-styles-msft";
 import { reactResolver } from "./utilities";
-import { componentDictionary } from "./msft-components/dictionary";
+import { componentDictionary } from "./msft-components";
 import style from "./preview.style";
 import { designSystemLinkedDataId } from "./creator";
 
