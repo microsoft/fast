358b6c2b8c5e7a1b6ab5b54e28bdaa71e24e67e3
Jeff Smith <37851214+eljefe223@users.noreply.github.com>
2020-05-05 07:55:16 -0700

    chore: add new marketing website (#3060)
    * feat: allow linked data attached to other linked data to be added to a data dictionary (#3049)
    
    * add eisenberg as codeowner (#3055)
    
    * Sets up intial fast-frame project
    
    * restored website files and renamed new site
    
    Co-authored-by: Jane Chu <7559015+janechu@users.noreply.github.com>
    Co-authored-by: Aaron Wentzel <16669785+awentzel@users.noreply.github.com>

---
+++ sites/fast-website/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Require  html-webpack-plugin plugin
+const appDir = path.resolve(__dirname, "./src/app");
+const publicDir = path.resolve(__dirname, "./src/public");
+const outDir = path.resolve(__dirname, "./www");
+
+module.exports = {
+    entry: {
+        main: path.resolve(appDir, "index.ts"),
+    },
+    output: {
+        path: path.resolve(__dirname, "/dist"),
+        filename: "bundle.js", // Name of generated bundle after build
+        publicPath: "/", // public URL of the output directory when referenced in a browser
+    },
+    module: {
+        // where we defined file patterns and their loaders
+        rules: [
+            {
+                test: /.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+            },
+        ],
+    },
+    plugins: [
+        // Array of plugins to apply to build chunk
+        new HtmlWebpackPlugin({
+            title: "FAST website",
+            template: path.resolve(publicDir, "index.html"),
+            contentBase: outDir,
+        }),
+    ],
+    devServer: {
+        // configuration for webpack-dev-server
+        contentBase: "./src/public", //source of static assets
+        open: true,
+        port: 7700, // port to run dev-server
+    },
+};
