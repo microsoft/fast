8efa0c8f203edfae4564eb09baff70329b393f19
Kham Udom <khamu@microsoft.com>
2020-05-06 14:27:07 -0700

    changed short attribute to a <p> tag

--- sites/fast-website/src/app/components/section-header/section-header.template.ts
+++ sites/fast-website/src/app/components/section-header/section-header.template.ts
@@ -4,8 +4,8 @@ import { SectionHeader } from "./section-header";
 export const SectionHeaderTemplate = html<SectionHeader>`
     <template>
         <slot name="heading"></slot>
-        <h1 part="short">${x => x.short}</h1>
-        <p part="body" class="body">${x => x.body}</p>
+        <p part="short">${x => x.short}</p>
+        <p part="body">${x => x.body}</p>
         <slot name="button"></slot>
     </template>
 `;
