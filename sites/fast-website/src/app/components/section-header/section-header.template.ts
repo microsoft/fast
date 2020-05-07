dbff5ed59fd1eced6f2f751df952a1f112120132
Kham Udom <khamu@microsoft.com>
2020-05-07 13:58:11 -0700

    changed subheading to badge

--- sites/fast-website/src/app/components/section-header/section-header.template.ts
+++ sites/fast-website/src/app/components/section-header/section-header.template.ts
@@ -4,7 +4,7 @@ import { SectionHeader } from "./section-header";
 export const SectionHeaderTemplate = html<SectionHeader>`
     <template>
         <slot></slot>
-        <slot name="subheading"></slot>
+        <slot name="badge"></slot>
         <slot name="body"></slot>
         <slot name="action"></slot>
     </template>
