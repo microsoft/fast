ed61218f141ebc2916fde8ca0f1c4bfc885fe31f
Jeff Smith <37851214+eljefe223@users.noreply.github.com>
2020-05-11 09:21:57 -0700

    feat: adds preview component (#3097)
    * feat: adds preview component
    
    * removed style override

---
+++ sites/fast-website/src/app/components/fast-frame/fast-frame.styles.ts
@@ -0,0 +1,36 @@
+import { css } from "@microsoft/fast-element";
+import { display } from "@microsoft/fast-components";
+
+export const FastFrameStyles = css`
+    ${display("block")} :host {
+        flex-direction: column;
+        font-family: var(--body-font);
+        color: var(--neutral-foreground-rest);
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    .icon {
+        pointer-events: none;
+    }
+
+    .wrapper {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        height: 500px;
+    }
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        align-items: start;
+    }
+
+    .preview {
+        display: flex;
+        background: #c8c8c8;
+        align-items: center;
+        justify-content: center;
+    }
+`;
