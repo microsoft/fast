6eef5ef132c8564f4253a94b02644d6ffa929cf1
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-05-12 09:16:07 -0700

    feat: add navigational items on the page, toolbar and menu

--- sites/fast-tooling-examples/app/index.ts
+++ sites/fast-tooling-examples/app/index.ts
@@ -1,4 +1,14 @@
 /* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const style = require("./style.css");
+
+import {
+    FASTAnchor,
+    FASTBadge,
+    FASTDesignSystemProvider,
+} from "@microsoft/fast-components";
+
 // prevent tree shaking
 style;
+FASTAnchor;
+FASTBadge;
+FASTDesignSystemProvider;
