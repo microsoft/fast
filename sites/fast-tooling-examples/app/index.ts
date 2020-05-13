f14963f8c99bb7f16a71c6a52d6422ae196ee5ed
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-05-12 20:17:27 -0700

    address comments relating to RTL, fix build break

--- sites/fast-tooling-examples/app/index.ts
+++ sites/fast-tooling-examples/app/index.ts
@@ -1,11 +1,11 @@
-/* eslint-disable-next-line @typescript-eslint/no-var-requires */
-const style = require("./style.css");
-
 import {
     FASTAnchor,
     FASTBadge,
     FASTDesignSystemProvider,
 } from "@microsoft/fast-components";
+
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+const style = require("./style.css");
 
 // prevent tree shaking
 style;
