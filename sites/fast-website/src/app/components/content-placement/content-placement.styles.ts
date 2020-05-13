e569794f0e2f11e9db05e531ba47ddeaa6745f8a
SamanthaAO <samanthaaorcutt@gmail.com>
2020-05-13 13:09:01 -0700

    feat: create content-placement component for marketing site (#3105)
    * add hover-card component
    
    * add components to index.html, update component index exports to include hover-card, update main styles so that cards wrap
    
    * updated style to be more layout centric
    
    * rename component and update spacing in styles
    
    * change component name to content-placement
    
    * made style updates so all cards are the same height and body stretches
    
    * replace FAST prefix with site
    
    * update divider attribute to boolean mode and remove true for divider in public index
    
    * updated styles to work woth changes in feature and changes icon styling so there is no unneeded space when slot is not used
    
    * update color for links to match figma using design system

---
+++ sites/fast-website/src/app/components/content-placement/content-placement.styles.ts
@@ -0,0 +1,57 @@
+import { css } from "@microsoft/fast-element";
+import { display } from "@microsoft/fast-components";
+
+export const ContentPlacementStyles = css`
+    ${display("block")} :host {
+        contain: content;
+        font-family: var(--body-font);
+        box-sizing: border-box;
+    }
+
+    :host([hidden]) {
+        display: none;
+    }
+
+    fast-card {
+        background: transparent;
+        padding: 0 calc(var(--design-unit) * 5px) calc(var(--design-unit) * 3px);
+        text-align: left;
+        display: flex;
+        flex-direction: column;
+    }
+
+    fast-card ::slotted([slot="body"]) {
+        flex: 1 1 auto;
+    }
+
+    fast-card:hover .contentPlacement_footer__hoverVisible {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .contentPlacement_footer__hoverVisible {
+        visibility: hidden;
+    }
+
+    fast-card:hover .contentPlacement_footer__hoverVisible {
+        visibility: visible;
+    }
+
+    fast-card:hover .contentPlacement_divider {
+        visibility: visible;
+    }
+
+    fast-card:hover .contentPlacement_icon {
+        visibility: visible;
+    }
+
+    .contentPlacement_divider {
+        visibility: hidden;
+    }
+
+    fast-card .contentPlacement_icon {
+        visibility: hidden;
+        margin-top: calc(var(--design-unit) * 4px);
+    }
+`;
