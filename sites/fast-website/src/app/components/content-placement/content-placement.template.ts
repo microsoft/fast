1ab11e84594d5af4f4c12222a4ea750200b0496e
SamanthaAO <samanthaaorcutt@gmail.com>
2020-05-13 13:09:01 -0700

    feat: create content-placement component for marketing site (#3105)
    * add hover-card component
    
    * add components to index.html, update component index exports to include hover-card, update main styles so that cards wrap
    
    * updated style to be more layout centric
    
    * rename component and update spacing in styles
    
    * change component name to content-placement
    
    * made style updates so all cards are the same height and body stretches
    
    * replace FAST prefix with site
    
    * update divider attribute to boolean mode and remove true for divider in public index
    
    * updated styles to work woth changes in feature and changes icon styling so there is no unneeded space when slot is not used
    
    * update color for links to match figma using design system

---
+++ sites/fast-website/src/app/components/content-placement/content-placement.template.ts
@@ -0,0 +1,28 @@
+import { html, when } from "@microsoft/fast-element";
+import { ContentPlacement } from "./content-placement";
+
+export const ContentPlacementTemplate = html<ContentPlacement>`<fast-card>
+    <div 
+        class=${x =>
+            x.divider
+                ? ""
+                : "contentPlacement_icon"}
+    >
+        <slot name="icon"></slot>
+    </div>
+    <slot></slot>
+    <slot name="body"></slot>
+    ${when(
+        x => x.divider,
+        html`<fast-divider class="contentPlacement_divider"></fast-divider>`
+    )}
+    <div
+        class=${x =>
+            x.divider
+                ? "contentPlacement_footer__hoverVisible"
+                : "contentPlacement_footer__alwaysVisible"}
+    >
+        <slot name="action"></slot>
+        <slot name="compatibility"></slot>
+    </div>
+</fast-card> `;
