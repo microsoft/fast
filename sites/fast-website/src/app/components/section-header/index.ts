6c6c8ff773de98157e8a49f74b0ca4bd8bfee5d4
Kham Udom <37851220+khamudom@users.noreply.github.com>
2020-05-13 11:28:17 -0700

    feat: add navigation menu component (#3111)
    * create initial files for navigation component
    
    * add slotted for nav items
    
    * use the prefix site for the components, addressed rtl, removed adding role attribute
    
    * rebased. add start and end template, removed slotted directive and observable, add withBehavior to styles file

--- sites/fast-website/src/app/components/section-header/index.ts
+++ sites/fast-website/src/app/components/section-header/index.ts
@@ -4,11 +4,11 @@ import { SectionHeaderTemplate as template } from "./section-header.template";
 import { SectionHeaderStyles as styles } from "./section-header.styles";
 
 @customElement({
-    name: "fast-section-header",
+    name: "site-section-header",
     template,
     styles,
 })
-export class FASTSectionHeader extends SectionHeader {}
+export class SiteSectionHeader extends SectionHeader {}
 export * from "./section-header.template";
 export * from "./section-header.styles";
 export * from "./section-header";
