8728ebc60e84a99d16a9df48d9eddeb6b9e79899
nicholasrice <nicholas.rice119@gmail.com>
2020-05-14 10:31:04 -0700

    fix documentation

--- packages/web-components/fast-foundation/src/design-system-provider/design-system-property.ts
+++ packages/web-components/fast-foundation/src/design-system-provider/design-system-property.ts
@@ -7,13 +7,23 @@ import { DesignSystemProvider } from "./design-system-provider.js";
 /**
  * Decorator to declare a property as a design-system property.
  * Accepts a config object with the following:
- *
+ * 
  * default:
  * The default value of the property. Will be assigned when the use-defaults attribute is used.
  *
+ * attribute?:
+ * The HTML attribute to map the property to - defaults to the property name.
+ * 
+ * mode?:
+ * The attr mode - see https://github.com/microsoft/fast-dna/blob/master/packages/web-components/fast-element/docs/guide/building-components.md#customizing-attributes
+ * Defaults to "fromView"
+ * 
+ * converter?:
+ * The attr converter - see https://github.com/microsoft/fast-dna/blob/master/packages/web-components/fast-element/docs/guide/building-components.md#customizing-attributes
+ *
  * cssCustomProperty?:
  * An optional property to control the name of the css custom property being created.
- * If omitted, the css custom property will share a name with the decorated property name.
+ * If omitted, the css custom property will share a name with attribute if specified, otherwise the property name being decorated.
  * If assigned a false value, no css custom property will be created.
  */
 export function designSystemProperty<T extends DesignSystemProvider>(
