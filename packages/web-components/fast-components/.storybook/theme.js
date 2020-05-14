542848081e73a45ef6958f1d7cb0947ac61838a9
Chris Holt <chhol@microsoft.com>
2020-05-14 10:22:54 -0700

    update storybook theme to include custom css to ensure accurate visuals

---
+++ packages/web-components/fast-components/.storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+    base: "dark",
+    colorPrimary: "#F33378",
+    appContentBg: "#181818",
+    textColor: "#FFF",
+    brandTitle: "FAST components storybook",
+});
