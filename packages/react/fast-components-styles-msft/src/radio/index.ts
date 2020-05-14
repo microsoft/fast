860b3bd6c69c3706aa55f266a49972ce4993ea28
Brian Heston <47367562+bheston@users.noreply.github.com>
2020-05-14 16:46:58 -0700

    fix: update colors for 3:1 accessibility on checkbox, radio, and toggle (#3073)
    Updated colors for 3:1 accessibility on checkbox, radio, and toggle
    Updated to support external focus indicator

--- packages/react/fast-components-styles-msft/src/radio/index.ts
+++ packages/react/fast-components-styles-msft/src/radio/index.ts
@@ -14,11 +14,10 @@ import {
     neutralFillInputActive,
     neutralFillInputHover,
     neutralFillInputRest,
-    neutralFocus,
     neutralForegroundRest,
-    neutralOutlineActive,
-    neutralOutlineHover,
-    neutralOutlineRest,
+    neutralOutlineContrastActive,
+    neutralOutlineContrastHover,
+    neutralOutlineContrastRest,
 } from "../utilities/color";
 import {
     densityCategorySwitch,
@@ -27,15 +26,17 @@ import {
 } from "../utilities/density";
 import { applyDisabledState } from "../utilities/disabled";
 import { applyScaledTypeRamp } from "../utilities/typography";
-import { designUnit, outlineWidth } from "../utilities/design-system";
+import { backgroundColor, designUnit, outlineWidth } from "../utilities/design-system";
 import { applyCursorDisabled, applyCursorPointer } from "../utilities/cursor";
 import {
     HighContrastColor,
+    highContrastDoubleOuterFocus,
     highContrastHighlightBackground,
     highContrastOptOutProperty,
     highContrastSelectedBackground,
     highContrastSelector,
 } from "../utilities/high-contrast";
+import { doubleOuterFocus } from "../patterns/input-field";
 
 const inputSize: DesignSystemResolver<string> = toPx(
     add(divide(heightNumber(), 2), designUnit)
@@ -68,14 +69,14 @@ const styles: ComponentStyles<RadioClassNameContract, DesignSystem> = {
         border: format(
             "{0} solid {1}",
             toPx<DesignSystem>(outlineWidth),
-            neutralOutlineRest
+            neutralOutlineContrastRest
         ),
         "&:enabled": {
             ...applyCursorPointer(),
         },
         "&:hover:enabled": {
             background: neutralFillInputHover,
-            "border-color": neutralOutlineHover,
+            "border-color": neutralOutlineContrastHover,
             [highContrastSelector]: {
                 background: "transparent",
                 border: format(
@@ -85,19 +86,13 @@ const styles: ComponentStyles<RadioClassNameContract, DesignSystem> = {
                 ),
             },
         },
-        "&:active": {
+        "&:active:enabled": {
             background: neutralFillInputActive,
-            "border-color": neutralOutlineActive,
+            "border-color": neutralOutlineContrastActive,
         },
         ...applyFocusVisible({
-            "box-shadow": format<DesignSystem>("0 0 0 1px {0} inset", neutralFocus),
-            "border-color": neutralFocus,
-            [highContrastSelector]: {
-                "box-shadow": format<DesignSystem>(
-                    "0 0 0 1px {0}",
-                    () => HighContrastColor.buttonText
-                ),
-            },
+            ...doubleOuterFocus,
+            ...highContrastDoubleOuterFocus,
         }),
         [highContrastSelector]: {
             ...highContrastOptOutProperty,
@@ -142,7 +137,7 @@ const styles: ComponentStyles<RadioClassNameContract, DesignSystem> = {
     radio__checked: {
         "& $radio_stateIndicator": {
             "&::before": {
-                background: neutralForegroundRest,
+                background: backgroundColor,
                 [highContrastSelector]: {
                     background: HighContrastColor.selectedBackground,
                 },
@@ -152,9 +147,14 @@ const styles: ComponentStyles<RadioClassNameContract, DesignSystem> = {
             ...highContrastSelectedBackground,
         },
         "& $radio_input": {
+            "background-color": neutralOutlineContrastRest,
             ...highContrastSelectedBackground,
-            "&:hover": {
+            "&:hover:enabled": {
+                "background-color": neutralOutlineContrastHover,
                 ...highContrastHighlightBackground,
+            },
+            "&:active:enabled": {
+                "background-color": neutralOutlineContrastActive,
             },
         },
     },
