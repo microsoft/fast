f5a147ef6c105ab84deaf409c179d67c7b00b6dd
nicholasrice <nicholas.rice119@gmail.com>
2020-05-14 09:40:34 -0700

    update decorator to create attribute or observable

--- packages/web-components/fast-foundation/src/design-system-provider/design-system-property.ts
+++ packages/web-components/fast-foundation/src/design-system-provider/design-system-property.ts
@@ -1,3 +1,8 @@
+import {
+    DecoratorAttributeConfiguration,
+    observable,
+    attr,
+} from "@microsoft/fast-element";
 import { DesignSystemProvider } from "./design-system-provider.js";
 /**
  * Decorator to declare a property as a design-system property.
@@ -12,20 +17,34 @@ import { DesignSystemProvider } from "./design-system-provider.js";
  * If assigned a false value, no css custom property will be created.
  */
 export function designSystemProperty<T extends DesignSystemProvider>(
-    config: DesignSystemPropertyDeclarationConfig
+    config: DecoratorDesignSystemPropertyConfiguration
 ): (source: T, property: string) => void {
     const decorator = (
         source: T,
         prop: string,
-        config: DesignSystemPropertyDeclarationConfig
+        config: DecoratorDesignSystemPropertyConfiguration
     ) => {
+        const { cssCustomProperty, attribute } = config;
+
         if (!source.designSystemProperties) {
             source.designSystemProperties = {};
         }
 
+        if (attribute === false) {
+            observable(source, prop);
+        } else {
+            attr(config as DecoratorAttributeConfiguration)(source, prop);
+        }
+
         source.designSystemProperties[prop] = {
             cssCustomProperty:
-                config.cssCustomProperty === void 0 ? prop : config.cssCustomProperty,
+                cssCustomProperty === false
+                    ? false
+                    : typeof cssCustomProperty === "string"
+                    ? cssCustomProperty
+                    : typeof attribute === "string"
+                    ? attribute
+                    : prop,
             default: config.default,
         };
     };
@@ -35,7 +54,14 @@ export function designSystemProperty<T extends DesignSystemProvider>(
     };
 }
 
-export interface DesignSystemPropertyDeclarationConfig {
+export interface DecoratorDesignSystemPropertyConfiguration
+    extends Omit<DecoratorAttributeConfiguration, "attribute"> {
+    attribute?: string | false;
     cssCustomProperty?: string | false;
     default: any;
 }
+
+export type DesignSystemPropertyDeclarationConfig = Pick<
+    DecoratorDesignSystemPropertyConfiguration,
+    "cssCustomProperty" | "default"
+>;
