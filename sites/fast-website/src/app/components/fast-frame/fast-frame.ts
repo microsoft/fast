6c70c782c9fc3afabb16b7e23f9c16a0ab9d921f
Jeff Smith <37851214+eljefe223@users.noreply.github.com>
2020-05-14 12:38:12 -0700

    feat: add accent color and background color selection (#3123)
    * feat: add color pickers and preview
    
    * formatting
    
    * added dark / light mode
    
    * add prettier
    
    * ran prettier
    
    * saved last index for selecting background
    
    * addressed Rob's comments
    
    * ran prettier
    
    * addressed comments

--- sites/fast-website/src/app/components/fast-frame/fast-frame.ts
+++ sites/fast-website/src/app/components/fast-frame/fast-frame.ts
@@ -1,3 +1,69 @@
-import { FASTElement } from "@microsoft/fast-element";
+import { FASTElement, attr, observable } from "@microsoft/fast-element";
+import { FASTRadio } from "@microsoft/fast-components";
+import { SiteColorSwatch } from "../color-swatch";
 
-export class FastFrame extends FASTElement {}
+export class FastFrame extends FASTElement {
+    @attr({ attribute: "accent-color" })
+    public accentColor: string = "#F33378";
+
+    @attr({ attribute: "background-color" })
+    public backgroundColor: string = "#1F1F1F";
+
+    @attr
+    public darkMode: boolean = true;
+
+    @observable
+    public backgroundPalette: string[] = [
+        "#1F1F1F",
+        "#2B2B2B",
+        "#333333",
+        "#3B3B3B",
+        "#424242",
+    ];
+
+    @observable
+    public accentPalette: string[] = [
+        "#F33378",
+        "#F34733",
+        "#10A7B5",
+        "#109B82",
+        "#E1A054",
+    ];
+
+    private darkPallette: string[] = this.backgroundPalette;
+
+    private lastSelectedIndex: number = 0;
+
+    public accentChangeHandler = (e: any): void => {
+        const element: HTMLInputElement = e.target;
+        if (element.checked) {
+            this.accentColor = e.target.value;
+        }
+    };
+
+    public backgroundChangeHandler = (e: any): void => {
+        const element: HTMLInputElement = e.target;
+        if (element.checked) {
+            this.backgroundColor = e.target.value;
+            this.lastSelectedIndex = Array.from(this.backgroundPalette).indexOf(
+                e.target.value
+            );
+        }
+    };
+
+    public themeChange = (e: any): void => {
+        this.darkMode = !this.darkMode;
+        if (!this.darkMode) {
+            this.backgroundPalette = [
+                "#FFFFFF",
+                "#F0F0F0",
+                "#DEDEDE",
+                "#D6D6D6",
+                "#C4C4C4",
+            ];
+        } else {
+            this.backgroundPalette = this.darkPallette;
+        }
+        this.backgroundColor = this.backgroundPalette[this.lastSelectedIndex];
+    };
+}
