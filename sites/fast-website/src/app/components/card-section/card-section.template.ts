ffbaf42794a650f70f2c5437999507854092cd88
Michael Benson <v-mbenso@microsoft.com>
2020-05-14 12:49:50 -0700

    fix file formatting

--- sites/fast-website/src/app/components/card-section/card-section.template.ts
+++ sites/fast-website/src/app/components/card-section/card-section.template.ts
@@ -1,4 +1,4 @@
 import { html } from "@microsoft/fast-element";
 import { CardSection } from "./card-section";
 
-export const CardSectionTemplate = html<CardSection>` <slot></slot> `;
+export const CardSectionTemplate = html<CardSection>`<slot></slot>`;
