2c49f6044cb1f4c7803182e92f43506e94b2e0b6
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-05-15 16:40:20 -0700

    chore: remove unused styles (#3142)

--- sites/fast-creator/app/creator.style.ts
+++ sites/fast-creator/app/creator.style.ts
@@ -1,17 +1,8 @@
+import { ComponentStyles, CSSRules } from "@microsoft/fast-jss-manager-react";
 import {
-    ComponentStyles,
-    CSSRules,
-    ManagedClasses,
-} from "@microsoft/fast-jss-manager-react";
-import {
-    applyCornerRadius,
     DesignSystem,
-    neutralFillActive,
-    neutralFillHover,
-    neutralFillRest,
     neutralLayerFloating,
     neutralLayerL2,
-    neutralLayerL3,
 } from "@microsoft/fast-components-styles-msft";
 
 export function applyScrollbarStyle(): CSSRules<{}> {
@@ -32,22 +23,7 @@ export function applyScrollbarStyle(): CSSRules<{}> {
     };
 }
 
-export interface CreatorClassNameContract {
-    creator?: string;
-    creator_uploadInput?: string;
-    creator_colorPicker?: string;
-    creator_devToolsPanel?: string;
-    creator_navigationPanel?: string;
-    creator_navigationPanelRegion?: string;
-    creator_navigationPanelContent?: string;
-    creator_paneTitleContainer?: string;
-    creator_propertiesPanel?: string;
-    creator_toolbar?: string;
-    creator_viewerControlRegion?: string;
-    creator_viewerPanel?: string;
-}
-
-const style: ComponentStyles<CreatorClassNameContract, DesignSystem> = {
+const style: ComponentStyles<{}, DesignSystem> = {
     "@font-face": [
         {
             fontFamily: "SegoeUIVF",
@@ -69,84 +45,6 @@ const style: ComponentStyles<CreatorClassNameContract, DesignSystem> = {
             padding: "0",
             margin: "0",
         },
-    },
-    creator: {},
-    creator_uploadInput: {
-        position: "absolute",
-        width: "20px",
-        overflow: "hidden",
-        opacity: "0",
-    },
-    creator_colorPicker: {
-        background: neutralFillRest(neutralLayerL2),
-        border: "none",
-        width: "23px",
-        height: "22px",
-        ...applyCornerRadius(),
-        "&:hover": {
-            background: neutralFillHover(neutralLayerL2),
-        },
-        "&:active": {
-            background: neutralFillActive(neutralLayerL2),
-        },
-        "&:focus": {
-            outline: "none",
-        },
-    },
-    creator_devToolsPanel: {
-        width: "100%",
-        boxShadow:
-            "0px 0.6px 1.8px rgba(0, 0, 0, 0.11) inset, 0px 3.2px 7.2px rgba(0, 0, 0, 0.13) inset",
-    },
-    creator_navigationPanel: {
-        overflowY: "auto",
-        overflowX: "hidden",
-        background: neutralLayerL3,
-        boxShadow:
-            "-0.6px 0px 1.8px rgba(0, 0, 0, 0.11) inset, -3.2px 0px 7.2px rgba(0, 0, 0, 0.13) inset",
-    },
-    creator_navigationPanelRegion: {
-        height: "calc(100% - 39px)",
-    },
-    creator_navigationPanelContent: {
-        height: "calc(100% - 32px)",
-        overflow: "auto",
-        ...applyScrollbarStyle(),
-    },
-    creator_paneTitleContainer: {
-        height: "32px",
-        display: "flex",
-        boxSizing: "border-box",
-        alignItems: "center",
-        justifyContent: "space-between",
-        paddingLeft: "10px",
-    },
-    creator_propertiesPanel: {
-        background: neutralLayerL3,
-        boxShadow:
-            "0.6px 0px 1.8px rgba(0, 0, 0, 0.11) inset, 3.2px 0px 7.2px rgba(0, 0, 0, 0.13) inset",
-        overflow: "auto",
-        ...applyScrollbarStyle(),
-    },
-    creator_viewerPanel: {
-        width: "100%",
-        height: "calc(100% - 32px)",
-        overflow: "auto",
-        ...applyScrollbarStyle(),
-    },
-    creator_viewerControlRegion: {
-        display: "flex",
-        alignItems: "center",
-    },
-    creator_toolbar: {
-        padding: "0 8px",
-        height: "32px",
-        display: "flex",
-        verticalAlign: "middle",
-        alignItems: "center",
-        background: neutralLayerL2,
-        boxShadow:
-            "0px -0.6px 1.8px rgba(0, 0, 0, 0.11) inset, 0px -3.2px 7.2px rgba(0, 0, 0, 0.13) inset",
     },
 };
 
