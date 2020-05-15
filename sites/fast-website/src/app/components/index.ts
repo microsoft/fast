c55503d573604681b29d0956b72fbd69ebd8949f
Michael Benson <v-mbenso@microsoft.com>
2020-05-15 11:43:44 -0700

    fix import and closing tags

--- sites/fast-website/src/app/components/index.ts
+++ sites/fast-website/src/app/components/index.ts
@@ -4,5 +4,6 @@ export { SiteSectionHeader } from "./section-header";
 export { FastFrame } from "./fast-frame";
 export { SiteNavigation } from "./navigation";
 export { SiteContentPlacement } from "./content-placement";
+export { SiteContentPlacementContainer } from "./content-placement-container";
 export { SiteFeatureCard } from "./feature-card";
 export { SiteCardSection } from "./card-section";
