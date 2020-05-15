250336ddf33e0492eea6a22d48bab87270f2c195
SamanthaAO <samanthaaorcutt@gmail.com>
2020-05-15 09:35:39 -0700

    feat: create content-placement-container to generate content from data files (#3134)
    * add content-placement-container
    
    * run lint, alphabatize data, update display in styles
    
    * run prettier
    
    * remove unused imports
    
    * fix issue with box shadow being blocked from overflowing container

---
+++ sites/fast-website/src/app/data/community.data.ts
@@ -0,0 +1,38 @@
+export interface CommunityContentPlacementData {
+    actionLink: string;
+    actionText: string;
+    body: string;
+    header: string;
+    iconAlt: string;
+    iconSrc: string;
+}
+
+export const communityContentPlacementData: CommunityContentPlacementData[] = [
+    {
+        actionLink: "https://discord.gg/FcSNfg4",
+        actionText: "Join the Discord Chat",
+        body:
+            "Join our active community on Discord. Follow the latest updates and contributions, ask questions, give feedback, or keep up on our reading list.",
+        header: "Discord",
+        iconAlt: "Discord",
+        iconSrc: "https://via.placeholder.com/16",
+    },
+    {
+        actionLink: "https://twitter.com/fast_dna",
+        actionText: "Follow us on Twitter",
+        body:
+            "Follow along as we share out the latest happenings on Twitter. You will find important updates, announcements, and sneak peeks.",
+        header: "Twitter",
+        iconAlt: "Twitter",
+        iconSrc: "https://via.placeholder.com/16",
+    },
+    {
+        actionLink: "https://github.com/microsoft/fast-dna",
+        actionText: "Get Started on Github",
+        body:
+            "Explore the FAST repository on Github and try out our components, utilities, and tools. Or, mix-and-match with your own solutions.",
+        header: "Github",
+        iconAlt: "Github",
+        iconSrc: "https://via.placeholder.com/16",
+    },
+];
