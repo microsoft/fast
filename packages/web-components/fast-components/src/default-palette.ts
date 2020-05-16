fc3c6c919a6f7c1e359d0d4280fe9129ef1267d3
eljefe223 <jes@microsoft.com>
2020-05-16 08:48:59 -0700

    revert fast component chnage

--- packages/web-components/fast-components/src/default-palette.ts
+++ packages/web-components/fast-components/src/default-palette.ts
@@ -96,7 +96,7 @@ export const neutralPalette: string[] = [
     "#151515",
     "#121212",
     "#101010",
-    "#000000"
+    "#000000",
 ];
 export const accentPalette: string[] = [
     "#FFFFFF",
@@ -192,5 +192,5 @@ export const accentPalette: string[] = [
     "#260511",
     "#21040E",
     "#1C030C",
-    "#000000"
+    "#000000",
 ];
