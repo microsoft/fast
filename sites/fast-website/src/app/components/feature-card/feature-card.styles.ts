231f75b8edd56eac6e2d2c44a512b5fee2e4708f
Michael Benson <freefaller@gmail.com>
2020-05-15 17:49:56 -0700

    feat: add feature-card and card-section components (#3119)
    * Add feature card component
    
    * Add feature card section
    
    * Add feature card section to index.html
    
    * Update card styles
    
    * change links slot to footer
    
    * Add neutralForegroundRestBehavior to feature-card
    
    * Remove unused css custom property
    
    * Add ::before separator element to cards
    
    * feat: add accent color and background color selection (#3123)
    
    * feat: add color pickers and preview
    
    * formatting
    
    * added dark / light mode
    
    * add prettier
    
    * ran prettier
    
    * saved last index for selecting background
    
    * addressed Rob's comments
    
    * ran prettier
    
    * addressed comments
    
    * styling fixes
    
    * fix file formatting
    
    * fix import
    
    * formatting issue
    
    * formatting
    
    * fix tabs to spaces
    
    * change selector from h5 to :first-child
    
    * Append Site prefix
    
    * make feature-card responsive
    
    * remove style property
    
    * fix import and closing tags
    
    * remove fast-anchor color
    
    * refactor card elements
    
    * modify class extension
    
    Co-authored-by: Michael Benson <v-mbenso@microsoft.com>
    Co-authored-by: Jeff Smith <37851214+eljefe223@users.noreply.github.com>

---
+++ sites/fast-website/src/app/components/feature-card/feature-card.styles.ts
@@ -0,0 +1,77 @@
+import { css } from "@microsoft/fast-element";
+import { display, neutralForegroundRestBehavior } from "@microsoft/fast-components";
+
+export const FeatureCardStyles = css`
+    ${display("grid")} :host {
+        contain: layout;
+        grid-template-columns: repeat(3, minmax(150px, 1fr));
+        grid-template-areas: "header main main";
+        color: inherit;
+        box-sizing: border-box;
+        padding: 20px;
+        box-shadow: unset;
+    }
+
+    :host::before {
+        content: "";
+        display: block;
+        background-color: currentColor;
+        position: fixed;
+        height: 1px;
+        width: 93%;
+        left: 20px;
+        top: 0;
+    }
+
+    :host(:hover) ::slotted(fast-anchor) {
+        opacity: 1;
+    }
+
+    :host(:hover)::before {
+        opacity: 0;
+    }
+
+    :host(:hover) ::slotted(:first-child) {
+        color: var(--accent-fill-rest);
+    }
+
+    @media screen and (max-width: 540px) {
+        ${display("grid")} :host {
+            grid-template-columns: repeat(3, minmax(50px, 1fr));
+            grid-template-areas:
+                "header header header"
+                "main main main";
+        }
+    }
+
+    .card_heading {
+        grid-area: header;
+        padding-bottom: 10px;
+    }
+
+    .card_body {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        grid-area: main;
+    }
+
+    ::slotted(h4) {
+        font-size: 20px;
+        margin: 0;
+    }
+
+    ::slotted(:first-child) {
+        font-weight: bold;
+        margin: 0 0 10px 0;
+    }
+
+    ::slotted(p) {
+        margin: 0;
+    }
+
+    ::slotted(fast-anchor) {
+        margin-right: 20px;
+        opacity: 0;
+    }
+`.withBehaviors(neutralForegroundRestBehavior);
