9ddc2a3dcc5b8daaa3e12d9c8e579b66b972c192
Michael Benson <freefaller@gmail.com>
2020-05-15 17:49:56 -0700

    feat: add feature-card and card-section components (#3119)
    * Add feature card component
    
    * Add feature card section
    
    * Add feature card section to index.html
    
    * Update card styles
    
    * change links slot to footer
    
    * Add neutralForegroundRestBehavior to feature-card
    
    * Remove unused css custom property
    
    * Add ::before separator element to cards
    
    * feat: add accent color and background color selection (#3123)
    
    * feat: add color pickers and preview
    
    * formatting
    
    * added dark / light mode
    
    * add prettier
    
    * ran prettier
    
    * saved last index for selecting background
    
    * addressed Rob's comments
    
    * ran prettier
    
    * addressed comments
    
    * styling fixes
    
    * fix file formatting
    
    * fix import
    
    * formatting issue
    
    * formatting
    
    * fix tabs to spaces
    
    * change selector from h5 to :first-child
    
    * Append Site prefix
    
    * make feature-card responsive
    
    * remove style property
    
    * fix import and closing tags
    
    * remove fast-anchor color
    
    * refactor card elements
    
    * modify class extension
    
    Co-authored-by: Michael Benson <v-mbenso@microsoft.com>
    Co-authored-by: Jeff Smith <37851214+eljefe223@users.noreply.github.com>

---
+++ sites/fast-website/src/app/components/card-section/card-section.styles.ts
@@ -0,0 +1,27 @@
+import { css } from "@microsoft/fast-element";
+import { display } from "@microsoft/fast-components";
+
+export const CardSectionStyles = css`
+    :host {
+        --flow: column;
+    }
+
+    @media screen and (max-width: 1000px) {
+        :host {
+            --flow: row;
+        }
+    }
+
+    ${display("grid")} :host {
+        grid-template-rows: repeat(4, min-content);
+        grid-auto-flow: var(--flow);
+    }
+
+    :host(:hover) ::slotted(site-feature-card) {
+        filter: blur(1px);
+    }
+
+    :host ::slotted(site-feature-card:hover) {
+        filter: blur(0);
+    }
+`;
