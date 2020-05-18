e56f7ef625abd52643b1867055671ba0776558d1
khamudom <khamu@microsoft.com>
2020-05-18 13:57:24 -0700

    put links in ul and li. add media query and responsive styles. removed styles for start and end template in navigation component.

--- sites/fast-website/src/app/components/navigation/navigation.styles.ts
+++ sites/fast-website/src/app/components/navigation/navigation.styles.ts
@@ -4,16 +4,10 @@ import { display } from "@microsoft/fast-foundation";
 
 export const NavigationStyles = css`
     ${display("flex")} :host {
-        justify-content: space-between;
+        contain: content;
         font-family: var(--body-font);
         color: var(--neutral-foreground-rest);
         box-sizing: border-box;
         width: 100%;
-    }
-    .start {
-        margin-inline-end: auto;
-    }
-    .end {
-        margin-inline-start: auto;
     }
 `.withBehaviors(neutralForegroundRestBehavior);
