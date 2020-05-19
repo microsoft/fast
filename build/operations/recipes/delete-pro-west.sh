ce62135b7fb8989b4358c13a143334613c0e470e
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2020-05-19 16:36:35 -0700

    update to bring deletion insync across regions

--- build/operations/recipes/delete-pro-west.sh
+++ build/operations/recipes/delete-pro-west.sh
@@ -9,6 +9,10 @@ This will delete all resources for Production in West Region
 # GLOBAL Configurations
     source config.sh
 
+    # Web Apps: valid subdomains to be created
+    # declare -a names=("app" "color" "create" "explore" "motion" "www")
+    declare -a names=("app")
+
     # Product: valid options {fast}
     product=fast
 
@@ -20,10 +24,24 @@ This will delete all resources for Production in West Region
 
     # Resource group: valid options {fast-westus-rg, fast-eastus-rg}
     resource_group=$product-$location-rg
-
+  
+    # DNS Zone
+    dns_zone=fast.design
 
 ## SHELL Arguments
 source inputs.sh --debug true --subscription $subscription --location $location --resource-group $resource_group
+
+# DELETE CNAME record
+for name in ${names[@]}; do
+    
+    # Compose names
+    dns_cname=$name.$dns_zone
+    web_app_name=$name-$location_abbr-app
+    
+    echo "deleting cnames for [$name] ..."
+    az network dns record-set cname remove-record --resource-group "fast-ops-rg" --zone-name $dns_zone --record-set-name $name --cname $dns_cname
+
+done
 
 # DELETE Resource group and all child services
 az group delete --name $resource_group
