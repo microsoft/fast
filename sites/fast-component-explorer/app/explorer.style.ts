6784b20081237263584e409f8e3d6d36c862175f
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-05-19 13:04:04 -0700

    chore: remove style overrides and bring style parity with Creator (#3141)

--- sites/fast-component-explorer/app/explorer.style.ts
+++ sites/fast-component-explorer/app/explorer.style.ts
@@ -1,27 +1,12 @@
 import { ComponentStyles, CSSRules } from "@microsoft/fast-jss-manager-react";
 import {
-    accentForegroundRest,
-    applyCornerRadius,
     DesignSystem,
-    neutralFillActive,
-    neutralFillHover,
-    neutralFillRest,
     neutralLayerFloating,
     neutralLayerL2,
-    neutralLayerL3,
 } from "@microsoft/fast-components-styles-msft";
 
 export interface ExplorerClassNameContract {
     explorer: string;
-    explorer_colorPicker: string;
-    explorer_devToolsPanel: string;
-    explorer_navigationPanel: string;
-    explorer_paneTitleContainer: string;
-    explorer_propertiesPanel: string;
-    explorer_toolbar: string;
-    explorer_viewerRegion: string;
-    explorer_viewerControlRegion: string;
-    explorer_viewerControls: string;
 }
 
 export function applyScrollbarStyle(): CSSRules<{}> {
@@ -63,79 +48,6 @@ const style: ComponentStyles<ExplorerClassNameContract, DesignSystem> = {
         },
     },
     explorer: {},
-    explorer_colorPicker: {
-        background: neutralFillRest(neutralLayerL2),
-        border: "none",
-        width: "23px",
-        height: "22px",
-        ...applyCornerRadius(),
-        "&:hover": {
-            background: neutralFillHover(neutralLayerL2),
-        },
-        "&:active": {
-            background: neutralFillActive(neutralLayerL2),
-        },
-        "&:focus": {
-            outline: "none",
-        },
-    },
-    explorer_devToolsPanel: {
-        boxShadow:
-            "0px 0.6px 1.8px rgba(0, 0, 0, 0.11) inset, 0px 3.2px 7.2px rgba(0, 0, 0, 0.13) inset",
-        width: "100%",
-        overflow: "auto",
-        "& a": {
-            color: accentForegroundRest(neutralLayerL2),
-        },
-    },
-    explorer_navigationPanel: {
-        overflowY: "auto",
-        overflowX: "hidden",
-        background: neutralLayerL3,
-        boxShadow:
-            "-0.6px 0px 1.8px rgba(0, 0, 0, 0.11) inset, -3.2px 0px 7.2px rgba(0, 0, 0, 0.13) inset",
-        ...applyScrollbarStyle(),
-    },
-    explorer_paneTitleContainer: {
-        height: "32px",
-        display: "flex",
-        boxSizing: "border-box",
-        alignItems: "center",
-        paddingLeft: "10px",
-    },
-    explorer_propertiesPanel: {
-        background: neutralLayerL3,
-        overflow: "auto",
-        boxShadow:
-            "0.6px 0px 1.8px rgba(0, 0, 0, 0.11) inset, 3.2px 0px 7.2px rgba(0, 0, 0, 0.13) inset",
-        ...applyScrollbarStyle(),
-    },
-    explorer_toolbar: {
-        padding: "0 8px",
-        height: "32px",
-        display: "flex",
-        alignItems: "center",
-        background: neutralLayerL2,
-        justifyContent: "flex-end",
-        boxShadow:
-            "0px -0.6px 1.8px rgba(0, 0, 0, 0.11) inset, 0px -3.2px 7.2px rgba(0, 0, 0, 0.13) inset",
-    },
-    explorer_viewerRegion: {
-        width: "100%",
-        height: "calc(100% - 32px)",
-        overflow: "auto",
-        ...applyScrollbarStyle(),
-    },
-    explorer_viewerControlRegion: {
-        display: "flex",
-        alignItems: "center",
-    },
-    explorer_viewerControls: {
-        display: "flex",
-        padding: "7px 10px",
-        marginLeft: "auto",
-        alignItems: "center",
-    },
 };
 
 export default style;
