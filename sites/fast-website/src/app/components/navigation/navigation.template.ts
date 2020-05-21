300d26e13f414efca50a8a17d4a49e951409a0f2
khamudom <khamu@microsoft.com>
2020-05-21 13:38:59 -0700

    update menu button to toggle icon from menu to close. add class to svgs

--- sites/fast-website/src/app/components/navigation/navigation.template.ts
+++ sites/fast-website/src/app/components/navigation/navigation.template.ts
@@ -1,7 +1,8 @@
-import { html } from "@microsoft/fast-element";
+import { html, when } from "@microsoft/fast-element";
 import { endTemplate, startTemplate } from "@microsoft/fast-foundation";
 import { Navigation } from "./navigation";
 import MenuIcon from "svg/icon-menu.svg";
+import CloseIcon from "svg/icon-close.svg";
 
 export const NavigationTemplate = html<Navigation>`
     <template class="site-navigation ${x => (x.opened ? "opened" : "")}">
@@ -16,7 +17,8 @@ export const NavigationTemplate = html<Navigation>`
             appearance="stealth"
             @click="${(x, c) => x.handleOpenNavClick(c.event)}"
         >
-            ${MenuIcon}
+            ${when(x => !x.opened, html` ${MenuIcon} `)}
+            ${when(x => x.opened, html` ${CloseIcon} `)}
         </fast-button>
     </template>
 `;
