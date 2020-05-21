7c04bbeee445ea3ce44ef1ba57ec122700951cf5
SamanthaAO <samanthaaorcutt@gmail.com>
2020-05-21 13:36:08 -0700

    chore: update styles for framework and community sections (#3162)
    * add svg, update padding and text variables, add borders to framework to align with design
    
    * switch twitter and github icons so they are assigned correctly
    
    * remove class's for framework content placement and use nth-of-type. update h3 style. update section container class naming function
    
    * update container naming function
    
    * add border variable
    
    * update borders to be more reusable
    
    * Change grid borders to be mobile-first
    
    Co-authored-by: John Kreitlow <john.kreitlow@microsoft.com>

--- sites/fast-website/src/app/components/content-placement-container/content-placement-container.template.ts
+++ sites/fast-website/src/app/components/content-placement-container/content-placement-container.template.ts
@@ -1,29 +1,22 @@
 import { html, repeat, when } from "@microsoft/fast-element";
 import { ContentPlacementContainer } from "./content-placement-container";
+import { CommunityContentPlacementData } from "../../data/community.data";
 
 export const ContentPlacementContainerTemplate = html<ContentPlacementContainer>`
-    <div class="container">
+    <div
+        class="container ${x =>
+            ["framework", "community"].includes(x.section)
+                ? `${x.section}_container`
+                : ""}"
+    >
         ${when(
             x => x.section === "framework",
             html<ContentPlacementContainer>`
                 ${repeat(
                     x => x.frameworkContentPlacementData,
-                    html`<site-content-placement divider>
+                    html`<site-content-placement class="framework_ContentPlacement">
                         <h3>${x => x.header}</h3>
                         <p slot="body">${x => x.body}</p>
-                        <span slot="compatibility"
-                            ><img
-                                src=${x => x.compatibilityImageSrc}
-                                alt=${x => x.compatibilityImageAlt}
-                            />
-                            ${x => x.compatibilityText}</span
-                        >
-                        <fast-anchor
-                            slot="action"
-                            appearance="lightweight"
-                            href=${x => x.actionLink}
-                            >${x => x.actionText}</fast-anchor
-                        >
                     </site-content-placement>`
                 )}
             `
@@ -33,10 +26,8 @@ export const ContentPlacementContainerTemplate = html<ContentPlacementContainer>
             html<ContentPlacementContainer>`
                 ${repeat(
                     x => x.communityContentPlacementData,
-                    html`<site-content-placement>
-                        <div slot="icon">
-                            <img src=${x => x.iconSrc} alt=${x => x.iconAlt} />
-                        </div>
+                    html`<site-content-placement icon>
+                        <div slot="icon" :innerHTML=${x => x.icon}></div>
                         <h3>${x => x.header}</h3>
                         <p slot="body">${x => x.body}</p>
                         <fast-anchor
