6449ba82486cc8a6b8a1d54796b19459ee9e46ec
Kham Udom <37851220+khamudom@users.noreply.github.com>
2020-05-22 15:20:58 -0700

    chore: add responsive navigation to marketing site (#3164)
    * put links in ul and li. add media query and responsive styles. removed styles for start and end template in navigation component.
    
    * update use of svg. add brand icon. add responsive footer
    
    * add menu icon
    
    * update navigation template and component implementation inside index.html. move click handler inside the template. use ::before instead of dummy div
    
    * fix footer for mobile
    
    * update menu button to toggle icon from menu to close. add class to svgs
    
    * Adjust stacking context and debounce for menu on mobile
    
    * prettier
    
    * fix logo size in footer
    
    Co-authored-by: John Kreitlow <john.kreitlow@microsoft.com>

--- sites/fast-website/src/app/components/navigation/navigation.template.ts
+++ sites/fast-website/src/app/components/navigation/navigation.template.ts
@@ -1,13 +1,27 @@
-import { html } from "@microsoft/fast-element";
+import { html, when } from "@microsoft/fast-element";
 import { endTemplate, startTemplate } from "@microsoft/fast-foundation";
+import CloseIcon from "svg/icon-close.svg";
+import MenuIcon from "svg/icon-menu.svg";
 import { Navigation } from "./navigation";
 
 export const NavigationTemplate = html<Navigation>`
-    <template>
+    <template
+        class="site-navigation ${x => (x.opened ? "opened" : "")}
+        ${x => (x.debounce ? "debounce" : "")}"
+    >
         ${startTemplate}
         <nav>
             <slot></slot>
         </nav>
         ${endTemplate}
+        <fast-button
+            class="nav-button"
+            part="nav-button"
+            appearance="stealth"
+            @click="${(x, c) => x.handleOpenNavClick(c.event)}"
+        >
+            ${when(x => !x.opened, html` ${MenuIcon} `)}
+            ${when(x => x.opened, html` ${CloseIcon} `)}
+        </fast-button>
     </template>
 `;
