5fdd97f9a5c299f4a3a52773c662f7886dbc22e9
Kham Udom <37851220+khamudom@users.noreply.github.com>
2020-05-22 15:20:58 -0700

    chore: add responsive navigation to marketing site (#3164)
    * put links in ul and li. add media query and responsive styles. removed styles for start and end template in navigation component.
    
    * update use of svg. add brand icon. add responsive footer
    
    * add menu icon
    
    * update navigation template and component implementation inside index.html. move click handler inside the template. use ::before instead of dummy div
    
    * fix footer for mobile
    
    * update menu button to toggle icon from menu to close. add class to svgs
    
    * Adjust stacking context and debounce for menu on mobile
    
    * prettier
    
    * fix logo size in footer
    
    Co-authored-by: John Kreitlow <john.kreitlow@microsoft.com>

--- sites/fast-website/src/app/components/navigation/navigation.styles.ts
+++ sites/fast-website/src/app/components/navigation/navigation.styles.ts
@@ -3,17 +3,20 @@ import { neutralForegroundRestBehavior } from "@microsoft/fast-components";
 import { display } from "@microsoft/fast-foundation";
 
 export const NavigationStyles = css`
-    ${display("flex")} :host {
-        justify-content: space-between;
+    ${display("grid")} :host {
+        grid-template-columns: auto 1fr auto;
         font-family: var(--body-font);
         color: var(--neutral-foreground-rest);
         box-sizing: border-box;
         width: 100%;
     }
-    .start {
-        margin-inline-end: auto;
+
+    .nav-button {
+        display: none;
     }
-    .end {
-        margin-inline-start: auto;
+
+    .nav-button svg {
+        width: 35px;
+        height: 19px;
     }
 `.withBehaviors(neutralForegroundRestBehavior);
--- sites/fast-website/src/app/components/navigation/navigation.ts
+++ sites/fast-website/src/app/components/navigation/navigation.ts
@@ -1,7 +1,35 @@
-import { FASTElement } from "@microsoft/fast-element";
+import { FASTElement, observable } from "@microsoft/fast-element";
 import { applyMixins, StartEnd } from "@microsoft/fast-foundation";
 
-export class Navigation extends FASTElement {}
+export class Navigation extends FASTElement {
+    @observable
+    public opened: boolean = false;
+
+    @observable
+    public debounce: boolean = true;
+
+    private mediaQueryList: MediaQueryList;
+
+    private mqlListener = (e: MediaQueryListEvent): void => {
+        this.debounce = true;
+
+        if (!e.matches) {
+            this.opened = false;
+        }
+    };
+
+    constructor() {
+        super();
+
+        this.mediaQueryList = window.matchMedia("screen and (max-width: 800px)");
+        this.mediaQueryList.addListener(this.mqlListener);
+    }
+
+    public handleOpenNavClick = (e: Event): void => {
+        this.opened = !this.opened;
+        this.debounce = false;
+    };
+}
 
 /* eslint-disable-next-line */
 export interface Navigation extends StartEnd {}
