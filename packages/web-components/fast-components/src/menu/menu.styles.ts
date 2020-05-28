83feecc730892d07ee759d08ac55982cafff0309
Kham Udom <37851220+khamudom@users.noreply.github.com>
2020-05-28 10:56:59 -0700

    high contrast pass on menu and menu-item (#3189)

--- packages/web-components/fast-components/src/menu/menu.styles.ts
+++ packages/web-components/fast-components/src/menu/menu.styles.ts
@@ -10,6 +10,7 @@ export const MenuStyles = css`
     ${display("block")} :host {
         --elevation: 11;
         background: var(--neutral-layer-floating);
+        border: calc(var(--outline-width) * 1px) solid transparent;
         border-radius: var(--elevated-corner-radius);
         ${elevation} margin: 0;
         border-radius: calc(var(--corner-radius) * 1px);
