f8476da3d5a8b697cb292cd7679a5580c840d36f
SamanthaAO <samanthaaorcutt@gmail.com>
2020-06-01 13:56:16 -0700

    add id's after rebase and add updateScrollIncreasing function for stutter on scroll when scrolling from the bottom to the top of the page

--- sites/fast-website/src/app/components/side-navigation/side-navigation.ts
+++ sites/fast-website/src/app/components/side-navigation/side-navigation.ts
@@ -20,9 +20,28 @@ export class SideNavigation extends FASTElement {
         x => x.header !== "Github"
     );
 
+    public lastScrollPosition: number = 0;
+    public scrollIncreasing: boolean = true;
+    public tick: boolean = false;
+
+    public updateScrollIncreasing = (): void => {
+        if (!this.tick) {
+            setTimeout((): void => {
+                this.lastScrollPosition = window.scrollY;
+                if (window.scrollY > this.lastScrollPosition) {
+                    this.scrollIncreasing = true;
+                } else {
+                    this.scrollIncreasing = false;
+                }
+                this.tick = false;
+            }, 100);
+        }
+        this.tick = true;
+    };
+
     public updateCurrentSection = (): void => {
         const config = {
-            threshold: 0.5,
+            threshold: this.scrollIncreasing ? 0.2 : 0.8,
         };
 
         let observer = new IntersectionObserver(entries => {
@@ -52,5 +71,6 @@ export class SideNavigation extends FASTElement {
         super();
 
         window.addEventListener("scroll", this.updateCurrentSection);
+        window.addEventListener("scroll", this.updateScrollIncreasing);
     }
 }
