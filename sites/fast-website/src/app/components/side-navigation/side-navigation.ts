783c4f51a705507a36ca42bf81fe1ebebaebb9d8
SamanthaAO <samanthaaorcutt@gmail.com>
2020-06-02 13:29:10 -0700

    feat: add side navigation for social links and scroll (#3199)
    * create side-navigation component, update main index and style, add svg for medium, update community data and content-placement-container community data
    
    * have section array pull from document to increate reuse
    
    * add parent attributes to repeat in template to reduce styles
    
    * update naming for greater reusability
    
    * update section id's in index to be the names of section and update functions accordingly
    
    * add intersection observer, add click events fro link so that event.prevent default removes hash from url, update display to flex
    
    * add id's after rebase and add updateScrollIncreasing function for stutter on scroll when scrolling from the bottom to the top of the page
    
    * moved intersection observer into constructor and removed eventListerners. Made style changes to scroll indicators so they change size less
    
    * make styling changes recomended in comments
    
    Co-authored-by: Sam Orcutt <v-sorcut@microsoft.com>

---
+++ sites/fast-website/src/app/components/side-navigation/side-navigation.ts
@@ -0,0 +1,66 @@
+import { attr, observable, FASTElement } from "@microsoft/fast-element";
+import {
+    CommunityContentPlacementData,
+    communityContentPlacementData,
+} from "../../data/community.data";
+
+export class SideNavigation extends FASTElement {
+    @attr
+    public category: string;
+
+    @observable
+    public currentSection: string = "hero";
+
+    @observable
+    public sectionArray: HTMLElement[] = Array.from(
+        document.querySelectorAll("section[id]")
+    );
+
+    public socialData: CommunityContentPlacementData[] = communityContentPlacementData.filter(
+        x => x.header !== "Github"
+    );
+
+    private previousRatio: number = 0;
+
+    public clickHandler = (e, parent: boolean): void => {
+        const link = parent
+            ? e.target.getAttribute("href")
+            : e.target.parentElement.getAttribute("href");
+
+        const selectedSection = document.querySelector(link);
+
+        e.preventDefault();
+
+        if (selectedSection) {
+            selectedSection.scrollIntoView({
+                behavior: "smooth",
+            });
+        }
+    };
+
+    constructor() {
+        super();
+
+        if (this.getAttribute("category") === "scroll") {
+            let observer = new IntersectionObserver(
+                entries => {
+                    entries.forEach(entry => {
+                        const currentRatio = entry.intersectionRatio;
+
+                        if (entry.isIntersecting) {
+                            if (currentRatio > this.previousRatio) {
+                                this.currentSection = entry.target.id;
+                            }
+                            this.previousRatio = currentRatio;
+                        }
+                    });
+                },
+                { threshold: [0.4, 0.6] }
+            );
+
+            this.sectionArray.forEach(section => {
+                observer.observe(section);
+            });
+        }
+    }
+}
