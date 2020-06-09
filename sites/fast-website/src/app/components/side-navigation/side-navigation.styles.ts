37b2a045c385556745b4cbe941914a2cd016bb48
SamanthaAO <samanthaaorcutt@gmail.com>
2020-06-09 15:04:16 -0700

    make resting background color white with --accent-foreground-cut-rest. update height change for active indicator to be done with transition and transform

--- sites/fast-website/src/app/components/side-navigation/side-navigation.styles.ts
+++ sites/fast-website/src/app/components/side-navigation/side-navigation.styles.ts
@@ -2,13 +2,12 @@ import { css } from "@microsoft/fast-element";
 import { display } from "@microsoft/fast-foundation";
 import {
     accentForegroundActiveBehavior,
+    accentForegroundCutRestBehavior,
     accentForegroundHoverBehavior,
-    neutralForegroundRestBehavior,
 } from "@microsoft/fast-components";
 
 export const SideNavigationStyles = css`
     ${display("flex")}:host {
-        --engaged-height: calc(var(--design-unit) * 9px);
         --design-unit-large: calc(var(--design-unit) * 5px);
         contain: content;
         font-family: var(--body-font);
@@ -22,11 +21,8 @@ export const SideNavigationStyles = css`
         cursor: pointer;
     }
 
-    li {
-        height: calc(var(--design-unit) * 12px);
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
+    .icon {
+        fill: var(--accent-foreground-cut-rest);
     }
 
     li:hover .icon {
@@ -34,24 +30,55 @@ export const SideNavigationStyles = css`
     }
 
     .scroll-indicator {
-        height: var(--design-unit-large);
+        height: calc(var(--design-unit) * 9px);
         width: calc(var(--design-unit) * 1px);
-        background-color: var(--neutral-foreground-rest);
+        background-color: var(--accent-foreground-cut-rest);
         border-radius: calc(var(--corner-radius) * 1px);
-        margin: 0 var(--design-unit-large);
+        margin: calc(var(--design-unit) * 3px) var(--design-unit-large);
+        position: relative;
+        transition: all 300ms;
     }
 
-    a:hover .scroll-indicator {
-        height: var(--engaged-height);
-        background-color: var(--accent-foreground-hover);
+    .scroll-indicator::before,
+    .scroll-indicator::after {
+        content: "";
+        height: 50%;
+        width: 100%;
+        border-radius: calc(var(--corner-radius) * 1px);
+        transition: all 300ms;
+        position: absolute;
+    }
+
+    .scroll-indicator::before {
+        top: 0;
+    }
+
+    .scroll-indicator::after {
+        bottom: 0;
     }
 
     .scroll-indicator-active {
-        height: var(--engaged-height);
+        margin: var(--design-unit-large);
         background-color: var(--accent-foreground-active);
+    }
+
+    .scroll-indicator-active.scroll-indicator::before {
+        transform: translateY(calc(var(--design-unit) * -2px));
+        background-color: var(--accent-foreground-active);
+    }
+
+    .scroll-indicator-active.scroll-indicator::after {
+        transform: translateY(calc(var(--design-unit) * 2px));
+        background-color: var(--accent-foreground-active);
+    }
+
+    a:hover .scroll-indicator,
+    a:hover .scroll-indicator-active.scroll-indicator::before,
+    a:hover .scroll-indicator-active.scroll-indicator::after {
+        background-color: var(--accent-foreground-hover);
     }
 `.withBehaviors(
     accentForegroundActiveBehavior,
-    accentForegroundHoverBehavior,
-    neutralForegroundRestBehavior
+    accentForegroundCutRestBehavior,
+    accentForegroundHoverBehavior
 );
