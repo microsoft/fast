7f28caec0a2e11cee919ef779ef64cea696f4110
SamanthaAO <samanthaaorcutt@gmail.com>
2020-06-08 17:06:59 -0700

    chore: update card/content styling to match figma (#3228)
    * grey out links on non hovered community cards, display numbers on feature cards using index instead of data file, add header subscript to framework cards
    
    * change numbering on feature cards from index to css and make styling changes associated witht it. change span for headingSubscript to small
    
    * remove positioning in feature since we no longer use index

--- sites/fast-website/src/app/components/content-placement-container/content-placement-container.template.ts
+++ sites/fast-website/src/app/components/content-placement-container/content-placement-container.template.ts
@@ -7,10 +7,17 @@ export const ContentPlacementContainerTemplate = html<ContentPlacementContainer>
         html<ContentPlacementContainer>`
             ${repeat(
                 x => x.frameworkContentPlacementData,
-                html`<site-content-placement class="framework_ContentPlacement">
-                    <h3>${x => x.header}</h3>
-                    <p slot="body">${x => x.body}</p>
-                </site-content-placement>`
+                html`
+                    <site-content-placement class="framework_ContentPlacement">
+                        <h3>
+                            ${x => (x.headerSubscript ? x.header + " " : x.header)}
+                            <small class="headerSubscript"
+                                >${x => x.headerSubscript}</small
+                            >
+                        </h3>
+                        <p slot="body">${x => x.body}</p>
+                    </site-content-placement>
+                `
             )}
         `
     )}
@@ -20,23 +27,24 @@ export const ContentPlacementContainerTemplate = html<ContentPlacementContainer>
             <site-card-section>
                 ${repeat(
                     x => x.featureCardData,
-                    html`<site-feature-card>
-                        <div>${x => x.item}</div>
-                        <h4>${x => x.header}</h4>
-                        <p slot="body">${x => x.body}</p>
-                        <fast-anchor
-                            slot="footer"
-                            href=${x => x.githubLink}
-                            appearance="lightweight"
-                            >View Github</fast-anchor
-                        >
-                        <fast-anchor
-                            slot="footer"
-                            href=${x => x.documentationLink}
-                            appearance="lightweight"
-                            >Read Documentation</fast-anchor
-                        >
-                    </site-feature-card>`
+                    html`
+                        <site-feature-card>
+                            <h4>${x => x.header}</h4>
+                            <p slot="body">${x => x.body}</p>
+                            <fast-anchor
+                                slot="footer"
+                                href=${x => x.githubLink}
+                                appearance="lightweight"
+                                >View Github</fast-anchor
+                            >
+                            <fast-anchor
+                                slot="footer"
+                                href=${x => x.documentationLink}
+                                appearance="lightweight"
+                                >Read Documentation</fast-anchor
+                            >
+                        </site-feature-card>
+                    `
                 )}
             </site-card-section>
         `
@@ -46,17 +54,19 @@ export const ContentPlacementContainerTemplate = html<ContentPlacementContainer>
         html<ContentPlacementContainer>`
             ${repeat(
                 x => x.communityContentPlacementData,
-                html`<site-content-placement icon>
-                    <div slot="icon" :innerHTML=${x => x.icon}></div>
-                    <h3>${x => x.header}</h3>
-                    <p slot="body">${x => x.body}</p>
-                    <fast-anchor
-                        slot="action"
-                        appearance="lightweight"
-                        href=${x => x.actionLink}
-                        >${x => x.actionText}</fast-anchor
-                    >
-                </site-content-placement>`
+                html`
+                    <site-content-placement icon>
+                        <div slot="icon" :innerHTML=${x => x.icon}></div>
+                        <h3>${x => x.header}</h3>
+                        <p slot="body">${x => x.body}</p>
+                        <fast-anchor
+                            slot="action"
+                            appearance="lightweight"
+                            href=${x => x.actionLink}
+                            >${x => x.actionText}</fast-anchor
+                        >
+                    </site-content-placement>
+                `
             )}
         `
     )}
--- sites/fast-website/src/app/data/framework.data.ts
+++ sites/fast-website/src/app/data/framework.data.ts
@@ -1,13 +1,15 @@
 export interface FrameworkContentPlacementData {
     body?: string;
     header?: string;
+    headerSubscript?: string;
 }
 
 export const frameworkContentPlacementData: FrameworkContentPlacementData[] = [
     {
         body:
             "Angular's default binding syntax will always set properties on an element. This works well for rich data, like objects and arrays, and also works well for primitive values so long as the Custom Element author has mapped any exposed attributes to corresponding properties.",
-        header: "Angular 8.2.14",
+        header: "Angular",
+        headerSubscript: "8.2.14",
     },
     {
         body:
@@ -17,21 +19,25 @@ export const frameworkContentPlacementData: FrameworkContentPlacementData[] = [
     {
         body:
             "hyperHTML will pass data to an element as properties, as long as the property is defined on the element's prototype. Otherwise it will fallback to passing data as attributes.",
-        header: "hyperHTML 2.32.2",
+        header: "hyperHTML",
+        headerSubscript: "2.32.2",
     },
     {
         body:
             "Dojo will pass data as attributes only when the data is a type of string, otherwise it is set as a property.",
-        header: "Dojo 6.0.4",
+        header: "Dojo",
+        headerSubscript: "6.0.4",
     },
     {
         body:
             "Hybrids will pass data to an element as properties, as long as the property is defined on the element's prototype. Otherwise it will fallback to passing data as attributes.",
-        header: "hybrids 4.1.7",
+        header: "hybrids",
+        headerSubscript: "4.1.7",
     },
     {
         body:
             "AngularJS can declaratively pass data to attributes using ng-attr, or to properties using ng-prop.",
-        header: "AngularJS (1.x) 1.7.9",
+        header: "AngularJS (1.x)",
+        headerSubscript: "1.7.9",
     },
 ];
