a2472c23ed33b97d38900d0442a9d59c26fc637d
nicholasrice <nicholas.rice119@gmail.com>
2020-06-10 14:28:18 -0700

    register recipe

--- packages/web-components/fast-components/src/accordion/accordion.stories.ts
+++ packages/web-components/fast-components/src/accordion/accordion.stories.ts
@@ -1,4 +1,5 @@
 import { FASTDesignSystemProvider } from "../design-system-provider";
+import { accentFillRestBehavior } from "../styles/recipes";
 import Examples from "./fixtures/base.html";
 import { FASTAccordionItem } from "./accordion-item";
 import { FASTAccordion } from ".";
@@ -13,3 +14,15 @@ export default {
 };
 
 export const Base = () => Examples;
+
+document.addEventListener("readystatechange", (e) => {
+    if (document.readyState === "complete") {
+        const provider = document.querySelector("fast-design-system-provider");
+
+        // Example depends on accentFillRest but component itself
+        // does not.
+        if (provider instanceof FASTDesignSystemProvider) {
+            provider.registerCSSCustomProperty(accentFillRestBehavior)
+        }
+    }
+})
