60f1184e9c0c21288ab9c069b0393396d6872a9e
nicholasrice <nicholas.rice119@gmail.com>
2020-06-12 12:23:30 -0700

    document anchor

--- packages/web-components/fast-foundation/src/anchor/anchor.ts
+++ packages/web-components/fast-foundation/src/anchor/anchor.ts
@@ -4,6 +4,7 @@ import { StartEnd } from "../patterns/start-end";
 import { applyMixins } from "../utilities/apply-mixins";
 
 /**
+ * Type types of anchor appearance.
  * @public
  */
 export type AnchorAppearance = ButtonAppearance | "hypertext";
@@ -17,44 +18,94 @@ export type AnchorAppearance = ButtonAppearance | "hypertext";
  */
 export class Anchor extends FASTElement {
     
-    @attr
     /**
-     * Appearance property
+     * The appearance the anchor should have.
      * 
-     * @test
-     * Test content
      * @public
+     * @remarks
+     * HTML Attribute: appearance
      */
+    @attr
     public appearance: AnchorAppearance = "neutral";
 
+    /**
+     * Prompts the user to save the linked URL. See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a | <a> element } for more information.
+     * @public
+     * @remarks
+     * HTML Attribute: download
+     */
     @attr
     public download: string;
 
+    /**
+     * The URL the hyperlink references. See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a | <a> element } for more information.
+     * @public
+     * @remarks
+     * HTML Attribute: href
+     */
     @attr
     public href: string;
 
+    /**
+     * Hints at the language of the referenced resource. See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a | <a> element } for more information.
+     * @public
+     * @remarks
+     * HTML Attribute: hreflang
+     */
     @attr
     public hreflang: string;
 
+    /**
+     * See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a | <a> element } for more information.
+     * @public
+     * @remarks
+     * HTML Attribute: ping
+     */
     @attr
     public ping: string;
 
+    /**
+     * See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a | <a> element } for more information.
+     * @public
+     * @remarks
+     * HTML Attribute: referrerpolicy
+     */
     @attr
     public referrerpolicy: string;
 
+    /**
+     * See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a | <a> element } for more information.
+     * @public
+     * @remarks
+     * HTML Attribute: rel
+     */
     @attr
     public rel: string;
 
+    /**
+     * See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a | <a> element } for more information.
+     * @public
+     * @remarks
+     * HTML Attribute: target
+     */
     @attr
     public target: "_self" | "_blank" | "_parent" | "_top";
 
+    /**
+     * See {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a | <a> element } for more information.
+     * @public
+     * @remarks
+     * HTML Attribute: type
+     */
     @attr
     public type: string;
 }
 
-/* eslint-disable-next-line */
 /**
- * @public
+ * Mark internal because exporting class and interface of the same name
+ * confuses API extractor
+ * @internal
  */
+/* eslint-disable-next-line */
 export interface Anchor extends StartEnd {}
 applyMixins(Anchor, StartEnd);
