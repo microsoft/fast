2f09be7a4fe497f27d1e31593c892ba7d02380d7
Connor Smith <cosmith@microsoft.com>
2020-06-15 13:49:49 -0700

    Add fter prop to context-menu-item

--- packages/react/fast-components-react-msft/src/context-menu-item/context-menu-item.spec.tsx
+++ packages/react/fast-components-react-msft/src/context-menu-item/context-menu-item.spec.tsx
@@ -11,6 +11,7 @@ import { ContextMenuItem, ContextMenuItemUnhandledProps } from "./index";
 configure({ adapter: new Adapter() });
 
 describe("context menu item", (): void => {
+    const afterExample: JSX.Element = <div className={"after"}>after</div>;
     const beforeExample: JSX.Element = <div className={"before"}>before</div>;
 
     test("should have a displayName that matches the component name", () => {
@@ -35,9 +36,24 @@ describe("context menu item", (): void => {
         expect(rendered.find("div").prop("aria-hidden")).toEqual(true);
     });
 
+    test("should render an element from the after prop", (): void => {
+        const rendered: any = mount(<ContextMenuItem after={afterExample} />);
+
+        expect(rendered.find(".after")).toHaveLength(1);
+    });
+
     test("should render an element from the before prop", (): void => {
         const rendered: any = mount(<ContextMenuItem before={beforeExample} />);
 
         expect(rendered.find(".before")).toHaveLength(1);
+    });
+
+    test("should render both elements from the after and before props", (): void => {
+        const rendered: any = mount(
+            <ContextMenuItem after={afterExample} before={beforeExample} />
+        );
+
+        expect(rendered.find(".before")).toHaveLength(1);
+        expect(rendered.find(".after")).toHaveLength(1);
     });
 });
