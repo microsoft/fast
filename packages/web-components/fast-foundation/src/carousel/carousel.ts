f0362f9a28f49399aea18a36a138f9449d82b7f5
Seth Donohue <sethdonohue@Admins-MBP.guest.corp.microsoft.com>
2020-06-18 13:51:48 -0700

    add autoplay, add aria attrs, add flipper controls, add basic keyboard tab navigation, add associated examples

--- packages/web-components/fast-foundation/src/carousel/carousel.ts
+++ packages/web-components/fast-foundation/src/carousel/carousel.ts
@@ -1,12 +1,19 @@
+// TODO: ADD flipper functionality
+// TODO: ADD animations and classes to previous, current, next slides
+// TODO: CHECK for error states, compare with other components
+// TODO: ADD more examples to test autoplay, timer, loop, etc...
+// TODO: ADD keyboard controls
+// TODO: ADD accessibility features, compare to SPEC and other components
 import { attr, DOM, FASTElement, observable } from "@microsoft/fast-element";
-import { KeyCodes } from "@microsoft/fast-web-utilities";
+import { KeyCodes, wrapInBounds, limit } from "@microsoft/fast-web-utilities";
+import { isNil } from "lodash-es";
 
 export class Carousel extends FASTElement {
     @attr({ mode: "boolean" })
-    public autoplay: boolean = false;
+    public autoplay: boolean = true;
 
     @attr({ attribute: "autoplay-interval" })
-    public autoplayInterval: number = 3000;
+    public autoplayInterval: number = 6000;
 
     @attr({ mode: "boolean" })
     public loop: boolean = true;
@@ -26,37 +33,36 @@ export class Carousel extends FASTElement {
     @attr({ attribute: "activeid" })
     public activeId: string;
 
+    public carousel: HTMLDivElement;
+
     @observable
     public items: HTMLElement[];
     public itemsChanged(): void {
         this.filteredItems = this.items
             .filter((item: HTMLElement) => item.nodeType === 1)
             //TODO: NEED TO FILTER BETTER so that i don't remove text nodes that may actually be content
+            //  - text nodes are not stylable though...
             .map((item: HTMLElement, index: number) => {
-                console.log(
-                    "IN itemsChanged activeTabIndex: ",
-                    this.activeTabIndex,
-                    " index: ",
-                    index
-                );
-
                 if (index === this.activeTabIndex) {
                     item.classList.add("active-slide");
+                    item.setAttribute("aria-hidden", "false");
                     item.removeAttribute("hidden");
                 } else {
                     item.setAttribute("hidden", "");
+                    item.setAttribute("aria-hidden", "true");
                 }
                 if (index === this.activeTabIndex + 1) {
                     item.classList.add("next-slide");
                 } else if (index === this.activeTabIndex - 1) {
                     item.classList.add("previous-slide");
+                    // TODO: ADD class for looped slides, if -1 doesn't exist then use length -1 slide.
                 }
 
                 item.classList.add("slide");
+                item.setAttribute("role", "tabpanel");
+                // TODO: ASK add aria-labelledby?
                 return item;
             });
-
-        console.log("ITEMS CHANGED: ", this.items, this.filteredItems);
     }
 
     @observable
@@ -67,27 +73,78 @@ export class Carousel extends FASTElement {
     public tabsChanged(): void {
         this.tabs.forEach((tab: HTMLElement, index: number) => {
             tab.addEventListener("click", this.handleTabClick(index));
+            tab.setAttribute("role", "tab");
+            if (this.activeTabIndex === index) {
+                tab.setAttribute("aria-selected", "true");
+            } else {
+                tab.setAttribute("aria-selected", "false");
+            }
+            //TODO: ADD class for when activeIndex matches this tab index so the tab is highlighted properly.
         });
     }
 
-    public carousel: HTMLDivElement;
+    public handleFlipperClick(direction: 1 | -1, e: Event): void {
+        this.incrementItem(direction);
+    }
 
-    // public connectedCallback(): void {
-    //     super.connectedCallback();
-    // }
+    public connectedCallback(): void {
+        super.connectedCallback();
 
-    // public disconnectedCallback(): void {
-    //     super.disconnectedCallback();
-    // }
-    @observable
-    public handleTabClick(index: number): (e: Event) => void {
+        this.carousel.addEventListener("keydown", this.handelKeyDown);
+
+        if (this.autoplay) {
+            this.autoplayTimer = window.setInterval(
+                this.incrementItem,
+                this.autoplayInterval
+            );
+        }
+    }
+
+    public disconnectedCallback(): void {
+        super.disconnectedCallback();
+        // this.carousel.removeEventListener("keydown");
+        if (!isNil(this.autoplayTimer)) {
+            this.autoplayTimer = window.clearInterval(this.autoplayTimer as number);
+        }
+    }
+
+    private autoplayTimer: number | void;
+
+    private activeTabIndex: number = 0;
+
+    private handleTabClick(index: number): (e: Event) => void {
         return (e: Event): void => {
-            console.log("HIT HANDLE TAB CLICK, index: ", index, this.activeTabIndex);
             this.activeTabIndex = index;
             this.itemsChanged();
         };
     }
 
-    @observable
-    private activeTabIndex: number = 0;
+    private incrementItem = (direction: 1 | -1 = 1): void => {
+        this.activeTabIndex = this.loop
+            ? wrapInBounds(
+                  0,
+                  this.filteredItems.length - 1,
+                  this.activeTabIndex + direction
+              )
+            : limit(0, this.filteredItems.length - 1, this.activeTabIndex + direction);
+
+        this.itemsChanged();
+    };
+
+    private handelKeyDown = (e: KeyboardEvent): void => {
+        switch (e.keyCode) {
+            case KeyCodes.arrowLeft:
+            case KeyCodes.arrowUp:
+                e.stopPropagation();
+                e.preventDefault();
+                this.incrementItem(-1);
+                break;
+            case KeyCodes.arrowRight:
+            case KeyCodes.arrowDown:
+                e.preventDefault();
+                e.stopPropagation();
+                this.incrementItem(1);
+                break;
+        }
+    };
 }
