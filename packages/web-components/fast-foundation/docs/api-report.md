7e70c3567b4b2d0ca8277e205dd849a9191e271a
Seth Donohue <sethdonohue@Admins-MBP.guest.corp.microsoft.com>
2020-06-18 13:51:48 -0700

    add autoplay, add aria attrs, add flipper controls, add basic keyboard tab navigation, add associated examples

--- packages/web-components/fast-foundation/docs/api-report.md
+++ packages/web-components/fast-foundation/docs/api-report.md
@@ -170,13 +170,9 @@ export class Carousel extends FASTElement {
     // (undocumented)
     carousel: HTMLDivElement;
     // (undocumented)
-    connectedCallback(): void;
-    // (undocumented)
-    disconnectedCallback(): void;
-    // (undocumented)
     filteredItems: HTMLElement[];
     // (undocumented)
-    handleTabClick(e: Event): void;
+    handleTabClick(index: number): (e: Event) => void;
     // (undocumented)
     items: HTMLElement[];
     // (undocumented)
@@ -187,6 +183,8 @@ export class Carousel extends FASTElement {
     paused: boolean;
     // (undocumented)
     tabs: HTMLElement[];
+    // (undocumented)
+    tabsChanged(): void;
 }
 
 // @public (undocumented)
--- packages/web-components/fast-foundation/src/carousel/carousel.template.ts
+++ packages/web-components/fast-foundation/src/carousel/carousel.template.ts
@@ -6,22 +6,24 @@ export const CarouselTemplate = html<Carousel>`
 <template
         class="carousel"
         aria-roledescription="carousel"
-        tabindex="-1"
+        tabindex="0"
         ${ref("carousel")}
     >
         <slot ${slotted("items")}></slot>
         <slot name="play-toggle"></slot>
-        <div class="previous-flipper">
+        <div class="previous-flipper" @click="${(x, c) =>
+            x.handleFlipperClick(-1, c.event as MouseEvent)}">
             <slot name="previous-button">
                 <fast-flipper direction=${FlipperDirection.previous}>
             </slot>
         </div>
-        <div class="next-flipper">
+        <div class="next-flipper" @click="${(x, c) =>
+            x.handleFlipperClick(1, c.event as MouseEvent)}">
             <slot name="next-button">
                 <fast-flipper direction=${FlipperDirection.next}>
             </slot>
         </div>
-        <div class="slide-tabs" ${children("tabs")}>${repeat<Carousel>(
+        <div role="tablist" class="slide-tabs" ${children("tabs")}>${repeat<Carousel>(
     x => x.filteredItems,
     html<Carousel>`<div class="slide-tab tab-${(x, c) => c.index + 1}" />`,
     { positioning: true }
