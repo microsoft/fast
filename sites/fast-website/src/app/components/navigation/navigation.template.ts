143a09c0bde16f28097353a196e6a0a80d96dead
khamudom <khamu@microsoft.com>
2020-06-23 11:26:05 -0700

    add tab index and add trap function

--- sites/fast-website/src/app/components/navigation/navigation.template.ts
+++ sites/fast-website/src/app/components/navigation/navigation.template.ts
@@ -1,4 +1,4 @@
-import { html, when } from "@microsoft/fast-element";
+import { html, when, ref } from "@microsoft/fast-element";
 import { endTemplate, startTemplate } from "@microsoft/fast-foundation";
 import CloseIcon from "svg/icon-close.svg";
 import MenuIcon from "svg/icon-menu.svg";
@@ -11,14 +11,15 @@ export const NavigationTemplate = html<Navigation>`
     >
         ${startTemplate}
         <nav>
-            <slot></slot>
+            <slot tabindex="${x => (x.opened ? "0" : "-1")}"></slot>
         </nav>
         ${endTemplate}
         <fast-button
             class="nav-button"
             part="nav-button"
+            ${ref("closeButton")}
             appearance="stealth"
-            @click="${(x, c) => x.handleOpenNavClick(c.event)}"
+            @click="${(x, c) => x.handleOpenNavClick(c.event as MouseEvent)}"
         >
             ${when(x => !x.opened, html` ${MenuIcon} `)}
             ${when(x => x.opened, html` ${CloseIcon} `)}
--- sites/fast-website/src/app/components/navigation/navigation.ts
+++ sites/fast-website/src/app/components/navigation/navigation.ts
@@ -1,5 +1,6 @@
 import { FASTElement, observable } from "@microsoft/fast-element";
 import { applyMixins, StartEnd } from "@microsoft/fast-foundation";
+import { keyCodeEscape, keyCodeTab } from "@microsoft/fast-web-utilities";
 
 export class Navigation extends FASTElement {
     @observable
@@ -7,6 +8,10 @@ export class Navigation extends FASTElement {
 
     @observable
     public debounce: boolean = true;
+
+    public closeButton: HTMLElement;
+
+    private firstTabStop: HTMLElement | null = null;
 
     private mediaQueryList: MediaQueryList;
 
@@ -18,6 +23,24 @@ export class Navigation extends FASTElement {
         }
     };
 
+    private openModal() {
+        const navPane = document.querySelector(".site-navigation");
+        const focusableElementString = "fast-anchor";
+
+        if (navPane) {
+            let focusableElement: NodeListOf<Element> = navPane.querySelectorAll(
+                focusableElementString
+            );
+
+            this.firstTabStop = focusableElement[0] as HTMLElement;
+
+            this.closeButton.focus();
+
+            document.addEventListener("keydown", this.trapTabKey);
+            focusableElement = Array.prototype.slice.call(focusableElement);
+        }
+    }
+
     constructor() {
         super();
 
@@ -28,6 +51,34 @@ export class Navigation extends FASTElement {
     public handleOpenNavClick = (e: Event): void => {
         this.opened = !this.opened;
         this.debounce = false;
+        this.openModal();
+    };
+
+    public trapTabKey = (e: KeyboardEvent) => {
+        if (!e.defaultPrevented) {
+            switch (e.keyCode) {
+                case keyCodeTab:
+                    this.handleTabKeyDown(e);
+                    break;
+            }
+        }
+    };
+
+    public handleTabKeyDown = (e: KeyboardEvent) => {
+        if (this.firstTabStop === null || this.closeButton === null) {
+            return;
+        }
+        if (e.shiftKey) {
+            if (document.activeElement === this.firstTabStop) {
+                e.preventDefault();
+                this.closeButton.focus();
+            }
+        } else {
+            if (document.activeElement === this.closeButton) {
+                e.preventDefault();
+                this.firstTabStop.focus();
+            }
+        }
     };
 }
 
