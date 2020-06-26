4a330910c54a458f5c6594a4a5b19565787a58c9
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2020-06-26 13:56:49 -0700

    update dns notes

--- build/operations/services/create-dns.sh
+++ build/operations/services/create-dns.sh
@@ -11,10 +11,17 @@ https://docs.microsoft.com/en-us/azure/dns/dns-getstarted-cli
 https://docs.microsoft.com/en-us/azure/frontdoor/front-door-how-to-onboard-apex-domain
 https://docs.microsoft.com/en-us/azure/dns/dns-getstarted-cli#create-a-dns-record
 '
+
+# TODOs
+# [] Create DNS Zone for development and testing
+# [] Update all entries unrelated to web app dynamic creation
+# [] Gracefull fail if not in use
+
+echo "creating dns zone ..."
 az network dns zone create --resource-group $resource_group -n $dns_zone
 
-# Create record
+echo "creating dns cname records"
 az network dns record-set cname add-record -g $resource_group -z $dns_zone -n www -a 10.10.10.10
 
-# View all records
+echo "view dns records ..."
 az network dns record-set list -g $resource_group -z $dns_zone
