69806b04efc2b4e0bd6be1c006acc4be77b907c4
SamanthaAO <samanthaaorcutt@gmail.com>
2020-06-29 12:23:46 -0700

    update naming in section data to be mroe specific, breakout functions for cleaner code and reduce the amount of times that currentSection is assigned, add function that checks scroll direction before assigning current section

--- sites/fast-website/src/app/components/side-navigation/side-navigation.ts
+++ sites/fast-website/src/app/components/side-navigation/side-navigation.ts
@@ -6,7 +6,7 @@ import {
 
 export interface sectionData {
     section: string;
-    value: number;
+    intersectionRatio: number;
 }
 
 export class SideNavigation extends FASTElement {
@@ -20,33 +20,51 @@ export class SideNavigation extends FASTElement {
     public sectionArray: HTMLElement[] = [];
 
     @observable
-    public objArray: sectionData[];
+    public sectionDataArray: sectionData[];
+
+    @observable
+    public previousY: number = 0;
 
     public socialData: CommunityContentPlacementData[] = communityContentPlacementData.filter(
         x => x.header !== "Github"
     );
 
+    reduceFunc = (prev: sectionData, current: sectionData) =>
+        prev.intersectionRatio > current.intersectionRatio ? prev : current;
+
+    scrollDirectionFunc = () => {
+        const currentY: number = window.pageYOffset;
+        const scrollArray: sectionData[] = this.sectionDataArray;
+
+        if (currentY < this.previousY) {
+            scrollArray.reverse();
+        }
+
+        this.previousY = currentY;
+        this.currentSection = scrollArray.reduce(this.reduceFunc).section;
+    };
+
+    createObserver = (entries: IntersectionObserverEntry[]) => {
+        entries.forEach(entry => {
+            const intersectingSection: sectionData = this.sectionDataArray.find(
+                ({ section }) => section === entry.target.id
+            ) as sectionData;
+            intersectingSection.intersectionRatio = entry.intersectionRatio;
+        });
+
+        this.scrollDirectionFunc();
+    };
+
     loadScrollers() {
         this.sectionArray = Array.from(document.querySelectorAll("section[id]"));
 
-        this.objArray = this.sectionArray.map(x => {
-            return { section: x.id, value: 0 };
+        this.sectionDataArray = this.sectionArray.map(x => {
+            return { section: x.id, intersectionRatio: 0 };
         });
 
-        let observer = new IntersectionObserver(
-            entries => {
-                entries.forEach(entry => {
-                    const intersectingSection: sectionData = this.objArray.find(
-                        ({ section }) => section === entry.target.id
-                    ) as sectionData;
-                    intersectingSection.value = entry.intersectionRatio;
-                    this.currentSection = this.objArray.reduce((prev, current) =>
-                        prev.value > current.value ? prev : current
-                    ).section;
-                });
-            },
-            { threshold: [0, 0.2, 0.4, 0.6, 0.8] }
-        );
+        let observer = new IntersectionObserver(this.createObserver, {
+            threshold: [0, 0.2, 0.4, 0.6, 0.8],
+        });
         this.sectionArray.forEach(section => {
             observer.observe(section);
         });
