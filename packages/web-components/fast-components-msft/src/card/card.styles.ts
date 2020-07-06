35c2bbaaa408f5af32369931e5009bdb04072750
Kham Udom <khamu@microsoft.com>
2020-07-06 12:27:14 -0700

    also update border in msft

--- packages/web-components/fast-components-msft/src/card/card.styles.ts
+++ packages/web-components/fast-components-msft/src/card/card.styles.ts
@@ -1,7 +1,8 @@
 import { css } from "@microsoft/fast-element";
-import { display } from "@microsoft/fast-foundation";
+import { display, forcedColorsStylesheetBehavior } from "@microsoft/fast-foundation";
 import { elevation } from "../styles";
 import { neutralLayerCardBehavior } from "../styles/index";
+import { SystemColors } from "@microsoft/fast-web-utilities";
 export const CardStyles = css`
     ${display("block")} :host {
         --elevation: 4;
@@ -13,6 +14,16 @@ export const CardStyles = css`
         background: ${neutralLayerCardBehavior.var};
         border-radius: calc(var(--elevated-corner-radius) * 1px);
         ${elevation};
-        border: calc(var(--outline-width) * 1px) solid transparent;
     }
-`.withBehaviors(neutralLayerCardBehavior);
+`.withBehaviors(
+    neutralLayerCardBehavior,
+    forcedColorsStylesheetBehavior(
+        css`
+            :host {
+                forced-color-adjust: none;
+                background: ${SystemColors.Canvas};
+                box-shadow: 0 0 0 1px ${SystemColors.CanvasText};
+            }
+        `
+    )
+);
