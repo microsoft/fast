eef3d1eaa30172da0241e7946eea514bfd2cbddf
Stephane Comeau <scomea@microsoft.com>
2020-07-09 15:43:37 -0700

    working

--- packages/web-components/fast-foundation/src/anchored-region/resize-observer.ts
+++ packages/web-components/fast-foundation/src/anchored-region/resize-observer.ts
@@ -1,17 +1,17 @@
-import { ResizeObserverEntry } from "./resize-observer-entry";
+// import { ResizeObserverEntry } from "./resize-observer-entry";
 
-export type ConstructibleResizeObserver = new (
-    callback: ResizeObserverCallback
-) => ResizeObserverClassDefinition;
+// export type ConstructibleResizeObserver = new (
+//     callback: ResizeObserverCallback
+// ) => ResizeObserverClassDefinition;
 
-export declare class ResizeObserverClassDefinition {
-    constructor(callback: ResizeObserverCallback);
-    public observe(target: Element): void;
-    public unobserve(target: Element): void;
-    public disconnect(): void;
-}
+// export declare class ResizeObserverClassDefinition {
+//     constructor(callback: ResizeObserverCallback);
+//     public observe(target: Element): void;
+//     public unobserve(target: Element): void;
+//     public disconnect(): void;
+// }
 
-export declare type ResizeObserverCallback = (
-    entries: ResizeObserverEntry[],
-    observer: ResizeObserverClassDefinition
-) => void;
+// export declare type ResizeObserverCallback = (
+//     entries: ResizeObserverEntry[],
+//     observer: ResizeObserverClassDefinition
+// ) => void;
