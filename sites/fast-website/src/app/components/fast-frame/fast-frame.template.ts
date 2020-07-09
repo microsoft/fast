650464699627750b647510b6d02f8b651f0559f7
Kham Udom <khamu@microsoft.com>
2020-07-09 16:09:38 -0700

    change aria label to read hex color on background-color and accent-color radio items

--- sites/fast-website/src/app/components/fast-frame/fast-frame.template.ts
+++ sites/fast-website/src/app/components/fast-frame/fast-frame.template.ts
@@ -60,7 +60,7 @@ export const FastFrameTemplate = html<FastFrame>`
                                     html<string>`
                                         <site-color-swatch
                                             tabindex="0"
-                                            aria-label="background color"
+                                            aria-label="${x => x}"
                                             value="${x => x}"
                                             background-color="${x => x}"
                                             checked="${(x, c) =>
@@ -83,7 +83,7 @@ export const FastFrameTemplate = html<FastFrame>`
                                     html<string>`
                                         <site-color-swatch
                                             tabindex="0"
-                                            aria-label="accent color"
+                                            aria-label="${x => x}"
                                             value="${x => x}"
                                             background-color="${x => x}"
                                             checked="${(x, c) =>
