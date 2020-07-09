b133516f5aed8bc31c119872aa239b485215af8c
Seth Donohue <sethdonohue@Admins-MBP.guest.corp.microsoft.com>
2020-07-09 13:14:04 -0700

    add slotted example, set tabindex=-1 for slotted items when in tabbed pattern, reordered for proper tab sequence, fixed tab not focusing on first keyboard increment

---
+++ sites/site-utilities/src/definitions/fast-components/fast-carousel.definition.ts
@@ -0,0 +1,88 @@
+import { WebComponentDefinition } from "@microsoft/fast-tooling/dist/data-utilities/web-component";
+import { DataType } from "@microsoft/fast-tooling";
+
+export const fastCarouselDefinition: WebComponentDefinition = {
+    version: 1,
+    tags: [
+        {
+            name: "fast-carousel",
+            description: "The FAST Carousel element",
+            attributes: [
+                {
+                    name: "autoplay",
+                    type: DataType.boolean,
+                    description: "The autoplay attribute",
+                    default: true,
+                    required: false,
+                },
+                {
+                    name: "autoplay-interval",
+                    description: "The autoplay-interval attribute",
+                    type: DataType.number,
+                    default: 6000,
+                    required: false,
+                },
+                {
+                    name: "loop",
+                    description: "The loop attribute",
+                    type: DataType.boolean,
+                    default: true,
+                    required: false,
+                },
+                {
+                    name: "aria-labelledby",
+                    description: "The aria-labelledby attribute",
+                    type: DataType.string,
+                    default: undefined,
+                    required: false,
+                },
+                {
+                    name: "aria-label",
+                    description: "The aria-labelledby attribute",
+                    type: DataType.string,
+                    default: undefined,
+                    required: false,
+                },
+                {
+                    name: "paused",
+                    description: "The paused attribute",
+                    type: DataType.boolean,
+                    default: false,
+                    required: false,
+                },
+                {
+                    name: "activeid",
+                    description: "The activeid attribute",
+                    type: DataType.string,
+                    default: undefined,
+                    required: false,
+                },
+                {
+                    name: "tabbed",
+                    description: "The tabbbed attribute",
+                    type: DataType.boolean,
+                    default: true,
+                    required: false,
+                },
+            ],
+            slots: [
+                {
+                    name: "",
+                    description: "The default slot",
+                },
+                {
+                    name: "rotation-control",
+                    description: "The rotation control slot for the pause/play button",
+                },
+                {
+                    name: "previous-button",
+                    description: "The control slot for the previous slide button",
+                },
+                {
+                    name: "next-button",
+                    description: "The control slot for the next slide button",
+                },
+            ],
+        },
+    ],
+};
