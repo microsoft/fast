9568f8766b2ce9de9661992d41351235372df595
eljefe223 <jes@microsoft.com>
2020-07-10 07:02:14 -0700

    fixed all elvations

--- packages/web-components/fast-components/src/menu/menu.styles.ts
+++ packages/web-components/fast-components/src/menu/menu.styles.ts
@@ -12,7 +12,7 @@ export const MenuStyles = css`
         background: ${neutralLayerFloatingBehavior.var};
         border: calc(var(--outline-width) * 1px) solid transparent;
         border-radius: var(--elevated-corner-radius);
-        ${elevation};
+        ${elevation}
         margin: 0;
         border-radius: calc(var(--corner-radius) * 1px);
         padding: calc(var(--design-unit) * 1px) 0;
