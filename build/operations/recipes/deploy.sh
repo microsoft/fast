e573818022c2168111003b72c51f46671bac74c3
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2020-07-29 07:18:13 -0700

    adds todo notes for better quality

--- build/operations/recipes/deploy.sh
+++ build/operations/recipes/deploy.sh
@@ -20,12 +20,20 @@ This will deploy from staging to production via Azure Web App Slot swapping.
 source inputs.sh --debug true --subscription $subscription
 
 ## JOBS
-echo "deploying from staging to production in west region ..."
-az webapp deployment slot swap --resource-group fast-westus-rg --name www-west-app --slot stage --action swap --target-slot production
+# Set IP address exception to match local IP address of the person running the script
+# Test access at https://www-east-app.azurewebsites.net 
 
 echo "deploying from staging to production in east region ..."
 az webapp deployment slot swap --resource-group fast-eastus-rg --name www-east-app --slot stage --action swap --target-slot production
 
+# Validate updated succeedeed.  Prompt user to choose yes or no based on success to proceed to primary
+# Set IP address exception to match local IP address of the person running the script
+# Test access at https://www-west-app.azurewebsites.net 
+
+echo "deploying from staging to production in west region ..."
+az webapp deployment slot swap --resource-group fast-westus-rg --name www-west-app --slot stage --action swap --target-slot production
+
+# Validate updated succeedeed.  Prompt user to choose yes or no based on success to proceed to primary
 
 echo "${bold}${green}Success !!!${reset}${unbold}"
 
