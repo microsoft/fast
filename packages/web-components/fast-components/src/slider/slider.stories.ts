ab2dc4b82d85910b2a85fc1b3f8a2c4ef1dc88b1
Mark Jones <marjon@microsoft.com>
2020-07-30 14:28:16 -0700

    refactored test setup code

--- packages/web-components/fast-components/src/slider/slider.stories.ts
+++ packages/web-components/fast-components/src/slider/slider.stories.ts
@@ -10,20 +10,10 @@ FASTDesignSystemProvider;
 
 addons.getChannel().addListener(STORY_RENDERED, (name: string) => {
     if (name.toLowerCase().endsWith("slider")) {
-        const slider1: HTMLElement | null = document.getElementById("switcher");
-        if (slider1) {
-            (slider1 as FASTSlider).valueTextFormatter = valueTextFormatter;
-        }
-
-        const slider2: HTMLElement | null = document.getElementById("switcher2");
-        if (slider2) {
-            (slider2 as FASTSlider).valueTextFormatter = valueTextFormatter;
-        }
-
-        const slider3: HTMLElement | null = document.getElementById("slider1");
-        if (slider3) {
-            (slider3 as FASTSlider).valueTextFormatter = valueTextFormatter;
-        }
+        ["switcher", "switcher2", "slider1"].forEach(x => {
+            const slider = document.getElementById(x);
+            slider && ((slider as FASTSlider).valueTextFormatter = valueTextFormatter);
+        });
     }
 });
 
