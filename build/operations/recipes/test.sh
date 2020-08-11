83c32790e7dcdf96c4f6ce0c169537dc002338a6
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2020-08-11 14:24:49 -0700

    update to config files

--- build/operations/recipes/test.sh
+++ build/operations/recipes/test.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-: 'AZURE FAST DEPLOYING
-This will deploy from staging to production via Azure Web App Slot swapping.
-
+: 'AZURE FAST TEST
+Enables access to west region to perform maintenance, tuning, and testing of staging servers. Upon
+completion, disables access and resumes normal operations.
 '
 # TODOs
-# [] if deployment fails, add revert logic. Probably easiest to include as another select option. [Y] to resume (default), [N] to cancel, [R] to revert.
 
 # GLOBAL Configurations
     source config.sh
 
     # Web App valid values
     declare -a subscriptions=("production" "development")
-    declare -a locations=("west" "east")
+    declare -a locations=("west")
 
     # Defaults
     product=fast
     subscription=production
 
 ## SHELL Arguments
-    echo "${bold}${green}DEPLOYMENT started ...${reset}"
+    echo "${bold}${green}TESTING started ...${reset}"
     echo "${green}Predefined defaults found${reset}" && echo ""
     source inputs.sh --debug false --subscription $subscription
 
 ## TASK Process
-    echo "${bold}${green}Application${reset} Select an application to deploy:"
+    echo "${bold}${green}Application${reset} Select an application to test:"
     status=false
     select application in app color create explore motion www exit
     do
@@ -33,7 +32,7 @@ This will deploy from staging to production via Azure Web App Slot swapping.
             app | color | create | explore | motion | www)
 
                 echo ""
-                echo "${green}Deploying ${bold}$application${reset} ${green}now ...${reset}"
+                echo "${green}Testing ${bold}$application${reset} ${green}now ...${reset}"
             
                 for location in ${locations[@]}; do
                     status=true
@@ -41,8 +40,8 @@ This will deploy from staging to production via Azure Web App Slot swapping.
                     # configure resource group and IP address
                     resource_group=fast-$location'us'-rg
                     public_ip="$(wget -qO- ipinfo.io/ip)"/16
-                    [[ $debug == true ]] && echo "public IP: ${public_ip}" && echo "performing release deployment into $resource_group ..."
-                    echo "${green}.. deploying into $location region started ...${reset}"
+                    [[ $debug == true ]] && echo "public IP: ${public_ip}" && echo "performing testing into $resource_group ..."
+                    echo "${green}.. testing on $location region started ...${reset}"
 
                     rule_name="Front Door IPv4 IP Testing"
                     rule_description="Allow public IP access for testing from local system"
@@ -53,20 +52,14 @@ This will deploy from staging to production via Azure Web App Slot swapping.
                     echo ""
                     az webapp config access-restriction add --priority 300 \
                         --resource-group $resource_group \
-                        --name $new_app_name \
+                        --name $new_app_name/slots/stage \
                         --description "$rule_description" \
                         --rule-name "$rule_name" \
                         --action Allow \
                         --ip-address ${public_ip}
 
-                    echo ".. swapping from staging to production in $resource_group ..."
-                    az webapp deployment slot swap --resource-group $resource_group --name $new_app_name --slot stage --action swap --target-slot production
-
                     echo ""
                     echo "${yellow}begin testing ..."
-                    echo ".. verify website on production at https://$new_app_name.azurewebsites.net"
-                    echo ".. verify files on production at https://$new_app_name.scm.azurewebsites.net/webssh/host"
-                    echo ""
                     echo ".. verify website on staging at https://$new_app_name-stage.azurewebsites.net"
                     echo ".. verify files on staging at https://$new_app_name-stage.scm.azurewebsites.net/webssh/host"
                     echo "end testing ...${reset}"
@@ -76,11 +69,11 @@ This will deploy from staging to production via Azure Web App Slot swapping.
                     az webapp config access-restriction remove -g $resource_group -n $new_app_name --rule-name "$rule_name"
                     echo ".. close network access for $public_ip ..."
                     
-                    echo "${green}.. deploying into $location region finished ...${reset}"
+                    echo "${green}.. testing on $location region finished ...${reset}"
                     echo ""
 
                 done
-                echo "${bold}${green}DEPLOYMENT finished. Continue with another?${reset}" 
+                echo "${bold}${green}TESTING finished. Continue with another?${reset}" 
                 ;;
 
             exit)
@@ -88,9 +81,9 @@ This will deploy from staging to production via Azure Web App Slot swapping.
                 echo ""
                 if [ status == true ];
                 then
-                    echo "${bold}${green}DEPLOYMENT finished.${reset}" 
+                    echo "${bold}${green}TESTING finished.${reset}" 
                 else
-                    echo "${bold}${green}DEPLOYMENT cancelled.${reset}" 
+                    echo "${bold}${green}TESTING cancelled.${reset}" 
                 fi    
                 break ;;
 
