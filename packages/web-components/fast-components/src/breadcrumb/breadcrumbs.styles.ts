78b2e3c041eb171f046721cdb4d6ca868ffc47c5
Kham Udom <37851220+khamudom@users.noreply.github.com>
2020-08-13 16:44:37 -0700

    remove transparent background, update to use base on font-size and line-height on breadcrumb, removed word widget

--- packages/web-components/fast-components/src/breadcrumb/breadcrumbs.styles.ts
+++ packages/web-components/fast-components/src/breadcrumb/breadcrumbs.styles.ts
@@ -5,7 +5,7 @@ export const BreadcrumbStyles = css`
     ${display("inline-block")} :host {
         box-sizing: border-box;
         font-family: var(--body-font);
-        font-size: var(--type-ramp-minus-1-font-size);
-        line-height: var(--type-ramp-minus-1-height);
+        font-size: var(--type-ramp-base-font-size);
+        line-height: var(--type-ramp-base-line-height);
     }
 `;
