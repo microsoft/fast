f51112beed5d123c0930be9ea73c5a60e12d9339
Rob Eisenberg <rob.eisenberg@microsoft.com>
2020-08-26 10:58:49 -0400

    fix(fast-element): do not remove user comments when compiling templates

--- packages/web-components/fast-element/src/__test__/helpers.ts
+++ packages/web-components/fast-element/src/__test__/helpers.ts
@@ -1,6 +1,10 @@
-export function toHTML(node: Node): string {
+export function toHTML(node: Node, preserveCommentMarkup = false): string {
     return Array.from(node.childNodes)
         .map((x: any) => {
+            if (preserveCommentMarkup && x.nodeType === 8) {
+                return `<!--${x.textContent}-->`;
+            }
+
             return x.outerHTML || x.textContent;
         })
         .join("");
