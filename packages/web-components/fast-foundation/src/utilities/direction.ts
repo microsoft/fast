3a9da1f035fb1d8ae4993896ed2ee01207d75fb0
Seth Donohue <sethdonohue@Admins-MBP.guest.corp.microsoft.com>
2020-09-03 11:47:16 -0700

    add tsdoc release tag to getDirection

--- packages/web-components/fast-foundation/src/utilities/direction.ts
+++ packages/web-components/fast-foundation/src/utilities/direction.ts
@@ -1,5 +1,11 @@
 import { Direction } from "@microsoft/fast-web-utilities";
 
+/**
+ * a method to determine the current localization direction of the view
+ * @param rootNode - the HTMLElement to begin the query from, usually "this" when used in a component controller
+ * @public
+ */
+
 export const getDirection = (rootNode: HTMLElement): Direction => {
     const dirNode: HTMLElement | null = rootNode.parentElement!.closest("[dir]");
     return dirNode !== null && dirNode.dir === "rtl" ? Direction.rtl : Direction.ltr;
