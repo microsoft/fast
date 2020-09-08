e76237048d53aaf88ad548ad1fa7c18917d36deb
Nicholas Rice <3213292+nicholasrice@users.noreply.github.com>
2020-09-08 14:25:57 -0700

    Update packages/web-components/fast-foundation/src/form-associated/form-associated.ts

--- packages/web-components/fast-foundation/src/form-associated/form-associated.ts
+++ packages/web-components/fast-foundation/src/form-associated/form-associated.ts
@@ -491,7 +491,7 @@ export abstract class FormAssociated<
             case keyCodeEnter:
                 if (this.form instanceof HTMLFormElement) {
                     // Implicit submission
-                    const defaultButton = this.form.querySelector("[type=submit]") as HTMLElement;
+                    const defaultButton = this.form.querySelector("[type=submit]") as (HTMLElement | null);
                     defaultButton?.click();
                 }
                 break;
