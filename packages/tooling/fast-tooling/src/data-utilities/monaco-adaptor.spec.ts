032f81528cdb0eaf843aeb923a27556336efcc59
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-09-16 15:06:45 -0700

    feat: add monaco adaptor and adaptor actions

---
+++ packages/tooling/fast-tooling/src/data-utilities/monaco-adaptor.spec.ts
@@ -0,0 +1,323 @@
+import {
+    DataDictionary,
+    MessageSystem,
+    MessageSystemType,
+    Register,
+} from "../message-system";
+import { MonacoAdaptor } from "./monaco-adaptor";
+import { mapDataDictionaryToMonacoEditorHTML } from "./monaco";
+import { MonacoAdaptorAction } from "./monaco-adaptor-action";
+
+describe("MonacoAdaptor", () => {
+    test("should not throw", () => {
+        expect(() => {
+            const messageSystem = new MessageSystem({
+                webWorker: "",
+                dataDictionary: [
+                    {
+                        foo: {
+                            schemaId: "foo",
+                            data: undefined,
+                        },
+                    },
+                    "foo",
+                ],
+                schemaDictionary: {
+                    foo: {},
+                },
+            });
+
+            new MonacoAdaptor({
+                messageSystem,
+            });
+        }).not.toThrow();
+    });
+    test("should not throw if the message system is undefined", () => {
+        expect(() => {
+            new MonacoAdaptor({
+                messageSystem: undefined,
+            });
+        }).not.toThrow();
+    });
+    test("should register to the message system", () => {
+        const messageSystem = new MessageSystem({
+            webWorker: "",
+            dataDictionary: [
+                {
+                    foo: {
+                        schemaId: "foo",
+                        data: undefined,
+                    },
+                },
+                "foo",
+            ],
+            schemaDictionary: {
+                foo: {},
+            },
+        });
+
+        expect(messageSystem["register"].size).toEqual(0);
+
+        new MonacoAdaptor({
+            messageSystem,
+        });
+
+        expect(messageSystem["register"].size).toEqual(1);
+    });
+    test("should deregister from the message system", () => {
+        const messageSystem = new MessageSystem({
+            webWorker: "",
+            dataDictionary: [
+                {
+                    foo: {
+                        schemaId: "foo",
+                        data: undefined,
+                    },
+                },
+                "foo",
+            ],
+            schemaDictionary: {
+                foo: {},
+            },
+        });
+
+        expect(messageSystem["register"].size).toEqual(0);
+
+        const shortcuts: MonacoAdaptor = new MonacoAdaptor({
+            messageSystem,
+        });
+
+        expect(messageSystem["register"].size).toEqual(1);
+
+        shortcuts.destroy();
+
+        expect(messageSystem["register"].size).toEqual(0);
+    });
+    test("should fire an action when a matching MessageSystemType is included", () => {
+        let expectedValue = [];
+        const dataDictionary: DataDictionary<unknown> = [
+            {
+                div: {
+                    schemaId: "div",
+                    data: {},
+                },
+            },
+            "div",
+        ];
+        const schemaDictionary = {
+            div: {
+                id: "div",
+                $id: "div",
+                type: "object",
+                mapsToTagName: "div",
+            },
+        };
+        const messageSystem = new MessageSystem({
+            webWorker: "",
+        });
+        new MonacoAdaptor({
+            messageSystem,
+            actions: [
+                new MonacoAdaptorAction({
+                    id: "foo",
+                    action: config => {
+                        expectedValue = config.getMonacoModelValue();
+                    },
+                    messageSystemType: MessageSystemType.initialize,
+                }),
+            ],
+        });
+
+        messageSystem["register"].forEach((registeredItem: Register) => {
+            registeredItem.onMessage({
+                data: {
+                    type: MessageSystemType.initialize,
+                    dataDictionary,
+                    schemaDictionary,
+                },
+            } as any);
+        });
+
+        expect(expectedValue).toEqual([
+            mapDataDictionaryToMonacoEditorHTML(dataDictionary, schemaDictionary),
+        ]);
+    });
+    test("should fire an action when the corresponding id is used", () => {
+        const dataDictionary: DataDictionary<unknown> = [
+            {
+                div: {
+                    schemaId: "div",
+                    data: {},
+                },
+            },
+            "div",
+        ];
+        const schemaDictionary = {
+            div: {
+                id: "div",
+                $id: "div",
+                type: "object",
+                mapsToTagName: "div",
+            },
+        };
+        const messageSystem = new MessageSystem({
+            webWorker: "",
+        });
+        const runAction = jest.fn();
+        const monacoAdaptor = new MonacoAdaptor({
+            messageSystem,
+            actions: [
+                new MonacoAdaptorAction({
+                    id: "foo",
+                    action: runAction,
+                }),
+            ],
+        });
+
+        messageSystem["register"].forEach((registeredItem: Register) => {
+            registeredItem.onMessage({
+                data: {
+                    type: MessageSystemType.initialize,
+                    dataDictionary,
+                    schemaDictionary,
+                },
+            } as any);
+        });
+
+        monacoAdaptor.action("foo").run();
+
+        expect(runAction).toHaveBeenCalledTimes(1);
+    });
+    test("should update the monaco value", () => {
+        const dataDictionary: DataDictionary<unknown> = [
+            {
+                div: {
+                    schemaId: "div",
+                    data: {},
+                },
+            },
+            "div",
+        ];
+        const schemaDictionary = {
+            div: {
+                id: "div",
+                $id: "div",
+                type: "object",
+                mapsToTagName: "div",
+            },
+        };
+        const messageSystem = new MessageSystem({
+            webWorker: "",
+        });
+        const monacoAdaptor = new MonacoAdaptor({
+            messageSystem,
+            actions: [
+                new MonacoAdaptorAction({
+                    id: "foo",
+                    action: config => {
+                        config.updateMonacoModelValue(["bar"]);
+                    },
+                }),
+            ],
+        });
+
+        messageSystem["register"].forEach((registeredItem: Register) => {
+            registeredItem.onMessage({
+                data: {
+                    type: MessageSystemType.initialize,
+                    dataDictionary,
+                    schemaDictionary,
+                },
+            } as any);
+        });
+
+        monacoAdaptor.action("foo").run();
+
+        expect(monacoAdaptor["monacoModelValue"]).toEqual(["bar"]);
+    });
+    test("should update the dataDictionary to correct values when the monaco value has been updated", () => {
+        const callback = jest.fn();
+        const dataDictionary: DataDictionary<unknown> = [
+            {
+                div: {
+                    schemaId: "div",
+                    data: {},
+                },
+            },
+            "div",
+        ];
+        const schemaDictionary = {
+            div: {
+                id: "div",
+                $id: "div",
+                type: "object",
+                mapsToTagName: "div",
+            },
+            ul: {
+                id: "ul",
+                $id: "ul",
+                type: "object",
+                mapsToTagName: "ul",
+            },
+            text: {
+                id: "text",
+                $id: "text",
+                type: "string",
+            },
+        };
+        const messageSystem = new MessageSystem({
+            webWorker: "",
+        });
+        messageSystem.postMessage = callback;
+        const monacoAdaptor = new MonacoAdaptor({
+            messageSystem,
+            actions: [
+                new MonacoAdaptorAction({
+                    id: "foo",
+                    action: config => {
+                        config.updateMonacoModelValue(["<ul>", "foobar", "</ul>"]);
+                    },
+                }),
+            ],
+        });
+        messageSystem["register"].forEach((registeredItem: Register) => {
+            registeredItem.onMessage({
+                data: {
+                    type: MessageSystemType.initialize,
+                    dataDictionary,
+                    schemaDictionary,
+                },
+            } as any);
+        });
+
+        monacoAdaptor.action("foo").run();
+
+        const updatedDataDictionary = callback.mock.calls[0][0].dataDictionary;
+        const root = updatedDataDictionary[1];
+        const textId = updatedDataDictionary[0][updatedDataDictionary[1]].data.Slot[0].id;
+
+        expect(updatedDataDictionary).toEqual([
+            {
+                [root]: {
+                    schemaId: "ul",
+                    data: {
+                        Slot: [
+                            {
+                                id: textId,
+                            },
+                        ],
+                    },
+                },
+                [textId]: {
+                    schemaId: "text",
+                    parent: {
+                        id: root,
+                        dataLocation: "Slot",
+                    },
+                    data: "foobar",
+                },
+            },
+            root,
+        ]);
+    });
+});
---
+++ packages/tooling/fast-tooling/src/data-utilities/monaco-adaptor.ts
@@ -0,0 +1,111 @@
+import {
+    InitializeMessageIncoming,
+    MessageSystemType,
+    SchemaDictionary,
+} from "../message-system";
+import { mapDataDictionaryToMonacoEditorHTML } from "./monaco";
+import {
+    MessageSystemUtility,
+    MessageSystemUtilityConfig,
+} from "./message-system-utility";
+import {
+    MonacoAdaptorAction,
+    MonacoAdaptorActionCallbackConfig,
+} from "./monaco-adaptor-action";
+import { mapVSCodeParsedHTMLToDataDictionary } from "./mapping.vscode-html-languageservice";
+
+export type actionCallback = () => void;
+
+const monacoAdaptorId: string = "monaco-adaptor";
+
+export class MonacoAdaptor extends MessageSystemUtility<
+    MonacoAdaptorActionCallbackConfig
+> {
+    private monacoModelValue: string[];
+    private schemaDictionary: SchemaDictionary;
+
+    constructor(config: MessageSystemUtilityConfig<MonacoAdaptorActionCallbackConfig>) {
+        super();
+
+        this.registerMessageSystem(config);
+        this.addConfigToActions();
+    }
+
+    /**
+     * Handles messages from the message system
+     */
+    handleMessageSystem = (e: MessageEvent): void => {
+        switch (e.data.type) {
+            case MessageSystemType.initialize:
+                if (e.data.from !== monacoAdaptorId) {
+                    this.schemaDictionary = e.data.schemaDictionary;
+                    this.monacoModelValue = [
+                        // TODO: format this with vscode-html-languageservice
+                        mapDataDictionaryToMonacoEditorHTML(
+                            e.data.dataDictionary,
+                            e.data.schemaDictionary
+                        ),
+                    ];
+
+                    this.registeredActions.forEach((action: MonacoAdaptorAction) => {
+                        if (action.matches(e.data.type)) {
+                            action.invoke();
+                        }
+                    });
+                }
+                break;
+        }
+    };
+
+    /**
+     * Gets the action config
+     */
+    getActionConfig = (
+        messageSystemType: MessageSystemType
+    ): MonacoAdaptorActionCallbackConfig => {
+        return {
+            getMonacoModelValue: this.getMonacoModelValue,
+            updateMonacoModelValue: this.updateMonacoModelValue,
+            messageSystemType,
+        };
+    };
+
+    /**
+     * Adds all config options to registered actions
+     */
+    private addConfigToActions(): void {
+        this.registeredActions.forEach((registeredAction: MonacoAdaptorAction) => {
+            registeredAction.addConfig({
+                getMonacoModelValue: this.getMonacoModelValue,
+                updateMonacoModelValue: this.updateMonacoModelValue,
+                messageSystemType: registeredAction.getMessageSystemType(),
+            });
+        });
+    }
+
+    /**
+     * Retrieve the Monaco Model value
+     */
+    private getMonacoModelValue = (): string[] => {
+        return this.monacoModelValue;
+    };
+
+    /**
+     * Update the Monaco Model value
+     */
+    private updateMonacoModelValue = (value: string[]): void => {
+        this.monacoModelValue = value;
+
+        this.messageSystem.postMessage({
+            type: MessageSystemType.initialize,
+            dataDictionary: mapVSCodeParsedHTMLToDataDictionary({
+                value,
+                schemaDictionary: this.schemaDictionary,
+            }),
+            schemaDictionary: this.schemaDictionary,
+            options: {
+                from: monacoAdaptorId,
+            },
+        } as InitializeMessageIncoming);
+    };
+}
