43b86fe5535ecdd68a16b860ab24c532d90c051b
Chris Holt <chhol@microsoft.com>
2020-09-16 15:22:35 -0700

    add tab tests

---
+++ packages/web-components/fast-foundation/src/tab/tab.spec.ts
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { Tab, TabTemplate as template } from "./index";
+import { fixture } from "../fixture";
+import { DOM, customElement, html } from "@microsoft/fast-element";
+
+describe("Tab", () => {
+    const name = "Tab";
+
+    @customElement({
+        name: "fast-tab",
+        template,
+    })
+    class FASTTab extends Tab {}
+
+    it("should have a role of `tab`", async () => {
+        const { element, connect, disconnect } = await fixture<FASTTab>("fast-tab");
+
+        await connect();
+
+        expect(element.getAttribute("role")).to.equal("tab");
+
+        await disconnect();
+    });
+
+    it("should have an attribute of `tab`", async () => {
+        const { element, connect, disconnect } = await fixture<FASTTab>("fast-tab");
+
+        await connect();
+
+        expect(element.getAttribute("slot")).to.equal("tab");
+
+        await disconnect();
+    });
+});
