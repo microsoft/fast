55e51b782d2bf82341a65bbcb138e9f498f5a78f
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2020-09-29 16:43:03 -0700

    create functions file with functions for code reuse

--- build/operations/functions.sh
+++ build/operations/functions.sh
@@ -1,8 +1,80 @@
 #!/bin/bash
+## Accepting parameters by $1, $2, etc
 
-function debug_print() {
+function debugPrint() {
 
-    [[ $debug == true ]] && echo "${bold}${green}$1"${reset}${unbold}
-    [[ $debug == true ]] && echo $2
+    if [[ $debug == true ]];
+    then
+        echo "debugging on"
+    else
+        echo "debuging off"
+    fi
 
+}
+
+function getSubscription() {
+    source inputs.sh --debug false --subscription $subscription
+}
+
+function setApplication() {
+    if [ -z "$application" ];
+    then
+        echo "Select an ${bold}${green}Application${reset}:"    
+        select application in ${applications[@]}
+        do
+            case $application in
+                app | color | create | explore | motion | www)
+                    source inputs.sh -a $application
+                    break ;;
+                exit)
+                    echo "${bold}${green}cancelled.${reset}" 
+                    exit ;;
+                *)
+                    echo "${red}invalid entry, try again${reset}" ;;
+            esac
+        done
+    fi
+}
+
+function setEnvironment() {
+    if [ -z "$environment" ];
+    then
+        echo "Select an ${bold}${green}Environment${reset}:"
+        select environment in ${environments[@]}
+        do
+            case $environment in
+                production | development)
+                    source inputs.sh -e $environment -s $environment 
+                    break ;;
+                staging)
+                    source inputs.sh -e $environment -s production
+                    break ;;
+                exit)
+                    echo "${bold}${green}cancelled.${reset}" 
+                    exit ;;
+                *)
+                    echo "${red}invalid entry, try again${reset}" ;;
+            esac
+        done
+    fi
+}
+
+function setRegion() {
+    if [ -z "$region" ];
+    then
+        echo "Select an ${bold}${green}Region${reset}:"    
+        select region in ${regions[@]}
+        do
+            case $region in
+                westus | eastus | centralus)  
+                    source inputs.sh -r $region
+                    break ;;
+                exit)
+                    echo "${bold}${green}cancelled.${reset}" 
+                    exit ;;
+                *)
+                    echo "${red}invalid entry, try again${reset}" ;;
+            esac
+        done
+    fi
 }
