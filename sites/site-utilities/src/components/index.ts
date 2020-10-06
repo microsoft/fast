2f6d280a80ab4a8cc8d641a751af5da85b61ad1a
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-10-06 14:36:49 -0700

    chore: refactor logic from explore site into a site-utilities abstract component (#3995)
    # Description
    
    <!--- Describe your changes. -->
    This change removes core logic from the explorer and places it in a new abstract class `Editor` so that it can be shared with the creator site.
    
    It includes the following changes:
    - Add declarations to the site-utilities for typescript (see tsconfig update)
    - Update `width` and `height` to the more accurate `viewerWidth` and `viewerHeight`
    - Remove any global styles from jssStylesheets and place them in the `.css` file for the `Editor`
    - Remove any logic that will need to be shared with the Creator and place it into the `Editor` class
    
    The refactor should not have changed any of the site functionality.
    
    ## Motivation & context
    
    <!--- What problem does this change solve? -->
    <!--- Provide a link if you are addressing an open issue. -->
    
    ## Issue type checklist
    
    <!--- What type of change are you submitting? Put an x in the box that applies: -->
    
    - [x] **Chore**: A change that does not impact distributed packages.
    - [ ] **Bug fix**: A change that fixes an issue, link to the issue above.
    - [ ] **New feature**: A change that adds functionality.
    
    **Is this a breaking change?**
    - [ ] This change causes current functionality to break.
    
    <!--- If yes, describe the impact. -->
    
    **Adding or modifying component(s) in `@microsoft/fast-components` checklist**
    
    <!-- Do your changes add or modify components in the @microsoft/fast-components package? Put an x in the box that applies: -->
    
    - [ ] I have added a new component
    - [ ] I have modified an existing component
    - [ ] I have updated the [definition file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#definition)
    - [ ] I have updated the [configuration file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#configuration)
    
    ## Process & policy checklist
    
    <!--- Review the list and check the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.
    
    <!---
    Formatting guidelines:
    
    Accepted peer review title format:
    <type>: <description>
    
    Example titles:
        chore: add unit tests for all components
        feat: add a border radius to button
        fix: update design system to use 3px border radius
    
        <type> is required to be one of the following:
    
            - chore: A change that does not impact distributed packages.
            - fix: A change which fixes an issue.
            - feat: A that adds functionality.
    
        <description> is required for the CHANGELOG and speaks to what the user gets from this PR:
    
            - Be concise.
            - Use all lowercase characters. 
            - Use imperative, present tense (e.g. `add` not `adds`.)
            - Do not end your description with a period.
            - Avoid redundant words.
    
    For additional information regarding working on FAST, check out our documentation site:
    https://www.fast.design/docs/community/contributor-guide
    -->

--- sites/site-utilities/src/components/index.ts
+++ sites/site-utilities/src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./accent-color-picker";
 export * from "./dimension";
 export * from "./direction-switch";
+export * from "./editor";
 export * from "./logo";
 export * from "./theme-selector";
 export * from "./transparency-toggle";
