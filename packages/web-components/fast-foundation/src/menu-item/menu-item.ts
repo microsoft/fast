6ce13c498038f7c3c721680cfb225e10bf511c21
John Kreitlow <john.kreitlow@microsoft.com>
2020-10-14 10:57:41 -0700

    Fix doc mismatch for menu item

--- packages/web-components/fast-foundation/src/menu-item/menu-item.ts
+++ packages/web-components/fast-foundation/src/menu-item/menu-item.ts
@@ -7,7 +7,7 @@ import { MenuItemRole } from "./menu-item.options";
 export { MenuItemRole };
 
 /**
- * A Switch Custom HTML Element.
+ * A Menu Item Custom HTML Element.
  * Implements {@link https://www.w3.org/TR/wai-aria-1.1/#menuitem | ARIA menuitem }, {@link https://www.w3.org/TR/wai-aria-1.1/#menuitemcheckbox | ARIA menuitemcheckbox}, or {@link https://www.w3.org/TR/wai-aria-1.1/#menuitemradio | ARIA menuitemradio }.
  *
  * @public
