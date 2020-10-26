e2e142c8ab91eb10906e74853f34afd5081ca12b
Kham Udom <37851220+khamudom@users.noreply.github.com>
2020-10-26 13:51:28 -0700

    feat: add fast-breadcrumb and fast-breadcrumb-item web components (#3627)
    * create fast-breadcrumb and fast-breadcrumb-item files
    
    * update TSDoc in breadcrumb-item template
    
    * add slot name to control slot. removed listitem style. add missing closed bracket in breadcrumb
    
    * add default slotted directive. add more examples.
    
    * update readme and spec files
    
    * adding files for component explorer. adding spec file
    
    * update style to last item with no href. update margins on start slot, end slot, and separator. fix typos.
    
    * add delegatesFocus. add DelegatesARIABreadcrumbItem extending from ARIAGlobalStatesAndProperties and aria attributes to anchor. update README and examples
    
    * update breadcrumb-item template to use when directive
    
    * update config and add definition files
    
    * remove transparent background, update to use base on font-size and line-height on breadcrumb, removed word widget
    
    * update spec, README and examples, based on new breadcrumb-item template
    
    * removed separator attribute from breadcrumb and just set the default in breadcrumb-item
    
    * add class and part to breadcrumb list
    
    * removed aria exanded
    
    * use DelegatesARIALink for mixin in breadcrumb item
    
    * rebased, update component definition and add open-ui definition
    
    * import AnchorTemplate and update logic on aria-current
    
    * add breadcrumb specs
    
    * add more test to breadcrumb item and add DelegatesARIALink
    
    * rename showSeparator to separator, update specs and add anchor test
    
    * add testing to check for role list and listitem
    
    * update readme files
    
    * add scenario file and update breadcrumb config file
    
    * update test specs including adding one for separator
    
    * check for slottedBreadcrumbItems
    
    * break up function to check for href and add aria-current. added test to check for href
    
    * update find href function
    
    * update file name on breadcrumb stories and styles file name

---
+++ packages/web-components/fast-components/src/breadcrumb-item/breadcrumb-item.styles.ts
@@ -0,0 +1,124 @@
+import { css } from "@microsoft/fast-element";
+import {
+    display,
+    focusVisible,
+    forcedColorsStylesheetBehavior,
+} from "@microsoft/fast-foundation";
+import {
+    accentForegroundActiveBehavior,
+    accentForegroundHoverBehavior,
+    accentForegroundRestBehavior,
+    neutralForegroundRestBehavior,
+    heightNumber,
+} from "../styles/index";
+import { SystemColors } from "@microsoft/fast-web-utilities";
+
+export const BreadcrumbItemStyles = css`
+    ${display("inline-flex")} :host {
+        background: transparent;
+        box-sizing: border-box;
+        font-family: var(--body-font);
+        font-size: var(--type-ramp-base-font-size);
+        fill: currentColor;
+        line-height: var(--type-ramp-base-line-height);
+        min-width: calc(${heightNumber} * 1px);
+        outline: none;
+    }
+
+    .listitem {
+        display: flex;
+        align-items: center;
+    }
+
+    .separator {
+        margin: 0 6px;
+    }
+
+    .control {
+        align-items: center;
+        box-sizing: border-box;
+        color: ${accentForegroundRestBehavior.var};
+        cursor: pointer;
+        display: flex;
+        fill: inherit;
+        outline: none;
+        text-decoration: none;
+        white-space: nowrap;
+    }
+
+    .control:hover {
+        color: ${accentForegroundHoverBehavior.var};
+    }
+
+    .control:active {
+        color: ${accentForegroundActiveBehavior.var};
+    }
+
+    .control .content {
+        position: relative;
+    }
+
+    .control .content::before {
+        content: "";
+        display: block;
+        height: calc(var(--outline-width) * 1px);
+        left: 0;
+        position: absolute;
+        right: 0;
+        top: calc(1em + 4px);
+        width: 100%;
+    }
+
+    .control:hover .content::before {
+        background: ${accentForegroundHoverBehavior.var};
+    }
+
+    .control:active .content::before {
+        background: ${accentForegroundActiveBehavior.var};
+    }
+
+    .control:${focusVisible} .content::before {
+        background: ${neutralForegroundRestBehavior.var};
+        height: calc(var(--focus-outline-width) * 1px);
+    }
+
+    .control:not([href]) {
+        color: ${neutralForegroundRestBehavior.var};
+        cursor: default;
+    }
+
+    .control:not([href]) .content::before {
+        background: none;
+    }
+
+    .start,
+    .end,
+    ::slotted(svg) {
+        ${
+            /* Glyph size and margin-left is temporary - 
+            replace when adaptive typography is figured out */ ""
+        } width: 16px;
+        height: 16px;
+    }
+
+    .start {
+        margin-inline-end: 6px;
+    }
+
+    .end {
+        margin-inline-start: 6px;
+    }
+`.withBehaviors(
+    accentForegroundRestBehavior,
+    accentForegroundHoverBehavior,
+    accentForegroundActiveBehavior,
+    accentForegroundHoverBehavior,
+    neutralForegroundRestBehavior,
+    forcedColorsStylesheetBehavior(
+        css`
+            .control:hover .content::before {
+                background: ${SystemColors.LinkText};
+            }
+        `
+    )
+);
