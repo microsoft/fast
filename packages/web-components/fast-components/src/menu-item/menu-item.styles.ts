65d276679fc0c2afd6c549ad26fa7f5c72063e43
Kham Udom <khamu@microsoft.com>
2020-10-26 19:01:30 -0700

    update grid-template-columns on host in menu-item style

--- packages/web-components/fast-components/src/menu-item/menu-item.styles.ts
+++ packages/web-components/fast-components/src/menu-item/menu-item.styles.ts
@@ -22,7 +22,7 @@ export const MenuItemStyles = css`
         outline: none;
         box-sizing: border-box;
         height: calc(${heightNumber} * 1px);
-        grid-template-columns: 42px auto 42px;
+        grid-template-columns: minmax(42px, auto) 1fr minmax(42px, auto);
         grid-template-rows: auto;
         justify-items: center;
         align-items: center;
