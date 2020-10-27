a32117e6edeb3710bf81590c6ee64a8028379821
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-10-27 14:53:18 -0700

    chore: add the converted MDN data as an export

---
+++ packages/tooling/fast-tooling/build/generate-mdn-data-files.js
@@ -0,0 +1,35 @@
+import { mapCSSProperties } from "../.tmp/data-utilities/mapping.mdn-data";
+import { css as mdnCSS } from "mdn-data";
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+
+const outFilePath = path.resolve(__dirname, "../src/css-data.ts");
+const comment = `/**
+ * This file is generated from build/generate-mdn-data-files.js
+ * any modifications will be overwritten.
+ */
+`;
+
+/**
+ * This file generates the file src/css-data.ts
+ */
+
+(function () {
+    fs.writeFile(
+        outFilePath,
+        `${comment}export default ${JSON.stringify(
+            mapCSSProperties({
+                properties: mdnCSS.properties,
+                syntaxes: mdnCSS.syntaxes,
+                types: mdnCSS.types,
+            }),
+            null,
+            2
+        )}`,
+        {},
+        error => {
+            chalk.red(error);
+        }
+    );
+})();
