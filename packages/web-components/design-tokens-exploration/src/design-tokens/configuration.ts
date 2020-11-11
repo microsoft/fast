4016cbfb62f83410b9d2c2fd167c15718763ee37
nicholasrice <nicholasrice@users.noreply.github.com>
2020-11-11 13:37:12 -0800

    add token registrtion interface

---
+++ packages/web-components/design-tokens-exploration/src/design-tokens/configuration.ts
@@ -0,0 +1,21 @@
+interface DesignTokenRegistration<T> {
+    /**
+     * The key for which the token can be accessed.
+     */
+    key: string;
+
+    /**
+     * The HTML attribute name
+     */
+    attribute?: string;
+
+    /**
+     * The CSS custom property name
+     */
+    customProperty?: string;
+
+    /**
+     * The default value.
+     */
+    default?: T;
+}
