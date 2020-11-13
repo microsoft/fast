e87804ef371cfc9fe58feb27f66201c84af30618
nicholasrice <nicholasrice@users.noreply.github.com>
2020-11-12 16:54:26 -0800

    remove design token config

--- packages/web-components/fast-foundation/src/design-tokens/tokens.ts
+++ packages/web-components/fast-foundation/src/design-tokens/tokens.ts
@@ -1,18 +1,6 @@
 import { DI, InterfaceSymbol } from "../di";
 import { FASTDesignTokenLibrary } from "./library";
 
-// export interface DesignTokenConfig {
-//     backgroundColor: string;
-// }
-//
-// const defaults: DesignTokenConfig = {
-//     backgroundColor: "#FFFFFF",
-// };
-//
-// export const DefaultDesignTokens = new FASTDesignTokenLibrary<DesignTokenConfig>(
-//     defaults
-// );
-
 export const DesignTokens: InterfaceSymbol<FASTDesignTokenLibrary<
     any
 >> = DI.createDOMInterface<FASTDesignTokenLibrary<any>>("DesignTokens").noDefault();
