2af544c42a67f07996b1ce33a9664b254a5db536
Jane Chu <7559015+janechu@users.noreply.github.com>
2020-12-08 13:45:47 -0800

    feat: add CSS property control (#4173)
    # Description
    
    <!--- Describe your changes. -->
    This work provides the structure for deciphering MDN data structures provided by `@microsoft/fast-tooling` into generated UI. It also integrates web components from `@microsoft/fast-components`.
    
    This work is going into the `features/form-custom-control` branch until such time that it is ready to merge to the main repository branch.
    
    ## Motivation & context
    
    <!--- What problem does this change solve? -->
    <!--- Provide a link if you are addressing an open issue. -->
    Provides the foundational work on #3773
    
    ## Issue type checklist
    
    <!--- What type of change are you submitting? Put an x in the box that applies: -->
    
    - [ ] **Chore**: A change that does not impact distributed packages.
    - [ ] **Bug fix**: A change that fixes an issue, link to the issue above.
    - [x] **New feature**: A change that adds functionality.
    
    **Is this a breaking change?**
    - [ ] This change causes current functionality to break.
    
    <!--- If yes, describe the impact. -->
    
    **Adding or modifying component(s) in `@microsoft/fast-components` checklist**
    
    <!-- Do your changes add or modify components in the @microsoft/fast-components package? Put an x in the box that applies: -->
    
    - [ ] I have added a new component
    - [ ] I have modified an existing component
    - [ ] I have updated the [definition file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#definition)
    - [ ] I have updated the [configuration file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#configuration)
    
    ## Process & policy checklist
    
    <!--- Review the list and check the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.
    
    <!---
    Formatting guidelines:
    
    Accepted peer review title format:
    <type>: <description>
    
    Example titles:
        chore: add unit tests for all components
        feat: add a border radius to button
        fix: update design system to use 3px border radius
    
        <type> is required to be one of the following:
    
            - chore: A change that does not impact distributed packages.
            - fix: A change which fixes an issue.
            - feat: A that adds functionality.
    
        <description> is required for the CHANGELOG and speaks to what the user gets from this PR:
    
            - Be concise.
            - Use all lowercase characters. 
            - Use imperative, present tense (e.g. `add` not `adds`.)
            - Do not end your description with a period.
            - Avoid redundant words.
    
    For additional information regarding working on FAST, check out our documentation site:
    https://www.fast.design/docs/community/contributor-guide
    -->

---
+++ packages/tooling/fast-tooling-react/src/form/custom-controls/control.css-ref.props.ts
@@ -0,0 +1,51 @@
+import {
+    CSSPropertyRef,
+    CSSPropertySyntax,
+} from "@microsoft/fast-tooling/dist/data-utilities/mapping.mdn-data";
+
+export interface CSSRefProps {
+    /**
+     * If this prop is available then this is the root level
+     * CSS property reference, if not it is a partial reference
+     * for a CSS property
+     */
+    mapsToProperty?: string;
+
+    /**
+     * The onChange callback to call when a value has been changed by
+     * a form element
+     */
+    onChange: (value: string) => void;
+
+    /**
+     * The syntax or reference used to determine the form element UI
+     */
+    syntax: CSSPropertySyntax | CSSPropertyRef;
+}
+
+export interface CSSRefState {
+    /**
+     * The index of the select dropdown determining which form UI to show,
+     * this will be used when multiple form element UI options are available
+     * according to the syntax.
+     */
+    index: number;
+
+    /**
+     * The values available, since CSS can comprise of multiple values for example:
+     *
+     * border: 1px solid #000000;
+     *
+     * Each value will be represented in the array, for example:
+     *
+     * ["1px", "solid", "#000"]
+     *
+     * A single string cannot be used as indexes will move around depending on if certain values are omitted.
+     * Example:
+     * "1px solid #000" equates to ["1px", "solid", "#000"]
+     * "1px #000" equates to ["1px", undefined, "#000"]
+     *
+     * Which preserves the order the string values must appear.
+     */
+    values: string[];
+}
