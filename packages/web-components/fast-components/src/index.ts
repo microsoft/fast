9d37a3fef5cb68228a6b70c06d3ae9210601055f
Rob Barber <robarb@microsoft.com>
2020-12-18 12:03:36 -0800

    Adding fast-number-field component

--- packages/web-components/fast-components/src/index.ts
+++ packages/web-components/fast-components/src/index.ts
@@ -16,6 +16,7 @@ export * from "./listbox/index";
 export * from "./listbox-option/index";
 export * from "./menu/index";
 export * from "./menu-item/index";
+export * from "./number-field/index";
 export * from "./progress/index";
 export * from "./progress-ring/index";
 export * from "./radio/index";
---
+++ packages/web-components/fast-foundation/src/number-field/number-field.template.ts
@@ -0,0 +1,75 @@
+import { html, ref, slotted } from "@microsoft/fast-element";
+import { endTemplate, startTemplate } from "../patterns";
+import { NumberField } from "./number-field";
+
+/**
+ * The template for the {@link @microsoft/fast-foundation#(NumberField:class)} component.
+ * @public
+ */
+export const NumberFieldTemplate = html<NumberField>`
+    <template
+        class="
+            ${x => (x.readOnly ? "readonly" : "")}
+        "
+    >
+        <label
+            part="label"
+            for="control"
+            class="${x =>
+                x.defaultSlottedNodes && x.defaultSlottedNodes.length
+                    ? "label"
+                    : "label label__hidden"}"
+        >
+            <slot ${slotted("defaultSlottedNodes")}></slot>
+        </label>
+        <div class="root" part="root">
+            ${startTemplate}
+            <input
+                class="control"
+                part="control"
+                id="control"
+                @input="${x => x.handleTextInput()}"
+                @change="${x => x.handleChange()}"
+                ?autofocus="${x => x.autofocus}"
+                ?disabled="${x => x.disabled}"
+                list="${x => x.list}"
+                maxlength="${x => x.maxlength}"
+                minlength="${x => x.minlength}"
+                placeholder="${x => x.placeholder}"
+                ?readonly="${x => x.readOnly}"
+                ?required="${x => x.required}"
+                size="${x => x.size}"
+                :value="${x => x.value}"
+                type="text"
+                min="${x => x.min}"
+                max="${x => x.max}"
+                step="${x => x.step}"
+                aria-atomic="${x => x.ariaAtomic}"
+                aria-busy="${x => x.ariaBusy}"
+                aria-controls="${x => x.ariaControls}"
+                aria-current="${x => x.ariaCurrent}"
+                aria-describedBy="${x => x.ariaDescribedby}"
+                aria-details="${x => x.ariaDetails}"
+                aria-disabled="${x => x.ariaDisabled}"
+                aria-errormessage="${x => x.ariaErrormessage}"
+                aria-flowto="${x => x.ariaFlowto}"
+                aria-haspopup="${x => x.ariaHaspopup}"
+                aria-hidden="${x => x.ariaHidden}"
+                aria-invalid="${x => x.ariaInvalid}"
+                aria-keyshortcuts="${x => x.ariaKeyshortcuts}"
+                aria-label="${x => x.ariaLabel}"
+                aria-labelledby="${x => x.ariaLabelledby}"
+                aria-live="${x => x.ariaLive}"
+                aria-owns="${x => x.ariaOwns}"
+                aria-relevant="${x => x.ariaRelevant}"
+                aria-roledescription="${x => x.ariaRoledescription}"
+                ${ref("control")}
+            />
+            <div class="controls">
+                <div class="step_up" @click="${x => x.stepUp()}"></div>
+                <div class="step_down" @click="${x => x.stepDown()}"></div>
+            </div>
+            ${endTemplate}
+        </div>
+    </template>
+`;
