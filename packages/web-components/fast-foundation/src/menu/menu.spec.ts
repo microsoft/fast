9c8a860ae08e42e056da1fbce969b7eff4a544dd
Stephane Comeau <scomea@microsoft.com>
2020-12-22 21:57:25 -0800

    add checkbox and radio menu item tests

--- packages/web-components/fast-foundation/src/menu/menu.spec.ts
+++ packages/web-components/fast-foundation/src/menu/menu.spec.ts
@@ -131,12 +131,12 @@ describe("Menu", () => {
     it("should navigate the menu on arrow up/down keys", async () => {
         const { element, connect, disconnect } = await fixture(html<FASTMenu>`
             <fast-menu>
-                <fast-menu-item id="id1">Foo</fast-menu-item>
-                <fast-menu-item role="menuitem" id="id2">Bar</fast-menu-item>
+                <fast-menu-item id="id1">One</fast-menu-item>
+                <fast-menu-item role="menuitem" id="id2">Two</fast-menu-item>
                 <div>I put a div in my menu</div>
-                <fast-menu-item role="menuitemradio" id="id3">Baz</fast-menu-item>
+                <fast-menu-item role="menuitemradio" id="id3">Three</fast-menu-item>
                 <div>I put a div in my menu</div>
-                <fast-menu-item role="menuitemcheckbox" id="id4">Baz</fast-menu-item>
+                <fast-menu-item role="menuitemcheckbox" id="id4">Four</fast-menu-item>
             </fast-menu>
         `);
 
@@ -170,6 +170,142 @@ describe("Menu", () => {
 
         document.activeElement?.dispatchEvent(arrowUpEvent);
         expect(document.activeElement?.id).to.equal("id1");
+
+        await disconnect();
+    });
+
+    it("should treat all checkbox menu items as individually selectable items", async () => {
+        const { element, connect, disconnect } = await fixture(html<FASTMenu>`
+            <fast-menu>
+                <fast-menu-item role="menuitemcheckbox" id="id1">One</fast-menu-item>
+                <fast-menu-item role="menuitemcheckbox" id="id2">Two</fast-menu-item>
+                <fast-menu-item role="menuitemcheckbox" id="id3">Three</fast-menu-item>
+            </fast-menu>
+        `);
+
+        await connect();
+        await DOM.nextUpdate();
+
+        const item1 = element.querySelector('[id="id1"]');
+        const item2 = element.querySelector('[id="id2"]');
+        const item3 = element.querySelector('[id="id3"]');
+
+        expect(item1?.getAttribute("aria-checked")).to.equal(null);
+        expect(item2?.getAttribute("aria-checked")).to.equal(null);
+        expect(item3?.getAttribute("aria-checked")).to.equal(null);
+
+        (item1 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("true");
+        expect(item2?.getAttribute("aria-checked")).to.equal(null);
+        expect(item3?.getAttribute("aria-checked")).to.equal(null);
+
+        (item2 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("true");
+        expect(item2?.getAttribute("aria-checked")).to.equal("true");
+        expect(item3?.getAttribute("aria-checked")).to.equal(null);
+
+        (item3 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("true");
+        expect(item2?.getAttribute("aria-checked")).to.equal("true");
+        expect(item3?.getAttribute("aria-checked")).to.equal("true");
+
+        (item3 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("true");
+        expect(item2?.getAttribute("aria-checked")).to.equal("true");
+        expect(item3?.getAttribute("aria-checked")).to.equal("false");
+
+        await disconnect();
+    });
+
+    it("should treat all radio menu items as a 'radio group' and limit selection to one item within the group by default", async () => {
+        const { element, connect, disconnect } = await fixture(html<FASTMenu>`
+            <fast-menu>
+                <fast-menu-item role="menuitemradio" id="id1">One</fast-menu-item>
+                <fast-menu-item role="menuitemradio" id="id2">Two</fast-menu-item>
+                <fast-menu-item role="menuitemradio" id="id3">Three</fast-menu-item>
+            </fast-menu>
+        `);
+
+        await connect();
+        await DOM.nextUpdate();
+
+        const item1 = element.querySelector('[id="id1"]');
+        const item2 = element.querySelector('[id="id2"]');
+        const item3 = element.querySelector('[id="id3"]');
+
+        expect(item1?.getAttribute("aria-checked")).to.equal(null);
+        expect(item2?.getAttribute("aria-checked")).to.equal(null);
+        expect(item3?.getAttribute("aria-checked")).to.equal(null);
+
+        (item1 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("true");
+        expect(item2?.getAttribute("aria-checked")).to.equal("false");
+        expect(item3?.getAttribute("aria-checked")).to.equal("false");
+
+        (item2 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("false");
+        expect(item2?.getAttribute("aria-checked")).to.equal("true");
+        expect(item3?.getAttribute("aria-checked")).to.equal("false");
+
+        (item3 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("false");
+        expect(item2?.getAttribute("aria-checked")).to.equal("false");
+        expect(item3?.getAttribute("aria-checked")).to.equal("true");
+
+        await disconnect();
+    });
+
+    it("should use elements with role='separator' to divide radio menu items into different radio groups ", async () => {
+        const { element, connect, disconnect } = await fixture(html<FASTMenu>`
+            <fast-menu>
+                <fast-menu-item role="menuitemradio" id="id1">One</fast-menu-item>
+                <fast-menu-item role="menuitemradio" id="id2">Two</fast-menu-item>
+                <div role="separator"></div>
+                <fast-menu-item role="menuitemradio" id="id3">Three</fast-menu-item>
+                <fast-menu-item role="menuitemradio" id="id4">Four</fast-menu-item>
+            </fast-menu>
+        `);
+
+        await connect();
+        await DOM.nextUpdate();
+
+        const item1 = element.querySelector('[id="id1"]');
+        const item2 = element.querySelector('[id="id2"]');
+        const item3 = element.querySelector('[id="id3"]');
+        const item4 = element.querySelector('[id="id4"]');
+
+        expect(item1?.getAttribute("aria-checked")).to.equal(null);
+        expect(item2?.getAttribute("aria-checked")).to.equal(null);
+        expect(item3?.getAttribute("aria-checked")).to.equal(null);
+        expect(item4?.getAttribute("aria-checked")).to.equal(null);
+
+        (item1 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("true");
+        expect(item2?.getAttribute("aria-checked")).to.equal("false");
+        expect(item3?.getAttribute("aria-checked")).to.equal(null);
+        expect(item4?.getAttribute("aria-checked")).to.equal(null);
+
+        (item2 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("false");
+        expect(item2?.getAttribute("aria-checked")).to.equal("true");
+        expect(item3?.getAttribute("aria-checked")).to.equal(null);
+        expect(item4?.getAttribute("aria-checked")).to.equal(null);
+
+        (item3 as HTMLElement).click();
+        await DOM.nextUpdate();
+        expect(item1?.getAttribute("aria-checked")).to.equal("false");
+        expect(item2?.getAttribute("aria-checked")).to.equal("true");
+        expect(item3?.getAttribute("aria-checked")).to.equal("true");
+        expect(item4?.getAttribute("aria-checked")).to.equal("false");
 
         await disconnect();
     });
