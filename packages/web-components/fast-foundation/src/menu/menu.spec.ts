66fab5958d0191ec1ccef22d20b81667bc87b59c
Stephane Comeau <scomea@microsoft.com>
2020-12-22 19:35:23 -0800

    add menu nav test

--- packages/web-components/fast-foundation/src/menu/menu.spec.ts
+++ packages/web-components/fast-foundation/src/menu/menu.spec.ts
@@ -3,6 +3,7 @@ import { Menu, MenuTemplate as template } from "./index";
 import { MenuItem, MenuItemTemplate as itemTemplate } from "../menu-item";
 import { fixture } from "../fixture";
 import { DOM, customElement, html } from "@microsoft/fast-element";
+import { KeyCodes } from "@microsoft/fast-web-utilities";
 
 @customElement({
     name: "fast-menu",
@@ -16,7 +17,18 @@ class FASTMenu extends Menu {}
 })
 class FASTMenuItem extends MenuItem {}
 
-// TODO: Add tests for keyboard handling
+const arrowUpEvent = new KeyboardEvent("keydown", {
+    key: "ArrowUp",
+    keyCode: KeyCodes.arrowUp,
+    bubbles: true,
+} as KeyboardEventInit);
+
+const arrowDownEvent = new KeyboardEvent("keydown", {
+    key: "ArrowDown",
+    keyCode: KeyCodes.arrowDown,
+    bubbles: true,
+} as KeyboardEventInit);
+
 describe("Menu", () => {
     it("should include a role of menu", async () => {
         const { element, connect, disconnect } = await fixture<Menu>("fast-menu");
@@ -112,6 +124,52 @@ describe("Menu", () => {
         expect(
             element.querySelector("[role='menuitemradio']")?.getAttribute("tabindex")
         ).to.equal("0");
+
+        await disconnect();
+    });
+
+    it("should navigate the menu on arrow up/down keys", async () => {
+        const { element, connect, disconnect } = await fixture(html<FASTMenu>`
+            <fast-menu>
+                <fast-menu-item id="id1">Foo</fast-menu-item>
+                <fast-menu-item role="menuitem" id="id2">Bar</fast-menu-item>
+                <div>I put a div in my menu</div>
+                <fast-menu-item role="menuitemradio" id="id3">Baz</fast-menu-item>
+                <div>I put a div in my menu</div>
+                <fast-menu-item role="menuitemcheckbox" id="id4">Baz</fast-menu-item>
+            </fast-menu>
+        `);
+
+        await connect();
+        await DOM.nextUpdate();
+
+        const item1 = element.querySelector('[id="id1"]');
+        (item1 as HTMLElement).focus();
+        expect(document.activeElement?.id).to.equal("id1");
+
+        document.activeElement?.dispatchEvent(arrowDownEvent);
+        expect(document.activeElement?.id).to.equal("id2");
+
+        document.activeElement?.dispatchEvent(arrowDownEvent);
+        expect(document.activeElement?.id).to.equal("id3");
+
+        document.activeElement?.dispatchEvent(arrowDownEvent);
+        expect(document.activeElement?.id).to.equal("id4");
+
+        document.activeElement?.dispatchEvent(arrowDownEvent);
+        expect(document.activeElement?.id).to.equal("id4");
+
+        document.activeElement?.dispatchEvent(arrowUpEvent);
+        expect(document.activeElement?.id).to.equal("id3");
+
+        document.activeElement?.dispatchEvent(arrowUpEvent);
+        expect(document.activeElement?.id).to.equal("id2");
+
+        document.activeElement?.dispatchEvent(arrowUpEvent);
+        expect(document.activeElement?.id).to.equal("id1");
+
+        document.activeElement?.dispatchEvent(arrowUpEvent);
+        expect(document.activeElement?.id).to.equal("id1");
 
         await disconnect();
     });
