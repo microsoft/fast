bfe1302255145c6e33d9cf53e4441c2e6a160356
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-01-04 13:32:18 -0800

    feat: add default control configurations for types and uncategorized values with mappings (#4202)
    # Description
    
    <!--- Describe your changes. -->
    This change adds MDN docs specified information (but not provided by the mdn-data package) as data structures to be taken advantage of when creating UI for number-fields, text-fields, CSS functions and multiple values.
    
    - Number-fields will specify any appended units
    - Multiple values will specify the number of values and split type
    - Functions will specify the name of the function (for value re-mapping e.g. "foo" supplied with function name "bar" will return "bar(foo)")
    
    ## Motivation & context
    
    <!--- What problem does this change solve? -->
    <!--- Provide a link if you are addressing an open issue. -->
    This provides a framework for more precise UI to be used and for values to be auto-formatted where possible. It will also allow the use of `<fast-number-field>` for a variety of related components.
    
    ## Issue type checklist
    
    <!--- What type of change are you submitting? Put an x in the box that applies: -->
    
    - [ ] **Chore**: A change that does not impact distributed packages.
    - [ ] **Bug fix**: A change that fixes an issue, link to the issue above.
    - [x] **New feature**: A change that adds functionality.
    
    **Is this a breaking change?**
    - [ ] This change causes current functionality to break.
    
    <!--- If yes, describe the impact. -->
    
    **Adding or modifying component(s) in `@microsoft/fast-components` checklist**
    
    <!-- Do your changes add or modify components in the @microsoft/fast-components package? Put an x in the box that applies: -->
    
    - [ ] I have added a new component
    - [ ] I have modified an existing component
    - [ ] I have updated the [definition file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#definition)
    - [ ] I have updated the [configuration file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#configuration)
    
    ## Process & policy checklist
    
    <!--- Review the list and check the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.
    
    <!---
    Formatting guidelines:
    
    Accepted peer review title format:
    <type>: <description>
    
    Example titles:
        chore: add unit tests for all components
        feat: add a border radius to button
        fix: update design system to use 3px border radius
    
        <type> is required to be one of the following:
    
            - chore: A change that does not impact distributed packages.
            - fix: A change which fixes an issue.
            - feat: A that adds functionality.
    
        <description> is required for the CHANGELOG and speaks to what the user gets from this PR:
    
            - Be concise.
            - Use all lowercase characters. 
            - Use imperative, present tense (e.g. `add` not `adds`.)
            - Do not end your description with a period.
            - Avoid redundant words.
    
    For additional information regarding working on FAST, check out our documentation site:
    https://www.fast.design/docs/community/contributor-guide
    -->

--- packages/tooling/fast-tooling-react/src/form/custom-controls/control.css.utilities.props.ts
+++ packages/tooling/fast-tooling-react/src/form/custom-controls/control.css.utilities.props.ts
@@ -3,6 +3,8 @@ import {
     CSSSyntaxRef,
 } from "@microsoft/fast-tooling/dist/data-utilities/mapping.mdn-data";
 import { XOR } from "@microsoft/fast-tooling/dist/data-utilities/type.utilities";
+import { Type } from "@microsoft/fast-tooling/dist/css-data.types";
+import { Syntax } from "@microsoft/fast-tooling/dist/css-data.syntax";
 
 export interface RenderControlConfig {
     /**
@@ -45,3 +47,200 @@ interface SelectControlOptions {
 export interface RenderSelectControlConfig extends RenderControlConfig {
     options: SelectControlOptions[];
 }
+
+type Uncategorized = "<x>" | "<y>" | "<name-repeat>";
+export type AllTypeKeys = Type | Syntax | Uncategorized;
+
+export type AllTypes<TConfig> = {
+    [key in AllTypeKeys]: TConfig;
+};
+
+export enum CSSControlType {
+    "number",
+    "text",
+    "multiple",
+    "function",
+}
+
+/**
+ * Number values
+ *
+ * These are often appended with text such as "px" or "deg"
+ */
+interface AppendType {
+    unit: string[];
+}
+
+export interface NumberFieldTypeConfig {
+    type: CSSControlType.number;
+    append?: AppendType;
+}
+
+type NumberFieldType = Pick<
+    AllTypes<NumberFieldTypeConfig>,
+    | "<angle>"
+    | "<dimension>"
+    | "<flex>"
+    | "<integer>"
+    | "<length>"
+    | "<number>"
+    | "<percentage>"
+    | "<resolution>"
+    | "<time>"
+    | "<x>"
+    | "<y>"
+>;
+
+export const numberFieldTypes: NumberFieldType = {
+    "<angle>": {
+        type: CSSControlType.number,
+        append: {
+            unit: ["deg", "grad", "rad", "turn"],
+        },
+    },
+    "<dimension>": {
+        type: CSSControlType.number,
+        append: {
+            unit: ["px", "rem", "pt", "ms", "s", "hz", "Hz"],
+        },
+    },
+    "<flex>": {
+        type: CSSControlType.number,
+        append: {
+            unit: ["fr"],
+        },
+    },
+    "<integer>": {
+        type: CSSControlType.number,
+    },
+    "<length>": {
+        type: CSSControlType.number,
+        append: {
+            unit: ["ch", "em", "ex", "rem", "vh", "vw", "vmin", "vmax"],
+        },
+    },
+    "<number>": {
+        type: CSSControlType.number,
+    },
+    "<percentage>": {
+        type: CSSControlType.number,
+        append: {
+            unit: ["%"],
+        },
+    },
+    "<resolution>": {
+        type: CSSControlType.number,
+        append: {
+            unit: ["dpi", "dpcm", "dppx", "x"],
+        },
+    },
+    "<time>": {
+        type: CSSControlType.number,
+        append: {
+            unit: ["ms", "s"],
+        },
+    },
+    "<x>": {
+        type: CSSControlType.number,
+    },
+    "<y>": {
+        type: CSSControlType.number,
+    },
+};
+
+/**
+ * Text values
+ */
+export interface TextFieldTypeConfig {
+    type: CSSControlType.text;
+}
+
+type TextFieldType = Pick<
+    AllTypes<TextFieldTypeConfig>,
+    "<custom-ident>" | "<ident>" | "<string>"
+>;
+
+export const textFieldTypes: TextFieldType = {
+    "<custom-ident>": {
+        type: CSSControlType.text,
+    },
+    "<ident>": {
+        type: CSSControlType.text,
+    },
+    "<string>": {
+        type: CSSControlType.text,
+    },
+};
+
+/**
+ * Multiple values
+ */
+export interface MultipleTypeConfig {
+    type: CSSControlType.multiple;
+    separator: string;
+    controls: TypeConfigs[];
+}
+
+type MultipleType = Pick<AllTypes<MultipleTypeConfig>, "<ratio>">;
+
+export const multipleTypes: MultipleType = {
+    "<ratio>": {
+        type: CSSControlType.multiple,
+        separator: "/",
+        controls: [
+            {
+                type: CSSControlType.number,
+            },
+            {
+                type: CSSControlType.number,
+            },
+        ],
+    },
+};
+
+/**
+ * CSS functions
+ */
+export interface FunctionTypeConfig {
+    type: CSSControlType.function;
+    name: string;
+    controls: TypeConfigs[];
+}
+
+type FunctionType = Pick<AllTypes<FunctionTypeConfig>, "<url>" | "<name-repeat>">;
+
+export const functionTypes: FunctionType = {
+    "<url>": {
+        type: CSSControlType.function,
+        name: "url",
+        controls: [
+            {
+                type: CSSControlType.text,
+            },
+        ],
+    },
+    "<name-repeat>": {
+        type: CSSControlType.function,
+        name: "repeat",
+        controls: [
+            {
+                type: CSSControlType.text,
+            },
+        ],
+    },
+};
+
+type AllType = NumberFieldType | TextFieldType | MultipleType | FunctionType;
+
+export const allTypes: AllType = {
+    ...numberFieldTypes,
+    ...textFieldTypes,
+    ...multipleTypes,
+    ...functionTypes,
+};
+
+type TypeConfigs =
+    | NumberFieldTypeConfig
+    | TextFieldTypeConfig
+    | MultipleTypeConfig
+    | FunctionTypeConfig;
