35c888f4863543909208553a969f58cd2ed52777
Ibrahimmaga <ibrahim.maga@yahoo.fr>
2021-01-07 21:48:31 -0800

    windows validation

--- beachball.config.js
+++ beachball.config.js
@@ -1,2 +1,2 @@
-require("./build/releasing/ts-node-register");
-module.exports = require("./build/releasing/index");
+/*require("./build/releasing/ts-node-register");
+module.exports = require("./build/releasing/index");*/
