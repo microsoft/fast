68af63f947277a76ce84e222ecba2d6998bdb5e6
John Kreitlow <john.kreitlow@microsoft.com>
2021-01-08 16:20:40 -0800

    oops those were not all imports

--- sites/fast-color-explorer/app/site-footer.tsx
+++ sites/fast-color-explorer/app/site-footer.tsx
@@ -1,9 +1,11 @@
-export { FASTAnchor, FooterTemplate, SiteNavigation } from "@microsoft/fast-website";
+import { FooterTemplate } from "@microsoft/fast-website";
 import "@microsoft/fast-website/src/app/css/footer.css";
 import "@microsoft/fast-website/src/app/css/logo.css";
 import "@microsoft/fast-website/src/app/css/root.css";
 import "@microsoft/fast-website/src/app/css/site-wrapper.css";
 import React from "react";
+
+export { FASTAnchor, SiteNavigation } from "@microsoft/fast-website";
 
 export class Footer extends React.Component<{}, {}> {
     render(): React.ReactNode {
