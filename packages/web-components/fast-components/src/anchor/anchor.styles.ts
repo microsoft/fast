0a41fad4dff0c43db6d0c5d8edee11818461dfd2
eljefe223 <jes@microsoft.com>
2021-01-13 10:39:19 -0800

    bug state

--- packages/web-components/fast-components/src/anchor/anchor.styles.ts
+++ packages/web-components/fast-components/src/anchor/anchor.styles.ts
@@ -1,18 +1,25 @@
 import { css } from "@microsoft/fast-element";
 import {
     AccentButtonStyles,
+    accentForegroundCutRestBehavior,
+    accentFillHoverBehavior,
+    accentFillActiveBehavior,
+    neutralFocusInnerAccentBehavior,
+    accentFillRestBehavior,
     BaseButtonStyles,
     HypertextStyles,
     LightweightButtonStyles,
     OutlineButtonStyles,
     StealthButtonStyles,
 } from "../styles/index";
+import { appearanceBehavior } from "../utilities/behaviors";
 
 export const AnchorStyles = css`
     ${BaseButtonStyles}
-    ${AccentButtonStyles}
-    ${HypertextStyles}
-    ${LightweightButtonStyles}
-    ${OutlineButtonStyles}
-    ${StealthButtonStyles}
-`;
+`.withBehaviors(
+    appearanceBehavior("accent", AccentButtonStyles),
+    appearanceBehavior("hypertext", HypertextStyles),
+    appearanceBehavior("lightweight", LightweightButtonStyles),
+    appearanceBehavior("outline", OutlineButtonStyles),
+    appearanceBehavior("stealth", StealthButtonStyles)
+);
