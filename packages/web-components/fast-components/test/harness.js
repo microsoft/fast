bcf702df0a428c13606ac765447c6156a45a0da1
John Kreitlow <863023+radium-v@users.noreply.github.com>
2021-01-19 14:35:20 -0800

    split harness and server (#4250)
    * add environment variables
    * fix tsconfig conflicts
    * add README

--- packages/web-components/fast-components/test/harness.js
+++ packages/web-components/fast-components/test/harness.js
@@ -1,48 +1,28 @@
 /* eslint-env node */
-/* eslint-disable @typescript-eslint/no-var-requires */
-import path from "path";
-import express from "express";
-import { expect } from "chai";
-const { chromium, firefox, webkit } = require("playwright");
+import { chromium, firefox, webkit } from "playwright";
 
-const PLAYWRIGHT_BROWSER = process.env.PLAYWRIGHT_BROWSER || "chromium";
-const PORT = process.env.PORT || 7001;
-
-const app = express();
-const browser = { chromium, firefox, webkit }[PLAYWRIGHT_BROWSER];
-let server;
-
-app.use("/dist", express.static(path.resolve(__dirname, "dist")));
-app.use("/*", express.static(path.resolve(__dirname, "fixtures")));
-
-export function mochaGlobalSetup() {
-    server = app.listen(PORT, () => {
-        console.log(`Test harness server listening on port ${PORT}`);
-    });
-}
-
-export function mochaGlobalTeardown() {
-    server.close(() => {
-        console.log(`Test harness server on port ${PORT} closed`);
-    });
-}
+const selectedBrowser = process.env.PW_BROWSER || "chromium";
+const FixtureURL = process.env.FIXTUREURL;
+const fixture = process.env.FIXTURE || "index.html";
+const expressPort = process.env.PW_PORT || 7001;
 
 export const mochaHooks = {
     async beforeAll() {
+        const browser = { chromium, firefox, webkit }[selectedBrowser];
         this.browser = await browser.launch();
-        console.log(`Launched ${PLAYWRIGHT_BROWSER} browser`);
+        console.log(`Launched ${selectedBrowser} browser`);
     },
 
     async beforeEach() {
         this.page = await this.browser.newPage();
-        // TODO: Add a way for tests to specify the fixture base
-        await this.page.goto("http://localhost:7001/index.html");
-
-        expect(await this.page.title()).to.equal("FAST Components Test Harness");
+        const fixtureUrl = FixtureURL || `http://localhost:${expressPort}/${fixture}`;
+        await this.page.goto(fixtureUrl);
     },
 
     async afterEach() {
-        await this.page.close();
+        if (this.page) {
+            await this.page.close();
+        }
     },
 
     async afterAll() {
