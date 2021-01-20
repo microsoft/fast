a2bcb9111bd1014beec6e4c4b7170e2571374a14
Ibrahim Maga <imaga75@hotmail.com>
2021-01-19 16:22:25 -0800

    Playwright (#4249)
    * initial playright wf commit
    
    * playright cross-browser testing
    
    * playright integration
    
    * feature: fix linux automation
    
    * feature: fix linux
    
    * feature: fix linux
    
    * feature: fix linux
    
    * feature: fix linux
    
    * feature: fix linux
    
    * feature: Plawright-latest
    
    * feature yarn install
    
    * feature: change to npm i
    
    * remove headless
    
    * remove headless false
    
    * set headless to flase
    
    * remove linux matrix for now
    
    Co-authored-by: Ibrahimmaga <ibrahim.maga@yahoo.fr>

---
+++ playwrightTest/index.js
@@ -0,0 +1,43 @@
+//@ts-check
+const { chromium, webkit, firefox } = require("playwright");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end("Hello, world");
+});
+
+const headless = !process.env.HEADFUL;
+
+async function checkBrowser(browserType) {
+    try {
+        console.log(`Running ${browserType.name()}`);
+        const browser = await browserType.launch({ headless });
+        const page = await browser.newPage();
+        await page.goto("http://localhost:8080");
+        console.log(
+            `- ${browserType.name()}:`,
+            await page.evaluate(() => ({
+                width: document.documentElement.clientWidth,
+                clientHeight: document.documentElement.clientHeight,
+            }))
+        );
+        await browser.close();
+        console.log(`SUCCESS running ${browserType.name()}`);
+        return true;
+    } catch (e) {
+        console.log(`FAILED running ${browserType.name()}`);
+        console.error(e);
+        return false;
+    }
+}
+
+server.listen(8080, async () => {
+    let success = true;
+    success = (await checkBrowser(chromium)) && success;
+    success = (await checkBrowser(webkit)) && success;
+    success = (await checkBrowser(firefox)) && success;
+    server.close();
+    // On browsers failed to close - exit process.
+    process.exit(success ? 0 : 1);
+});
