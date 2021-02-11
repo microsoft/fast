3bf1f673de93e73e0b9f0eea38bef2b4df456985
John Kreitlow <john.kreitlow@microsoft.com>
2021-02-10 16:17:55 -0800

    remove provider wrapper for progress-ring story

--- packages/web-components/fast-components/src/progress-ring/progress-ring.stories.ts
+++ packages/web-components/fast-components/src/progress-ring/progress-ring.stories.ts
@@ -1,13 +1,8 @@
-import { FASTDesignSystemProvider } from "../design-system-provider";
 import Examples from "./fixtures/circular.html";
-import { FASTProgressRing } from "./index";
-
-// Prevent tree-shaking
-FASTProgressRing;
-FASTDesignSystemProvider;
+import "./index";
 
 export default {
     title: "Progress Ring",
 };
 
-export const Base = () => Examples;
+export const ProgressRing = () => Examples;
