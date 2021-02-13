6d42bf02ef66f4daad9bce121fda42c147ccdb04
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-02-12 18:03:31 -0800

    chore: add a browser manual test environment for the shortcuts service (#4357)
    # Description
    
    <!--- Describe your changes. -->
    This change adds a manual test environment to test the shortcuts service. It can be run in the fast-tooling package with `yarn start`.
    
    Reliant on #4355, #4356
    
    ## Issue type checklist
    
    <!--- What type of change are you submitting? Put an x in the box that applies: -->
    
    - [x] **Chore**: A change that does not impact distributed packages.
    - [ ] **Bug fix**: A change that fixes an issue, link to the issue above.
    - [ ] **New feature**: A change that adds functionality.
    
    **Is this a breaking change?**
    - [ ] This change causes current functionality to break.
    
    <!--- If yes, describe the impact. -->
    
    **Adding or modifying component(s) in `@microsoft/fast-components` checklist**
    
    <!-- Do your changes add or modify components in the @microsoft/fast-components package? Put an x in the box that applies: -->
    
    - [ ] I have added a new component
    - [ ] I have modified an existing component
    - [ ] I have updated the [definition file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#definition)
    - [ ] I have updated the [configuration file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#configuration)
    
    ## Process & policy checklist
    
    <!--- Review the list and check the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.
    
    <!---
    Formatting guidelines:
    
    Accepted peer review title format:
    <type>: <description>
    
    Example titles:
        chore: add unit tests for all components
        feat: add a border radius to button
        fix: update design system to use 3px border radius
    
        <type> is required to be one of the following:
    
            - chore: A change that does not impact distributed packages.
            - fix: A change which fixes an issue.
            - feat: A that adds functionality.
    
        <description> is required for the CHANGELOG and speaks to what the user gets from this PR:
    
            - Be concise.
            - Use all lowercase characters. 
            - Use imperative, present tense (e.g. `add` not `adds`.)
            - Do not end your description with a period.
            - Avoid redundant words.
    
    For additional information regarding working on FAST, check out our documentation site:
    https://www.fast.design/docs/community/contributor-guide
    -->

---
+++ packages/tooling/fast-tooling/app/examples/shortcuts/index.ts
@@ -0,0 +1,84 @@
+import {
+    MessageSystem,
+    MessageSystemDataTypeAction,
+    MessageSystemType,
+} from "../../../src";
+import {
+    Shortcuts,
+    shortcutsId,
+} from "../../../src/message-system-service/shortcuts.service";
+import { ShortcutsAction } from "../../../src/message-system-service/shortcuts.service-action";
+import dataDictionaryConfig from "./data-dictionary-config";
+import schemaDictionary from "./schema-dictionary";
+
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+const FASTMessageSystemWorker = require("../../../dist/message-system.min.js");
+const dataElement = document.getElementById("data");
+const inputElement = document.getElementById("input");
+document.body.setAttribute("style", "margin: 0");
+
+const fastMessageSystemWorker = new FASTMessageSystemWorker();
+let fastMessageSystem: MessageSystem;
+let fastShortcuts: Shortcuts;
+let dataDictionary;
+
+function handleMessageSystem(e: MessageEvent) {
+    if (e.data) {
+        if (
+            e.data.type === MessageSystemType.initialize ||
+            e.data.type === MessageSystemType.data
+        ) {
+            dataDictionary = e.data.dataDictionary;
+            dataElement.innerHTML = JSON.stringify(dataDictionary, null, 2);
+        }
+
+        if (e.data.type === MessageSystemType.initialize) {
+            const config: any = fastMessageSystem.getConfigById(shortcutsId) as any;
+            inputElement.removeAttribute("disabled");
+            inputElement.addEventListener(config.eventListenerType, config.eventListener);
+        }
+    }
+}
+
+if ((window as any).Worker) {
+    fastMessageSystem = new MessageSystem({
+        webWorker: fastMessageSystemWorker,
+        dataDictionary: dataDictionaryConfig as any,
+        schemaDictionary,
+    });
+    fastMessageSystem.add({
+        onMessage: handleMessageSystem,
+    });
+
+    fastShortcuts = new Shortcuts({
+        messageSystem: fastMessageSystem,
+        actions: [
+            new ShortcutsAction({
+                id: "foo",
+                name: "Foo",
+                keys: [
+                    {
+                        ctrlKey: true,
+                    },
+                    {
+                        shiftKey: true,
+                    },
+                    {
+                        value: "D",
+                    },
+                ],
+                action: () => {
+                    fastMessageSystem.postMessage({
+                        type: MessageSystemType.data,
+                        action: MessageSystemDataTypeAction.removeLinkedData,
+                        dataLocation: "text",
+                        linkedData: [{ id: "text" }],
+                        options: {
+                            originatorId: shortcutsId,
+                        },
+                    });
+                },
+            }),
+        ],
+    });
+}
