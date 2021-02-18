be996eea6c2a19dbf7b1f8afac51cba22bbc8045
Nicholas Rice <3213292+nicholasrice@users.noreply.github.com>
2021-02-18 15:26:05 -0800

    feat: add playwright (#4337)
    * add playwright harness script and configs (#4229)
    
    * split harness and server
    
    * add environment variables
    * fix tsconfig conflicts
    * add README
    
    * update yarn.lock after rebase
    
    * install playwright latest
    
    * Replace rollup-plugin-copy with copyfiles
    
    * cross-env environment var setting
    
    * add playwright CI
    
    * add ci-workflow to linux machine
    
    * add playwright ci to linux environment
    
    * work flow fixes in linux
    
    * resolve conflict issue
    
    * playwright worflow
    
    * playwright workflow
    
    * fix run workflow
    
    * install playwright for servers
    
    * change tittle for the build
    
    * Update .github/workflows/ci-weekly.yml
    
    Co-authored-by: Jane Chu <7559015+janechu@users.noreply.github.com>
    
    * Update build/copy.js
    
    Co-authored-by: Jane Chu <7559015+janechu@users.noreply.github.com>
    
    * remove blank lines
    
    * renaming test dir to __test__
    
    * organize gitignore files
    
    * Update packages/web-components/fast-components/__test__/README.md
    
    * fix environment variable casing
    
    * remove abbreviation from environment variable
    
    * sort package.json scripts
    
    Co-authored-by: John Kreitlow <863023+radium-v@users.noreply.github.com>
    Co-authored-by: John Kreitlow <john.kreitlow@microsoft.com>
    Co-authored-by: nicholasrice <nicholasrice@users.noreply.github.com>
    Co-authored-by: Ibrahim Maga <imaga75@hotmail.com>
    Co-authored-by: Jane Chu <7559015+janechu@users.noreply.github.com>

---
+++ build/copy.js
@@ -0,0 +1,22 @@
+/* eslint-env node, es2015 */
+/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-var-requires, @typescript-eslint/typedef */
+const copyfiles = require("copyfiles");
+
+function copy(files, destination, config = { flat: true, verbose: true, up: true }) {
+    return new Promise((resolve, reject) => {
+        return copyfiles([...files, destination], config, err =>
+            err ? reject(err.details) : resolve()
+        );
+    }).catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+}
+
+/**
+ * Copy files from one place to another.
+ * @param {string[]} files - Paths and globs to copy
+ * @param {string} destination - Directory to copy the files to
+ * @param {{ flat?: boolean; verbose?: boolean; up?: boolean; }} config - Configuration options passed to `copyfiles`.
+ */
+module.exports = copy;
