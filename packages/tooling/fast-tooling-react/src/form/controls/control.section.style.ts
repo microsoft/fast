bd2178d46a27aebcb0447ea419d7037358f27049
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-02-23 18:34:17 -0800

    fix: apply styling to categories (#4387)
    # Description
    
    <!--- Describe your changes. -->
    This change adds styling to form categories changing from the default `<fieldset>` and `<legend>` which displays as:
    ![Screen Shot 2021-02-22 at 4 55 35 PM](https://user-images.githubusercontent.com/7559015/108789293-ce98d500-752e-11eb-89fd-270d7d8cd5d0.png)
    
    To:
    ![Screen Shot 2021-02-22 at 4 50 44 PM](https://user-images.githubusercontent.com/7559015/108789212-a315ea80-752e-11eb-89f0-5240a39f7e3a.png)
    
    
    ## Motivation & context
    
    <!--- What problem does this change solve? -->
    <!--- Provide a link if you are addressing an open issue. -->
    Continues to address #2575
    
    ## Issue type checklist
    
    <!--- What type of change are you submitting? Put an x in the box that applies: -->
    
    - [ ] **Chore**: A change that does not impact distributed packages.
    - [x] **Bug fix**: A change that fixes an issue, link to the issue above.
    - [ ] **New feature**: A change that adds functionality.
    
    **Is this a breaking change?**
    - [ ] This change causes current functionality to break.
    
    <!--- If yes, describe the impact. -->
    
    **Adding or modifying component(s) in `@microsoft/fast-components` checklist**
    
    <!-- Do your changes add or modify components in the @microsoft/fast-components package? Put an x in the box that applies: -->
    
    - [ ] I have added a new component
    - [ ] I have modified an existing component
    - [ ] I have updated the [definition file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#definition)
    - [ ] I have updated the [configuration file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#configuration)
    
    ## Process & policy checklist
    
    <!--- Review the list and check the boxes that apply. -->
    
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.
    
    <!---
    Formatting guidelines:
    
    Accepted peer review title format:
    <type>: <description>
    
    Example titles:
        chore: add unit tests for all components
        feat: add a border radius to button
        fix: update design system to use 3px border radius
    
        <type> is required to be one of the following:
    
            - chore: A change that does not impact distributed packages.
            - fix: A change which fixes an issue.
            - feat: A that adds functionality.
    
        <description> is required for the CHANGELOG and speaks to what the user gets from this PR:
    
            - Be concise.
            - Use all lowercase characters. 
            - Use imperative, present tense (e.g. `add` not `adds`.)
            - Do not end your description with a period.
            - Avoid redundant words.
    
    For additional information regarding working on FAST, check out our documentation site:
    https://www.fast.design/docs/community/contributor-guide
    -->

--- packages/tooling/fast-tooling-react/src/form/controls/control.section.style.ts
+++ packages/tooling/fast-tooling-react/src/form/controls/control.section.style.ts
@@ -11,6 +11,21 @@ const styles: ComponentStyles<SectionControlClassNameContract, {}> = {
     sectionControl__disabled: {
         opacity: "1", // override the fieldset disabled inherited style
     },
+    sectionControl_category: {
+        margin: "0",
+        padding: "0",
+        border: "none",
+        "min-inline-size": "unset", // override for fieldsets inherited style
+        "font-size": "14px",
+        "line-height": "20px",
+    },
+    sectionControl_categoryTitle: {
+        "font-weight": "bold",
+        padding: "unset",
+        float: "left",
+        "padding-top": "16px",
+        "padding-bottom": "12px",
+    },
 };
 
 export default styles;
