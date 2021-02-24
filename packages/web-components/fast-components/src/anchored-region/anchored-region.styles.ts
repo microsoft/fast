36bb6ad8b45225677cb1b3926bba29d294e2e44d
Stephane Comeau <scomea@microsoft.com>
2021-02-23 16:00:20 -0800

    working

--- packages/web-components/fast-components/src/anchored-region/anchored-region.styles.ts
+++ packages/web-components/fast-components/src/anchored-region/anchored-region.styles.ts
@@ -4,5 +4,11 @@ export const AnchoredRegionStyles = css`
     :host {
         contain: layout;
         display: block;
+        pointer-events: "none";
+    }
+
+    :host(.loading) {
+        pointer-events: "none";
+        opacity: 0;
     }
 `;
--- packages/web-components/fast-foundation/src/anchored-region/anchored-region.ts
+++ packages/web-components/fast-foundation/src/anchored-region/anchored-region.ts
@@ -901,10 +901,12 @@ export class AnchoredRegion extends FASTElement {
         this.classList.toggle("inset-left", this.horizontalPosition === "insetLeft");
         this.classList.toggle("inset-right", this.horizontalPosition === "insetRight");
 
+        // this.classList.toggle("loading", !this.initialLayoutComplete);
+
+        this.style.opacity = this.initialLayoutComplete ? "1" : "0";
+        this.style.pointerEvents = this.initialLayoutComplete ? "" : "none";
         this.style.position = this.fixedPlacement ? "fixed" : "absolute";
         this.style.transformOrigin = `${this.yTransformOrigin} ${this.xTransformOrigin}`;
-        this.style.opacity = this.initialLayoutComplete ? "1" : "0";
-        this.style.pointerEvents = this.initialLayoutComplete ? "unset" : "none";
 
         if (this.horizontalPositioningMode === "uncontrolled") {
             this.style.width = "unset";
