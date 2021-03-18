e9d3b345870b35c8ea9f2079537232c110129be1
John Kreitlow <863023+radium-v@users.noreply.github.com>
2021-03-18 09:43:56 -0700

    fix: combobox with typescript ^4.0 (#4475)

--- packages/web-components/fast-foundation/src/combobox/combobox.form-associated.ts
+++ packages/web-components/fast-foundation/src/combobox/combobox.form-associated.ts
@@ -2,15 +2,21 @@ import { Listbox } from "../listbox/listbox";
 import { FormAssociated } from "../form-associated/form-associated";
 
 /**
+ * Normally this would be passed as an anonymous class directly to the
+ * {@link (FormAssociated:function)} mixin function. TypeScript 4+ throws an
+ * error when base accessors are overridden in a subclass. Combobox overrides
+ * the `options` accessors in Listbox.
+ */
+class ComboboxListbox extends Listbox {
+    public proxy: HTMLInputElement = document.createElement("input");
+}
+
+/**
  * A form-associated base class for the {@link (Combobox:class)} component.
  *
  * @internal
  */
-export class FormAssociatedCombobox extends FormAssociated(
-    class extends Listbox {
-        public proxy: HTMLInputElement = document.createElement("input");
-    }
-) {}
+export class FormAssociatedCombobox extends FormAssociated(ComboboxListbox) {}
 
 /**
  * @internal
