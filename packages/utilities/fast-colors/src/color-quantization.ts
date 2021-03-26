c93341687f457eb779258434f3a7f1d7f8d26d92
Matthew Owen <eigenvector@speakeasy.net>
2021-03-26 11:23:46 -0700

    Add histogram option

--- packages/utilities/fast-colors/src/color-quantization.ts
+++ packages/utilities/fast-colors/src/color-quantization.ts
@@ -110,6 +110,21 @@ export function quantize(
         config.pixelSkipping,
         config.isHistogramPixelValid
     );
+    return quantizeHistogram(histogram, config);
+}
+
+/**
+ * The data in the color histogram is reduced down to a small set of colors.
+ * It can be useful to create the Histogram manually in cases where one wants to remove or alter the colors in it
+ * or to re-use it in order to quantize multiple times with different config settings.
+ * Based on the Modified Median Cut Quantization implementation from https://github.com/DanBloomberg/leptonica/blob/master/src/colorquant2.c
+ *
+ * @public
+ */
+export function quantizeHistogram(
+    histogram: Histogram,
+    config: QuantizeConfig = defaultQuantizeConfig
+): QuantizedColor[] {
     const initialBox: PixelBox = new PixelBox(
         histogram,
         histogram.minRed,
