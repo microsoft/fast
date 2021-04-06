4424dd9b5c007131050dbfd18c55e3597e87964e
William Wagner <williamw@microsoft.com>
2021-04-06 11:37:17 -0700

    Temp

--- packages/tooling/fast-tooling/src/web-components/html-render/html-render.ts
+++ packages/tooling/fast-tooling/src/web-components/html-render/html-render.ts
@@ -1,18 +1,36 @@
-import {customElement, DOM, FASTElement, $global, attr, observable} from '@microsoft/fast-element';
-import { htmlMapper, htmlResolver, mapDataDictionary, ResolverConfig } from '../../data-utilities/mapping';
-import { WebComponentDefinition, WebComponentDefinitionTag } from '../../data-utilities/web-component';
-import { DataDictionary, MessageSystem, MessageSystemType, SchemaDictionary } from '../../message-system';
-import {HTMLRenderTemplate} from './html-render.template';
-import { nativeElementDefinitions } from '../../../../../../sites/site-utilities/src/definitions';
-import { doc } from 'prettier';
+import {
+    customElement,
+    DOM,
+    FASTElement,
+    $global,
+    attr,
+    observable,
+} from "@microsoft/fast-element";
+import {
+    htmlMapper,
+    htmlResolver,
+    mapDataDictionary,
+    ResolverConfig,
+} from "../../data-utilities/mapping";
+import {
+    WebComponentDefinition,
+    WebComponentDefinitionTag,
+} from "../../data-utilities/web-component";
+import {
+    DataDictionary,
+    MessageSystem,
+    MessageSystemType,
+    SchemaDictionary,
+} from "../../message-system";
+import { HTMLRenderTemplate } from "./html-render.template";
+import { nativeElementDefinitions } from "../../../../../../sites/site-utilities/src/definitions";
+import { doc } from "prettier";
 
 @customElement({
-    name: 'fast-tooling-html-render', 
-    template: HTMLRenderTemplate
+    name: "fast-tooling-html-render",
+    template: HTMLRenderTemplate,
 })
-export class HTMLRender extends FASTElement
-{
-
+export class HTMLRender extends FASTElement {
     private dataDictionary: DataDictionary<unknown>;
 
     private schemaDictionary: SchemaDictionary;
@@ -22,13 +40,12 @@ export class HTMLRender extends FASTElement
 
     @observable
     public messageSystem: MessageSystem;
-    private messageSystemChanged(): void
-    {
+    private messageSystemChanged(): void {
         if (this.messageSystem !== undefined) {
-            this.messageSystem.add({onMessage: this.handleMessageSystem});
+            this.messageSystem.add({ onMessage: this.handleMessageSystem });
         }
     }
-    
+
     private handleMessageSystem = (e: MessageEvent): void => {
         if (e.data) {
             if (
@@ -40,20 +57,18 @@ export class HTMLRender extends FASTElement
                 this.RenderMarkup();
             }
         }
-    }
-    
+    };
+
     // private markupPolicy = $global.trustedTypes.createPolicy('markupPolicy',{
     //     createHTML: html => html
     // });
 
-    connectedCallback()
-    {
+    connectedCallback() {
         super.connectedCallback();
-//        DOM.setHTMLPolicy(this.markupPolicy);
+        //        DOM.setHTMLPolicy(this.markupPolicy);
     }
 
-    public RenderMarkup(): void
-    {
+    public RenderMarkup(): void {
         this.markup = mapDataDictionary({
             dataDictionary: this.dataDictionary,
             schemaDictionary: this.schemaDictionary,
@@ -75,37 +90,52 @@ export class HTMLRender extends FASTElement
                     []
                 ),
             }),
-            resolver: this.renderHtmlResolver
+            resolver: this.renderHtmlResolver,
         });
         console.log(this.markup);
     }
 
-    private clickHandler(e:MouseEvent): boolean
-    {
+    public clickHandler(e: MouseEvent): boolean {
+        /*         messageSystem.postMessage({
+            type: MessageSystemType.navigation,
+            action: MessageSystemNavigationTypeAction.update,
+            activeDictionaryId: dictionaryId,
+            activeNavigationConfigId: navigationConfigId,
+            options: {
+                originatorId: navigationId,
+            },
+        });
+ */
+
         console.log("click", e);
         e.stopPropagation();
         return false;
     }
 
-    private renderHtmlResolver(config: ResolverConfig<any>): HTMLElement | Text
-    {
+    private renderHtmlResolver = (config: ResolverConfig<any>): HTMLElement | Text => {
         htmlResolver(config);
         let wrapper = document.createElement("div");
         wrapper.id = config.dictionaryId;
         wrapper.className = "wrap";
-        wrapper.addEventListener("click", this.clickHandler);
-        if(config.dataDictionary[0][config.dictionaryId].parent !== undefined)
-        {
+        wrapper.addEventListener("click", (e: MouseEvent) => {
+            console.log("Click!");
+        });
+        if (config.dataDictionary[0][config.dictionaryId].parent !== undefined) {
             let parentid = config.dataDictionary[0][config.dictionaryId].parent.id;
-            config.dataDictionary[0][parentid].data.childNodes.forEach((value)=>wrapper.appendChild(value));
+            config.dataDictionary[0][parentid].data.childNodes.forEach(value =>
+                wrapper.appendChild(value)
+            );
             config.dataDictionary[0][parentid].data.prepend(wrapper);
-        }
-        else
-        {
+        } else {
             wrapper.appendChild(config.dataDictionary[0][config.dictionaryId].data);
+            let button = document.createElement("button");
+            button.textContent = "Button!";
+            button.addEventListener("click", (e: MouseEvent) => {
+                console.log("Click!");
+            });
+            wrapper.appendChild(button);
             config.dataDictionary[0][config.dictionaryId].data = wrapper;
         }
         return config.dataDictionary[0][config.dictionaryId].data;
-    }
+    };
 }
-
