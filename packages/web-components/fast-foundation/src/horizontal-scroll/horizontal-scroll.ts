825af03550563e0ef5dfc034204013db6d8e84e7
Rob Barber <robarb@microsoft.com>
2021-04-07 15:36:55 -0700

    Updating param

--- packages/web-components/fast-foundation/src/horizontal-scroll/horizontal-scroll.ts
+++ packages/web-components/fast-foundation/src/horizontal-scroll/horizontal-scroll.ts
@@ -163,8 +163,8 @@ export class HorizontalScroll extends FASTElement {
 
     /**
      * Updates scroll stops and flippers when scroll items change
-     * @param previous current scroll items
-     * @param next new updated scroll items
+     * @param previous - current scroll items
+     * @param next - new updated scroll items
      * @public
      */
     public scrollItemsChanged(previous, next) {
