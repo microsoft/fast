6af83fa99f204892a409f9877c0753167eefa194
nicholasrice <nicholasrice@users.noreply.github.com>
2021-04-13 13:25:39 -0700

    fixing logging colors

--- packages/utilities/fast-benchmarks/build/loggers/console.js
+++ packages/utilities/fast-benchmarks/build/loggers/console.js
@@ -2,11 +2,20 @@
 
 const chalk = require("chalk");
 
+function colorByKey(key, value) {
+    switch (key) {
+        case "hz":
+            return value >= 0 ? chalk.green : chalk.red;
+        case "mean":
+            return value >= 0 ? chalk.red : chalk.green;
+        default:
+            return value >= 0 ? chalk.red : chalk.green;
+    }
+}
+
 module.exports = (name, result) => {
     console.log(`Emitting results for "${name.replace(".html", "")}"`);
     Object.keys(result).forEach(key => {
-        const color = result[key] >= 0 ? chalk.green : chalk.red;
-
-        console.log(color(`${key}: ${result[key]}`));
+        console.log(colorByKey(key, result[key])(`${key}: ${result[key]}`));
     });
 };
