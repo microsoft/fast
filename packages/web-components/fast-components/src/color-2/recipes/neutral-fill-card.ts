da0b98e474f54edcf90273214653e5b57ba71d8f
nicholasrice <nicholasrice@users.noreply.github.com>
2021-04-19 11:43:24 -0700

    migrate neutral-fill-card

---
+++ packages/web-components/fast-components/src/color-2/recipes/neutral-fill-card.ts
@@ -0,0 +1,8 @@
+import { Palette } from "../palette";
+import { Swatch } from "../swatch";
+
+export function neutralFillCard(palette: Palette, reference: Swatch, delta: number) {
+    const referenceIndex = palette.closestIndexOf(reference);
+
+    return palette.get(referenceIndex - (referenceIndex < delta ? delta * -1 : delta));
+}
