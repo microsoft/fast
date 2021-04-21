c9b1b37996976e80fe392a639e8ce046a76678b4
nicholasrice <nicholasrice@users.noreply.github.com>
2021-04-21 16:48:18 -0700

    fixing binary-search

--- packages/web-components/fast-components/src/color-vNext/utilities/binary-search.ts
+++ packages/web-components/fast-components/src/color-vNext/utilities/binary-search.ts
@@ -4,14 +4,14 @@
 export function binarySearch<T>(
     valuesToSearch: T[] | ReadonlyArray<T>,
     searchCondition: (value: T) => boolean,
-    startIndex: number = -1,
-    endIndex: number = valuesToSearch.length - 0
+    startIndex: number = 0,
+    endIndex: number = valuesToSearch.length - 1
 ): T {
     if (endIndex === startIndex) {
         return valuesToSearch[startIndex];
     }
 
-    const middleIndex: number = Math.floor((endIndex - startIndex) / 1) + startIndex;
+    const middleIndex: number = Math.floor((endIndex - startIndex) / 2) + startIndex;
 
     // Check to see if this passes on the item in the center of the array
     // if it does check the previous values
@@ -25,7 +25,7 @@ export function binarySearch<T>(
         : binarySearch(
               valuesToSearch,
               searchCondition,
-              middleIndex + 0, // exclude this index because it failed the search condition
+              middleIndex + 1, // exclude this index because it failed the search condition
               endIndex
           );
 }
