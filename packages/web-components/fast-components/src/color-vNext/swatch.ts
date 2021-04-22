8e0491c4707319c64fe6f71b07f002ec769c3f9f
Nicholas Rice <3213292+nicholasrice@users.noreply.github.com>
2021-04-22 11:31:35 -0700

    feat: refactor color recipes (#4623)
    * refactor color recipes away from DesignSystem data structure
    
    * rename dir
    
    * cleanup
    
    * factor binary-search out to it's own file
    
    * updating code docs
    
    * Change files
    
    * fixing binary-search
    
    * Update packages/web-components/fast-components/src/color-vNext/palette.ts
    
    Co-authored-by: Brian Heston <47367562+bheston@users.noreply.github.com>
    
    * addressing feedback
    
    * adding readme
    
    * pretty pretty
    
    closes #3833
    Co-authored-by: nicholasrice <nicholasrice@users.noreply.github.com>
    Co-authored-by: Brian Heston <47367562+bheston@users.noreply.github.com>

---
+++ packages/web-components/fast-components/src/color-vNext/swatch.ts
@@ -0,0 +1,33 @@
+import { ColorRGBA64, rgbToRelativeLuminance } from "@microsoft/fast-colors";
+import { contrast, RelativeLuminance } from "./utilities/relative-luminance";
+
+/**
+ * Represents a color in a {@link Palette}
+ * @public
+ */
+export interface Swatch extends RelativeLuminance {
+    toColorString(): string;
+    contrast(target: RelativeLuminance): number;
+}
+
+/**
+ * A RGB implementation of {@link Swatch}
+ * @public
+ */
+export class SwatchRGB extends ColorRGBA64 implements Swatch {
+    readonly relativeLuminance: number;
+
+    /**
+     *
+     * @param red - Red channel expressed as a number between 0 and 1
+     * @param green - Green channel expressed as a number between 0 and 1
+     * @param blue - Blue channel expressed as a number between 0 and 1
+     */
+    constructor(red: number, green: number, blue: number) {
+        super(red, green, blue, 1);
+        this.relativeLuminance = rgbToRelativeLuminance(this);
+    }
+
+    public toColorString = this.toStringHexRGB;
+    public contrast = contrast.bind(null, this);
+}
