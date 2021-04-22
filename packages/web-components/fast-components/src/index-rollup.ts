3dabb942abf7730a945d24c167104f75edcf2367
Nicholas Rice <3213292+nicholasrice@users.noreply.github.com>
2021-04-22 15:39:08 -0700

    fix: update storybook to use custom element definitions and fix rollup (#4629)
    * update storybook to use custom element defintions
    
    * Change files
    
    Co-authored-by: nicholasrice <nicholasrice@users.noreply.github.com>

--- packages/web-components/fast-components/src/index-rollup.ts
+++ packages/web-components/fast-components/src/index-rollup.ts
@@ -1,4 +1,17 @@
 // TODO: Is exporting Foundation still necessary with the updated API's?
 // export * from "@microsoft/fast-element";
-// export * from "@microsoft/fast-foundation";
+import { DesignSystem } from "@microsoft/fast-foundation";
+import * as fastComponents from "./custom-elements";
+
 export * from "./index";
+
+/**
+ * TODO rename this to FASTDesignSystem when {@link @FASTDesignSystem} interface is removed.
+ */
+export const fastDesignSystem = new DesignSystem();
+
+Object.values(fastComponents).forEach(definition => {
+    fastDesignSystem.register(definition());
+});
+
+fastDesignSystem.applyTo(document.body);
