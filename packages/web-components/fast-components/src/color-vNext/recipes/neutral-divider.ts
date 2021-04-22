02173cf8739ed3a0a6b48ff44632a6aabea50fd1
Nicholas Rice <3213292+nicholasrice@users.noreply.github.com>
2021-04-22 11:31:35 -0700

    feat: refactor color recipes (#4623)
    * refactor color recipes away from DesignSystem data structure
    
    * rename dir
    
    * cleanup
    
    * factor binary-search out to it's own file
    
    * updating code docs
    
    * Change files
    
    * fixing binary-search
    
    * Update packages/web-components/fast-components/src/color-vNext/palette.ts
    
    Co-authored-by: Brian Heston <47367562+bheston@users.noreply.github.com>
    
    * addressing feedback
    
    * adding readme
    
    * pretty pretty
    
    closes #3833
    Co-authored-by: nicholasrice <nicholasrice@users.noreply.github.com>
    Co-authored-by: Brian Heston <47367562+bheston@users.noreply.github.com>

---
+++ packages/web-components/fast-components/src/color-vNext/recipes/neutral-divider.ts
@@ -0,0 +1,17 @@
+import { Swatch } from "../swatch";
+import { Palette } from "../palette";
+import { directionByIsDark } from "../utilities/direction-by-is-dark";
+
+/**
+ * The neutralDivider color recipe
+ * @param palette - The palette to operate on
+ * @param reference - The reference color
+ * @param delta - The offset from the reference
+ *
+ * @internal
+ */
+export function neutralDivider(palette: Palette, reference: Swatch, delta: number) {
+    return palette.get(
+        palette.closestIndexOf(reference) + directionByIsDark(reference) * delta
+    );
+}
