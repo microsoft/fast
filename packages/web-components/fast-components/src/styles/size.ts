06361b0bf123f8be8e964edec93837d91f84e834
nicholasrice <nicholasrice@users.noreply.github.com>
2021-04-23 11:32:51 -0700

    add refactor todo

--- packages/web-components/fast-components/src/styles/size.ts
+++ packages/web-components/fast-components/src/styles/size.ts
@@ -1,10 +1,12 @@
-import { Behavior, CSSDirective, FASTElement } from "@microsoft/fast-element";
+import { Behavior, css, CSSDirective, FASTElement } from "@microsoft/fast-element";
 import { baseHeightMultiplier, density, designUnit } from "../design-tokens";
 
 /**
  * A formula to retrieve the control height.
  * Use this as the value of any CSS property that
  * accepts a pixel size.
+ *
+ * TODO: refactor with https://github.com/microsoft/fast/issues/4633
  */
 class HeightNumber extends CSSDirective implements Behavior {
     private behaviors: Behavior[] = [
