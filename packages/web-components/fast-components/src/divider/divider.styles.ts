7efc7821d50a96f4801ac3ed9f0fdcdef06d914e
Nicholas Rice <3213292+nicholasrice@users.noreply.github.com>
2021-04-23 11:49:34 -0700

    feat: convert FASTDesignSystem to Design Token implementations (#4631)
    * add design token definitions
    
    * create non-color design tokens
    
    * update stylesheets to use design token implementations
    
    * add refactor todo
    
    Co-authored-by: nicholasrice <nicholasrice@users.noreply.github.com>

--- packages/web-components/fast-components/src/divider/divider.styles.ts
+++ packages/web-components/fast-components/src/divider/divider.styles.ts
@@ -1,5 +1,6 @@
 import { css } from "@microsoft/fast-element";
 import { display } from "@microsoft/fast-foundation";
+import { designUnit, outlineWidth } from "../design-tokens";
 import { neutralDividerRestBehavior } from "../styles/index";
 
 export const dividerStyles = (context, definition) =>
@@ -7,9 +8,9 @@ export const dividerStyles = (context, definition) =>
         ${display("block")} :host {
             box-sizing: content-box;
             height: 0;
-            margin: calc(var(--design-unit) * 1px) 0;
+            margin: calc(${designUnit} * 1px) 0;
             border: none;
-            border-top: calc(var(--outline-width) * 1px) solid
+            border-top: calc(${outlineWidth} * 1px) solid
                 ${neutralDividerRestBehavior.var};
         }
     `.withBehaviors(neutralDividerRestBehavior);
