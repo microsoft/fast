57023e8a05f1dc193ea20f2843b4d7e7f96b76ae
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-04-26 14:40:54 -0700

    remove the soft remove from default controls that store a limited set of values

--- packages/tooling/fast-tooling-react/src/form/controls/control.section.tsx
+++ packages/tooling/fast-tooling-react/src/form/controls/control.section.tsx
@@ -184,7 +184,8 @@ class SectionControl extends React.Component<
         required: boolean,
         disabled: boolean,
         label: string,
-        invalidMessage: string | null
+        invalidMessage: string | null,
+        index: number
     ): React.ReactNode => {
         // if this is a root level object use it to generate the form and do not generate a link
         if (
@@ -227,11 +228,14 @@ class SectionControl extends React.Component<
                 messageSystem={this.props.messageSystem}
                 strings={this.props.strings}
                 messageSystemOptions={this.props.messageSystemOptions}
+                type={this.props.type}
+                categories={this.props.categories}
+                index={index}
             />
         );
     };
 
-    private getFormControl(item: string): React.ReactNode {
+    private getFormControl(item: string, index: number): React.ReactNode {
         const splitDataLocation: string[] = this.props.navigation[
             item
         ].relativeDataLocation.split(".");
@@ -254,7 +258,8 @@ class SectionControl extends React.Component<
             getErrorFromDataLocation(
                 this.props.navigation[item].relativeDataLocation,
                 this.props.validationErrors
-            )
+            ),
+            index
         );
     }
 
@@ -319,18 +324,23 @@ class SectionControl extends React.Component<
                                         .sectionControl_categoryContentRegion
                                 }
                             >
-                                {category.dataLocations.map((dataLocation: string) => {
-                                    if (
-                                        navigationItem.items.findIndex(
-                                            item => item === dataLocation
-                                        ) !== -1
-                                    ) {
-                                        categorizedControls.push(dataLocation);
-                                        return this.getFormControl(dataLocation);
-                                    }
+                                {category.dataLocations.map(
+                                    (dataLocation: string, index: number) => {
+                                        if (
+                                            navigationItem.items.findIndex(
+                                                item => item === dataLocation
+                                            ) !== -1
+                                        ) {
+                                            categorizedControls.push(dataLocation);
+                                            return this.getFormControl(
+                                                dataLocation,
+                                                index
+                                            );
+                                        }
 
-                                    return null;
-                                })}
+                                        return null;
+                                    }
+                                )}
                             </div>
                         </fieldset>
                     );
@@ -350,16 +360,16 @@ class SectionControl extends React.Component<
 
                         return accumulation;
                     }, [])
-                    .map(uncategorizedControl => {
-                        return this.getFormControl(uncategorizedControl);
+                    .map((uncategorizedControl: string, index: number) => {
+                        return this.getFormControl(uncategorizedControl, index);
                     }),
                 ...formControls,
             ];
         }
 
         return navigationItem.items.map(
-            (item: string): React.ReactNode => {
-                return this.getFormControl(item);
+            (item: string, index: number): React.ReactNode => {
+                return this.getFormControl(item, index);
             }
         );
     }
@@ -487,7 +497,8 @@ class SectionControl extends React.Component<
                         true,
                         this.props.disabled || this.state.schema.disabled,
                         "",
-                        invalidMessage
+                        invalidMessage,
+                        0
                     )}
                     {this.renderAdditionalProperties()}
                 </div>
