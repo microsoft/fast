99dda35392ee7c23aa79cd318d865293f8c187eb
Chris Holt <chhol@microsoft.com>
2021-05-18 21:48:32 -0700

    add select

--- packages/web-components/fast-foundation/src/select/select.template.ts
+++ packages/web-components/fast-foundation/src/select/select.template.ts
@@ -40,16 +40,7 @@ export const selectTemplate: (context, definition) => ViewTemplate<Select> = (
                 </div>
                 <div class="indicator" part="indicator" aria-hidden="true">
                     <slot name="indicator">
-                        <svg
-                            class="select-indicator"
-                            part="select-indicator"
-                            viewBox="0 0 12 7"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M11.85.65c.2.2.2.5 0 .7L6.4 6.84a.55.55 0 01-.78 0L.14 1.35a.5.5 0 11.71-.7L6 5.8 11.15.65c.2-.2.5-.2.7 0z"
-                            />
-                        </svg>
+                        ${definition.inidicator || ""}
                     </slot>
                 </div>
             </slot>
