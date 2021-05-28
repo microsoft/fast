2cb109c12971f5014fdac693837573d38ccbe03f
nicholasrice <nicholasrice@users.noreply.github.com>
2021-05-27 18:20:29 -0700

    tests

--- packages/web-components/di-dt-demo/src/main.ts
+++ packages/web-components/di-dt-demo/src/main.ts
@@ -1,4 +1,22 @@
-import { fastCard } from "@microsoft/fast-components";
-import { DesignSystem } from "@microsoft/fast-foundation";
+import {
+    fastCard,
+    fillColor,
+    NeutralFillCard,
+    NeutralForeground,
+    SwatchRGB,
+} from "@microsoft/fast-components";
+import { DesignSystem, DI, Registration } from "@microsoft/fast-foundation";
+
+DI.getOrCreateDOMContainer(document.getElementById("root")!).register(
+    Registration.instance(NeutralFillCard, (element: HTMLElement) => {
+        return SwatchRGB.create(1, 0, 0);
+    })
+);
+
+DI.getOrCreateDOMContainer().register(
+    Registration.instance(NeutralForeground, (target: HTMLElement) => {
+        return SwatchRGB.create(0, 0, 1);
+    })
+);
 
 DesignSystem.getOrCreate().register(fastCard());
