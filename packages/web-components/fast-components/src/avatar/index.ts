db8777e1ee22156acacba6ed1d8733f80abdc5fa
Dexter Lesaca <v-dlesac@microsoft.com>
2021-05-27 17:18:26 -0700

    add avatar

---
+++ packages/web-components/fast-components/src/avatar/index.ts
@@ -0,0 +1,65 @@
+import { attr, html, when } from "@microsoft/fast-element";
+import {
+    Avatar as FoundationAvatar,
+    AvatarOptions,
+    avatarTemplate as template,
+} from "@microsoft/fast-foundation";
+import { avatarStyles as styles } from "./avatar.styles";
+
+export class Avatar extends FoundationAvatar {
+    /**
+     * Indicates the Avatar should have an image source
+     *
+     * @public
+     * @remarks
+     * HTML Attribute: src
+     */
+    @attr({ attribute: "src" })
+    public imgSrc: string;
+
+    /**
+     * Indicates the Avatar should have alt text
+     *
+     * @public
+     * @remarks
+     * HTML Attribute: alt
+     */
+    @attr public alt: string;
+}
+
+/**
+ *  The FAST Avatar Element. Implements {@link @microsoft/fast-foundation#Avatar},
+ *  {@link @microsoft/fast-foundation#AvatarTemplate}
+ *
+ *
+ * @public
+ * @remarks
+ * HTML Element: \<fast-avatar\>
+ */
+export const fastAvatar = Avatar.compose<AvatarOptions>({
+    baseName: "avatar",
+    template,
+    styles,
+    media: html<Avatar>`
+        ${when(
+            x => x.imgSrc,
+            html`
+                <img
+                    src="${x => x.imgSrc}"
+                    alt="${x => x.alt}"
+                    class="image"
+                    part="image"
+                />
+            `
+        )}
+    `,
+    shadowOptions: {
+        delegatesFocus: true,
+    },
+});
+
+/**
+ * Styles for Badge
+ * @public
+ */
+export const avatarStyles = styles;
