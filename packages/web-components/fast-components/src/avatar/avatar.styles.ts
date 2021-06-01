2dbec609f26897f08d3077333e9bf176fa4418b5
Dexter Lesaca <v-dlesac@microsoft.com>
2021-06-01 13:05:44 -0700

    add base and styles

--- packages/web-components/fast-components/src/avatar/avatar.styles.ts
+++ packages/web-components/fast-components/src/avatar/avatar.styles.ts
@@ -12,66 +12,66 @@ import {
 
 export const avatarStyles = (context, definition: AvatarOptions) =>
     css`
-    ${display("flex")} :host {
-        position: relative;
-        height: var(--avatar-size, var(--avatar-size-default));
-        max-width: var(--avatar-size, var(--avatar-size-default));
-        --avatar-size-default: calc(
-            (
-                    (${baseHeightMultiplier} + ${density})) * ${designUnit} +
-                        (${designUnit} * 10)
-                ) * 1px
-        );
-        --avatar-text-size: ${typeRampBaseFontSize};
-        --avatar-text-ratio: ${designUnit};
-    }
+        ${display("flex")} :host {
+            position: relative;
+            height: var(--avatar-size, var(--avatar-size-default));
+            max-width: var(--avatar-size, var(--avatar-size-default));
+            --avatar-size-default: calc(
+                (
+                        (${baseHeightMultiplier} + ${density}) * ${designUnit} +
+                            ((${designUnit} * 8) - 40)
+                    ) * 1px
+            );
+            --avatar-text-size: ${typeRampBaseFontSize};
+            --avatar-text-ratio: ${designUnit};
+        }
 
-    .link {
-        text-decoration: none;
-        color: ${neutralForegroundRest};
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        min-width: 100%;
-    }
+        .link {
+            text-decoration: none;
+            color: ${neutralForegroundRest};
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            min-width: 100%;
+        }
 
-    .square {
-        border-radius: calc(${cornerRadius} * 1px);
-        min-width: 100%;
-        overflow: hidden;
-    }
+        .square {
+            border-radius: calc(${cornerRadius} * 1px);
+            min-width: 100%;
+            overflow: hidden;
+        }
 
-    .circle {
-        border-radius: 100%;
-        min-width: 100%;
-        overflow: hidden;
-    }
+        .circle {
+            border-radius: 100%;
+            min-width: 100%;
+            overflow: hidden;
+        }
 
-    .coin {
-        position: relative;
-        display: flex;
-    }
+        .coin {
+            position: relative;
+            display: flex;
+        }
 
-    .image {
-        max-width: 100%;
-        position: absolute;
-        display: block;
-    }
+        .image {
+            max-width: 100%;
+            position: absolute;
+            display: block;
+        }
 
-    .name {
-        font-size: calc(
-            (var(--avatar-text-size) + var(--avatar-size, var(--avatar-size-default))) /
-                var(--avatar-text-ratio)
-        );
-        line-height: ${typeRampPlus5LineHeight};
-        display: block;
-    }
+        .name {
+            font-size: calc(
+                (var(--avatar-text-size) + var(--avatar-size, var(--avatar-size-default))) /
+                    var(--avatar-text-ratio)
+            );
+            line-height: ${typeRampPlus5LineHeight};
+            display: block;
+        }
 
-    ::slotted(fast-badge) {
-        position: absolute;
-        display: block;
-        bottom: 0;
-        right: 0;
-    }
-`;
+        ::slotted(fast-badge) {
+            position: absolute;
+            display: block;
+            bottom: 0;
+            right: 0;
+        }
+    `;
