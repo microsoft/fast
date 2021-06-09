28f2cd0f0741bf2d3cdd5087b89f91bb68a81436
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-06-09 14:31:30 -0700

    chore: split libraries in preparation for dynamic imports (#4844)
    # Pull Request
    
    ## 📖 Description
    
    <!---
    Provide some background and a description of your work.
    What problem does this change solve?
    Is this a breaking change, chore, fix, feature, etc?
    -->
    Planned work for the Creator includes making libraries of components dynamic imports. To facilitate this the libraries have been split which will make them easier to work with as a potential dynamic import in the future. This change also adds a spec file for the Creator site to allow the speccing out of features that the site will have.
    
    Related to #4845
    
    ## 👩‍💻 Reviewer Notes
    
    <!---
    Provide some notes for reviewers to help them provide targeted feedback and testing.
    
    Do you recommend a smoke test for this PR? What steps should be followed?
    Are there particular areas of the code the reviewer should focus on?
    -->
    This change only re-organizes some exports and does not affect the current implementation of the data examples. To check this, run `yarn start` in the FAST creator site folder and add components that have examples using the Form.
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have included a change request file using `$ yarn change`
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [x] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.
    
    ### Component-specific
    
    <!--- Review the list and put an x in the boxes that apply. -->
    <!--- Remove this section if not applicable. -->
    
    - [ ] I have added a new component
    - [ ] I have modified an existing component
    - [ ] I have updated the [definition file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#definition)
    - [ ] I have updated the [configuration file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#configuration)
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->

---
+++ sites/fast-creator/app/configs/library.fast.ts
@@ -0,0 +1,228 @@
+import { uniqueId } from "lodash-es";
+import { fastComponentSchemas, textSchema } from "@microsoft/site-utilities";
+import {
+    fastAnchorTag,
+    fastBadgeTag,
+    fastButtonTag,
+    fastCardTag,
+    fastCheckboxTag,
+    fastDialogTag,
+    fastDividerTag,
+    fastFlipperTag,
+    fastMenuItemTag,
+    fastMenuTag,
+    fastProgressRingTag,
+    fastProgressTag,
+    fastRadioGroupTag,
+    fastRadioTag,
+    fastSliderLabelTag,
+    fastSliderTag,
+    fastSwitchTag,
+    fastTabPanelTag,
+    fastTabsTag,
+    fastTabTag,
+    fastTextAreaTag,
+    fastTextFieldTag,
+} from "./library.fast.tags";
+import { heading2Example, imageExample, paragraphExample } from "./library.native";
+
+export const fastAnchorExample = {
+    schemaId: fastComponentSchemas[fastAnchorTag].id,
+    data: {
+        href: "#",
+    },
+    dataLocation: "Slot",
+    linkedData: [
+        {
+            schemaId: textSchema.id,
+            data: "Anchor",
+            dataLocation: "Slot",
+        },
+    ],
+};
+export const fastButtonExample = {
+    schemaId: fastComponentSchemas[fastButtonTag].id,
+    data: {},
+    dataLocation: "Slot",
+    linkedData: [
+        {
+            schemaId: textSchema.id,
+            data: "Button",
+            dataLocation: "Slot",
+        },
+    ],
+};
+export const fastBadgeExample = {
+    schemaId: fastComponentSchemas[fastBadgeTag].id,
+    data: {},
+    dataLocation: "Slot",
+    linkedData: [
+        {
+            schemaId: textSchema.id,
+            data: "LOREM",
+            dataLocation: "Slot",
+        },
+    ],
+};
+export const fastCardExample = {
+    schemaId: fastComponentSchemas[fastCardTag].id,
+    data: {
+        style: "width: 320px",
+    },
+    dataLocation: "Slot",
+    linkedData: [
+        imageExample,
+        {
+            ...fastBadgeExample,
+            data: {
+                style:
+                    "margin: 5px 10px; --badge-fill-fill: #3278cd; --badge-color-color: white;",
+                fill: "fill",
+                color: "color",
+            },
+        },
+        {
+            ...heading2Example,
+            data: {
+                style: "margin: 5px 10px",
+            },
+        },
+        {
+            ...paragraphExample,
+            data: {
+                style: "margin: 0 10px",
+            },
+        },
+        {
+            ...fastButtonExample,
+            data: {
+                style: "margin: 10px",
+            },
+        },
+    ],
+};
+export const fastCheckboxExample = {
+    schemaId: fastComponentSchemas[fastCheckboxTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastDialogExample = {
+    schemaId: fastComponentSchemas[fastDialogTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastDividerExample = {
+    schemaId: fastComponentSchemas[fastDividerTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastFlipperExample = {
+    schemaId: fastComponentSchemas[fastFlipperTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastMenuItemExample = {
+    schemaId: fastComponentSchemas[fastMenuItemTag].id,
+    data: {},
+    dataLocation: "Slot",
+    linkedData: [
+        {
+            schemaId: textSchema.id,
+            data: "Lorem ipsum sit amet",
+            dataLocation: "Slot",
+        },
+    ],
+};
+export const fastMenuExample = {
+    schemaId: fastComponentSchemas[fastMenuTag].id,
+    data: {},
+    dataLocation: "Slot",
+    linkedData: [fastMenuItemExample, fastMenuItemExample, fastMenuItemExample],
+};
+export const fastProgressExample = {
+    schemaId: fastComponentSchemas[fastProgressTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastProgressRingExample = {
+    schemaId: fastComponentSchemas[fastProgressRingTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastRadioGroupExample = {
+    schemaId: fastComponentSchemas[fastRadioGroupTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastRadioExample = {
+    schemaId: fastComponentSchemas[fastRadioTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastSliderExample = {
+    schemaId: fastComponentSchemas[fastSliderTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastSliderLabelExample = {
+    schemaId: fastComponentSchemas[fastSliderLabelTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastSwitchExample = {
+    schemaId: fastComponentSchemas[fastSwitchTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastTabExample = {
+    schemaId: fastComponentSchemas[fastTabTag].id,
+    data: {
+        id: uniqueId("Tab"),
+    },
+    dataLocation: "Slot",
+    linkedData: [
+        {
+            schemaId: textSchema.id,
+            data: "Lorem ipsum sit amet",
+            dataLocation: "Slot",
+        },
+    ],
+};
+export const fastTabPanelExample = {
+    schemaId: fastComponentSchemas[fastTabPanelTag].id,
+    data: {
+        id: uniqueId("TabPanel"),
+    },
+    dataLocation: "Slot",
+    linkedData: [
+        {
+            schemaId: textSchema.id,
+            data:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ante non massa vulputate facilisis. Vestibulum ac consequat sem. Etiam sodales turpis nec ante hendrerit, ut cursus risus sollicitudin. Curabitur et suscipit ex, sit amet faucibus neque. In ac nunc elementum, porta purus sed, scelerisque dui. Nullam ut tincidunt mi, id imperdiet risus. Curabitur a metus in ligula pellentesque pellentesque. Vivamus porta velit sollicitudin turpis aliquam, sit amet accumsan purus gravida. Etiam euismod ex neque, vel lobortis ligula semper ac. Proin eget metus vel turpis tempus accumsan. Aenean rhoncus ex lectus, a tincidunt justo volutpat quis. Sed id tincidunt purus. Phasellus ultrices, magna id viverra pellentesque, nisl turpis ullamcorper metus, sed varius nunc magna ut lacus.",
+            dataLocation: "Slot",
+        },
+    ],
+};
+export const fastTabsExample = {
+    schemaId: fastComponentSchemas[fastTabsTag].id,
+    data: {},
+    dataLocation: "Slot",
+    linkedData: [
+        { ...fastTabExample, data: { id: uniqueId("Tab") } },
+        { ...fastTabExample, data: { id: uniqueId("Tab") } },
+        { ...fastTabExample, data: { id: uniqueId("Tab") } },
+        { ...fastTabPanelExample, data: { id: uniqueId("Tabpanel") } },
+        { ...fastTabPanelExample, data: { id: uniqueId("Tabpanel") } },
+        { ...fastTabPanelExample, data: { id: uniqueId("Tabpanel") } },
+    ],
+};
+export const fastTextAreaExample = {
+    schemaId: fastComponentSchemas[fastTextAreaTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
+export const fastTextFieldExample = {
+    schemaId: fastComponentSchemas[fastTextFieldTag].id,
+    data: {},
+    dataLocation: "Slot",
+};
