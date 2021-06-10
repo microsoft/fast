dfa05028cc9dad9b5052c517558361ffc94807f9
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-06-10 14:22:47 -0700

    chore: update the component explorer with the new design tokens and remove the design-system-provider component (#4855)
    # Pull Request
    
    ## 📖 Description
    
    <!---
    Provide some background and a description of your work.
    What problem does this change solve?
    Is this a breaking change, chore, fix, feature, etc?
    -->
    This updates the Component Explorer app by removing the `<fast-design-system-provider>` component as a wrapper and uses the design tokens when updates occur.
    
    ## 👩‍💻 Reviewer Notes
    
    <!---
    Provide some notes for reviewers to help them provide targeted feedback and testing.
    
    Do you recommend a smoke test for this PR? What steps should be followed?
    Are there particular areas of the code the reviewer should focus on?
    -->
    
    This change does not update the theme, @bheston do you have a reference as to what needs to happen for theme switching between light and dark mode to occur?
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have included a change request file using `$ yarn change`
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.
    
    ### Component-specific
    
    <!--- Review the list and put an x in the boxes that apply. -->
    <!--- Remove this section if not applicable. -->
    
    - [ ] I have added a new component
    - [ ] I have modified an existing component
    - [ ] I have updated the [definition file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#definition)
    - [ ] I have updated the [configuration file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#configuration)
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->

--- sites/fast-component-explorer/app/app.tsx
+++ sites/fast-component-explorer/app/app.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { Redirect, Route, Router, Switch } from "react-router-dom";
-import { FASTDesignSystemProvider } from "@microsoft/fast-components";
 import ExplorerRegion from "./explorer-region";
 import Preview from "./preview";
 import { history, initialComponentRoute } from "./config";
-
-FASTDesignSystemProvider;
 
 /**
  * The root level app
@@ -15,21 +12,19 @@ FASTDesignSystemProvider;
 export default class App extends React.Component<{}, {}> {
     public render(): React.ReactNode {
         return (
-            <fast-design-system-provider use-defaults density="-2">
-                <Router history={history}>
-                    <Switch>
-                        <Route
-                            component={ExplorerRegion}
-                            exact={true}
-                            path="/components/:component"
-                        />
-                        <Route component={Preview} exact={true} path="/preview" />
-                        <Route>
-                            <Redirect to={initialComponentRoute} />
-                        </Route>
-                    </Switch>
-                </Router>
-            </fast-design-system-provider>
+            <Router history={history}>
+                <Switch>
+                    <Route
+                        component={ExplorerRegion}
+                        exact={true}
+                        path="/components/:component"
+                    />
+                    <Route component={Preview} exact={true} path="/preview" />
+                    <Route>
+                        <Redirect to={initialComponentRoute} />
+                    </Route>
+                </Switch>
+            </Router>
         );
     }
 }
