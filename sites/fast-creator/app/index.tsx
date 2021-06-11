752bb345d6688558bd00139dc374ea5153af0a1f
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-06-10 17:29:18 -0700

    chore: update the creator with the new design tokens and remove the design-system-provider component (#4853)
    # Pull Request
    
    ## 📖 Description
    
    <!---
    Provide some background and a description of your work.
    What problem does this change solve?
    Is this a breaking change, chore, fix, feature, etc?
    -->
    This updates the Creator app by removing the `<fast-design-system-provider>` component as a wrapper and uses the design tokens when updates occur.
    
    ## 👩‍💻 Reviewer Notes
    
    <!---
    Provide some notes for reviewers to help them provide targeted feedback and testing.
    
    Do you recommend a smoke test for this PR? What steps should be followed?
    Are there particular areas of the code the reviewer should focus on?
    -->
    
    This change does not update the theme, @bheston do you have a reference as to what needs to happen for theme switching between light and dark mode to occur?
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have included a change request file using `$ yarn change`
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.
    
    ### Component-specific
    
    <!--- Review the list and put an x in the boxes that apply. -->
    <!--- Remove this section if not applicable. -->
    
    - [ ] I have added a new component
    - [ ] I have modified an existing component
    - [ ] I have updated the [definition file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#definition)
    - [ ] I have updated the [configuration file](https://github.com/Microsoft/fast/blob/master/packages/web-components/fast-components/CONTRIBUTING.md#configuration)
    
    ## ⏭ Next Steps
    
    <!---
    If there is relevant follow-up work to this PR, please list any existing issues or provide brief descriptions of what you would like to do next.
    -->

--- sites/fast-creator/app/index.tsx
+++ sites/fast-creator/app/index.tsx
@@ -1,5 +1,3 @@
-import { fastAnchor } from "@microsoft/fast-components";
-import { DesignSystem } from "@microsoft/fast-foundation";
 import React from "react";
 import { DndProvider } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
@@ -15,8 +13,6 @@ const style: HTMLStyleElement = document.createElement("style");
 style.innerText =
     "@font-face { font-family: SegoeUIVF; src: url(https://static.fast.design/assets/fonts/SegoeUI-Roman-VF_web.ttf) format(\"truetype\"); font-weight: \"1 1000\"; } @font-face { font-family: 'Segoe UI'; src: url('//c.s-microsoft.com/static/fonts/segoe-ui/west-european/Normal/latest.woff2') format('woff2'); } body, html { font-family: SegoeUIVF, Segoe UI, SegoeUI, Helvetica Neue, Helvetica, Arial, sans-serif; font-size: 12px; padding: 0; margin: 0; }";
 document.head.appendChild(style);
-
-DesignSystem.getOrCreate().register(fastAnchor());
 
 /**
  * Primary render function for app. Called on store updates
