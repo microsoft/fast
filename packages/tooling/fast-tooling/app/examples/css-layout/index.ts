968d039d5deecfd9bcd08ed450a0470f57917b79
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-06-24 12:56:18 -0700

    chore: update fast-tooling manual test app to not use the design system provider

--- packages/tooling/fast-tooling/app/examples/css-layout/index.ts
+++ packages/tooling/fast-tooling/app/examples/css-layout/index.ts
@@ -1,9 +1,6 @@
-import { FASTDesignSystemProvider, fastSwitch } from "@microsoft/fast-components";
+import { fastSwitch } from "@microsoft/fast-components";
 import { DesignSystem } from "@microsoft/fast-foundation";
 import { fastToolingCSSLayout } from "../../../src/web-components/css-layout";
-
-// Prevent tree shaking
-FASTDesignSystemProvider;
 
 DesignSystem.getOrCreate()
     .withPrefix("fast-tooling")
