a94dd0f7b4aceddd93dab23e81c2b1eaa4f3dd62
Rob Eisenberg <EisenbergEffect@users.noreply.github.com>
2021-06-24 18:11:31 -0400

    docs(fast-foundation): add api docs for design system and foundation ele (#4903)
    * docs(fast-foundation): add api docs for design system and foundation ele
    
    Also, ensures certain internal, dynamically attached properties are less likely to clash with end user code.
    
    * Change files
    
    * Update packages/web-components/fast-foundation/src/design-system/design-system.ts
    
    Co-authored-by: Brian Heston <47367562+bheston@users.noreply.github.com>
    
    Co-authored-by: EisenbergEffect <roeisenb@microsoft.com>
    Co-authored-by: Brian Heston <47367562+bheston@users.noreply.github.com>

--- packages/web-components/fast-foundation/src/design-system/design-system.ts
+++ packages/web-components/fast-foundation/src/design-system/design-system.ts
@@ -7,39 +7,94 @@ import { Container, DI, InterfaceSymbol, Registration } from "../di/di";
 import { ComponentPresentation } from "./component-presentation";
 
 /**
- * Defines an element within the context of a design system.
- * @alpha
+ * Enables defining an element within the context of a design system.
+ * @public
  */
 export type ContextualElementDefinition = Omit<PartialFASTElementDefinition, "name">;
 
 /**
  * The design system context in which an element can be defined.
- * @alpha
+ * @public
  */
 export interface ElementDefinitionContext {
+    /**
+     * The name that the element will be defined as.
+     * @public
+     */
     readonly name: string;
+
+    /**
+     * The type that will be defined.
+     * @public
+     */
     readonly type: Constructable;
+
+    /**
+     * The dependency injection container associated with the design system.
+     * @public
+     */
     readonly container: Container;
+
+    /**
+     * Indicates whether or not a platform define call will be made in order
+     * to define the element.
+     * @public
+     */
     readonly willDefine: boolean;
+
+    /**
+     * The shadow root mode specified by the design system's configuration.
+     * @public
+     */
     readonly shadowRootMode: ShadowRootMode | undefined;
+
+    /**
+     * Defines the element.
+     * @param definition - The definition for the element.
+     * @public
+     */
     defineElement(definition?: ContextualElementDefinition): void;
+
+    /**
+     * Defines a presentation for the element.
+     * @param presentation - The presentation configuration.
+     * @public
+     */
     definePresentation(presentation: ComponentPresentation): void;
+
+    /**
+     * Returns the HTML element tag name that the type will be defined as.
+     * @param type - The type to lookup.
+     * @public
+     */
     tagFor(type: Constructable): string;
 }
 
 /**
  * The callback type that is invoked when an element can be defined by a design system.
- * @alpha
+ * @public
  */
 export type ElementDefinitionCallback = (ctx: ElementDefinitionContext) => void;
 
 /**
  * Design system contextual APIs and configuration usable within component
  * registries.
- * @alpha
+ * @public
  */
 export interface DesignSystemRegistrationContext {
+    /**
+     * The element prefix specified by the design system's configuration.
+     * @public
+     */
     readonly elementPrefix: string;
+
+    /**
+     * Used to attempt to define a custom element.
+     * @param name - The name of the element to define.
+     * @param type - The type of the constructor to use to define the element.
+     * @param callback - A callback to invoke if definition will happen.
+     * @public
+     */
     tryDefineElement(
         name: string,
         type: Constructable,
@@ -48,7 +103,9 @@ export interface DesignSystemRegistrationContext {
 }
 
 /**
- * @alpha
+ * Design system contextual APIs and configuration usable within component
+ * registries.
+ * @public
  */
 export const DesignSystemRegistrationContext: InterfaceSymbol<DesignSystemRegistrationContext> = DI.createInterface<
     DesignSystemRegistrationContext
@@ -57,7 +114,7 @@ export const DesignSystemRegistrationContext: InterfaceSymbol<DesignSystemRegist
 /**
  * The callback type that is invoked when two elements are trying to define themselves with
  * the same name.
- * @alpha
+ * @public
  */
 export type ElementDisambiguationCallback = (
     nameAttempt: string,
@@ -69,19 +126,46 @@ const elementTypesByTag = new Map<string, Constructable>();
 const elementTagsByType = new Map<Constructable, string>();
 
 /**
- * @alpha
+ * Represents a configurable design system.
+ * @public
  */
 export interface DesignSystem {
+    /**
+     * Registers components and services with the design system and the
+     * underlying dependency injection container.
+     * @param params - The registries to pass to the design system
+     * and the underlying dependency injection container.
+     * @public
+     */
     register(...params: any[]): DesignSystem;
+
+    /**
+     * Configures the prefix to add to each custom element name.
+     * @param prefix - The prefix to use for custom elements.
+     * @public
+     */
     withPrefix(prefix: string): DesignSystem;
+
+    /**
+     * Overrides the default Shadow DOM mode for custom elements.
+     * @param mode - The Shadow DOM mode to use for custom elements.
+     * @public
+     */
     withShadowRootMode(mode: ShadowRootMode): DesignSystem;
+
+    /**
+     * Provides a custom callback capable of resolving scenarios where
+     * two different elements request the same element name.
+     * @param callback - The disambiguation callback.
+     * @public
+     */
     withElementDisambiguation(callback: ElementDisambiguationCallback): DesignSystem;
 }
 
 const designSystemKey = DI.createInterface<DesignSystem>(x =>
     x.cachedCallback(handler => {
         const element = document.body as any;
-        const owned = element.$designSystem as DesignSystem;
+        const owned = element.$$designSystem$$ as DesignSystem;
 
         if (owned) {
             return owned;
@@ -92,15 +176,28 @@ const designSystemKey = DI.createInterface<DesignSystem>(x =>
 );
 
 /**
- * @alpha
+ * An API gateway to design system features.
+ * @public
  */
 export const DesignSystem = Object.freeze({
+    /**
+     * Returns the HTML element name that the type is defined as.
+     * @param type - The type to lookup.
+     * @public
+     */
     tagFor(type: Constructable): string {
         return elementTagsByType.get(type)!;
     },
 
+    /**
+     * Searches the DOM hierarchy for the design system that is responsible
+     * for the provided element.
+     * @param element - The element to locate the design system for.
+     * @returns The located design system.
+     * @public
+     */
     responsibleFor(element: HTMLElement): DesignSystem {
-        const owned = (element as any).$designSystem as DesignSystem;
+        const owned = (element as any).$$designSystem$$ as DesignSystem;
 
         if (owned) {
             return owned;
@@ -110,8 +207,15 @@ export const DesignSystem = Object.freeze({
         return container.get(designSystemKey);
     },
 
+    /**
+     * Gets the DesignSystem if one is explicitly defined on the provided element;
+     * otherwise creates a design system defined directly on the element.
+     * @param element - The element to get or create a design system for.
+     * @returns The design system.
+     * @public
+     */
     getOrCreate(element: HTMLElement = document.body): DesignSystem {
-        const owned = (element as any).$designSystem as DesignSystem;
+        const owned = (element as any).$$designSystem$$ as DesignSystem;
 
         if (owned) {
             return owned;
@@ -139,7 +243,7 @@ class DefaultDesignSystem implements DesignSystem {
     private context: DesignSystemRegistrationContext;
 
     constructor(private host: HTMLElement, private container: Container) {
-        (host as any).$designSystem = this;
+        (host as any).$$designSystem$$ = this;
 
         container.register(
             Registration.callback(DesignSystemRegistrationContext, () => this.context)
