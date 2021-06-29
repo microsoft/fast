91cafd03cb8acc6ef870fe5254faa04ed39fd96d
nicholasrice <nicholasrice@users.noreply.github.com>
2021-06-29 11:15:31 -0700

    export function and document export

--- packages/web-components/fast-foundation/src/utilities/composed-contains.ts
+++ packages/web-components/fast-foundation/src/utilities/composed-contains.ts
@@ -1,5 +1,16 @@
 import { composedParent } from "./composed-parent";
 
+/**
+ * Determines if the reference element contains the test element in a "composed" DOM tree that
+ * ignores shadow DOM boundaries.
+ *
+ * Returns true of the test element is a descendent of the reference, or exist in
+ * a shadow DOM that is a logical descendent of the reference. Otherwise returns false.
+ * @param reference - The element to test for containment against.
+ * @param test - The element being tested for containment.
+ *
+ * @public
+ */
 export function composedContains(reference: HTMLElement, test: HTMLElement): boolean {
     let current: HTMLElement | null = test;
 
