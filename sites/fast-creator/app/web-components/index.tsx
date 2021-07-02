bd177941e59ce85bca8492ed2221ad2abd835385
William Wagner <44823142+williamw2@users.noreply.github.com>
2021-07-02 11:11:04 -0700

    Another merge fix

--- sites/fast-creator/app/web-components/index.tsx
+++ sites/fast-creator/app/web-components/index.tsx
@@ -426,23 +426,6 @@ export class CSSLayout extends React.Component<CSSLayoutProps, {}> {
     }
 }
 
-export class HTMLRenderReact extends React.Component {
-    public renderRef: React.RefObject<HTMLDivElement>;
-
-    private setRenderRef = el => {
-        this.renderRef = el;
-    };
-
-    render() {
-        return (
-            <fast-tooling-html-render ref={this.setRenderRef}>
-                <fast-tooling-html-render-layer-navigation role="htmlrenderlayer"></fast-tooling-html-render-layer-navigation>
-                <fast-tooling-html-render-layer-inline-edit role="htmlrenderlayer"></fast-tooling-html-render-layer-inline-edit>
-            </fast-tooling-html-render>
-        );
-    }
-}
-
 export function renderPreviewSwitch(
     switchState: boolean,
     onChangeCallback: (newState: boolean) => void,
