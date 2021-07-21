780054018d689fd3600c9061b032066f3f5ae940
Brian Heston <47367562+bheston@users.noreply.github.com>
2021-07-21 09:16:03 -0700

    Updated to contrast-based palette

--- packages/web-components/fast-components/src/color/palette.ts
+++ packages/web-components/fast-components/src/color/palette.ts
@@ -1,13 +1,40 @@
 import {
     clamp,
+    ColorHSL,
+    ColorLAB,
     ColorRGBA64,
-    ComponentStateColorPalette,
-    parseColorHexRGB,
+    hslToRGB,
+    interpolateRGB,
+    labToRGB,
+    rgbToHSL,
+    rgbToLAB,
+    rgbToRelativeLuminance,
 } from "@microsoft/fast-colors";
 import { isSwatchRGB, Swatch, SwatchRGB } from "./swatch";
 import { binarySearch } from "./utilities/binary-search";
 import { directionByIsDark } from "./utilities/direction-by-is-dark";
 import { contrast, RelativeLuminance } from "./utilities/relative-luminance";
+
+/**
+ * Options to tailer the generation of the color palette.
+ * @public
+ */
+export interface PaletteOptions {
+    /**
+     * Increases the saturation of a color in the palette if it would otherwise be lower than the source color.
+     */
+    bumpSaturation: boolean;
+
+    /**
+     * The minimum amount of contrast between steps in the palette.
+     */
+    stepContrast: number;
+}
+
+const defaultPaletteOptions: PaletteOptions = {
+    bumpSaturation: true,
+    stepContrast: 1.06,
+};
 
 /**
  * A collection of {@link Swatch} instances
@@ -69,12 +96,12 @@ function create(rOrSource: SwatchRGB | number, g?: number, b?: number): PaletteR
  * Creates a PaletteRGB from a source color object.
  * @param source - The source color
  */
-function from(source: SwatchRGB): PaletteRGB;
+function from(source: SwatchRGB, options?: Partial<PaletteOptions>): PaletteRGB;
 function from(source: Record<"r" | "g" | "b", number>): PaletteRGB;
-function from(source: any): PaletteRGB {
+function from(source: any, options?: Partial<PaletteOptions>): PaletteRGB {
     return isSwatchRGB(source)
-        ? PaletteRGBImpl.from(source)
-        : PaletteRGBImpl.from(SwatchRGB.create(source.r, source.g, source.b));
+        ? PaletteRGBImpl.from(source, options)
+        : PaletteRGBImpl.from(SwatchRGB.create(source.r, source.g, source.b), options);
 }
 /** @public */
 export const PaletteRGB = Object.freeze({
@@ -178,22 +205,148 @@ class PaletteRGBImpl implements Palette<SwatchRGB> {
     }
 
     /**
+     * Gets the color with the requested or darker luminance.
+     * @param lum Max requested luminance
+     * @param palette Palette to search
+     * @returns
+     */
+    private static maxRelativeLuminance(
+        lum: number,
+        palette: ColorRGBA64[]
+    ): ColorRGBA64 {
+        const color = palette.find(clr => rgbToRelativeLuminance(clr) <= lum);
+        return color || palette[palette.length - 1];
+    }
+
+    /**
+     * Bump the saturation if it falls below the reference color saturation.
+     * @param reference Color with target saturation
+     * @param color Color to check and bump if below target saturation
+     * @returns
+     */
+    private static saturationBump(
+        reference: ColorRGBA64,
+        color: ColorRGBA64
+    ): ColorRGBA64 {
+        const hslReference = rgbToHSL(reference);
+        const saturationTarget = hslReference.s;
+        const hslColor = rgbToHSL(color);
+        if (hslColor.s < saturationTarget) {
+            const hslNew = new ColorHSL(hslColor.h, saturationTarget, hslColor.l);
+            return hslToRGB(hslNew);
+        }
+        return color;
+    }
+
+    /**
+     * Scales input from 0 to 100 to 0 to 0.5.
+     * @param l Input number, 0 to 100
+     * @returns Output number, 0 to 0.5
+     */
+    private static ramp(l: number) {
+        const inputval = l / 100;
+        if (inputval > 0.5) return (inputval - 0.5) / 0.5; //from 0.500001in = 0.00000001out to 1.0in = 1.0out
+        return 2 * inputval; //from 0in = 0out to 0.5in = 1.0out
+    }
+
+    private static createSaturationPalette(
+        baseColor: ColorRGBA64,
+        bumpSaturation: boolean
+    ): ColorRGBA64[] {
+        const targetPalette: ColorRGBA64[] = [];
+
+        const labBaseColor = rgbToLAB(baseColor);
+        const lab0 = labToRGB(new ColorLAB(0, labBaseColor.a, labBaseColor.b)).clamp();
+        const lab50 = labToRGB(new ColorLAB(50, labBaseColor.a, labBaseColor.b)).clamp();
+        const lab100 = labToRGB(
+            new ColorLAB(100, labBaseColor.a, labBaseColor.b)
+        ).clamp();
+        const rgbMin = new ColorRGBA64(0, 0, 0);
+        const rgbMax = new ColorRGBA64(1, 1, 1);
+
+        // 257 levels total
+        for (let l = 114; l >= -14; l -= 0.5) {
+            let rgb: ColorRGBA64;
+
+            if (l < 0) {
+                // For L less than 0, scale from black to L=0
+                const percentFromRgbMinToLab0 = l / 14 + 1;
+                rgb = interpolateRGB(percentFromRgbMinToLab0, rgbMin, lab0);
+            } else if (l <= 50) {
+                // For L less than 50, we scale from L=0 to the base color
+                rgb = interpolateRGB(PaletteRGBImpl.ramp(l), lab0, lab50);
+            } else if (l <= 100) {
+                // For L less than 100, scale from the base color to L=100
+                rgb = interpolateRGB(PaletteRGBImpl.ramp(l), lab50, lab100);
+            } else {
+                // For L greater than 100, scale from L=100 to white
+                const percentFromLab100ToRgbMax = (l - 100.0) / 14;
+                rgb = interpolateRGB(percentFromLab100ToRgbMax, lab100, rgbMax);
+            }
+
+            if (bumpSaturation) {
+                rgb = PaletteRGBImpl.saturationBump(lab50, rgb);
+            }
+            rgb = rgb.roundToPrecision(4);
+
+            targetPalette.push(rgb);
+        }
+
+        return targetPalette;
+    }
+
+    private static createColorPaletteByContrast(
+        baseColor: ColorRGBA64,
+        options: PaletteOptions
+    ): ColorRGBA64[] {
+        const referencePalette: ColorRGBA64[] = PaletteRGBImpl.createSaturationPalette(
+            baseColor,
+            options.bumpSaturation
+        );
+
+        const targetPalette: ColorRGBA64[] = [];
+        targetPalette.push(referencePalette[0]);
+
+        const steppedLum = (lum: number) => (lum + 0.05) / options.stepContrast - 0.05;
+
+        let lum: number = 1;
+        do {
+            lum = steppedLum(lum);
+            const peekLum = steppedLum(lum);
+            // TODO: average the drop to black
+            if (peekLum < 0) {
+                lum = 0;
+            }
+
+            const refRgb = PaletteRGBImpl.maxRelativeLuminance(lum, referencePalette);
+            if (lum > 0) {
+                lum = rgbToRelativeLuminance(refRgb);
+            }
+
+            targetPalette.push(refRgb);
+        } while (lum > 0);
+
+        return targetPalette;
+    }
+
+    /**
      * Create a color palette from a provided swatch
      * @param source - The source swatch to create a palette from
      * @returns
      */
-    static from(source: SwatchRGB): PaletteRGB {
+    static from(source: SwatchRGB, options?: Partial<PaletteOptions>): PaletteRGB {
+        const opts =
+            options === void 0 || null
+                ? defaultPaletteOptions
+                : { ...defaultPaletteOptions, ...options };
+
         return new PaletteRGBImpl(
             source,
             Object.freeze(
-                new ComponentStateColorPalette({
-                    /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
-                    baseColor: ColorRGBA64.fromObject(source)!,
-                }).palette.map(x => {
-                    /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
-                    const _x = parseColorHexRGB(x.toStringHexRGB())!;
-                    return SwatchRGB.create(_x.r, _x.g, _x.b);
-                })
+                PaletteRGBImpl.createColorPaletteByContrast(
+                    ColorRGBA64.fromObject(source)!,
+                    opts
+                ).map(x => SwatchRGB.from(x))
             )
         );
     }
