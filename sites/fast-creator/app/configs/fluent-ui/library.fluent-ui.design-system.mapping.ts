2adce3852b578b4924de17333eb487cdac9b1c2b
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-08-03 16:33:51 -0700

    use the western european segoe ui as a fallback

--- sites/fast-creator/app/configs/fluent-ui/library.fluent-ui.design-system.mapping.ts
+++ sites/fast-creator/app/configs/fluent-ui/library.fluent-ui.design-system.mapping.ts
@@ -53,6 +53,41 @@ export function setupFluentUIComponentDesignSystem(
             font-style: normal
         }
         @font-face {
+            font-family: 'Segoe UI';
+            src: url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-light.woff2) format('woff2'),
+                 url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-light.woff) format('woff');
+            font-weight: 100;
+            font-style: normal
+        }
+        @font-face {
+            font-family: 'Segoe UI';
+            src: url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semilight.woff2) format('woff2'),
+                 url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semilight.woff) format('woff');
+            font-weight: 300;
+            font-style: normal
+        }
+        @font-face {
+            font-family: 'Segoe UI';
+            src: url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-regular.woff2) format('woff2'),
+                 url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-regular.woff) format('woff');
+            font-weight: 400;
+            font-style: normal
+        }
+        @font-face {
+            font-family: 'Segoe UI';
+            src: url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semibold.woff2) format('woff2'),
+                 url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-semibold.woff) format('woff');
+            font-weight: 600;
+            font-style: normal
+        }
+        @font-face {
+            font-family: 'Segoe UI';
+            src: url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-bold.woff2) format('woff2'),
+                 url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-westeuropean/segoeui-bold.woff) format('woff');
+            font-weight: 700;
+            font-style: normal
+        }
+        @font-face {
             font-family: 'Segoe UI Web (Arabic)';
             src: url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-light.woff2) format('woff2'),
                  url(https://static2.sharepointonline.com/files/fabric/assets/fonts/segoeui-arabic/segoeui-light.woff) format('woff');
