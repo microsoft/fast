93f740f1da065c93e0373efc50d09a45cbae046a
Chris Holt <chhol@microsoft.com>
2021-08-02 19:50:06 -0700

    menu should check for slot before automatically assigning a value

--- packages/web-components/fast-foundation/src/menu/menu.template.ts
+++ packages/web-components/fast-foundation/src/menu/menu.template.ts
@@ -1,8 +1,8 @@
 import { html, slotted } from "@microsoft/fast-element";
 import type { ViewTemplate } from "@microsoft/fast-element";
-import type { Menu } from "./menu";
 import type { FoundationElementDefinition } from "../foundation-element";
 import type { ElementDefinitionContext } from "../design-system";
+import type { Menu } from "./menu";
 
 /**
  * The template for the {@link @microsoft/fast-foundation#Menu} component.
@@ -16,7 +16,7 @@ export const menuTemplate: (
     definition: FoundationElementDefinition
 ) => html`
     <template
-        slot="${x => (x.isNestedMenu() ? "submenu" : void 0)}"
+        slot="${x => (x.slot ? x.slot : x.isNestedMenu() ? "submenu" : void 0)}"
         role="menu"
         @keydown="${(x, c) => x.handleMenuKeyDown(c.event as KeyboardEvent)}"
         @focusout="${(x, c) => x.handleFocusOut(c.event as FocusEvent)}"
