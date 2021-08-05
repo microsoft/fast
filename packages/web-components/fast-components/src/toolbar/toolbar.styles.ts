b165e565e1533e52109f8f88d7af20fcd0a2e209
Seth Donohue <sethdonohue@Admins-MBP.guest.corp.microsoft.com>
2021-08-05 13:57:07 -0700

    remove __hidden

--- packages/web-components/fast-components/src/toolbar/toolbar.styles.ts
+++ packages/web-components/fast-components/src/toolbar/toolbar.styles.ts
@@ -69,11 +69,6 @@ export const toolbarStyles: (
             margin-inline: 0;
         }
 
-        .start__hidden,
-        .end__hidden {
-            display: none;
-        }
-
         ::slotted(svg) {
             /* TODO: adaptive typography https://github.com/microsoft/fast/issues/2432 */
             width: 16px;
