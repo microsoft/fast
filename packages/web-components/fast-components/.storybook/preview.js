f5241264d4503acc37b456dca0ef234c76f79319
John Kreitlow <john.kreitlow@microsoft.com>
2021-08-16 12:30:13 -0700

    add basic preview configuration for storybook fixture colors

--- packages/web-components/fast-components/.storybook/preview.js
+++ packages/web-components/fast-components/.storybook/preview.js
@@ -1,5 +1,18 @@
 import "../src/index-rollup";
+import * as FAST from "../src/index-rollup";
+import { parseColor } from "@microsoft/fast-colors";
 
 export const parameters = {
     layout: "fullscreen",
 };
+
+const root = document.body;
+
+// By default, we grab the current background color to set the fillColor token.
+// This value can be changed to see the components in light mode.
+const backgroundColor = getComputedStyle(root).backgroundColor;
+
+FAST.fillColor.setValueFor(root, FAST.SwatchRGB.from(parseColor(backgroundColor)));
+
+root.style.backgroundColor = FAST.fillColor.createCSS();
+root.style.color = FAST.neutralForegroundRest.createCSS();
