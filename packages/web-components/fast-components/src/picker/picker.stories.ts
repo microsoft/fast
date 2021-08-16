c8b28c0242e6f5dfa7003a1f67ae9a9f81fd5086
Stephane Comeau <scomea@microsoft.com>
2021-08-16 10:54:01 -0700

    remove unused story code

--- packages/web-components/fast-components/src/picker/picker.stories.ts
+++ packages/web-components/fast-components/src/picker/picker.stories.ts
@@ -1,11 +1,4 @@
-import { STORY_RENDERED } from "@storybook/core-events";
-import addons from "@storybook/addons";
 import PickerTemplate from "./fixtures/picker.html";
-
-addons.getChannel().addListener(STORY_RENDERED, (name: string) => {
-    // if (name.toLowerCase().startsWith("picker")) {
-    // }
-});
 
 export default {
     title: "Picker",
