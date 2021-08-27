29a4d7fcdc0ffb906dd891f061ed268d5a96cc99
Brian Heston <47367562+bheston@users.noreply.github.com>
2021-08-27 11:59:59 -0700

    Fixed website fill color darkness (#5115)
    Co-authored-by: Rob Eisenberg <EisenbergEffect@users.noreply.github.com>

--- sites/fast-website/src/app/index.ts
+++ sites/fast-website/src/app/index.ts
@@ -1,7 +1,9 @@
 import { DesignSystem } from "@microsoft/fast-foundation";
-import { allComponents } from "@microsoft/fast-components";
+import { allComponents, baseLayerLuminance } from "@microsoft/fast-components";
 import "./css/style.css";
 
 DesignSystem.getOrCreate().register(Object.values(allComponents).map(x => x()));
+
+baseLayerLuminance.setValueFor(document.body, 0.09);
 
 export * from "./components";
