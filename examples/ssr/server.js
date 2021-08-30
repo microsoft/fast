af6437fa8e10ed8d9a9417fe40a4624928bbfee4
nicholasrice <nicholasrice@users.noreply.github.com>
2021-08-30 15:22:58 -0700

    remove un-necessary expression keeping process open

--- examples/ssr/server.js
+++ examples/ssr/server.js
@@ -3,9 +3,6 @@ import { render } from "@lit-labs/ssr/lib/render-with-global-dom-shim.js";
 import { Readable } from "stream";
 import { html } from "lit";
 
-// Keep process open until stream closes
-setInterval(() => {}, 1 << 30);
-
 function myTemplate() {
     return html`
         <my-element>foo</my-element>
@@ -16,7 +13,7 @@ const templateResult = myTemplate();
 const ssrResult = render(templateResult);
 const stream = Readable.from(ssrResult);
 
-stream.on("readable", function() {
+stream.on("readable", function () {
     let data;
 
     while ((data = this.read())) {
