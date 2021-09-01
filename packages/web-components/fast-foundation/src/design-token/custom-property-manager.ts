96992612d0cc10da5916ab5dcb147565a09629c6
nicholasrice <nicholasrice@users.noreply.github.com>
2021-09-01 10:22:51 -0700

    configure system to use a single CSSStyleSheet for custom property setting

--- packages/web-components/fast-foundation/src/design-token/custom-property-manager.ts
+++ packages/web-components/fast-foundation/src/design-token/custom-property-manager.ts
@@ -1,86 +1,46 @@
-import { css, DOM, ElementStyles, FASTElement } from "@microsoft/fast-element";
+import { Constructable, DOM, ElementStyles, FASTElement } from "@microsoft/fast-element";
 
 /**
  * Caching mechanism for CSS custom properties
  */
 class CustomPropertyManagerImpl {
-    private static cache = new Map<string, Map<any, ElementStyles>>();
-    private static appliedCache = new WeakMap<HTMLElement, Map<string, ElementStyles>>();
-
-    /**
-     * {@inheritdoc CustomPropertyManager.get}
-     */
-    private getElementStyles(
-        key: { cssCustomProperty: string },
-        value: any
-    ): ElementStyles {
-        let keyCache = CustomPropertyManagerImpl.cache.get(key.cssCustomProperty);
-
-        if (!keyCache) {
-            keyCache = new Map();
-            CustomPropertyManagerImpl.cache.set(key.cssCustomProperty, keyCache);
-        }
-
-        let v = keyCache.get(value);
-
-        if (!v) {
-            v = this.createElementStyles(key, value);
-            keyCache.set(value, v);
-        }
-
-        return v;
-    }
-
-    private getOrCreateAppliedCache(element: HTMLElement): Map<string, ElementStyles> {
-        if (CustomPropertyManagerImpl.appliedCache.has(element)) {
-            return CustomPropertyManagerImpl.appliedCache.get(element)!;
-        }
-
-        return (
-            CustomPropertyManagerImpl.appliedCache.set(element, new Map()) &&
-            CustomPropertyManagerImpl.appliedCache.get(element)!
-        );
-    }
-
-    /**
-     * Creates an ElementStyles with the key/value CSS custom property
-     * on the host
-     */
-    private createElementStyles(
-        token: { cssCustomProperty: string },
-        value: any
-    ): ElementStyles {
-        return css`:host{${token.cssCustomProperty}:${value};}`;
-    }
-
     public addTo(
         element: HTMLElement,
         token: { cssCustomProperty: string },
         value: any
     ): void {
-        if (isFastElement(element)) {
-            const styles = this.getElementStyles(token, value);
-            element.$fastController.addStyles(styles);
-            this.getOrCreateAppliedCache(element).set(token.cssCustomProperty, styles);
-        } else {
-            DOM.queueUpdate(() =>
-                element.style.setProperty(token.cssCustomProperty, value)
-            );
-        }
+        DOM.queueUpdate(() =>
+            PropertyManager.getOrCreate(element).setProperty(
+                token.cssCustomProperty,
+                value
+            )
+        );
+        // if (isFastElement(element)) {
+        //     const styles = this.getElementStyles(token, value);
+        //     // element.$fastController.addStyles(styles);
+        //     this.getOrCreateAppliedCache(element).set(token.cssCustomProperty, styles);
+        // } else {
+        // DOM.queueUpdate(() =>
+        //     element.style.setProperty(token.cssCustomProperty, value)
+        // );
+        // }
     }
 
     public removeFrom(element: HTMLElement, token: { cssCustomProperty: string }): void {
-        if (isFastElement(element)) {
-            const cache = this.getOrCreateAppliedCache(element);
-            const styles = cache.get(token.cssCustomProperty);
+        DOM.queueUpdate(() =>
+            PropertyManager.getOrCreate(element).removeProperty(token.cssCustomProperty)
+        );
+        // if (isFastElement(element)) {
+        //     const cache = this.getOrCreateAppliedCache(element);
+        //     const styles = cache.get(token.cssCustomProperty);
 
-            if (styles) {
-                element.$fastController.removeStyles(styles);
-                cache.delete(token.cssCustomProperty);
-            }
-        } else {
-            DOM.queueUpdate(() => element.style.removeProperty(token.cssCustomProperty));
-        }
+        //     if (styles) {
+        //         element.$fastController.removeStyles(styles);
+        //         cache.delete(token.cssCustomProperty);
+        //     }
+        // } else {
+        // DOM.queueUpdate(() => element.style.removeProperty(token.cssCustomProperty));
+        // }
     }
 }
 
@@ -92,3 +52,63 @@ function isFastElement(element: HTMLElement | FASTElement): element is FASTEleme
  * @internal
  */
 export const CustomPropertyManager = new CustomPropertyManagerImpl();
+
+interface PropertyTarget {
+    setProperty(name: string, value: string | null): void;
+    removeProperty(name: string);
+}
+
+class ConstructableStyleSheetTarget implements PropertyTarget {
+    private target: PropertyTarget;
+    constructor(source: HTMLElement & FASTElement) {
+        const sheet = new CSSStyleSheet();
+        this.target = (sheet.cssRules[sheet.insertRule(":host{}")] as CSSStyleRule).style;
+        source.$fastController.addStyles(ElementStyles.create([sheet]));
+    }
+    public setProperty(name: string, value: string) {
+        this.target.setProperty(name, value);
+    }
+    public removeProperty(name: string) {
+        this.target.removeProperty(name);
+    }
+}
+
+class StyleElementStyleSheetTarget implements PropertyTarget {
+    constructor(target: HTMLElement & FASTElement) {
+        throw new Error();
+    }
+    public setProperty(name: string, value: string) {
+        console.log("setting");
+    }
+    public removeProperty(name: string) {
+        console.log("removing");
+    }
+}
+
+class PropertyManager {
+    private static cache: WeakMap<HTMLElement, PropertyManager> = new WeakMap();
+    public target: PropertyTarget;
+    private static propertyTargetCtor: Constructable<
+        PropertyTarget
+    > = ConstructableStyleSheetTarget;
+    constructor(public readonly source: HTMLElement) {
+        PropertyManager.cache.set(source, this);
+        if (!isFastElement(source)) {
+            this.target = source.style;
+        } else {
+            this.target = new PropertyManager.propertyTargetCtor(source);
+        }
+    }
+
+    public setProperty(name: string, value: any) {
+        this.target.setProperty(name, value);
+    }
+
+    public removeProperty(name) {
+        this.target.removeProperty(name);
+    }
+
+    public static getOrCreate(source: HTMLElement) {
+        return PropertyManager.cache.get(source) || new PropertyManager(source);
+    }
+}
