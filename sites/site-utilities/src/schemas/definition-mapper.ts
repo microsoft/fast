ceb410988cab40c8c03df42a45c7065999923626
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-09-01 13:17:05 -0700

    feat: allow an autocomplete in the form to use an alias

--- sites/site-utilities/src/schemas/definition-mapper.ts
+++ sites/site-utilities/src/schemas/definition-mapper.ts
@@ -16,6 +16,23 @@ const fastComponentExtendedSchemas: { [key: string]: any } = {};
 const fluentUIComponentExtendedSchemas: { [key: string]: any } = {};
 const nativeElementExtendedSchemas: { [key: string]: any } = {};
 
+const headingAlias: string = "Heading level";
+const alias: { [key: string]: string } = {
+    h1: `${headingAlias} 1`,
+    h2: `${headingAlias} 2`,
+    h3: `${headingAlias} 3`,
+    h4: `${headingAlias} 4`,
+    h5: `${headingAlias} 5`,
+    h6: `${headingAlias} 6`,
+    p: "Paragraph",
+    hr: "Thematic Break (Horizontal Rule)",
+    ol: "Ordered list",
+    ul: "Unordered list",
+    li: "List item",
+    div: "Container",
+    a: "Hyperlink",
+};
+
 function mapToJSONSchemas(
     definitions: { [key: string]: WebComponentDefinition },
     schemas: { [key: string]: any },
@@ -26,7 +43,14 @@ function mapToJSONSchemas(
             mapWebComponentDefinitionToJSONSchema(definition).forEach(
                 (definitionTagItem: any) => {
                     const jsonSchema = definitionTagItem;
-                    schemas[jsonSchema.$id] = jsonSchema;
+                    schemas[jsonSchema.$id] = {
+                        ...jsonSchema,
+                        ...(alias[jsonSchema.$id]
+                            ? {
+                                  alias: alias[jsonSchema.$id],
+                              }
+                            : {}),
+                    };
 
                     if (libraryName) {
                         schemas[jsonSchema.$id].title = `${
