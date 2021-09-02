497b019bfa4a9af65ee5d5382cb1eef2c8d7e073
Brian Heston <47367562+bheston@users.noreply.github.com>
2021-09-01 22:00:42 -0700

    Removed remaining 'use-defaults' which no longer exists (#5145)

--- packages/tooling/fast-tooling-react/src/form/custom-elements.d.ts
+++ packages/tooling/fast-tooling-react/src/form/custom-elements.d.ts
@@ -4,9 +4,7 @@ declare namespace JSX {
         "fast-design-system-provider": React.DetailedHTMLProps<
             React.HTMLAttributes<HTMLElement>,
             HTMLElement
-        > & {
-            "use-defaults"?: boolean;
-        };
+        >;
         "fast-checkbox": React.DetailedHTMLProps<
             React.HTMLAttributes<HTMLElement>,
             HTMLElement
@@ -76,7 +74,7 @@ declare namespace JSX {
         "fast-tooling-file-action-objecturl": React.DetailedHTMLProps<
             React.HTMLAttributes<HTMLElement>,
             HTMLElement
-        > & {};
+        >;
     }
 }
 
