6a79b93d7d7f582ffaf2e41fcc4f0d75cab5ac03
nicholasrice <nicholasrice@users.noreply.github.com>
2021-09-07 11:56:35 -0700

    Revert "updata accordion, anchor and button add a few tokens and recipes"
    This reverts commit 3776bfe141a53ba2735d777b4b758668dfa5d784.

--- packages/web-components/fast-components/src/button/button.styles.ts
+++ packages/web-components/fast-components/src/button/button.styles.ts
@@ -5,17 +5,22 @@ import {
     forcedColorsStylesheetBehavior,
     FoundationElementDefinition,
 } from "@microsoft/fast-foundation";
+import { SystemColors } from "@microsoft/fast-web-utilities";
+import {
+    accentFillRest,
+    accentForegroundRest,
+    disabledOpacity,
+    neutralFillRest,
+    neutralFillStealthRest,
+} from "../design-tokens";
 import {
     AccentButtonStyles,
-    baseButtonStyles,
+    BaseButtonStyles,
     LightweightButtonStyles,
     OutlineButtonStyles,
     StealthButtonStyles,
-} from "../styles/";
+} from "../styles/index";
 import { appearanceBehavior } from "../utilities/behaviors";
-import { disabledOpacity } from "../design-tokens";
-
-const interactivitySelector: string = ":not([disabled])";
 
 export const buttonStyles: (
     context: ElementDefinitionContext,
@@ -25,34 +30,132 @@ export const buttonStyles: (
     definition: FoundationElementDefinition
 ) =>
     css`
-        :host([disabled]) {
+        :host([disabled]),
+        :host([disabled]:hover),
+        :host([disabled]:active) {
             opacity: ${disabledOpacity};
+            background-color: ${neutralFillRest};
             cursor: ${disabledCursor};
         }
 
-        ${baseButtonStyles(context, definition, interactivitySelector)}
+        ${BaseButtonStyles}
     `.withBehaviors(
         forcedColorsStylesheetBehavior(
             css`
-                :host([disabled]) {
+                :host([disabled]),
+                :host([disabled]) .control,
+                :host([disabled]:hover),
+                :host([disabled]:active) {
+                    forced-color-adjust: none;
+                    background-color: ${SystemColors.ButtonFace};
+                    border-color: ${SystemColors.GrayText};
+                    color: ${SystemColors.GrayText};
+                    cursor: ${disabledCursor};
                     opacity: 1;
                 }
             `
         ),
         appearanceBehavior(
             "accent",
-            AccentButtonStyles(context, definition, interactivitySelector)
+            css`
+                :host([appearance="accent"][disabled]),
+                :host([appearance="accent"][disabled]:hover),
+                :host([appearance="accent"][disabled]:active) {
+                    background: ${accentFillRest};
+                }
+
+                ${AccentButtonStyles}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([appearance="accent"][disabled]) .control,
+                        :host([appearance="accent"][disabled]) .control:hover {
+                            background: ${SystemColors.ButtonFace};
+                            border-color: ${SystemColors.GrayText};
+                            color: ${SystemColors.GrayText};
+                        }
+                    `
+                )
+            )
         ),
         appearanceBehavior(
             "lightweight",
-            LightweightButtonStyles(context, definition, interactivitySelector)
+            css`
+                :host([appearance="lightweight"][disabled]:hover),
+                :host([appearance="lightweight"][disabled]:active) {
+                    background-color: transparent;
+                    color: ${accentForegroundRest};
+                }
+
+                :host([appearance="lightweight"][disabled]) .content::before,
+                :host([appearance="lightweight"][disabled]:hover) .content::before,
+                :host([appearance="lightweight"][disabled]:active) .content::before {
+                    background: transparent;
+                }
+
+                ${LightweightButtonStyles}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([appearance="lightweight"].disabled) .control {
+                            forced-color-adjust: none;
+                            color: ${SystemColors.GrayText};
+                        }
+
+                        :host([appearance="lightweight"].disabled)
+                            .control:hover
+                            .content::before {
+                            background: none;
+                        }
+                    `
+                )
+            )
         ),
         appearanceBehavior(
             "outline",
-            OutlineButtonStyles(context, definition, interactivitySelector)
+            css`
+                :host([appearance="outline"][disabled]),
+                :host([appearance="outline"][disabled]:hover),
+                :host([appearance="outline"][disabled]:active) {
+                    background: transparent;
+                    border-color: ${accentFillRest};
+                }
+
+                ${OutlineButtonStyles}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([appearance="outline"][disabled]) .control {
+                            border-color: ${SystemColors.GrayText};
+                        }
+                    `
+                )
+            )
         ),
         appearanceBehavior(
             "stealth",
-            StealthButtonStyles(context, definition, interactivitySelector)
+            css`
+                :host([appearance="stealth"][disabled]),
+                :host([appearance="stealth"][disabled]:hover),
+                :host([appearance="stealth"][disabled]:active) {
+                    background: ${neutralFillStealthRest};
+                }
+
+                ${StealthButtonStyles}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([appearance="stealth"][disabled]) {
+                            background: ${SystemColors.ButtonFace};
+                        }
+
+                        :host([appearance="stealth"][disabled]) .control {
+                            background: ${SystemColors.ButtonFace};
+                            border-color: transparent;
+                            color: ${SystemColors.GrayText};
+                        }
+                    `
+                )
+            )
         )
     );
