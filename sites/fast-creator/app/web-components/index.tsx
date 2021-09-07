7a3438e6dafbb47f13e0b39bcf611e8e5a6872a0
William Wagner <44823142+williamw2@users.noreply.github.com>
2021-09-07 16:30:14 -0700

    feat: add margin, padding and width to css styles in creator (#5161)
    * Start
    
    * add width padding and margin to css styles
    
    * revert changes to css-data

--- sites/fast-creator/app/web-components/index.tsx
+++ sites/fast-creator/app/web-components/index.tsx
@@ -214,6 +214,30 @@ export function getSliderControls(
     ];
 }
 
+function renderCSSTextControl(
+    styleName: string,
+    labelText: string,
+    config: CSSControlConfig
+): React.ReactNode {
+    const id: string = "style-" + styleName;
+    return (
+        <div>
+            <label htmlFor={id}>{labelText}</label>
+            <fast-text-field
+                id={id}
+                value={config.css[styleName]}
+                events={{
+                    change: (e: React.ChangeEvent): void => {
+                        config.onChange({
+                            [styleName]: (e.target as HTMLInputElement).value,
+                        });
+                    },
+                }}
+            ></fast-text-field>
+        </div>
+    );
+}
+
 function getCSSControls(): StandardControlPlugin {
     return new StandardControlPlugin({
         id: "style",
@@ -236,6 +260,27 @@ function getCSSControls(): StandardControlPlugin {
                                         onChange={config.onChange}
                                     />
                                 );
+                            },
+                        }),
+                        new CSSStandardControlPlugin({
+                            id: "margin",
+                            propertyNames: ["margin"],
+                            control: (config: CSSControlConfig) => {
+                                return renderCSSTextControl("margin", "margin", config);
+                            },
+                        }),
+                        new CSSStandardControlPlugin({
+                            id: "padding",
+                            propertyNames: ["padding"],
+                            control: (config: CSSControlConfig) => {
+                                return renderCSSTextControl("padding", "padding", config);
+                            },
+                        }),
+                        new CSSStandardControlPlugin({
+                            id: "width",
+                            propertyNames: ["width"],
+                            control: (config: CSSControlConfig) => {
+                                return renderCSSTextControl("width", "width", config);
                             },
                         }),
                     ]}
