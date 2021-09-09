44cf26dc0514d49972c4a747ae0d587f06612611
William Wagner <44823142+williamw2@users.noreply.github.com>
2021-09-09 15:00:07 -0700

    fix: fixed html renderer exception in some messaging scenarios (#5174)
    * fixing drag messaging in preview
    
    * Change files
    
    * prettier
    
    Co-authored-by: Jane Chu <7559015+janechu@users.noreply.github.com>

--- sites/fast-creator/app/preview/preview.tsx
+++ sites/fast-creator/app/preview/preview.tsx
@@ -10,7 +10,9 @@ import {
     MessageSystemOutgoing,
     MessageSystemType,
     NavigationMessageOutgoing,
+    RemoveLinkedDataDataMessageOutgoing,
     SchemaDictionary,
+    UpdateDataMessageIncoming,
 } from "@microsoft/fast-tooling";
 import FASTMessageSystemWorker from "@microsoft/fast-tooling/dist/message-system.min.js";
 import { ViewerCustomAction } from "@microsoft/fast-tooling-react";
@@ -227,7 +229,6 @@ class Preview extends Foundation<{}, {}, PreviewState> {
             } catch (e) {
                 return;
             }
-
             if (
                 messageData !== undefined &&
                 (!(messageData as any).options ||
@@ -247,15 +248,31 @@ class Preview extends Foundation<{}, {}, PreviewState> {
                             this.updateDOM(messageData as MessageSystemOutgoing)
                         );
                         break;
-                    case MessageSystemType.data:
+                    case MessageSystemType.data: {
+                        const dictionaryId: Partial<PreviewState> =
+                            typeof (messageData as RemoveLinkedDataDataMessageOutgoing)
+                                .activeDictionaryId === "string"
+                                ? {
+                                      activeDictionaryId: (messageData as RemoveLinkedDataDataMessageOutgoing)
+                                          .activeDictionaryId,
+                                  }
+                                : typeof (messageData as UpdateDataMessageIncoming)
+                                      .dictionaryId === "string"
+                                ? {
+                                      activeDictionaryId: (messageData as UpdateDataMessageIncoming)
+                                          .dictionaryId,
+                                  }
+                                : {};
                         this.setState(
                             {
                                 dataDictionary: (messageData as DataMessageOutgoing)
                                     .dataDictionary,
+                                ...(dictionaryId as {}),
                             },
                             this.updateDOM(messageData as MessageSystemOutgoing)
                         );
                         break;
+                    }
                     case MessageSystemType.navigation:
                         if (
                             !(messageData as any).options ||
