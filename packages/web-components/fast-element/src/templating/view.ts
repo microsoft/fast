ce22168145a8c364c853c429c4d03ef1926cdb7c
nicholasrice <nicholasrice@users.noreply.github.com>
2021-09-16 12:53:45 -0700

    remove dependency on Range object

--- packages/web-components/fast-element/src/templating/view.ts
+++ packages/web-components/fast-element/src/templating/view.ts
@@ -81,10 +81,6 @@ export interface SyntheticView extends View {
     dispose(): void;
 }
 
-// A singleton Range instance used to efficiently remove ranges of DOM nodes.
-// See the implementation of HTMLView below for further details.
-const range = document.createRange();
-
 /**
  * The standard View implementation, which also implements ElementView and SyntheticView.
  * @public
@@ -257,18 +253,8 @@ export class HTMLView implements ElementView, SyntheticView {
             return;
         }
 
-        range.setStartBefore(views[0].firstChild);
-        range.setEndAfter(views[views.length - 1].lastChild);
-        range.deleteContents();
-
-        for (let i = 0, ii = views.length; i < ii; ++i) {
-            const view = views[i] as any;
-            const behaviors = view.behaviors as Behavior[];
-            const oldSource = view.source;
-
-            for (let j = 0, jj = behaviors.length; j < jj; ++j) {
-                behaviors[j].unbind(oldSource);
-            }
-        }
+        views.forEach(view => {
+            view.dispose();
+        });
     }
 }
