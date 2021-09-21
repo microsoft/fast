69dcdb5c2dc4ad2868c33081be4e2479258a8324
Jane Chu <7559015+janechu@users.noreply.github.com>
2021-09-21 09:49:50 -0700

    chore: remove the fast-tooling, fast-tooling-react, and fast-tooling-wasm packages as they are now in a separate fast-tooling repository (#5202)
    # Pull Request
    
    ## 📖 Description
    
    <!---
    Provide some background and a description of your work.
    What problem does this change solve?
    Is this a breaking change, chore, fix, feature, etc?
    -->
    This change removes the following packages:
    - `@microsoft/fast-tooling`
    - `@microsoft/fast-tooling-react`
    - `@microsoft/fast-tooling-wasm`
    
    Reliant on #5203 to pass the build.
    
    It also modifies files that make direct reference to the packages as part of the pipeline.
    
    ## 👩‍💻 Reviewer Notes
    
    <!---
    Provide some notes for reviewers to help them provide targeted feedback and testing.
    
    Do you recommend a smoke test for this PR? What steps should be followed?
    Are there particular areas of the code the reviewer should focus on?
    -->
    Ensure that this is in no way blocking publishing of sites and packages that remain in the repository.
    
    ## ✅ Checklist
    
    ### General
    
    <!--- Review the list and put an x in the boxes that apply. -->
    
    - [ ] I have included a change request file using `$ yarn change`
    - [ ] I have added tests for my changes.
    - [x] I have tested my changes.
    - [ ] I have updated the project documentation to reflect my changes.
    - [x] I have read the [CONTRIBUTING](https://github.com/Microsoft/fast/blob/master/CONTRIBUTING.md) documentation and followed the [standards](https://www.fast.design/docs/community/code-of-conduct/#our-standards) for this project.

--- packages/tooling/fast-tooling/src/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import linkedDataSchema, { dictionaryLink } from "./linked-data.schema";
-import webComponentSchema from "./web-component.schema";
-export * from "./keywords";
-
-export { linkedDataSchema, dictionaryLink, webComponentSchema };
