331a16ba34c67ac71890b723fdef1c611a3f6b59
Ibrahim Maga <imaga75@hotmail.com>
2021-09-23 09:19:43 -0700

    beachball config

--- beachball.config.js
+++ beachball.config.js
@@ -1,2 +1,19 @@
-require("./build/releasing/ts-node-register");
-module.exports = require("./build/releasing/index");
+/*require("./build/releasing/ts-node-register");
+module.exports = require("./build/releasing/index");*/
+
+module.exports = {
+    disallowedChangeTypes: ["major"],
+    ignorePatterns: [
+        ".ignore",
+        ".github/",
+        ".prettierrc",
+        ".vscode/",
+        "docs/**",
+        "jest..js",
+        "src/e2e/",
+        "src/tests/",
+        "src/fixtures/**",
+        // This one is especially important (otherwise dependabot would be blocked by change file requirements)
+        "yarn.lock",
+    ],
+};
