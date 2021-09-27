46ecbea0bf52c270a962286cd680511cfadbb54a
nicholasrice <nicholasrice@users.noreply.github.com>
2021-09-27 15:10:48 -0700

    wire up express server to serve SSR results

--- examples/ssr/server.ts
+++ examples/ssr/server.ts
@@ -1,27 +1,38 @@
 /* eslint-disable */
+import express, { Request, Response } from "express";
 import "./dom";
 import { render } from "@lit-labs/ssr/lib/render-lit-html";
 import { Readable } from "stream";
 import { FASTElementRenderer } from "./element-renderer";
 import { myTemplate } from "./experience";
 
-const templateResult = myTemplate();
-const ssrResult = render(templateResult, {
-    elementRenderers: [FASTElementRenderer],
-    customElementHostStack: [],
-    customElementInstanceStack: [],
-});
-const stream = (Readable as any).from(ssrResult);
+function handleRequest(req: Request, res: Response) {
+    res.set("Content-Type", "text/html");
+    const templateResult = myTemplate();
+    const ssrResult = render(templateResult, {
+        elementRenderers: [FASTElementRenderer],
+        customElementHostStack: [],
+        customElementInstanceStack: [],
+    });
+    const stream = (Readable as any).from(ssrResult);
 
-stream.on("readable", function (this: any) {
-    let data;
+    stream.on("readable", function(this: any) {
+        let data;
 
-    while ((data = this.read())) {
-        console.log(data);
-    }
-});
-stream.on("close", () => process.exit(0));
-stream.on("error", (e: Error) => {
-    console.error(e);
-    process.exit(1);
-});
+        while ((data = this.read())) {
+            res.write(data);
+        }
+    });
+    stream.on("close", () => res.end());
+    stream.on("error", (e: Error) => {
+        console.error(e);
+        process.exit(1);
+    });
+}
+
+const port = 8080;
+const app = express();
+app.get("/", handleRequest);
+
+console.log(`FAST SSR demo started at http://localhost:${port}`);
+app.listen(8080);
