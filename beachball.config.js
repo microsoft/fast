572680ca0a727c185e2fd41e4c7b99c43bdb81f6
Ibrahim Maga <imaga75@hotmail.com>
2021-09-28 13:12:09 -0700

    add include

--- beachball.config.js
+++ beachball.config.js
@@ -6,7 +6,7 @@ module.exports = {
     groups: [
         {
             name: "excluded packages",
-            excludes: ["sites/website"],
+            include: ["packages/*"],
         },
     ],
     disallowedChangeTypes: ["major"],
