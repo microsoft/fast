bdf433f2a4d707d8113aed11c35cbc17317e0b55
Chris Holt <chhol@microsoft.com>
2021-10-05 20:21:50 -0700

    add vscode and open-ui definitions

--- packages/web-components/fast-components/src/abbr/index.ts
+++ packages/web-components/fast-components/src/abbr/index.ts
@@ -3,7 +3,7 @@ import { abbrStyles as styles } from "./abbr.styles";
 
 /**
  * A function that returns a {@link @microsoft/fast-foundation#Abbr} registration for configuring the component with a DesignSystem.
- * Implements {@link @microsoft/fast-foundation#abbrTemplate} and {@link @microsoft/fast-foundation#Abbr:class}.
+ * Implements {@link @microsoft/fast-foundation#abbrTemplate} and {@link @microsoft/fast-foundation#(Abbr:class)}.
  *
  *
  * @public
