439e0754a4bbeaa5add5c50746618cdc0f32a370
nicholasrice <nicholasrice@users.noreply.github.com>
2021-10-07 16:26:33 -0700

    adding default slot test

--- examples/ssr/tests/ssr.spec.ts
+++ examples/ssr/tests/ssr.spec.ts
@@ -78,3 +78,18 @@ test.skip(`should render a custom element registered with a closed shadow root w
 
 	expect(shadowRoot).toBeNull();
 });
+
+test("should render text into the default slot of an element with a default slot", async ({page}) => {
+	await page.goto(ROOT_URL);
+	const target = await page.$("fast-main #slotted-text");
+	const text = await target.innerText();
+
+	expect(text).toBe("Shadow DOM slotted text");
+});
+
+test("should render an element into the default slot of an element with a default slot", async ({page}) => {
+	await page.goto(ROOT_URL);
+	const target = await page.$("fast-main #slotted-element > fast-leaf");
+
+	expect(target).not.toBeNull();
+});
