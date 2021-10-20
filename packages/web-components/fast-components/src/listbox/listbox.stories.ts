323af90aa740b2a437ec0fccfc4f6bb7d5b07e3d
John Kreitlow <john.kreitlow@microsoft.com>
2021-10-20 14:29:49 -0700

    update fixtures for listbox and select

--- packages/web-components/fast-components/src/listbox/listbox.stories.ts
+++ packages/web-components/fast-components/src/listbox/listbox.stories.ts
@@ -1,11 +1,7 @@
 import Base from "./fixtures/base.html";
-import Multiple from "./fixtures/multiple.html";
-import "../listbox-option";
-import "./index";
 
 export default {
     title: "Listbox",
 };
 
 export const Listbox = () => Base;
-export const ListboxMultiple = () => Multiple;
--- packages/web-components/fast-components/src/select/select.stories.ts
+++ packages/web-components/fast-components/src/select/select.stories.ts
@@ -1,9 +1,7 @@
 import Examples from "./fixtures/base.html";
-import Multiple from "./fixtures/multiple.html";
 
 export default {
     title: "Select",
 };
 
 export const Select = () => Examples;
-export const SelectMultiple = () => Multiple;
