e12c863b89d680f19da75f1c89386803bfee7779
Chris Holt <chhol@microsoft.com>
2021-10-20 14:31:20 -0700

    fix react issues and disable ban types for empty object as its useful in tsx defs

--- sites/site-utilities/.eslintrc.js
+++ sites/site-utilities/.eslintrc.js
@@ -1,7 +1,20 @@
 module.exports = {
-    extends: ["@microsoft/eslint-config-fast-dna", "prettier"],
+    extends: [
+        "@microsoft/eslint-config-fast-dna",
+        "plugin:react/recommended",
+        "prettier",
+    ],
     rules: {
         "@typescript-eslint/no-unused-vars": "off",
         "react/display-name": "off",
+        "@typescript-eslint/ban-types": [
+            "error",
+            {
+                types: {
+                    "{}": false,
+                },
+                extendDefaults: true,
+            },
+        ],
     },
 };
