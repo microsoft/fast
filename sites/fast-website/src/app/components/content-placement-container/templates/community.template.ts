a5799eb9fee2b277b2cfa7c4570fe1103e26f388
Chris Holt <chhol@microsoft.com>
2021-10-21 13:53:29 -0700

    add links to integration docs for frameworks on website (#5306)

--- sites/fast-website/src/app/components/content-placement-container/templates/community.template.ts
+++ sites/fast-website/src/app/components/content-placement-container/templates/community.template.ts
@@ -1,13 +1,12 @@
 import { html } from "@microsoft/fast-element";
+import { linkTemplate } from "./link.template";
 
 const communityTemplate = html`
     <site-content-placement icon>
         <div slot="icon" :innerHTML=${x => x.icon}></div>
         <h3>${x => x.header}</h3>
         <p slot="body">${x => x.body}</p>
-        <fast-anchor slot="action" appearance="lightweight" href=${x => x.actionLink}>
-            ${x => x.actionText}
-        </fast-anchor>
+        ${linkTemplate("action")}
     </site-content-placement>
 `;
 
