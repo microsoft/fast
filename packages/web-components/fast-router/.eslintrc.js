abc64fc780599b18ec7e983bf030b4dabce13f3f
Chris Holt <chhol@microsoft.com>
2021-10-28 16:00:00 -0700

    turn off no-non-null-assertion in eslint

--- packages/web-components/fast-router/.eslintrc.js
+++ packages/web-components/fast-router/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ["@microsoft/eslint-config-fast-dna", "prettier"],
     rules: {
+        "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/ban-types": [
             "error",
             {
