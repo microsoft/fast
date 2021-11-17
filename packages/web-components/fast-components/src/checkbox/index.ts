d1015d2dd6a12a52a67893f48b34b089cccb0e5c
Kham Udom <khamu@microsoft.com>
2021-11-17 15:11:46 -0800

    updated checkbox visual

--- packages/web-components/fast-components/src/checkbox/index.ts
+++ packages/web-components/fast-components/src/checkbox/index.ts
@@ -19,21 +19,14 @@ export const fastCheckbox = Checkbox.compose<CheckboxOptions>({
     template,
     styles,
     checkedIndicator: `
-        <svg
-            part="checked-indicator"
-            class="checked-indicator"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M8.143 12.6697L15.235 4.5L16.8 5.90363L8.23812 15.7667L3.80005 11.2556L5.27591 9.7555L8.143 12.6697Z"
-            />
+        <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6.375 10.0429L4.47855 8.14645C4.28329 7.9512 3.96671 7.9512 3.77145 8.14645C3.57619 8.3417 3.57619 8.6583 3.77145 8.85355L6.02145 11.1035C6.21671 11.2988 6.53329 11.2988 6.72856 11.1035L12.2285 5.60355C12.4238 5.40829 12.4238 5.09171 12.2285 4.89645C12.0333 4.70119 11.7167 4.70119 11.5215 4.89645L6.375 10.0429Z"/>
         </svg>
     `,
     indeterminateIndicator: `
-        <div part="indeterminate-indicator" class="indeterminate-indicator"></div>
+        <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <rect x="4" y="7" width="8" height="2" rx="1"/>
+        </svg>
     `,
 });
 
