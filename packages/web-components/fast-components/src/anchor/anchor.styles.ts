607441680d5fa7eb4b26939459e12e06008e7abe
Kham Udom <37851220+khamudom@users.noreply.github.com>
2021-11-17 14:01:59 -0800

    chore: visual refresh on buttons and anchors (#5382)
    * update button style, anchor style and pattern button styles
    
    * formatting pattern button style
    
    * update active state on lightweight and hypertext. use underline on hypertext focus
    
    * moved high contrast href style to anchor styles. removed !important. default interactivitySelector string in pattern button style
    
    * removed background on none href anchor
    
    * update interactivitySelector and add nonInteractivitySelector
    
    * removed neutral selector and moved outline selector into outline appearance

--- packages/web-components/fast-components/src/anchor/anchor.styles.ts
+++ packages/web-components/fast-components/src/anchor/anchor.styles.ts
@@ -1,5 +1,12 @@
 import { css, ElementStyles } from "@microsoft/fast-element";
-import { AnchorOptions, ElementDefinitionContext } from "@microsoft/fast-foundation";
+import {
+    AnchorOptions,
+    ElementDefinitionContext,
+    focusVisible,
+    forcedColorsStylesheetBehavior,
+} from "@microsoft/fast-foundation";
+import { SystemColors } from "@microsoft/fast-web-utilities";
+import { focusStrokeWidth, strokeWidth } from "../design-tokens";
 import {
     AccentButtonStyles,
     BaseButtonStyles,
@@ -7,8 +14,11 @@ import {
     LightweightButtonStyles,
     OutlineButtonStyles,
     StealthButtonStyles,
-} from "../styles/index";
+} from "../styles/";
 import { appearanceBehavior } from "../utilities/behaviors";
+
+const interactivitySelector: string = "[href]";
+const nonInteractivitySelector: string = ":not([href])";
 
 /**
  * Styles for Anchor
@@ -19,11 +29,178 @@ export const anchorStyles: (
     definition: AnchorOptions
 ) => ElementStyles = (context: ElementDefinitionContext, definition: AnchorOptions) =>
     css`
-        ${BaseButtonStyles}
+        :host .control:not([href]) {
+            cursor: default;
+        }
+        ${BaseButtonStyles(
+            context,
+            definition,
+            interactivitySelector,
+            nonInteractivitySelector
+        )}
     `.withBehaviors(
-        appearanceBehavior("accent", AccentButtonStyles),
-        appearanceBehavior("hypertext", HypertextStyles),
-        appearanceBehavior("lightweight", LightweightButtonStyles),
-        appearanceBehavior("outline", OutlineButtonStyles),
-        appearanceBehavior("stealth", StealthButtonStyles)
+        forcedColorsStylesheetBehavior(
+            css`
+              :host([href]) .control {
+                  border-color: ${SystemColors.LinkText};
+                  background: ${SystemColors.ButtonFace};
+                  color: ${SystemColors.LinkText};
+                  fill: currentcolor;
+              }
+              :host([href]) .control:hover {
+                  forced-color-adjust: none;
+                  border-color: ${SystemColors.LinkText} ;
+                  box-shadow: 0 0 0 1px ${SystemColors.LinkText} ;
+                  background: ${SystemColors.ButtonFace};
+                  color: ${SystemColors.LinkText};
+              }
+              :host([href]) .control:${focusVisible} {
+                  forced-color-adjust: none;
+                  border-color: ${SystemColors.LinkText} ;
+                  box-shadow: 0 0 0 calc(${focusStrokeWidth} * 1px) ${SystemColors.LinkText} ;
+              }
+          `
+        ),
+        appearanceBehavior(
+            "accent",
+            css`
+                ${AccentButtonStyles(
+                    context,
+                    definition,
+                    interactivitySelector,
+                    nonInteractivitySelector
+                )}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([href]) .control {
+                            background: ${SystemColors.LinkText};
+                            color: ${SystemColors.HighlightText};
+                        }
+                        :host([href]) .control:hover {
+                            background: ${SystemColors.ButtonFace};
+                            border-color: ${SystemColors.LinkText};
+                            box-shadow: none;
+                            color: ${SystemColors.LinkText};
+                            fill: currentcolor;
+                        }
+                        :host([href]) .control:${focusVisible} {
+                            box-shadow: 0 0 0 calc((${focusStrokeWidth} - ${strokeWidth}) * 1px) ${SystemColors.LinkText} inset,
+                              0 0 0 calc(((${focusStrokeWidth} * 2) - ${strokeWidth}) * 1px) ${SystemColors.HighlightText} inset ;
+                        }
+                    `
+                )
+            )
+        ),
+        appearanceBehavior(
+            "hypertext",
+            css`
+                ${HypertextStyles(
+                    context,
+                    definition,
+                    interactivitySelector,
+                    nonInteractivitySelector
+                )}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([href]) .control:hover,
+                        :host([href]) .control:${focusVisible} {
+                            box-shadow: none ;
+                            text-decoration: underline 3px;
+                        }
+                    `
+                )
+            )
+        ),
+        appearanceBehavior(
+            "lightweight",
+            css`
+                ${LightweightButtonStyles(
+                    context,
+                    definition,
+                    interactivitySelector,
+                    nonInteractivitySelector
+                )}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([href]) .control {
+                            border-color: ${SystemColors.ButtonFace};
+                        }
+                        :host([href]) .control:hover,
+                        :host([href]) .control:${focusVisible} {
+                            border-color: ${SystemColors.ButtonFace} ;
+                            box-shadow: none ;
+                            background: ${SystemColors.ButtonFace};
+                            color: ${SystemColors.LinkText};
+                        }
+                        :host([href]) .control:hover .content::before,
+                        :host([href]) .control:${focusVisible} .content::before {
+                            background: ${SystemColors.LinkText};
+                        }
+                    `
+                )
+            )
+        ),
+        appearanceBehavior(
+            "outline",
+            css`
+                ${OutlineButtonStyles(
+                    context,
+                    definition,
+                    interactivitySelector,
+                    nonInteractivitySelector
+                )}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([href]) {
+                            border-color: ${SystemColors.LinkText};
+                        }
+                        :host([href]) .control${interactivitySelector}:${focusVisible} {
+                            forced-color-adjust: none;
+                            border-color: ${SystemColors.LinkText} ;
+                            box-shadow: 0 0 0 calc(${focusStrokeWidth} * 1px) ${SystemColors.LinkText} ;
+                            color: ${SystemColors.LinkText};
+                        }
+                        :host([href]) .control:hover {
+                            forced-color-adjust: none;
+                            border-color: ${SystemColors.LinkText} ;
+                            box-shadow: 0 0 0 1px ${SystemColors.LinkText} ;
+                            background: ${SystemColors.ButtonFace};
+                            color: ${SystemColors.LinkText};
+                        }
+                    `
+                )
+            )
+        ),
+        appearanceBehavior(
+            "stealth",
+            css`
+                ${StealthButtonStyles(
+                    context,
+                    definition,
+                    interactivitySelector,
+                    nonInteractivitySelector
+                )}
+            `.withBehaviors(
+                forcedColorsStylesheetBehavior(
+                    css`
+                        :host([href]) .control {
+                            border-color: ${SystemColors.ButtonFace};
+                            color: ${SystemColors.LinkText};
+                        }
+                        :host([href])  .control${interactivitySelector}:hover,
+                        :host([href])  .control${interactivitySelector}:active,
+                        :host([href])  .control${interactivitySelector}:${focusVisible} {
+                            background: ${SystemColors.LinkText};
+                            border-color: ${SystemColors.LinkText} ;
+                            color: ${SystemColors.HighlightText};
+                            fill: currentcolor;
+                        }
+                    `
+                )
+            )
+        )
     );
