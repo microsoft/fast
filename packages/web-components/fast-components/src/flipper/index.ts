73aeb9240cb1a67cce11c71e1c7470691d6fb3b7
Kham Udom <37851220+khamudom@users.noreply.github.com>
2021-11-18 15:03:22 -0800

    update flipper state visual and svg (#5405)

--- packages/web-components/fast-components/src/flipper/index.ts
+++ packages/web-components/fast-components/src/flipper/index.ts
@@ -19,17 +19,21 @@ export const fastFlipper = Flipper.compose<FlipperOptions>({
     template,
     styles,
     next: `
-        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M4.023 15.273L11.29 8 4.023.727l.704-.704L12.71 8l-7.984 7.977-.704-.704z"
-            />
+        <svg
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M5.53 2.86a.67.67 0 0 0 0 .94L9.73 8l-4.2 4.2a.67.67 0 1 0 .94.94l4.67-4.67a.67.67 0 0 0 0-.94L6.47 2.86a.67.67 0 0 0-.94 0Z"/>
         </svg>
     `,
     previous: `
-        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M11.273 15.977L3.29 8 11.273.023l.704.704L4.71 8l7.266 7.273-.704.704z"
-            />
+        <svg
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M10.47 2.86c.26.26.26.68 0 .94L6.27 8l4.2 4.2a.67.67 0 1 1-.94.94L4.86 8.47a.67.67 0 0 1 0-.94l4.67-4.67a.67.67 0 0 1 .94 0Z"/>
         </svg>
     `,
 });
