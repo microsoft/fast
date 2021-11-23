21d5ea2a9ffcb4eccbfe8f044726d257fbf78be0
Kham Udom <37851220+khamudom@users.noreply.github.com>
2021-11-23 08:45:30 -0800

    chore: visual refresh on switch component (#5403)
    * update visual on switch
    
    * add accent color on checked state. update indicator position within the control
    
    * update focus color
    
    * update focus color on checkbox

--- packages/web-components/fast-components/src/switch/index.ts
+++ packages/web-components/fast-components/src/switch/index.ts
@@ -19,7 +19,9 @@ export const fastSwitch = Switch.compose<SwitchOptions>({
     template,
     styles,
     switch: `
-        <span class="checked-indicator" part="checked-indicator"></span>
+        <svg width="8" height="8" viewBox="0 0 8 8" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="4" cy="4" r="4"/>
+        </svg>
     `,
 });
 
