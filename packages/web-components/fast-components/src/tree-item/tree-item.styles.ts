ea9ca6c0c8179e3e49d14ecaa9b6c0ad99bc6fa7
Kham Udom <khamu@microsoft.com>
2021-12-02 09:54:20 -0800

    format tree-item styles

--- packages/web-components/fast-components/src/tree-item/tree-item.styles.ts
+++ packages/web-components/fast-components/src/tree-item/tree-item.styles.ts
@@ -39,12 +39,15 @@ const ltr = css`
     .expand-collapse-button svg {
         transform: rotate(0deg);
     }
+
     :host(.nested) .expand-collapse-button {
         left: var(--expand-collapse-button-nested-width, calc(${heightNumber} * -1px));
     }
+
     :host([selected])::after {
         left: calc(${focusStrokeWidth} * 1px);
     }
+
     :host([expanded]) > .positioning-region .expand-collapse-button svg {
         transform: rotate(90deg);
     }
@@ -54,12 +57,15 @@ const rtl = css`
     .expand-collapse-button svg {
         transform: rotate(180deg);
     }
+
     :host(.nested) .expand-collapse-button {
         right: var(--expand-collapse-button-nested-width, calc(${heightNumber} * -1px));
     }
+
     :host([selected])::after {
         right: calc(${focusStrokeWidth} * 1px);
     }
+
     :host([expanded]) > .positioning-region .expand-collapse-button svg {
         transform: rotate(270deg);
     }
@@ -95,175 +101,198 @@ export const treeItemStyles: (
     definition: TreeItemOptions
 ) => ElementStyles = (context: ElementDefinitionContext, definition: TreeItemOptions) =>
     css`
-    ${display("block")} :host {
-      contain: content;
-      position: relative;
-      outline: none;
-      color: ${neutralForegroundRest};
-      fill: currentcolor;
-      cursor: pointer;
-      font-family: ${bodyFont};
-      --expand-collapse-button-size: calc(${heightNumber} * 1px);
-      --tree-item-nested-width: 0;
-    }
-    :host(:focus) > .positioning-region,
-    :host(:focus) .content-region {
-      outline: none;
-    }
-    .positioning-region {
-      display: flex;
-      position: relative;
-      box-sizing: border-box;
-      background: ${neutralFillStealthRest};
-      border: calc(${strokeWidth} * 1px) solid transparent;
-      border-radius: calc(${controlCornerRadius} * 1px);
-      height: calc((${heightNumber} + 1) * 1px);
-    }
-    :host(:not([disabled])) .positioning-region:hover {
-      background: ${neutralFillStealthHover};
-      color: ${neutralForegroundHover};
-    }
-    :host(:not([disabled])) .positioning-region:active {
-      background: ${neutralFillStealthActive};
-      color: ${neutralForegroundActive};
-    }
-    :host(:${focusVisible}) .positioning-region {
-      background: ${neutralFillStealthFocus}
-      border-color: ${focusStrokeOuter};
-      box-shadow: 0 0 0 calc(${focusStrokeWidth} * 1px) ${focusStrokeOuter} inset;
-      color: ${neutralForegroundHover};
-    }
-    .positioning-region::before {
-      content: "";
-      display: block;
-      width: var(--tree-item-nested-width);
-      flex-shrink: 0;
-    }
-    .content-region {
-      display: inline-flex;
-      align-items: center;
-      white-space: nowrap;
-      width: 100%;
-      height: calc(${heightNumber} * 1px);
-      margin-inline-start: calc(${designUnit} * 2px + 8px);
-      font-size: ${typeRampBaseFontSize};
-      line-height: ${typeRampBaseLineHeight};
-      font-weight: 400;
-    }
-    .items {
-      display: none;
-      ${
-          /* Font size should be based off calc(1em + (design-unit + glyph-size-number) * 1px) -
-            update when density story is figured out */ ""
-      } font-size: calc(1em + (${designUnit} + 16) * 1px);
-    }
-    .expand-collapse-button {
-      background: none;
-      border: none;
-      border-radius: calc(${controlCornerRadius} * 1px);
-      outline: none;
-      ${
-          /* Width and Height should be based off calc(glyph-size-number + (design-unit * 4) * 1px) -
-            update when density story is figured out */ ""
-      } width: calc((${expandCollapseButtonSize} + (${designUnit} * 2)) * 1px);
-      height: calc((${expandCollapseButtonSize} + (${designUnit} * 2)) * 1px);
-      padding: 0;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      cursor: pointer;
-      margin: 0 6px;
-    }
-    .expand-collapse-button svg {
-      transition: transform 0.1s linear;
-      pointer-events: none;
-    }
-    .start,
-    .end {
-      display: flex;
-    }
-    .start {
-      ${
-          /* need to swap out once we understand how horizontalSpacing will work */ ""
-      } margin-inline-end: calc(${designUnit} * 2px + 2px);
-    }
-    .end {
-      ${
-          /* need to swap out once we understand how horizontalSpacing will work */ ""
-      } margin-inline-start: calc(${designUnit} * 2px + 2px);
-    }
-    :host(.expanded) > .items {
-      display: block;
-    }
-    :host([disabled]) {
-      opacity: ${disabledOpacity};
-      cursor: ${disabledCursor};
-    }
-    :host(.nested) .content-region {
-      position: relative;
-      margin-inline-start: var(--expand-collapse-button-size);
-    }
-    :host(.nested) .expand-collapse-button {
-      position: absolute;
-    }
-    :host(.nested) .expand-collapse-button:hover {
-      background: ${expandCollapseHoverBehavior};
-    }
-    :host(:not([disabled])[selected]) .positioning-region {
-      background: ${neutralFillStealthActive};
-      color: ${neutralForegroundActive}
-    }
-    :host(:not([disabled])[selected]) .expand-collapse-button:hover {
-      background: ${selectedExpandCollapseHoverBehavior};
-    }
-    ::slotted(${context.tagFor(TreeItem)}) {
-      --tree-item-nested-width: 1em;
-      --expand-collapse-button-nested-width: calc(${heightNumber} * -1px);
-    }
+        ${display("block")} :host {
+            --expand-collapse-button-size: calc(${heightNumber} * 1px);
+            --tree-item-nested-width: 0;
+            contain: content;
+            position: relative;
+            outline: none;
+            color: ${neutralForegroundRest};
+            fill: currentcolor;
+            cursor: pointer;
+            font-family: ${bodyFont};
+        }
+
+        :host(:focus) > .positioning-region,
+        :host(:focus) .content-region {
+            outline: none;
+        }
+
+        .positioning-region {
+            display: flex;
+            position: relative;
+            box-sizing: border-box;
+            background: ${neutralFillStealthRest};
+            border: calc(${strokeWidth} * 1px) solid transparent;
+            border-radius: calc(${controlCornerRadius} * 1px);
+            height: calc((${heightNumber} + 1) * 1px);
+        }
+
+        :host(:not([disabled])) .positioning-region:hover {
+            background: ${neutralFillStealthHover};
+            color: ${neutralForegroundHover};
+        }
+
+        :host(:not([disabled])) .positioning-region:active {
+            background: ${neutralFillStealthActive};
+            color: ${neutralForegroundActive};
+        }
+
+        :host(:${focusVisible}) .positioning-region {
+            background: ${neutralFillStealthFocus}
+            border-color: ${focusStrokeOuter};
+            box-shadow: 0 0 0 calc(${focusStrokeWidth} * 1px) ${focusStrokeOuter} inset;
+            color: ${neutralForegroundHover};
+        }
+
+        .positioning-region::before {
+            content: "";
+            display: block;
+            width: var(--tree-item-nested-width);
+            flex-shrink: 0;
+        }
+
+        .content-region {
+            display: inline-flex;
+            align-items: center;
+            white-space: nowrap;
+            width: 100%;
+            height: calc(${heightNumber} * 1px);
+            margin-inline-start: calc(${designUnit} * 2px + 8px);
+            font-size: ${typeRampBaseFontSize};
+            line-height: ${typeRampBaseLineHeight};
+            font-weight: 400;
+        }
+
+        .items {
+            display: none;
+            /* Font size should be based off calc(1em + (design-unit + glyph-size-number) * 1px) - update when density story is figured out */
+            font-size: calc(1em + (${designUnit} + 16) * 1px);
+        }
+
+        .expand-collapse-button {
+            background: none;
+            border: none;
+            border-radius: calc(${controlCornerRadius} * 1px);
+            outline: none;
+            /* Width and Height should be based off calc(glyph-size-number + (design-unit * 4) * 1px) - update when density story is figured out */
+            width: calc((${expandCollapseButtonSize} + (${designUnit} * 2)) * 1px);
+            height: calc((${expandCollapseButtonSize} + (${designUnit} * 2)) * 1px);
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+            margin: 0 6px;
+        }
+
+        .expand-collapse-button svg {
+            transition: transform 0.1s linear;
+            pointer-events: none;
+        }
+
+        .start,
+        .end {
+            display: flex;
+        }
+
+        .start {
+            /* need to swap out once we understand how horizontalSpacing will work */
+            margin-inline-end: calc(${designUnit} * 2px + 2px);
+        }
+
+        .end {
+            /* need to swap out once we understand how horizontalSpacing will work */
+            margin-inline-start: calc(${designUnit} * 2px + 2px);
+        }
+
+        :host(.expanded) > .items {
+            display: block;
+        }
+
+        :host([disabled]) {
+            opacity: ${disabledOpacity};
+            cursor: ${disabledCursor};
+        }
+
+        :host(.nested) .content-region {
+            position: relative;
+            margin-inline-start: var(--expand-collapse-button-size);
+        }
+
+        :host(.nested) .expand-collapse-button {
+            position: absolute;
+        }
+
+        :host(.nested) .expand-collapse-button:hover {
+            background: ${expandCollapseHoverBehavior};
+        }
+
+        :host(:not([disabled])[selected]) .positioning-region {
+            background: ${neutralFillStealthActive};
+            color: ${neutralForegroundActive}
+        }
+
+        :host(:not([disabled])[selected]) .expand-collapse-button:hover {
+            background: ${selectedExpandCollapseHoverBehavior};
+        }
+
+        ::slotted(${context.tagFor(TreeItem)}) {
+            --tree-item-nested-width: 1em;
+            --expand-collapse-button-nested-width: calc(${heightNumber} * -1px);
+        }
   `.withBehaviors(
         new DirectionalStyleSheetBehavior(ltr, rtl),
         forcedColorsStylesheetBehavior(
             css`
-              :host {
-                forced-color-adjust: none;
-                border-color: transparent;
-                background: ${SystemColors.ButtonFace};
-                color: ${SystemColors.ButtonText};
-              }
-              .positioning-region {
-                background: ${SystemColors.ButtonFace};
-              }
-              :host(:not([disabled])) .positioning-region:hover,
-              :host(:not([disabled])[selected]) .positioning-region {
-                background: ${SystemColors.Highlight};
-                color: ${SystemColors.HighlightText};
-              }
-              :host([selected])::after {
-                background: ${SystemColors.HighlightText};
-              }
-              :host(:${focusVisible}) .positioning-region {
-                border-color: ${SystemColors.ButtonText};
-                box-shadow: 0 0 0 2px inset ${SystemColors.ButtonFace};
-                color: ${SystemColors.ButtonText};
-              }
-              :host([disabled]) {
-                opacity: 1;
-              }
-              :host([disabled]) .positioning-region {
-                color: ${SystemColors.GrayText};
-                fill: currentcolor;
-              }
-              :host([disabled]:${focusVisible}) .positioning-region {
-                border-color: ${SystemColors.GrayText};
-                box-shadow: none;
-                background: ${SystemColors.ButtonFace};
-                color:  ${SystemColors.GrayText};
-              }
-              :host(.nested) .expand-collapse-button:hover,
-              :host(:not([disabled])[selected]) .expand-collapse-button:hover {
-                background: ${SystemColors.ButtonFace};
-                fill: ${SystemColors.ButtonText};
-              }
+                :host {
+                    forced-color-adjust: none;
+                    border-color: transparent;
+                    background: ${SystemColors.ButtonFace};
+                    color: ${SystemColors.ButtonText};
+                }
+
+                .positioning-region {
+                    background: ${SystemColors.ButtonFace};
+                }
+
+                :host(:not([disabled])) .positioning-region:hover,
+                :host(:not([disabled])[selected]) .positioning-region {
+                    background: ${SystemColors.Highlight};
+                    color: ${SystemColors.HighlightText};
+                }
+
+                :host([selected])::after {
+                    background: ${SystemColors.HighlightText};
+                }
+
+                :host(:${focusVisible}) .positioning-region {
+                    border-color: ${SystemColors.ButtonText};
+                    box-shadow: 0 0 0 2px inset ${SystemColors.ButtonFace};
+                    color: ${SystemColors.ButtonText};
+                }
+
+                :host([disabled]) {
+                    opacity: 1;
+                }
+
+                :host([disabled]) .positioning-region {
+                    color: ${SystemColors.GrayText};
+                    fill: currentcolor;
+                }
+
+                :host([disabled]:${focusVisible}) .positioning-region {
+                    border-color: ${SystemColors.GrayText};
+                    box-shadow: none;
+                    background: ${SystemColors.ButtonFace};
+                    color:  ${SystemColors.GrayText};
+                }
+
+                :host(.nested) .expand-collapse-button:hover,
+                :host(:not([disabled])[selected]) .expand-collapse-button:hover {
+                    background: ${SystemColors.ButtonFace};
+                    fill: ${SystemColors.ButtonText};
+                }
             `
         )
     );
