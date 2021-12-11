cfbfbe3872571ed26d9cbdbc974b8af4b8fb3e6c
Kham Udom <khamu@microsoft.com>
2021-12-10 17:08:04 -0800

    update menu examples

--- packages/web-components/fast-components/src/menu-item/menu-item.styles.ts
+++ packages/web-components/fast-components/src/menu-item/menu-item.styles.ts
@@ -60,75 +60,94 @@ export const menuItemStyles: (
         border: calc(${strokeWidth} * 1px) solid transparent;
         background: ${neutralFillStealthRest};
     }
+
     :host(.indent-0) {
         grid-template-columns: auto 1fr minmax(32px, auto);
     }
+
     :host(.indent-0) .content {
       grid-column: 1;
       grid-row: 1;
       margin-inline-start: 10px;
     }
+
     :host(.indent-0[aria-haspopup="menu"]) {
         display: grid;
         grid-template-columns: minmax(32px, auto) auto 1fr minmax(32px, auto) minmax(32px, auto);
         align-items: center;
         min-height: 32px;
     }
+
     :host(.indent-0[aria-haspopup="menu"]) .content {
         grid-area: 1 / 1 / auto / auto;
         margin-inline-start: 10px;
     }
+
     :host(.indent-0[aria-haspopup="menu"]) .expand-collapse-glyph-container {
         grid-column: 5;
     }
+
     :host(.indent-2) {
         grid-template-columns: minmax(32px, auto) minmax(32px, auto) 1fr minmax(32px, auto) minmax(32px, auto);
     }
+
     :host(.indent-2) .content {
         grid-column: 3;
         grid-row: 1;
         margin-inline-start: 10px;
     }
+
     :host(.indent-2) .expand-collapse-glyph-container {
         grid-column: 5;
         grid-row: 1;
     }
+
     :host(.indent-2) .start {
         grid-column: 2;
     }
+
     :host(.indent-2) .end {
         grid-column: 4;
     }
+
     :host(:not([disabled]):hover) {
         background: ${neutralFillStealthHover};
         color: ${neutralForegroundHover};
+        position: relative;
+        z-index: 1;
     }
+
     :host(:not([disabled]):active),
     :host(.expanded) {
         background: ${neutralFillStealthActive};
         color: ${neutralForegroundActive};
     }
+
     :host(:${focusVisible}) {
         background: ${neutralFillStealthFocus};
         border: calc(${strokeWidth} * 1px) solid ${focusStrokeOuter};
         box-shadow: 0 0 0 calc(${focusStrokeWidth} * 1px) ${focusStrokeOuter} inset;
         color: ${neutralForegroundFocus};
     }
+
     :host([disabled]) {
         cursor: ${disabledCursor};
         opacity: ${disabledOpacity};
     }
+
     .content {
         grid-column-start: 2;
         justify-self: start;
         overflow: hidden;
         text-overflow: ellipsis;
     }
+
     .start,
     .end {
         display: flex;
         justify-content: center;
     }
+
     :host(.indent-1[aria-haspopup="menu"]),
     :host(.indent-1[role="menuitemcheckbox"]),
     :host(.indent-1[role="menuitemradio"]) {
@@ -137,28 +156,34 @@ export const menuItemStyles: (
         align-items: center;
         min-height: 32px;
     }
+
     :host(.indent-2:not([aria-haspopup="menu"])) .end {
         grid-column: 5;
     }
+
     :host .input-container,
     :host .expand-collapse-glyph-container {
         display: none;
     }
+
     :host([aria-haspopup="menu"]) .expand-collapse-glyph-container,
     :host([role="menuitemcheckbox"]) .input-container,
     :host([role="menuitemradio"]) .input-container {
         display: grid;
     }
+
     :host([aria-haspopup="menu"]) .content,
     :host([role="menuitemcheckbox"]) .content,
     :host([role='menuitemradio']) .content {
         grid-column-start: 3;
     }
+
     :host([aria-haspopup="menu"]) .end,
     :host([role="menuitemcheckbox"]) .end,
     :host([role="menuitemradio"]) .end {
         grid-column-start: 4;
     }
+
     :host .expand-collapse,
     :host .checkbox,
     :host .radio {
@@ -171,16 +196,19 @@ export const menuItemStyles: (
         box-sizing: border-box;
         outline: none;
     }
+
     :host .checkbox-indicator,
     :host .radio-indicator,
     slot[name="checkbox-indicator"],
     slot[name="radio-indicator"] {
         display: none;
     }
+
     ::slotted([slot="end"]:not(svg)) {
         margin-inline-end: 10px;
         color: ${neutralForegroundHint};
     }
+
     :host([aria-checked="true"]) .checkbox-indicator,
     :host([aria-checked="true"]) slot[name="checkbox-indicator"],
     :host([aria-checked="true"]) .radio-indicator,
@@ -197,6 +225,7 @@ export const menuItemStyles: (
                     color: ${SystemColors.ButtonText};
                     fill: ${SystemColors.ButtonText};
                 }
+
                 :host(:not([disabled]):hover),
                 :host(:not([disabled]):hover) ::slotted([slot='end']:not(svg)),
                 :host(:not([disabled]):${focusVisible}) ::slotted([slot='end']:not(svg)),
@@ -209,6 +238,7 @@ export const menuItemStyles: (
                     color: ${SystemColors.HighlightText};
                     fill: currentcolor;
                 }
+
                 :host([disabled]),
                 :host([disabled]:hover),
                 :host([disabled]:hover) .start,
@@ -219,9 +249,11 @@ export const menuItemStyles: (
                     fill: currentcolor;
                     opacity: 1;
                 }
+
                 :host([disabled]:${focusVisible}) {
                     border-color: ${SystemColors.GrayText};
                 }
+
                 :host(:not([disabled]):hover) .checkbox,
                 :host(:not([disabled]):hover) .radio,
                 :host(:not([disabled]):${focusVisible}) .checkbox,
@@ -229,9 +261,11 @@ export const menuItemStyles: (
                     background: ${SystemColors.Field};
                     border-color: ${SystemColors.HighlightText};
                 }
+
                 :host([aria-checked="true"]) slot[name="radio-indicator"] {
                     border-radius: 999px;
                 }
+
                 :host([aria-checked="true"]) slot[name="checkbox-indicator"],
                 :host([aria-checked="true"]) slot[name="radio-indicator"] {
                     background: ${SystemColors.Field};
