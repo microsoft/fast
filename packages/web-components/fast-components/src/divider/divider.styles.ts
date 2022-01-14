4da904e15ebeebd1d290eb16d7b600fe04ded01a
eljefe223 <jes@microsoft.com>
2022-01-14 10:40:24 -0800

    addressed PR comment

--- packages/web-components/fast-components/src/divider/divider.styles.ts
+++ packages/web-components/fast-components/src/divider/divider.styles.ts
@@ -22,14 +22,13 @@ export const dividerStyles: (
             box-sizing: content-box;
             height: 0;
             margin: calc(${designUnit} * 1px) 0;
-            border: none;
             border-top: calc(${strokeWidth} * 1px) solid ${neutralStrokeDividerRest};
+            border-left: none;
         }
 
         :host([orientation="vertical"]) {
             height: 100%;
             margin: 0 calc(${designUnit} * 1px);
-            border: none;
             border-top: none;
             border-left: calc(${strokeWidth} * 1px) solid ${neutralStrokeDividerRest};
         }
