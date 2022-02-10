59baafa0f76bfcebff0aa038eb702a8425b7811e
nicholasrice <nicholasrice@users.noreply.github.com>
2022-02-10 15:53:49 -0800

    project files and starting to set up test infrastructure

---
+++ packages/web-components/fast-ssr/src/template-renderer/test/template-renderer.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect, ElementHandle } from '@playwright/test';
+/* eslint-disable */
+import express, { Request, Response } from "express";
+import { Readable } from "stream";
+
+
+test(`should render the 'fast-main' custom element with a shadow root`, async ({ page }) => {
+	expect(1).toBe(1);
+});
