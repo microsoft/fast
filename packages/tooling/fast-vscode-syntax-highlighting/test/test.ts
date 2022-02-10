28d281227c3a9fdb369888ce13c777cfdc1af3b3
John Kreitlow <john.kreitlow@microsoft.com>
2022-02-10 14:44:15 -0800

    prevent husky from formatting tests

--- packages/tooling/fast-vscode-syntax-highlighting/test/test.ts
+++ packages/tooling/fast-vscode-syntax-highlighting/test/test.ts
@@ -162,20 +162,27 @@ css`
 /* Comment-style */
 
 /* html */ `<div></div>`;
+/*             html                */                   `<div></div>`;
+/*html*/`<div></div>`;
 /* html<TBase, any> */ `<div></div>`;
 /*           html            */ `<div></div>`;
 
-/* css */ ``;
 /* css */ `.css {}`;
+/*css*/`.css {}`;
+/*              css          */               `.css {}`;
 /* css */ `.${exp} {}`;
-/*              css          */ ``;
 
-// should not match
+
+/* should not match */
 let b = `${something}.css`;
 b = `something else`;
+
+/* html */ 'asdf' + `<template></template>`;
 
 /* html */
 `<template></template>`;
 
 /* css */
 `.class {}`;
+
+/* css */ 'asdf' + `.class {}`;
