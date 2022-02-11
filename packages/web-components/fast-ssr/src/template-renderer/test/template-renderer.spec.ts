a4576ad0fe88276f899039dd34c454ce62f70e77
nicholasrice <nicholasrice@users.noreply.github.com>
2022-02-11 12:05:02 -0800

    incorporating ts project references and getting tests working

--- packages/web-components/fast-ssr/src/template-renderer/test/template-renderer.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect, ElementHandle } from '@playwright/test';
-/* eslint-disable */
-import express, { Request, Response } from "express";
-import { Readable } from "stream";
-
-
-test(`should render the 'fast-main' custom element with a shadow root`, async ({ page }) => {
-	expect(1).toBe(1);
-});
---
+++ packages/web-components/fast-ssr/test/playwright.config.ts
@@ -0,0 +1,11 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from "@playwright/test";
+const config: PlaywrightTestConfig = {
+    webServer: {
+        command: "npm run test-server",
+        port: 8080,
+        timeout: 120 * 1000,
+        reuseExistingServer: false,
+    },
+};
+export default config;
