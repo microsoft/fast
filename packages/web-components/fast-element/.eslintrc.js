3c254518e3f271aaffde622af7b127b2ed21074b
nicholasrice <nicholasrice@users.noreply.github.com>
2022-02-14 14:22:38 -0800

    add lint rule to enforce file extensions in imports

--- packages/web-components/fast-element/.eslintrc.js
+++ packages/web-components/fast-element/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ["@microsoft/eslint-config-fast-dna", "prettier"],
     rules: {
+        "import/extensions": ["error", "always"],
         "max-classes-per-file": "off",
         "no-case-declarations": "off",
         "@typescript-eslint/ban-types": [
