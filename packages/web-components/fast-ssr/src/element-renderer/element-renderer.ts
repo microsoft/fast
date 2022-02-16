14d77c40ff7493fd2e43d8d183821c2f55fe1bfd
nicholasrice <nicholasrice@users.noreply.github.com>
2022-02-16 15:02:16 -0800

    adding code comments

--- packages/web-components/fast-ssr/src/element-renderer/element-renderer.ts
+++ packages/web-components/fast-ssr/src/element-renderer/element-renderer.ts
@@ -2,15 +2,30 @@ import { ElementRenderer, RenderInfo } from "@lit-labs/ssr";
 import { FASTElement } from "@microsoft/fast-element";
 
 export class FASTElementRenderer extends ElementRenderer {
+    /**
+     * The element instance represented by the {@link FASTElementRenderer}.
+     */
     public readonly element!: FASTElement;
-    static matchesClass(ctor: typeof HTMLElement): boolean {
+
+    /**
+     * Tests a constructor to determine if it should be managed by a {@link FASTElementRenderer}.
+     * @param ctor - The constructor to test.
+     */
+    public static matchesClass(ctor: typeof HTMLElement): boolean {
         return ctor.prototype instanceof FASTElement;
     }
 
-    connectedCallback(): void {
+    /**
+     * Indicate to the {@link FASTElementRenderer} that the instance should execute DOM connection behavior.
+     */
+    public connectedCallback(): void {
         this.element.connectedCallback();
     }
 
+    /**
+     * Constructs a new {@link FASTElementRenderer}.
+     * @param tagName - the tag-name of the element to create.
+     */
     constructor(tagName: string) {
         super(tagName);
 
@@ -20,25 +35,35 @@ export class FASTElementRenderer extends ElementRenderer {
             this.element = new ctor();
         } else {
             throw new Error(
-                `could not find a constructor for a custom element with a tag name '${tagName}'.`
+                `FASTElementRenderer was unable not find a constructor for a custom element with a tag name '${tagName}'.`
             );
         }
     }
 
     /**
-     * Renders the component to light DOM instead of shadow DOM.
-     * @param renderInfo - information about the current rendering context
+     * Renders the component internals to light DOM instead of shadow DOM.
+     * @param renderInfo - information about the current rendering context.
      */
     *renderLight(renderInfo: RenderInfo): IterableIterator<string> {
         // TODO - this will yield out the element's template using the template renderer, skipping any shadow-DOM specific emission.
         yield "";
     }
 
+    /**
+     * Render the component internals to shadow DOM.
+     * @param renderInfo - information about the current rendering context.
+     */
     *renderShadow(renderInfo: RenderInfo): IterableIterator<string> {
         // TODO - this will yield out the element's template using the template renderer
         yield "";
     }
 
+    /**
+     * Indicate to the {@link FASTElementRenderer} that an attribute has been changed.
+     * @param name - The name of the changed attribute
+     * @param old - The old attribute value
+     * @param value - The new attribute value
+     */
     attributeChangedCallback(
         name: string,
         old: string | null,
