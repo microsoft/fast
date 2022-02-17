a88624a1c8379d8a1cc2f9fc3e2bffa6ff2535c4
nicholasrice <nicholasrice@users.noreply.github.com>
2022-02-17 15:57:17 -0800

    generate AST from ViewTemplate

--- packages/web-components/fast-ssr/src/element-renderer/template-parser.ts
+++ packages/web-components/fast-ssr/src/element-renderer/template-parser.ts
@@ -1,4 +1,5 @@
 import { ViewTemplate } from "@microsoft/fast-element";
+import { parseFragment } from "parse5";
 import { Op } from "./op-codes.js";
 
 const opCache: Map<ViewTemplate, Op[]> = new Map();
@@ -13,13 +14,20 @@ export function parseTemplateToOpCodes(template: ViewTemplate): Op[] {
         return ops;
     }
 
-    ops = [];
     const { html } = template;
 
     if (typeof html !== "string") {
         throw new Error(
             "@microsoft/fast-ssr does not support rendering a ViewTemplate with an HTMLTemplateElement html source."
         );
+    }
+
+    ops = [];
+    const ast = parseFragment(html, { sourceCodeLocationInfo: true });
+
+    if (!("nodeName" in ast)) {
+        // I'm not sure when exactly this is encountered but the type system seems to say it's possible.
+        throw new Error(`Error parsing template:\n${template}`);
     }
 
     return ops;
