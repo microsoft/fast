ce65f01ee1b65994a415a59fa87919227ca97d70
Kham Udom <37851220+khamudom@users.noreply.github.com>
2022-02-18 11:17:22 -0800

    chore: visual refresh on radio component (#5402)
    * update radio visual
    
    * update to add back accent color on checked state and use outline for focus state
    
    * update focus color
    
    * formatting to use double quotes and update comments
    
    * spacing selectors
    
    * revert to use disabled and readonly attribute and add readonly example
    
    * replaced check class to use aria-checked set to true

--- packages/web-components/fast-components/src/radio-group/radio-group.styles.ts
+++ packages/web-components/fast-components/src/radio-group/radio-group.styles.ts
@@ -1,6 +1,6 @@
 import { css, ElementStyles } from "@microsoft/fast-element";
 import { display, FoundationElementTemplate } from "@microsoft/fast-foundation";
-import { designUnit } from "../design-tokens.js";
+import { designUnit, neutralForegroundRest } from "../design-tokens.js";
 
 /**
  * Styles for Radio Group
@@ -14,14 +14,18 @@ export const radioGroupStyles: FoundationElementTemplate<ElementStyles> = (
         align-items: flex-start;
         margin: calc(${designUnit} * 1px) 0;
         flex-direction: column;
+        color: ${neutralForegroundRest};
     }
+
     .positioning-region {
         display: flex;
         flex-wrap: wrap;
     }
+
     :host([orientation="vertical"]) .positioning-region {
         flex-direction: column;
     }
+
     :host([orientation="horizontal"]) .positioning-region {
         flex-direction: row;
     }
