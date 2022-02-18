03d9c19337ac215ac67f212421ea343fcb00ac38
nicholasrice <nicholasrice@users.noreply.github.com>
2022-02-17 16:20:59 -0800

    implement AST traverser

--- packages/web-components/fast-ssr/src/element-renderer/template-parser.ts
+++ packages/web-components/fast-ssr/src/element-renderer/template-parser.ts
@@ -1,8 +1,38 @@
+/**
+ * This code is largely a fork of lit's rendering implementation: https://github.com/lit/lit/blob/main/packages/labs/ssr/src/lib/render-lit-html.ts
+ * with changes as necessary to render FAST components. A big thank you to those who contributed to lit's code above.
+ */
 import { ViewTemplate } from "@microsoft/fast-element";
-import { parseFragment } from "parse5";
+import { DefaultTreeNode, DefaultTreeParentNode, parseFragment } from "parse5";
 import { Op } from "./op-codes.js";
 
 const opCache: Map<ViewTemplate, Op[]> = new Map();
+
+interface Visitor {
+    visit?: (node: DefaultTreeNode) => void;
+    leave?: (node: DefaultTreeNode) => void;
+}
+
+/**
+ * Traverses a tree of nodes depth-first, invoking callbacks from visitor for each node as it goes.
+ * @param node - the node to traverse
+ * @param visitor - callbacks to be invoked during node traversal
+ */
+function traverse(node: DefaultTreeNode | DefaultTreeParentNode, visitor: Visitor) {
+    if (visitor.visit) {
+        visitor.visit(node);
+    }
+    if ("childNodes" in node) {
+        const { childNodes } = node;
+        for (const child of childNodes) {
+            traverse(child, visitor);
+        }
+    }
+
+    if (visitor.leave) {
+        visitor.leave(node);
+    }
+}
 
 /**
  * Parses a template into a set of operation instructions
