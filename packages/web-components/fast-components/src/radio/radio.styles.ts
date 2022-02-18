3fa967d4a8c77d5d30db72e6a0923115022486e8
Kham Udom <37851220+khamudom@users.noreply.github.com>
2022-02-18 11:17:22 -0800

    chore: visual refresh on radio component (#5402)
    * update radio visual
    
    * update to add back accent color on checked state and use outline for focus state
    
    * update focus color
    
    * formatting to use double quotes and update comments
    
    * spacing selectors
    
    * revert to use disabled and readonly attribute and add readonly example
    
    * replaced check class to use aria-checked set to true

--- packages/web-components/fast-components/src/radio/radio.styles.ts
+++ packages/web-components/fast-components/src/radio/radio.styles.ts
@@ -1,4 +1,5 @@
 import { css, ElementStyles } from "@microsoft/fast-element";
+import { SystemColors } from "@microsoft/fast-web-utilities";
 import {
     disabledCursor,
     display,
@@ -7,31 +8,30 @@ import {
     FoundationElementTemplate,
     RadioOptions,
 } from "@microsoft/fast-foundation";
-import { SystemColors } from "@microsoft/fast-web-utilities";
+import { heightNumber } from "../styles";
 import {
     accentFillActive,
+    accentFillFocus,
     accentFillHover,
     accentFillRest,
     bodyFont,
     designUnit,
     disabledOpacity,
-    fillColor,
     focusStrokeOuter,
-    foregroundOnAccentActive,
-    foregroundOnAccentHover,
+    focusStrokeWidth,
     foregroundOnAccentRest,
     neutralFillInputActive,
-    neutralFillInputHover,
     neutralFillInputRest,
     neutralForegroundRest,
-    neutralStrokeActive,
-    neutralStrokeHover,
-    neutralStrokeRest,
+    strokeControlStrongActive,
+    strokeControlStrongFocus,
+    strokeControlStrongHover,
+    strokeControlStrongRest,
     strokeWidth,
     typeRampBaseFontSize,
     typeRampBaseLineHeight,
 } from "../design-tokens";
-import { heightNumber } from "../styles/index";
+
 /**
  * Styles for Radio
  * @public
@@ -41,175 +41,175 @@ export const radioStyles: FoundationElementTemplate<ElementStyles, RadioOptions>
     definition
 ) =>
     css`
-    ${display("inline-flex")} :host {
-        --input-size: calc((${heightNumber} / 2) + ${designUnit});
-        align-items: center;
-        outline: none;
-        margin: calc(${designUnit} * 1px) 0;
-        /* Chromium likes to select label text or the default slot when
-            the radio is clicked. Maybe there is a better solution here? */
-        user-select: none;
-        position: relative;
-        flex-direction: row;
-        transition: all 0.2s ease-in-out;
-    }
+        ${display("inline-flex")} :host {
+            --input-size: calc((${heightNumber} / 2) + (${strokeWidth} * 2));
+            align-items: center;
+            outline: none;
+            margin: calc(${designUnit} * 1px) 0;
+            /*
+            * Chromium likes to select label text or the default slot when
+            * the radio button is clicked. Maybe there is a better solution here?
+            */
+            user-select: none;
+            position: relative;
+            flex-direction: row;
+            transition: all 0.2s ease-in-out;
+        }
 
-    .control {
-        position: relative;
-        width: calc((${heightNumber} / 2 + ${designUnit}) * 1px);
-        height: calc((${heightNumber} / 2 + ${designUnit}) * 1px);
-        box-sizing: border-box;
-        border-radius: 999px;
-        border: calc(${strokeWidth} * 1px) solid ${neutralStrokeRest};
-        background: ${neutralFillInputRest};
-        outline: none;
-        cursor: pointer;
-    }
+        .control {
+            position: relative;
+            width: calc(var(--input-size) * 1px);
+            height: calc(var(--input-size) * 1px);
+            box-sizing: border-box;
+            border-radius: 50%;
+            border: calc(${strokeWidth} * 1px) solid ${strokeControlStrongRest};
+            background: ${neutralFillInputRest};
+            outline: none;
+            cursor: pointer;
+        }
 
-    .label {
-        font-family: ${bodyFont};
-        color: ${neutralForegroundRest};
-        /* Need to discuss with Brian how HorizontalSpacingNumber can work.
-            https://github.com/microsoft/fast/issues/2766 */
-        padding-inline-start: calc(${designUnit} * 2px + 2px);
-        margin-inline-end: calc(${designUnit} * 2px + 2px);
-        cursor: pointer;
-        font-size: ${typeRampBaseFontSize};
-        line-height: ${typeRampBaseLineHeight};
-    }
+        :host(:enabled:hover) .control {
+            border-color: ${strokeControlStrongHover};
+        }
 
-    .label__hidden {
-        display: none;
-        visibility: hidden;
-    }
+        :host(:enabled:active) .control {
+            background: ${neutralFillInputActive};
+            border-color: ${strokeControlStrongActive};
+        }
 
-    .control, .checked-indicator {
-        flex-shrink: 0;
-    }
+        :host(:${focusVisible}) .control {
+            border-color: ${strokeControlStrongFocus};
+            outline: calc(${focusStrokeWidth} * 1px) solid ${focusStrokeOuter};
+            outline-offset: calc(${strokeWidth} * 1px);
+        }
 
-    .checked-indicator {
-        position: absolute;
-        top: 5px;
-        left: 5px;
-        right: 5px;
-        bottom: 5px;
-        border-radius: 999px;
-        display: inline-block;
-        background: ${foregroundOnAccentRest};
-        fill: ${foregroundOnAccentRest};
-        opacity: 0;
-        pointer-events: none;
-    }
+        :host([aria-checked="true"]) .control {
+            background: ${accentFillRest};
+            border-color: ${accentFillRest};
+        }
 
-    :host(:not([disabled])) .control:hover{
-        background: ${neutralFillInputHover};
-        border-color: ${neutralStrokeHover};
-    }
+        :host([aria-checked="true"]:enabled:hover) .control {
+            background: ${accentFillHover};
+            border-color: ${accentFillHover};
+        }
 
-    :host(:not([disabled])) .control:active {
-        background: ${neutralFillInputActive};
-        border-color: ${neutralStrokeActive};
-    }
+        :host([aria-checked="true"]:enabled:active) .control {
+            background: ${accentFillActive};
+            border-color: ${accentFillActive};
+        }
 
-    :host(:${focusVisible}) .control {
-        box-shadow: 0 0 0 2px ${fillColor}, 0 0 0 4px ${focusStrokeOuter};
-    }
+        :host([aria-checked="true"]:enabled:${focusVisible}) .control {
+            background: ${accentFillFocus};
+            border-color: ${accentFillFocus};
+        }
 
-    :host([aria-checked="true"]) .control {
-        background: ${accentFillRest};
-        border: calc(${strokeWidth} * 1px) solid ${accentFillRest};
-    }
+        .label__hidden {
+            display: none;
+            visibility: hidden;
+        }
 
-    :host([aria-checked="true"]:not([disabled])) .control:hover {
-        background: ${accentFillHover};
-        border: calc(${strokeWidth} * 1px) solid ${accentFillHover};
-    }
+        .label {
+            font-family: ${bodyFont};
+            color: ${neutralForegroundRest};
+            /* Need to discuss with Brian how HorizontalSpacingNumber can work. https://github.com/microsoft/fast/issues/2766 */
+            padding-inline-start: calc(${designUnit} * 2px + 2px);
+            margin-inline-end: calc(${designUnit} * 2px + 2px);
+            cursor: pointer;
+            font-size: ${typeRampBaseFontSize};
+            line-height: ${typeRampBaseLineHeight};
+        }
 
-    :host([aria-checked="true"]:not([disabled])) .control:hover .checked-indicator {
-        background: ${foregroundOnAccentHover};
-        fill: ${foregroundOnAccentHover};
-    }
+        slot[name="checked-indicator"] {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-shrink: 0;
+            width: 100%;
+            height: 100%;
+            fill: ${foregroundOnAccentRest};
+            opacity: 0;
+            pointer-events: none;
+        }
 
-    :host([aria-checked="true"]:not([disabled])) .control:active {
-        background: ${accentFillActive};
-        border: calc(${strokeWidth} * 1px) solid ${accentFillActive};
-    }
+        :host(:enabled:active) slot[name="checked-indicator"] {
+            opacity: 1;
+        }
 
-    :host([aria-checked="true"]:not([disabled])) .control:active .checked-indicator {
-        background: ${foregroundOnAccentActive};
-        fill: ${foregroundOnAccentActive};
-    }
+        :host([disabled]) .label,
+        :host([readonly]) .label,
+        :host([readonly]) .control,
+        :host([disabled]) .control {
+            cursor: ${disabledCursor};
+        }
 
-    :host([aria-checked="true"]:${focusVisible}:not([disabled])) .control {
-        box-shadow: 0 0 0 2px ${fillColor}, 0 0 0 4px ${focusStrokeOuter};
-    }
+        :host([aria-checked="true"]) slot[name="checked-indicator"] {
+            opacity: 1;
+        }
 
-    :host([disabled]) .label,
-    :host([readonly]) .label,
-    :host([readonly]) .control,
-    :host([disabled]) .control {
-        cursor: ${disabledCursor};
-    }
-
-    :host([aria-checked="true"]) .checked-indicator {
-        opacity: 1;
-    }
-
-    :host([disabled]) {
-        opacity: ${disabledOpacity};
-    }
-`.withBehaviors(
+        :host([disabled]) {
+            opacity: ${disabledOpacity};
+        }
+  `.withBehaviors(
         forcedColorsStylesheetBehavior(
             css`
-            .control,
-            :host([aria-checked="true"]:not([disabled])) .control {
-                forced-color-adjust: none;
-                border-color: ${SystemColors.FieldText};
-                background: ${SystemColors.Field};
-            }
-            :host(:not([disabled])) .control:hover {
-                border-color: ${SystemColors.Highlight};
-                background: ${SystemColors.Field};
-            }
-            :host([aria-checked="true"]:not([disabled])) .control:hover,
-            :host([aria-checked="true"]:not([disabled])) .control:active {
-                border-color: ${SystemColors.Highlight};
-                background: ${SystemColors.Highlight};
-            }
-            :host([aria-checked="true"]) .checked-indicator {
-                background: ${SystemColors.Highlight};
-                fill: ${SystemColors.Highlight};
-            }
-            :host([aria-checked="true"]:not([disabled])) .control:hover .checked-indicator,
-            :host([aria-checked="true"]:not([disabled])) .control:active .checked-indicator {
-                background: ${SystemColors.HighlightText};
-                fill: ${SystemColors.HighlightText};
-            }
-            :host(:${focusVisible}) .control {
-                border-color: ${SystemColors.Highlight};
-                box-shadow: 0 0 0 2px ${SystemColors.Field}, 0 0 0 4px ${SystemColors.FieldText};
-            }
-            :host([aria-checked="true"]:${focusVisible}:not([disabled])) .control {
-                border-color: ${SystemColors.Highlight};
-                box-shadow: 0 0 0 2px ${SystemColors.Field}, 0 0 0 4px ${SystemColors.FieldText};
-            }
-            :host([disabled]) {
-                forced-color-adjust: none;
-                opacity: 1;
-            }
-            :host([disabled]) .label {
-                color: ${SystemColors.GrayText};
-            }
-            :host([disabled]) .control,
-            :host([aria-checked="true"][disabled]) .control:hover, .control:active {
-                background: ${SystemColors.Field};
-                border-color: ${SystemColors.GrayText};
-            }
-            :host([disabled]) .checked-indicator,
-            :host([aria-checked="true"][disabled]) .control:hover .checked-indicator {
-                fill: ${SystemColors.GrayText};
-                background: ${SystemColors.GrayText};
-            }
-        `
+                .control {
+                    border-color: ${SystemColors.FieldText};
+                    background: ${SystemColors.Field};
+                }
+
+                :host(:enabled:hover) .control,
+                .control:active {
+                    border-color: ${SystemColors.Highlight};
+                    background: ${SystemColors.Field};
+                }
+
+                :host([aria-checked="true"]) .control {
+                    background: ${SystemColors.Highlight};
+                    border-color: ${SystemColors.Highlight};
+                }
+
+                :host([aria-checked="true"]:enabled:hover) .control,
+                .control:active {
+                    border-color: ${SystemColors.Highlight};
+                    background: ${SystemColors.HighlightText};
+                }
+
+                :host(:${focusVisible}) .control,
+                :host([aria-checked="true"]:enabled:${focusVisible}) .control {
+                    background: ${SystemColors.Field};
+                    border-color: ${SystemColors.FieldText};
+                    outline: calc(${focusStrokeWidth} * 1px) solid ${SystemColors.FieldText};
+                    outline-offset: calc(${strokeWidth} * 1px);
+                }
+
+                :host([aria-checked="true"]) slot[name="checked-indicator"] {
+                    fill: ${SystemColors.HighlightText};
+                }
+
+                :host([aria-checked="true"]:enabled:hover) slot[name="checked-indicator"],
+                :host([aria-checked="true"]:enabled:${focusVisible}) slot[name="checked-indicator"] {
+                    fill: ${SystemColors.Highlight};
+                }
+
+                :host([disabled]) {
+                    opacity: 1;
+                }
+
+                :host([disabled]) .label {
+                    color: ${SystemColors.GrayText};
+                }
+
+                :host([disabled]) .control,
+                :host([aria-checked="true"][disabled]:hover) .control,
+                .control:active {
+                    background: ${SystemColors.Field};
+                    border-color: ${SystemColors.GrayText};
+                }
+
+                :host([disabled]) slot[name="checked-indicator"],
+                :host([aria-checked="true"][disabled]) .control:hover slot[name="checked-indicator"] {
+                    fill: ${SystemColors.GrayText};
+                }
+            `
         )
     );
