496e13e067fe8a0f846d68d15318d3e625d599c7
nicholasrice <nicholasrice@users.noreply.github.com>
2022-02-22 15:47:43 -0800

    writing a few test fixtures for pure HTML templates

--- packages/web-components/fast-ssr/src/element-renderer/template-parser.spec.ts
+++ packages/web-components/fast-ssr/src/element-renderer/template-parser.spec.ts
@@ -2,7 +2,8 @@
 import "@lit-labs/ssr/lib/install-global-dom-shim.js";
 import { test, expect } from "@playwright/test";
 import { parseTemplateToOpCodes} from "./template-parser.js";
-import { ViewTemplate } from "@microsoft/fast-element"
+import { ViewTemplate, html } from "@microsoft/fast-element"
+import { Op, OpType } from "./op-codes.js";
 
 test.describe("parseTemplateToOpCodes", () => {
 	test("should throw when invoked with a ViewTemplate with a HTMLTemplateElement template", () => {
@@ -15,4 +16,15 @@ test.describe("parseTemplateToOpCodes", () => {
 			parseTemplateToOpCodes(new ViewTemplate("", []));
 		}).not.toThrow();
 	});
+
+	const fixtures: {input: ViewTemplate, result: Op[]}[] = [
+		{input: html`<p>Hello world</p>`, result: [{type: OpType.text, value: "<p>Hello world</p>"}]},
+		{input: html`<!DOCTYPE html><html><head></head><body></body></html>`, result: [{type: OpType.text, value: "<!DOCTYPE html><html><head></head><body></body></html>"}]}
+	];
+
+	fixtures.forEach(({ input, result}, index) => {
+		test(`should parse template to op codes for fixture ${index}`, () => {
+			expect(parseTemplateToOpCodes(input)).toEqual(result)
+		})
+	})
 })
