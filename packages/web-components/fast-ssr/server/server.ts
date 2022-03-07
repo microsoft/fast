eb9e16064b5e32e25ce01e114d120c13cb903e0e
William Wagner <44823142+williamw2@users.noreply.github.com>
2022-03-07 10:08:34 -0800

    Add fast command buffer (#5708)

--- packages/web-components/fast-ssr/server/server.ts
+++ packages/web-components/fast-ssr/server/server.ts
@@ -23,50 +23,55 @@ function handleRequest(req: Request, res: Response) {
     });
 }
 
-function handleStyleRequest(req: Request, res: Response) {
-    res.set("Content-Type", "text/html");
-    fs.readFile(
-        path.resolve(__dirname, "./src/fast-style/index.fixture.html"),
-        { encoding: "utf8" },
-        (err, data) => {
-            const stream = (Readable as any).from(data);
-            stream.on("readable", function (this: any) {
-                while ((data = this.read())) {
-                    res.write(data);
-                }
-            });
-            stream.on("close", () => res.end());
-            stream.on("error", (e: Error) => {
-                console.error(e);
-                process.exit(1);
-            });
-        }
-    );
-}
-
-function handleStyleScriptRequest(req: Request, res: Response) {
-    res.set("Content-Type", "application/javascript");
-    fs.readFile(
-        path.resolve(__dirname, "./dist/esm/fast-style/index.js"),
-        { encoding: "utf8" },
-        (err, data) => {
-            const stream = (Readable as any).from(data);
-            stream.on("readable", function (this: any) {
-                while ((data = this.read())) {
-                    res.write(data);
-                }
-            });
-            stream.on("close", () => res.end());
-            stream.on("error", (e: Error) => {
-                console.error(e);
-                process.exit(1);
-            });
-        }
-    );
+function handlePathRequest(
+    mapPath: string,
+    contentType: string,
+    req: Request,
+    res: Response
+) {
+    res.set("Content-Type", contentType);
+    fs.readFile(path.resolve(__dirname, mapPath), { encoding: "utf8" }, (err, data) => {
+        const stream = (Readable as any).from(data);
+        stream.on("readable", function (this: any) {
+            while ((data = this.read())) {
+                res.write(data);
+            }
+        });
+        stream.on("close", () => res.end());
+        stream.on("error", (e: Error) => {
+            console.error(e);
+            process.exit(1);
+        });
+    });
 }
 
 const app = express();
 app.get("/", handleRequest);
-app.get("/fast-style", handleStyleRequest);
-app.get("/fast-style.js", handleStyleScriptRequest);
+app.get("/fast-style", (req: Request, res: Response) =>
+    handlePathRequest("./src/fast-style/index.fixture.html", "text/html", req, res)
+);
+app.get("/fast-style.js", (req: Request, res: Response) =>
+    handlePathRequest(
+        "./dist/esm/fast-style/index.js",
+        "application/javascript",
+        req,
+        res
+    )
+);
+app.get("/fast-command-buffer", (req: Request, res: Response) =>
+    handlePathRequest(
+        "./src/fast-command-buffer/index.fixture.html",
+        "text/html",
+        req,
+        res
+    )
+);
+app.get("/fast-command-buffer.js", (req: Request, res: Response) =>
+    handlePathRequest(
+        "./dist/esm/fast-command-buffer/index.js",
+        "application/javascript",
+        req,
+        res
+    )
+);
 app.listen(PORT);
