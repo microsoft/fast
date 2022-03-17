7942c2838a31afbeb013fc3ce2889e018922e9dd
Jane Chu <7559015+janechu@users.noreply.github.com>
2022-03-16 19:25:51 -0700

    Add an eslint:fix script and fix the spec file import order

--- packages/tooling/cfp-template/template/src/components/welcome/welcome.pw.spec.ts
+++ packages/tooling/cfp-template/template/src/components/welcome/welcome.pw.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe("fast-welcome", () => {
     test.beforeEach(async ({ page }) => {
