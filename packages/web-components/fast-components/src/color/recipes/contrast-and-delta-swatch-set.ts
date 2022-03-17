68270284496a9a41649105277f1ed1a46f5b3a42
Brian Heston <47367562+bheston@users.noreply.github.com>
2022-03-17 13:55:58 -0700

    Removed "ByLuminance" recipes

--- packages/web-components/fast-components/src/color/recipes/contrast-and-delta-swatch-set.ts
+++ packages/web-components/fast-components/src/color/recipes/contrast-and-delta-swatch-set.ts
@@ -2,7 +2,6 @@ import { Palette } from "../palette";
 import { InteractiveSwatchSet } from "../recipe";
 import { Swatch } from "../swatch";
 import { directionByIsDark } from "../utilities/direction-by-is-dark";
-import { isDark } from "../utilities/is-dark";
 
 /**
  * @internal
@@ -30,48 +29,4 @@ export function contrastAndDeltaSwatchSet(
         active: palette.get(baseIndex + direction * activeDelta),
         focus: palette.get(baseIndex + direction * focusDelta),
     };
-}
-
-/**
- * @internal
- */
-export function contrastAndDeltaSwatchSetByLuminance(
-    palette: Palette,
-    reference: Swatch,
-    lightBaseContrast: number,
-    lightRestDelta: number,
-    lightHoverDelta: number,
-    lightActiveDelta: number,
-    lightFocusDelta: number,
-    lightDirection: -1 | 1 | undefined | null = undefined,
-    darkBaseContrast: number,
-    darkRestDelta: number,
-    darkHoverDelta: number,
-    darkActiveDelta: number,
-    darkFocusDelta: number,
-    darkDirection: -1 | 1 | undefined | null = undefined
-): InteractiveSwatchSet {
-    if (isDark(reference)) {
-        return contrastAndDeltaSwatchSet(
-            palette,
-            reference,
-            darkBaseContrast,
-            darkRestDelta,
-            darkHoverDelta,
-            darkActiveDelta,
-            darkFocusDelta,
-            darkDirection
-        );
-    } else {
-        return contrastAndDeltaSwatchSet(
-            palette,
-            reference,
-            lightBaseContrast,
-            lightRestDelta,
-            lightHoverDelta,
-            lightActiveDelta,
-            lightFocusDelta,
-            lightDirection
-        );
-    }
 }
--- packages/web-components/fast-components/src/color/recipes/delta-swatch-set.ts
+++ packages/web-components/fast-components/src/color/recipes/delta-swatch-set.ts
@@ -2,7 +2,6 @@ import { Palette } from "../palette";
 import { InteractiveSwatchSet } from "../recipe";
 import { Swatch } from "../swatch";
 import { directionByIsDark } from "../utilities/direction-by-is-dark";
-import { isDark } from "../utilities/is-dark";
 
 /**
  * Color algorithm using deltas from the reference color for states.
@@ -37,59 +36,4 @@ export function deltaSwatchSet(
         active: palette.get(referenceIndex + direction * activeDelta),
         focus: palette.get(referenceIndex + direction * focusDelta),
     };
-}
-
-/**
- * Color algorithm using deltas from the reference color for states, allowing different deltas based on a light or dark reference color.
- *
- * @param palette The palette to operate on
- * @param reference The reference color to calculate a color for
- * @param lightRestDelta The rest offset for a light reference
- * @param lightHoverDelta The hover offset for a light reference
- * @param lightActiveDelta The rest offset for a light reference
- * @param lightFocusDelta The hover offset for a light reference
- * @param lightDirection The direction the deltas move on the ramp, default goes darker for light references
- * @param darkRestDelta The rest offset for a dark reference
- * @param darkHoverDelta The hover offset for a dark reference
- * @param darkActiveDelta The rest offset for a dark reference
- * @param darkFocusDelta The hover offset for a dark reference
- * @param darkDirection The direction the deltas move on the ramp, default goes lighter for dark references
- *
- * @internal
- */
-export function deltaSwatchSetByLuminance(
-    palette: Palette,
-    reference: Swatch,
-    lightRestDelta: number,
-    lightHoverDelta: number,
-    lightActiveDelta: number,
-    lightFocusDelta: number,
-    lightDirection: -1 | 1 | undefined | null = undefined,
-    darkRestDelta: number,
-    darkHoverDelta: number,
-    darkActiveDelta: number,
-    darkFocusDelta: number,
-    darkDirection: -1 | 1 | undefined | null = undefined
-): InteractiveSwatchSet {
-    if (isDark(reference)) {
-        return deltaSwatchSet(
-            palette,
-            reference,
-            darkRestDelta,
-            darkHoverDelta,
-            darkActiveDelta,
-            darkFocusDelta,
-            darkDirection
-        );
-    } else {
-        return deltaSwatchSet(
-            palette,
-            reference,
-            lightRestDelta,
-            lightHoverDelta,
-            lightActiveDelta,
-            lightFocusDelta,
-            lightDirection
-        );
-    }
 }
