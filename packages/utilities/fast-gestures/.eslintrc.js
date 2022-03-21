6de6207ea8451a298afae82e5e88c43e98b4228a
Martin Lijanto <mlijanto@microsoft.com>
2022-03-21 13:01:09 -0700

    Resolved rebase conflicts.

--- packages/utilities/fast-gestures/.eslintrc.js
+++ packages/utilities/fast-gestures/.eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     extends: ["@microsoft/eslint-config-fast-dna", "prettier"],
     rules: {
         "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-namespace": "off"
+        "@typescript-eslint/no-namespace": "off",
+        "max-classes-per-file": "off",
+        "no-case-declarations": "off",
     },
 };
