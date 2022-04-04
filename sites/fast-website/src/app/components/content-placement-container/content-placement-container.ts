1abcb29973022e584580d5272e6a4c322083e848
Steph Huynh <steph@huynhicode.dev>
2022-04-03 22:42:00 -0700

    remove unecessary files

--- sites/fast-website/src/app/components/content-placement-container/content-placement-container.ts
+++ sites/fast-website/src/app/components/content-placement-container/content-placement-container.ts
@@ -8,12 +8,10 @@ import {
     frameworkContentPlacementData,
 } from "../../data/framework.data";
 import { FeatureCardData, featureCardData } from "../../data/feature.data";
-import { ProductData, productData } from "../../data/product.data";
 import { DesignSystemData, designSystemData } from "../../data/design-system.data";
 import frameworkTemplate from "./templates/framework.template";
 import featureTemplate from "./templates/feature.template";
 import communityTemplate from "./templates/community.template";
-import productTemplate from "./templates/product.template";
 import designSystemTemplate from "./templates/design-system.template";
 export class ContentPlacementContainer extends FASTElement {
     @attr section: string;
@@ -23,14 +21,12 @@ export class ContentPlacementContainer extends FASTElement {
     communityData: CommunityContentPlacementData[] = communityContentPlacementData.filter(
         x => x.header !== "Medium"
     );
-    productData: ProductData[] = productData;
     designSystemData: DesignSystemData[] = designSystemData;
 
     dataByType = {
         framework: this.frameworkData,
         feature: this.featureData,
         community: this.communityData,
-        product: this.productData,
         designSystem: this.designSystemData,
     };
 
@@ -38,7 +34,6 @@ export class ContentPlacementContainer extends FASTElement {
         framework: frameworkTemplate,
         feature: featureTemplate,
         community: communityTemplate,
-        product: productTemplate,
         designSystem: designSystemTemplate,
     };
 
