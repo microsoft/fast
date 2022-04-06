69f8fbfed8cf70186ba3d2c7b4727b7260005f97
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-04-06 16:38:19 -0700

    fast-element benchmark running

---
+++ packages/web-components/fast-benchmarks-wendy/fast-element/utils.ts
@@ -0,0 +1,79 @@
+declare global {
+    interface Window {
+        usedJSHeapSize: any;
+        gc: any;
+    }
+    interface Performance {
+        memory: any;
+    }
+}
+
+export const measureMemory = () => {
+    if ("gc" in window && "memory" in performance) {
+        // Report results in MBs\
+        window.gc();
+        window.usedJSHeapSize = performance.memory.usedJSHeapSize / 1e6;
+    } else {
+        window.usedJSHeapSize = 0;
+    }
+};
+
+export const _random = (max: number) => {
+    return Math.round(Math.random() * 1000) % max;
+};
+
+export const adjectives = [
+    "pretty",
+    "large",
+    "big",
+    "small",
+    "tall",
+    "short",
+    "long",
+    "handsome",
+    "plain",
+    "quaint",
+    "clean",
+    "elegant",
+    "easy",
+    "angry",
+    "crazy",
+    "helpful",
+    "mushy",
+    "odd",
+    "unsightly",
+    "adorable",
+    "important",
+    "inexpensive",
+    "cheap",
+    "expensive",
+    "fancy",
+];
+export const colours = [
+    "red",
+    "yellow",
+    "blue",
+    "green",
+    "pink",
+    "brown",
+    "purple",
+    "brown",
+    "white",
+    "black",
+    "orange",
+];
+export const nouns = [
+    "table",
+    "chair",
+    "house",
+    "bbq",
+    "desk",
+    "car",
+    "pony",
+    "cookie",
+    "sandwich",
+    "burger",
+    "pizza",
+    "mouse",
+    "keyboard",
+];
