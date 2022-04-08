8cfc73be37a079fc2f3b12614a1eac68ddb01827
John Kreitlow <john.kreitlow@microsoft.com>
2022-04-08 13:24:05 -0700

    add storybook helpers module

---
+++ packages/web-components/fast-components/src/storybook-helpers.ts
@@ -0,0 +1,19 @@
+import { ViewTemplate } from "@microsoft/fast-element";
+import type { ComponentAnnotations } from "@storybook/csf";
+import type { Args } from "@storybook/addons";
+
+export type renderComponent = (template: ViewTemplate) => ComponentAnnotations<any, Args>;
+
+/**
+ * A helper that returns a function to bind a Storybook story to a ViewTemplate.
+ *
+ * @param template - The ViewTemplate to render
+ * @returns - a function to bind a Storybook story
+ */
+export const renderComponent = (template: ViewTemplate) => {
+    return function (args: Args) {
+        const storyFragment = new DocumentFragment();
+        template.render(args, storyFragment);
+        return storyFragment.firstElementChild;
+    };
+};
