882ee0f8e491d1294ccb5801c4ce56beaf92f38a
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2022-04-18 08:22:34 -0700

    update to use error() not log() for better output attribution

--- sites/site-utilities/statics/server/server.js
+++ sites/site-utilities/statics/server/server.js
@@ -48,11 +48,11 @@ app.use(fallback("index.html", { root: publicDir }));
 // Manage search engine crawlers if staging add robots.txt, otherwise delete
 if (process.env.WEBSITE_HOSTNAME.indexOf("-stage") > -1) {
     fs.writeFile("robots.txt", "User-agent: *\r\nDisallow: /", function (err) {
-        if (err) console.log(err);
+        if (err) console.error(err);
     });
 } else {
     fs.unlink("robots.txt", function (err) {
-        if (err) console.log(err);
+        if (err) console.error(err);
     });
 }
 
