7cc35613ec2914f77e13afbfbd445a83be69b02d
Steph <35910788+huynhicode@users.noreply.github.com>
2022-04-20 13:35:11 -0700

    homepage: updates community section with subsection for design systems (#5824)
    * add icons
    
    * add section for microsoft products that use fast
    
    * add section for design systems built on fast
    
    * wip updates
    
    * styling updates
    
    * update fluent ui logo
    
    * add furious logo and wip for review
    
    * update design system section styling
    
    * remove unecessary files
    
    * fix typo
    
    * styling updates
    
    * revert changes to docusaurus config file
    
    * remove references to product
    
    * fix incorrect class name
    
    * remove unncessary commented out code
    
    Co-authored-by: Aaron Wentzel <16669785+awentzel@users.noreply.github.com>

--- sites/fast-website/src/app/components/content-placement-container/content-placement-container.ts
+++ sites/fast-website/src/app/components/content-placement-container/content-placement-container.ts
@@ -8,10 +8,11 @@ import {
     frameworkContentPlacementData,
 } from "../../data/framework.data";
 import { FeatureCardData, featureCardData } from "../../data/feature.data";
+import { DesignSystemData, designSystemData } from "../../data/design-system.data";
 import frameworkTemplate from "./templates/framework.template";
 import featureTemplate from "./templates/feature.template";
 import communityTemplate from "./templates/community.template";
-
+import designSystemTemplate from "./templates/design-system.template";
 export class ContentPlacementContainer extends FASTElement {
     @attr section: string;
 
@@ -20,17 +21,20 @@ export class ContentPlacementContainer extends FASTElement {
     communityData: CommunityContentPlacementData[] = communityContentPlacementData.filter(
         x => x.header !== "Medium"
     );
+    designSystemData: DesignSystemData[] = designSystemData;
 
     dataByType = {
         framework: this.frameworkData,
         feature: this.featureData,
         community: this.communityData,
+        designSystem: this.designSystemData,
     };
 
     templateByType = {
         framework: frameworkTemplate,
         feature: featureTemplate,
         community: communityTemplate,
+        designSystem: designSystemTemplate,
     };
 
     selectTemplate() {
