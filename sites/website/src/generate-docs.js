617561d433ef4c4878547f5456f116a08ffe0886
Steph <35910788+huynhicode@users.noreply.github.com>
2022-04-21 07:33:58 -0700

    docs: fix unresolved links (#5873)
    * add branch guide doc paths
    
    * update branch guide doc route
    
    * add branch guide to cheat sheet
    
    * remove code introduced in a separate PR
    
    Co-authored-by: Aaron Wentzel <16669785+awentzel@users.noreply.github.com>

--- sites/website/src/generate-docs.js
+++ sites/website/src/generate-docs.js
@@ -138,6 +138,18 @@ async function copyArticleMarkdown() {
             },
         },
         {
+            src: path.resolve(root, "BRANCH_GUIDE.md"),
+            dest: path.resolve(outputDir, "community/branch-guide.md"),
+            metadata: {
+                id: "branch-guide",
+                title: "Branch Guide",
+                sidebar_label: "Branch Guide",
+                custom_edit_url:
+                    "https://github.com/microsoft/fast/blob/master/BRANCH_GUIDE.md",
+                desciption: "A branch guide for the FAST repository.",
+            },
+        },
+        {
             src: path.resolve(root, "LICENSE"),
             dest: path.resolve(outputDir, "resources/license.md"),
             metadata: {
