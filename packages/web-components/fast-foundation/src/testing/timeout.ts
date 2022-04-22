a3f20ae06873f1f90573d0bc36abf29b89692c59
Rob Eisenberg <rob@bluespire.com>
2022-04-22 10:01:23 -0400

    docs: improve testing helper code docs

--- packages/web-components/fast-foundation/src/testing/timeout.ts
+++ packages/web-components/fast-foundation/src/testing/timeout.ts
@@ -1,5 +1,8 @@
 /**
- * Timeout for use in async tets.
+ * A timeout helper for use in tests.
+ * @param timeout The length of the timeout.
+ * @returns A promise that resolves once the configured time has elapsed.
+ * @public
  */
 export async function timeout(timeout = 0): Promise<void> {
     return new Promise((resolve, reject) => {
