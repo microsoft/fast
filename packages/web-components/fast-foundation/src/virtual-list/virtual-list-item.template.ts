0169bda62eabdb0363b0c30bc0ffd4990da9192f
Stephane Comeau <scomea@microsoft.com>
2022-04-25 17:10:25 -0700

    fix extensions

--- packages/web-components/fast-foundation/src/virtual-list/virtual-list-item.template.ts
+++ packages/web-components/fast-foundation/src/virtual-list/virtual-list-item.template.ts
@@ -1,6 +1,6 @@
 import { html, ViewTemplate, when } from "@microsoft/fast-element";
-import type { FoundationElementTemplate } from "../foundation-element";
-import type { VirtualListItem } from "./virtual-list-item";
+import type { FoundationElementTemplate } from "../foundation-element/index.js";
+import type { VirtualListItem } from "./virtual-list-item.js";
 
 /**
  * The template for the {@link @microsoft/fast-foundation#VirtualListItem} component.
