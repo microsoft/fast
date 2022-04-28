46f935158aca1a4b28c0d9b4e74c083b31a40fdb
Chris Holt <chhol@microsoft.com>
2022-04-28 14:11:58 -0700

    ensure tab and tabpanel instances without ids do not go out of sync due to separate consecutive calls (#5907)

--- packages/web-components/fast-components/src/tabs/tabs.stories.ts
+++ packages/web-components/fast-components/src/tabs/tabs.stories.ts
@@ -1,4 +1,27 @@
+import { STORY_RENDERED } from "@storybook/core-events";
+import addons from "@storybook/addons";
 import Examples from "./fixtures/base.html";
+
+function addItem(): void {
+    const tabsElement = document.getElementById("addTabsExample");
+
+    if (tabsElement?.children !== undefined) {
+        const tab: any = document.createElement("fast-tab");
+        const tabPanel: any = document.createElement("fast-tab-panel");
+        tab.textContent = "Added tab";
+        tabPanel.textContent = "Added panel";
+
+        tabsElement?.appendChild(tab);
+        tabsElement.insertBefore(tabPanel, tab);
+    }
+}
+
+addons.getChannel().addListener(STORY_RENDERED, (name: string) => {
+    if (name.toLowerCase() === "tabs--tabs") {
+        const button = document.getElementById("add-button") as HTMLElement;
+        button.addEventListener("click", () => addItem());
+    }
+});
 
 export default {
     title: "Tabs",
