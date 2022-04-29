8bf2992defb1b127c98d5fc9b7a3fd32619d92b5
Chris Holt <chhol@microsoft.com>
2022-04-28 19:53:06 -0700

    feat: trim unnecessary nodes from start end (#5892)
    * remove unnecessary DOM nodes from start/end slot templates
    
    * Change files

--- packages/web-components/fast-foundation/src/patterns/start-end.ts
+++ packages/web-components/fast-foundation/src/patterns/start-end.ts
@@ -25,29 +25,18 @@ export type EndOptions = {
 export type StartEndOptions = StartOptions & EndOptions;
 
 /**
- * A mixin class implementing start and end elements.
+ * A mixin class implementing start and end slots.
  * These are generally used to decorate text elements with icons or other visual indicators.
  * @public
  */
 export class StartEnd {
     public start: HTMLSlotElement;
-    public startContainer: HTMLSpanElement;
-    public handleStartContentChange(): void {
-        this.startContainer.classList.toggle(
-            "start",
-            this.start.assignedNodes().length > 0
-        );
-    }
 
     public end: HTMLSlotElement;
-    public endContainer: HTMLSpanElement;
-    public handleEndContentChange(): void {
-        this.endContainer.classList.toggle("end", this.end.assignedNodes().length > 0);
-    }
 }
 
 /**
- * The template for the end element.
+ * The template for the end slot.
  * For use with {@link StartEnd}
  *
  * @public
@@ -59,19 +48,11 @@ export const endSlotTemplate: (
     context: ElementDefinitionContext,
     definition: EndOptions
 ) => html`
-    <span
-        part="end"
-        ${ref("endContainer")}
-        class=${x => (definition.end ? "end" : void 0)}
-    >
-        <slot name="end" ${ref("end")} @slotchange="${x => x.handleEndContentChange()}">
-            ${definition.end || ""}
-        </slot>
-    </span>
+    <slot name="end" ${ref("end")}>${definition.end || ""}</slot>
 `;
 
 /**
- * The template for the start element.
+ * The template for the start slots.
  * For use with {@link StartEnd}
  *
  * @public
@@ -82,52 +63,7 @@ export const startSlotTemplate: (
 ) => ViewTemplate<StartEnd> = (
     context: ElementDefinitionContext,
     definition: StartOptions
-) => html`
-    <span
-        part="start"
-        ${ref("startContainer")}
-        class="${x => (definition.start ? "start" : void 0)}"
-    >
-        <slot
-            name="start"
-            ${ref("start")}
-            @slotchange="${x => x.handleStartContentChange()}"
-        >
-            ${definition.start || ""}
-        </slot>
-    </span>
-`;
-
-/**
- * The template for the end element.
- * For use with {@link StartEnd}
- *
- * @public
- * @deprecated - use endSlotTemplate
- */
-export const endTemplate: ViewTemplate<StartEnd> = html`
-    <span part="end" ${ref("endContainer")}>
-        <slot
-            name="end"
-            ${ref("end")}
-            @slotchange="${x => x.handleEndContentChange()}"
-        ></slot>
-    </span>
-`;
-
-/**
- * The template for the start element.
- * For use with {@link StartEnd}
- *
- * @public
- * @deprecated - use startSlotTemplate
- */
-export const startTemplate: ViewTemplate<StartEnd> = html`
-    <span part="start" ${ref("startContainer")}>
-        <slot
-            name="start"
-            ${ref("start")}
-            @slotchange="${x => x.handleStartContentChange()}"
-        ></slot>
-    </span>
-`;
+) =>
+    html`
+        <slot name="start" ${ref("start")}>${definition.start || ""}</slot>
+    `;
