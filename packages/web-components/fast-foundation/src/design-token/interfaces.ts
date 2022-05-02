4f59665b8fa1e7dfc2372e531daca7cdebdfe85c
Aaron Wentzel <16669785+awentzel@users.noreply.github.com>
2022-05-02 10:39:55 -0700

    Revert "reconcile rebase"
    This reverts commit 50421715ad4fb2abb061320091cfd52d96498390.

--- packages/web-components/fast-foundation/src/design-token/interfaces.ts
+++ packages/web-components/fast-foundation/src/design-token/interfaces.ts
@@ -1,5 +1,5 @@
 /**
- * A {@link (DesignToken#interface)} value that is derived. These values can depend on other {@link (DesignToken#interface)}s
+ * A {@link (DesignToken:interface)} value that is derived. These values can depend on other {@link (DesignToken:interface)}s
  * or arbitrary observable properties.
  * @public
  */
@@ -14,23 +14,23 @@ export type DerivedDesignTokenValue<T> = T extends Function
 export type StaticDesignTokenValue<T> = T extends Function ? never : T;
 
 /**
- * The type that a {@link (DesignToken#interface)} can be set to.
+ * The type that a {@link (DesignToken:interface)} can be set to.
  * @public
  */
 export type DesignTokenValue<T> = StaticDesignTokenValue<T> | DerivedDesignTokenValue<T>;
 
 /**
- * Describes a {@link (DesignToken#interface)} configuration
+ * Describes a {@link (DesignToken:interface)} configuration
  * @public
  */
 export interface DesignTokenConfiguration {
     /**
-     * The name of the {@link (DesignToken#interface)}.
+     * The name of the {@link (DesignToken:interface)}.
      */
     name: string;
 
     /**
-     * The name of the CSS custom property to associate to the {@link (DesignToken#interface)}, or null
+     * The name of the CSS custom property to associate to the {@link (DesignToken:interface)}, or null
      * if not CSS custom property should be associated.
      */
     cssCustomPropertyName?: string | null;
