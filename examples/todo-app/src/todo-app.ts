a8a446c9119b953a4d4774d26146584725666f81
Rob Eisenberg <rob@bluespire.com>
2022-06-14 15:33:51 -0400

    doc: comment the sample and metadata API

--- examples/todo-app/src/todo-app.ts
+++ examples/todo-app/src/todo-app.ts
@@ -7,6 +7,15 @@ export class TodoApp extends FASTElement {
     @TodoList todos!: TodoList;
 }
 
+// By using this API instead of the @customElement
+// decorator, we are able to assemble the component
+// with its name, template, and styles, but without
+// immediately defining it with the platform. We do
+// this so that we can control the startup timing of
+// the app. See the main.ts file for further
+// explanation.
+// Note: This API is planned to be renamed from
+// "metadata" to "compose" in a future beta.
 export const app = FASTElement.metadata(TodoApp, {
     name: "todo-app",
     template,
--- examples/todo-app/src/todo-list.ts
+++ examples/todo-app/src/todo-list.ts
@@ -18,6 +18,13 @@ export class DefaultTodoList {
 
     @volatile
     public get filtered(): readonly Todo[] {
+        // This property is decorated with @volatile because the exact
+        // observable dependencies of the property can change between
+        // invocations. Normally, FAST assumes that the dependencies of
+        // a binding are the same across invocations, for optimization
+        // purposes. So, in this case, we need to tell the system not to
+        // make that assumption.
+
         switch (this.activeFilter) {
             case "active":
                 return this._todos.filter(x => !x.done);
@@ -37,8 +44,16 @@ export class DefaultTodoList {
         index !== -1 && this.splice(index, 1);
     }
 
+    /**
+     * This method centralizes all updates to the internal array so that we
+     * can guarantee that observers are notified in the appropriate cases.
+     */
     private splice(index: number, removeCount: number, ...newItem: Todo[]) {
         this._todos.splice(index, removeCount, ...newItem);
+
+        // Because the filtered property returns different arrays depending
+        // on the filter, we need to notify FAST that the dependent _todos
+        // observable has changed whenever we splice the internal data structure.
         this.activeFilter !== "all" && Observable.notify(this, "_todos");
     }
 }
