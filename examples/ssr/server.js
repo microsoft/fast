f1a9b99b4badcea74add05bd2c7636792fa77821
nicholasrice <nicholasrice@users.noreply.github.com>
2022-06-20 10:38:16 -0700

    remove 'hydrate' mechanism and avoid rendering error

--- examples/ssr/server.js
+++ examples/ssr/server.js
@@ -22,36 +22,17 @@ const template = html`
     <html lang="en">
         <head>
             <meta charset="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>SSR Example</title>
-            <script>
-                // Be careful in production environments embedding JSON.
-                // In general the JSON should be sanitized to prevent
-                // JSON injection attacks.
-                window.__SSR_STATE__ = ${() => JSON.stringify(todoData)};
-            </script>
-            <style>
-                #hydration-message {
-                    max-width: 500px;
-                }
-            </style>
+                <!--
+                    Use caution in production environments embedding JSON.
+                    In general the JSON should be sanitized to prevent
+                    JSON injection attacks.
+                -->
+            <script>window.__SSR_STATE__ = ${() => JSON.stringify(todoData)};</script>
         <body>
             <todo-app></todo-app>
-            <div id="hydration-message">
-                <p>To better illustrate the output of <code>@microsoft/fast-ssr</code>, this page does not automatically load the JavaScript  define the custom elements. In most cases, defining the custom elements ASAP will lead to better user experience. To define the elements and make them functional, click the <a href="#hydrate">hydrate button</a> blow.</p>
-                <button id="hydrate">hydrate</button>
-            </div>
-            <script>
-                const scriptLoader = document.getElementById("hydrate");
-                const container = document.getElementById('hydration-message');
-                scriptLoader.addEventListener("click", () => {
-                    const script = document.createElement("script");
-                    script.src = "/bundle.js";
-                    document.body.appendChild(script);
-                    container.parentNode.removeChild(container);
-                });
-            </script>
+            <script src="/bundle.js" defer></script>
         </body>
     </html>
 `;
--- examples/ssr/src/main.ts
+++ examples/ssr/src/main.ts
@@ -3,7 +3,5 @@ import { app, DefaultTodoList, TodoList } from "fast-todo-app";
 
 const SSRState = (window as any).__SSR_STATE__ || [];
 
-fetch("http://localhost:8080/todos").then(data => {
-    TodoList.provide(document, new DefaultTodoList(SSRState));
-    app.define();
-});
+TodoList.provide(document, new DefaultTodoList(SSRState));
+app.define();
