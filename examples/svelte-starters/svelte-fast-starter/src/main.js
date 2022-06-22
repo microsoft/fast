e07f3f694abf7202fe70e716d6d573537e584488
Steph Huynh <steph@huynhicode.dev>
2022-06-21 22:32:38 -0700

    create svelte starter project

---
+++ examples/svelte-starters/svelte-fast-starter/src/main.js
@@ -0,0 +1,9 @@
+import "./app.css";
+import App from "./App.svelte";
+
+const app = new App({
+    // eslint-disable-next-line no-undef
+    target: document.getElementById("app"),
+});
+
+export default app;
