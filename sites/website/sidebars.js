1e4a383fada3a4895623e6b54088f9f2a07c7a78
Steph <35910788+huynhicode@users.noreply.github.com>
2022-06-24 07:37:50 -0700

    docs: add integration guide for Rollup (#6069)
    * add rollup to sidebar and create integration guide (legacy docs)
    
    * add content
    
    * add rollup config and md file to docs current version
    
    * Change files
    
    * update change file type and dependentChangeType to none
    
    * minor update to text - both doc versions
    
    * revert yarn.lock updates
    
    * update html script src
    
    * minor verbaige updates
    
    * add additional plugin
    
    * remove comments
    
    * verbiage update to note re tsconfig conflicts (both doc versions)
    
    Co-authored-by: Rob Eisenberg <EisenbergEffect@users.noreply.github.com>

--- sites/website/sidebars.js
+++ sites/website/sidebars.js
@@ -114,6 +114,7 @@ module.exports = {
                 "integrations/blazor",
                 "integrations/ember",
                 "integrations/react",
+                "integrations/rollup",
                 "integrations/svelte",
                 "integrations/vite",
                 "integrations/vue",
