0bcfde02ba5d58a52c71ac9c7adcb12b9256d371
Brian Heston <47367562+bheston@users.noreply.github.com>
2022-06-27 10:26:11 -0700

    Moved recipe to correct place

--- packages/utilities/adaptive-ui/src/design-tokens/elevation.ts
+++ packages/utilities/adaptive-ui/src/design-tokens/elevation.ts
@@ -1,19 +1,5 @@
+import { ElevationRecipe } from "../elevation/recipe.js";
 import { create, createNonCss } from "./create.js";
-
-/**
- * A recipe that evaluates to an elevation treatment, commonly, but not limited to, a box .
- *
- * @public
- */
-export interface ElevationRecipe {
-    /**
-     * Evaluate an elevation treatment.
-     *
-     * @param element - The element for which to evaluate the recipe
-     * @param size - The size of the elevation
-     */
-    evaluate(element: HTMLElement, size: number): string;
-}
 
 /**
  * @public
