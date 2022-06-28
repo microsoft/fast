02e56f1576efc31812ccea16b80ed595937e50b7
Rob Eisenberg <rob@bluespire.com>
2022-06-28 10:35:11 -0400

    feat: convert avatar to new template pattern

--- packages/web-components/fast-foundation/src/anchored-region/anchored-region.template.ts
+++ packages/web-components/fast-foundation/src/anchored-region/anchored-region.template.ts
@@ -2,11 +2,11 @@ import { ElementViewTemplate, html, when } from "@microsoft/fast-element";
 import type { FASTAnchoredRegion } from "./anchored-region.js";
 
 /**
- * The template for the {@link @microsoft/fast-foundation#(AnchoredRegion:class)} component.
+ * The template for the {@link @microsoft/fast-foundation#(FASTAnchoredRegion:class)} component.
  * @public
  */
 export function anchoredRegionTemplate(): ElementViewTemplate<FASTAnchoredRegion> {
-    return html`
+    return html<FASTAnchoredRegion>`
         <template class="${x => (x.initialLayoutComplete ? "loaded" : "")}">
             ${when(
                 x => x.initialLayoutComplete,
