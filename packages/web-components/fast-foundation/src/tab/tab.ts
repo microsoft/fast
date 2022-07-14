414d3d7bc865036e6631055d70faeb2736a5fe45
Chris Holt <chhol@microsoft.com>
2022-07-14 14:29:09 -0700

    add start and end slots to tab

--- packages/web-components/fast-foundation/src/tab/tab.ts
+++ packages/web-components/fast-foundation/src/tab/tab.ts
@@ -1,4 +1,12 @@
 import { attr, FASTElement } from "@microsoft/fast-element";
+import { StartEnd, StartEndOptions } from "../patterns/index.js";
+import { applyMixins } from "../utilities/apply-mixins.js";
+
+/**
+ * Tab option configuration options
+ * @public
+ */
+export type TabOptionOptions = StartEndOptions;
 
 /**
  * A Tab Component to be used with {@link @microsoft/fast-foundation#(FASTTabs:class)}
@@ -17,3 +25,13 @@ export class FASTTab extends FASTElement {
     @attr({ mode: "boolean" })
     public disabled: boolean;
 }
+
+/**
+ * Mark internal because exporting class and interface of the same name
+ * confuses API documenter.
+ * TODO: https://github.com/microsoft/fast/issues/3317
+ * @internal
+ */
+/* eslint-disable-next-line */
+export interface FASTTab extends StartEnd {}
+applyMixins(FASTTab, StartEnd);
