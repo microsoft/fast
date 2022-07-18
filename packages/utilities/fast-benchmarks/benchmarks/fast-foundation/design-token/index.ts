086554cb56dc350c7fa5b42016c6568806c9cf8a
nicholasrice <nicholasrice@users.noreply.github.com>
2022-07-18 12:59:48 -0700

    adding test

---
+++ packages/utilities/fast-benchmarks/benchmarks/fast-foundation/design-token/index.ts
@@ -0,0 +1,1 @@
+( window as any ).test = () => {console.log("Hello world")}
---
+++ packages/utilities/fast-benchmarks/src/test10k.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+import {
+    destroy,
+    getTestStartName,
+    measureMemory,
+    updateComplete,
+} from "../utils/index.js";
+(async () => {
+    const container = document.getElementById("container");
+    const render = async () => {
+        const start = getTestStartName(test);
+        performance.mark(start);
+        window.test();
+        await updateComplete();
+        performance.measure(test, start);
+        destroy(container);
+    };
+    await render();
+    measureMemory();
+})();
