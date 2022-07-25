349a30c989da19b377e627fdb6e64974345a4b1b
Brian Heston <47367562+bheston@users.noreply.github.com>
2022-07-25 10:18:26 -0700

    Adaptive UI: fixed missing export and incorrect min-contrast token values (#6235)

--- packages/utilities/adaptive-ui/src/color/recipes/index.ts
+++ packages/utilities/adaptive-ui/src/color/recipes/index.ts
@@ -1,3 +1,4 @@
+export * from "./black-or-white-by-contrast-set.js";
 export * from "./black-or-white-by-contrast.js";
 export * from "./contrast-and-delta-swatch-set.js";
 export * from "./contrast-swatch.js";
