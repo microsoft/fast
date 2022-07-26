8ce86928e5d970ee36e17f61947749cac53792ee
Wendy Hsu <prudepixie@users.noreply.github.com>
2022-07-26 10:56:53 -0700

    test benchmark

---
+++ packages/utilities/fast-benchmarks/benchmarks/fast-element/observable/index.ts
@@ -0,0 +1,53 @@
+import { Observable } from "@microsoft/fast-element";
+
+export class Person {
+    private _firstName: string;
+    private _lastName: string;
+
+    get firstName() {
+        Observable.track(this, "firstName");
+        return this._firstName;
+    }
+
+    set firstName(value: string) {
+        this._firstName = value;
+        Observable.notify(this, "firstName");
+    }
+
+    get lastName() {
+        Observable.track(this, "lastName");
+        return this._lastName;
+    }
+
+    set lastName(value: string) {
+        this._lastName = value;
+        Observable.notify(this, "lastName");
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+(window as any).test = () => {
+    const person = new Person();
+    const notifier = Observable.getNotifier(person);
+    const handler = {
+        handleChange(source: any, propertyName: string) {
+            // respond to the change here
+            // source will be the person instance
+            // propertyName will be "firstName"
+            source._firstName += "!!!!!!!!!!!!!!!";
+            source._lastName.trim();
+        },
+    };
+
+    notifier.subscribe(handler, "firstName");
+    notifier.subscribe(handler, "lastName");
+
+    person.firstName = "wendy";
+    person.lastName = "hsu                     ";
+
+    notifier.unsubscribe(handler, "firstName");
+    notifier.unsubscribe(handler, "lastName");
+};
