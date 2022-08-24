fd60ac9662d5e1c1a8c1babc3d0b1cac220b1a74
John Kreitlow <863023+radium-v@users.noreply.github.com>
2022-08-23 19:26:28 -0700

    normalize horizontal-scroll stories

--- packages/web-components/fast-foundation/src/horizontal-scroll/stories/horizontal-scroll.stories.ts
+++ packages/web-components/fast-foundation/src/horizontal-scroll/stories/horizontal-scroll.stories.ts
@@ -1,87 +1,67 @@
-import { html, repeat } from "@microsoft/fast-element";
-import type { Args, Meta } from "@storybook/html";
+import { css, html, repeat } from "@microsoft/fast-element";
+import { storyTemplate as cardStoryTemplate } from "../../card/stories/card.stories.js";
+import type { Meta, Story, StoryArgs } from "../../__test__/helpers.js";
+import { renderComponent } from "../../__test__/helpers.js";
 import type { FASTHorizontalScroll } from "../horizontal-scroll.js";
+import { HorizontalScrollView, ScrollEasing } from "../horizontal-scroll.options.js";
 
-type HorizontalScrollStoryArgs = Args & FASTHorizontalScroll;
-type HorizontalScrollStoryMeta = Meta<HorizontalScrollStoryArgs>;
-
-const componentTemplate = html<HorizontalScrollStoryArgs>`
-    <fast-horizontal-scroll>
-        ${repeat(
-            x => x.items,
-            html`
-                ${x => x}
-            `,
-            {
-                positioning: true,
-            }
-        )}
+const storyTemplate = html<StoryArgs<FASTHorizontalScroll>>`
+    <fast-horizontal-scroll
+        speed="${x => x.speed}"
+        duration="${x => x.duration}"
+        easing="${x => x.easing}"
+        flippers-hidden-from-at="${x => x.flippersHiddenFromAT}"
+        view="${x => x.view}"
+    >
+        ${x => x.storyContent}
     </fast-horizontal-scroll>
 `;
 
 export default {
     title: "Horizontal Scroll",
     args: {
-        items: new Array(16).fill(html`
-            <fast-card><fast-button>A button</fast-button></fast-card>
-        `),
+        flippersHiddenFromAT: false,
+        storyContent: html`
+            ${repeat(x => x.storyItems, cardStoryTemplate)}
+        `,
+        storyItems: new Array(16).fill(null).map((_, i) => ({
+            storyContent: html`
+                <div>Card ${i + 1}</div>
+                <fast-button>button</fast-button>
+            `,
+        })),
     },
     argTypes: {
-        items: { table: { disable: true } },
+        duration: { control: "text" },
+        flippersHiddenFromAT: { control: "boolean" },
+        easing: { control: "text", options: Object.values(ScrollEasing) },
+        speed: { control: { type: "number", min: 0 } },
+        storyContent: { table: { disable: true } },
+        storyItems: { table: { disable: true } },
+        view: { control: "radio", options: Object.values(HorizontalScrollView) },
     },
     decorators: [
         Story => {
             const renderedStory = Story() as FASTHorizontalScroll;
 
-            const styles = document.createElement("style");
-            styles.innerHTML = /* css */ `
-                fast-card {
-                    width: 120px;
-                    height: 200px;
+            renderedStory.$fastController.addStyles(css`
+                ::slotted(fast-card) {
                     color: var(--neutral-foreground-rest);
+                    height: 200px;
+                    width: 120px;
                 }
 
-                fast-horizontal-scroll {
+                :host {
                     max-width: 620px;
                     margin: 20px;
                 }
-
-                .right-gradient {
-                    --scroll-fade-next: var(--fill-color);
-                }
-
-                .both-gradient {
-                    --scroll-fade-next: var(--fill-color);
-                    --scroll-fade-previous: var(--fill-color);
-                }
-
-                .top-align {
-                    --scroll-align: flex-start;
-                }
-
-                .bottom-align {
-                    --scroll-align: flex-end;
-                }
-
-                .full-width,
-                .full-width fast-card {
-                    width: 600px;
-                    height: 300px;
-                }
-                .window-resize {
-                    max-width: unset;
-                }
-            `;
-
-            renderedStory.append(styles);
+            `);
 
             return renderedStory;
         },
     ],
-} as HorizontalScrollStoryMeta;
+} as Meta<FASTHorizontalScroll>;
 
-export const HorizontalScroll = (args: HorizontalScrollStoryArgs) => {
-    const storyFragment = new DocumentFragment();
-    componentTemplate.render(args, storyFragment);
-    return storyFragment.firstElementChild;
-};
+export const HorizontalScroll: Story<FASTHorizontalScroll> = renderComponent(
+    storyTemplate
+).bind({});
