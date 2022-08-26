8eca2885d1e59e29affef17cff76877114b1bb1d
John Kreitlow <863023+radium-v@users.noreply.github.com>
2022-08-25 18:38:26 -0700

    convert search tests to playwright

--- packages/web-components/fast-foundation/src/search/search.template.ts
+++ packages/web-components/fast-foundation/src/search/search.template.ts
@@ -11,18 +11,14 @@ export function searchTemplate<T extends FASTSearch>(
     options: SearchOptions = {}
 ): ElementViewTemplate<T> {
     return html<T>`
-        <template
-            class="
-            ${x => (x.readOnly ? "readonly" : "")}
-        "
-        >
+        <template class="${x => (x.readOnly ? "readonly" : "")}">
             <label
                 part="label"
                 for="control"
                 class="${x =>
-                    x.defaultSlottedNodes && x.defaultSlottedNodes.length
-                        ? "label"
-                        : "label label__hidden"}"
+                    ["label", !x.defaultSlottedNodes?.length && "label__hidden"]
+                        .filter(Boolean)
+                        .join(" ")}"
             >
                 <slot
                     ${slotted({
