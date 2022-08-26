475e0220f6202f7156ce743bd64af2029cfb2dd7
John Kreitlow <863023+radium-v@users.noreply.github.com>
2022-08-25 18:43:47 -0700

    convert switch tests to playwright

---
+++ packages/web-components/fast-foundation/src/switch/switch.pw.spec.ts
@@ -0,0 +1,402 @@
+import { expect, test } from "@playwright/test";
+import { fixtureURL } from "../__test__/helpers.js";
+import type { FASTSwitch } from "./switch.js";
+
+test.describe("Switch", () => {
+    test("should have a role of `switch`", async ({ page }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await expect(element).toHaveAttribute("role", "switch");
+    });
+
+    test("should set the `aria-checked` attribute equal to the `checked` value", async ({
+        page,
+    }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.checked = true;
+        });
+
+        await expect(element).toHaveAttribute("aria-checked", "true");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.checked = false;
+        });
+
+        await expect(element).toHaveAttribute("aria-checked", "false");
+    });
+
+    test("should add a class of `checked` when checked is true", async ({ page }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.checked = true;
+        });
+
+        await expect(element).toHaveClass(/checked/);
+    });
+
+    test("should set a default `aria-checked` value when `checked` is not defined", async ({
+        page,
+    }) => {
+        await page.goto(
+            fixtureURL("switch--switch", {
+                checked: false,
+            })
+        );
+
+        const element = page.locator("fast-switch");
+
+        await expect(element).toHaveAttribute("aria-checked", "false");
+    });
+
+    test("should set the `aria-disabled` attribute equal to the `disabled` value", async ({
+        page,
+    }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.disabled = true;
+        });
+
+        await expect(element).toHaveAttribute("aria-disabled", "true");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.disabled = false;
+        });
+
+        await expect(element).toHaveAttribute("aria-disabled", "false");
+    });
+
+    test("should set a default `aria-disabled` value when `disabled` is not defined", async ({
+        page,
+    }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await expect(element).toHaveAttribute("aria-disabled", "false");
+    });
+
+    test("should set the `aria-readonly` attribute equal to the `readonly` value", async ({
+        page,
+    }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.readOnly = true;
+        });
+
+        await expect(element).toHaveAttribute("aria-readonly", "true");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.readOnly = false;
+        });
+
+        await expect(element).toHaveAttribute("aria-readonly", "false");
+    });
+
+    test("should NOT set a default `aria-readonly` value when `readonly` is not defined", async ({
+        page,
+    }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        expect(await element.getAttribute("aria-readonly")).toBeNull();
+    });
+
+    test("should set a tabindex of 0 on the element", async ({ page }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await expect(element).toHaveAttribute("tabindex", "0");
+    });
+
+    test("should NOT set a tabindex when disabled is `true`", async ({ page }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.disabled = true;
+        });
+
+        await expect(element).toHaveAttribute("tabindex", "");
+    });
+
+    test("should initialize to the initial value if no value property is set", async ({
+        page,
+    }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        const initialValue = await element.evaluate<string, FASTSwitch>(
+            node => node.initialValue
+        );
+
+        expect(await element.evaluate<string, FASTSwitch>(node => node.value)).toBe(
+            initialValue
+        );
+    });
+
+    test("should initialize to the provided value attribute if set pre-connection", async ({
+        page,
+    }) => {
+        await page.goto(
+            fixtureURL("switch--switch", {
+                value: "foobar",
+            })
+        );
+
+        const element = page.locator("fast-switch");
+
+        await expect(element).toHaveJSProperty("value", "foobar");
+    });
+
+    test("should initialize to the provided value attribute if set post-connection", async ({
+        page,
+    }) => {
+        await page.goto(fixtureURL("switch--switch"));
+
+        const element = page.locator("fast-switch");
+
+        await element.evaluate<void, FASTSwitch>(node => {
+            node.setAttribute("value", "foobar");
+        });
+
+        await expect(element).toHaveJSProperty("value", "foobar");
+    });
+
+    test("should initialize to the provided value property if set pre-connection", async ({
+        page,
+    }) => {
+        await page.goto(fixtureURL("debug--blank"));
+
+        const element = page.locator("fast-switch");
+
+        await page.evaluate(() => {
+            const switchElement = document.createElement("fast-switch") as FASTSwitch;
+            switchElement.value = "foobar";
+            document.body.appendChild(switchElement);
+        });
+
+        await expect(element).toHaveJSProperty("value", "foobar");
+    });
+
+    test.describe("label", () => {
+        test("should add a class of `label` to the internal label when default slotted content exists", async ({
+            page,
+        }) => {
+            await page.goto(fixtureURL("switch--switch", { storyContent: "label" }));
+
+            const element = page.locator("fast-switch");
+
+            const label = element.locator(".label");
+
+            await expect(label).toHaveClass(/label/);
+
+            await expect(label).not.toHaveClass(/label__hidden/);
+        });
+
+        test("should add classes of `label` and `label__hidden` to the internal label when default slotted content exists", async ({
+            page,
+        }) => {
+            await page.goto(fixtureURL("switch--switch", { storyContent: "" }));
+
+            const element = page.locator("fast-switch");
+
+            const label = element.locator(".label");
+
+            await expect(label).toHaveClass(/label/);
+
+            await expect(label).toHaveClass(/label__hidden/);
+        });
+    });
+
+    test.describe("events", () => {
+        test("should fire an event on click", async ({ page }) => {
+            await page.goto(fixtureURL("switch--switch"));
+
+            const element = page.locator("fast-switch");
+
+            const [wasClicked] = await Promise.all([
+                element.evaluate(
+                    node =>
+                        new Promise(resolve => {
+                            node.addEventListener("click", () => resolve(true));
+                        })
+                ),
+                element.click(),
+            ]);
+
+            expect(wasClicked).toBe(true);
+        });
+
+        test("should fire an event when spacebar is invoked", async ({ page }) => {
+            await page.goto(fixtureURL("switch--switch"));
+
+            const element = page.locator("fast-switch");
+
+            const [wasEmitted] = await Promise.all([
+                element.evaluate(
+                    node =>
+                        new Promise(resolve => {
+                            node.addEventListener("keydown", () => resolve(true));
+                        })
+                ),
+                element.evaluate(node => {
+                    node.dispatchEvent(new KeyboardEvent("keydown", { key: " " }));
+                }),
+            ]);
+
+            expect(wasEmitted).toBe(true);
+        });
+
+        test("should fire an event when enter is invoked", async ({ page }) => {
+            await page.goto(fixtureURL("switch--switch"));
+
+            const element = page.locator("fast-switch");
+
+            const [wasEmitted] = await Promise.all([
+                element.evaluate(
+                    node =>
+                        new Promise(resolve => {
+                            node.addEventListener("keydown", () => resolve(true));
+                        })
+                ),
+                element.evaluate(node => {
+                    node.dispatchEvent(new KeyboardEvent("keydown", { key: "Enter" }));
+                }),
+            ]);
+
+            expect(wasEmitted).toBe(true);
+        });
+    });
+
+    test.describe("when required", () => {
+        test("should be invalid when unchecked", async ({ page }) => {
+            await page.goto(fixtureURL("switch--switch-in-form", { checked: false }));
+
+            const element = page.locator("fast-switch");
+
+            expect(
+                await element.evaluate<boolean, FASTSwitch>(
+                    node => node.validity.valueMissing
+                )
+            ).toBe(true);
+        });
+
+        test("should be valid when checked", async ({ page }) => {
+            await page.goto(fixtureURL("switch--switch-in-form", { checked: true }));
+
+            const element = page.locator("fast-switch");
+
+            expect(
+                await element.evaluate<boolean, FASTSwitch>(
+                    node => node.validity.valueMissing
+                )
+            ).toBe(false);
+        });
+    });
+
+    test.describe("who's parent form has it's reset() method invoked", () => {
+        test("should set its checked property to false if the checked attribute is unset", async ({
+            page,
+        }) => {
+            await page.goto(fixtureURL("switch--switch-in-form", { checked: false }));
+
+            const element = page.locator("fast-switch");
+
+            const form = page.locator("form");
+
+            await expect(element).not.toHaveBooleanAttribute("checked");
+
+            await element.evaluate<void, FASTSwitch>(node => {
+                node.checked = true;
+            });
+
+            await expect(element).toHaveJSProperty("checked", true);
+
+            await form.evaluate<void, HTMLFormElement>(node => {
+                node.reset();
+            });
+
+            await expect(element).toHaveJSProperty("checked", false);
+        });
+
+        test("should set its checked property to true if the checked attribute is set", async ({
+            page,
+        }) => {
+            await page.goto(fixtureURL("switch--switch-in-form", { checked: true }));
+
+            const element = page.locator("fast-switch");
+
+            const form = page.locator("form");
+
+            await expect(element).toHaveBooleanAttribute("checked");
+
+            await element.evaluate<void, FASTSwitch>(node => {
+                node.checked = false;
+            });
+
+            await expect(element).toHaveJSProperty("checked", false);
+
+            await form.evaluate<void, HTMLFormElement>(node => {
+                node.reset();
+            });
+
+            await expect(element).toHaveJSProperty("checked", true);
+        });
+
+        test("should put the control into a clean state, where `checked` attribute modifications update the `checked` property prior to user or programmatic interaction", async ({
+            page,
+        }) => {
+            await page.goto(fixtureURL("debug--blank"));
+
+            const element = page.locator("fast-switch");
+
+            const form = page.locator("form");
+
+            await page.evaluate(() => {
+                const switchElement = document.createElement("fast-switch") as FASTSwitch;
+
+                const form = document.createElement("form");
+                document.getElementById("root")?.appendChild(form);
+                form.appendChild(switchElement);
+            });
+
+            await element.evaluate<void, FASTSwitch>(node => {
+                node.checked = true;
+                node.removeAttribute("checked");
+            });
+
+            await expect(element).toHaveJSProperty("checked", true);
+
+            await form.evaluate<void, HTMLFormElement>(node => {
+                node.reset();
+            });
+
+            await expect(element).toHaveJSProperty("checked", false);
+
+            await element.evaluate<void, FASTSwitch>(node => {
+                node.setAttribute("checked", "");
+            });
+
+            await expect(element).toHaveJSProperty("checked", true);
+        });
+    });
+});
