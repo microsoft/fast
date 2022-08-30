dcfe0274c8736e25e6af81259814deea53795f88
Rob Eisenberg <rob@bluespire.com>
2022-08-30 12:34:28 -0400

    test: add tests for ownedState

--- packages/web-components/fast-element/src/state/state.spec.ts
+++ packages/web-components/fast-element/src/state/state.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { state } from "./state.js";
+import { ownedState, state } from "./state.js";
 import { Observable } from "../observation/observable.js";
 import { Updates } from "../observation/update-queue.js";
 
@@ -92,5 +92,95 @@ describe("State", () => {
 
         expect("set" in readable).false;
         expect(() => (readable as any).current = 2).throws();
+    });
+});
+
+describe("OwnedState", () => {
+    it("can get and set the value for different owners", () => {
+        const sut = ownedState(1);
+        const owner1 = {};
+        const owner2 = {};
+
+        expect(sut(owner1)).equal(1);
+        expect(sut(owner2)).equal(1);
+
+        sut.set(owner1, 2);
+        sut.set(owner2, 3);
+
+        expect(sut(owner1)).equal(2);
+        expect(sut(owner2)).equal(3);
+    });
+
+    it("transfers its 2nd arg to the function name", () => {
+        const name = "Answer to the Ultimate Question of Life, the Universe, and Everything";
+        const sut = ownedState(42, name);
+
+        expect(sut.name).to.equal(name);
+    });
+
+    it("transfers its name option to the function name", () => {
+        const name = "Answer to the Ultimate Question of Life, the Universe, and Everything";
+        const sut = ownedState(42, { name });
+
+        expect(sut.name).to.equal(name);
+    });
+
+    it("can have its value observed", async () => {
+        const sut = ownedState(1);
+        const owner1 = {};
+        let wasCalled = false;
+
+        Observable.binding(sut, {
+            handleChange() {
+                wasCalled = true;
+            }
+        }).observe(owner1);
+
+        sut.set(owner1, 2);
+
+        await Updates.next();
+
+        expect(wasCalled).to.be.true;
+    });
+
+    it("can be deeply observed", async () => {
+        const sut = ownedState({
+            a: {
+                b: 1,
+                c: 2
+            }
+        }, { deep: true });
+        const owner1 = {};
+
+        let callCount = 0;
+        const subscriber = {
+            handleChange(binding, observer) {
+                callCount++;
+            }
+        };
+
+        Observable.binding(x => sut(x).a.b, subscriber).observe(owner1);
+        Observable.binding(x => sut(x).a.c, subscriber).observe(owner1);
+
+        sut(owner1).a.b = 2;
+        sut(owner1).a.c = 3;
+
+        await Updates.next();
+
+        expect(callCount).equals(2);
+    });
+
+    it("can create a readonly version of the state", () => {
+        const writable = ownedState(1);
+        const owner1 = {};
+        const readable = writable.asReadonly();
+
+        expect(readable(owner1)).equal(1);
+
+        writable.set(owner1, 2);
+
+        expect(readable(owner1)).equal(2);
+
+        expect("set" in readable).false;
     });
 });
