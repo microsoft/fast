b0bcc608f754ca4762b67902c74b82f00fbabe58
nicholasrice <nicholasrice@users.noreply.github.com>
2022-08-31 14:30:22 -0700

    adding pending task protocol implementation

---
+++ packages/web-components/fast-element/src/pending-task.ts
@@ -0,0 +1,9 @@
+/**
+ * Implementation of the https://github.com/webcomponents-cg/community-protocols/blob/main/proposals/pending-task.md proposal.
+ * @beta
+ */
+export class PendingTask extends Event {
+    constructor(public complete: Promise<void>) {
+        super("pending-task", { bubbles: true, composed: true });
+    }
+}
