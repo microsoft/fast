d18936afbd868121955828b7ecd3c8fa5472c26f
Rob Eisenberg <rob@bluespire.com>
2022-08-30 21:19:36 -0400

    docs: add code docs for new state APIs

--- packages/web-components/fast-element/src/state/state.ts
+++ packages/web-components/fast-element/src/state/state.ts
@@ -5,8 +5,19 @@ import type { Subscriber } from "../observation/notifier.js";
 import { ExpressionNotifier, Observable } from "../observation/observable.js";
 import { reactive } from "./reactive.js";
 
+/**
+ * Options for creating state.
+ * @beta
+ */
 export type StateOptions = {
+    /**
+     * Indicates whether to deeply make the state value observable.
+     */
     deep?: boolean;
+
+    /**
+     * A friendly name for the state.
+     */
     name?: string;
 };
 
@@ -14,17 +25,51 @@ const defaultStateOptions: StateOptions = {
     deep: false
 };
 
+/**
+ * A readonly stateful value.
+ * @beta
+ */
 export type ReadonlyState<T> = {
+    /**
+     * Gets the current state value.
+     */
     (): T;
+
+    /**
+     * Gets the current state value.
+     */
     readonly current: T;
 };
 
+/**
+ * A read/write stateful value.
+ * @beta
+ */
 export type State<T> = ReadonlyState<T> & {
+    /**
+     * Gets or sets the current state value.
+     */
     current: T;
+
+    /**
+     * Sets the current state value.
+     * @param value The new state value.
+     */
     set(value: T): void;
+
+    /**
+     * Creates a readonly version of the state.
+     */
     asReadonly(): ReadonlyState<T>;
 }
 
+/**
+ * Creates a reactive state value.
+ * @param value - The initial state value.
+ * @param options - Options to customize the state or a friendly name.
+ * @returns A State instance.
+ * @beta
+ */
 export function state<T>(
     value: T,
     options: string | StateOptions = defaultStateOptions
@@ -63,15 +108,42 @@ export function state<T>(
     return state;
 }
 
+/**
+ * A readonly stateful value associated with an object owner.
+ * @beta
+ */
 export type ReadonlyOwnedState<T> = {
+    /**
+     * Gets the current stateful value for the owner.
+     */
     (owner: any): T;
 }
 
+/**
+ * A read/write stateful value associated with an owner.
+ * @beta
+ */
 export type OwnedState<T> = ReadonlyOwnedState<T> & {
+    /**
+     * Sets
+     * @param owner - The object to set the state for the owner.
+     * @param value - The new state value.
+     */
     set(owner: any, value: T): void;
+
+    /**
+     * Creates a readonly version of the state.
+     */
     asReadonly(): ReadonlyOwnedState<T>;
 }
 
+/**
+ * Creates a reactive state that has its value associated with a specific owner.
+ * @param value - The initial value or a factory that provides an initial value for each owner.
+ * @param options - Options to customize the state or a friendly name.
+ * @returns An OwnedState instance.
+ * @beta
+ */
 export function ownedState<T>(
     value: T | (() => T),
     options: string | StateOptions = defaultStateOptions
@@ -124,38 +196,66 @@ export function ownedState<T>(
     return state;
 }
 
+/**
+ * State whose value is computed from other dependencies.
+ * @beta
+ */
 export type ComputedState<T> = ReadonlyState<T> & Disposable & {
     /**
-     * Subscribes to notification of changes in an object's state.
+     * Subscribes to notification of changes in the state.
      * @param subscriber - The object that is subscribing for change notification.
      */
      subscribe(subscriber: Subscriber): void;
 
      /**
-      * Unsubscribes from notification of changes in an object's state.
+      * Unsubscribes from notification of changes in the state.
       * @param subscriber - The object that is unsubscribing from change notification.
       */
      unsubscribe(subscriber: Subscriber): void;
 };
 
-export type ComputedSetupCallback = {
-    (): (() => void) | void;
-};
+/**
+ * A callback that enables computation setup.
+ * @beta
+ */
+export type ComputedSetupCallback
+    = () => (() => void) | void;
 
+/**
+ * Provides computed state capabilities.
+ * @beta
+ */
 export type ComputedBuilder = {
+    /**
+     * Callbacks related to computed state.
+     */
     on: {
+        /**
+         * Provides a setup callback for the computation.
+         * @param callback The callback to run to setup the computation.
+         */
         setup(callback: ComputedSetupCallback): void
     }
 };
 
-export type ComputedInitializer<T> = {
-    (controller: ComputedBuilder): () => T;
-};
+/**
+ * A callback that initializes the computation.
+ * @beta
+ */
+export type ComputedInitializer<T>
+    = (builder: ComputedBuilder) => () => T;
 
+/**
+ * Creates a ComputedState.
+ * @param initialize - The initialization callback.
+ * @param name - A friendly name for this computation.
+ * @returns A ComputedState
+ * @beta
+ */
 export function computedState<T>(
     initialize: ComputedInitializer<T>,
     name = "ComputedState"
-) {
+): ComputedState<T> {
     let setupCallback: ComputedSetupCallback | null = null;
     const builder: ComputedBuilder  = {
         on: {
