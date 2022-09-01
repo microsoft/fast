0aaecf35ec017613bb82bcd9833cc97f7631a445
Chris Holt <chhol@microsoft.com>
2022-09-01 10:28:04 -0700

    update breadcrumb item

--- packages/web-components/fast-foundation/src/breadcrumb-item/breadcrumb-item.template.ts
+++ packages/web-components/fast-foundation/src/breadcrumb-item/breadcrumb-item.template.ts
@@ -6,15 +6,15 @@ import type { BreadcrumbItemOptions, FASTBreadcrumbItem } from "./breadcrumb-ite
  * The template for the {@link @microsoft/fast-foundation#(FASTBreadcrumbItem:class)} component.
  * @public
  */
-export function breadcrumbItemTemplate(
+export function breadcrumbItemTemplate<T extends FASTBreadcrumbItem>(
     options: BreadcrumbItemOptions = {}
-): ElementViewTemplate<FASTBreadcrumbItem> {
-    return html<FASTBreadcrumbItem>`
+): ElementViewTemplate<T> {
+    return html<T>`
         <div role="listitem" class="listitem" part="listitem">
             ${anchorTemplate(options)}
             ${when(
                 x => x.separator,
-                html<FASTBreadcrumbItem>`
+                html<T>`
                     <span class="separator" part="separator" aria-hidden="true">
                         <slot name="separator">${options.separator || ""}</slot>
                     </span>
