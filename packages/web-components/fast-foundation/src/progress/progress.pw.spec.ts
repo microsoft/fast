6f73dfb0b985d1eab52136168b09d56a5d3076ab
John Kreitlow <863023+radium-v@users.noreply.github.com>
2022-09-14 18:16:36 -0700

    progress tests

--- packages/web-components/fast-foundation/src/progress/progress.pw.spec.ts
+++ packages/web-components/fast-foundation/src/progress/progress.pw.spec.ts
@@ -1,95 +1,92 @@
+import type { Locator, Page } from "@playwright/test";
 import { expect, test } from "@playwright/test";
 import { fixtureURL } from "../__test__/helpers.js";
 import type { FASTProgress } from "./progress.js";
 
 test.describe("Progress ring", () => {
-    test("should include a role of progressbar", async ({ page }) => {
+    let page: Page;
+    let element: Locator;
+
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+
+        element = page.locator("fast-progress");
+
         await page.goto(fixtureURL("progress--progress"));
+    });
 
-        const element = page.locator("fast-progress");
+    test.afterAll(async () => {
+        await page.close();
+    });
 
+    test("should include a role of progressbar", async () => {
         await expect(element).toHaveAttribute("role", "progressbar");
     });
 
-    test("should set the `aria-valuenow` attribute with the `value` property when provided", async ({
-        page,
-    }) => {
-        await page.goto(fixtureURL("progress--progress", { value: 50 }));
-
-        const element = page.locator("fast-progress");
+    test("should set the `aria-valuenow` attribute with the `value` property when provided", async () => {
+        await page.setContent(/* html */ `
+            <fast-progress value="50"></fast-progress>
+        `);
 
         await expect(element).toHaveAttribute("aria-valuenow", "50");
     });
 
-    test("should set the `aria-valuemin` attribute with the `min` property when provided", async ({
-        page,
-    }) => {
-        await page.goto(fixtureURL("progress--progress", { min: 0 }));
+    test("should set the `aria-valuemin` attribute with the `min` property when provided", async () => {
+        await page.setContent(/* html */ `
+            <fast-progress min="50"></fast-progress>
+        `);
 
-        const element = page.locator("fast-progress");
-
-        await expect(element).toHaveAttribute("aria-valuemin", "0");
+        await expect(element).toHaveAttribute("aria-valuemin", "50");
     });
 
-    test("should set the `aria-valuemax` attribute with the `max` property when provided", async ({
-        page,
-    }) => {
-        await page.goto(fixtureURL("progress--progress", { max: 75 }));
+    test("should set the `aria-valuemax` attribute with the `max` property when provided", async () => {
+        await page.setContent(/* html */ `
+            <fast-progress max="50"></fast-progress>
+        `);
 
-        const element = page.locator("fast-progress");
-
-        await expect(element).toHaveAttribute("aria-valuemax", "75");
+        await expect(element).toHaveAttribute("aria-valuemax", "50");
     });
 
-    test("should add a `paused` class when `paused` is true", async ({ page }) => {
-        await page.goto(fixtureURL("progress--progress", { paused: true }));
-
-        const element = page.locator("fast-progress");
+    test("should add a `paused` class when `paused` is true", async () => {
+        await page.setContent(/* html */ `
+            <fast-progress paused></fast-progress>
+        `);
 
         await expect(element).toHaveClass(/paused/);
     });
 
-    test("should render an element with a `determinate` slot when a value is provided", async ({
-        page,
-    }) => {
-        await page.goto(fixtureURL("progress--progress", { value: 50 }));
-
-        const element = page.locator("fast-progress");
+    test("should render an element with a `determinate` slot when a value is provided", async () => {
+        await page.setContent(/* html */ `
+            <fast-progress value="50"></fast-progress>
+        `);
 
         const progress = element.locator(".progress");
 
         await expect(progress).toHaveAttribute("slot", "determinate");
     });
 
-    test("should render an element with an `indeterminate` slot when no value is provided", async ({
-        page,
-    }) => {
-        await page.goto(fixtureURL("progress--progress", { value: "!undefined" }));
-
-        const element = page.locator("fast-progress");
+    test("should render an element with an `indeterminate` slot when no value is provided", async () => {
+        await page.setContent(/* html */ `
+            <fast-progress></fast-progress>
+        `);
 
         const progress = element.locator(".progress");
 
         await expect(progress).toHaveAttribute("slot", "indeterminate");
     });
 
-    test("should return the `percentComplete` property as a value between 0 and 100 when `min` and `max` are unset", async ({
-        page,
-    }) => {
-        const value = 50;
-        await page.goto(fixtureURL("progress--progress", { value }));
+    test("should return the `percentComplete` property as a value between 0 and 100 when `min` and `max` are unset", async () => {
+        await page.setContent(/* html */ `
+            <fast-progress value="50"></fast-progress>
+        `);
 
-        const element = page.locator("fast-progress");
-
-        await expect(element).toHaveJSProperty("percentComplete", value);
+        await expect(element).toHaveJSProperty("percentComplete", 50);
     });
 
-    test("should set the `percentComplete` property to match the current `value` in the range of `min` and `max`", async ({
-        page,
-    }) => {
-        await page.goto(fixtureURL("progress--progress", { value: 0 }));
-
-        const element = page.locator("fast-progress");
+    test("should set the `percentComplete` property to match the current `value` in the range of `min` and `max`", async () => {
+        await page.setContent(/* html */ `
+            <fast-progress value="0"></fast-progress>
+        `);
 
         await expect(element).toHaveJSProperty("percentComplete", 0);
 
