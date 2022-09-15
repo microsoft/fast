82dd6645c7733412cd230dab7c3b8cc09ae6a669
John Kreitlow <863023+radium-v@users.noreply.github.com>
2022-09-14 18:16:11 -0700

    tooltip tests

--- packages/web-components/fast-foundation/src/tooltip/tooltip.pw.spec.ts
+++ packages/web-components/fast-foundation/src/tooltip/tooltip.pw.spec.ts
@@ -1,49 +1,28 @@
+import type { Locator, Page } from "@playwright/test";
 import { expect, test } from "@playwright/test";
 import { fixtureURL } from "../__test__/helpers.js";
 import type { FASTTooltip } from "./tooltip.js";
 
 test.describe("Tooltip", () => {
-    test("should not render the tooltip by default", async ({ page }) => {
+    let page: Page;
+    let element: Locator;
+    let anchoredRegion: Locator;
+
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+
+        element = page.locator("fast-tooltip");
+
+        anchoredRegion = element.locator("fast-anchored-region");
+
         await page.goto(fixtureURL("tooltip--tooltip", { delay: 0 }));
-
-        const element = page.locator("fast-tooltip");
-
-        const anchoredRegion = element.locator("fast-anchored-region");
-
-        await expect(element).toHaveJSProperty("tooltipVisible", false);
-
-        await expect(anchoredRegion).toBeHidden();
     });
 
-    test("should render the tooltip when visible is true", async ({ page }) => {
-        await page.goto(fixtureURL("tooltip--tooltip", { delay: 0, visible: true }));
-
-        const element = page.locator("fast-tooltip");
-
-        const anchoredRegion = element.locator("fast-anchored-region");
-
-        await expect(element).toHaveJSProperty("tooltipVisible", true);
-
-        await expect(anchoredRegion).toBeVisible();
+    test.afterAll(async () => {
+        await page.close();
     });
 
-    test("should not render the tooltip when visible is false", async ({ page }) => {
-        await page.goto(fixtureURL("tooltip--tooltip", { delay: 0, visible: false }));
-
-        const element = page.locator("fast-tooltip");
-
-        const anchoredRegion = element.locator("fast-anchored-region");
-
-        await expect(element).toHaveJSProperty("tooltipVisible", false);
-
-        await expect(anchoredRegion).toBeHidden();
-    });
-
-    test("should set anchored region defaults", async ({ page }) => {
-        await page.goto(fixtureURL("tooltip--tooltip"));
-
-        const element = page.locator("fast-tooltip");
-
+    test("should set anchored region defaults", async () => {
         await expect(element).toHaveJSProperty("verticalPositioningMode", "dynamic");
 
         await expect(element).toHaveJSProperty("horizontalPositioningMode", "dynamic");
@@ -59,12 +38,36 @@ test.describe("Tooltip", () => {
         await expect(element).toHaveJSProperty("horizontalScaling", "content");
     });
 
-    test("should set anchored region defaults when `position` is set to `top`", async ({
-        page,
-    }) => {
-        await page.goto(fixtureURL("tooltip--tooltip", { position: "top" }));
+    test("should not render the tooltip by default", async () => {
+        await expect(element).toHaveJSProperty("tooltipVisible", false);
 
-        const element = page.locator("fast-tooltip");
+        await expect(anchoredRegion).toBeHidden();
+    });
+
+    test("should set viewport lock attributes to `undefined` by default", async () => {
+        await expect(element).toHaveJSProperty("verticalViewportLock", undefined);
+
+        await expect(element).toHaveJSProperty("horizontalViewportLock", undefined);
+    });
+
+    test("should render the tooltip when visible is true", async () => {
+        await page.goto(fixtureURL("tooltip--tooltip", { visible: true }));
+
+        await expect(element).toHaveJSProperty("tooltipVisible", true);
+
+        await expect(anchoredRegion).toBeVisible();
+    });
+
+    test("should not render the tooltip when visible is false", async () => {
+        await page.goto(fixtureURL("tooltip--tooltip", { visible: false }));
+
+        await expect(element).toHaveJSProperty("tooltipVisible", false);
+
+        await expect(anchoredRegion).toBeHidden();
+    });
+
+    test("should set anchored region defaults when `position` is set to `top`", async () => {
+        await page.goto(fixtureURL("tooltip--tooltip", { position: "top" }));
 
         await expect(element).toHaveJSProperty(
             "verticalPositioningMode",
@@ -82,12 +85,8 @@ test.describe("Tooltip", () => {
         await expect(element).toHaveJSProperty("horizontalScaling", "content");
     });
 
-    test("should set anchored region defaults when `position` is set to `bottom`", async ({
-        page,
-    }) => {
+    test("should set anchored region defaults when `position` is set to `bottom`", async () => {
         await page.goto(fixtureURL("tooltip--tooltip", { position: "bottom" }));
-
-        const element = page.locator("fast-tooltip");
 
         await expect(element).toHaveJSProperty(
             "verticalPositioningMode",
@@ -105,12 +104,8 @@ test.describe("Tooltip", () => {
         await expect(element).toHaveJSProperty("horizontalScaling", "content");
     });
 
-    test("should set anchored region defaults when `position` is set to `left`", async ({
-        page,
-    }) => {
+    test("should set anchored region defaults when `position` is set to `left`", async () => {
         await page.goto(fixtureURL("tooltip--tooltip", { position: "left" }));
-
-        const element = page.locator("fast-tooltip");
 
         await expect(element).toHaveJSProperty(
             "verticalPositioningMode",
@@ -128,12 +123,8 @@ test.describe("Tooltip", () => {
         await expect(element).toHaveJSProperty("horizontalScaling", "content");
     });
 
-    test("should set anchored region defaults when `position` is set to `right`", async ({
-        page,
-    }) => {
+    test("should set anchored region defaults when `position` is set to `right`", async () => {
         await page.goto(fixtureURL("tooltip--tooltip", { position: "right" }));
-
-        const element = page.locator("fast-tooltip");
 
         await expect(element).toHaveJSProperty(
             "verticalPositioningMode",
@@ -151,24 +142,8 @@ test.describe("Tooltip", () => {
         await expect(element).toHaveJSProperty("horizontalScaling", "content");
     });
 
-    test("should set viewport lock attributes to `undefined` by default", async ({
-        page,
-    }) => {
+    test("should change anchor element when the `anchor` attribute changes", async () => {
         await page.goto(fixtureURL("tooltip--tooltip"));
-
-        const element = page.locator("fast-tooltip");
-
-        await expect(element).toHaveJSProperty("verticalViewportLock", undefined);
-
-        await expect(element).toHaveJSProperty("horizontalViewportLock", undefined);
-    });
-
-    test("should change anchor element when the `anchor` attribute changes", async ({
-        page,
-    }) => {
-        await page.goto(fixtureURL("tooltip--tooltip"));
-
-        const element = page.locator("fast-tooltip");
 
         await page.evaluate(() => {
             const newAnchor = document.createElement("div");
