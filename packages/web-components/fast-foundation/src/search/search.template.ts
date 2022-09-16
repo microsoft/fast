001480718f66f474b8e6e69d3b61dd198f444248
John Kreitlow <863023+radium-v@users.noreply.github.com>
2022-09-16 16:29:31 -0700

    revert unrelated search template changes

--- packages/web-components/fast-foundation/src/search/search.template.ts
+++ packages/web-components/fast-foundation/src/search/search.template.ts
@@ -11,7 +11,11 @@ export function searchTemplate<T extends FASTSearch>(
     options: SearchOptions = {}
 ): ElementViewTemplate<T> {
     return html<T>`
-        <template class="${x => (x.readOnly ? "readonly" : "")}">
+        <template
+            class="
+            ${x => (x.readOnly ? "readonly" : "")}
+        "
+        >
             <label
                 part="label"
                 for="control"
