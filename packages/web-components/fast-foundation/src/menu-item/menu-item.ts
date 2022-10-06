30a9e92c29e4613217bee0588f0c0935777032c8
Ryan Merrill <ryan@ryanmerrill.net>
2022-10-06 02:20:14 -0400

    Fix: Allow Escape key to close menu items (#6428)
    * Change files
    
    * Adds ability to close menu with escape key. Closes #5984
    
    * Changes keyArrowLeft action to use method
    
    * Adds test for Escape close feature
    
    * Update change/@microsoft-fast-foundation-e12ef57f-4ba5-4e76-a9b1-32d898326966.json
    
    Co-authored-by: Chris Holt <chhol@microsoft.com>
    
    * Update change/@microsoft-fast-foundation-e12ef57f-4ba5-4e76-a9b1-32d898326966.json
    
    Co-authored-by: Chris Holt <chhol@microsoft.com>
    
    * Update packages/web-components/fast-foundation/src/menu/menu.pw.spec.ts
    
    Co-authored-by: John Kreitlow <863023+radium-v@users.noreply.github.com>
    
    * Adds break to Switch statement
    
    * Runs yarn prepare
    
    Co-authored-by: Chris Holt <chhol@microsoft.com>
    Co-authored-by: John Kreitlow <863023+radium-v@users.noreply.github.com>

--- packages/web-components/fast-foundation/src/menu-item/menu-item.ts
+++ packages/web-components/fast-foundation/src/menu-item/menu-item.ts
@@ -10,6 +10,7 @@ import {
     keyArrowLeft,
     keyArrowRight,
     keyEnter,
+    keyEscape,
     keySpace,
 } from "@microsoft/fast-web-utilities";
 import type { FASTAnchoredRegion } from "../anchored-region/anchored-region.js";
@@ -221,11 +222,18 @@ export class FASTMenuItem extends FASTElement {
                 this.expandAndFocus();
                 return false;
 
+            case keyEscape:
+                // close submenu
+                if (this.expanded) {
+                    this.closeSubMenu();
+                    return false;
+                }
+                break;
+
             case keyArrowLeft:
                 //close submenu
                 if (this.expanded) {
-                    this.expanded = false;
-                    this.focus();
+                    this.closeSubMenu();
                     return false;
                 }
         }
@@ -283,6 +291,15 @@ export class FASTMenuItem extends FASTElement {
         this.expanded = false;
 
         return false;
+    };
+
+    /**
+     * @internal
+     */
+    private closeSubMenu = (): void => {
+        // close submenu
+        this.expanded = false;
+        this.focus();
     };
 
     /**
