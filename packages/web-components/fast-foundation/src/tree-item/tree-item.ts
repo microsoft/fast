c43b677ac710aab849d86c0afa4507a0cc80107f
Chris Holt <chhol@microsoft.com>
2022-10-12 12:00:39 -0700

    add readonly property to identify tree items and udpate isTreeItemElement check

--- packages/web-components/fast-foundation/src/tree-item/tree-item.ts
+++ packages/web-components/fast-foundation/src/tree-item/tree-item.ts
@@ -15,10 +15,7 @@ import { applyMixins } from "../utilities/apply-mixins.js";
  * determines if element is an HTMLElement and if it has the role treeitem
  */
 export function isTreeItemElement(el: Element): el is HTMLElement {
-    return (
-        isHTMLElement(el) &&
-        (el.getAttribute("role") === "treeitem" || el.tagName.includes("TREE-ITEM"))
-    );
+    return isHTMLElement(el) && el.hasOwnProperty("isTreeItem");
 }
 
 /**
@@ -90,6 +87,13 @@ export class FASTTreeItem extends FASTElement {
      * @internal
      */
     public expandCollapseButton: HTMLDivElement;
+
+    /**
+     *  Readonly property identifying the element as a tree item
+     *
+     * @internal
+     */
+    public readonly isTreeItem: boolean = true;
 
     /**
      * Whether the item is focusable
