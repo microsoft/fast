47b31baa945ab92d4e2abec754ef8416a81f7412
Stephane Comeau <stephcomeau@msn.com>
2022-10-13 19:18:33 -0700

    add hypotenuse

--- packages/web-components/fast-foundation/src/anchored-region/stories/examples/anchored-region-pointer.ts
+++ packages/web-components/fast-foundation/src/anchored-region/stories/examples/anchored-region-pointer.ts
@@ -33,9 +33,20 @@ export class AnchoredRegionPointer extends FASTAnchoredRegion {
         return (Math.atan2(dy, dx) * 180) / Math.PI + 180;
     }
 
-    private updatePosition(e: MouseEvent) {
-        this.pointAnchorX = e.pageX - document.documentElement.scrollLeft;
-        this.pointAnchorY = e.pageY - document.documentElement.scrollTop;
+    public getDistance(
+        anchorRect: DOMRect | undefined,
+        regionRect: DOMRect | undefined
+    ): number {
+        if (!anchorRect || !regionRect) {
+            return 0;
+        }
+
+        const anchorX: number = anchorRect.top + anchorRect.height / 2;
+        const anchorY: number = anchorRect.left + anchorRect.width / 2;
+        const regionX: number = regionRect.top + regionRect.height / 2;
+        const regionY: number = regionRect.left + regionRect.width / 2;
+
+        return Math.hypot(anchorX - regionX, anchorY - regionY);
     }
 }
 
@@ -53,8 +64,12 @@ export function anchoredRegionPointerTemplate<
                 html<T>`
                     <div
                         class="pointer"
-                        style="transform:rotate(${x =>
-                            x.getRotation(x.anchorRect, x.regionRect)}deg);"
+                        style="
+                            transform:rotate(${x =>
+                            x.getRotation(x.anchorRect, x.regionRect)}deg);
+                            opacity:${x =>
+                            (600 - x.getDistance(x.anchorRect, x.regionRect)) / 600};
+                        "
                     >
                         <slot name="pointer"></slot>
                     </div>
@@ -72,7 +87,7 @@ export const anchoredRegionPointerStyles = css`
         box-sizing: border-box;
     }
     .pointer {
-        will-change: transform;
+        will-change: transform, opacity;
         position: absolute;
         transform-origin: 50% 50%;
         height: 100%;
