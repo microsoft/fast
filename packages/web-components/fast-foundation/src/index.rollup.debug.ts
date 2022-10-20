b9f67e9a8a440ccb323e904c7851f1597a049897
Rob Eisenberg <rob@bluespire.com>
2022-10-20 11:23:35 -0400

    feat: add default security policy to all rollup builds

--- packages/web-components/fast-foundation/src/index.rollup.debug.ts
+++ packages/web-components/fast-foundation/src/index.rollup.debug.ts
@@ -1,3 +1,8 @@
+import { DOM } from "@microsoft/fast-element";
 import "@microsoft/fast-element/debug";
+import { DOMPolicy } from "@microsoft/fast-element/dom-policy";
+
 export * from "@microsoft/fast-element";
 export * from "./index.js";
+
+DOM.setPolicy(DOMPolicy.create());
--- packages/web-components/fast-foundation/src/index.rollup.ts
+++ packages/web-components/fast-foundation/src/index.rollup.ts
@@ -1,2 +1,7 @@
+import { DOM } from "@microsoft/fast-element";
+import { DOMPolicy } from "@microsoft/fast-element/dom-policy";
+
 export * from "@microsoft/fast-element";
 export * from "./index.js";
+
+DOM.setPolicy(DOMPolicy.create());
