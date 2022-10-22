0c306b9711ee7716b639b4a6579877d319d7ed6f
John Kreitlow <863023+radium-v@users.noreply.github.com>
2022-10-22 08:07:54 -0700

    rewrite tooltip to use floating-ui

--- packages/web-components/fast-foundation/src/tooltip/tooltip.options.ts
+++ packages/web-components/fast-foundation/src/tooltip/tooltip.options.ts
@@ -1,98 +1,26 @@
 /**
- * Enumerates possible tooltip positions
+ * Enumerates possible tooltip placements.
  *
  * @public
  */
-export const TooltipPosition = {
-    /**
-     * The tooltip is positioned above the element
-     */
-    top: "top",
-
-    /**
-     * The tooltip is positioned to the right of the element
-     */
-    right: "right",
-
-    /**
-     * The tooltip is positioned below the element
-     */
+export const TooltipPlacement = {
     bottom: "bottom",
-
-    /**
-     * The tooltip is positioned to the left of the element
-     */
-    left: "left",
-
-    /**
-     * The tooltip is positioned in the center of the element
-     */
-    center: "center",
-
-    /**
-     * The tooltip is positioned before the element
-     */
-    start: "start",
-
-    /**
-     * The tooltip is positioned after the element
-     */
-    end: "end",
-
-    /**
-     * The tooltip is positioned above the element and to the left
-     */
-    topLeft: "top-left",
-
-    /**
-     * The tooltip is positioned above the element and horizontally centered
-     */
-    topCenter: "top-center",
-
-    /**
-     * The tooltip is positioned above the element and to the right
-     */
-    topRight: "top-right",
-
-    /**
-     * The tooltip is positioned below the element and to the left
-     */
-    bottomLeft: "bottom-left",
-
-    /**
-     * The tooltip is positioned below the element and horizontally centered
-     */
-    bottomCenter: "bottom-center",
-
-    /**
-     * The tooltip is positioned below the element and to the right
-     */
-    bottomRight: "bottom-right",
-
-    /**
-     * The tooltip is positioned above the element and to the left
-     */
-    topStart: "top-start",
-
-    /**
-     * The tooltip is positioned above the element and to the right
-     */
-    topEnd: "top-end",
-
-    /**
-     * The tooltip is positioned below the element and to the left
-     */
-    bottomStart: "bottom-start",
-
-    /**
-     * The tooltip is positioned below the element and to the right
-     */
     bottomEnd: "bottom-end",
+    bottomStart: "bottom-start",
+    left: "left",
+    leftEnd: "left-end",
+    leftStart: "left-start",
+    right: "right",
+    rightEnd: "right-end",
+    rightStart: "right-start",
+    top: "top",
+    topEnd: "top-end",
+    topStart: "top-start",
 } as const;
 
 /**
- * The possible tooltip positions
+ * The possible tooltip placements.
  *
  * @public
  */
-export type TooltipPosition = typeof TooltipPosition[keyof typeof TooltipPosition];
+export type TooltipPlacement = typeof TooltipPlacement[keyof typeof TooltipPlacement];
