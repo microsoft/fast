45c6cf200f4fb1636ff710b0cdaa6bce8c6e049b
Rob Eisenberg <rob@bluespire.com>
2023-02-05 16:07:48 -0500

    fix: correct types, docs on PendingTaskEvent, make API public

--- packages/web-components/fast-element/src/pending-task.ts
+++ packages/web-components/fast-element/src/pending-task.ts
@@ -1,21 +1,37 @@
 /**
  * An implementation of the https://github.com/webcomponents-cg/community-protocols/blob/main/proposals/pending-task.md proposal.
- * @beta
+ * @public
  */
 export interface PendingTask extends Event {
-    complete: Promise<void>;
+    /**
+     * A promise that resolves when the pending task is complete.
+     */
+    readonly complete: Promise<void>;
 }
 
 /**
  * A concrete implementation of {@link PendingTask}
- * @beta
+ * @public
  */
 export class PendingTaskEvent extends Event implements PendingTask {
+    /**
+     * The type of the pending task event.
+     */
     public static readonly type = "pending-task";
-    constructor(public complete: Promise<void>) {
+
+    /**
+     * Creates an instance of PendingTaskEvent.
+     * @param complete - A promise that resolves when the pending task is complete.
+     */
+    public constructor(public readonly complete: Promise<void>) {
         super(PendingTaskEvent.type, { bubbles: true, composed: true });
     }
 
+    /**
+     * Determines whether a value is a PendingTaskEvent.
+     * @param value - The value to check.
+     * @returns True if the value is a PendingTaskEvent; false otherwise.
+     */
     public static isPendingTask<T extends Event>(
         value: T | PendingTask
     ): value is PendingTask {
