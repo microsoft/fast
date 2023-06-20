fdb41ac8f1b1f3c7165481f6e49c0dc108c59d3e
Chris Holt <chhol@microsoft.com>
2023-06-20 13:19:34 -0700

    decouple website from packagaes

--- packages/utilities/adaptive-ui/src/color/utilities/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./color-constants.js";
-export * from "./direction-by-is-dark.js";
-export * from "./is-dark.js";
-export * from "./luminance-swatch.js";
-export * from "./opacity.js";
-export * from "./relative-luminance.js";
--- sites/fast-color-explorer/app/custom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.css" {
-    const content: any;
-    export default content;
-}
-
-declare module "*.svg" {
-    const content: any;
-    export default content;
-}
