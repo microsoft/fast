838533d9b65b0595df32ccb552ec938ed38cbfc5
Chris Holt <chhol@microsoft.com>
2023-09-30 02:34:58 -0700

    update router testing

---
+++ packages/web-components/fast-router/web-test-runner.config.mjs
@@ -0,0 +1,19 @@
+import { playwrightLauncher } from "@web/test-runner-playwright";
+
+export default {
+    concurrency: 10,
+    coverage: true,
+    nodeResolve: true,
+    // in a monorepo you need to set set the root dir to resolve modules
+    rootDir: "../../../",
+    playwright: true,
+    browsers: [
+        playwrightLauncher({ product: "chromium" }),
+        playwrightLauncher({ product: "firefox" }),
+        playwrightLauncher({ product: "webkit" }),
+    ],
+    files: [
+        "dist/esm/**/*.spec.js",
+        "!**/node_modules/**"
+    ],
+};
