577ab9be149e6eb11fcd827250a1db9d1f4a400c
Chris Holt <chhol@microsoft.com>
2023-09-30 00:25:31 -0700

    update fast-foundation deps

--- packages/web-components/fast-foundation/src/design-token/event-strategy.spec.ts
+++ packages/web-components/fast-foundation/src/design-token/event-strategy.spec.ts
@@ -1,7 +1,6 @@
 
 import { css, customElement, FASTElement, HostController, html, Observable, Updates } from "@microsoft/fast-element";
-import chai, { expect } from "chai";
-import spies from "chai-spies";
+import { expect } from "@esm-bundle/chai";
 import { uniqueElementName } from "@microsoft/fast-element/testing.js";
 import {DesignTokenEventResolutionStrategy} from "./event-strategy.js"
 
