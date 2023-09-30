1a52a257e38588f64ac9c4eab5dbc468faa67bc8
Chris Holt <chhol@microsoft.com>
2023-09-30 03:15:38 -0700

    workflow updates for playwright

--- packages/utilities/fast-web-utilities/web-test-runner.config.mjs
+++ packages/utilities/fast-web-utilities/web-test-runner.config.mjs
@@ -6,12 +6,6 @@ export default {
     nodeResolve: true,
     // in a monorepo you need to set set the root dir to resolve modules
     rootDir: "../../../",
-    playwright: true,
-    browsers: [
-        playwrightLauncher({ product: "chromium" }),
-        playwrightLauncher({ product: "firefox" }),
-        playwrightLauncher({ product: "webkit" }),
-    ],
     files: [
         "dist/**/*.spec.js",
         "!**/node_modules/**"
