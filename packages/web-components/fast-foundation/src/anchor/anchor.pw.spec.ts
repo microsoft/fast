c887404c361e6fa41b3af0e4e9909f37578044d0
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-02-11 21:26:02 -0800

    make anchor tests async

--- packages/web-components/fast-foundation/src/anchor/anchor.pw.spec.ts
+++ packages/web-components/fast-foundation/src/anchor/anchor.pw.spec.ts
@@ -1,24 +1,8 @@
 import { spinalCase } from "@microsoft/fast-web-utilities";
 import { expect, test } from "@playwright/test";
-import type { Locator, Page } from "@playwright/test";
 import { fixtureURL } from "../__test__/helpers.js";
 
 test.describe("Anchor", () => {
-    let page: Page;
-    let element: Locator;
-
-    test.beforeAll(async ({ browser }) => {
-        page = await browser.newPage();
-
-        element = page.locator("fast-anchor");
-
-        await page.goto(fixtureURL("anchor", attributes));
-    });
-
-    test.afterAll(async () => {
-        await page.close();
-    });
-
     const attributes = {
         href: "href",
         ping: "ping",
@@ -52,7 +36,13 @@ test.describe("Anchor", () => {
     for (const [attribute, value] of Object.entries(attributes)) {
         const attributeSpinalCase = spinalCase(attribute);
 
-        test(`should set the \`${attributeSpinalCase}\` attribute to \`${value}\` on the internal control`, async () => {
+        test(`should set the \`${attributeSpinalCase}\` attribute to \`${value}\` on the internal control`, async ({
+            page,
+        }) => {
+            const element = page.locator("fast-anchor");
+
+            await page.goto(fixtureURL("anchor", attributes));
+
             await expect(element).toHaveAttribute(attributeSpinalCase, `${value}`);
         });
     }
