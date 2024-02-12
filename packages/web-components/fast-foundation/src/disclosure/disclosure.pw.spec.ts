f069d58d8e0f2fbcbb13c026fab123431e5e829d
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-02-11 23:13:32 -0800

    make disclosure tests async

--- packages/web-components/fast-foundation/src/disclosure/disclosure.pw.spec.ts
+++ packages/web-components/fast-foundation/src/disclosure/disclosure.pw.spec.ts
@@ -1,33 +1,28 @@
 import { expect, test } from "@playwright/test";
-import type { Locator, Page } from "@playwright/test";
 import { fixtureURL } from "../__test__/helpers.js";
 import type { FASTDisclosure } from "./disclosure.js";
 
 test.describe("Disclosure", () => {
     test.describe("States, Attributes, and Properties", () => {
-        let page: Page;
-        let element: Locator;
-        let summary: Locator;
+        test("should set the `aria-controls` attribute on the internal summary element", async ({
+            page,
+        }) => {
+            const element = page.locator("fast-disclosure");
 
-        test.beforeAll(async ({ browser }) => {
-            page = await browser.newPage();
-
-            element = page.locator("fast-disclosure");
-
-            summary = element.locator("summary");
+            const summary = element.locator("summary");
 
             await page.goto(fixtureURL("disclosure--disclosure"));
-        });
 
-        test.afterAll(async () => {
-            await page.close();
-        });
-
-        test("should set the `aria-controls` attribute on the internal summary element", async () => {
             await expect(summary).toHaveAttribute("aria-controls", "disclosure-content");
         });
 
-        test("should toggle the `expanded` attribute based on the value of the `expanded` property", async () => {
+        test("should toggle the `expanded` attribute based on the value of the `expanded` property", async ({
+            page,
+        }) => {
+            const element = page.locator("fast-disclosure");
+
+            await page.goto(fixtureURL("disclosure--disclosure"));
+
             await expect(element).not.toHaveAttribute("expanded");
 
             await element.evaluate((node: FASTDisclosure) => {
@@ -43,7 +38,15 @@ test.describe("Disclosure", () => {
             await expect(element).not.toHaveAttribute("expanded");
         });
 
-        test("should set summary slot content to the value of the summary attribute", async () => {
+        test("should set summary slot content to the value of the summary attribute", async ({
+            page,
+        }) => {
+            const element = page.locator("fast-disclosure");
+
+            const summary = element.locator("summary");
+
+            await page.goto(fixtureURL("disclosure--disclosure"));
+
             const summaryContent =
                 "Should set the summary slot content to the value of the summary attribute";
 
@@ -54,7 +57,13 @@ test.describe("Disclosure", () => {
             await expect(summary).toHaveText(summaryContent);
         });
 
-        test("should toggle the content when the `toggle()` method is invoked", async () => {
+        test("should toggle the content when the `toggle()` method is invoked", async ({
+            page,
+        }) => {
+            const element = page.locator("fast-disclosure");
+
+            await page.goto(fixtureURL("disclosure--disclosure"));
+
             await element.evaluate((node: FASTDisclosure) => {
                 node.toggle();
             });
@@ -68,7 +77,13 @@ test.describe("Disclosure", () => {
             await expect(element).toHaveJSProperty("expanded", false);
         });
 
-        test("should expand and collapse the content when the `show()` and `hide()` methods are invoked", async () => {
+        test("should expand and collapse the content when the `show()` and `hide()` methods are invoked", async ({
+            page,
+        }) => {
+            const element = page.locator("fast-disclosure");
+
+            await page.goto(fixtureURL("disclosure--disclosure"));
+
             await expect(element).toHaveJSProperty("expanded", false);
 
             await element.evaluate((node: FASTDisclosure) => {
@@ -84,7 +99,15 @@ test.describe("Disclosure", () => {
             await expect(element).toHaveJSProperty("expanded", false);
         });
 
-        test("should set the `aria-expanded` attribute on the internal summary element equal to the `expanded` property", async () => {
+        test("should set the `aria-expanded` attribute on the internal summary element equal to the `expanded` property", async ({
+            page,
+        }) => {
+            const element = page.locator("fast-disclosure");
+
+            const summary = element.locator("summary");
+
+            await page.goto(fixtureURL("disclosure--disclosure"));
+
             await expect(summary).toHaveAttribute("aria-expanded", "false");
 
             await element.evaluate((node: FASTDisclosure) => {
