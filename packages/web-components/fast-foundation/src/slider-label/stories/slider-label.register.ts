2ba8729473156176840521f1332e98032b3114b0
Brian Heston <47367562+bheston@users.noreply.github.com>
2024-03-07 13:16:24 -0800

    Foundation: Update Slider templates (#6800)

--- packages/web-components/fast-foundation/src/slider-label/stories/slider-label.register.ts
+++ packages/web-components/fast-foundation/src/slider-label/stories/slider-label.register.ts
@@ -9,26 +9,27 @@ const styles = css`
         color: var(--neutral-foreground-rest);
         fill: currentcolor;
     }
-    .root {
-        position: absolute;
-        display: grid;
-    }
+
     .container {
+        position: absolute;
         display: grid;
         justify-self: center;
     }
-    .label {
+
+    .content {
         justify-self: center;
         align-self: center;
         white-space: nowrap;
         max-width: 30px;
     }
+
     .mark {
         width: calc((var(--design-unit) / 4) * 1px);
         height: calc(var(--height-number) * 0.25 * 1px);
         background: var(--neutral-stroke-rest);
         justify-self: center;
     }
+
     :host([aria-disabled="true"]) {
         opacity: var(--disabled-opacity);
     }
@@ -42,11 +43,13 @@ const horizontalSliderLabelStyles = css`
         height: calc((var(--height-number) / 2 + var(--design-unit)) * 1px);
         width: auto;
     }
+
     .container {
         grid-template-rows: auto auto;
         grid-template-columns: 0;
     }
-    .label {
+
+    .content {
         margin: 2px 0;
     }
 `;
@@ -59,17 +62,20 @@ const verticalSliderLabelStyles = css`
         height: auto;
         width: calc((var(--height-number) / 2 + var(--design-unit)) * 1px);
     }
+
     .container {
         grid-template-columns: auto auto;
         grid-template-rows: 0;
         min-width: calc(var(--thumb-size) * 1px);
         height: calc(var(--thumb-size) * 1px);
     }
+
     .mark {
         transform: rotate(90deg);
         align-self: center;
     }
-    .label {
+
+    .content {
         margin-left: calc((var(--design-unit) / 2) * 3px);
         align-self: center;
     }
