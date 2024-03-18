bdba1697279c6c8e112c2b41067912046be52437
John Kreitlow <863023+radium-v@users.noreply.github.com>
2024-03-17 21:05:34 -0700

    add eslint rules for type imports

---
+++ packages/web-components/fast-foundation/.eslintrc.cjs
@@ -0,0 +1,64 @@
+const path = require("path");
+
+module.exports = {
+    extends: [
+        "@microsoft/eslint-config-fast-dna",
+        "prettier",
+        "plugin:storybook/recommended",
+    ],
+    rules: {
+        "@typescript-eslint/naming-convention": [
+            "error",
+            {
+                selector: "typeLike",
+                format: ["UPPER_CASE", "camelCase", "PascalCase"],
+                leadingUnderscore: "allow",
+            },
+        ],
+        "@typescript-eslint/ban-types": [
+            "error",
+            {
+                types: {
+                    "{}": false,
+                    Function: false,
+                    Object: false,
+                },
+                extendDefaults: true,
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ["**/*.cjs"],
+            env: {
+                commonjs: true,
+                node: true,
+            },
+            rules: {
+                "@typescript-eslint/no-var-requires": "off",
+            },
+        },
+        {
+            files: ["**/*.ts"],
+            excludedFiles: ["**/*.stories.ts", "playwright.config.ts"],
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                project: path.resolve(__dirname, "./tsconfig.json"),
+            },
+            rules: {
+                "no-restricted-imports": [
+                    "error",
+                    {
+                        patterns: ["**/stories/**", "**/*.pw.spec.ts"],
+
+                    },
+                ],
+                "@typescript-eslint/consistent-type-imports": "error",
+                "@typescript-eslint/consistent-type-exports": [
+                    "error",
+                    { fixMixedExportsWithInlineTypeSpecifier: false },
+                ],
+            },
+        },
+    ],
+};
