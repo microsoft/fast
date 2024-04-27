92a2b959c8f58d0cd4021694ac537472dc46baf0
Jane Chu <7559015+janechu@users.noreply.github.com>
2024-04-26 20:53:12 -0700

    Fix docusaurus site by removing frontpage

--- sites/fast-website/src/app/components/content-placement/content-placement.ts
+++
@@ -1,6 +0,0 @@
-import { attr, FASTElement } from "@microsoft/fast-element";
-
-export class ContentPlacement extends FASTElement {
-    @attr({ mode: "boolean" })
-    public icon: boolean = false;
-}
