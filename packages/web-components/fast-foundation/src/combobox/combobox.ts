f8071fb913310eb35dc19da017b554c2608b7f12
Jane Chu <7559015+janechu@users.noreply.github.com>
2024-06-14 13:37:36 -0700

    fix: Various Combobox fixes with tests. (#6145) (updated) (#6986)
    # Pull Request
    
    ## 📖 Description
    
    This PR fixes the tests by converting them to Playwright from #6226. Closes #6226.

--- packages/web-components/fast-foundation/src/combobox/combobox.ts
+++ packages/web-components/fast-foundation/src/combobox/combobox.ts
@@ -357,7 +357,7 @@ export class FASTCombobox extends FormAssociatedCombobox {
      * @internal
      */
     public focusoutHandler(e: FocusEvent): boolean | void {
-        this.updateValue();
+        this.syncValue();
 
         if (!this.open) {
             return true;
@@ -384,7 +384,13 @@ export class FASTCombobox extends FormAssociatedCombobox {
         this.filter = this.control.value;
         this.filterOptions();
 
-        if (e.inputType === "deleteContentBackward" || !this.filter.length) {
+        if (!this.isAutocompleteInline) {
+            this.selectedIndex = this.options
+                .map(option => option.text)
+                .indexOf(this.control.value);
+        }
+
+        if (e.inputType.includes("deleteContent") || !this.filter.length) {
             return true;
         }
 
@@ -392,12 +398,15 @@ export class FASTCombobox extends FormAssociatedCombobox {
             this.open = true;
         }
 
-        if (this.isAutocompleteInline && this.filteredOptions.length) {
-            this.selectedOptions = [this.filteredOptions[0]];
-            this.selectedIndex = this.options.indexOf(this.firstSelectedOption);
-            this.setInlineSelection();
+        if (this.isAutocompleteInline) {
+            if (this.filteredOptions.length) {
+                this.selectedOptions = [this.filteredOptions[0]];
+                this.selectedIndex = this.options.indexOf(this.firstSelectedOption);
+                this.setInlineSelection();
+            } else {
+                this.selectedIndex = -1;
+            }
         }
-
         return;
     }
 
@@ -416,7 +425,7 @@ export class FASTCombobox extends FormAssociatedCombobox {
 
         switch (key) {
             case "Enter": {
-                this.updateValue(true);
+                this.syncValue();
                 if (this.isAutocompleteInline) {
                     this.filter = this.value;
                 }
@@ -444,7 +453,7 @@ export class FASTCombobox extends FormAssociatedCombobox {
             }
 
             case "Tab": {
-                this.updateValue();
+                this.setInputToSelection();
 
                 if (!this.open) {
                     return true;
@@ -469,7 +478,6 @@ export class FASTCombobox extends FormAssociatedCombobox {
                 }
 
                 if (this.isAutocompleteInline) {
-                    this.updateValue();
                     this.setInlineSelection();
                 }
 
@@ -565,21 +573,42 @@ export class FASTCombobox extends FormAssociatedCombobox {
     }
 
     /**
-     * Focus and select the content of the control based on the first selected option.
+     * Focus and set the content of the control based on the first selected option.
      *
-     * @param start - The index for the starting range
+     * @internal
+     */
+    private setInputToSelection(): void {
+        if (this.firstSelectedOption) {
+            this.control.value = this.firstSelectedOption.text;
+            this.control.focus();
+        }
+    }
+
+    /**
+     * Focus, set and select the content of the control based on the first selected option.
+     *
      * @internal
      */
     private setInlineSelection(): void {
         if (this.firstSelectedOption) {
-            this.control.value = this.firstSelectedOption.text;
-            this.control.focus();
+            this.setInputToSelection();
             this.control.setSelectionRange(
                 this.filter.length,
                 this.control.value.length,
                 "backward"
             );
         }
+    }
+
+    /**
+     * Determines if a value update should involve emitting a change event, then updates the value.
+     *
+     * @internal
+     */
+    private syncValue(): void {
+        const newValue =
+            this.selectedIndex > -1 ? this.firstSelectedOption?.text : this.control.value;
+        this.updateValue(this.value !== newValue);
     }
 
     /**
@@ -671,6 +700,7 @@ export class FASTCombobox extends FormAssociatedCombobox {
     private updateValue(shouldEmit?: boolean) {
         if (this.$fastController.isConnected) {
             this.value = this.firstSelectedOption?.text || this.control.value;
+            this.control.value = this.value;
         }
 
         if (shouldEmit) {
