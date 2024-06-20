b5916ea1bd435240584420d8152e8daf3d4608e0
Jane Chu <7559015+janechu@users.noreply.github.com>
2024-06-20 11:34:42 -0700

    Remove starter examples for vite and svelte (#6991)
    # Pull Request
    
    ## 📖 Description
    
    Remove the starter examples as these are difficult to maintain and we are taking a more pared back approach to implementation examples.

--- examples/svelte-starters/svelte-fast-starter/src/main.js
+++
@@ -1,9 +0,0 @@
-import "./app.css";
-import App from "./App.svelte";
-
-const app = new App({
-    // eslint-disable-next-line no-undef
-    target: document.getElementById("app"),
-});
-
-export default app;
