651771223af7cfaba6d91d70aa81b9e3cdccf262
rajsite <rajsite@users.noreply.github.com>
2025-01-03 16:14:22 -0600

    Update top-level config

--- beachball.config.js
+++ beachball.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    disallowedChangeTypes: ["major"],
-    ignorePatterns: [
-        ".ignore",
-        ".github/",
-        ".prettierrc",
-        ".vscode/",
-        "jest..js",
-        "src/e2e/",
-        "src/tests/",
-        "src/fixtures/**",
-        // This one is especially important (otherwise dependabot would be blocked by change file requirements)
-        "yarn.lock",
-    ],
+    changelog: {
+        customRenderers: {
+            // Original template: https://github.com/microsoft/beachball/blob/aefbc1ac37ee85961cc787133c827f1fd3925550/src/changelog/renderPackageChangelog.ts#L93
+            renderEntry: entry => {
+                if (entry.author === 'beachball') {
+                    return `- ${entry.comment}`;
+                  }
+                  // Imitate GitHub's commit format https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/autolinked-references-and-urls#commit-shas
+                  return `- ${entry.comment} ([ni/fast@${entry.commit.substring(0,7)}](https://github.com/ni/fast/commit/${entry.commit}))`;
+            }
+        }
+    }
 };
