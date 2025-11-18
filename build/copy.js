8603a6f1f86db912f4f59b8f65fe466af36a8323
John Kreitlow <863023+radium-v@users.noreply.github.com>
2025-11-18 08:27:55 -0800

    chore: update dependencies (#7215)
    * update packages
    
    * chore: update @microsoft/api-documenter to version 7.28.0
    
    * chore: update rimraf to version 6.1.0
    
    * rewrite copy script to not use copyfiles
    
    * chore: update glob dependency and remove unused copyfiles package
    
    * drop to glob 10.5, add overrides for transitive deps

--- build/copy.js
+++ build/copy.js
@@ -1,20 +1,58 @@
-const copyfiles = require("copyfiles");
+const fs = require("fs");
+const path = require("path");
+const { glob } = require("glob");
 
-function copy(files, destination, config = { flat: true, verbose: true, up: true }) {
-    return new Promise((resolve, reject) => {
-        return copyfiles([...files, destination], config, err =>
-            err ? reject(err.details) : resolve()
-        );
-    }).catch(err => {
+/**
+ * Recursively create directories
+ */
+function ensureDir(dirPath) {
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+    }
+}
+
+/**
+ * Copy a single file
+ */
+function copyFile(src, dest) {
+    ensureDir(path.dirname(dest));
+    fs.copyFileSync(src, dest);
+}
+
+async function copy(files, destination, config = { flat: true, verbose: true, up: true }) {
+    try {
+        ensureDir(destination);
+
+        const allFiles = [];
+        for (const pattern of files) {
+            const matches = await glob(pattern, { nodir: true });
+            allFiles.push(...matches);
+        }
+
+        for (const file of allFiles) {
+            let destPath;
+            if (config.flat) {
+                destPath = path.join(destination, path.basename(file));
+            } else {
+                destPath = path.join(destination, file);
+            }
+
+            copyFile(file, destPath);
+
+            if (config.verbose) {
+                console.log(`Copied ${file} to ${destPath}`);
+            }
+        }
+    } catch (err) {
         console.error(err);
         process.exit(1);
-    });
+    }
 }
 
 /**
  * Copy files from one place to another.
  * @param {string[]} files - Paths and globs to copy
  * @param {string} destination - Directory to copy the files to
- * @param {{ flat?: boolean; verbose?: boolean; up?: boolean; }} config - Configuration options passed to `copyfiles`.
+ * @param {{ flat?: boolean; verbose?: boolean; up?: boolean; }} config - Configuration options.
  */
 module.exports = copy;
