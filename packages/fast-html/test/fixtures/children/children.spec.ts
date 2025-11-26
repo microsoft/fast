f43283cb9bb5e028eab3b3010bfc421cfaec56aa
John Kreitlow <863023+radium-v@users.noreply.github.com>
2025-11-25 18:44:29 -0800

    rewrite children test

--- packages/fast-html/test/fixtures/children/children.spec.ts
+++ packages/fast-html/test/fixtures/children/children.spec.ts
@@ -5,28 +5,20 @@ test.describe("f-template", async () => {
         await page.goto("/fixtures/children/");
 
         const element = page.locator("test-element");
+        const listItems = element.locator("li");
 
         await expect(element).toHaveCount(1);
 
-        const filteredItems = await element.evaluate((el: HTMLElement & { listItems: Node[] }) => {
-            return el.listItems.filter(item => item.nodeType === Node.ELEMENT_NODE);
-        });
+        await expect(listItems).toHaveCount(2);
 
-        expect(filteredItems).toHaveLength(2);
-
-        await expect(element).toHaveText("Foo Bar");
+        await expect(listItems).toHaveText(["Foo", "Bar"]);
 
         await element.evaluate((node: HTMLElement & { list: Array<string> }) => {
             node.list = ["A", "B", "C"];
         });
 
-        expect(await element.evaluate((node: HTMLElement & { listItems: Node[] }) => {
-            const listItems = node.listItems.filter((listItem: Node) => {
-                return listItem instanceof HTMLLIElement;
-            });
-            return listItems?.length;
-        })).toEqual(3);
+        await expect(listItems).toHaveCount(3);
 
-        await expect(element).toHaveText("A B C");
+        await expect(listItems).toHaveText(["A", "B", "C"]);
     });
 });
