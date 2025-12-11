71355144bb9d946266335fa06fdfe7fde58af114
Zacky Ma <machi@microsoft.com>
2025-12-11 12:08:38 -0800

    convert dom-policy tests

--- packages/fast-element/test/main.ts
+++ packages/fast-element/test/main.ts
@@ -1,2 +1,4 @@
 export { Context } from "../src/context.js";
+export { DOM, DOMAspect } from "../src/dom.js";
+export { DOMPolicy } from "../src/dom-policy.js";
 export { uniqueElementName } from "../src/testing/fixture.js";
