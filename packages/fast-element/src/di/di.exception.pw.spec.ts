29b72e24139e1aa3af92ce7d6041f1e115c16fcf
Jane Chu <7559015+janechu@users.noreply.github.com>
2026-02-10 11:13:28 -0800

    Remove page.evaluate wrapper

--- packages/fast-element/src/di/di.exception.pw.spec.ts
+++ packages/fast-element/src/di/di.exception.pw.spec.ts
@@ -1,84 +1,62 @@
 import { expect, test } from "@playwright/test";
+import "../debug";
+import { DI, inject, optional } from "./di.js";
 
 test.describe("DI Exception", () => {
-    test("No registration for interface", async ({ page }) => {
-        await page.goto("/");
+    test("No registration for interface", async () => {
+        const container = DI.createContainer();
 
-        const { throwsOnce, throwsTwice, throwsOnInject } = await page.evaluate(
-            async () => {
-                // @ts-expect-error: Client module.
-                const { DI } = await import("/main.js");
+        const Foo = DI.createContext("Foo");
 
-                const container = DI.createContainer();
+        class Bar {
+            public constructor(public readonly foo: any) {}
+        }
+        inject(...[Foo])(Bar, "Foo", 0);
 
-                const Foo = DI.createContext("Foo");
+        let throwsOnce = false;
+        let throwsTwice = false;
+        let throwsOnInject = false;
 
-                class Bar {
-                    public constructor(public readonly foo: any) {}
-                }
+        try {
+            container.get(Foo);
+        } catch (e: any) {
+            throwsOnce = /.*Foo*/.test(e.message);
+        }
 
-                // Manually set inject property since decorators don't work in evaluate
-                (Bar as any).inject = [Foo];
+        try {
+            container.get(Foo);
+        } catch (e: any) {
+            throwsTwice = /.*Foo*/.test(e.message);
+        }
 
-                let throwsOnce = false;
-                let throwsTwice = false;
-                let throwsOnInject = false;
-
-                try {
-                    container.get(Foo);
-                } catch (e: any) {
-                    throwsOnce = /.*Foo*/.test(e.message);
-                }
-
-                try {
-                    container.get(Foo);
-                } catch (e: any) {
-                    throwsTwice = /.*Foo*/.test(e.message);
-                }
-
-                try {
-                    container.get(Bar);
-                } catch (e: any) {
-                    throwsOnInject = /.*Foo.*/.test(e.message);
-                }
-
-                return { throwsOnce, throwsTwice, throwsOnInject };
-            }
-        );
+        try {
+            container.get(Bar);
+        } catch (e: any) {
+            throwsOnInject = /.*Foo.*/.test(e.message);
+        }
 
         expect(throwsOnce).toBe(true);
         expect(throwsTwice).toBe(true);
         expect(throwsOnInject).toBe(true);
     });
 
-    test("cyclic dependency", async ({ page }) => {
-        await page.goto("/");
+    test("cyclic dependency", async () => {
+        const container = DI.createContainer();
 
-        const throwsCyclic = await page.evaluate(async () => {
-            // @ts-expect-error: Client module.
-            const { DI, optional } = await import("/main.js");
+        const Foo = DI.createContext("IFoo", x => x.singleton(FooImpl));
 
-            const container = DI.createContainer();
+        class FooImpl {
+            public constructor(public parent: any) {}
+        }
+        inject(...[optional(Foo)])(FooImpl, "IFoo", 0);
 
-            const Foo = DI.createContext("IFoo", x => x.singleton(FooImpl));
+        let throwsCyclic = false;
 
-            class FooImpl {
-                public constructor(public parent: any) {}
-            }
-
-            // Manually set inject property with optional decorator behavior
-            (FooImpl as any).inject = [optional(Foo)];
-
-            let throwsCyclic = false;
-
-            try {
-                container.get(Foo);
-            } catch (e: any) {
-                throwsCyclic = /.*Cycl*/.test(e.message);
-            }
-
-            return throwsCyclic;
-        });
+        try {
+            container.get(Foo);
+        } catch (e: any) {
+            throwsCyclic = /.*Cycl*/.test(e.message);
+        }
 
         expect(throwsCyclic).toBe(true);
     });
