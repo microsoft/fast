b72666cda67a40d5e84d1925aff2e6c2cc1e2b54
Jane Chu <7559015+janechu@users.noreply.github.com>
2026-02-11 14:35:13 -0800

    Convert observable tests to Playwright

---
+++ packages/fast-element/src/testing/models.ts
@@ -0,0 +1,63 @@
+import { Observable, observable } from "../observation/observable.js";
+
+class ChildModel {}
+observable(ChildModel.prototype, "value");
+ChildModel.prototype.value = "value";
+
+class Model {
+    childChangedCalled = false;
+
+    childChanged() {
+        this.childChangedCalled = true;
+    }
+
+    incrementTrigger() {
+        this.trigger++;
+    }
+
+    decrementTrigger() {
+        this.trigger--;
+    }
+
+    get ifConditional() {
+        Observable.trackVolatile();
+
+        if (this.trigger < 1) {
+            return 42;
+        }
+
+        return this.value;
+    }
+}
+observable(Model.prototype, "child");
+Model.prototype.child = new ChildModel();
+observable(Model.prototype, "child2");
+Model.prototype.child2 = new ChildModel();
+observable(Model.prototype, "trigger");
+Model.prototype.trigger = 0;
+observable(Model.prototype, "value");
+Model.prototype.value = 10;
+Object.defineProperty(Model.prototype, "ternaryConditional", {
+    get() {
+        Observable.trackVolatile();
+        return this.trigger < 1 ? 42 : this.value;
+    },
+});
+Object.defineProperty(Model.prototype, "andCondition", {
+    get() {
+        Observable.trackVolatile();
+        return this.trigger && this.value;
+    },
+});
+
+class DerivedModel extends Model {
+    child2ChangedCalled = false;
+
+    child2Changed() {
+        this.child2ChangedCalled = true;
+    }
+}
+observable(DerivedModel.prototype, "derivedChild");
+DerivedModel.prototype.derivedChild = new ChildModel();
+
+export { ChildModel, DerivedModel, Model };
