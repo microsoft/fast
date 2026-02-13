f776288c1aa496ce1e9bc50c7a6ecf1d823a1eaa
Jane Chu <7559015+janechu@users.noreply.github.com>
2026-02-13 10:53:03 -0800

    Convert view tests to Playwright

--- packages/fast-element/test/main.ts
+++ packages/fast-element/test/main.ts
@@ -91,6 +91,7 @@ export {
 export { repeat, RepeatBehavior, RepeatDirective } from "../src/templating/repeat.js";
 export { slotted, SlottedDirective } from "../src/templating/slotted.js";
 export { isString } from "../src/interfaces.js";
+export { FAST } from "../src/platform.js";
 export function removeWhitespace(str: string): string {
     return str
         .trim()
