33dd66bb2e4661455cf966cb7623102128059d19
Jane Chu <7559015+janechu@users.noreply.github.com>
2026-02-27 16:08:28 -0800

    Remove devices for now to limit number of times tests are run

--- packages/fast-router/playwright.config.ts
+++ packages/fast-router/playwright.config.ts
@@ -4,23 +4,25 @@ export default defineConfig({
     testDir: ".",
     testMatch: "**/*.pw.spec.ts",
     retries: 3,
-    projects: [
-        {
-            name: "chromium",
-            use: { ...devices["Desktop Chrome"] },
-        },
-        {
-            name: "firefox",
-            use: { ...devices["Desktop Firefox"] },
-        },
-        {
-            name: "webkit",
-            use: { ...devices["Desktop Safari"] },
-        },
-    ],
-    webServer: {
-        command: "npm run test-server",
-        port: 5173,
-        reuseExistingServer: true,
-    },
+    // TODO: uncomment when further testing is added
+    // see: https://github.com/microsoft/fast/pull/7276
+    // projects: [
+    //     {
+    //         name: "chromium",
+    //         use: { ...devices["Desktop Chrome"] },
+    //     },
+    //     {
+    //         name: "firefox",
+    //         use: { ...devices["Desktop Firefox"] },
+    //     },
+    //     {
+    //         name: "webkit",
+    //         use: { ...devices["Desktop Safari"] },
+    //     },
+    // ],
+    // webServer: {
+    //     command: "npm run test-server",
+    //     port: 5173,
+    //     reuseExistingServer: true,
+    // },
 });
