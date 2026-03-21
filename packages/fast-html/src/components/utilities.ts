3936d53f7db39360a991616c2e8dd2045d3919a6
Jane Chu <7559015+janechu@users.noreply.github.com>
2026-03-20 19:29:58 -0700

    Move syntax interface and exports to their own file

--- packages/fast-html/src/components/utilities.ts
+++ packages/fast-html/src/components/utilities.ts
@@ -7,6 +7,19 @@ import {
     refPropertyName,
     Schema,
 } from "./schema.js";
+import {
+    attributeDirectivePrefix,
+    clientSideCloseExpression,
+    clientSideOpenExpression,
+    closeExpression,
+    openExpression,
+    repeatDirectiveClose,
+    repeatDirectiveOpen,
+    unescapedCloseExpression,
+    unescapedOpenExpression,
+    whenDirectiveClose,
+    whenDirectiveOpen,
+} from "./syntax.js";
 
 type BehaviorType = "dataBinding" | "templateDirective";
 
@@ -72,56 +85,10 @@ interface ObservedTargetsAndProperties {
     rootProperty: string;
 }
 
-interface Syntax {
-    clientSideOpenExpression: string;
-    clientSideCloseExpression: string;
-    openExpression: string;
-    closeExpression: string;
-    unescapedOpenExpression: string;
-    unescapedCloseExpression: string;
-    repeatDirectiveOpen: string;
-    repeatDirectiveClose: string;
-    whenDirectiveOpen: string;
-    whenDirectiveClose: string;
-    attributeDirectivePrefix: string;
-}
-
-const {
-    clientSideOpenExpression,
-    clientSideCloseExpression,
-    openExpression,
-    closeExpression,
-    unescapedOpenExpression,
-    unescapedCloseExpression,
-    repeatDirectiveOpen,
-    repeatDirectiveClose,
-    whenDirectiveOpen,
-    whenDirectiveClose,
-    attributeDirectivePrefix,
-}: Syntax = {
-    clientSideOpenExpression: "{",
-    clientSideCloseExpression: "}",
-    openExpression: "{{",
-    closeExpression: "}}",
-    unescapedOpenExpression: "{{{",
-    unescapedCloseExpression: "}}}",
-    repeatDirectiveOpen: "<f-repeat",
-    repeatDirectiveClose: "</f-repeat>",
-    whenDirectiveOpen: "<f-when",
-    whenDirectiveClose: "</f-when>",
-    attributeDirectivePrefix: "f-",
-};
-
-export const contextPrefix: string = "$c";
-
 export const contextPrefixDot: string = `${contextPrefix}.`;
-
 const startInnerHTMLDiv = `<div :innerHTML="{{`;
-
 const startInnerHTMLDivLength = startInnerHTMLDiv.length;
-
 const endInnerHTMLDiv = `}}"></div>`;
-
 const endInnerHTMLDivLength = endInnerHTMLDiv.length;
 
 const LogicalOperator = {
