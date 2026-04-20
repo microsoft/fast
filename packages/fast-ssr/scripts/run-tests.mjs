53583047d0b28db364c2f4136c672f4cf7318335
Jane Chu <7559015+janechu@users.noreply.github.com>
2026-04-20 16:38:53 -0700

    chore: remove @microsoft/fast-ssr

--- packages/fast-ssr/scripts/run-tests.mjs
+++
@@ -1,8 +0,0 @@
-import { globSync } from "node:fs";
-import { execSync } from "node:child_process";
-
-const files = globSync("dist/esm/**/*.spec.js", {
-    exclude: ["dist/esm/**/*.pw.spec.js"],
-});
-
-execSync(`node --test ${files.join(" ")}`, { stdio: "inherit" });
--- packages/fast-ssr/src/event-utilities.ts
+++
@@ -1,3 +0,0 @@
-export function shouldBubble(event: Event) {
-    return event.bubbles && !event.cancelBubble;
-}
